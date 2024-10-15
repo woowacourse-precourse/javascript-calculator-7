6154fff8142fd0e25c7b8ac7637a18c9708bd7cb
sanghyeonLee <hyeon012366@gmail.com>
2024-10-15 21:57:08 +0900

    refactor: 계산 메서드 누적 값 계산시 숫자형으로 형변환

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ class App {
   async run() { //풀이 결과 반환 담당
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
     this.calculator([...input])
+    Console.print(this.sum)
   }
 
   isSeparator(value){//구분자 판단
-    return value === "," || ";" ? 0 : value
+    return value === "," || value === ";" ? 0 : value
   }
 
   calculator(arr){ //계산 담당
     for (const element of arr) {
-      this.sum += this.isSeparator(element)
+      this.sum += Number(this.isSeparator(element))
     }
   }
 
