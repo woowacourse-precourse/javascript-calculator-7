4017fa9c142585b3c53db9714dc4c74e8a3897f6
sebn <sebn1247@gmail.com>
2024-10-15 22:52:40 +0900

    feat: implemented user input process with trimming and sanitization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    Console.print("덧셈할 문자를 입력해 주세요.");
+    const STRING_INPUT = await Console.readLineAsync("");
+    const CLEANED_INPUT = STRING_INPUT.replace(/"/g, "").trim();
+    Console.print(CLEANED_INPUT);
+  }
 }
 
 export default App;
