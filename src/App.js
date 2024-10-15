f4056a801f66fad80f6aa52ad1029edcaf9d29da
sw326 <sungwoo326@gmail.com>
2024-10-15 21:49:09 +0900

    feat: implement basic structure and user input functionality
    - Implement getUserInput method using Console.readLineAsync
    - Use Console.readLineAsync() to receive user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import {Console} from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await this.getUserInput();
+    Console.print(input);
+  }
+  async getUserInput() {
+    return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요: ");
+  }
 }
 
 export default App;
