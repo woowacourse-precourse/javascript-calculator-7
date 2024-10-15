c3abe524c0f68464207738ee946482d664c943e3
@u_limit_ <yurim@ajou.ac.kr>
2024-10-16 01:34:06 +0900

    feat: 쉼표(,) 또는 콜론(;)을 구분자로 지정하는 기능
    refactor: ","와 ":"를 구분자로 지정하는 기능 코드 스타일 변경
    
    - "," 와 ":" 동시에 사용시 제대로 동작하지 않던 문제 해결
    - 디폴트 구분자 변수 추가
    - 계산 부분 모듈화
    
    feat: 계산 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import {Console} from "@woowacourse/mission-utils";
+
+let separator = [",",":"];
+
+function getNumber(input) {
+  const inputArray=input.split("");
+  let currentNum = '';
+  const result=[];
+
+  inputArray.forEach(string=>{
+    if(separator.includes(string)){
+      if(currentNum.length>0){
+        result.push(currentNum);
+        currentNum='';
+      }
+    }else{
+      currentNum += string;
+    }
+  })
+  if(currentNum !== ''){
+    result.push(currentNum);
+  }
+  return result;
+}
+
+function calculate(input){
+  let result = 0;
+  getNumber(input).map((number)=>{
+    result+=Number(number);
+  })
+  return result;
+}
+
 class App {
   async run(
   ) {
-    const result = 0 ;
     let input="";
     Console.print("덧셈할 문자열을 입력해 주세요.");
-    input=await Console.readLineAsync(input)
-    Console.print("결과 : "+result);
+    input=await Console.readLineAsync(input);
+    Console.print("결과 : "+calculate(input));
   }
 }
 
