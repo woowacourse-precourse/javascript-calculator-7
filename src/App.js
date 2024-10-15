3cfc66fef4d3ffa38f115c9ae3052259454ba44e
@u_limit_ <yurim@ajou.ac.kr>
2024-10-16 00:20:07 +0900

    feat: 입출력 메세지 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import {Console} from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run(
+  ) {
+    const result = 0 ;
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    Console.print("결과 : "+result);
+  }
 }
 
 export default App;
