b64d1ae481aba33de5443402600195b9ed7dd96a
Murphybread <nargene@gmail.com>
2024-10-15 23:41:07 +0900

    feat: 커스텀 구분자 함수 추가

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const INPUT_PROMPT = '덧셈할 문자열을 입력해주세요.\n'; 
+const DEFAULT_SEPARATOR = '[,:]';
 
 
 const isPositiveNumber = (arr) => {
     return arr.every(num => Number(num) > 0);
 }
 
+const splitUserInput = (input) => {
+    const match = input.match(/\/\/(.)\\n/);
+    const CUSTOM_SEPARATOR = match[1];
+
+    let userInputArray = undefined;
+    console.log(CUSTOM_SEPARATOR);
+
+    if (CUSTOM_SEPARATOR){
+        const numbersPart = CUSTOM_SEPARATOR ? input.split("\\n")[1] : input;
+        console.log(numbersPart)
+        userInputArray = numbersPart.split(new RegExp(`[${CUSTOM_SEPARATOR}]`));
+        console.log(userInputArray);
+    }
+    else{
+        userInputArray = input.split( new RegExp(DEFAULT_SEPARATOR));
+    }
+
+    return userInputArray ;
+}
+
+
 
 async function getUserNumbers(){
 
     try{
         const userInput = await Console.readLineAsync(INPUT_PROMPT);
-        let userInputArray = userInput.split(/[,:]/);
-        
-        const isValid = isPositiveNumber(userInputArray);
+        const userInputArray = splitUserInput(userInput);
+
         Console.print(userInputArray);
 
+        const isValid = isPositiveNumber(userInputArray);
 
         if(!isValid){
             throw new Error('[ERROR]: 입력한 숫자가 양수가 아닙니다.');
         }
+
+
         return userInputArray.map(Number);
 
     }
