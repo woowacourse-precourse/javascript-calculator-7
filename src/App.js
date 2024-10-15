bf3f0a3077e7a551c523f358150b9972cbea6ddf
eunseopi <asxzqw12@naver.com>
2024-10-15 16:32:33 +0900

    feat: 커스텀 구분자 지원 기능 추가 refactor: calculate 함수에서 delimiter와 newInput 변수 사용으로 코드 정리

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ class App {
   }
     calculate(input) {
       if (input === "") return 0;
-      const NUMBERS = input.split(/[,][:]/).map(Number);
+
+      let delimiter = /[,][;]/;
+      let newInput = input;
+
+      if(input.startsWith("//")){
+        let parts = input.split("\n");
+        if(parts.length < 2){
+          throw new Error("[ERROR] 잘못된 형식의 입력입니다.");
+        }
+
+        let customDelimiter = parts[0].slice(2); // '//' 제거 후 ; 만 남게 된다.
+        if (customDelimiter !== 1) {
+          throw new Error("[ERROR] 커스텀 구분자는 하나의 문자만 됩니다.");
+        }
+        delimiter = new RegExp(`[${customDelimiter}]`);
+        newInput = parts[1];
+      }
+
+      const NUMBERS = newInput.split(delimiter).map(Number);
       return NUMBERS.reduce((a,b) => a+b, 0);
     }
 }
