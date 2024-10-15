33c4bb5103d9fd70982b9804688e0f209e1c413c
장한울 <swingjhw@naver.com>
2024-10-15 17:03:57 +0900

    feat: 입력값 받아오기, 출력 함수구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {}
+  getInput() {
+    return Console.readLineAsync();
+  }
+  print(output) {
+    Console.print(output);
+  }
 }
 
 export default App;
