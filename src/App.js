2e593ef6dc68367a5354312ecb06580fa3029aed
san1313 <skan134679@gmail.com>
2024-10-15 20:36:32 +0900

    feat(processString): 입력 받은 문자열을 처리하고 숫자로 형변환하여 배열에 추가하는 로직 추가
    요구되는 기능을 수행하기 위한 processString 및 기타 함수를 작성하였고, 작성된 함수 목록은 아래와 같습니다. 자세한 기능 등은 Javadoc 주석을 확인 해주십시오.
    
    BREAKING CHANGE: 추가된 함수:
    - processString - 입력 받은 문자열을 처리하여 커스텀 구분자를 배열에 push, 구분자를 이용해 숫자를 분리하여 배열에 저장
    - throwError : 지정된 양식에 따라 에러 throw
    - processCustomSeparator : 커스텀 구분자 처리
    - separateNumbers : 구분자로 문자열을 분리, numbers 배열에 Push
    - isNumber : 숫자로만 이루어진 문자열인지 체크
    - checkValidNumber : 숫자가 0 이하이거나 NaN인지 체크

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.separator = [':', ','];
+    this.numbers = [];
+  }
+
   async run() {
     // 1. 사용자에게 문자열을 입력 받는다.
     const STR = await this.readLine('덧셈할 문자열을 입력해 주세요.\n');
+    // 2. 입력받은 문자열을 처리한다.
+    this.processString(STR);
+    // this.printResult(this.numbers); // 테스트용
+    // 3. 배열의 숫자를 모두 더해 반환한다.
+    // 4. 반환된 숫자를 출력한다.
   }
 
   /**
@@ -13,6 +23,92 @@ class App {
    */
   readLine(message) {
     return Console.readLineAsync(message);
+  }
+
+  /**
+   * 문자열을 처리하여 커스텀 구분자를 this.separator에 push하고
+   * this.separator에 저장된 구분자들을 이용해 숫자들을 분리하여 배열에 저장한다.
+   * @param str
+   */
+  processString(str) {
+    // 커스텀 구분자를 배열에 저장하고 //와 \n을 제거한 문자열을 반환한다.
+    const STRING_AFTER_PROCESS = this.processCustomSeparator(str);
+    this.separateNumbers(STRING_AFTER_PROCESS); // 배열에 있는 모든 구분자를 통해 숫자를 분리해 배열에 저장한다.
+    this.checkValidNumber(); // 숫자가 양수가 아니면 throw
+  }
+
+  /**
+   * Error를 지정된 양식으로 throw 한다.
+   */
+  throwError() {
+    throw new Error('[ERROR] 입력 값이 잘못되었습니다.');
+  }
+
+  /**
+   * 커스텀 구분자를 분리해 this.separator에 push하고
+   * 커스텀 구분자를 선언하는 부분을 제거한 문자열을 return 한다.
+   * @param str
+   * @return {string}
+   * @throws {Error} 구분자가 1글자가 아닌 경우
+   * @example
+   * param = "1//;\n//-\n2"
+   * return = "1;-2"
+   */
+  processCustomSeparator(str) {
+    const REGEX = /\/\/(?<=\/\/)(.?)(?=\\n)\\n/g; // 커스텀 구분자 선언부에서 구분자만 캡처링할 수 있게
+    const result = [...str.matchAll(REGEX)];
+    result.forEach(matches => {
+      if (matches[1].length !== 1) {
+        this.throwError(); // 구분자가 1글자가 아닌 경우 throw. *요구 사항에서 구분자는 "문자"임
+      }
+      this.separator.push(matches[1]);
+    }); // 구분자를 this.speparator에 push
+    return str.replaceAll(REGEX, ''); // return은 커스텀 구분자 선언부를 제거한 문자열
+  }
+
+  /**
+   * 문자열을 this.separator에 있는 구분자를 사용하여 split한다.
+   * split된 배열을 검사하여 양수인지 확인하고 아닌경우 throw한다.
+   * @param str
+   * @throws {Error} 배열에 양수가 아닌 문자열이 포함된 경우
+   */
+  separateNumbers(str) {
+    const REGEX = new RegExp(`[${this.separator.join('')}]`, 'g');
+    const SPLITTED_STR = str.split(REGEX);
+    SPLITTED_STR.forEach(string => {
+      if (!this.isNumber(string)) {
+        this.throwError();
+      }
+      this.numbers.push(Number.parseInt(string, 10));
+    });
+  }
+
+  /**
+   * 숫자로만 이루어진 문자열인지 확인한다.
+   * @param str
+   * @return {boolean} 숫자로만 이루어진 문자열인 경우 true
+   */
+  isNumber(str) {
+    const REGEX = /[^-+0-9]/;
+    return !REGEX.test(str);
+  }
+
+  /**
+   * this.numbers의 숫자들이 허용 범위(양수) 안에 있는지 확인한다.
+   * @throws {Error} 숫자가 양수가 아닐 때
+   */
+  checkValidNumber() {
+    this.numbers.forEach(number => {
+      if (number <= 0 || !number) this.throwError(); // 양수가 아니거나 NaN인 경우 throw
+    })
+  }
+
+  /**
+   * message를 콘솔에 출력한다.
+   * @param message
+   */
+  printResult(message) {
+    Console.print(message);
   }
 }
 
