68a65659d39fde8242a41ecf27100dfcb4aa300d
장정안 <wjddks96@naver.com>
2024-10-16 00:35:31 +0900

    feat: 유틸함수 go 추가

---
+++ src/util/Util.js
@@ -0,0 +1,14 @@
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+
+const go = (...args) => reduce((a, f) => f(a), args);
+
+export { reduce, go };
