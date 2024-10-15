727a4760839661327d2c715f1c421e16d1375b8d
Juyudang <ssuw223@gmail.com>
2024-10-16 00:52:08 +0900

    docs(guide): updated README.md

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('닉네임을 입력해주세요.')
+    Console.print(username)
+  }
 }
 
 export default App;
