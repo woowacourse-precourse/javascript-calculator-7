01c50963e6dd690bcf200911b87af1349cda0082
ChaDongmin <cdm1263@gmail.com>
2024-10-16 06:14:17 +0900

    fix(App): 커스텀 구분자 양쪽 특수문자 미제거 오류 해결
    - seperators 배열에 양쪽 특수문자가 포함된 커스텀 구분자 추가

--- src/App.js
+++ src/App.js
@@ -43,7 +43,10 @@ class App {
         throw new Error("[ERROR] 커스텀 구분자의 길이는 0일 수 없습니다.");
       }
 
-      this.seperators.unshift(customSeperator);
+      this.seperators.unshift(
+        CUSTOM_SEPERATOR_START + customSeperator + CUSTOM_SEPERATOR_END,
+        customSeperator
+      );
     }
   }
 
