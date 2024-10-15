e211c6444a8dc725d40839359697a9bbbcbc3d3f
Seio924 <seio9241@gmail.com>
2024-10-15 18:03:04 +0900

    feat(App, utils): 입출력 기능 추가 및 사용자 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { printOuput, readInput } from "./utils.js";
+
 class App {
-  async run() {}
+  async run() {
+    const inputValue = await readInput("덧셈할 문자열을 입력해 주세요.\n");
+    printOuput(inputValue);
+  }
 }
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export async function readInput(string) {
+  let inputValue = "";
+
+  try {
+    inputValue = await MissionUtils.Console.readLineAsync(string);
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  return inputValue;
+}
+
+export async function printOuput(string) {
+  try {
+    MissionUtils.Console.print(string);
+  } catch (err) {
+    throw new Error(err);
+  }
+}
