d6df0859970eaa9d2926eda130fef0e1e9eca271
jakejun98 <111010564+jakejun98@users.noreply.github.com>
2024-10-15 22:29:19 +0900

    Feat: "빈 문자열 입력시 0출력"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from '@woowacourse/mission-utils'; 
+
 class App {
-  async run() {}
+  async run() {
+    try {
+
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요."); 
+
+      const result = this.calculator(input); 
+      Console.print(`결과 : ${result}`); 
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`); 
+    }
+  }
+
+  
+  calculator(input){
+    if(input.length===0){
+      return 0;
+    }
+  }
+
+
 }
+
 
 export default App;
