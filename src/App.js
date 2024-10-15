9715f889b1727ebc8fc858b854eb5235fd270c51
kim jihwan <kjhwan0802@naver.com>
2024-10-15 19:00:39 +0900

    feat: 커스텀 구분자 인식

--- src/App.js
+++ src/App.js
@@ -6,20 +6,45 @@ class App {
       "덧셈할 문자열을 입력해 주세요\n"
     );
 
-    const separatedInputArray = inputString.split(/[,:]/g);
+    if (
+      inputString.slice(0, 5).startsWith("//") &&
+      inputString.slice(0, 5).endsWith("\\n")
+    ) {
+      const customSeparator = inputString[2];
 
-    const answerNumber = separatedInputArray.reduce((acc, cur) => {
-      if (Number(cur) === NaN) {
-        throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
-      }
-      if (Number(cur) < 0) {
-        throw new Error("[ERROR] 음수를 입력했습니다");
-      }
+      let separatedInputArray = inputString.slice(5).split(/[,:]/g);
+      separatedInputArray = separatedInputArray
+        .map((str) => str.split(customSeparator))
+        .flat();
 
-      return acc + Number(cur);
-    }, 0);
+      const answerNumber = separatedInputArray.reduce((acc, cur) => {
+        if (Number(cur) === NaN) {
+          throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
+        }
+        if (Number(cur) < 0) {
+          throw new Error("[ERROR] 음수를 입력했습니다");
+        }
 
-    Console.print(`결과 : ${answerNumber}`);
+        return acc + Number(cur);
+      }, 0);
+
+      Console.print(`결과 : ${answerNumber}`);
+    } else {
+      const separatedInputArray = inputString.split(/[,:]/g);
+
+      const answerNumber = separatedInputArray.reduce((acc, cur) => {
+        if (Number(cur) === NaN) {
+          throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
+        }
+        if (Number(cur) < 0) {
+          throw new Error("[ERROR] 음수를 입력했습니다");
+        }
+
+        return acc + Number(cur);
+      }, 0);
+
+      Console.print(`결과 : ${answerNumber}`);
+    }
   }
 }
 
