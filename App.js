a8e13ec5d1ad6bb6b4a0b8f1eec69f7eadb2b331
Murphybread <nargene@gmail.com>
2024-10-15 18:58:53 +0900

    feat: 양수 판별 함수 추가

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const INPUT_PROMPT = '덧셈할 문자열을 입력해주세요.\n'; 
+
+
+const isPositiveNumber = (arr) => {
+    return arr.every(num => Number(num) > 0);
+}
+
+
+
