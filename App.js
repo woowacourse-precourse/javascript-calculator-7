55b4ceba5ecc4e4c6e5b984ea6aca541f89b759d
Murphybread <nargene@gmail.com>
2024-10-16 03:01:27 +0900

    feat: 오류 처리 기능 추가

--- App.js
+++ App.js
@@ -3,24 +3,36 @@ import { Console } from "@woowacourse/mission-utils";
 const INPUT_PROMPT = '덧셈할 문자열을 입력해주세요.\n'; 
 const OUTPUT_PROMPT = '결과 : ';
 const DEFAULT_SEPARATOR = '[,:]';
+const CUSTOM_SEPARATOR = '//(.)\\\\n';
+
+
+const isValidSeparator = (input) => {
+    
+    const customSeparatorMatch  = input.match(new RegExp(CUSTOM_SEPARATOR));
+    if (!customSeparatorMatch  && !input.match(DEFAULT_SEPARATOR)) {
+        throw new Error('[ERROR]: 구분자가 없거나 잘못됐습니다.');
+    }
+};
 
 const isPositiveNumber = (arr) => {
-    return arr.every(num => Number(num) > 0);
-}
+    if (!arr.every(num => Number(num) > 0)) {
+        throw new Error('[ERROR]: 입력한 숫자가 양수가 아닙니다.');
+    }
+};
 
 const getSum = (arr) => {
     return arr.reduce( (acc,cur) => acc + cur,  0);
 }
 
 const splitUserInput = (input) => {
-    const match = input.match(/\/\/(.)\\n/);
-    const CUSTOM_SEPARATOR = match ? match[1] : null ;
+    const customSeparatorMatch = input.match(new RegExp(CUSTOM_SEPARATOR));
+    const customToken = customSeparatorMatch ? customSeparatorMatch[1] : null ;
 
-    let userInputArray = undefined;
+    let userInputArray;
 
-    if (CUSTOM_SEPARATOR){
-        const numbersPart = CUSTOM_SEPARATOR ? input.split("\\n")[1] : input;
-        userInputArray = numbersPart.split(new RegExp(`[${CUSTOM_SEPARATOR}]`));
+    if (customToken){
+        const numbersPart = input.split("\\n")[1];
+        userInputArray = numbersPart.split(new RegExp(`[${customToken}]`));
     }
     else{
         userInputArray = input.split( new RegExp(DEFAULT_SEPARATOR));
@@ -35,11 +47,10 @@ async function getUserNumbers(){
         const userInput = await Console.readLineAsync(INPUT_PROMPT);
         const userInputArray = splitUserInput(userInput);
 
-        const isValid = isPositiveNumber(userInputArray);
 
-        if(!isValid){
-            throw new Error('[ERROR]: 입력한 숫자가 양수가 아닙니다.');
-        }
+        // 검증 블록
+        isValidSeparator(userInput);
+        isPositiveNumber(userInputArray);
 
         return userInputArray.map(Number);
 
