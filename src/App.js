f8ba1f833827c4da4df2b22fb8602a4c7647a447
sanghyeonLee <hyeon012366@gmail.com>
2024-10-15 23:05:48 +0900

    feat: 커스텀 구분자 판단 메서드 추가 및 커스텀 구분자 필드 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import {Console} from "@woowacourse/mission-utils";
 
 class App {
   sum = 0
+  customSeparator = ""
   async run() { //풀이 결과 반환 담당
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+
+    this.isCustom(input)
     this.calculator([...input])
     this.resultOutput()
+  }
+
+  isCustom(value){
+    const startIdx = value.indexOf("//")+2
+    const lastIdx = value.indexOf("\\n")
+    this.customSeparator = value.slice(startIdx,lastIdx)
+    return value.slice(lastIdx+2)
   }
 
   isSeparator(value){//구분자 판단
