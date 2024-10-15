c73d56917ffcdb645209e77109f79cc8a40b1bfc
장정안 <wjddks96@naver.com>
2024-10-15 22:33:07 +0900

    feat: 유저 입력 값 덧셈 로직 추가

--- src/controller/Calculator.js
+++ src/controller/Calculator.js
@@ -1,5 +1,8 @@
+// @ts-check
 import { Console } from '@woowacourse/mission-utils';
 import User from '../user/User.js';
+import { GAME_MESSAGE } from '../constants/gameMessages.js';
+import { delimiter } from '../constants/delimiter.js';
 
 //덧셈기 게임 관리
 class Calculator {
@@ -7,10 +10,21 @@ class Calculator {
     this.user = new User();
   }
 
-  start() {
-    this.user.readAnswer();
+  async start() {
+    const input = await this.user.readAnswer();
+    this.calculate(input);
   }
 
+  /**@param {string} input */
+  calculate(input) {
+    //실제 계산 로직
+    const result = input
+      .split(delimiter)
+      .map(Number)
+      .reduce((acc, cur) => acc + cur);
+  }
+
+  /**@param {string} message */
   printMessage(message) {
     Console.print(message);
   }
