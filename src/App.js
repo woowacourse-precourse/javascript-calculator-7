3a3b65cc58bd35ec3084a8e6d3bed1bc78658d44
eunseopi <asxzqw12@naver.com>
2024-10-15 15:56:02 +0900

    feat: 빈 문자열 입력 시 0 반환 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
 
   }
     calculate(input) {
+      if (input === "") return 0;
       const NUMBERS = input.split(/[,][:]/).map(Number);
       return NUMBERS.reduce((a,b) => a+b, 0);
     }
