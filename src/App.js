d0f50d7d188c68430bf4e2a3bd1a8ae54e158ebb
minjeong9919 <minjeong9919@naver.com>
2024-10-16 03:28:03 +0900

    feat(sum-calculation): 분리된 숫자들의 합 계산 및 출력
    split을 이용해 '!(느낌표)' 로 분리된 숫자들의 문자열을 분리합니다.
    분리된 숫자들의 배열을 reduce 함수를 이용해 숫자들의 합을 계산합니다.
    숫자 외의 문자열이 섞여있을 경우 0을 더합니다.

--- src/App.js
+++ src/App.js
@@ -12,15 +12,23 @@ class App {
       };
 
       const input = await Console.readLineAsync(
-        "덧셈할 문자열을 입력해 주세요."
+        "덧셈할 문자열을 입력해 주세요.\n"
       );
 
       const regexString = checkHasCustom(input)
         ? `${checkHasCustom(input)}|//|\\\\n`
         : ",|:";
       const regex = new RegExp(regexString, "g");
+      const numbersWithMark = input.replace(regex, "!");
 
-      console.log(input.replace(regex, "!"));
+      const sum = numbersWithMark
+        .split("!")
+        .map(Number)
+        .reduce((acc, cur) => {
+          return acc + (isNaN(cur) ? 0 : cur);
+        }, 0);
+
+      Console.print("결과 : " + sum);
     } catch (error) {
       console.error(error);
     }
