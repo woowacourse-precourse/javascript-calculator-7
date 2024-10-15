e3c9c2b361408449b525bf31c52ebe688d55ebdd
jinoo <jinoo0306@naver.com>
2024-10-15 17:55:06 +0900

    feat: 음수 입력 시 에러 처리 기능 추가 (#4)

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App {
       return 0;
     }
     // 3번 기능: 커스텀 구분자가 있는지 확인
-    let numbers;
+    let numbers = [];
     const customSeparatorFormat = input.match(/^\/\/(.)\\n(.*)/); // 커스텀 구분자가 있는지 match 메서드로 확인
 
     if (customSeparatorFormat) {
@@ -30,6 +30,12 @@ class App {
     } else {
       // 2번 기능: 쉼표(,) 또는 콜론(:)을 구분자로 분리하여 숫자를 더함
       numbers = input.split(/[,|:]/).map(Number); // 쉼표 또는 콜론을 기준으로 분리 후 숫자들을 numbers 배열로 변환
+    }
+
+    // 4번 기능: 음수가 포함된 경우 에러를 발생
+    const negatives = numbers.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error("[ERROR] 음수는 입력할 수 없습니다");
     }
 
     return numbers.reduce((acc, current) => acc + current, 0); // reduce 메서드를 이용해 숫자 합산
