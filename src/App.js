265467526bfd441d78fa666f127da9e3c1020189
guesung <gueit214@naver.com>
2024-10-15 16:35:18 +0900

    feat: 입력된 문자열로 커스텀 문자열 사용

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const userInputString = await this.getUserInputString();
+    const separatorString = this.getSeperatorString(userInputString);
   }
   async getUserInputString() {
     return await Console.readLineAsync("문자열을 입력해주세요.");
+  }
+  getSeperatorString(str) {
+    const seperatorRegExr = /\/\/.\\n/;
+    if (seperatorRegExr.test(str.slice(0, 5))) return str.at(2);
   }
 }
 
