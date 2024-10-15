4894d6724e366e04942302ebe5da97297d2b01c1
guesung <gueit214@naver.com>
2024-10-15 16:29:06 +0900

    feat: 사용자의 입력 전달받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInputString = await this.getUserInputString();
+  }
+  async getUserInputString() {
+    return await Console.readLineAsync("문자열을 입력해주세요.");
+  }
 }
 
 export default App;
