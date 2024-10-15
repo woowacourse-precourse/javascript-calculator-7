56d3816fa858938830783082a613317d7f77e0a6
bbjbc <jobc4550@gmail.com>
2024-10-15 22:21:01 +0900

    feat: 계산을 도와줄 StringCalculator 클래스 생성

---
+++ src/StringCalculator.js
@@ -0,0 +1,21 @@
+class StringCalculator {
+  // 입력값을 받아 최종 결과를 반환하는 메서드
+  calculate(input) {}
+
+  // 문자열이 비어있는지 확인하는 메서드
+  isEmpty(input) {}
+
+  // 문자열을 숫자로 변환하는 메서드
+  parseNumbers(input) {}
+
+  // 숫자 배열을 받아 합을 구하는 메서드
+  calculateSum(numbers) {}
+
+  // 커스텀 구분자 파싱 메서드
+  parseDelimiter(input) {}
+
+  // 입력값 유효성 검사 메서드
+  validateInput(input) {}
+}
+
+export default StringCalculator;
