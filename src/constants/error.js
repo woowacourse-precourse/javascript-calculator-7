bee90d1163cac8ec8a6fc876e2abe4351a3f0eac
RohSungKyun <songkyun1108@naver.com>
2024-10-15 18:05:32 +0900

    feat : 상수 따로 분리

---
+++ src/constants/error.js
@@ -0,0 +1,5 @@
+export const ERROR = {
+    INPUT_BLANK_ERROR : "[ERROR] 값을 입력해 주세요.",
+    INPUT_NUMBER_TYPE : "[ERROR] 양수로 이루어진 문자열을 입력해 주세요.",
+    INPUT_DELIMITER_TYPE : "[ERROR] 쉼표나 콜론으로 구분된 문자열을 입력해 주세요",
+}
