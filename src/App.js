7f4175e6a887f94ad830dc51f2893c610bbf59f2
sanghyeonLee <hyeon012366@gmail.com>
2024-10-15 20:01:39 +0900

    feat: 문자열 입력 메소드, 입력 값을 저장하는 필드 값 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import {Console} from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  inputValue = ""
+  async run() {
+    this.inputValue = this.input()
+
+  }
+  input(){
+    return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+  }
+
 }
 
 export default App;
