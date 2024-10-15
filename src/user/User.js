cfc859dd4f7c635d1ff11bc9dfbf41877d666aac
장정안 <wjddks96@naver.com>
2024-10-15 22:30:25 +0900

    feat: user가 입력한 숫자 유효성 검증 연결

--- src/user/User.js
+++ src/user/User.js
@@ -1,10 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 import { GAME_MESSAGE } from '../constants/gameMessages.js';
+import Validation from '../validation/Validation.js';
 
 class User {
   async readAnswer() {
     //유저가 입력한 숫자 받기
-    const answer = await Console.readLineAsync(GAME_MESSAGE.START);
+    const input = await Console.readLineAsync(GAME_MESSAGE.START);
+    return Validation.validateInput(input);
   }
 }
 
