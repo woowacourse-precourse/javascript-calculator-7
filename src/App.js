e38e32c450b4f6816a622b3103ccea5a59a6e81e
guesung <gueit214@naver.com>
2024-10-15 17:36:45 +0900

    fix: 오타 수정(separator)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const userInput = await this.getUserInput();
-    const separator = this.getSeperator(userInput);
+    const separator = this.getSeparator(userInput);
   }
   async getUserInput() {
     return await Console.readLineAsync("문자열을 입력해주세요.");
   }
-  getSeperator(str) {
-    const seperatorRegExr = /\/\/.\\n/;
-    if (seperatorRegExr.test(str.slice(0, 5))) return str.at(2);
+  getSeparator(str) {
+    const separatorRegExr = /\/\/.\\n/;
+    if (separatorRegExr.test(str.slice(0, 5))) return str.at(2);
   }
 }
 
