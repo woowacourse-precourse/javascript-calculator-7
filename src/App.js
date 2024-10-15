2f018aecf053115c23a4e498ad3d9a8587c7c402
guesung <gueit214@naver.com>
2024-10-15 19:14:42 +0900

    feat: 구분자 문자열을 정규표현식으로 변환

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ class App {
   async run() {
     const userInput = await this.getUserInput();
     const separator = this.getSeparator(userInput);
+    const separatorRegExp = this.getSeparatorRegExp(separator);
 
     let separatedUserInput;
     if (this.customSeparatorRegExr.test(userInput))
       separatedUserInput = this.getSeparatedString(
         userInput.split(this.customSeparatorRegExr)[1],
-        separator
+        separatorRegExp
       );
-    else separatedUserInput = this.getSeparatedString(userInput, separator);
+    else
+      separatedUserInput = this.getSeparatedString(userInput, separatorRegExp);
 
     const separatedUserInputsNumberArray = separatedUserInput.map((it) =>
       Number(it)
@@ -35,6 +37,9 @@ class App {
       return customSeparatorMatchedString[0].slice(2, -2);
 
     return this.defaultSeparator;
+  }
+  getSeparatorRegExp(separator) {
+    return new RegExp(`[${separator}]`);
   }
   getSeparatedString(str, separator) {
     return str.split(separator);
