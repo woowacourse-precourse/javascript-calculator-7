65f6d623fc202412b396220e9aa744b13c0b809d
guesung <gueit214@naver.com>
2024-10-15 16:46:48 +0900

    refactor: 변수 및 함수명에서 이미 의미를 드러내기에 `-String` 제거

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const userInputString = await this.getUserInputString();
-    const separatorString = this.getSeperatorString(userInputString);
+    const userInput = await this.getUserInput();
+    const separator = this.getSeperator(userInput);
   }
-  async getUserInputString() {
+  async getUserInput() {
     return await Console.readLineAsync("문자열을 입력해주세요.");
   }
-  getSeperatorString(str) {
+  getSeperator(str) {
     const seperatorRegExr = /\/\/.\\n/;
     if (seperatorRegExr.test(str.slice(0, 5))) return str.at(2);
   }
