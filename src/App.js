04d70620d862cc500e9dd4e75e813572f1102d63
kim jihwan <kjhwan0802@naver.com>
2024-10-15 17:23:56 +0900

    feat: 사용자 입출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputString = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요\n"
+    );
+
+    Console.print(`결과 : ${inputString}`);
+  }
 }
 
 export default App;
