475eafe4be202b5ba94bffadc23bd41d35bf1c62
sanghyeonLee <hyeon012366@gmail.com>
2024-10-15 22:03:21 +0900

    feat: 결과값 출력 메소드 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ class App {
   async run() { //풀이 결과 반환 담당
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
     this.calculator([...input])
-    Console.print(this.sum)
+    this.resultOutput()
   }
 
   isSeparator(value){//구분자 판단
     return value === "," || value === ";" ? 0 : value
+  }
+
+  resultOutput(){
+    Console.print(`결과 : ${this.sum}`)
   }
 
   calculator(arr){ //계산 담당
