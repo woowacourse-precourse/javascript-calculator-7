ad58ab7c8b42b4f0113665bc5143c353061858f3
unknown <kevin123488@naver.com>
2024-10-15 23:00:35 +0900

    feat(input): add input function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 입력 기능 구현
+    // 1. 덧셈할 문자열을 입력해 주세요. 문구를 띄워주자
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.")
+    const STRING_INPUT = await MissionUtils.Console.readLineAsync('')
+    // MissionUtils.Console.print(`입력값: ${STRING_INPUT}`) // 입력값 체크
+    
+  }
 }
 
 export default App;
