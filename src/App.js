a4a787e34575cb4387b253baaefb399f5779d83a
geun99 <lich1115@naver.com>
2024-10-15 23:11:37 +0900

    feat 1번 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const INPUT_MESSAGE = Console.readLineAsync();
+    let delimiter = /[,:]/;
+
+    Console.print(
+      INPUT_MESSAGE.split(delimiter)
+        .map(Number)
+        .reduce((a, c) => a + c, 0)
+    );
+  }
 }
 
 export default App;
