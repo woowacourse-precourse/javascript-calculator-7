60443a8334578e9bdee8bd52ab6dfb0850eb6d1b
KIMJINYOUNG <k5444862@naver.com>
2024-10-15 22:08:52 +0900

    사용자 입력값 받기 및 print

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  async run() {}
+  async run() {
+    const userInput = await this.getUserInput();
+    Console.print(userInput);
+  }
+
+  async getUserInput() {
+    return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+  }
 }
 
 export default App;
