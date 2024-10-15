1fef85faf721c1350eca20b83b11119f152226c8
minjeongss <dotoriido@gmail.com>
2024-10-15 23:50:39 +0900

    feat(function): 기본 구분자로 가지는 문자열을 구분자와 숫자로 분리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  #plusString;
+  #plusNumberArray;
+  #plusResult;
+
+  constructor() {
+    this.#plusString = "";
+    this.#plusNumberArray = [];
+  }
+
   async generatePlusString() {
     const plusString = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    return plusString;
+    this.#plusString = plusString;
   }
+
+  isContainBasicSeparator(partString) {
+    if (partString === "," || partString === ":") {
+      return true;
+    }
+    return false;
+  }
+
+  pushPlusNumberArray(tmpPlusNumber) {
+    this.#plusNumberArray.push(Number(tmpPlusNumber));
+  }
+
+  splitPlusString() {
+    let tmpPlusNumber = "";
+    for (let i = 0; i < this.#plusString.length; i++) {
+      if (this.isContainBasicSeparator(this.#plusString[i])) {
+        this.pushPlusNumberArray(tmpPlusNumber);
+        tmpPlusNumber = "";
+        continue;
+      }
+      tmpPlusNumber += this.#plusString[i];
+      if (i === this.#plusString.length - 1) {
+        this.pushPlusNumberArray(tmpPlusNumber);
+      }
+    }
+  }
+
   async run() {
     await this.generatePlusString();
+    this.splitPlusString();
+    Console.print(this.#plusNumberArray);
   }
 }
 
