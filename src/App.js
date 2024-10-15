b3859b0390b2114f36d78a710225f7b76ed02b6d
jinoo <jinoo0306@naver.com>
2024-10-15 17:46:27 +0900

    feat : 커스텀 구분자를 사용하여 숫자 더하기 기능 추가 (#3)

--- src/App.js
+++ src/App.js
@@ -20,9 +20,18 @@ class App {
     if (input === "") {
       return 0;
     }
+    // 3번 기능: 커스텀 구분자가 있는지 확인
+    let numbers;
+    const customSeparatorFormat = input.match(/^\/\/(.)\\n(.*)/); // 커스텀 구분자가 있는지 match 메서드로 확인
 
-    // 2번 기능: 쉼표(,) 또는 콜론(:)을 구분자로 분리하여 숫자를 더함
-    const numbers = input.split(/[,|:]/).map(Number); // 쉼표 또는 콜론을 기준으로 분리 후 숫자들을 numbers 배열로 변환
+    if (customSeparatorFormat) {
+      const customDelimiter = customSeparatorFormat[1]; // 커스텀 구분자를 추출
+      numbers = customSeparatorFormat[2].split(customDelimiter).map(Number); // 커스텀 구분자로 숫자를 분리
+    } else {
+      // 2번 기능: 쉼표(,) 또는 콜론(:)을 구분자로 분리하여 숫자를 더함
+      numbers = input.split(/[,|:]/).map(Number); // 쉼표 또는 콜론을 기준으로 분리 후 숫자들을 numbers 배열로 변환
+    }
+
     return numbers.reduce((acc, current) => acc + current, 0); // reduce 메서드를 이용해 숫자 합산
   }
 }
