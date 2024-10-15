90ec92de2813df75ac63fadf3c1b5b2846e51224
kim jihwan <kjhwan0802@naver.com>
2024-10-15 17:38:03 +0900

    feat: 기본 구분자 [,:] 인식

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ class App {
       "덧셈할 문자열을 입력해 주세요\n"
     );
 
-    Console.print(`결과 : ${inputString}`);
+    const separatedInputArray = inputString.split(/[,:]/g);
+
+    const answerNumber = separatedInputArray.reduce((acc, cur) => {
+      if (Number(cur) === NaN) {
+        throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
+      }
+      if (Number(cur) < 0) {
+        throw new Error("[ERROR] 음수를 입력했습니다");
+      }
+
+      return acc + Number(cur);
+    }, 0);
+
+    Console.print(`결과 : ${answerNumber}`);
   }
 }
 
