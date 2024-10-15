077299cbf5cedcc4f98cdd5520bc019dcfadecca
kim jihwan <kjhwan0802@naver.com>
2024-10-15 20:58:24 +0900

    refactor: run() 안에서 여러 일을 하던 것을 함수로 분리 - stringSplitSumCalc() - validateNumber()

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const inputString = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요\n"
+      "덧셈할 문자열을 입력해 주세요.\n"
     );
 
     if (
@@ -12,39 +12,41 @@ class App {
     ) {
       const customSeparator = inputString[2];
 
-      let separatedInputArray = inputString.slice(5).split(/[,:]/g);
-      separatedInputArray = separatedInputArray
+      Console.print(
+        `결과 : ${this.stringSplitSumCalc(
+          inputString.slice(5),
+          customSeparator
+        )}`
+      );
+    } else {
+      Console.print(`결과 : ${this.stringSplitSumCalc(inputString)}`);
+    }
+  }
+
+  stringSplitSumCalc(string, customSeparator) {
+    console.log(customSeparator);
+    let separatedArray = string.split(/[,:]/g);
+    if (customSeparator) {
+      separatedArray = separatedArray
         .map((str) => str.split(customSeparator))
         .flat();
-
-      const answerNumber = separatedInputArray.reduce((acc, cur) => {
-        if (Number(cur) === NaN) {
-          throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
-        }
-        if (Number(cur) < 0) {
-          throw new Error("[ERROR] 음수를 입력했습니다");
-        }
-
-        return acc + Number(cur);
-      }, 0);
-
-      Console.print(`결과 : ${answerNumber}`);
-    } else {
-      const separatedInputArray = inputString.split(/[,:]/g);
-
-      const answerNumber = separatedInputArray.reduce((acc, cur) => {
-        if (Number(cur) === NaN) {
-          throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
-        }
-        if (Number(cur) < 0) {
-          throw new Error("[ERROR] 음수를 입력했습니다");
-        }
-
-        return acc + Number(cur);
-      }, 0);
-
-      Console.print(`결과 : ${answerNumber}`);
     }
+
+    return separatedArray.reduce(
+      (sum, str) => sum + this.validateNumber(str),
+      0
+    );
+  }
+
+  validateNumber(string) {
+    if (Number(string) === NaN) {
+      throw new Error("[ERROR] 숫자가 아닌 값을 입력했습니다");
+    }
+    if (Number(string) < 0) {
+      throw new Error("[ERROR] 음수를 입력했습니다");
+    }
+
+    return Number(string);
   }
 }
 
