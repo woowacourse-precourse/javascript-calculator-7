0deb8ca2a024a11689b6948c57ea71d020859672
@u_limit_ <yurim@ajou.ac.kr>
2024-10-16 00:21:46 +0900

    feat: 사용자 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ class App {
   async run(
   ) {
     const result = 0 ;
+    let input="";
     Console.print("덧셈할 문자열을 입력해 주세요.");
+    input=await Console.readLineAsync(input)
     Console.print("결과 : "+result);
   }
 }
