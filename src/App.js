0affc977fe95a75e33926379c26e567d7ae63d14
Minhyuk Kim <labcs@naver.com>
2024-10-15 19:03:32 +0900

    feat: 사용자 입력 기반 함수 실행 코드 1차 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+
+
 class App {
-  async run() {}
+  async run() {
+    let SUM = 0;
+    const INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    for (let i = 0; i < INPUT.length; i++) {
+      if (INPUT[i] === '-') {
+        throw new Error('[ERROR]');
+      }
+      if (INPUT[i] >= '0' && INPUT[i] <= '9') {
+        SUM += +INPUT[i];
+      }
+    }
+
+    Console.print(`결과 : ${SUM}`);
+  }
 }
 
 export default App;
+
+// 커스텀 구분자 보완할 것
