6a5f6a619c6608f82fcc81e4cef7a8826264f0e4
jongbin26 <010jjj@naver.com>
2024-10-15 17:59:33 +0900

    feat(utils): 구분자 배열 함수 추가 - constant 폴더를 만들어 상수 변수들을 관리합니다. - 구분자 배열에 Default 구분자를 우선적으로 넣고, 커스텀 구분자가 존재하는 경우 추가합니다. - 선언적 프로그래밍을 위해 함수 내에서 JS 내장 문법을 활용해 let, var 변수를 최소한으로 사용했습니다.

---
+++ src/constant/index.js
@@ -0,0 +1,7 @@
+const SEPERATOR_STRING = {
+    START : '\/\/',
+    END : '\\n'
+}
+const DEFAULT_SEPERATOR = [',', ':'];
+
+export {SEPERATOR_STRING, DEFAULT_SEPERATOR}
---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+import {SEPERATOR_STRING, DEFAULT_SEPERATOR} from '../constant/index.js'
+
+export function getSeperatorArray(expression){
+    const seperator = []
+    DEFAULT_SEPERATOR.map((element)=>{
+        seperator.push(element);
+    })
+    if(expression.startsWith(SEPERATOR_STRING.START)){
+        const seperatorStartIndex = expression.indexOf(SEPERATOR_STRING.START) + 2;
+        const seperatorEndIndex = expression.indexOf(SEPERATOR_STRING.END);
+        seperator.push(expression.slice(seperatorStartIndex, seperatorEndIndex))
+    }
+    return seperator;
+}
