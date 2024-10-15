c26715e6c92e9799720f2560cd61823247009fb9
ChaDongmin <cdm1263@gmail.com>
2024-10-16 05:27:06 +0900

    feat(App): 라이브러리 유틸리티 함수 메서드 정의
    - 사용자의 값을 입력받는 함수 read 정의
    
    - 콘솔에 출력하는 함수 print 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async run() {}
+
+  async read(input) {
+    return MissionUtils.Console.readLineAsync(input);
+  }
+
+  print(input) {
+    return MissionUtils.Console.print(input);
+  }
 }
 
 export default App;
