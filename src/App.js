eae330977e1a66a3bad8f9ad16b22fbd750ba7e0
RohSungKyun <songkyun1108@naver.com>
2024-10-16 00:59:42 +0900

    fix : 유효성 검사 옵션 수정

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import { ERROR } from "./constants/error.js";
 
 class App {
   async run() {
-    this.startInput();
+    await this.calculator();
   }
 
-  startInput() {
-    const inputStr = this.inputString();
-
+  async calculator() {
+    const inputStr = await this.inputString();
   }
   async inputString() {
     const input = await Console.readLineAsync(MESSAGE.INPUT); // 문자열을 입력받는다.
@@ -18,26 +17,31 @@ class App {
     return input;
     
   }
+
    validate(input) {
     if (!input){ // 입력값이 없는 경우.
       throw new Error(ERROR.INPUT_BLANK_ERROR);
     }
 
-    if(input[0] && input[1] === '/'){ //커스텀 구분자를 사용하는 경우
-      if(!input.includes("\n")){
-        throw new Error(ERROR.INPUT_DELIMITER_TYPE);
-      } 
-    }
-
-    for(let x of input.split(/,|:/)){
-      if(isNaN(x) || x < 0){ //숫자가 아니거나 음수일 경우
+    if(input.startsWith("//")){ //커스텀 구분자를 사용하는 경우
+      const checkNum = input.split("\\n");
+      if(checkNum < 2){
         throw new Error(ERROR.INPUT_NUMBER_TYPE);
+      } 
+      const Delimiter = checkNum[0].substring(2,);
+      if(!Delimiter){ // 커스텀 구분자가 없을 경우
+        throw new Error(ERROR.INPUT_NO_CUSTOMDELIMITER);
       }
+    } else {
+      for(let x of input.split(/,|:/)){
+        if(isNaN(x) || x < 0){ //숫자가 아니거나 음수일 경우
+          throw new Error(ERROR.INPUT_NUMBER_TYPE);
+        }
+      }      
     }
+
+
    }
-
-  
-
 
 }
 
--- src/constants/error.js
+++ src/constants/error.js
@@ -2,4 +2,5 @@ export const ERROR = {
     INPUT_BLANK_ERROR : "[ERROR] 값을 입력해 주세요.",
     INPUT_NUMBER_TYPE : "[ERROR] 양수로 이루어진 문자열을 입력해 주세요.",
     INPUT_DELIMITER_TYPE : "[ERROR] 쉼표나 콜론으로 구분된 문자열을 입력해 주세요",
+    INPUT_NO_CUSTOMDELIMITER : "[ERROR] 커스텀 구분자가 없습니다."
 }
