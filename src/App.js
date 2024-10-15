47f3a94d349b2a40dab2a285f88ed3f8c7e1cc2f
daun-up <awhekdns@kookmin.ac.kr>
2024-10-15 20:38:35 +0900

    feat: 기본 계산 로직 구현 (쉼표(,) 또는 콜론(:) 을 구분자로 사용하여 숫자를 더한다.)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.");
+    let input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    try {
+      let result = this.caculate(input);
+      if (isNaN(result)) {throw new Error("숫자가 아닌 값이 포함되어 있습니다.")}
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch(e) {
+      MissionUtils.Console.print("[ERROR] 잘못된 형식의 입력입니다.");
+    }
+  }
+
+  caculate(input) {
+    if (input === "") {
+      return 0;
+    }
+
+    // 커스텀 구분자도 추가할 것이기 때문에 변수로 선언
+    let delimiter = /[, :]/
+
+    let numbers = input.split(delimiter);
+    let sum  = 0
+
+    for (let number of numbers) {
+      sum += parseInt(number);
+    }
+
+
+
+
+    return sum;
+  }
 }
 
 export default App;
