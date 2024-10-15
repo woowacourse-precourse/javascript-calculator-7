37da6e2aa345f3289291f846f81c28a9e47b64da
san1313 <skan134679@gmail.com>
2024-10-15 18:01:48 +0900

    feat(readLine): 사용자에게 문자를 입력받기 위한 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    // 1. 사용자에게 문자열을 입력 받는다.
+    const STR = await this.readLine('덧셈할 문자열을 입력해 주세요.\n');
+  }
+
+  /**
+   * message를 출력하고 사용자의 입력을 받는다.
+   * @param message
+   * @return {Promise<string>} 사용자의 입력값
+   */
+  readLine(message) {
+    return Console.readLineAsync(message);
+  }
 }
 
 export default App;
