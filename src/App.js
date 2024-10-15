640d55dbe499c59c68b3475172d40ba30e2ebf40
minjeong9919 <minjeong9919@naver.com>
2024-10-16 00:46:31 +0900

    feat(parser): 입력 문자열에서 커스텀 구분자 감지 기능 추가
    입력 문자열의 앞부분에서 커스텀 구분자의 유무를 판단하는 기능을 하는 함수를 추가했습니다.
    "//"와 "\n" 사이에 커스텀 구분자가 있는지 확인한 후, Console.print를 통해 결과가 출력되도록 처리했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const checkIsCustom = (string) => {
+        return string.startsWith("//") && string.includes("\\n");
+      };
+
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요."
+      );
+      Console.print(checkIsCustom(input));
+    } catch (error) {
+      console.error("Error");
+    }
+  }
 }
 
 export default App;
