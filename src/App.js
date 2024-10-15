c100857f647b8d714cacaf71090cd0398d9be735
eunseopi <asxzqw12@naver.com>
2024-10-15 15:54:18 +0900

    feat: 기본 구분자(쉼표, 콜론)를 이용한 문자열 덧셈 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+
+  }
+    calculate(input) {
+      const NUMBERS = input.split(/[,][:]/).map(Number);
+      return NUMBERS.reduce((a,b) => a+b, 0);
+    }
 }
 
 export default App;
