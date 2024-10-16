dab72f69efea820a8d9cdfe032859054464bcabc
hjoo830 <hjoo830@naver.com>
2024-10-17 01:43:52 +0900

    feat: 커스텀 구분자 저장하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const inputString = await Console.readLineAsync('계산할 문자열을 입력: ');
+    let delimiters = [',', ':'];
+    const customDelimiter = inputString.match(/\/\/(.*?)\\n/);
+
+    if (customDelimiter) {
+      delimiters.push(customDelimiter[1]);
+    }
   }
 }
 
