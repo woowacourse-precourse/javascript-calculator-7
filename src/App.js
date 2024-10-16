3e4e0e7869f7c2d613e23cfe4b24e0e8e8e60407
@u_limit_ <yurim@ajou.ac.kr>
2024-10-17 03:00:00 +0900

    feat: 커스텀 구분자 지정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import {Console} from "@woowacourse/mission-utils";
 
 let separator = [",",":"];
-
-function getNumber(input) {
+function findSeparator(input){
   const inputArray=input.split("");
+  if(input.slice(0,2)==="//"&&input.slice(3,5)==="\\n"){
+    if(isNaN(input[2])){
+      separator.push(input[2]);
+    }
+    return inputArray.splice(5);
+  }
+  return inputArray;
+}
+
+function getNumber(inputArray) {
   let currentNum = '';
   const result=[];
-
   inputArray.forEach(string=>{
     if(separator.includes(string)){
       if(currentNum.length>0){
@@ -14,7 +22,9 @@ function getNumber(input) {
         currentNum='';
       }
     }else{
-      currentNum += string;
+      if(!isNaN(currentNum)){
+        currentNum += string;
+      }
     }
   })
   if(currentNum !== ''){
@@ -37,7 +47,7 @@ class App {
     let input="";
     Console.print("덧셈할 문자열을 입력해 주세요.");
     input=await Console.readLineAsync(input);
-    Console.print("결과 : "+calculate(input));
+    Console.print("결과 : "+calculate(findSeparator(input)));
   }
 }
 
