5abfc430c01d1c1ac010744f44eee9c74eda7f94
kimdayeon37 <kdy37912@naver.com>
2024-10-16 10:49:43 +0900

    feat: Console API를 사용한 입출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+      const result = this.calculateSum(input);
+      Console.print(`결과: ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  calculateSum(input) {
+
+  }
 }
 
 export default App;
