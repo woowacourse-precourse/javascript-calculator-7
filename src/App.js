a480dce948f47cfcc14e4674e79293bcb898dfe0
dawn0314 <sdawn0314@gmail.com>
2024-10-16 21:48:38 +0900

    feat: get user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+    if (input === "") {
+      throw new Error("[ERROR] 문자열을 입력해주세요");
+    }
+  }
 }
 
 export default App;
