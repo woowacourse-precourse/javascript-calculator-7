0208330231111f1fb1deed5d76fa37e0c38a008a
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-16 15:43:55 +0900

    :sparkles: feat: 에러메시지 상수화 및 동결

--- src/constant/messages.js
+++ src/constant/messages.js
@@ -1,8 +1,14 @@
 const BASIC_MESSAGES = Object.freeze({
   stringInputMessage: "덧셈할 문자열을 입력해 주세요.\n",
   calcOutputMessage: "결과 : ",
+  exitMessage: "계산기를 종료합니다",
 });
 
 const ERROR_WORD = "[ERROR]";
 
-const ERROR_MESSAGE = Object.freeze({});
+const ERROR_MESSAGE = Object.freeze({
+  invalidPositiveNumber: "${ERROR_WORD} 음수는 입력할 수 없습니다.",
+  invalidString: "${ERROR_WORD} 문자는 입력할 수 없습니다.",
+});
+
+export { BASIC_MESSAGES, ERROR_MESSAGE };
