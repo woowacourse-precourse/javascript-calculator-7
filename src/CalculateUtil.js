df4244864e3e20c92613fa497c10aaaf7a4a8fc1
jiyeeeah <sije3757@snu.ac.kr>
2024-10-16 14:28:06 +0900

    refactor: 커스텀 구분자에 기본 구분자도 포함되도록

--- src/CalculateUtil.js
+++ src/CalculateUtil.js
@@ -1,12 +1,17 @@
 const CalculateUtil = {
   parseInput(input) {
+    const separator = [",", ":"];
     const re = /(\/\/).+(\\n)/;
+
     if (input.match(re)) {
-      const separator = input.substring(2, input.indexOf("\\")).split("");
+      input
+        .substring(2, input.indexOf("\\"))
+        .split("")
+        .map((sep) => separator.push(sep));
       const expression = input.substring(input.indexOf("n") + 1);
       return { separator, expression };
     }
-    const separator = [",", ":"];
+
     const expression = input;
     return { separator, expression };
   },
