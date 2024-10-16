53706260aab0075b12c08d228b802604b949bf49
DodoJBNU <dh723@jbnu.ac.kr>
2024-10-16 20:47:09 +0900

    feat: add function to check custom sepeartor and remove it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    
+    const [seperatorSet, filteredInput] = this.handleCustomSeperator(input);
+  }
+
+  handleCustomSeperator(input){
+    const seperatorSet = new Set([':', ',']);
+    const regex = /\/\/(.)\\n/g;
+    let matchData = null;
+    let lastIndex = 0;
+
+    while((matchData = regex.exec(input)) !== null){
+      const customSeperator = matchData[0][2];
+
+      seperatorSet.add(customSeperator);
+      lastIndex = matchData.index + matchData[0].length;
+    }
+
+    const filteredInput = input.slice(lastIndex);
+    
+    return [seperatorSet, filteredInput];
+  }
 }
 
 export default App;
