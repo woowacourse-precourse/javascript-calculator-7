f764956be1ea05375a4c788b7803f15d202ba197
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-16 20:45:50 +0900

    🔧 refactor(separator): 기본 및 커스텀 구분자 정규표현식으로 변경
    기존의 SEPARATOR 상수를 배열로 선언했었는데 문자열 파싱을 쉽게 하기 위해 기본 구분자와 커스텀 구분자를 정규표현식으로 변경했습니다.
    - 기본 구분자(DEFAULT_SEPARATOR)를 쉼표(,)와 콜론(:)을 처리하는 정규표현식으로 수정
    - 커스텀 구분자(CUSTOM_SEPARATOR)를 //로 시작하고 \n으로 끝나는 정규표현식으로 수정

--- src/constant.js
+++ src/constant.js
@@ -3,8 +3,8 @@ export const PROMPT_USER_INPUT = '덧셈할 문자열을 입력해 주세요.';
 export const RESULT_MESSAGE = '결과: ';
 
 // 입력 구분자 관련 상수
-export const DEFAULT_SEPARATOR = [',', ':'];
-export const CUSTOM_SEPARATOR = ['//', '\n'];
+export const DEFAULT_SEPARATOR = /,|:/;
+export const CUSTOM_SEPARATOR = /^\/\/.*\n/;
 
 // 에러 메시지 관련 상수
 export const ERROR_INVALID_INPUT = '[ERROR] 잘못된 입력입니다.';
