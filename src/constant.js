5d3b2edc1246b7d9edeb3d4deff02935d438334e
Yeongi Park <yeongi0111@naver.com>
2024-10-16 13:49:59 +0900

    feat : 출력 메시지 상수 저장
    constant.js파일에 출력시 필요한 메시지 상수로 저장

--- src/constant.js
+++ src/constant.js
@@ -1,14 +1,18 @@
-const errorMessage = {
-  usePositive: "[ERROR] 양수만 입력해 주세요",
+export const errorMessage = {
+  usePositive: '[ERROR] 양수만 입력해 주세요',
 
-  useCommaOrColon: "[ERROR] 구분자로 쉼표와 클론만 사용해 주세요",
+  useCommaOrColon: '[ERROR] 구분자로 쉼표와 클론만 사용해 주세요',
 
-  useNewLine: "[ERROR] \n을 포함시켜 주세요",
+  useNewLine: '[ERROR] \n을 포함시켜 주세요',
 
-  useCoustomSeparator: "[ERROR] 커스텀 구분자를 입력해 주세요",
+  useCoustomSeparator: '[ERROR] 커스텀 구분자를 입력해 주세요',
 
   useCustomOrBasicSeparator:
-    "[ERROR] 커스텀 구분자 혹은 쉼표와 클론만 사용해 주세요",
+    '[ERROR] 커스텀 구분자 혹은 쉼표와 클론만 사용해 주세요',
 
-  useNumberOrSlash: "[ERROR] 입력값을 확인해 주세요",
+  useNumberOrSlash: '[ERROR] 입력값을 확인해 주세요',
+};
+
+export const outputMessage = {
+  startMessage: '덧셈할 문자열을 입력해 주세요.',
 };
