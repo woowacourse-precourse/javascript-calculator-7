28bddf3bba638e3cb407796a3326b2d4b6359761
gyur1kim <qri1114@gmail.com>
2024-10-17 00:30:19 +0900

    feat(i/o): 사용자 입력 & 출력 구현
    사용자에게 입력 받고, 해당 값 출력.
    userInput이 적합하지 않으면 error처리한다.
    예외처리할 케이스는 추후 고민.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async getUserInput() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+      if (userInput === '') throw new Error('[Error]');
+      return userInput;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async run() {
+    try {
+      const userInput = await this.getUserInput();
+      MissionUtils.Console.print(`사용자가 입력한 값 : ${userInput}`);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
+  }
 }
 
 export default App;
