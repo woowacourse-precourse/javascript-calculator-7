2e2800d60441add94d360e9b94cabae8f1976a9e
장정안 <wjddks96@naver.com>
2024-10-16 23:35:02 +0900

    feat: 커스텀 구분자 입력 여부에 따른 로직 처리

--- src/controller/Calculator.js
+++ src/controller/Calculator.js
@@ -3,6 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 import User from '../user/User.js';
 import { GAME_MESSAGE } from '../constants/gameMessages.js';
 import { delimiter } from '../constants/delimiter.js';
+import { go } from '../util/Util.js';
 
 //덧셈기 게임 관리
 class Calculator {
@@ -18,12 +19,52 @@ class Calculator {
   /**@param {string} input */
   calculate(input) {
     //실제 계산 로직
-    const result = input
-      .split(delimiter)
-      .map(Number)
-      .reduce((acc, cur) => acc + cur);
 
-    this.printResult(result);
+    // '//'로 시작한다면
+    if (input.startsWith('//')) {
+      return go(
+        input,
+
+        /**@type {(str:string) => string[]}  */
+        (str) => str.split('\\n'),
+
+        /**@type {(str:string[]) => {customDelimiter:string, numbersPart:string}}  */
+        ([delimiterPart, numbersPart]) => ({
+          customDelimiter: delimiterPart.slice(2),
+          numbersPart,
+        }),
+        ({ customDelimiter, numbersPart }) =>
+          numbersPart.split(customDelimiter),
+
+        /**@type {(numbers:string[]) => number[]}  */
+        (numbers) => numbers.map(Number),
+
+        /**@type {(numbers:number[]) => number}   */
+        (numbers) => numbers.reduce((acc, cur) => acc + cur),
+
+        /**@type {(result: number) => void}   */
+        (result) => this.printResult(result)
+      );
+    }
+
+    // '//'로 시작하지 않고 일반 숫자가 먼저 입력된다면
+    if (/^\d/.test(input)) {
+      go(
+        input,
+
+        /**@type {(input:string) => string[]}  */
+        (input) => input.split(delimiter),
+
+        /**@type {(nums:string[]) => number[]}  */
+        (nums) => nums.map(Number),
+
+        /**@type {(nums:number[]) => number}   */
+        (nums) => nums.reduce((acc, cur) => acc + cur),
+
+        /**@type {(result: number) => void}   */
+        (result) => this.printResult(result)
+      );
+    }
   }
 
   /**@param {number} result */
