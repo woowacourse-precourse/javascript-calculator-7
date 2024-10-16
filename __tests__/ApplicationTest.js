c3c136e32c3a8f86994de558adbe9fcb2194f8f8
san1313 <skan134679@gmail.com>
2024-10-17 00:56:01 +0900

    test(jest): README에 있던 테스트 케이스를 ApplicationTest.js에 추가
    기존 README에 작성해두었던 테스트 케이스를 ApplicationTest.js 내 테스트 함수 데이터로 적용하였습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,11 +1,10 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-const mockQuestions = (inputs) => {
+const mockQuestions = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
     return Promise.resolve(input);
   });
 };
@@ -17,27 +16,34 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
-  test("커스텀 구분자 사용", async () => {
-    const inputs = ["//;\\n1"];
-    mockQuestions(inputs);
+  test.each([
+    ["//;\\n1", "결과 : 1"],
+    ["1,2:3", "결과 : 6"],
+    ["//;\\n1;2;3", "결과 : 6"],
+    ["1//;\\n//-\\n;2-3", "결과 : 6"],
+    ["//a\\n1a2a3a4a5", "결과 : 15"],
+    ["//1\\n2131415", "결과 : 14"],
+    ["1,2,+3,+4,5", "결과 : 15"],
+    ["1////\\n2", "결과 : 3"],
+    ["123,456", "결과 : 579"],
+  ])("커스텀 구분자 사용", async (input, output) => {
+    mockQuestions(input);
 
     const logSpy = getLogSpy();
-    const outputs = ["결과 : 1"];
 
     const app = new App();
     await app.run();
 
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  });
+
+  test.each(["-1,2,3", "//\\n1,2,3", "1,2,3a,4d,5", "1,,,,2,,,,3", "1,2,+3,-4", "1,2,++3"])(
+    "예외 테스트",
+    async (input) => {
+      mockQuestions(input);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow("[ERROR]");
     });
-  });
-
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
-    mockQuestions(inputs);
-
-    const app = new App();
-
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
 });
