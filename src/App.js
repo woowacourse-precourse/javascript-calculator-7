56a446643d10c9be06a97094eea1b390a65f9ea8
Kimyerim <yeye092302@gmail.com>
2024-10-16 23:38:37 +0900

    feat(App): add method to get input string for addition
    - Implement getString method to prompt user for input
    - Use Console.readLineAsync to read input asynchronously
    - Handle errors and print error messages if input fails

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputStr = await this.getString();
+  }
+
+  async getString(){
+    try{
+      const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요\n');
+      return inputStr;
+    } catch(error){
+      Console.print('Error: ' + error.message);
+      return null;
+    }
+  }
 }
 
 export default App;
