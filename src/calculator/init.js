b91f4548d7f30163616040922edb8fe6251267c0
SanghyunLee <lgw9736@naver.com>
2024-10-16 19:58:47 +0900

    feat(split): 구분자 집합으로 분리하는 기능 추가
    - 함수명은 splitBySeparators로 정의
    - 유효하지 않은 구분자가 있거나, 음수가 있을 경우 에러 발생
    - init()에서 중복되는 부분이 생겨 함수로 묶어서 처리
    - 함수의 주석과 중복되는 주석은 제거

--- src/calculator/init.js
+++ src/calculator/init.js
@@ -1,10 +1,14 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { hasCustomSeparator, setCustomSeparator } from './separator.js';
+import {
+  hasCustomSeparator,
+  setCustomSeparator,
+  splitBySeparators,
+} from './separator.js';
 
 /**
  * 사용자에게 문자열을 입력받는 함수
  * @returns {Promise<string>} 입력받은 문자열
- * @throws 입력 도중 오류 발생 시 에러를 던집니다.
+ * @throws 입력 도중 오류 발생 시 에러를 발생
  */
 const getInput = async () => {
   try {
@@ -18,20 +22,28 @@ const getInput = async () => {
 };
 
 /**
+ * 입력된 문자열을 통해 구분자 집합과 계산이 필요한 문자열 반환
+ */
+const processInputString = (inputString) => {
+  if (hasCustomSeparator(inputString)) {
+    return setCustomSeparator(inputString);
+  } else {
+    return {
+      separators: new Set([',', ':']),
+      remainingString: inputString,
+    };
+  }
+};
+
+/**
  * 프로그램 실행 함수
  * 사용자에게 입력을 받아 구분자를 확인 후, 구분자로 문자열을 나누고 덧셈을 한 후 결과를 출력한다.
  */
 const init = async () => {
   try {
     const inputString = await getInput();
-
-    // 입력된 문자열에 커스텀 구분자가 있는지 확인
-    if (hasCustomSeparator(inputString)) {
-      // 커스텀 구분자 추출 및 커스텀 구분자 지정 형식을 제거한 문자열을 반환
-      const { separators, remainingString } = setCustomSeparator(inputString);
-    } else {
-      const separators = new Set([',', ':']);
-    }
+    const { separators, remainingString } = processInputString(inputString);
+    const calculationArr = splitBySeparators(separators, remainingString);
   } catch (error) {
     MissionUtils.Console.print(`[Error] ${error.message}`);
   }
--- src/calculator/separator.js
+++ src/calculator/separator.js
@@ -4,54 +4,92 @@
  * @returns {boolean} 커스텀 구분자를 지정하는 문자열이 포함하면 true, 그렇지 않으면 false
  */
 export const hasCustomSeparator = (inputString) => {
-  // 문자열의 시작이 '/'인지 확인하는 정규 표현식
-  const pattern = /^\//;
-
-  // 정규 표현식이 일치하면 true 반환
-  if (pattern.test(inputString)) {
-    return true;
-  }
-
-  // 정규 표현식이 일치하지 않으면 false 반환
-  return false;
-};
-
-/**
- * 입력된 문자열에서 커스텀 구분자를 추출하고 구분자 집합과 계산할 문자열을 반환하는 함수
- *
- * @param {string} inputString - 사용자로부터 입력받은 문자열
- * @returns {{ separators: Set<string>, remainingString: string }} 구분자 집합과 구분자 제거 후 문자열을 반환
- * @throws {Error} 구분자 형식이 맞지 않는 경우 에러를 던짐
- */
-export const setCustomSeparator = (inputString) => {
-  const separators = new Set([',', ':']);
-
-  // '//'로 시작하고 '\n'으로 끝나는 부분을 추출하는 정규 표현식
-  const customSeparatorPattern = /^\/\/(.+?)\\n/;
-  const isCustomSeparatorMatch = customSeparatorPattern.exec(inputString);
-
-  if (!isCustomSeparatorMatch) {
-    throw new Error(
-      '커스텀 구분자 지정 형식에 맞지 않거나 커스텀 구분자가 작성되지 않았습니다.'
+    // 문자열의 시작이 '/'인지 확인하는 정규 표현식
+    const pattern = /^\//;
+  
+    // 정규 표현식이 일치하면 true 반환
+    if (pattern.test(inputString)) {
+      return true;
+    }
+  
+    // 정규 표현식이 일치하지 않으면 false 반환
+    return false;
+  };
+  
+  /**
+   * 입력된 문자열에서 커스텀 구분자를 추출하고 구분자 집합과 계산할 문자열을 반환하는 함수
+   *
+   * @param {string} inputString - 사용자로부터 입력받은 문자열
+   * @returns {{ separators: Set<string>, remainingString: string }} 구분자 집합과 구분자 제거 후 문자열을 반환
+   * @throws {Error} 구분자 형식이 맞지 않는 경우 에러를 던짐
+   */
+  export const setCustomSeparator = (inputString) => {
+    const separators = new Set([',', ':']);
+  
+    // '//'로 시작하고 '\n'으로 끝나는 부분을 추출하는 정규 표현식
+    const customSeparatorPattern = /^\/\/(.+?)\\n/;
+    const isCustomSeparatorMatch = customSeparatorPattern.exec(inputString);
+  
+    if (!isCustomSeparatorMatch) {
+      throw new Error(
+        '커스텀 구분자 지정 형식에 맞지 않거나 커스텀 구분자가 작성되지 않았습니다.'
+      );
+    }
+  
+    const customSeparator = isCustomSeparatorMatch[1];
+  
+    if (customSeparator.length > 1) {
+      throw new Error('커스텀 구분자는 단일 문자를 사용해야합니다.');
+    }
+  
+    if (customSeparator) {
+      separators.add(customSeparator);
+    }
+  
+    // 커스텀 구분자 제거 후 남은 문자열 추출
+    const remainingStringPattern = /\\n([^]*)$/;
+    const isRemainingStringMatch = remainingStringPattern.exec(inputString);
+  
+    // 남은 문자열이 없을 경우 빈 문자열 반환
+    const remainingString = isRemainingStringMatch
+      ? isRemainingStringMatch[1]
+      : '';
+  
+    return { separators, remainingString };
+  };
+  
+  /**
+   * 주어진 구분자 집합에 따라 문자열을 분리하고, 배열의 요소를 숫자형으로 변환한 후 반환하는 함수
+   *
+   * @param {Set<string>} separators - 문자열을 분리할 구분자 집합
+   * @param {string} calculationString - 계산할 문자열
+   * @returns {number[]} 분리된 문자열을 숫자형으로 변환한 배열
+   * @throws {Error} 유효하지 않은 구분자가 있거나 음수가 포함된 경우 에러 발생
+   */
+  export const splitBySeparators = (separators, calculationString) => {
+    // 구분자를 모아 정규표현식 패턴으로 변환
+    let splitPattern = '';
+    separators.forEach((separator) => {
+      splitPattern += separator;
+    });
+  
+    // 구분자로 문자열 분리
+    const calculationArr = calculationString.split(
+      new RegExp(`[${splitPattern}]`)
     );
-  }
-
-  const customSeparator = isCustomSeparatorMatch[1];
-
-  if (customSeparator.length > 1) {
-    throw new Error('커스텀 구분자는 단일 문자를 사용해야합니다.');
-  }
-
-  if (customSeparator) {
-    separators.add(customSeparator);
-  }
-
-  // 커스텀 구분자 제거 후 남은 문자열 추출
-  const remainingStringPattern = /\\n([^]*)$/;
-  const isRemainingStringMatch = remainingStringPattern.exec(inputString);
-
-  // 남은 문자열이 없을 경우 빈 문자열 반환
-  const remainingString = isRemainingStringMatch ? isRemainingStringMatch[1] : '';
-
-  return { separators, remainingString };
-};
+  
+    // 배열의 각 요소를 숫자로 변환하고 유효성 검사 후 반환
+    return calculationArr.map((element) => {
+      const num = Number(element);
+  
+      if (isNaN(num)) {
+        throw new Error('입력 문자열에 유효하지 않은 구분자가 있습니다.');
+      }
+  
+      if (num < 0) {
+        throw new Error('입력 문자열에 음수가 있습니다.');
+      }
+  
+      return num;
+    });
+  };
