53796de8820472ce4ab5600566077e60a2d70ae9
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-16 15:11:48 +0900

    :sparkles: feat: 기본메시지 상수화 및 동결

---
+++ src/constant/messages.js
@@ -0,0 +1,8 @@
+const BASIC_MESSAGES = Object.freeze({
+  stringInputMessage: "덧셈할 문자열을 입력해 주세요.\n",
+  calcOutputMessage: "결과 : ",
+});
+
+const ERROR_WORD = "[ERROR]";
+
+const ERROR_MESSAGE = Object.freeze({});
