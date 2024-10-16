4bf3525adc1afc707bddb07af834c3c286e4c193
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-16 22:17:16 +0900

    ✨ feat(app): 입력 처리 및 에러 핸들링 기능 추가
    App 클래스의 run 메서드에 사용자 입력 처리와 에러 핸들링 기능을 추가했습니다.
    - `userInput`을 통해 입력을 받고, `Separator` 클래스를 사용하여 구분
    - `validatePositiveNumbers` 메서드로 양수 검증 후 에러 발생 시 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import ErrorHandler from './ErrorHandler';
+import printMessage from './print-message';
+import Separator from './Separator';
+import userInput from './user-input';
+import calculateSum from './calculate';
+
 class App {
-  async run() {}
+  async run() {
+    const input = userInput();
+    const separator = new Separator(input);
+    const value = separator.separate();
+
+    try {
+      ErrorHandler.validatePositiveNumbers(value);
+      printMessage(calculateSum(value));
+    } catch (error) {
+      printMessage(error.message);
+    }
+  }
 }
 
 export default App;
