09b9309612c3935e62324a52629d24acea0bb97c
rhtjddls123 <jjjk0605@naver.com>
2024-10-16 21:28:33 +0900

    fix: 커스텀 구분자 생성부분이 문자열 중앙에 있어도 작동하는 오류 해결

--- src/App.js
+++ src/App.js
@@ -6,10 +6,28 @@ const SEP_REGEX = /\/\/(.*?)\\n/g;
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    this.validateString(input);
     const { str, separator } = this.makeCustomSep(input);
     const sepStrArr = this.extractInput(str, separator);
     const sepNumArr = this.toNumberArray(sepStrArr);
     Console.print(`결과 : ${this.sumNumbers(sepNumArr)}`);
+  }
+
+  validateString(str) {
+    // 커스텀 구분자를 만들때 해당 부분이 문자열 앞쪽부터 연속적으로 등장하는지 확인
+    const matches = [...str.matchAll(SEP_REGEX)];
+    if (matches.length === 0) return;
+
+    if (matches[0].index !== 0) throw Error("[ERROR] 잘못된 입력입니다! 구분자는 문자열의 맨 앞쪽에 와야합니다!");
+
+    for (let i = 0; i < matches.length - 1; i += 1) {
+      const endIndex = matches[i].index + matches[i][0].length;
+      const nextStartIndex = matches[i + 1].index;
+
+      if (endIndex !== nextStartIndex) {
+        throw Error("[ERROR] 잘못된 입력입니다! 구분자는 문자열의 맨 앞쪽부터 연속적으로 와야합니다!");
+      }
+    }
   }
 
   extractInput(str, sep) {
