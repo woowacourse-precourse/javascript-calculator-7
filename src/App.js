e39bb63b9965d22fb905927d2d15155d30f2aeda
jiyeeeah <sije3757@snu.ac.kr>
2024-10-16 14:21:08 +0900

    feat: 입력값이 구분자와 양수로만 이루어져 있는지 유효성 검사
    - validation의 isInputValid함수를 통해 검사
    - 해당 함수에서는 expression의 문자들을 배열에 넣어, every 메서드를 통해
      문자가 구분자이거나 양수일 경우에 true를 return하도록 함
    - App.js에서 isInputValid가 false일 경우 throw문을 통해 예외를 발생시키도록 함

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import InterfaceUtil from "./InterfaceUtil.js";
 import CalculateUtil from "./CalculateUtil.js";
+import { isInputValid } from "./validation.js";
 
 class App {
   async run() {
     const input = await InterfaceUtil.inputString();
 
     const { separator, expression } = CalculateUtil.parseInput(input);
+    if (!isInputValid(separator, expression)) throw new Error("[ERROR]");
   }
 }
 
---
+++ src/validation.js
@@ -0,0 +1,7 @@
+export function isInputValid(separator, expression) {
+  const expressionArr = expression.split("");
+  if (expressionArr.every((str) => separator.includes(str) || (!isNaN(str) && Number(str) > 0))) {
+    return true;
+  }
+  return false;
+}
