4fe9a6b1779911eff0694adb7206f85582a06b12
govl0407 <govl0407@gmail.com>
2024-10-16 23:48:40 +0900

    feat : 커스텀 구분자를 이용한 덧셈 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ class App {
   async run() {
     let str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");//사용자 입력
 
-    let splitter = new RegExp(`[,:]+`);// 구분자
-    let nums = str.split(splitter);//구분자를 기준으로 문자열을 split
-    let sum = 0;//합을 저장할 변수
+    let splitter = new RegExp(`[,:]+`);//구분자
+
+    //커스텀 구분자가 있는지 검사
+    str = str.replace('\\n', '\n');
+    const pattern = /^\/\/.\n/;
+    let custom_splitter;
+    if(pattern.test(str)){
+      custom_splitter = str[2];
+      str = str.slice(4)//커스텀 구분자가 있는 부분을 제거
+      splitter = new RegExp(`[,:;${custom_splitter}]+`);//커스텀 구분자를 구분자 목록에 추가
+    }
+
+    //구분자로 문자열 split
+    let nums = str.split(splitter);
 
     //배열에 있는 수를 sum에 더해줌
+    let sum = 0;
     nums.forEach(num => {
       sum += Number(num);
     })
     
-    //결과값 출력
-    Console.print("결과 : ");
+    sum = "결과 : "+ sum; //출력할 결과
     Console.print(sum);
   }
 }
