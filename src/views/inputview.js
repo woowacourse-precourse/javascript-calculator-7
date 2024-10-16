999f6209b1e82235cbae1d52d08891836e8d4ba8
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 02:22:58 +0900

    :sparkles: feat: 입력 창

---
+++ src/views/inputview.js
@@ -0,0 +1,8 @@
+import { BASIC_MESSAGES } from "../constant/messages.js";
+import { Console } from 
+
+class InputView {
+    async getCalcNumber() {
+        return await Console.readLineAsync
+    }
+}
