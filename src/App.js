c3a3b9dfb3b3cda9f17c6af86e174147f2f89b9b
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-10-16 18:47:42 +0900

    feat 기본 구분자를 기준으로 분리하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { DEFAULT_SEPARATOR } from './constant.js';
 
 class App {
   async run() {
@@ -6,7 +7,16 @@ class App {
       '덧셈할 문자열을 입력해 주세요.\n',
     );
 
+    const splittedString = App.splitWithSeparator(input, DEFAULT_SEPARATOR);
+
     Console.print(input);
+    Console.print(splittedString);
+  }
+
+  static splitWithSeparator(str, separator) {
+    const sepToRegex = new RegExp(separator.join('|'));
+
+    return str.split(sepToRegex);
   }
 }
 
