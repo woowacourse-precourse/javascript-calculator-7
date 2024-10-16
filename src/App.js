a86a904b671ab75d5af0b9b23231134bee05830c
guddnboy <hung3018@naver.com>
2024-10-17 00:30:56 +0900

    feat: 기본 구분자로 숫자 합산하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Console from "../node_modules/@woowacourse/mission-utils/src/console.js";
 // import { Console } from "@woowacourse/mission-utils";
 
-// [x] 문자열 커스텀 식별자 처리
-// [ ] 숫자와 식별자 제외한 문자 제거
-// [ ] 콜론과 콤마를 구분자로 사용하여 배열로 변환
-// [ ] 각 요소들 숫자로 변환하여 합산
+// [x] 문자열 커스텀 구분자 처리
+// [x] 숫자와 구분자 제외한 문자 제거
+// [x] 콜론과 콤마를 구분자로 사용하여 배열로 변환
+// [x] 각 요소들 숫자로 변환하여 합산
 // [ ] 결과 출력
 
 class App {
@@ -15,17 +15,32 @@ class App {
       const customRegex = /\/\/(.)\\n/;
 
       let customString = userInput.match(customRegex);
-      // Console.print(customString);
       const targetCustom = customString[1];
 
       let filteredUserInput = userInput.replaceAll(targetCustom, ",");
-      // Console.print(filteredUserInput);
 
       return filteredUserInput;
     }
 
-    let result_custom = customIdentifer(userInput);
-    Console.print(result_custom);
+    function getSum(userInput) {
+      let sum = 0;
+      const targetRegex = /[^0-9:,]/g;
+      let targetString = userInput.replace(targetRegex, "");
+      let splitedString = targetString.split(/[:,]/);
+      Console.print(splitedString);
+
+      splitedString.forEach((element) => {
+        let number = parseInt(element);
+        sum += number;
+      });
+      return sum;
+    }
+
+    // let result_custom = customIdentifer(userInput);
+    // Console.print(result_custom);
+
+    let sum = getSum(userInput);
+    Console.print(sum);
   }
 }
 
