f5ff3eeb46b9c6f3f506b8964aeeceb29d121677
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-16 17:56:06 +0900

    style: 코드스타일 변경

--- src/models/extractnumber.js
+++ src/models/extractnumber.js
@@ -7,12 +7,12 @@ function extractNumbers(input) {
   let NUMBERS = input;
 
   //커스텀 구분자 사용 시 배열 변환 후 숫자 부분 추출
-  if (input.startwith("//")) {
-    const SEPARATORENDLNDEX = input.indexof("\n");
-    numbers = input.substring(SEPARATORENDLNDEX + 1);
+  if (input.startswith("//")) {
+    const SEPARATORENDLNDEX = input.indexOf("\n");
+    NUMBERS = input.substring(SEPARATORENDLNDEX + 1);
   }
   //기본 구분자를 기준으로 문자열 분리 후 숫자 변환
-  return numbers.split(new RegExp("[${SEPRATOR}]")).map(Number);
+  return NUMBERS.split(new RegExp(`[${SEPRATOR}]`)).map(Number);
 }
 
 export default extractNumbers;
--- src/models/extractseparator.js
+++ src/models/extractseparator.js
@@ -1,6 +1,6 @@
 const EXTRACTSEPARTOR = function extractSeparator(input) {
   if (input.startsWith("//")) {
-    const SEPARATORENDLNDEX = input.indexof("\n");
+    const SEPARATORENDLNDEX = input.indexOf("\n");
     if (SEPARATORENDLNDEX !== -1) {
       return input.substring(2, SEPARATORENDLNDEX);
     }
