baf88aaf2e25ce0399b03dbecad07a6daa8f2b67
Heesu01 <rulru0413@naver.com>
2024-10-16 20:09:12 +0900

    feat(App): 구분자에 따른 문자열 분리 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ class App {
     // 문자열 입력
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const input = await Console.readLineAsync("");
-    Console.print(input);
+
+    let numbers;
+
+    //커스텀 구분자 처리
+    if (input.startsWith("//") && input.substring(3, 5) === "\\n") {
+      numbers = input.substring(5).split(input[2]).map(Number);
+    } else {
+      // 기본 구분자 처리
+      numbers = input.split(/,|;/).map(Number);
+    }
   }
 }
 
