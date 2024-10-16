71ddb660170ca5669a3d38091bce8af77e4487ae
dlsxjzld <sinjuk1@naver.com>
2024-10-16 17:12:19 +0900

    feat: 커스텀 구분자 추가 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Console } from "@woowacourse/mission-utils"
 const DIVIDER = [",", ":"]
+
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n",
-    )
+    let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
 
-    Console.print(input.split(new RegExp(`[${DIVIDER}]`, "g")))
+    const customDivider = input.match(/^\/\/(.+)\\n/)
+    if (customDivider !== null) {
+      input = input.replace(/^\/\/.+\\n/, "")
+      DIVIDER.push(customDivider[1])
+    }
   }
 }
 
