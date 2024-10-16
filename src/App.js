890f8318a55b5eea3800d6dce426d03a982e9bd4
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-16 22:08:07 +0900

    feat: "덧셈할 문자열을 입력해 주세요."라는 문구와 함께 값을 입력 받는다

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
+    const inputString = await this.getInputString();
+    this.calculate(inputString);
+  }
 
+  async getInputString() {    
+    return await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
   }
 
   calculate(inputString) {
