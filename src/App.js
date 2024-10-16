0470ca477a5f0524fdcbefe58319ea4736c4d866
장한울 <swingjhw@naver.com>
2024-10-16 15:53:35 +0900

    feat: input에서 구분자 추출함수, 추출과정에서 진행되는 구분자 검사함수 구현

--- src/App.js
+++ src/App.js
@@ -2,28 +2,42 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const basicDelimiter = ",:";
+    const basicDelimiter = [",", ":"];
     let input = await this.getInput();
 
     if (input.length === 0) {
       this.printResult(0);
     }
 
-    this.printResult(input);
+    const [customDelimiter, remain] = this.extractDelimiter(input);
+
+    this.printResult(answer);
   }
 
-  findCustomDelimiter(input) {
-    if (input.includes("//") || input.includes("\n")) {
-      if (!input.includes("//") || !input.includes("\n")) {
+  extractDelimiter(input) {
+    if (this.isCustomDelimiter(input) === false) {
+      return ["", input];
+    }
+    const splited = input.split("\\n");
+    return [splited[0].slice(2), splited[1]];
+  }
+
+  isCustomDelimiter(input) {
+    if (!input.includes("//") && !input.includes("\\n")) {
+      return false;
+    }
+
+    if (input.includes("//") || input.includes("\\n")) {
+      if (!input.includes("//") || !input.includes("\\n")) {
         throw new Error("[ERROR] 커스텀 구분자 설정이 잘못되었습니다.");
       }
     }
 
-    return input.match(/(?<=\/\/)(.+?)(?=\\n)/g) ?? "";
+    return true;
   }
 
   getInput() {
-    return Console.readLineAsync();
+    return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
   }
   printResult(output) {
     Console.print(`결과 : ${output}`);
