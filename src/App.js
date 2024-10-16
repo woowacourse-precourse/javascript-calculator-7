6668aaf0d91f7658cf38b5c2b80482b1187e886a
sw326 <sungwoo326@gmail.com>
2024-10-16 16:35:40 +0900

    fear: 문자열 파싱 및 숫자 추출 로직 구현
    - 입력 문자열에서 숫자를 추출하는 로직 수현
    - 쉼표와 콜론을 기본 구분자로 문자열 분리
    - 유효하지 않은 입력(숫자가 아닌 값, 음수) 검출 및 예외 처리)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import {Console} from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
-    const input = await this.getUserInput();
-    Console.print(input);
+  parseInput(input) {
+    // 쉼표나 콜론으로 문자열 분리(split)
+    const numbers = input.split(/[,;]/).map((num) => {
+      // 문자열 숫자로 변환
+      const parsedNum = parseInt(num.trim(), 10);
+      if (isNaN(parsedNum) || parsedNum < 0) {
+        throw new Error("유효하지 않은 입력입니다.");
+      }
+      return parsedNum;
+    });
+    return numbers;
   }
+
+  async run() {
+    try {
+      const input = await this.getUserInput();
+      const numbers = this.parseInput(input);
+      Console.print(numbers);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
+
   async getUserInput() {
     return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요: ");
   }
