61f619a5fff018354dd09c647572b04875f17b41
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-17 01:09:20 +0900

    feat: 커스텀 구분자로 숫자 피싱 기능 추가

--- src/models/CalculatorModel.js
+++ src/models/CalculatorModel.js
@@ -1,7 +1,34 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 export function parseNumbers(input) {
-  const delimiter = /,|:/;
-  const numbers = input.split(delimiter).map(Number);
-  return numbers;
+  // input 예외처리 함수
+
+  const delimiter = /,|:/; // 기본구분자
+  // 커스텀 구분자 추출
+  const customDelimiter = input.startsWith('//')
+    ? input.substring(2, input.indexOf('\n'))
+    : null;
+
+  // 커스텀 부분 제외한 문자열
+  const processedInput = customDelimiter
+    ? input.substring(input.indexOf('\n') + 1)
+    : input;
+
+  // 커스텀 구분자로 나눈 문자 배열 (split 하면 배열로 만들어짐. )
+  const splitByCustomDelimiter = customDelimiter
+    ? processedInput.split(customDelimiter)
+    : [processedInput];
+
+  const splitByDefaultDelimiters = splitByCustomDelimiter.flatMap((part) =>
+    part.split(delimiter),
+  );
+
+  // 빈 문자열이 있으면 '0'으로 대체
+  const cleanedNumbers = splitByDefaultDelimiters.map((value) =>
+    value === '' ? '0' : value,
+  );
+
+  MissionUtils.Console.print(cleanedNumbers);
+
+  return cleanedNumbers.map(Number);
 }
