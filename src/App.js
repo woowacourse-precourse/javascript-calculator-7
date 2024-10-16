d7b9966765309716a28b181ec328099751ba481e
rhtjddls123 <jjjk0605@naver.com>
2024-10-16 17:54:28 +0900

    feat: 입력값을 검증하여 숫자 배열로 반환하는 기능 구현
    1. toNumberArray함수에 구분자로 분리된 문자 배열을 넘겨준다
    2. toNumberArray에서는 isValid함수를 통해 입력값을 검증하여 숫자 배열로
       변환
    3. isValid함수는 입력값이 모두 양수일경우 true반환, 아닐경우 에러 생성
    3-1. 입력값이 음수일 경우와 숫자가 아닐 경우에 대해 에러를 다르게 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    this.extractInput("1,2:3", [",", ":"]);
+    try {
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+      const separator = [",", ":"];
+      const sepStrArr = this.extractInput(input, separator);
+      const sepNumArr = this.toNumberArray(sepStrArr);
+      Console.print(sepNumArr);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
   }
 
   extractInput(str, sep) {
@@ -8,6 +18,27 @@ class App {
     // 구분자를 이용하여 정규 표현식을 만들어준뒤 split함수를 통해 분리
     const sepRegex = new RegExp(`[${sep.join("")}]+`);
     return str.split(sepRegex);
+  }
+
+  toNumberArray(strArr) {
+    // 입력된 값을 검증하여 숫자 배열로 만들어주는 함수
+    const numArr = strArr.map((v) => {
+      if (this.isValid(v)) {
+        return +v;
+      }
+    });
+
+    return numArr;
+  }
+
+  isValid(str) {
+    // 정규표현식을 사용하여 양수인지 확인 후 아닐경우 오류 발생
+    const numRegex = /^[0-9]+$/;
+    if (numRegex.test(str)) {
+      return true;
+    }
+    if (+str < 0) throw Error("잘못된 입력입니다. 숫자는 양수만 입력해주세요!");
+    throw Error("잘못된 입력입니다. 커스텀 구분자를 확인해주세요!");
   }
 }
 
