759bc180fc8a1cca8a2adacdc3e6861d82151cac
Jee Hwan <jeehwan98@naver.com>
2024-10-16 16:26:04 +0900

    입력 값 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() { }
+  async run() {
+    const INPUTTED_NUMBER = await Console.readLineAsync('덧셈할 문자열을 입력하세요:\n');
+    console.log('입력한 값은:', INPUTTED_NUMBER);
+  }
 }
 
 export default App;
