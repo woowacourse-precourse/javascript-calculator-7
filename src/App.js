da6e04ccb01e68fe65bc370a65b39912b3a3ee4a
최영빈 <bin778@naver.com>
2024-10-16 16:23:14 +0900

    feat: Add Exception Handling Test and Index Check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class StrParts {
-  // 첫 번째 배열 분리
   async FirstParts(str, idx) {
     const FIRST_STR = str.substring(0, idx + 1);
 
@@ -19,53 +18,65 @@ class StrParts {
     return REPLACED_STR.split(REGEX).filter(Boolean);
   }
 
-  // 마지막 배열 분리
   async LastParts(str, idx) {
     const LAST_STR = str.substring(idx + 1);
-    return LAST_STR.match(/\d+|[^a-zA-Z0-9\s]/g);
+    return LAST_STR.match(/\d+|[^a-zA-Z0-9\s]/g).map((item) =>
+      item.match(/\d+/) ? Number(item) : item
+    );
   }
 }
 
 class App {
   async run() {
-    // 1. 사용자로부터 덧셈을 연산할 문자열을 입력한다.
-    const CALCULATOR_STR = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해주세요.\n"
-    );
+    const ERROR = "[ERROR]";
+    try {
+      const CALCULATOR_STR = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해주세요.\n"
+      );
 
-    // 2. 구분자 배열과 숫자 배열을 각각 따로 사용하여 처리한다.
-    // 구분자, 숫자 배열
-    const SEPARATOR_ARR = [",", ":"];
-    const NUM_ARR = CALCULATOR_STR.match(/\d+/g).map(Number);
+      // 구분자, 숫자 배열
+      const SEPARATOR_ARR = [",", ":"];
+      const NUM_ARR = CALCULATOR_STR.match(/\d+/g).map(Number);
 
-    // 'n' 기준 문자 인덱스 생성
-    const LAST_SEPARATOR_INDEX = CALCULATOR_STR.lastIndexOf("n");
+      // 'n' 기준 문자 인덱스 생성
+      const LAST_SEPARATOR_INDEX = CALCULATOR_STR.lastIndexOf("n");
 
-    // '\n'를 기준으로 배열 분리하기
-    const STR_PARTS = new StrParts();
-    let FIRST_PARTS = [];
-    if (LAST_SEPARATOR_INDEX > 0) {
-      FIRST_PARTS = await STR_PARTS.FirstParts(
+      // '\n'를 기준으로 배열 분리하기
+      const STR_PARTS = new StrParts();
+      let FIRST_PARTS = [];
+      if (LAST_SEPARATOR_INDEX > 0) {
+        FIRST_PARTS = await STR_PARTS.FirstParts(
+          CALCULATOR_STR,
+          LAST_SEPARATOR_INDEX
+        );
+
+        // '//'와 '\n' 사이에 있는 문자 추가하기
+        for (let i = 0; i < FIRST_PARTS.length; i += 3) {
+          if (FIRST_PARTS[i] === "//" && FIRST_PARTS[i + 2] === "\n")
+            SEPARATOR_ARR.push(FIRST_PARTS[i + 1]);
+          else throw ERROR;
+        }
+      }
+
+      const LAST_PARTS = await STR_PARTS.LastParts(
         CALCULATOR_STR,
         LAST_SEPARATOR_INDEX
       );
 
-      // '//'와 '\n' 사이에 있는 문자 추가하기
-      for (let i = 0; i < FIRST_PARTS.length; i += 3) {
-        if (FIRST_PARTS[i] === "//" && FIRST_PARTS[i + 2] === "\n")
-          SEPARATOR_ARR.push(FIRST_PARTS[i + 1]);
-        // 에러 예외 처리 해아 함
+      // 마지막 배열의 모든 문자 요소가 구분자 배열에 포함되있는 지 확인
+      for (let PARTS = 1; PARTS < LAST_PARTS.length; PARTS += 2) {
+        if (!SEPARATOR_ARR.includes(LAST_PARTS[PARTS])) {
+          throw ERROR;
+        }
       }
+
+      // 모두 일치하면 배열의 모든 숫자의 합 출력
+      const RESULT = NUM_ARR.reduce((acc, val) => acc + val);
+      Console.print("결과 : " + RESULT);
+    } catch (err) {
+      console.error(ERROR);
+      // console.error(err);
     }
-
-    const LAST_PARTS = await STR_PARTS.LastParts(
-      CALCULATOR_STR,
-      LAST_SEPARATOR_INDEX
-    );
-
-    console.log(LAST_PARTS);
-    console.log(SEPARATOR_ARR);
-    console.log(NUM_ARR);
   }
 }
 
