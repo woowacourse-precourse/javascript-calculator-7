12ad1a017789b19bde89a96229c86bef5b509b21
kimdayeon37 <kdy37912@naver.com>
2024-10-16 22:14:59 +0900

    feat: 커스텀 구분자 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요. \n"
+      );
       const result = this.calculateSum(input);
       Console.print(`결과: ${result}`);
     } catch (error) {
@@ -12,14 +14,19 @@ class App {
   }
 
   calculateSum(input) {
-
-    let defaultDelimiter = /[,:]/;
+    let defaultDelimiter = /[,:]/; // 기본 구분자 , :
     let numberString = input;
-
-    // `split`를 사용하여 문자열을 구분
-    const numbers = numberString.split(defaultDelimiter);
     
-    return numbers
+    // '\n' 문자를 실제 개행 문자로 교체
+    input = input.replace(/\\n/g, '\n');
+
+    // 커스텀 구분자인지 확인
+    if (input.startsWith('//')) {
+      const delimiterEnd = input.indexOf('\n');
+      const customDelimiter = input.slice(2, delimiterEnd);
+      defaultDelimiter = new RegExp(`[${customDelimiter}]`);
+    }
+    return     
   }
 }
 
