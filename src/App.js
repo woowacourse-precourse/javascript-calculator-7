8be5e7fdd1395bbfa6194072cd146fc48a4bf5c8
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-16 13:02:30 +0900

    docs: '쉼표(,) 구분자를 기준으로 문자열을 분리한다.'에 대한 상세 요구사항 추가 작성

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ class App {
       accumulator + number, initialValue);
   }
 
-  // 쉼표(,) 구분자를 만나기 전까지, 문자열을 더한다.
-  // 쉼표(,) 구분자를 만나면, 문자열을 배열에 넣는다.
-  // 마지막 쉼표(,) 구분자를 기준으로 뒤에 문자열은 배열에 넣는다.
   splitStringByDelimiter(inputString) {
     let currentToken = '';
     let TokenArray = [];
