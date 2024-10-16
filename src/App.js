a4b94196eab065018b590692e23839861847771e
minseok <alstjrwjd99@naver.com>
2024-10-16 11:08:56 +0900

    feat(split): 구분자를 기준으로 문자열을 숫자로 분리
    - 문자열에서 ,와 :을 구분자로 하여 숫자를 분리하는 기능 구현
    - 구분자를 배열로 관리하여 구분자 추가 시 확장성을 고려함

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const Console = MissionUtils.Console;
-
+    const separators = [',', ':'];
     const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
-    Console.print(`결과 : ${userInput}`);
+    let numbers = separators.reduce((acc, separator) => {
+      return acc.flatMap(item => item.split(separator));
+    }, [userInput]);
+
+    numbers = numbers.map(Number);    
+    const resultSum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+    Console.print(`결과 : ${resultSum}`);
   }
 }
 
