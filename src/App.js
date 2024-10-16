85f52a25d39f8f846a0af3e186d3f49a60cdf6c0
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-16 13:29:46 +0900

    feat : 커스텀 구분자가 있는지 확인하고 커스텀 구분자를 기존 구분자에 추가

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ class App {
     // 입력 메세지 출력 및 입력 받기
     const INPUT = await Console.readLineAsync(Messages.INPUT_MESSAGE);
 
+    // 커스텀 구분자가 있는지 확인
+    const CUSTOM_DELIMITER = this.checkCustomDelimiter(INPUT);
+
+    // 정규식을 동적으로 생성
+    const delimiterRegex = new RegExp(`[,:${CUSTOM_DELIMITER}]`);
+
     // 입력 받은 문자열을 숫자로 변환
-    const numbers = INPUT.split(/[, :]/).map(Number);
+    const numbers = INPUT.split(delimiterRegex).map(Number);
 
     //에러처리
     // 숫자가 아닌 문자열이 포함되어 있는 경우
@@ -19,6 +25,20 @@ class App {
     // 음수가 포함되어 있는 경우
     if (numbers.some((number) => number < 0)) {
       throw new Error(Errors.HAS_NEGATIVE);
+    }
+  }
+
+  // 커스텀 구분자가 있는지 확인하는 함수
+  checkCustomDelimiter(INPUT) {
+    // 커스텀 구분자가 있는지 확인
+    if (INPUT.startsWith("//")) {
+      if (INPUT.includes("\n")) {
+        // //와 \n 사이의 문자열을 구분자로 사용
+        const CUSTOMDELIMITER = INPUT.slice(2, INPUT.indexOf("\n"));
+        return CUSTOMDELIMITER;
+      }
+      // 커스텀 구분자가 잘못된 경우
+      throw new Error(Errors.WRONG_CUSTOM_DELIMITER);
     }
   }
 }
