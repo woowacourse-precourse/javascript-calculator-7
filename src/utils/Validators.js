bd432db940e3d57f7e8c3aa2f3c51eb4747846cd
yooju00 <yooju00@naver.com>
2024-10-16 20:52:28 +0900

    fix: 양수만 허용하도록 Validators 로직 변경
    - Validators 클래스의 정규표현식에서 음수 허용 부분 제거
    - 에러 메세지 형식 수정 ("[ERROR]")

--- src/utils/Validators.js
+++ src/utils/Validators.js
@@ -1,11 +1,11 @@
 class Validators {
     static CUSTOM_SEPARATOR_REGEX = /^\/\/(.)\n/;
-    static NUMBER_AND_SEPARATOR_REGEX = /^-?\d+([,:]|-?\d+)*$/;
+    static NUMBER_AND_SEPARATOR_REGEX = /^\d+([,:]|\d+)*$/;
 
     static validateInput(input) {
         // 1. 빈 입력 검사
         if (input.trim() === '') {
-            throw new Error('입력값이 비어 있습니다.');
+            throw new Error('[ERROR] 입력값이 비어 있습니다.');
         }
 
         // 2. 커스텀 구분자 확인
@@ -14,12 +14,12 @@ class Validators {
             // 2-1. 커스텀 구분자가 있는 경우
             const numberPart = input.split('\n')[1];
             if (!this.isValidNumberString(numberPart, customSeparator[1])) {
-                throw new Error('유효하지 않은 입력값입니다.');
+                throw new Error('[ERROR] 유효하지 않은 입력값입니다.');
             }
         } else {
             // 2-2. 기본 구분자만 있는 경우
             if (!this.isValidNumberString(input)) {
-                throw new Error('유효하지 않은 입력값입니다.');
+                throw new Error('[ERROR] 유효하지 않은 입력값입니다.');
             }
         }
     }
@@ -27,7 +27,7 @@ class Validators {
     static isValidNumberString(str, customSeparator = null) {
         if (customSeparator) {
             // 커스텀 구분자를 사용하는 경우: 새로운 정규식으로 검사
-            const regex = new RegExp(`^-?\\d+(${customSeparator}-?\\d+)*$`);
+            const regex = new RegExp(`^\\d+(${customSeparator}\\d+)*$`);
             return regex.test(str);
         }
         // 기본 구분자를 사용하는 경우
