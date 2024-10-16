b8e37ec780fb4a0d6af541037fc9c78c80b745cb
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 01:53:55 +0900

    :sparkles: feat: 유효성 검증 추가

--- src/models/extractnumber.js
+++ src/models/extractnumber.js
@@ -1,3 +1,5 @@
+import validateInput from "./validateinput.js";
+
 function extractSeparator(input) {
   if (input.startsWith("//")) {
     const SEPARATORENDINDEX = input.indexOf("\n");
@@ -9,6 +11,10 @@ function extractSeparator(input) {
 }
 
 function extractNumbers(input) {
+  if (!validateInput(input)) {
+    return [0]; //필요할 시 처리
+  }
+
   if (!input) return [0]; // 빈 입력일 경우 0을 반환
 
   const SEPARATOR = extractSeparator(input);
