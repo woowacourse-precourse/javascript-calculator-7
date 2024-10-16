24a38026dded24fd66e580bc0be417ea7cd3c50f
YujeongLEE <dldbwjd24@gmail.com>
2024-10-16 23:24:15 +0900

    feat(input) : 기능 리스트1 - 입력값 받기
    feat(input) : 기능 구현 리스트1 - 입력값 받기
    
    - Console.readLineAsync를 이용한 사용자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import {Console} from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+
+    async run() {
+        //instruction message
+        Console.print("덧셈할 문자열을 입력해 주세요.")
+
+        let input = ""
+        const output = Console.readLineAsync(input)
+
+        output.then((value) => {
+            Console.print(value) //입력값 임시 확인용 출력
+        }).catch(() => {
+            Console.print("[ERROR] 문자열을 입력해주세요.")
+        })
+
+    }
 }
 
 export default App;
