f8cac636cfeeb7d8853935936fbe18bda3875b2d
8leamnk <silkviewrk@gmail.com>
2024-10-16 11:57:28 +0900

    test: 계산 기능 테스트
    테스트 내용:
    - 양수로 이루어진 배열을 입력하면 양수들의 총합이 반환되는지 테스트

---
+++ __tests__/CalculatorTest.js
@@ -0,0 +1,15 @@
+import Calculator from '../src/domain/Calculator';
+
+describe('계산 기능 테스트', () => {
+  test('양수로 이루어진 배열을 입력하면 원소의 총합이 반환된다.', () => {
+    // given
+    const NUMBERS = [1, 2, 3, 4, 5];
+    const TOTAL = 15;
+
+    // when
+    const sum = new Calculator(NUMBERS).add();
+
+    // then
+    expect(sum).toBe(TOTAL);
+  });
+});
