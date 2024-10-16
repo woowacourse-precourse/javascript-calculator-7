2468cb3fbfeb662524551c26b31f919f698fc504
hanheel <hanhee630630@gmail.com>
2024-10-16 16:20:50 +0900

    docs(README.md): 연산 전 오류 검증 부분 보완

--- src/model/SplitModel.js
+++ src/model/SplitModel.js
@@ -1,8 +1,6 @@
 class SplitModel {
   constructor() {}
   stringSplit(delimiter = /[:,]/, operationText) {
-    console.log(`delimiter: ${delimiter.toString()}`);
-    console.log(`operationText: ${operationText}`);
     const splitResult = operationText.split(delimiter);
     //임시 출력, 삭제 예정
     splitResult.map((i) => console.log(i));
