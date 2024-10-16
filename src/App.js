620877aecce85edea8e994d8b4edd4caef3c7b67
young24n <won8560@gmail.com>
2024-10-16 14:32:29 +0900

    feat: 사용자 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      const INPUT_DATA = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      // MissionUtils.Console.print(INPUT_DATA)
+      
+    } catch (error) {
+
+    }  
+  }
 }
 
 export default App;
