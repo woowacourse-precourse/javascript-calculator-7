0b79bcd6dbd13d30697759bf3e36551a06afdc30
smimdla0205 <sangmin0205@gmail.com>
2024-10-16 20:06:49 +0900

    feat(calculator): 음수 및 잘못된 값 입력 시 에러 처리 기능 추가
    다음과 같은 경우 에러를 처리하는 기능을 추가했습니다:
    1. 음수 입력: '-1,2,3' -> [ERROR]
    2. 숫자가 아닌 값 입력: '1,a,3' -> [ERROR]
    3. 잘못된 커스텀 구분자: '//;n1;2;3' -> [ERROR]
    4. 빈 문자열 포함: '1,,2,,3' -> [ERROR]
    
    기능 요구사항을 맞추기 위해 각 에러 메시지를 [ERROR]로 통일했고,
    각 에러 상황을 구분해 if문을 통해 구체적으로 처리했습니다.

--- src/App.js
+++ src/App.js
@@ -23,16 +23,31 @@ class App {
     if (input.startsWith("//")) {
       const delimiterEndIndex = input.indexOf("\\n");
 
+      if (delimiterEndIndex === -1) {
+        throw new Error("[ERROR]");
+      }
+
       const customDelimiter = input.substring(2, delimiterEndIndex);
+
       numbersString = input.substring(delimiterEndIndex + 2);
+
       delimiter = customDelimiter;
     }
 
     const numbers = numbersString.split(delimiter).map((num) => {
-      const parsedNumber = Number(num.trim());
+      const trimmedNum = num.trim();
 
-      return parsedNumber;
+      if (trimmedNum === "") {
+        throw new Error("[ERROR]");
+      } else if (isNaN(Number(trimmedNum))) {
+        throw new Error("[ERROR]");
+      }
+      return Number(trimmedNum);
     });
+
+    if (numbers.some((num) => num < 0)) {
+      throw new Error("[ERROR]");
+    }
 
     return numbers.reduce((acc, curr) => acc + curr, 0);
   }
