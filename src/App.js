24bd4f30f88fb59f80637d5c73ca0b938184092d
minseok <alstjrwjd99@naver.com>
2024-10-16 11:52:13 +0900

    feat(newSeparator): 커스텀 구분자 추가 기능 구현
    - 사용자 입력 시 //와 n 사이에 위치하는 문자를 커스텀 구분자로 사용
    - 여러 개의 커스텀 구분자를 처리할 수 있도록 구현

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ class App {
   async run() {
     const Console = MissionUtils.Console;
     const separators = [',', ':'];
-    const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    let userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+    while (userInput.startsWith('//')) {
+      const end = userInput.indexOf('\\n');
+      if (end === -1) break
+      const newSeparator = userInput.slice(2,end);
+      separators.push(newSeparator);
+      userInput = userInput.slice(end+2)
+    }
 
     let numbers = separators.reduce((acc, separator) => {
       return acc.flatMap(item => item.split(separator));
