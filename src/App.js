081ec5c1fd603405bf42b3c337ca8f7e309e5702
H-sooyeon <wndqhr__@naver.com>
2024-10-16 16:56:08 +0900

    feat: 덧셈할 문자열 입력 기능 추가
    - woowacourse에서 제공하는 Console 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  solve = (input) => {};
+
+  async run() {
+    // 사용자로부터 덧셈할 문자열을 입력받는다.
+    Console.readLineAsync("덧셈할 문자열을 입력해 주세요.").then((input) => {
+      this.solve(input);
+    });
+  }
 }
 
 export default App;
