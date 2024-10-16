d3cee456b3e4a0bb92288b37939d3de99251f315
jongbin26 <010jjj@naver.com>
2024-10-16 15:21:41 +0900

    refactor(utils): 구분자 배열 함수 로직 통합 - 분리된 기본 구분자와 커스텀 구분자 로직을 통합했습니다. getSeperator 함수 내부에서 상수로 선언한 기본 구분자로 초기화를 진행했습니다. - 커스텀 구분자 여부를 확인하는 함수, hasCustomSeperator 함수를 추가했습니다. if문 내에서 반복적으로 검사하던 조건을 재사용할 수 있게 됐습니다.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,11 @@
-import {SEPERATOR_STRING,DEFAULT_SEPERATOR} from '../constant/index.js'
+import {
+    SEPERATOR_STRING,
+    DEFAULT_SEPERATOR,
+} from '../constant/index.js'
 
-export function initSeperatorArray(){
-    const seperator = [];
-    DEFAULT_SEPERATOR.map((element)=> {
-        seperator.push(element);
-    })
-    return seperator;
-}
-
-export function getSeperatorArray(expression, seperator){
-    if(expression.startsWith(SEPERATOR_STRING.START)){
+function getSeperatorArray(expression){
+    const seperator = DEFAULT_SEPERATOR;
+    if(hasCustomSeperator(expression)){
         const seperatorStartIndex = expression.indexOf(SEPERATOR_STRING.START) + 2;
         const seperatorEndIndex = expression.indexOf(SEPERATOR_STRING.END);
         seperator.push(expression.slice(seperatorStartIndex, seperatorEndIndex))
@@ -17,10 +13,19 @@ export function getSeperatorArray(expression, seperator){
     return seperator;
 }
 
-export function getExpressionString(expression){
-    if(expression.startsWith(SEPERATOR_STRING.START)){
+function getExpressionString(expression){
+    if(hasCustomSeperator(expression)){
         const expressionStartIndex = expression.indexOf(SEPERATOR_STRING.END) + 2;
         return expression.slice(expressionStartIndex);
     }
     return expression;
+}
+
+function hasCustomSeperator(expression){
+    return expression.startsWith(SEPERATOR_STRING.START) && expression.includes(SEPERATOR_STRING.END);
+}
+
+export {
+    getSeperatorArray,
+    getExpressionString
 }
