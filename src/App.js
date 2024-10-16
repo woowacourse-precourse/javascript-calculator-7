422eaac9f0daeacf80bec703f24f2ba9a925442e
govl0407 <govl0407@gmail.com>
2024-10-16 22:02:33 +0900

    docs : 구현 완료된 기능, 구현할 기능 추가
    feat : 기본 덧셈 계산기 구현
    
    문자열 입력기능
    입력받은 문자열을 기본 구분자를 기준으로 split
    split한 값을 다 더해서 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    let str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");//사용자 입력
+
+    let splitter = new RegExp(`[,:]+`);// 구분자
+    let nums = str.split(splitter);//구분자를 기준으로 문자열을 split
+    let sum = 0;//합을 저장할 변수
+
+    //배열에 있는 수를 sum에 더해줌
+    nums.forEach(num => {
+      sum += Number(num);
+    })
+    
+    //결과값 출력
+    Console.print("결과 : ");
+    Console.print(sum);
+  }
 }
 
 export default App;
