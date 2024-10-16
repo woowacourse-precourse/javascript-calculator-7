87eb4e900eb2c9434f89ad7b5035047cce354de0
Jinny <jinny@Jinnyui-MacBookPro.local>
2024-10-16 15:12:10 +0900

    feat: 1. '입력' 요구 사항 기능 구현 - App.js에 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+import UserInput from './View/UserInput.js';
+
 class App {
-  async run() {}
+  async run() {
+    Console.print(UserInput);
+  }
 }
 
 export default App;
--- src/View/UserInput.js
+++ src/View/UserInput.js
@@ -1,3 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
 
 let userInput = Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+
+export default userInput;
