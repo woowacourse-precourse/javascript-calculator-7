47887c7818822156d8d3f447c20ecb51ff871f93
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-16 12:34:50 +0900

    feat : Errors.js에 에러 메세지 추가

--- src/constants/Errors.js
+++ src/constants/Errors.js
@@ -1,3 +1,8 @@
-const Errors = Object.freeze({});
+const Errors = Object.freeze({
+  NO_COMMA_AND_COLON: "[ERROR] 숫자 사이에 콤마(,)나 콜론(:)이 없습니다.",
+  WRONG_CUSTOM_DELIMITER: "[ERROR] 커스텀 구분자가 잘못되었습니다.",
+  NOT_NUMBER: "[ERROR] 숫자가 아닌 문자가 있습니다.",
+  NO_NEGATIVE: "[ERROR] 음수가 있습니다.",
+});
 
 export default Errors;
