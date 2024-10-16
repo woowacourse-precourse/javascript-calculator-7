bbb67e4b6fdd786a72727a121002720b343fb014
Hina0222 <hina0222@naver.com>
2024-10-17 01:28:24 +0900

    feat(App) : 문자열 입력 없을 때 0 반환, 잘못된 값 입력시 error 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열 입력: ");
-    if (input == '') return 0;
+    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    if (input == '') return MissionUtils.Console.print(`결과 : 0`);;
+
     const COLON = ':';
     const COMMA = ',';
     const regex = /\/\/(.*)\\n/;
     const custom = input.match(regex);
     const customSeperate = custom ? custom[1] : '';
 
-    let inputArr = input.split('');
-
-    let newInput = inputArr.map(str => {
+    let newInput = input.split('').map(str => {
       if (str === COLON || str === COMMA || str === customSeperate) {
         return " ";
       }
       return str;
     });
 
-    console.log(newInput);
+    let strInput = '';
 
-    MissionUtils.Console.print(`입력 값: ${input}`);
+    if (customSeperate) {
+      strInput = newInput.slice(5).join('').split(/\s+/);
+    } else {
+      strInput = newInput.join('').split(/\s+/);
+    }
+    const result = strInput.reduce((acc, cur) => acc + Number(cur), 0);
+
+    if (strInput.some(num => (isNaN(num) || num <= 0))) {
+      throw new Error("[ERROR]");
+    } else {
+      MissionUtils.Console.print(`결과 : ${result}`);
+    }
   }
 }
 
