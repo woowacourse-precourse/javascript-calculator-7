41c7d6daded3d3053647bf0fab984d41d22de39e
df2854 <df2854@icloud.com>
2024-10-16 15:16:47 +0900

    feat: basic sum function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const string = await this.getString();
-    // Console.print(string); 테스트용 출력
+    const nums = await this.basicSumNums(string);
+    Console.print("결과 : "+ nums);
   }
 
   // 사용자 입력 받기
@@ -11,9 +12,17 @@ class App {
       const string = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
       return string;
     } catch (error) {
-      
+      throw new Error('[ERROR]');
     }
   }
+
+  // 쉼표와 콜론 덧셈
+  async basicSumNums(arrayNum) {
+    const nums = await arrayNum.split(/,|:/).map(function(num){return parseInt(num);});
+    const sumNum = await nums.reduce((a, b) => a + b);
+    return sumNum
+  }
+  
 }
 
 export default App;
