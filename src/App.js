6340e3b811a8d785f1f2002da4433ed9f36d982d
Teayeon-Lim <wingofyeon@naver.com>
2024-10-16 20:06:11 +0900

    feat(calculator): add addtion of default separator
    기본 구분자를 가진 문자열의 덧셈.
    
    default separator:
    - comma( , )
    - colon( : )

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  #text;
+
+  plus(n1, n2) {
+    return BigInt(n1) + BigInt(n2);
+  }
+
+  sum(array) {
+    return array.reduce((total, number) => this.plus(total, number), 0);
+  }
+
+  print(result) {
+    Console.print(`결과 : ${result}`);
+  }
+
+  async run() {
+    this.#text = (
+      await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+    ).trim();
+
+    if (this.#text === "") {
+      this.print(0);
+      return;
+    }
+
+    let separatorRegex = ",|;";
+
+    const allSeparatorRegex = new RegExp(separatorRegex, "g");
+    const numbers = this.#text.split(allSeparatorRegex);
+
+    const result = this.sum(numbers);
+
+    this.print(result);
+  }
 }
 
 export default App;
