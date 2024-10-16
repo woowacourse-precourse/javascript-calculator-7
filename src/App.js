e2b8d97712879108dad8c4aa1f6d5c0fc794e5c8
백지윤 <jy.baek079@gmail.com>
2024-10-17 04:42:03 +0900

    feat: 빈 문자열 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 
+// 빈 문자열 처리
+function handleEmptyInput(input) {
+  return input === '' ? 0 : null;
+}
+
 // 기본 구분자로 문자열 분리 및 합산
 function sumWithDefaultDelimiters(input) {
   const delimiters = /[,|:]/;
@@ -7,12 +12,20 @@ function sumWithDefaultDelimiters(input) {
   return numbers.reduce((acc, num) => acc + num, 0);
 }
 
+function sum(input) {
+  const result = handleEmptyInput(input);
+  if (result !== null) {
+    return result;
+  }
+  return sumWithDefaultDelimiters(input);
+}
+
 class App {
   async run() {
     const input = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요.\n',
     );
-    const result = sumWithDefaultDelimiters(input);
+    const result = sum(input);
     Console.print(`결과 : ${result}`);
   }
 }
