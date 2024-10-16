4c1dc98179c0594edaeb794b2a5b1ac116a7465f
youngju6143 <youngju6143@naver.com>
2024-10-16 16:58:34 +0900

    fix: 1. @woowacourse/mission-utils 사용하여 입출력 기능 구현으로 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import { readFileSync } from 'fs';
 import readline from 'readline';
+import { Console } from "@woowacourse/mission-utils";
+
+// 1. 입출력 기능 -  입력 : '덧셈할 문자열을 입력해 주세요.\n', 한 줄로 입력받기
+async function getString() {
+  try {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    return input;
+  } catch (err) {
+    Console.print(err)
+  }
+}
 
 class App {
   async run() {
-    // 1. 입출력 기능 -  입력 : '덧셈할 문자열을 입력해 주세요.\n', 한 줄로 입력받기
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    })
-    
-    rl.question('덧셈할 문자열을 입력해 주세요.\n', (input) => {
+    try {
+      // 1. 입출력 기능 -  입력
+      const input = await getString();
+
       // 결과 출력
-      console.log(`결과 : ${input}`); // 후에 출력값으로 바뀔 예정
-      rl.close();
-    })
+      Console.print(`결과 : ${input}`); // 후에 출력값으로 바뀔 예정
+      return;
+    } catch(err) {
+      Console.print(err); // 임시 로직
+    }
   }
+  
 }
 
 export default App;
