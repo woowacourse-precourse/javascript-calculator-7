ce9046075d9aee27e4a84ea72293b032210c1f74
yoo_min <vendetta12@naver.com>
2024-10-16 20:18:02 +0900

    feat: 빈 문자열 입력 시 에러 메세지 출력

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요."
     );
-    Console.print(`결과 : ${input}`);
+    isEmptyString(input);
   }
 }
+
+function isEmptyString(input) {
+  const ERROR_MESSAGE = "ERROR : 빈 문자열이 입력되었습니다";
+  if (input.trim() === "") {
+    Console.print(ERROR_MESSAGE);
+    return false;
+  }
+  return true;
+}
+
 
 export default App;
