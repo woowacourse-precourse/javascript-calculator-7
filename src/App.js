c3bc24779dd8c1cc8b020559f47c996753a865e3
정해성 <gotjd0230@naver.com>
2024-10-17 01:08:53 +0900

    feat: 최대 길이 에러 처리 및 함수명 변경

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ const DEFAULT_SEPARATORS = [',', ':'];
 
 const sum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
 
-const parse = (str, sep) => sep.reduce((acc, cur) => acc.replaceAll(cur, ','), str).split(',').map(Number);
+const separate = (str, sep) => sep.reduce((acc, cur) => acc.replaceAll(cur, ','), str).split(',').map(Number);
 
-const customChecked = (str) => str.includes('//') ? [str.substring(5), str.substr(2, 1)] : [str, null];
+const customParse = (str) => str.includes('//') ? [str.substring(5), str.substr(2, 1)] : [str, null];
+
+const checkMaximumLength = (line) => {
+  if (line.length > 15) throw('[Error] 최대 글자 제한인 15자를 초과하였습니다.');
+}
+
+
+const check = (line) => {
+  checkMaximumLength(line);
+}
 
 class App {
   async run() {
     const separators = [...DEFAULT_SEPARATORS];
 
     const line = await Console.readLineAsync('');
-    const [checkedLine, customSeparator] = customChecked(line);
+    check(line);
+
+    const [parsedLine, customSeparator] = customParse(line);
     separators.push(customSeparator);
 
-    const numbers = parse(checkedLine, separators);
+    const numbers = separate(parsedLine, separators);
     const result = sum(numbers);
 
     Console.print(result);
