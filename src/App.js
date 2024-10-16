61fa6888cfac32c574b6e8e86ef610d36cf455ef
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-16 12:55:22 +0900

    feat : 입력 메세지 출력 및 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+import Messages from "./constants/Messages.js";
+
 class App {
-  async run() {}
+  async run() {
+    // 입력 메세지 출력 및 입력 받기
+    const INPUT = await Console.readLineAsync(Messages.INPUT_MESSAGE);
+
+    //Console.print(INPUT);
+  }
 }
 
 export default App;
