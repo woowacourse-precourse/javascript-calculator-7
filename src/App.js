b6e879d817620b4789274bf8565e9421d1913aa5
yooju00 <yooju00@naver.com>
2024-10-16 20:50:34 +0900

    feat: 결과 출력을 담당하는 OutputHandler 클래스 추가
    - printResult 메소드로 계산 결과 출력 기능 구현
    - printError 메소드로 에러 메시지 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import InputHandler from './InputHandler.js';
 import Validators from './utils/Validators.js';
 import StringParser from "./StringParser.js";
 import Calculator from "./Calculator.js";
+import OutputHandler from "./OutputHandler.js";
 
 class App {
   async run() {
@@ -10,10 +11,10 @@ class App {
       Validators.validateInput(input);
       const numbers = StringParser.parse(input);
       const result = Calculator.sum(numbers);
-
-      console.log('Result:', result);
+      OutputHandler.printResult(result);
     } catch (error) {
-      console.error('[ERROR]', error.message);
+      OutputHandler.printError(error);
+      throw error;
     }
   }
 }
