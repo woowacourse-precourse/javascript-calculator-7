e88f1360c7c0476524520382f9166368ef056fa6
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-16 22:16:22 +0900

    test: 기본구분자로 숫자 파싱 테스트 추가

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,11 +1,21 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { printInputMessage } from './views/CalculatorView.js';
+import { parseNumbers } from './models/CalculatorModel.js';
 
-describe('문자열 계산기', () => {
+// View 테스트
+describe('문자열 계산기 View', () => {
   test('입력 안내 문구 출력', () => {
     const consoleSpy = jest.spyOn(MissionUtils.Console, 'print');
     printInputMessage();
     expect(consoleSpy).toHaveBeenCalledWith('덧셈할 문자열을 입력해 주세요.');
     consoleSpy.mockRestore();
+  });
+});
+
+// model 테스트
+describe('문자열 계산기 model', () => {
+  test('기본 구분자로 구분된 숫자 파싱', () => {
+    const result = parseNumbers('4,5,6,7:8:9');
+    expect(result).toEqual([4, 5, 6, 7, 8, 9]);
   });
 });
