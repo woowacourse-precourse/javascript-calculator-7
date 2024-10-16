96bb80c6b94ac8bb1e29dd65a5ee4122c534360c
백지윤 <jy.baek079@gmail.com>
2024-10-17 04:17:28 +0900

    feat: 기본 구분자로 문자열 분리 및 합산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
+// 기본 구분자로 문자열 분리 및 합산
+function sumWithDefaultDelimiters(input) {
+  const delimiters = /[,|:]/;
+  const numbers = input.split(delimiters).map(Number);
+  return numbers.reduce((acc, num) => acc + num, 0);
+}
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n',
+    );
+    const result = sumWithDefaultDelimiters(input);
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
