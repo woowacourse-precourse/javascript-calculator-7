27a3ff088424e61e62e74ae994281dc10b04d428
yundogi <lvbhkwak@gmail.com>
2024-10-16 12:10:24 +0900

    feat(StringCalculator): add custom separator parsing
    - Implement parseInput method to detect custom separators
    - Use regex to match custom separator format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+class StringCalculator {
+  static CUSTOM_SEPARATOR_REGEX = /^\/\/(.)\n(.*)/;
+  static DEFAULT_SEPARATORS = [',', ':'];
+
+  // 입력 문자열을 파싱하여 구분자와 숫자 문자열 분리
+  static parseInput(input) {
+    const customSeparatorMatch = input.match(this.CUSTOM_SEPARATOR_REGEX);
+    if (customSeparatorMatch) {
+      return {
+        separator: customSeparatorMatch[1],
+        numbers: customSeparatorMatch[2],
+      };
+    }
+    return { separator: this.DEFAULT_SEPARATORS, numbers: input };
+  }
+}
+
 class App {
   async run() {
     try{
