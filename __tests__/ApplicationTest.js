526f079aa8c9472453410045bcde9e8128102971
guesung <gueit214@naver.com>
2024-10-16 14:54:21 +0900

    refactor: `결과 :`를 반복하지 않도록 추상화

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -18,7 +18,7 @@ const getLogSpy = () => {
 
 const runCalcualatorTest = async (input, output) => {
   const inputs = [input];
-  const outputs = [output];
+  const outputs = [`결과 : ${output}`];
   mockQuestions(inputs);
   const logSpy = getLogSpy();
   const app = new App();
@@ -31,31 +31,31 @@ const runCalcualatorTest = async (input, output) => {
 describe("문자열 계산기", () => {
   describe("커스텀 구분자 사용", () => {
     test("숫자 1개 입력", async () => {
-      await runCalcualatorTest("//;\\n1", "결과 : 1");
+      await runCalcualatorTest("//;\\n1", "1");
     });
     test("숫자 1개 입력 (다른 형식)", async () => {
-      await runCalcualatorTest("//;\n1", "결과 : 1");
+      await runCalcualatorTest("//;\n1", "1");
     });
     test("숫자 3개 입력", async () => {
-      await runCalcualatorTest("//v\n1v2v3", "결과 : 6");
+      await runCalcualatorTest("//v\n1v2v3", "6");
     });
     test("숫자 10개 입력", async () => {
-      await runCalcualatorTest("//;\n1;2;3;4;5;6;7;8;9;10", "결과 : 55");
+      await runCalcualatorTest("//;\n1;2;3;4;5;6;7;8;9;10", "55");
     });
     test("두 자리 이상 숫자로 5개 입력", async () => {
-      await runCalcualatorTest("//;\\n10;20;30;40;50", "결과 : 150");
+      await runCalcualatorTest("//;\\n10;20;30;40;50", "150");
     });
   });
 
   describe("기본 구분자 사용", () => {
     test("숫자 1개 입력", async () => {
-      await runCalcualatorTest("1", "결과 : 1");
+      await runCalcualatorTest("1", "1");
     });
     test("숫자 3개 입력", async () => {
-      await runCalcualatorTest("1,2:3", "결과 : 6");
+      await runCalcualatorTest("1,2:3", "6");
     });
     test("숫자 10개 입력", async () => {
-      await runCalcualatorTest("1,2:3,4:5,6:7,8:9,10", "결과 : 55");
+      await runCalcualatorTest("1,2:3,4:5,6:7,8:9,10", "55");
     });
   });
 
