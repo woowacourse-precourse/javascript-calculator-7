d99fbfa121db0aae92fd7f900afef1bf0db5e7cf
guesung <gueit214@naver.com>
2024-10-16 14:30:24 +0900

    test: 커스텀 구분자 사용의 각 테스트 케이스 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -17,8 +17,8 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
-  describe("커스텀 구분자 사용", async () => {
-    test("숫자 한 개 입력", async () => {
+  describe("커스텀 구분자 사용", () => {
+    test("숫자 1개 입력", async () => {
       const inputs = ["//;\\n1"];
       mockQuestions(inputs);
 
@@ -32,9 +32,65 @@ describe("문자열 계산기", () => {
         expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
       });
     });
+    test("숫자 1개 입력", async () => {
+      const inputs = ["//;\n1"];
+      mockQuestions(inputs);
+
+      const logSpy = getLogSpy();
+      const outputs = ["결과 : 1"];
+
+      const app = new App();
+      await app.run();
+
+      outputs.forEach((output) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      });
+    });
+    test("숫자 3개 입력", async () => {
+      const inputs = ["//;\n1;2;3"];
+      mockQuestions(inputs);
+
+      const logSpy = getLogSpy();
+      const outputs = ["결과 : 6"];
+
+      const app = new App();
+      await app.run();
+
+      outputs.forEach((output) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      });
+    });
+    test("숫자 10개 입력", async () => {
+      const inputs = ["//;\n1;2;3;4;5;6;7;8;9;10"];
+      mockQuestions(inputs);
+
+      const logSpy = getLogSpy();
+      const outputs = ["결과 : 55"];
+
+      const app = new App();
+      await app.run();
+
+      outputs.forEach((output) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      });
+    });
+    test("두 자리 이상 숫자로 5개 입력", async () => {
+      const inputs = ["//;\\n1;2;3;4;5;6;7;8;9;10"];
+      mockQuestions(inputs);
+
+      const logSpy = getLogSpy();
+      const outputs = ["결과 : 55"];
+
+      const app = new App();
+      await app.run();
+
+      outputs.forEach((output) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      });
+    });
   });
-  describe("기본 구분자 사용", async () => {
-    test("숫자 한 개 입력", async () => {
+  describe("기본 구분자 사용", () => {
+    test("숫자 1개 입력", async () => {
       const inputs = ["1"];
       mockQuestions(inputs);
 
@@ -49,7 +105,7 @@ describe("문자열 계산기", () => {
       });
     });
   });
-  describe("예외 테스트", async () => {
+  describe("예외 테스트", () => {
     test("음수 입력", async () => {
       const inputs = ["-1,2,3"];
       mockQuestions(inputs);
