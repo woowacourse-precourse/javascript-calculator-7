22f0476ee054a546d02a763a5ff99a0f2f071ce2
Hina0222 <hina0222@naver.com>
2024-10-17 00:45:31 +0900

    feat(App) : 커스텀 구분자 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열 입력: ");
+
     const COLON = ':';
     const COMMA = ',';
-
-    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열 입력: ");
+    const regex = /\/\/(.*)\\n/;
+    const custom = input.match(regex);
 
     MissionUtils.Console.print(`입력 값: ${input}`);
   }
