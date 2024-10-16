2f37c2af3bbe4ef81cfcc8e0e86d98775df946cc
rhtjddls123 <jjjk0605@naver.com>
2024-10-16 16:33:19 +0900

    feat: 입력된 값을 구분자를 통해 분리하는 기능 추가
    1. 입력된 구분자 배열을 통해 정규표현식 생성
    2. 정규표현식을 사용하여 문자열 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 class App {
-  async run() {}
+  async run() {
+    this.extractInput("1,2:3", [",", ":"]);
+  }
+
+  extractInput(str, sep) {
+    // 입력된 값을 구분자를 통해 분리
+    // 구분자를 이용하여 정규 표현식을 만들어준뒤 split함수를 통해 분리
+    const sepRegex = new RegExp(`[${sep.join("")}]+`);
+    return str.split(sepRegex);
+  }
 }
 
 export default App;
