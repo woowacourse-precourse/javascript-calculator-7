1dbbe8445026ec3b59bb1409204bb11a515c1e53
H-sooyeon <wndqhr__@naver.com>
2024-10-16 17:16:44 +0900

    feat: 입력값이 공백이라면 0 반환
    - 입력값의 문자열 길이가 0이라면 0을 반환

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  solve = (input) => {};
+  splitBySeparator = (input) => {
+    // 입력받은 문자열을 구분자로 나눈다.
+    if (input.length === 0) {
+      return 0;
+    }
+  };
+
+  solve = (input) => {
+    try {
+      const seperatedInput = this.splitBySeparator(input);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   async run() {
     // 사용자로부터 덧셈할 문자열을 입력받는다.
