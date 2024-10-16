33c1cbc4953a21758e917eeb5e83130670e11a63
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-16 21:14:41 +0900

    🔧 refactor(separator): 커스텀 구분자 추출 방식으로 정규표현식 변경
    커스텀 구분자를 추출하는 방식으로 `CUSTOM_SEPARATOR`를 변경했습니다.
    - 기존: `/^\/\/.*\n/` 는 커스텀 구분자 전체를 추출하지 않고, 단순히 구분자 정의 부분만 매칭
    - 변경: `/^\/\/(.*?)\n/` 는 커스텀 구분자 정의 부분을 그룹화하여 추출 가능
    - 최소 매칭(`(.*?)`) 을 사용해, 커스텀 구분자를 추출하고 이후 처리가 가능하도록 개선

--- src/constant.js
+++ src/constant.js
@@ -4,7 +4,7 @@ export const RESULT_MESSAGE = '결과: ';
 
 // 입력 구분자 관련 상수
 export const DEFAULT_SEPARATOR = /,|:/;
-export const CUSTOM_SEPARATOR = /^\/\/.*\n/;
+export const CUSTOM_SEPARATOR = /^\/\/(.*?)\n/;
 
 // 에러 메시지 관련 상수
 export const ERROR_INVALID_INPUT = '[ERROR] 잘못된 입력입니다.';
