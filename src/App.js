bf4fb134c4a9c659efa681cb153ffc29d786f981
Hina0222 <hina0222@naver.com>
2024-10-17 01:00:17 +0900

    feat(App) : 구분자를 공백으로 변경

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열 입력: ");
-
+    if (input == '') return 0;
     const COLON = ':';
     const COMMA = ',';
     const regex = /\/\/(.*)\\n/;
     const custom = input.match(regex);
+    const customSeperate = custom ? custom[1] : '';
+
+    let inputArr = input.split('');
+
+    let newInput = inputArr.map(str => {
+      if (str === COLON || str === COMMA || str === customSeperate) {
+        return " ";
+      }
+      return str;
+    });
+
+    console.log(newInput);
 
     MissionUtils.Console.print(`입력 값: ${input}`);
   }
