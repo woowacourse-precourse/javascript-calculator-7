008a1942e89c1e8d1170b83741419c93fb89a164
bbjbc <jobc4550@gmail.com>
2024-10-16 21:12:09 +0900

    feat: 커스텀 구분자 위치와 여러 개의 구분자 테스트 케이스 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -91,4 +91,49 @@ describe("문자열 계산기", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  test("커스텀 구분자가 맨 앞에 위치하지 않은 경우", async () => {
+    const inputs = ["1,2,3//;\n4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] 커스텀 구분자는 맨 앞에 위치해야 합니다."];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("구분자가 여러 번 사용된 경우", async () => {
+    const inputs = ["1,2,3,,4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] 구분자는 한 번만 사용할 수 있습니다."];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자를 여러 번 사용한 경우", async () => {
+    const inputs = ["//;\n1;;2;3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] 구분자는 한 번만 사용할 수 있습니다."];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
