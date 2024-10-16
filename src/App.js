bba6a70a44589c60e0ac9ca0ce25b872a1fb3d6e
df2854 <df2854@icloud.com>
2024-10-16 16:43:32 +0900

    feat: extract custom separator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
-    const string = await this.getString();
-    const nums = await this.basicSumNums(string);
-    Console.print("결과 : "+ nums);
+    let string = await this.getString();
+    const NUMS = await this.basicSumNums(string);
+    Console.print("결과 : "+ NUMS);
   }
 
   // 사용자 입력 받기
   async getString() {
     try {
-      const string = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      let string = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
       return string;
     } catch (error) {
       throw new Error('[ERROR]');
@@ -17,12 +17,19 @@ class App {
   }
 
   // 쉼표와 콜론 덧셈
-  async basicSumNums(arrayNum) {
-    const nums = await arrayNum.split(/,|:/).map(function(num){return parseInt(num);});
-    const sumNum = await nums.reduce((a, b) => a + b);
-    return sumNum
+  async basicSumNums(arrayString) {
+    let nums = await arrayString.split(/,|:/).map(function(num){return parseInt(num);});
+    const SUM_NUMS = await nums.reduce((a, b) => a + b);
+    return SUM_NUMS;
   }
-  
+
+  // 커스텀 구분자 추출
+  async customSeparator(arrayString) {
+    if (arrayString.indexOf('//') != -1 && arrayString.indexOf('\\n') != -1) {
+      const SEPARATOR = arrayString.substring(arrayString.indexOf('//') + 2, arrayString.indexOf('\\n'));
+      return SEPARATOR;
+    }
+  }
 }
 
 export default App;
