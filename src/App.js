4e79dca77f7dcf30dbbd16a56202ab3b772a644d
yusuShin <tlsdbtn0507@naver.com>
2024-10-16 19:17:20 +0900

    feat(app): 문자열 덧셈을 위한 입력 및 출력 기능 구현
    - Console.readLineAsync를 사용한 사용자 입력 기능 추가
    - 결과를 출력하는 print 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await Console.readLineAsync('문자열 덧셈을 해보아요:');
+    this.print(userInput)
+
+  }
+
+  print(str) {
+    Console.print('결과:' + str);
+  }
 }
 
 export default App;
