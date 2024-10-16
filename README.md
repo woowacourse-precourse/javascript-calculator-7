eb70e4210cc9bb44edf42361c6bc8b4f1835ae36
mindaaaa <devalc735@gmail.com>
2024-10-16 14:58:25 +0900

    chore: set up basic HTML layout for string addition calculator app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript-calculator-precourse
+
+## 문자열 덧셈 계산기(String Addition Calculato)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+// 입력
+// async function getUsername() {
+//   try {
+//     const username = await Console.readLineAsync('닉네임을 입력해주세요.');
+//   } catch (error) {
+//     // reject 되는 경우
+//   }
+// }
+// 출력
+// Console.print('안녕하세요.');
+
 class App {
-  async run() {}
+  async run() {
+    // 1. 입력을 문자열로 전환하거나 문자열만 받음 String()
+    // 2. 정규표현식, includes(), split(), slice(), startsWith() 등으로 :나 ,로 구분된 문자열 기준으로 숫자 합 반환
+    // 3. indexOf, split(), slice()로 커스텀 구분자
+    // 4. 예외처리로 ERROR
+    // - 커스텀 구분자가 앞부분에 선언되었는지와 문자(1)인지
+    // - 별도의 구분자가 들어갔는지
+    // - 양수인지
+  }
 }
 
 export default App;
