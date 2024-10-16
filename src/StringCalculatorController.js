6de6a123f1a3e8698f0b99660a9f663e7844ba2e
ho8ae <xogh2242@gmail.com>
2024-10-16 23:52:33 +0900

    feat : 문자열 덧셈 계산기 컨트롤러 구현

---
+++ src/StringCalculatorController.js
@@ -0,0 +1,23 @@
+import StringCalculator from "./StringCalculator.js";
+import InputValidator from "./InputValidator.js";
+import View from "./View.js";
+
+class StringCalculatorController {
+    constructor() {
+        this.calculator = new StringCalculator();
+    }
+
+    async startCalculation() {
+        try {
+            const input = await View.readInputStirng();
+            const numbers = InputValidator.validateInputString(input);
+            const result = this.calculator.calculate(numbers);
+            View.printResult(result);
+        } catch (error) {
+            View.printErrorMessage(error.message);
+            throw error; // 예외를 다시 던져서 테스트에서 감지할 수 있게 함
+        }
+    }
+}
+
+export default StringCalculatorController;
