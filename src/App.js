e2b16a966738b281512cf149749ca6cca4a88abe
xxziiko <xxziiko@gmail.com>
2024-10-16 15:06:05 +0900

    feat(app): 문자열을 입력받는 기능 추가
    - Console.readLineAsync() 함수를 사용하여 사용자 입력을 비동기로 처리하는 readInput() 함수를 추가했습니다.
    - readInput()는 사용자로부터 입력 받은 문자열을 비동기적으로 반환합니다.
    - 문자열 상수를 관리하는 constants.js 파일 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./constants.js";
+
 class App {
-  async run() {}
+  async readInput() {
+    const userInput = await Console.readLineAsync(MESSAGE.INPUT);
+    return userInput;
+  }
+
+  async run() {
+    const input = await this.readInput();
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const MESSAGE = {
+  INPUT: "덧셈할 문자열을 입력해 주세요. \n",
+};
