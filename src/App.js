318cec96d0cc4d85cbfb7d42aea65606798a3094
dawn0314 <sdawn0314@gmail.com>
2024-10-16 21:50:32 +0900

    feat: add numbers that are split by comma and colon

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ class App {
     if (input === "") {
       throw new Error("[ERROR] 문자열을 입력해주세요");
     }
+
+    let delimiters = /,|:/;
+    let numArray = input.split(delimiters);
+    let sum = 0;
+
+    for (let num of numArray) {
+      if (isNaN(Number(num)) || Number(num) < 0) {
+        throw new Error("[ERROR] 올바른 구분자를 입력하세요.");
+      }
+
+      sum += parseInt(num);
+    }
+
+    return Console.print(`결과 : ${sum}`);
   }
 }
 
