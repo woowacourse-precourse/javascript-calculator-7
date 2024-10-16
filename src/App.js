9b982f1a0eac99bdcb274f77116affc1bfc2a8ae
Xen-alpha <hky7548@naver.com>
2024-10-16 13:45:22 +0900

    feat(App): add mission module demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+const Console = MissionUtils.Console;
+
 class App {
-  async run() {}
+  async run() {
+    Console.print(
+      this.processInput(
+        await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.")
+      )
+    ); // 1단계: 그대로 출력
+  }
+  processInput(inputstr) {
+    const condCustomInput = /(\/\/.{1}\\n.+)/;
+    // 커스텀 구분자가 있는지 확인
+    // 커스텀 구분자가 있을 경우
+    // 커스텀 구분자가 없을 경우
+    return inputstr;
+  }
 }
 
 export default App;
