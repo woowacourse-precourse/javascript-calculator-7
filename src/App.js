7e7221c936d6a99ea25d7ed20c108f0852bc3ac4
Yeongi Park <yeongi0111@naver.com>
2024-10-16 13:55:58 +0900

    feat : 사용자에게 문자열 입력받기 구현 완료
    App.js의 App클래스 run 메서드 내부에 사용자에게 입력 받는 코드 작성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { outputMessage } from './constant.js';
 class App {
   async run() {
     Console.print(outputMessage.startMessage);
+    const userInput = await Console.readLineAsync('');
   }
 }
 
