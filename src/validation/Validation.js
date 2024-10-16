e3b65e9be3e878f9c4db4f7a51b5be5f566ac515
장정안 <wjddks96@naver.com>
2024-10-16 18:05:28 +0900

    feat: 커스텀 구분자에 숫자가 포함된 경우 유효성 검증
    터미널 상에 입력되는 숫자는 string으로 인식이 되어 정규표현식으로 검증 불가하다고 판단하여 따로 로직을 처리해주었습니다

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -4,6 +4,25 @@ import { delimiter } from '../constants/delimiter.js';
 import { ERROR_MESSAGE } from '../constants/errorMessages.js';
 
 class Validation {
+  /**@param {string} input*/
+  //커스텀 구분자 추출함수
+  //터미널 상에 입력되는 숫자는 string으로 인식이 되어 정규표현식으로 검증 불가하다고 판단
+  static extractCustomDelimiter(input) {
+    if (input.startsWith('//')) {
+      const delimiter = input.indexOf('\\n');
+      const hasNumber = input
+        .substring(2, delimiter)
+        .split('')
+        .filter((item) => !isNaN(Number(item)));
+
+      if (hasNumber.length > 0) {
+        throw new Error(
+          `${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.NO_NUMBER_CUSTOM_SEPARATOR}`
+        );
+      }
+    }
+  }
+
   /**
    * @param {string} input
    * @returns {undefined}
@@ -20,7 +39,7 @@ class Validation {
     // 남은 문자들이 모두 기본 구분자인지 확인
     if (!nonDigits.split('').every((char) => delimiter.test(char))) {
       throw new Error(
-        `${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.INVALID_DEFAULT_DELIMITER}`
+        `${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.INVALID_DELIMITER}`
       );
     }
   }
@@ -50,18 +69,17 @@ class Validation {
       );
     }
 
-    // "//"와 "\n" 사이에 숫자가 들어간 경우
-    const customPattern = /^\/\/(\d)\n/;
-    if (customPattern.test(input)) {
-      throw new Error(
-        `${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.NO_NUMBER_CUSTOM_SEPARATOR}`
-      );
-    }
-
     // 입력이 숫자 혹은 "//"로 시작하지 않은 경우
     if (!/^\d|^\/\//.test(input)) {
       throw new Error(
         `${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.INVALID_START_CHARACTER}`
+      );
+    }
+
+    //음수를 입력한 경우
+    if (/-\d/.test(input)) {
+      throw new Error(
+        `${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.NO_NEGATIVE_NUMBER}`
       );
     }
 
@@ -77,7 +95,12 @@ class Validation {
 
     //커스텀 구분자없이 숫자와 기본 구분자를 입력하는데, 그 구분자가 ',' ':'가 아닐경우
     this.validateDefaultDelimiter(input);
+
+    //커스텀 구분자에 숫자가 포함된 경우
+    this.extractCustomDelimiter(input);
     return input;
+
+    /**@todo 소수점 처리, 공백은 0도 생각해보기! */
   }
 }
 
