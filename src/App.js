964d18a8096a89454e36435b55535a5b973ee819
정해성 <gotjd0230@naver.com>
2024-10-16 23:33:34 +0900

    feat: 기본 구분자로 구분된 숫자들을 더하여 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const separators = [',', ':'];
+
+const sum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
+
+const parse = (line) => {
+  line += ',';
+  let buffer = '';
+  const numbers = [];
+
+  for (let i = 0; i < line.length; i += 1) {
+    if (separators.includes(line[i])) {
+      numbers.push(Number(buffer));
+      buffer = '';
+      continue;
+    }
+
+    if (i == line.length - 1) {
+      numbers.push(Number(buffer));
+      continue;
+    }
+
+    buffer += line[i];
+  }
+
+  return numbers;
+}
+
 class App {
   async run() {
     const line = await Console.readLineAsync('');
-    Console.print(line);
+    const numbers = parse(line);
+    const result = sum(numbers);
+
+    Console.print(result);
   }
 }
 
