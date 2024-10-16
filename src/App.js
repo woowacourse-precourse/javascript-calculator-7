48f43ae1a8e4a2da2dea62d2a6277daaee4a30e9
Teayeon-Lim <wingofyeon@naver.com>
2024-10-16 20:20:31 +0900

    feat(error): generate error message from wrong user input
    잘못된 입력을 받으면, "[ERROR]"로 시작되는 메시지와 함께 ERROR 발생
    
    Error Case:
    - 구분자 앞 또는 뒤에 숫자가 없음
    - 구분자에 포함되어 있지 않은 문자
    - 0 또는 음수 포함

--- src/App.js
+++ src/App.js
@@ -11,8 +11,20 @@ class App {
     return array.reduce((total, number) => this.plus(total, number), 0);
   }
 
+  isNaturalNumber(n) {
+    return isNaN(Math.sign(n));
+  }
+
+  isPositiveNumber(n) {
+    return Math.sign(n) > 0;
+  }
+
   print(result) {
     Console.print(`결과 : ${result}`);
+  }
+
+  throwErrorMessage(msg) {
+    throw new Error(`[ERROR] ${msg}`);
   }
 
   async run() {
@@ -32,12 +44,27 @@ class App {
     if (custom && custom.length > 0) {
       custom.forEach(([caturedString, separator]) => {
         this.#text = this.#text.substring(caturedString.length);
+
         separatorRegex += `|${separator}`;
       });
     }
 
     const allSeparatorRegex = new RegExp(separatorRegex, "g");
     const numbers = this.#text.split(allSeparatorRegex);
+
+    for (const n of numbers) {
+      if (n === "") {
+        this.throwErrorMessage("구분자의 앞 또는 뒤에 숫자를 입력하세요");
+      }
+
+      if (this.isNaturalNumber(n)) {
+        this.throwErrorMessage("가능한 구분자가 아닙니다");
+      }
+
+      if (!this.isPositiveNumber(n)) {
+        this.throwErrorMessage("양수를 입력하세요");
+      }
+    }
 
     const result = this.sum(numbers);
 
