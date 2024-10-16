53f192424a6d7cdb607d118b4079cab11c89edf9
Hina0222 <hina0222@naver.com>
2024-10-16 23:56:25 +0900

    feat(App) : 문자열 입력 받기, 입력받은 문자열 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열 입력: ");
+
+    MissionUtils.Console.print(`입력 값: ${input}`);
+  }
 }
 
 export default App;
