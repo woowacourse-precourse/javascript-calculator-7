0aafa06097bc9d257f4a251151b6f76348af962b
장한울 <swingjhw@naver.com>
2024-10-16 13:26:15 +0900

    feat: 빈 입력값이 들어온 경우 0 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {}
+  async run() {
+    const basicDelimiter = ",:";
+    let input = await this.getInput();
+
+    if (input.length === 0) {
+      this.printResult(0);
+    }
+
+    this.printResult(input);
+  }
+
+  findCustomDelimiter(input) {
+    if (input.includes("//") || input.includes("\n")) {
+      if (!input.includes("//") || !input.includes("\n")) {
+        throw new Error("[ERROR] 커스텀 구분자 설정이 잘못되었습니다.");
+      }
+    }
+
+    return input.match(/(?<=\/\/)(.+?)(?=\\n)/g) ?? "";
+  }
+
   getInput() {
     return Console.readLineAsync();
   }
-  print(output) {
-    Console.print(output);
+  printResult(output) {
+    Console.print(`결과 : ${output}`);
   }
 }
 
