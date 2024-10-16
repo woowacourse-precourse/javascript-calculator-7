28a5b278e41b1b9f6ed25d05207f181205caca52
guesung <gueit214@naver.com>
2024-10-16 14:38:07 +0900

    refactor: `runCalcualatorTest`함수를 이용하여 각 테스트케이스를 모듈화

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -16,95 +16,43 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const runCalcualatorTest = async (input, output) => {
+  const inputs = [input];
+  const outputs = [output];
+  mockQuestions(inputs);
+  const logSpy = getLogSpy();
+  const app = new App();
+  await app.run();
+  outputs.forEach((output) => {
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  });
+};
+
 describe("문자열 계산기", () => {
   describe("커스텀 구분자 사용", () => {
     test("숫자 1개 입력", async () => {
-      const inputs = ["//;\\n1"];
-      mockQuestions(inputs);
-
-      const logSpy = getLogSpy();
-      const outputs = ["결과 : 1"];
-
-      const app = new App();
-      await app.run();
-
-      outputs.forEach((output) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-      });
+      await runCalcualatorTest("//;\\n1", "결과 : 1");
     });
-    test("숫자 1개 입력", async () => {
-      const inputs = ["//;\n1"];
-      mockQuestions(inputs);
-
-      const logSpy = getLogSpy();
-      const outputs = ["결과 : 1"];
-
-      const app = new App();
-      await app.run();
-
-      outputs.forEach((output) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-      });
+    test("숫자 1개 입력 (다른 형식)", async () => {
+      await runCalcualatorTest("//;\n1", "결과 : 1");
     });
     test("숫자 3개 입력", async () => {
-      const inputs = ["//;\n1;2;3"];
-      mockQuestions(inputs);
-
-      const logSpy = getLogSpy();
-      const outputs = ["결과 : 6"];
-
-      const app = new App();
-      await app.run();
-
-      outputs.forEach((output) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-      });
+      await runCalcualatorTest("//;\n1;2;3", "결과 : 6");
     });
     test("숫자 10개 입력", async () => {
-      const inputs = ["//;\n1;2;3;4;5;6;7;8;9;10"];
-      mockQuestions(inputs);
-
-      const logSpy = getLogSpy();
-      const outputs = ["결과 : 55"];
-
-      const app = new App();
-      await app.run();
-
-      outputs.forEach((output) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-      });
+      await runCalcualatorTest("//;\n1;2;3;4;5;6;7;8;9;10", "결과 : 55");
     });
     test("두 자리 이상 숫자로 5개 입력", async () => {
-      const inputs = ["//;\\n1;2;3;4;5;6;7;8;9;10"];
-      mockQuestions(inputs);
-
-      const logSpy = getLogSpy();
-      const outputs = ["결과 : 55"];
-
-      const app = new App();
-      await app.run();
-
-      outputs.forEach((output) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-      });
+      await runCalcualatorTest("//;\\n10;20;30;40;50", "결과 : 150");
     });
   });
+
   describe("기본 구분자 사용", () => {
     test("숫자 1개 입력", async () => {
-      const inputs = ["1"];
-      mockQuestions(inputs);
-
-      const logSpy = getLogSpy();
-      const outputs = ["결과 : 1"];
-
-      const app = new App();
-      await app.run();
-
-      outputs.forEach((output) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-      });
+      await runCalcualatorTest("1", "결과 : 1");
     });
   });
+
   describe("예외 테스트", () => {
     test("음수 입력", async () => {
       const inputs = ["-1,2,3"];
