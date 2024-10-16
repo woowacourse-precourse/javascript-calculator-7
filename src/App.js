732f679ad909b08c14e5fee886cb42057d1391ab
minseok <alstjrwjd99@naver.com>
2024-10-16 10:42:31 +0900

    feat(console): 덧셈할 문자열 입출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const Console = MissionUtils.Console;
+
+    const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+    Console.print(`결과 : ${userInput}`);
+  }
 }
 
 export default App;
