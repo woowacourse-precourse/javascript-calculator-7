2eb9ed0189a90a02154f7033161ac1c7765c2444
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 01:28:09 +0900

    :sparkles: feat: 에러메시지 추가

--- src/constant/messages.js
+++ src/constant/messages.js
@@ -8,7 +8,9 @@ const ERROR_WORD = "[ERROR]";
 
 const ERROR_MESSAGE = Object.freeze({
   invalidPositiveNumber: `${ERROR_WORD} 음수는 입력할 수 없습니다.`,
-  invalidString: `${ERROR_WORD} 문자는 입력할 수 없습니다.`,
+  invalidString: `${ERROR_WORD} 입력은 문자열이어야 합니다.`,
+  invalidSeparator: `${ERROR_WORD} 구분자에 유효하지 않은 문자가 포함되어 있습니다.`,
+  invalidCostom: `${ERROR_WORD} 잘못된 커스텀 구분자 형식입니다.`,
 });
 
 export { BASIC_MESSAGES, ERROR_MESSAGE };
