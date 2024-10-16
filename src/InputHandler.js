5f2a55d975996d6740535a47efa0a6504de9197e
yooju00 <yooju00@naver.com>
2024-10-16 20:20:53 +0900

    refactor: 모든 모듈을 클래스로 변경하여 일관성 개선
    - InputHandler, Validators를 클래스로 변경
    - 각 클래스의 메서드를 정적 메서드로 구현
    - 모듈 간 구조적 일관성 확보

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -1,15 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const InputHandler = {
-    async getInput() {
+class InputHandler {
+    static async getInput() {
         const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
         return this.preprocessInput(input);
-    },
+    }
 
-    preprocessInput(input) {
+    static preprocessInput(input) {
         // '\\n'을 '\n'으로 변환
         return input.replace('\\n', '\n').trim();
     }
-};
+}
 
 export default InputHandler;
--- src/utils/Validators.js
+++ src/utils/Validators.js
@@ -1,15 +1,15 @@
-const CUSTOM_SEPARATOR_REGEX = /^\/\/(.)\n/;
-const NUMBER_AND_SEPARATOR_REGEX = /^-?\d+([,:]|-?\d+)*$/;
+class Validators {
+    static CUSTOM_SEPARATOR_REGEX = /^\/\/(.)\n/;
+    static NUMBER_AND_SEPARATOR_REGEX = /^-?\d+([,:]|-?\d+)*$/;
 
-const Validators = {
-    validateInput(input) {
+    static validateInput(input) {
         // 1. 빈 입력 검사
         if (input.trim() === '') {
             throw new Error('입력값이 비어 있습니다.');
         }
 
         // 2. 커스텀 구분자 확인
-        const customSeparator = input.match(CUSTOM_SEPARATOR_REGEX);
+        const customSeparator = input.match(this.CUSTOM_SEPARATOR_REGEX);
         if (customSeparator) {
             // 2-1. 커스텀 구분자가 있는 경우
             const numberPart = input.split('\n')[1];
@@ -22,16 +22,17 @@ const Validators = {
                 throw new Error('유효하지 않은 입력값입니다.');
             }
         }
-    },
-    isValidNumberString(str, customSeparator = null) {
+    }
+
+    static isValidNumberString(str, customSeparator = null) {
         if (customSeparator) {
             // 커스텀 구분자를 사용하는 경우: 새로운 정규식으로 검사
             const regex = new RegExp(`^-?\\d+(${customSeparator}-?\\d+)*$`);
             return regex.test(str);
         }
         // 기본 구분자를 사용하는 경우
-        return NUMBER_AND_SEPARATOR_REGEX.test(str);
+        return this.NUMBER_AND_SEPARATOR_REGEX.test(str);
     }
-};
+}
 
 export default Validators;
