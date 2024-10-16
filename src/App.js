570863cb8d0cd1d7f1083eb9f9d6358d891f2374
Yeongi Park <yeongi0111@naver.com>
2024-10-16 15:49:23 +0900

    feat : 이외의 경우는 예외 처리 구현
    사용자의 입력이 숫자로 시작하지 않거나, 슬래시로 시작하지 않는 경우는
    예외 처리한다

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
-import { outputMessage } from './constant.js';
+import { errorMessage, outputMessage } from './constant.js';
 
 class App {
   async run() {
     Console.print(outputMessage.startMessage);
     const userInput = await Console.readLineAsync('');
     if (this.isStartWithNumber(userInput)) {
-    }
-    if (this.isStartWidthDubbleSlash(userInput)) {
+    } else if (this.isStartWidthDubbleSlash(userInput)) {
+    } else {
+      throw new Error(errorMessage.useNumberOrSlash);
     }
   }
 
