f6fc7c6f6987078ba3649f8c7814dabbf6de9ec8
jeongeunLee <leeju1009@gmail.com>
2024-10-16 16:29:40 +0900

    feat(app): 덧셈 기능을 구현한 calculate 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 class App {
   async run() {}
+
+  calculate(numberArray) {
+    const result = numberArray.reduce((currentSum, targetNumber) => {
+      return currentSum + targetNumber;
+    }, 0);
+
+    return result;
+  }
 }
 
 export default App;
