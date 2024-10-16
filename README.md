422481f92e17705c9dd4549f9ae92556549384c8
youngsu <everdudtn@gmail.com>
2024-10-16 23:33:26 +0900

    docs: README.md 작성

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascript-calculator-precourse
+
+## 구현 기능 목록
+
+- 문자열 입력
--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    Console.print("문자열을 입력해 주세요");
+    const input = await Console.readLineAsync("");
+
+    let numbers;
+
+    numbers = input.split(/,|:/);
+    Console.print(arr);
+  }
 }
 
 export default App;
