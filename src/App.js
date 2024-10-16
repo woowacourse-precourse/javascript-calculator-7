31b02c5311f80021d0885deafa1a214539c78a13
rubberchin <88rubber@gmail.com>
2024-10-16 10:53:57 +0900

    Get string input from a user.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const USER_INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+    //console.log(USER_INPUT);
+    //console.log(typeof(USER_INPUT));
+  }
 }
+
+const APP = new App();
+APP.run();
 
 export default App;
