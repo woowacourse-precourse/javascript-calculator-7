998d5438541ee1c9b60463ca9b255f21d71acc02
Heesu01 <rulru0413@naver.com>
2024-10-16 20:03:30 +0900

    feat(App): 문자열 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 문자열 입력
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    const input = await Console.readLineAsync("");
+    Console.print(input);
+  }
 }
 
 export default App;
