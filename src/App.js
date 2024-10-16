40c5cb8dcf66bf3f4e46e74a00133a496fc3560b
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-16 23:36:45 +0900

    feat(App) 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 덧셈을 하기 위한 문자열 입력 받기
+    const inputValue = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    // MissionUtils.Console.print(input);
+  }
 }
 
 export default App;
