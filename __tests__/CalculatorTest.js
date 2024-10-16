58ac408661bb3aa530a78c5f54de735a80d14231
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-16 11:10:23 +0900

    test: '3개 이상의 숫자에 대해 덧셈이 가능하다'에 대한 테스트 코드 작성

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -7,4 +7,18 @@ describe("문자열 계산기", () => {
 
     expect(app.add([1, 2])).toBe(3);
   });
+  
+  test("3개의 숫자에 대해 덧셈이 가능하다.", async () => {
+
+    const app = new App();
+
+    expect(app.add([1, 2, 3])).toBe(6);
+  });
+
+  test("4개의 숫자에 대해 덧셈이 가능하다.", async () => {
+
+    const app = new App();
+
+    expect(app.add([1, 2, 3, 4])).toBe(10);
+  });
 });
