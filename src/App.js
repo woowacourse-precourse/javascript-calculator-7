e54c7d7e943c3e0bcbd963144da77cca2b0ec245
df2854 <df2854@icloud.com>
2024-10-16 10:43:19 +0900

    feat: get input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
-    
+    const string = await this.getString();
+    // Console.print(string); 테스트용 출력
   }
 
+  // 사용자 입력 받기
+  async getString() {
+    try {
+      const string = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      return string;
+    } catch (error) {
+      
+    }
+  }
 }
 
 export default App;
