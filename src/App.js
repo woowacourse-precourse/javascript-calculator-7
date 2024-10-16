b2faf22de8cc923db455b40f41ec63baa4fc2196
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-16 13:08:00 +0900

    feat : 숫자가 아닌 문자열이 포함된 경우 에러처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 import Messages from "./constants/Messages.js";
+import Errors from "./constants/Errors.js";
 
 class App {
   async run() {
     // 입력 메세지 출력 및 입력 받기
     const INPUT = await Console.readLineAsync(Messages.INPUT_MESSAGE);
 
-    //Console.print(INPUT);
+    // 입력 받은 문자열을 숫자로 변환
+    const numbers = INPUT.split(/[, :]/).map(Number);
+
+    //에러처리
+    // 숫자가 아닌 문자열이 포함되어 있는 경우
+    if (numbers.some((number) => isNaN(Number(number)))) {
+      throw new Error(Errors.NOT_NUMBER);
+    }
   }
 }
 
--- src/constants/Errors.js
+++ src/constants/Errors.js
@@ -2,7 +2,7 @@ const Errors = Object.freeze({
   NO_COMMA_AND_COLON: "[ERROR] 숫자 사이에 콤마(,)나 콜론(:)이 없습니다.",
   WRONG_CUSTOM_DELIMITER: "[ERROR] 커스텀 구분자가 잘못되었습니다.",
   NOT_NUMBER: "[ERROR] 숫자가 아닌 문자가 있습니다.",
-  NO_NEGATIVE: "[ERROR] 음수가 있습니다.",
+  HAS_NEGATIVE: "[ERROR] 음수가 있습니다.",
 });
 
 export default Errors;
