6145d2f58badc8f11954d3ed90fc7172e9f46123
jakejun98 <111010564+jakejun98@users.noreply.github.com>
2024-10-16 21:08:33 +0900

    Feat: "예외상황 에러 발생 구현"
    -음수 값 입력시
    -숫자가 아닌 값 입력시
    -구분자가 올바르게 사용되지 않을시

--- src/App.js
+++ src/App.js
@@ -23,13 +23,27 @@ class App {
     
     if(input.startsWith('//')){
       const part = input.split('\\n');   
-    
+      if (part.length !== 2 || !part[1]) {
+        throw new Error("잘못된 입력 형식입니다."); 
+      }
 
       delimiter = new RegExp(part[0].slice(2));
       input = part[1];
     }
 
     const numbers = input.split(delimiter).map(Number);
+
+    if(numbers.some(n=>n<0)){
+      throw new Error("음수입니다.")
+    }
+    if(numbers.some(isNaN)){
+      throw new Error("잘못된 값을 입력하였습니다.")
+    }
+    for (let i = 0; i < input.length - 1; i++) {
+      if (delimiter.test(input[i]) && delimiter.test(input[i + 1])) {
+          throw new Error("잘못된 값을 입력하였습니다."); 
+      }
+    }
     return numbers.reduce((acc, num) => acc + num, 0);
 
   }
