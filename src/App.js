2f2485abcf0bb3ecd849ac63c058b1e609c5ba6c
chaeyoung103 <thdcodud103@naver.com>
2024-10-16 11:15:52 +0900

    feat: , 와 : 만을 구분자로 한 기본 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let splitters = ",:";
+
+    let regex = new RegExp(`[${splitters}]`);
+
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요. :"
+    );
+    const result = input
+      .split(regex)
+      .map((str) => parseInt(str, 10))
+      .reduce((acc, cur) => acc + cur, 0);
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
