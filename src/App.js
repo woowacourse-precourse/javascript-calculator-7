556fe4ba4e08835df94eed15df5638c9e9205921
hjoo830 <hjoo830@naver.com>
2024-10-16 10:09:35 +0900

    feat: Console.readLineAsync()를 이용해 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const inputString = await Console.readLineAsync('계산할 문자열을 입력: ');
+  }
 }
 
 export default App;
