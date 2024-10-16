7197c719980293e365d8e5aabe11620f29d03702
sebn <sebn1247@gmail.com>
2024-10-16 14:25:04 +0900

    feat: add default delimiter for str split  and number conversion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App {
     Console.print("덧셈할 문자를 입력해 주세요.");
     const STRING_INPUT = await Console.readLineAsync("");
     const CLEANED_INPUT = STRING_INPUT.replace(/"/g, "").trim();
-    Console.print(CLEANED_INPUT);
+    const WORDS = CLEANED_INPUT.split(/[,|:]/);
+    const STRING_TO_NUM = WORDS.map(Number);
+    Console.print(STRING_TO_NUM);
   }
 }
 
