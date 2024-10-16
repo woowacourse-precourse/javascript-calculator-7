509b5df818daf66065a33413b71d84cf561b7c83
LeeDongHyeuk <deeprogrammer404@gmail.com>
2024-10-16 11:45:11 +0900

    feat : add getNumberString func
    - Write a getNumberString function that takes a string input from the user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const getNumberString = async () => {
+  try {
+    const numberString = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+  } catch (error) {
+    MissionUtils.Console.print(`[ERROR] ${error}`);
+  }
+};
+
 class App {
-  async run() {}
+  async run() {
+    getNumberString();
+  }
 }
 
 export default App;
