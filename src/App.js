b2dcbe0f6ff4bfabc7b75c3044ab34b93925fab4
Parksangwoo <psu8430@gmail.com>
2024-10-17 04:04:56 +0900

    feat(user) : Adding a user's string input feature
    - Add functionality to receive any character string entered by the user with `Console API`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const user_string = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+  }
 }
 
 export default App;
