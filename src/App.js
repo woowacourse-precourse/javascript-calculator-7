1647883ca55c8c72561e90cca94762355a917fbd
silver <ekdldkaa@naver.com>
2024-10-16 12:32:44 +0900

    ✨feat1(getStr) : get str from user and sort out
    Added getStr function:
    - get str input from user
    - sort out it with regex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+
+//정규표현식
+const REGEX1 = /^[\d][\d|\,|\:]+\d$/;
+const REGEX2 = /^\/{2}.+\d$/;
+
 class App {
-  async run() {}
+  getStr = async () => {
+    const userStr = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+
+    //빈 문자열인 경우
+    if (userStr == '') {
+      Console.print(`결과 : 0`);
+    } else if (REGEX1.test(userStr)) {
+      //, 또는 : 구분자 문자열
+    } else if (REGEX2.test(userStr)) {
+      // 커스텀 구분자 문자열
+    } else {
+      //에러
+    }
+  };
 }
+
+let app = new App();
+app.getStr(); //문자열 입력받기
 
 export default App;
