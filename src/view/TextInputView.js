e05f32f07029a04c550efe5c31d3ee07c2d51acf
hanheel <hanhee630630@gmail.com>
2024-10-16 16:43:57 +0900

    feat(TextInputView): 빈 입력값 검증

--- src/view/TextInputView.js
+++ src/view/TextInputView.js
@@ -6,9 +6,18 @@ class TextInputView {
   }
   async getInputText() {
     const textInput = await Console.readLineAsync(
-      "계산할 문자열을 입력해 주세요 :"
+      `계산할 문자열을 입력해 주세요 :
+      1) 일반 계산 예시 : 1;2,3 / 1,2,3
+      2) 커스텀 구분자 입력 예시 : //*\\n1*2*3\n\n`
     );
+    this.isValidateInput(textInput);
     return textInput;
+  }
+
+  isValidateInput(textInput) {
+    if (textInput === "") {
+      throw Error("[ERROR] 문자열을 입력해 주세요.");
+    }
   }
 }
 
