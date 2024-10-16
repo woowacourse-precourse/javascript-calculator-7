ec81a59ac02a896ef87ce6584aa394d672116960
hjoo830 <hjoo830@naver.com>
2024-10-17 01:51:32 +0900

    feat: 커스텀 구분자, 쉼표, 콜론을 기준으로 문자열 분리하기

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ class App {
     const inputString = await Console.readLineAsync('계산할 문자열을 입력: ');
     let delimiters = [',', ':'];
     const customDelimiter = inputString.match(/\/\/(.*?)\\n/);
+    let numbers = inputString;
 
     if (customDelimiter) {
       delimiters.push(customDelimiter[1]);
+      numbers = inputString.split('\\n')[1];
     }
+
+    const delimiterRegex = new RegExp(`[${delimiters.join('')}]`);
+    const numberArray = numbers.split(delimiterRegex);
   }
 }
 
