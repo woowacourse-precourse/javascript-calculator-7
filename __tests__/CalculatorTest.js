19c008ae2a10dd248092d1fe33cfcdfcb7953a33
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-16 11:37:04 +0900

    test: 테스트를 케이스에 맞게 그룹화

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -7,19 +7,24 @@ describe("문자열 계산기", () => {
     app = new App();
   });
 
-  test("2개의 숫자에 대해 덧셈이 가능하다.", async () => {
-    expect(app.add([1, 2])).toBe(3);
-  });
+  describe("숫자 개수에 따른 덧셈", () => {
+    test("2개의 숫자에 대해 덧셈이 가능하다.", async () => {
+      expect(app.add([1, 2])).toBe(3);
+    });
+    
+    test("3개의 숫자에 대해 덧셈이 가능하다.", async () => {
+      expect(app.add([1, 2, 3])).toBe(6);
+    });
   
-  test("3개의 숫자에 대해 덧셈이 가능하다.", async () => {
-    expect(app.add([1, 2, 3])).toBe(6);
-  });
+    test("4개의 숫자에 대해 덧셈이 가능하다.", async () => {
+      expect(app.add([1, 2, 3, 4])).toBe(10);
+    });
+  
+    test("0개의 숫자는 0으로 처리한다.", async () => {
+      expect(app.add([])).toBe(0);
+    });
+  })
 
-  test("4개의 숫자에 대해 덧셈이 가능하다.", async () => {
-    expect(app.add([1, 2, 3, 4])).toBe(10);
-  });
-
-  test("0개의 숫자는 0으로 처리한다.", async () => {
-    expect(app.add([])).toBe(0);
-  });
+  describe("구분자를 기준으로 문자열 분리", () => {
+  })
 });
