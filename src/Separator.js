267b4678593a1c12809b07df2323435022c81e8f
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-16 20:55:36 +0900

    ✨ feat(separator): 기본 구분자로 문자열 분리 기능 구현
    기본 구분자(DEFAULT_SEPARATOR)를 사용하여 입력 문자열을 분리하는 `splitByDefaultSeparator` 메서드를 구현했습니다.
    - 쉼표(,)와 콜론(:)을 구분자로 입력 문자열을 분리하여 `this.value` 에 저장

--- src/Separator.js
+++ src/Separator.js
@@ -1,11 +1,14 @@
+import { DEFAULT_SEPARATOR } from './constant';
 class Separator {
   constructor(input) {
     this.input = input;
-    this.numbers = [];
+    this.value = [];
   }
 
   // 기본 구분자로 분리
-  splitByDefaultSeparator() {}
+  splitByDefaultSeparator() {
+    this.value = this.input.split(DEFAULT_SEPARATOR);
+  }
 
   // 커스텀 구분자 확인
   hasCustomSeparator() {}
@@ -18,6 +21,6 @@ class Separator {
     if (this.hasCustomSeparator) this.splitByCustomSeparator();
     else this.splitByDefaultSeparator();
 
-    return this.numbers;
+    return this.value;
   }
 }
