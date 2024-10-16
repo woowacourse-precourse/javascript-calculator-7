06c687abbd0c9b72fcf4e6506cdc2b0b68b30623
chaeyoung103 <thdcodud103@naver.com>
2024-10-16 15:29:51 +0900

    feat: 커스텀 구분자를 처리하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    let splitters = ",:";
-
-    let regex = new RegExp(`[${splitters}]`);
-
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요. :"
     );
-    const result = input
-      .split(regex)
-      .map((str) => parseInt(str, 10))
-      .reduce((acc, cur) => acc + cur, 0);
+
+    const splitNums = this.splitStrings(input);
+    const result = this.addCalculate(splitNums);
     Console.print(`결과 : ${result}`);
+  }
+
+  splitStrings(input) {
+    const result = input.match(/\/\/(.*?)\\n(.*)/);
+    if (result) {
+      const splitters = this.setSplitters(result[1]);
+      const splitInput = result[2];
+
+      const regex = new RegExp(`[${splitters}]`);
+      return splitInput.split(regex).map((str) => parseInt(str, 10));
+    } else {
+      const regex = new RegExp("[,:]");
+      return input.split(regex).map((str) => parseInt(str, 10));
+    }
+  }
+
+  setSplitters(splitter) {
+    const escapedSplitter = splitter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    return ",:" + escapedSplitter;
+  }
+
+  addCalculate(nums) {
+    return nums.reduce((acc, cur) => acc + cur, 0);
   }
 }
 
