b796a5988f6ffa54fed187fc45c5ab1c503a5bb2
smimdla0205 <sangmin0205@gmail.com>
2024-10-16 17:52:00 +0900

    feat(calculator): 빈 문자열 입력 시 0을 반환하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+      const result = this.calculate(input);
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR] ${error.message}`);
+      throw error;
+    }
+  }
+
+  calculate(input) {
+    if (!input) {
+      return 0;
+    }
+  }
 }
 
 export default App;
