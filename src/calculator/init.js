9d9de1b6f5ca007babbe396f98da25643c1d77b6
SanghyunLee <lgw9736@naver.com>
2024-10-16 20:00:55 +0900

    feat(sum): 배열의 요소를 합하여 출력하는 기능 추가
    - 함수명은 sumArrayElements로 정의

--- src/calculator/init.js
+++ src/calculator/init.js
@@ -36,6 +36,16 @@ const processInputString = (inputString) => {
 };
 
 /**
+ * 배열의 요소들을 모두 더한 값을 반환하는 함수
+ *
+ * @param {number[]} arr - 더할 숫자들이 포함된 배열
+ * @returns {number} 배열 요소들의 합계
+ */
+const sumArrayElements = (arr) => {
+  return arr.reduce((acc, element) => acc + element, 0);
+};
+
+/**
  * 프로그램 실행 함수
  * 사용자에게 입력을 받아 구분자를 확인 후, 구분자로 문자열을 나누고 덧셈을 한 후 결과를 출력한다.
  */
@@ -44,6 +54,7 @@ const init = async () => {
     const inputString = await getInput();
     const { separators, remainingString } = processInputString(inputString);
     const calculationArr = splitBySeparators(separators, remainingString);
+    MissionUtils.Console.print(`결과 : ${sumArrayElements(calculationArr)}`);
   } catch (error) {
     MissionUtils.Console.print(`[Error] ${error.message}`);
   }
