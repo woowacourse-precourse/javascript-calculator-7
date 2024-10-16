3ef8e8d5825aa8093224dca04687260256a3ceb6
eunoia-jaxson <eunoia@handong.ac.kr>
2024-10-16 16:36:30 +0900

    feat: 문자열 입력 받기
    문자열을 입력받는 기능을 구현하였습니다.
    mission-utils의 Console API를 활용하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+import { CALCULATOR_MESSAGE } from "./constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    this.enter();
+  }
+
+  async enter() {
+    Console.print(CALCULATOR_MESSAGE.CALCULATOR_START);
+    this.input = await Console.readLineAsync("");
+    Console.print(this.input);
+  }
 }
 
 export default App;
