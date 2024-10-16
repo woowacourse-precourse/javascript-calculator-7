03e355eec7101b8447d2143c2535b5855149ba43
dlsxjzld <sinjuk1@naver.com>
2024-10-16 17:14:21 +0900

    feat: 구분자를 기준으로 분리 및 숫자로 변환 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Console } from "@woowacourse/mission-utils"
 const DIVIDER = [",", ":"]
 
+function escapeRegExp(string) {
+  // 정규표현식 특수문자를 이스케이프 처리
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+}
 class App {
   async run() {
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
@@ -10,6 +14,10 @@ class App {
       input = input.replace(/^\/\/.+\\n/, "")
       DIVIDER.push(customDivider[1])
     }
+
+    const escapedDivider = DIVIDER.map(escapeRegExp).join("|")
+    const splitRegex = new RegExp(escapedDivider)
+    const numbers = input.split(splitRegex).map(Number)
   }
 }
 
