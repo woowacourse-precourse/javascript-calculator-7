bfbb0ce74d60f4368d7cb152261c7d98c07ae051
eunoia-jaxson <eunoia@handong.ac.kr>
2024-10-16 18:16:26 +0900

    refactor: 메소드 구조, 이름 변경.
    예외 처리 구현 전, 메소드의 구조와 이름을 정리하였습니다.
    확장성을 위하여, 프로그램이 좀 더 커지기 전에
    한 번 정리하는 과정을 거쳤습니다.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { Console } from "@woowacourse/mission-utils";
-import { CALCULATOR_MESSAGE } from "./constants.js";
+import { Console } from '@woowacourse/mission-utils';
+import { CALCULATOR_MESSAGE } from './constants.js';
 
 class App {
   async run() {
-    await this.enter();
+    await this.enterInput();
     this.calculateResult();
+    this.printResult();
   }
 
-  async enter() {
+  async enterInput() {
     Console.print(CALCULATOR_MESSAGE.CALCULATOR_START);
-    this.input = await Console.readLineAsync("");
+    this.input = await Console.readLineAsync('');
   }
 
-  async calculateResult() {
-    if (this.input.startsWith("//")) {
+  splitInput() {
+    if (this.input.startsWith('//')) {
       const [customDelimiter, separatedInput] = this.input
         .slice(2)
-        .split("\\n");
-      const numbers = separatedInput
-        .split(customDelimiter)
-        .map((number) => parseInt(number));
-      const sum = numbers.reduce((acc, cur) => acc + cur, 0);
-      Console.print(CALCULATOR_MESSAGE.CALCULATOR_RESULT + sum);
-      return;
+        .split('\\n');
+      return separatedInput.split(customDelimiter);
     }
-    // 문자열 앞부분의 "//", "\n" 사이의 커스텀 구분자가 없는 경우, "," 또는 ":"
-    const numbers = this.input.split(/,|:/).map((number) => parseInt(number));
-    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
-    Console.print(CALCULATOR_MESSAGE.CALCULATOR_RESULT + sum);
+
+    return this.input.split(/,|:/);
+  }
+
+  calculateResult() {
+    const numbers = this.splitInput();
+    this.sum = numbers.reduce((acc, cur) => acc + Number(cur), 0);
+  }
+
+  printResult() {
+    Console.print(CALCULATOR_MESSAGE.CALCULATOR_RESULT + this.sum);
   }
 }
 
