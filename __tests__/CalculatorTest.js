41007497c8e34d62090aeaa500e1712c0eada8e8
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-16 11:32:41 +0900

    test: 애플리케이션 불러오기가 중복돼서 하나로 통일

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -1,31 +1,25 @@
 import App from "../src/App.js";
 
 describe("문자열 계산기", () => {
+  let app;
+
+  beforeEach(() => {
+    app = new App();
+  });
+
   test("2개의 숫자에 대해 덧셈이 가능하다.", async () => {
-
-    const app = new App();
-
     expect(app.add([1, 2])).toBe(3);
   });
   
   test("3개의 숫자에 대해 덧셈이 가능하다.", async () => {
-
-    const app = new App();
-
     expect(app.add([1, 2, 3])).toBe(6);
   });
 
   test("4개의 숫자에 대해 덧셈이 가능하다.", async () => {
-
-    const app = new App();
-
     expect(app.add([1, 2, 3, 4])).toBe(10);
   });
 
   test("0개의 숫자는 0으로 처리한다.", async () => {
-
-    const app = new App();
-
     expect(app.add([])).toBe(0);
   });
 });
