81f877cd87a72f40d6e9e8d8c99593b4ee6b7c69
seonae-j <wkdtjsdo135@naver.com>
2024-10-16 16:54:46 +0900

    feat: 기본 구분자로 숫자 배열 생성
    기본 구분자(, :)로 사용자 입력값에서 숫자 배열만 가져옵니다.
    - USER_INPUT_NUMBER에 사용자가 입력한 숫자 배열만 선언

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ class App {
     const USER_INPUT = (
       await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
     ).trim();
+
+    const USER_INPUT_NUMBER = USER_INPUT.split(/[,:]/);
+    // 콤마, 클론을 기준으로 구분
+
+    // 확인
+    Console.print(USER_INPUT_NUMBER);
   }
 }
 
