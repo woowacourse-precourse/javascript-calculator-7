7bcac72aa384093482559bc41c81a1fd91a07531
dlsxjzld <sinjuk1@naver.com>
2024-10-16 14:43:46 +0900

    feat: 기본 구분자 추가  완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Console } from "@woowacourse/mission-utils"
-
+const DIVIDER = [",", ":"]
 class App {
   async run() {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n",
     )
 
-    Console.print(input)
+    Console.print(input.split(new RegExp(`[${DIVIDER}]`, "g")))
   }
 }
 
