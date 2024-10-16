8a7d3ac00f0d5bb131b67fcb7fe2816d0bc2cadf
kimdayeon37 <kdy37912@naver.com>
2024-10-16 23:13:14 +0900

    feat: 숫자 하나만 있는 경우 에러

--- src/App.js
+++ src/App.js
@@ -16,14 +16,19 @@ class App {
   calculateSum(input) {
     let defaultDelimiter = /[,:]/; // 기본 구분자 , :
     let numberString = input;
-    
-      // 빈 문자열일 경우 0을 반환
-      if (input === "") {
-        return 0;
-      }
+
+    // 빈 문자열일 경우 0을 반환
+    if (input === "") {
+      return 0;
+    }
+
+    // 숫자 하나만 있는 경우 에러 처리
+    if (/^\d+$/.test(input)) {
+      throw new Error();
+    }
 
     // '\n' 문자를 실제 개행 문자로 교체
-    input = input.replace(/\\n/g, '\n');
+    input = input.replace(/\\n/g, "\n");
 
     // 기본 구분자 연속 사용 여부 검사
     const invalidDelimiter = /[,|:]{2,}/;
@@ -32,8 +37,8 @@ class App {
     }
 
     // 커스텀 구분자인지 확인
-    if (input.startsWith('//')) {
-      const delimiterEnd = input.indexOf('\n');
+    if (input.startsWith("//")) {
+      const delimiterEnd = input.indexOf("\n");
 
       // \n 가 없는 경우 에러
       if (delimiterEndIndex === -1) {
@@ -43,15 +48,14 @@ class App {
       const customDelimiter = input.slice(2, delimiterEnd);
       defaultDelimiter = new RegExp(`[${customDelimiter}]`);
 
-      numberString = input.slice(delimiterEnd + 1)
+      numberString = input.slice(delimiterEnd + 1);
     }
 
     const numbers = numberString.split(defaultDelimiter);
 
     const sum = numbers.reduce((sum, numStr) => {
       const num = Number(numStr);
-    
-      
+
       // 숫자가 아니거나 비양수인 경우 에러 발생
       if (isNaN(numStr) || num <= 0) {
         throw new Error();
