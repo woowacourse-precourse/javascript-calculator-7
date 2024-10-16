378a6c829250bb51e8ca2f8705e77280e6bdae84
sooinice <sooin9674@naver.com>
2024-10-16 22:47:28 +0900

    feat(app): 구분자를 기준으로 분리한 각 숫자의 합을 반환하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요\n"
+    );
+    const numbers = this.parseInput(input);
+
+    const result = numbers.reduce((acc, num) => acc + num, 0);
+    MissionUtils.Console.print(`결과: ${result}`);
+  }
+  parseInput(input) {
+    const delimiter = /,|:/;
+    const numbers = input.split(delimiter).map(Number);
+    return numbers;
+  }
 }
 
 export default App;
