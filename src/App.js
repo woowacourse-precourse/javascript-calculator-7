a6cdaecf5f3d525e2a70887ee9282aee3f1df138
Hina0222 <hina0222@naver.com>
2024-10-16 23:57:38 +0900

    feat(App) : 구분자 지정 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    const COLON = ':';
+    const COMMA = ',';
+
     const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열 입력: ");
 
     MissionUtils.Console.print(`입력 값: ${input}`);
