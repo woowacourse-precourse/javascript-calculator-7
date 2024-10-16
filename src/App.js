8b8e050856a3a7dcd6a8a1a3d57c0eac58a178da
yundogi <lvbhkwak@gmail.com>
2024-10-16 11:56:36 +0900

    feat(App): implement string input functionality
    - Use MissionUtils.Console.readLineAsync for user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
 }
 
 export default App;
