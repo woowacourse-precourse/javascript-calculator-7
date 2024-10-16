414ecd1f24eb6f1747423526ba374729fba7f3f8
youngju6143 <youngju6143@naver.com>
2024-10-16 14:27:29 +0900

    feat: 1. 입출력 기능 (한 줄 입력 & 질문 넣기, 출력 로직) 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { readFileSync } from 'fs';
+import readline from 'readline';
+
 class App {
-  async run() {}
+  async run() {
+    // 1. 입출력 기능 -  입력 : '덧셈할 문자열을 입력해 주세요.\n', 한 줄로 입력받기
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+    
+    rl.question('덧셈할 문자열을 입력해 주세요.\n', (input) => {
+      // 결과 출력
+      console.log(`결과 : ${input}`); // 후에 출력값으로 바뀔 예정
+      rl.close();
+    })
+  }
 }
 
 export default App;
