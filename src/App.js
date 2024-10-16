a58d40af2a69fc97b11ff5278f3d0c28c4ba0a75
rhtjddls123 <jjjk0605@naver.com>
2024-10-16 20:47:33 +0900

    feat: 커스텀 구분자 생성기능 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class App {
   async run() {
     try {
       const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-      const separator = [",", ":"];
-      const sepStrArr = this.extractInput(input, separator);
+      const { str, separator } = this.makeCustomSep(input);
+      const sepStrArr = this.extractInput(str, separator);
       const sepNumArr = this.toNumberArray(sepStrArr);
       Console.print(sepNumArr);
     } catch (error) {
@@ -16,7 +16,7 @@ class App {
   extractInput(str, sep) {
     // 입력된 값을 구분자를 통해 분리
     // 구분자를 이용하여 정규 표현식을 만들어준뒤 split함수를 통해 분리
-    const sepRegex = new RegExp(`[${sep.join("")}]+`);
+    const sepRegex = new RegExp(`[${sep.join("|")}]+`);
     return str.split(sepRegex);
   }
 
@@ -39,6 +39,17 @@ class App {
     }
     if (+str < 0) throw Error("잘못된 입력입니다. 숫자는 양수만 입력해주세요!");
     throw Error("잘못된 입력입니다. 커스텀 구분자를 확인해주세요!");
+  }
+
+  makeCustomSep(str) {
+    // 커스텀 구분자 생성함수
+    // 정규표현식을 활용하여 커스텀 구분자를 분리 후 기본 구분자와 합쳐줌
+    // 기존 문자열에서 커스텀 구분자를 만들기 위한 문자열을 삭제 후 반환해줌
+    const sepRegex = /\/\/(.*?)\\n/g;
+    const separator = [",", ":"];
+    const customeSep = [...str.matchAll(sepRegex)].map((v) => v[1]);
+    str = str.replace(sepRegex, "");
+    return { str, separator: [...customeSep, ...separator] };
   }
 }
 
