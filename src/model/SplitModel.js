ccb676f0bedd3c17e9f9608921718c0f09f4d845
hanheel <hanhee630630@gmail.com>
2024-10-16 16:45:29 +0900

    refactor(SplitModel): console.log()제거

--- src/model/SplitModel.js
+++ src/model/SplitModel.js
@@ -2,8 +2,6 @@ class SplitModel {
   constructor() {}
   stringSplit(delimiter = /[:,]/, operationText) {
     const splitResult = operationText.split(delimiter);
-    //임시 출력, 삭제 예정
-    splitResult.map((i) => console.log(i));
     return splitResult;
   }
 }
