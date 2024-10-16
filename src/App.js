1e5c5b559924428b81d96c1e8b5009406c0a2998
Injun Jeong <jijun897@naver.com>
2024-10-16 14:46:44 +0900

    feat(app): add a function to find a custom sepatator

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input_data = await MissionUtils.Console.readLineAsync();
-    const input_str = [...input_data];
+    const sep = this.separator(input_data);
+  }
+
+  separator(str) {
+    if (str[0] !== "/" || str[1] !== "/") {
+      return ""; // 커스텀 구분자가 없다.
+    }
+    var customSep = "";
+    var i = 2;
+    var checkEnd = str[i] + str[i + 1];
+    while (i < str.length - 2 && checkEnd !== "\\n") {
+      customSep = customSep + str[i];
+      i += 1;
+      checkEnd = str[i] + str[i + 1];
+    }
+    return customSep;
   }
 }
 
