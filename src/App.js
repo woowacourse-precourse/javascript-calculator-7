f434b6dc87d8ab259919019e124ed2a9668de87a
ryanbae94 <ryanbae94@gmail.com>
2024-10-16 13:56:02 +0900

    feat (App): 사용자 input 기능
    - userInput 함수를 구현하여 사용자 입력에 대한 처리를 구현
    - 출력될 메시지를 MESSAGE 상수로 정의
    - 앱이 시작되면 구분자와 계산할 번호를 담은 배열을 생성할  생성자 설정
    
    구현 과제 목록 중 사용자 input 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const MESSAGE = {
+	INPUT_MESSAGE: '덧셈할 문자열을 입력해 주세요.\n',
+};
+
 class App {
-  async run() {}
+	constructor() {
+		this.seperators = [',', ':'];
+		this.numbers = [];
+	}
+
+	async userInput() {
+		const input = await Console.readLineAsync(MESSAGE.INPUT_MESSAGE);
+		return input;
+	}
+
+	async run() {
+		this.userInput();
+	}
 }
 
 export default App;
