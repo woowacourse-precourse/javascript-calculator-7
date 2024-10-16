95482e3ad72a79bab6323c891d840f29e758376e
parksiwoon <parksw003@gmail.com>
2024-10-16 17:34:26 +0900

    feat(calculate): 커스텀 구분자 지정 기능 구현

--- src/App.js
+++ src/App.js
@@ -29,6 +29,17 @@ class App {
     const numberStrings = numbersString.split(delimiterRegex);
 
     //4. 커스텀 구분자를 지정. 구분자는 //와 \n 사이에 위치
+    if (input.startsWith("//")) {
+      //커스텀 구분자 시작
+      const delimiterEndIndex = input.indexOf("\n");
+      if (delimiterEndIndex === -1) {
+        throw new Error("커스텀 구분자가 정의되지 않았습니다.");
+      }
+
+      const customDelimiter = input.substring(2, delimiterEndIndex); //'커스텀 구분자~끝나는 구분자' 사이를 커스텀 구분자로 설정
+      delimiters.push(customDelimiter); //구분자 배열에 추가
+      numbersString = input.substring(delimiterEndIndex + 1); //입력 숫자 부분만 추출
+    }
 
     //5. 구분자와 숫자 이외의 값 또는 음수를 입력할 경우 [ERROR]로 시작하는 메시지를 출력하고 프로그램을 종료
 
