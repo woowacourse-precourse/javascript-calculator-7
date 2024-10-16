f6be5a4723a29c277ca9a9e751483851c8658954
rhtjddls123 <jjjk0605@naver.com>
2024-10-16 20:58:04 +0900

    fix: 구분자로 분리된 문자열 배열 공백문자 제거

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App {
     // 입력된 값을 구분자를 통해 분리
     // 구분자를 이용하여 정규 표현식을 만들어준뒤 split함수를 통해 분리
     const sepRegex = new RegExp(`[${sep.join("|")}]+`);
-    return str.split(sepRegex);
+    return str.split(sepRegex).filter((v) => v != "");
   }
 
   toNumberArray(strArr) {
