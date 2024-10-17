e7f3bf9730bdff3fd3bfb457e02ed99cd043e292
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-17 19:41:54 +0900

    feat: 문자 분류 기능
    문자 분류(classifyCharacters), 자연수 확인(isNaturalNumber),구분자 추출(extrackDelimiter)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ const { Console } = require("@woowacourse/mission-utils");
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+
+    let numbers = [];
+    let delimiters = [",", ";"];
+
+    this.classifyCharacters(input, numbers, delimiters);
+  }
+
+  classifyCharacters(input, numbers, delimiters) {
+    const chars = input.split("");
+
+    for (let i = 0; i < chars.length; i++) {
+      if (this.isNaturalNumber(Number(chars[i]))) {
+        numbers.push(Number(chars[i]));
+      } else if (delimiters.includes(chars[i])) {
+        continue;
+      } else if (chars[i] === "/") {
+        this.extrackDelimiter(chars.slice(i, i + 5), delimiters);
+        i += 4;
+      } else {
+        throw new Error("[ERROR]: 유효하지 않은 문자열 입력");
+      }
+    }
+  }
+  isNaturalNumber(value) {
+    return value > 0 && Number.isInteger(value);
+  }
+
+  extrackDelimiter(slicedchars, delimiters) {
+    console.log(slicedchars);
+    if (
+      slicedchars[0] == "/" &&
+      slicedchars[1] == "/" &&
+      slicedchars[3] == "\\" &&
+      slicedchars[4] == "n"
+    ) {
+      delimiters.push(slicedchars[2]);
+    }
   }
 }
 
