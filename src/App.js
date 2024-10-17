24251a01b4d5a2ec5b5aaa86b4406c2e398f543d
sunkiteo-o <hyhy020301@gmail.com>
2024-10-17 17:51:04 +0900

    feat: #1 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import readline from "readline";
+
 class App {
-  async run() {}
+  async run() {
+    //문자열 입력받기, 입력받은 문자열을 INPUT에 저장
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    let INPUT = "";
+
+    rl.on("line", (line) => {
+      INPUT = line;
+      rl.close();
+    });
+    rl.on("close", () => {
+      //, or ; 구분해서 배열에 하나씩 넣기
+      // "" => 0, "1,2" => 3, "1,2,3" => 6, "1,2:3" => 6
+      const OUTPUT = INPUT.split(/,|:/).map(Number);
+      const sum = OUTPUT.reduce((acc, cur) => acc + cur, 0);
+      console.log(sum);
+    });
+  }
 }
 
 export default App;
