53701cf77a0e0ab7d6565731e1ac540dc2c89a6a
KimYoungWoo <rladuddn3924@gmail.com>
2024-10-17 21:45:35 +0900

    feat: 빈 문자열 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ class App {
   async run() {
     // 입력 받기
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+    // 빈 문자열 처리
+    if (input === "") {
+      return Console.print("결과 : 0");
+    }
   }
 }
 
