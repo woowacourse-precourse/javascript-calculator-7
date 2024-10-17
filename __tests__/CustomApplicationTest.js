7330d28d5bccec584e497f2396a86edafc86e6e0
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-17 13:56:20 +0900

    test: '공백이 입력으로 들어오면, 0으로 처리한다'에 대한 테스트 코드 작성

---
+++ __tests__/CustomApplicationTest.js
@@ -0,0 +1,34 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("문자열 계산기", () => {
+  test("공백은 0으로 처리한다.", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
