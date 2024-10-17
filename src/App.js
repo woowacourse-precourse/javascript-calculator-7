64a065a2943a75308cb71f778a97933c8a606ac4
rubberchin <88rubber@gmail.com>
2024-10-17 20:03:22 +0900

    feat($App): 문자열 내부 숫자들을 더하기 <br> - 구분자에 의해 분리된 문자열이 숫자로만 이뤄졌는지 판별 - 숫자를 제외한 문자가 포함돼 있으면 에러 메세지 출력 - 에러 메세지 출력 시 앱을 종료 - 숫자로만 이뤄졌다면 배열(문자열)내의 숫자들을 모두 더함 - 더한 값을 "결과 : 더한 값" 형태로 출력 <br> Update - 에러 메세지 출력을 함수로 묶음

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ class App {
     const separateInput = function separateInputWithSeperator(userInput) {
       //커스텀 구분자 인식
       let numbers = userInput;
-      let custom_separator;
+      let customSeparator;
       let finalNumbers;
 
       const CUSTOM_START = userInput.indexOf("//");
       const CUSTOM_END = userInput.indexOf("\\n");
      
       if(CUSTOM_START !== -1 && CUSTOM_END !== -1 && CUSTOM_START === 0) {
-        custom_separator = userInput.substring(CUSTOM_START+2, CUSTOM_END);
+        customSeparator = userInput.substring(CUSTOM_START+2, CUSTOM_END);
         userInput = userInput.slice(CUSTOM_END+2);
         //커스텀 구분자로 절단 후 기본 구분자로 절단
-        numbers = userInput.split(custom_separator);
+        numbers = userInput.split(customSeparator);
         finalNumbers = numbers.map(num => num.split(/[,;]/));
       } else {
         //기본 구분자로 절단
@@ -28,7 +28,7 @@ class App {
       return finalNumbers;
     }
     
-    /*
+
     const isAllNum = function isAllNumericStrings(array) {
       return array.every(element => 
         typeof element === 'string' && /^\d+$/.test(element)
@@ -36,26 +36,42 @@ class App {
     }
 
     //error 함수
-    function inputError() {
-      Console.print("[ERROR]");
-    }*/
-
+    const errorMessage = function printErrorMessage(errortype) {
+      if (errortype === "spaceInInput") {
+        Console.print("[ERROR] 공백 없이 입력해 주세요!");
+      } else if (errortype === "notAllNum") {
+        Console.print("[ERROR] 구분자와 숫자만 입력해주세요!");
+      }
+      
+    }
     //함수 선언 끝
-
 
     //App start
     //유저에게 입력받기
-    let USER_INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
-    if (USER_INPUT.includes(" ")) {
-      Console.print("[ERROR] 공백 없이 입력해 주세요!");
+    let userInput = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+    if (userInput.includes(" ")) {
+      errorMessage("spaceInInput");
+      return;
     } else {
-      const USER_NUMBERS = separateInput(USER_INPUT);
+      userInput = separateInput(userInput);
     }
+
+    //숫자만으로 이루어진 배열인지 체크
+    //숫자만 있으면 더하기
+    if (isAllNum(userInput)) {
+      const inputSum = userInput.reduce((sum, num) => sum + Number(num), 0);
+      Console.print(`결과 : ${inputSum}`);
+    } else {
+      errorMessage("notAllNum");
+      return;
+    }
+    //구분자가 없으면?
+
 
   }   
 }
 
-const APP = new App();
-APP.run();
+const app = new App();
+app.run();
 
 export default App;
