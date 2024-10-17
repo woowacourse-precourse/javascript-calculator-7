cd79d7dd442dac9e381ecfdc73e5088f7400a23e
yechan <kevin2695@naver.com>
2024-10-17 17:58:13 +0900

    [k1my3ch4n] 문자열 입력 받도록 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요. \n"
+    );
+
+    console.log("결과 : ", input);
+  }
 }
 
 export default App;
