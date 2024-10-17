d1fdce8310c53369011537335880e07d3a27d05c
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 20:53:42 +0900

    :sparkles: feat: 문자열에서 숫자추출 후 합

--- src/model.js
+++ src/model.js
@@ -10,4 +10,9 @@ calculate(input);
   if (!input) {
     return 0; // 빈 문자열은 0 반환
   }
+
+  const { SEPARATOR, NUMBERS } = this.parseInput(input);
+  this.validateNumbers(NUMBERS);
+
+  return NUMBERS, reduce((sum, num) => sum + num, 0);
 }
