1ab1220b7217d700d0e68105ba5c6418f62fa105
chaeyoung103 <thdcodud103@naver.com>
2024-10-17 13:08:41 +0900

    feat: 잘못된 입력 예외처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -22,21 +22,41 @@ class App {
       const splitInput = result[2];
 
       const regex = new RegExp(`[${splitters}]`);
-      return splitInput.split(regex).map((str) => parseInt(str, 10));
+      return splitInput.split(regex).map(this.validateAndParseNumber);
     } else {
       const defaultSplitters = ",:";
       const regex = new RegExp(`[${defaultSplitters}]`);
-      return input.split(regex).map((str) => parseInt(str, 10));
+      return input.split(regex).map(this.validateAndParseNumber);
     }
   }
 
   setSplitters(splitter) {
     const defaultSplitters = ",:";
+
+    if (/\d/.test(splitter)) {
+      throw new Error(
+        `[ERROR] 구분자로 숫자는 사용할 수 없습니다: '${splitter}'`
+      );
+    }
     const escapedSplitter = splitter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
     return defaultSplitters + escapedSplitter;
   }
 
+  validateAndParseNumber(str) {
+    const num = parseInt(str, 10);
+    if (isNaN(num)) {
+      throw new Error(`[ERROR] 유효하지 않은 숫자: '${str}'`);
+    }
+    return num;
+  }
+
   addNums(nums) {
+    nums.forEach((num) => {
+      if (num < 0) {
+        throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
+      }
+    });
     return nums.reduce((acc, cur) => acc + cur, 0);
   }
 }
