2c698d6309ff8bd5163d44621f6aa92d7952c150
kim jiyeong <youngdev96@naver.com>
2024-10-17 14:29:58 +0900

    feat: 입력받은 문자열에서 커스텀 구분자 확인

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import View from "./view/View.js";
 class App {
   async run() {
     const view = new View();
-    const inputValue = await view.inputPrompt();
+
+    const userInput = await view.inputPrompt();
+    const customSeperator = this.isCustomSeparator(userInput);
+  }
+
+  isCustomSeparator(user) {
+    const customCondition = /^\/\/(.)\\n/;
+    const match = user.match(customCondition);
+
+    return match ? match[1] : null;
   }
 }
 
