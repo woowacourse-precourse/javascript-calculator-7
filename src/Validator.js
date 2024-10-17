973323c425cacd3139a77e6c4ba3283bd54ae6fd
siggu <m0_w_0m@naver.com>
2024-10-17 17:21:39 +0900

    feat: 유효성 검사 기능 추가
    구분자, 커스텀 구분자, 계산 결과 유효성 검사

---
+++ src/Validator.js
@@ -0,0 +1,32 @@
+import { MAX_NUM, MESSAGES } from './constants.js';
+
+class Validator {
+  constructor() {
+    this.MAX_NUM = new MAX_NUM();
+  }
+
+  // 구분자 유효성 검사
+  checkValidSeparator(sep) {
+    if (sep === '-') {
+      throw new Error(MESSAGES.ERROR.INPUT_DASH_SEPARATOR);
+    }
+  }
+
+  // 커스텀 구분자 유효성 검사
+  checkValidCustomSeparator(customSep) {
+    if ([',', ':'].includes(customSep)) {
+      throw new Error(MESSAGES.ERROR.INPUT_BASIC_SEPARATOR);
+    } else if (!!Number(customSep)) {
+      throw new Error(MESSAGES.ERROR.INPUT_NUMBER_SEPARATOR);
+    }
+  }
+
+  // 계산 결과 유효성 검사
+  checkValidCalculateResult(calcResult) {
+    if (calcResult > this.MAX_NUM) {
+      throw new Error(MESSAGES.ERROR.OVER_MAX_NUMBER);
+    }
+  }
+}
+
+export default Validator;
