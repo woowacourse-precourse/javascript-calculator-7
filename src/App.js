f3c30a5373da2d6f30f3d6dcfe4c5d91c9a3138e
ockko <ocko119@gmail.com>
2024-10-17 19:36:22 +0900

    feat: 입력값에서 숫자 추출하여 더하기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ class App {
     const userStr = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요."
     );
+    this.result(userStr);
+  }
+
+  // 입력값에서 숫자 추출하여 더하기
+  result(userStr) {
+    let found = userStr.match(/\d+/g);
+
+    let num = 0;
+    found.map((item) => {
+      num += item;
+    });
   }
 }
 
