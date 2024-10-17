563b3ddf6c4168983298e283eb1422010ca8e015
ssingfly32 <ssingfly3232@gmail.com>
2024-10-17 21:37:45 +0900

    feat(calculator): handleApplication 메서드 추가

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,23 +2,29 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import { errorMessage } from "./utils/errorMessage.js";
 
 class Calculator {
-
     async start() {
+        let keepGoing = true;
         MissionUtils.Console.print('문자열 덧셈 계산기를 실행합니다.');
-        await this.GetUserInput();   
+        while(keepGoing) {
+            try {
+                await this.GetUserInput();
+                keepGoing = await this.handleApplication();  
+            } catch (error) {
+                MissionUtils.Console.print(error.message); 
+                keepGoing = false;
+                this.closeCalculator();  
+            }
+        }
     }
 
     async GetUserInput() {
         const userInput = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해주세요.(쉼표와 콜론 외에 //와 \\n 사이에 입력한 문자열을 커스텀 구분자로 사용 가능합니다.):\n');
         
         // 올바른 값인지 확인
-        try {
-            const result = this.validateInput(userInput);
-            if (result) console.log(`결과: ${result}`);
-        } catch (error) {
-            console.log(error.message);
-        }
+        const result = this.validateInput(userInput);
+        if (result) MissionUtils.Console.print(`결과: ${result}`);
     }
+
     validateInput(input) {
         let result = 0;
         let userInput = input.trim();
@@ -45,16 +51,16 @@ class Calculator {
         // 구분자가 아닌 문자가 포함되어 있는지
         const includeInvalidStr = userInput.split(/[,|:]/).filter(Boolean).map(Number).some(v => Number.isNaN(v));
 
-       // 양수인지
-       let isPositiveNum = false;
+        // 양수인지
+        let isPositiveNum = false;
        
-       const numbers = hasDefaultSeparator ? userInput.split(/[,|:]/).filter(Boolean).map(Number) : 
-       userInput.match(/-?\d+/g).map(Number);
+        const numbers = hasDefaultSeparator ? userInput.split(/[,|:]/).filter(Boolean).map(Number) : 
+        userInput.match(/-?\d+/g).map(Number);
 
-       if(numbers && numbers.length > 0) {
-           isPositiveNum = numbers.every(v => v > 0);
-           // 올바른 값일 때
-           if(isPositiveNum && hasDefaultSeparator && !includeInvalidStr) {
+        if(numbers && numbers.length > 0) {
+            isPositiveNum = numbers.every(v => v > 0);
+            // 올바른 값일 때
+            if(isPositiveNum && hasDefaultSeparator && !includeInvalidStr) {
                const sum = numbers.reduce((acc, curr) => acc + curr, 0);
                result = sum;
             }
@@ -76,6 +82,31 @@ class Calculator {
            } else if(!isPositiveNum) {
                throw new Error(errorMessage.NOT_POSITIVE_NUM);
            }
+    }
+
+    async handleApplication() {
+        let isValidInput = false;
+        while (!isValidInput) {
+            const keepGoingInput = await MissionUtils.Console.readLineAsync('계속하시겠습니까?(y/n)\n');
+            isValidInput = this.validateKeepGoingInput(keepGoingInput);
+            if(isValidInput){
+                if(keepGoingInput === 'n') this.closeCalculator();
+                return keepGoingInput === 'y' ? true : false;
+            }
+        }
+    }
+
+    closeCalculator() {
+        MissionUtils.Console.print('문자열 덧셈 계산기를 종료합니다.');
+    }
+
+    validateKeepGoingInput(string) {
+        const isValid = string === 'y' || string === 'n';
+        if(!isValid) {
+            MissionUtils.Console.print('y 또는 n으로 입력해주세요.');
+            return false;
+        }
+        return isValid;
     }
 }
 
