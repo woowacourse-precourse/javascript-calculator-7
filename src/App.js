308423bf38c856daa3c1b763933814008d14b3b7
siggu <m0_w_0m@naver.com>
2024-10-17 16:26:49 +0900

    feat: 프로그램 시작 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+import Input from './Input.js';
+
 class App {
-  async run() {}
+  constructor() {
+    this.input = new Input();
+  }
+
+  async run() {
+    await this.input.getUserNumber();
+  }
 }
+
+const app = new App();
+app.run();
 
 export default App;
