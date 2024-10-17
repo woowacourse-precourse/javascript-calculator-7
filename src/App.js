e4dbd812bc575d4c1f9a1f8cd6ccc07061b4458a
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 22:20:16 +0900

    :sparkles: feat: 시작점

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-class App {}
+import { CalculatorController as controller } from "./controller.js";
+import CalculatorModel, { CalculatorModel as model } from "./model.js";
+import { CalculatorView as view } from "./view.js";
+
+class App {
+  #controller;
+
+  constructor() {
+    this.#controller = new controller(new model(), new view());
+  }
+  async run() {
+    await this.#controller.handleCalculate();
+  }
+}
 
 export default App;
