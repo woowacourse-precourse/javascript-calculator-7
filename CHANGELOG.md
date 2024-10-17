2000f5afb0879873ed69043cf4fe2dc4a8c948a9
Juyudang <ssuw223@gmail.com>
2024-10-17 17:37:15 +0900

    feat(fuction): add custom seperator
    check the stirng start with "//[?]\n" and make new regex with adding [?].
    if new regex is made read after string by slice method.
    Otherwise work same

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
-- docs(guide): updated README.md
-- feat: setup project
+feat(function): make it split by [,:] and sum the numbers
+docs(guide): updated README.md
+feat: setup project
--- src/App.js
+++ src/App.js
@@ -3,49 +3,38 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    // MissionUtils.Console.print(input)
-    readInput(input);
+    MissionUtils.Console.print(readInput(input));
   }
 }
 
 function readInput(input) {
-  // set diver
-  const regex = /[,:]/
-  // let re = new RegExp("[,:]");
-  // if (input.length > 5) {
-  //   if (input.charCodeAt(0) == 47 && input.charCodeAt(1) == 47 && input.charCodeAt(3) == 92 && input.charCodeAt(4) == 110) {
-  //     regex = myRegex(input[2])
-  //     }
-  //   }
-  // const nums = input.split(/[,:]/)
+  // set regex
+  let regex = /[,:]/
 
-  const sum = mySum(input, regex)
-  // const temp = input.split(/[,:]/)
+  // check additional rule
+  if (input.length > 5) {
+    if (input.charCodeAt(0) == 47 && input.charCodeAt(1) == 47 && input.charCodeAt(3) == 92 && input.charCodeAt(4) == 110) {
+      regex = myRegex(input[2])
+      return mySum(input.slice(5), regex)
+      }
+    }
 
-  MissionUtils.Console.print(sum)
-  
-  // for (let i = 0; i < input.length; i++){
-  //   // MissionUtils.Console.print(input[i])
-  //   // MissionUtils.Console.print(typeof input[i])
-  //   // console.log(typeof input[i])
-  //   MissionUtils.Console.print(input[i] + input.charCodeAt(i));
-  // }
+  return mySum(input, regex)
 }
 
-// function myRegex(new_seperator) {
-//   const BASE1 = "/[,:"
-//   const BASE2 = "]/"
-//   const REGEX = BASE1 + new_seperator + BASE2
-//   return REGEX
-// }
+// set new regex
+function myRegex(new_seperator) {
+  const BASE1 = "[,:"
+  const BASE2 = "]"
+  const RE = BASE1 + new_seperator + BASE2
+  // const REGEX = new RegExp(RE)
+  return RegExp(RE)
+}
 
-// function isRightInput(input) {
-//   return true
-//   return false
-// }
-
-// regex대로 문자열을 나누고 합을 리턴하는 함수
+// split inputs by regex and get sum
 function mySum(inputs, regex) {
+  // console.log(regex, inputs)
+  // console.log(inputs)
   const nums = inputs.split(regex)
   let sum = 0
   for (let i = 0;i < nums.length;i++){
@@ -54,7 +43,4 @@ function mySum(inputs, regex) {
   return sum
 }
 
-// tests
-//;\n1;2;3
-// 1,2:3
 export default App;
