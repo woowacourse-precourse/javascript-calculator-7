2580c80618246e13dfd25008d5b91dc1e718816c
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 22:26:56 +0900

    :recycle: refactor: 중복 불러오기 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { CalculatorController as controller } from "./controller.js";
-import CalculatorModel, { CalculatorModel as model } from "./model.js";
-import { CalculatorView as view } from "./view.js";
+import CalculatorController from "./controller.js";
+import CalculatorModel from "./model.js";
+import CalculatorView from "./view.js";
 
 class App {
   #controller;
 
   constructor() {
-    this.#controller = new controller(new model(), new view());
+    this.#controller = new CalculatorController(
+      new CalculatorModel(),
+      new CalculatorView()
+    );
   }
+
   async run() {
     await this.#controller.handleCalculate();
   }
