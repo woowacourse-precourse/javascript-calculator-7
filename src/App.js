1e95777d639188ceb6bedfab7b67e68ad4f5d27e
kim jiyeong <youngdev96@naver.com>
2024-10-17 14:33:02 +0900

    feat: 구분자 기준으로 문자열 분리하여 배열 처리

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
 
     const userInput = await view.inputPrompt();
     const customSeperator = this.isCustomSeparator(userInput);
+    const inputArr = this.userValueArr(userInput, customSeperator);
   }
 
   isCustomSeparator(user) {
@@ -13,6 +14,25 @@ class App {
     const match = user.match(customCondition);
 
     return match ? match[1] : null;
+  }
+
+  userValueArr(user, customSeperator) {
+    let arr = [];
+    if (customSeperator) {
+      let newUserStirng = this.removeCustomCondition(user);
+      arr = newUserStirng.split(/[,:]/);
+      arr = newUserStirng.split(customSeperator);
+      return arr;
+    }
+    arr = user.split(/[,:]/);
+    return arr;
+  }
+
+  removeCustomCondition(user) {
+    const start = user.indexOf("/");
+    const end = user.indexOf("n") + 1;
+
+    return user.slice(0, start) + user.slice(end);
   }
 }
 
