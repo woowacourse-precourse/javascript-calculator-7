2e3b28eb50ef48d8a69a02451aa06ed3a67dac24
kimdongyoung <wwweric12@naver.com>
2024-10-17 13:36:28 +0900

    feat(splitBySymbol): 커스텀 구분자 기능 구현

--- src/utils/splitBySymbol.js
+++ src/utils/splitBySymbol.js
@@ -5,8 +5,17 @@ const splitBySymbol = (value) => {
         commaSeparatedValue.forEach((item)=>{
             seperatedValue = seperatedValue.concat(item.split(":"));
         })
+        // 만약 다른 기호가 들어가 있을때, 양수가 아닐때 에러 반환 추후에 구현 예정
         return seperatedValue;
-    }else{}  
+    }else{
+        const index = 2;
+        const lineBreakIndex = value.indexOf("\\n");
+        const customSeperator = value.slice(index,lineBreakIndex);
+        const substringValue = value.slice(lineBreakIndex+2);
+        const seperatedValue = substringValue.split(customSeperator)
+        // 만약 다른 기호가 들어가 있을때, 양수가 아닐때 에러 반환 추후에 구현 예정 
+        return seperatedValue;
+    }  
 }
 
 export default splitBySymbol
