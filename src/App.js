c2db78677b6b8b023e735272e98b62ee7d062bc6
jllee000 <jllee000@naver.com>
2024-10-17 15:15:58 +0900

    feat : mission-utils API 연결 및 출력 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputContents = await Console.readLineAsync("입력값을 입력해주세요.");
+    console.log(inputContents);
+  }
 }
 
 export default App;
