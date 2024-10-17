833095f424700bc1186213fb9e8690fd3bf90892
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-17 18:57:31 +0900

    feat: 숫자 검증 및 합산 기능 구현
    - StringCalculator.js 파일에 숫자 검증 및 합산 기능 구현
    - 배열의 요소들이 숫자인지 검증
    - 배열의 요소들의 합을 계산해 반환환

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import StringInputProcessor from './classes/StringInputProcessor.js';
 import StringInputReader from './classes/StringInputReader.js';
+import StringCalculator from './classes/StringCalculator.js';
 
 class App {
   async run() {
     const inputReader = new StringInputReader();
     const inputString = await inputReader.getInput();
     const processedInput = StringInputProcessor.processInput(inputString);
+    StringCalculator.validateNumbersArray(processedInput);
+    const result = StringCalculator.sum(processedInput);
   }
 }
 
---
+++ src/classes/StringCalculator.js
@@ -0,0 +1,17 @@
+class StringCalculator {
+  // 숫자 검증
+  static validateNumbersArray(numbersArray) {
+    for (const value of numbersArray) {
+      if (Number.isNaN(value)) {
+        throw new Error(`[ERROR] ${value} -> 숫자가 아닌 값입니다.`);
+      }
+    }
+  }
+
+  // 숫자 합산
+  static sum(numbersArray) {
+    return numbersArray.reduce((acc, value) => acc + Number(value), 0);
+  }
+}
+
+export default StringCalculator;
