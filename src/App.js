4c05d8fadacb60af7a93f7d1f69cb9861b9cbaa7
devjaeyoon <devjaeyoon@gmail.com>
2024-10-17 11:43:07 +0900

    feat(App): 사용자가 아무 값도 입력하지 않았는지 검사하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 
+function isInputEmpty(input) {
+  if (input === '') {
+    return true;
+  }
+  return false;
+}
+
 class App {
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
