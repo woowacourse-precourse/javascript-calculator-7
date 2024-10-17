e25598213c00d24db9f51d73ba7573973836ec1d
HS <hasang2691@naver.com>
2024-10-17 15:18:39 +0900

    feat: 커스텀 구분자 지정 기능 구현
    - 커스텀 구분자를 지정하는 handleCustomSeparator 함수 생성
    - 이 함수는 //과 n 사이에 있는 문자를 구분자 배열에 넣음
    - 그 이후에는 문자열에서 //문자n을 삭제
    - //문자n이 없을 때까지 기능 반복
    - 임시로 입력과 현재 구분자 목록을 출력하게 구현

--- src/App.js
+++ src/App.js
@@ -9,16 +9,21 @@ class App {
       '덧셈할 문자열을 입력해주세요.\n'
     );
 
+    const PROCESSED_INPUT = this.handleCustomSeparator(INPUT);
+
+    Console.print(`입력: ${PROCESSED_INPUT}`);
+    Console.print(`현재 구분자 목록: ${this.separators.join('|')}`);
+
     let result;
 
-    if (this.isEmpty(INPUT)) {
+    if (this.isEmpty(PROCESSED_INPUT)) {
       result = 0;
-    } else if (INPUT === 'error') {
+    } else if (PROCESSED_INPUT === 'error') {
       return this.printError("'error'를 입력했습니다");
-    } else if (!this.validateNumber(INPUT)) {
+    } else if (!this.validateNumber(PROCESSED_INPUT)) {
       return this.printError('숫자가 아닌 문자를 입력하셨습니다');
     } else {
-      result = INPUT;
+      result = PROCESSED_INPUT;
     }
 
     this.printResult(result);
@@ -26,6 +31,23 @@ class App {
 
   isEmpty(str) {
     return str.trim().length === 0;
+  }
+
+  handleCustomSeparator(input) {
+    const CUSTOM_SEPARATOR_REGEX = /^\/\/(.)\\n(.*)$/;
+    let remainingString = input;
+    let match;
+
+    while ((match = remainingString.match(CUSTOM_SEPARATOR_REGEX))) {
+      const CUSTOM_SEPARATOR = match[1];
+      remainingString = match[2];
+
+      if (!this.separators.includes(CUSTOM_SEPARATOR)) {
+        this.separators.push(CUSTOM_SEPARATOR);
+      }
+    }
+
+    return remainingString;
   }
 
   validateNumber(num) {
