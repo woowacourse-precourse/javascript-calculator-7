feb7b9ddc639d5fd9e79af43e1ad8e725933b733
kochungwon <yud01063@gmail.com>
2024-10-17 13:34:51 +0900

    feat(메서드): 입력받은 input을 배열로 바꾸는 메서드 추가
    - 입력값의 유효성을 검사하기 위해 입력받은 값을 배열로 변환하는 메서드를 추가했습니다.
    - 이 메서드는 입력받은 문자열을 배열로 변환해 유효성 검사를 쉽게 처리할 수 있도록 만들었습니다.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App {
 
       //유효한 입력인지 검사
       if (this.isValid(input)) {
-        // 유효한 입력값일 경우, 계산하는 함수를 실행
+        // 유효한 입력값일 경우, 계산하는 메서드 를 실행
         this.calculate();
       } else {
         MissionUtils.Console.print('[ERROR] 잘못된 값을 입력하셨습니다.');
@@ -26,6 +26,7 @@ class App {
     MissionUtils.Console.print('계산 성공');
   }
 
+  //유효값을 검사하는 메서드 
   isValid(input) {
     const delimiters = [',', ':'];
 
@@ -40,11 +41,64 @@ class App {
         }
       }
     }
-    MissionUtils.Console.print(delimiters);
-    MissionUtils.Console.print(input);
+    const inputArr=this.parseInputToArray(input)
+    MissionUtils.Console.print(inputArr);
+  }  
+  
+  //입력받은 input 을 배열로 바꿔주는 메서드
+  parseInputToArray(input){
+    let temp = '';
+    const result = [];
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+    
+      // char가 숫자인 경우
+      if (typeof Number(char) === 'number' && !isNaN(Number(char)) && char !== ' ') {
+        if (temp) {
+          result.push(temp); 
+          temp = '';
+        }
+        result.push(char);
+      } 
+      // char가 숫자가 아닌 경우
+      else if (typeof char === 'string' && char !== ' ') {
+        temp += char;
+      }
+    }
+    
+    // 마지막 temp가 비어있지 않으면 결과에 추가
+    if (temp) {
+      result.push(temp);
+    }
+    
+   
+    const combinedResult = [];
+    let numberString = '';
+    for (let j = 0; j < result.length; j++) {
+      const current = result[j];
+    
+      // 현재 요소가 숫자인 경우
+      if (!isNaN(current)) {
+        numberString += current; 
+      } else {
+        if (numberString) {
+          combinedResult.push(numberString);
+          numberString = ''; 
+        }
+        combinedResult.push(current);
+      }
+    }
+    
+    // 마지막 숫자 문자열이 남아있으면 추가
+    if (numberString) {
+      combinedResult.push(numberString);
+    }
+    
+    return combinedResult;
   }
 
-  // 입력값을 계산하는 함수
+
+  // 입력값을 계산하는 메서드 
   calculate() {
     // 구분자를 기준으로 숫자들을 분리한다.
     // 숫자 중 음수가 있는지 체크한다.
