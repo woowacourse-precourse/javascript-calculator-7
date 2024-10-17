a46dbfa5724dbad02ba2424384a0c5a12236a3df
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-17 22:37:50 +0900

    feat(app): 유틸 함수를 활용한 조건부 렌더링 및 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
+import { isEmptyOrNull } from './utils/isEmptyOrNull.js';
+import { isSingleCharacter } from './utils/isSingleCharacter.js';
+import { splitByCustomSeparator } from './utils/splitByCustomSeparator.js';
+import { splitByDefaultSeparators } from './utils/splitByDefaultSeparators.js';
 
 class App {
 	async run() {
 		try {
 			const res = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+			let answer = null;
 
-			Console.print(`결과 : ${res}`);
+			if (isEmptyOrNull(res)) {
+				answer = 0;
+			} else if (isSingleCharacter(res)) {
+				answer = +res;
+			} else if (res.startsWith('//')) {
+				answer = splitByCustomSeparator(res);
+			} else {
+				answer = splitByDefaultSeparators(res);
+			}
+
+			Console.print(`결과 : ${answer}`);
 		} catch (err) {
 			throw new Error(err);
 		}
