70ed39053ffb3aa0f7448bb5fd054a36e8e9ae84
mynameSumin <msm4167@naver.com>
2024-10-18 01:29:59 +0900

    feat: 입출력 양식 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let separator = [',',':',];
+    let number = [];
+    let sum = 0;
+
+    Console.print('덧셈할 문자열을 입력해 주세요.');
+    const data = await Console.readLineAsync('');
+
+
+    Console.print(`결과 : ${sum}`);
+    function getCustomSeparator(data) {
+      if(data.startsWith('//')){ 
+        if(data.include('\n')){
+          if(data.substring(3, 5) == "\n"){
+            separator.push(data[2]); 
+          } 
+          else{
+            Console.print('[ERROR] 잘못된 커스텀 구분자입니다. 커스텀 구분자는 한 개의 문자만 설정 가능합니다.');
+          }
+        }
+        else{
+          Console.print('[ERROR] 잘못된 문자열입니다. 문자열에는 구분자, 숫자만 입력 가능합니다.');
+        }
+      }
+    }
+  }
 }
 
 export default App;
