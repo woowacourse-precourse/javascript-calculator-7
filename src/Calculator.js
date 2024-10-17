c7fa6112931f73364a1cde4f9ac4c74f31974d2d
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-18 04:11:29 +0900

    feat: add validateInput for exception handling

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,21 @@
+import { ERROR_MESSAGE } from './constants/error.js';
+
 export default class Calculator {
   constructor(input) {
     this.input = input;
     this.delimiter = [',', ':'];
   }
 
+  validateInput() {
+    const isInputValid = new RegExp(`^[0-9${this.delimiter.join('')}\n]*$`);
+    if (!isInputValid.test(this.input)) {
+      throw new Error(ERROR_MESSAGE.INVALID_INPUT);
+    }
+
+  }
 
   calculate() {
+    this.validateInput();
 
     const input = this.input.split(new RegExp(`[${this.delimiter.join('')}\n]`));
 
---
+++ src/constants/error.js
@@ -0,0 +1,3 @@
+export const ERROR_MESSAGE = {
+    INVALID_INPUT: '[ERROR] input에 음수 혹은 숫자가 아닌 값이 포함, delimiter 오류',
+  };
