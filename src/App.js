668819890287b61ca968d2d1a2b4bae39b6f47e3
chungyo <kimchungy98@gmail.com>
2024-10-17 15:23:53 +0900

    feat: 예외처리 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import addCal from "./calculator.js";
 
 class App {
   async run() {
-    const result = addCal("//;\n 1;3;4;5");
-    console.log(`Result: ${result}`);
+    const result = addCal("//;\n 1;3;4;5;-1");
+    console.log(`결과: ${result}`);
   }
 }
 
