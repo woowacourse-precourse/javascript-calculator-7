46536a9681e0d38ba9b86400990f6f8feca3beaf
K0seoyoung <koseo12@hufs.ac.kr>
2024-10-18 03:16:17 +0900

    feat : 기본 구분자로 숫자를 분리하고 합계를 계산하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,37 @@ class App {
   run() {
     return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.')
       .then((input) => {
-        // 입력값을 처리할 예정
+        const result = this.calculateSum(input);
+        Console.print(`결과 : ${result}`);
       });
+  }
+
+  calculateSum(input) {
+    if (input === '') {
+      return 0;
+    }
+  
+    const delimiters = [',', ':'];
+    const tokens = this.splitNumbers(input, delimiters);
+  
+    const numbers = tokens.map((token) => {
+      const num = Number(token);
+      if (isNaN(num) || num <= 0) {
+        throw new Error('[ERROR] 잘못된 숫자입니다.');
+      }
+      return num;
+    });
+  
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+    return sum;
+  }
+  
+  splitNumbers(numbersStr, delimiters) {
+    const escapedDelimiters = delimiters.map((delim) =>
+      delim.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+    );
+    const pattern = new RegExp(escapedDelimiters.join('|'));
+    return numbersStr.split(pattern);
   }
 }
 
