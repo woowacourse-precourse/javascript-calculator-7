615d6d7aa30af6b97c3fcb042b95589fb016e174
ssingfly32 <ssingfly3232@gmail.com>
2024-10-17 18:07:32 +0900

    fix(calculator): isPositiveNum 변수의 값 변경 및 예외 처리 추가

--- src/Calculator.js
+++ src/Calculator.js
@@ -23,10 +23,11 @@ class Calculator {
         let result = 0;
         let userInput = input.trim();
         
-        // 입력값이 없을 때 early return
+        // 입력값이 없을 때 throw new Error
         if(!userInput) throw new Error(errorMessage.NO_INPUT);
 
         const separators = [',',':'];
+
         // 커스텀 구분자가 있는지
         if(userInput.startsWith("//")) {
             const nIndex = userInput.indexOf("\\n");
@@ -34,43 +35,48 @@ class Calculator {
                 const customSeparator = userInput.slice(2,nIndex);
                 
                 // 커스텀 구분자를 기본 구분자 중 하나인 쉼표로 치환
-                userInput = (customSeparator + userInput.slice(nIndex + 2)).split(customSeparator).join(',');
+                userInput = userInput.slice(nIndex + 2).split(customSeparator).join(',');
             }
         }
-      
+        
         // 쉼표 또는 콜론 구분자가 있는지
-       const hasDefaultSeparator = separators.some(v => userInput.includes(v));
-       
+        const hasDefaultSeparator = separators.some(v => userInput.includes(v));
+
+        // 구분자가 아닌 문자가 포함되어 있는지
+        const includeInvalidStr = userInput.split(/[,|:]/).filter(Boolean).map(Number).some(v => Number.isNaN(v));
+
        // 양수인지
-       const numbers = userInput.split(/[,|:]/).filter(Boolean).map(Number);
-       let isPositiveNum = [];
+       let isPositiveNum = false;
+       
+       const numbers = hasDefaultSeparator ? userInput.split(/[,|:]/).filter(Boolean).map(Number) : 
+       userInput.match(/-?\d+/g).map(Number);
 
        if(numbers && numbers.length > 0) {
            isPositiveNum = numbers.every(v => v > 0);
-           
            // 올바른 값일 때
-           if(isPositiveNum) {
+           if(isPositiveNum && hasDefaultSeparator && !includeInvalidStr) {
                const sum = numbers.reduce((acc, curr) => acc + curr, 0);
                result = sum;
             }
         }
 
         // 예외 처리
-        this.handleError(hasDefaultSeparator, isPositiveNum);
+        this.handleError(hasDefaultSeparator, isPositiveNum, includeInvalidStr);
         
         return result;
     }
 
-    handleError(hasDefaultSeparator, isPositiveNum) {
+    handleError(hasDefaultSeparator, isPositiveNum, includeInvalidStr) {
         if(!hasDefaultSeparator && !isPositiveNum) {
             throw new Error(errorMessage.NONE_OF_THE_VALUE);
            } else if(!hasDefaultSeparator) {
                throw new Error(errorMessage.NO_SEPARATOR);
-           } else if(isPositiveNum.length < 1) {
+           } else if(includeInvalidStr) {
+                throw new Error(errorMessage.INCLUDE_INVALID_VALUE);
+           } else if(!isPositiveNum) {
                throw new Error(errorMessage.NOT_POSITIVE_NUM);
            }
     }
-
 }
 
 export default Calculator;
--- src/utils/errorMessage.js
+++ src/utils/errorMessage.js
@@ -2,5 +2,6 @@ export const errorMessage = {
     NO_INPUT: "[Error] 문자열을 입력해주세요.",
     NOT_POSITIVE_NUM: "[Error] 양수가 포함된 문자열을 입력해주세요.",
     NO_SEPARATOR: "[Error] 문자열에 구분자를 포함해주세요. (기본 구분자 쉼표 또는 콜론 외에 '//'와 '\\n' 사이에 문자열을 커스텀 구분자로 지정 가능합니다.)",
-    NONE_OF_THE_VALUE: "[Error] 구분자와 양수를 포함한 문자열을 입력해주세요."
+    NONE_OF_THE_VALUE: "[Error] 구분자와 양수를 포함한 문자열을 입력해주세요.",
+    INCLUDE_INVALID_VALUE: "[Error] 구분자와 양수가 아닌 문자가 포함되어 있습니다.",
 }
