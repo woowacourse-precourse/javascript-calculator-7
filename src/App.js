6ee03e5a30d87152db180fbf7a781f117925c66b
Rigu1 <dlsrbtla@gmail.com>
2024-10-17 16:49:14 +0900

    feat(input): add input processing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils"; 
 class App {
+  async receiveInput() {
+    return await MissionUtils.Console.readLineAsync();
+  }
+
   async run() {
+    const str = await this.receiveInput();
   }
 }
 
