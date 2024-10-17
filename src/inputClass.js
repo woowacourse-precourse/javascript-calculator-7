1ab844e65caaaeca74767bfdbb764749e2206cfc
jeonsm22 <jeonsm22@daum.net>
2024-10-17 15:35:39 +0900

    입력값이 커스텀 구분자를 사용하는 입력인지 검증하는 클래스 함수 생성

--- src/inputClass.js
+++ src/inputClass.js
@@ -4,12 +4,22 @@ export default class Input {
     this.result = 0;
   }
 
-  isEmpty(){
+  isEmpty() {
     return this.value.length === 0;
   }
 
-  colonCommaValidation(){
+  colonCommaValidation() {
     const numberList = this.value.split(/\,|\:/);
     return numberList.every((number) => !isNaN(Number(number)));
+  }
+
+  customSeparatorValidation() {
+    const FIRST_SECOND_STRING = this.value.slice(0, 2);
+    const FORTH_FIFTH_STRING = this.value.slice(3, 5);
+
+    if (FIRST_SECOND_STRING === "//" && FORTH_FIFTH_STRING === "\\n") {
+      return true;
+    }
+    return false;
   }
 }
