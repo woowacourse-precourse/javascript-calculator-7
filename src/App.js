bccea2ae76bf678ef08a6351a5307292d2c8d31e
jeonsm22 <jeonsm22@daum.net>
2024-10-17 15:46:41 +0900

    생성한 인스턴스와 함수를 이용하여 로직 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ class App {
   async run() {
     const INPUT_STRING = await getInput();
     const inputData = new Input(INPUT_STRING);
+
+    if (inputData.isEmpty()) {
+      Console.print("결과 : " + inputData.result);
+    } else if (!inputData.isEmpty() && allValidationAndCalc(inputData) !== 0) {
+      Console.print("결과 : " + inputData.result);
+    } else {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
   }
 }
 
