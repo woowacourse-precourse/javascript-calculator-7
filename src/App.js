d5aca3b06f3a1f90315fa8cec291199fc4fba302
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-17 16:26:57 +0900

    feat: 문자열 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+  }
 }
 
 export default App;
