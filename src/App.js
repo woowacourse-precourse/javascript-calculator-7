c747ccffd9605f0b929fdc11c98c39cc865eeb43
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-10-17 10:04:17 +0900

    feat: 입력값이 문자열인지 확인하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ class App {
     const input = await Console.readLineAsync();
 
     const seperator = [":", ","];
+
+    const isString = await this.isString(input);
+
+    if (isString) {
+      Console.print(input);
+    }
+  }
+
+  async isString(input) {
+    if (typeof input === "string") {
+      return true;
+    }
+
+    throw new Error("[ERROR] 문자열을 입력해주세요.");
   }
 }
 
