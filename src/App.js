4db91e8488c76d9a4e1264c8eb81e3faf5ffa066
ockko <ocko119@gmail.com>
2024-10-17 18:54:36 +0900

    feat: 사용자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userStr = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요."
+    );
+  }
 }
 
 export default App;
