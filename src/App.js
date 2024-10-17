ba871cd4822c34c0917e9f6d447cb37bb37125c6
jungwho <jungwho0328@gmail.com>
2024-10-17 18:42:26 +0900

    feat(output): print output
    requirements3: print output

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
+import { stringSumCalculator } from "./functions";
 
 class App {
   async run() {
+    //input
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    Console.print(input);
+    //output
+    Console.print(stringSumCalculator(input));
   }
 }
 
