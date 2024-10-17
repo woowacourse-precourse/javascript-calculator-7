9f552bb3c13b591bce532017950e6f69fa52dc3f
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-18 04:09:25 +0900

    fix :regex 공백 테스트 수정

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,20 +9,23 @@ export class Calculator {
   }
 
   regexTest(input) {
+    if (input == "") return 0;
+
     let customInput = input.match(/^\/\/(.)\n/); // ; 추출
     let regexText = /[,:]/; // 기본 구분자 설정
+    let result = "";
 
     // 커스텀 구분자가 있는 케이스
     if (customInput) {
-      input.split("\\n");
+      let sliceInput = input.split("\n")[1];
       regexText = new RegExp(`[${customInput[1]},:]`); // 정규식 생성
+      result = sliceInput.split(regexText);
     } else {
+      result = input.split(regexText);
     }
-    let inpurtString = input;
-
-    const result = inpurtString.split(regexText);
     return result;
   }
+
   // add 함수내 파라미터를 레스트 파라미터로 설정
   add(...numbers) {
     let result = 0;
