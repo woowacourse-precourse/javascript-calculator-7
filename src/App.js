8e8f8413e771b1a166c29a18c4c1d5a26f190b76
zzzzzuuuuu <agu0830@naver.com>
2024-10-17 22:36:10 +0900

    feat: 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
+
 class App {
-  async run() {}
+  async run() {
+    // 문자열 입력
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    const result = await Console.readLineAsync("");
+    Console.print(result);
+  }
 }
 
 export default App;
