4c517642b09398b542bef02d9ae3b6a173250b20
K0seoyoung <koseo12@hufs.ac.kr>
2024-10-18 03:13:20 +0900

    feat:사용자 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  run() {
+    return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.')
+      .then((input) => {
+        // 입력값을 처리할 예정
+      });
+  }
 }
 
 export default App;
