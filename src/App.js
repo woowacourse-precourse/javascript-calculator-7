a8682ec1be2b7ab3fbf1233421d130223e3030a6
Yubeenpark <yoobinpark@gmail.com>
2024-10-17 21:53:11 +0900

    feat(parser): specify custom separator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const { Console } = MissionUtils;
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+      let separator = "";
+      if (input.indexOf("//") === 0 && input.indexOf("\\n") > 2) {
+        separator = input.slice(2, input.indexOf("\\n"));
+        input = input.slice(input.indexOf("\\n") + 2);
+        Console.print(separator);
+      }
+    } catch (err) {
+      Console.print(`[ERROR]${err}`);
+    }
+  }
 }
 
 export default App;
