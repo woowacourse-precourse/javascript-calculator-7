e85f734c0f3989486649b61e283e540b67d5edf4
dev-dino22 <wlsskdud02@naver.com>
2024-10-18 03:48:48 +0900

    feat(App.js): 사용자 입력 받기(+README.md 메모 수정)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     Console.print('문자열 덧셈 계산기가 실행되었습니다.');
+
+    try {
+      const inputNum = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n> ');
+      if (inputNum.length <= 0 || inputNum == null) {
+        throw new Error('[ERROR] 입력값이 비었습니다.');
+      }
+
+      Console.print(inputNum);
+
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 }
 
