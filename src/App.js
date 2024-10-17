927049dc2c19d83beaacbdff67fa77fb7df6d016
Jinny <jinny@Jinnyui-MacBookPro.local>
2024-10-17 20:51:01 +0900

    feat: 3.에러 - 에러시 '[ERROR]'메시지가 나타나게 하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 import UserInput from './View/UserInput.js';
 import notCustomConditionCheck from './Model/notCustomConditionCheck.js';
+import numberSum from './Model/numberSum.js';
+import customConditionCheck from './Model/customConditionCheck.js';
 
 class App {
   async run() {
     const input = await UserInput();
-    notCustomConditionCheck(input);
+    if (notCustomConditionCheck(input) === true || customConditionCheck(input) === true) {
+      numberSum(input);
+    } else {
+      throw new Error('ERROR] 잘못된 입력입니다.');
+    }
   }
 }
 
--- src/View/UserInput.js
+++ src/View/UserInput.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const UserInput = async () => {
-  const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+  const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. : ');
   return input;
 };
 
