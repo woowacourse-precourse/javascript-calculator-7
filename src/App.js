4ec0c1f37cfb59f1da33d0a8a960f1b7321f79ca
misung.dev <misung.dev@gmail.com>
2024-10-18 00:22:09 +0900

    feat: 첫 번째 기능 요구 사항 구현
    쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환하는 기능 구현
    - '덧셈할 문자열을 입력해 주세요.' 문구를 출력한다.
    - 덧셈할 문자열을 입력받는다.
    - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한다.
    - 분리한 각 숫자들을 'String'에서 'Number' 타입으로 변환한다.
    - 변환한 'Number' 타입의 각 숫자들의 합을 계산한다.
    - 계산된 합을 반환한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  static DEFAULT_SEPARATOR_LIST = [",", ":"];
+
+  static sumNumericStringList(numericStringList) {
+    return numericStringList.reduce(
+      (accumulator, currentValue) => accumulator + Number(currentValue),
+      0
+    );
+  }
+
+  async run() {
+    const inputString = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    let splitResultList = [inputString];
+
+    for (const separator of App.DEFAULT_SEPARATOR_LIST) {
+      let tempSplitResultList = [];
+
+      for (const splitResult of splitResultList) {
+        tempSplitResultList = [
+          ...tempSplitResultList,
+          ...splitResult.split(separator),
+        ];
+      }
+
+      splitResultList = tempSplitResultList;
+    }
+
+    return App.sumNumericStringList(splitResultList);
+  }
 }
 
 export default App;
