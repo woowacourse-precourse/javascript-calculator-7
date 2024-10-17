4f059d600bd5b7966e53a25fd0223e2fca6a465a
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-10-17 13:41:33 +0900

    feat: 커스텀 구분자 사용 여부 구분, 커스텀 구분자 추가하기

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ class App {
     const input = await Console.readLineAsync();
 
     const seperator = [":", ","];
+    const CUSTOM_PATTERN = /\/\/\D+\\n/;
+    const WHOLE_PATTERN = /\/\/\D+\\n[\s\S]+/;
 
     const isString = this.isString(input);
 
     if (isString) {
-      Console.print(input);
+      if (WHOLE_PATTERN.test(input)) {
+        const custom = input.match(CUSTOM_PATTERN);
+        const customSeperator = custom[0]
+          .split(/[//,\\n]/)
+          .filter((i) => i.length > 0);
+        seperator.push(...customSeperator);
+      }
     }
   }
 
