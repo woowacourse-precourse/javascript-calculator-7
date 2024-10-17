d9a76d7856424b491902252c3384a7ee2ecb8268
sanghyeonLee <hyeon012366@gmail.com>
2024-10-17 21:00:56 +0900

    feat : 조건부 커스텀 구분자 판단 메서드 호출 기능 및 커스텀 구분자 반환값을 계산 메서드로 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class App { //하나의 메소드는 하나의 역할
   customSeparator = "" //인스턴스 변수는 최대 2개
   async run() { //풀이 결과 반환 담당, 메소드명만 봐도 무슨역할인지 알 수 있게
     const input = await this.input()
+    if(input.slice(0,2) === "//"){
+      this.isCustom(input)
+    }
     this.calculator([...this.isCustom(input)])
     this.resultOutput()
   }
@@ -14,13 +17,14 @@ class App { //하나의 메소드는 하나의 역할
     return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
   }
   isCustom(value){ //isCustom 을 무조건 거치게 만들자 조건 넣기
-    const startIdx = value.indexOf("//")+2 //2
+    //const startIdx = value.indexOf("//")+2 //2
     const lastIdx = value.indexOf("\\n") //3
-    if(startIdx-lastIdx < 0){
-      this.customSeparator = value.slice(startIdx,lastIdx)
-      return value.slice(lastIdx+2)
+    // "\n" 가 없는 경우
+    if(lastIdx !== -1){
+      this.customSeparator = value.slice(2,lastIdx)
+      return this.calculator([...value.slice(lastIdx+2)])
     }
-    return value
+    throw new Error("[Error]: 에러발생")
   }
 
   isSeparator(value){//구분자 판단
