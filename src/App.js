5f920391066195907978930e30d75133e4e6ecae
Castillou <joajoa70584@gmail.com>
2024-10-17 15:30:55 +0900

    feat(run): add delimiter handler

--- src/App.js
+++ src/App.js
@@ -14,8 +14,24 @@ class App {
 				return;
 			}
 
-			// 2. 결과 출력
-			Console.print(`결과 : ${input}`);
+			let delimiters;
+			let numArr;
+
+			// 3. 커스텀 및 기본 구분자 체크
+			if (input.startsWith('//')) {
+				delimiters = input[2];
+				numArr = input
+					.substring(5)
+					.split(delimiters)
+					.map((v) => +v);
+			} else {
+				delimiters = /,|;/;
+				numArr = input.split(delimiters).map((v) => +v);
+			}
+
+			// 4. 결과 출력
+			const result = numArr && numArr.reduce((acc, cur) => acc + cur, 0);
+			Console.print(`결과 : ${result}`);
 		} catch (error) {
 			Console.print(error.message);
 		}
