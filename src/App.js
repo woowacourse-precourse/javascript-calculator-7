3b64c2c8ccdafae8ab2b6f74b44997b020012cd0
Castillou <joajoa70584@gmail.com>
2024-10-17 14:49:21 +0900

    feat(run): add user input handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+	async run() {
+		try {
+			// 1. 사용자 입력 받기
+			const input = await Console.readLineAsync(
+				'덧셈할 문자열을 입력해주세요.\n'
+			);
+
+			// 2. 결과 출력
+			Console.print(`결과 : ${input}`);
+		} catch (error) {
+			Console.print(error.message);
+		}
+	}
 }
 
 export default App;
