12d8c1f2689c9dadb75d3c2edc7ca5bcdbc8d75b
ockko <ocko119@gmail.com>
2024-10-17 23:18:27 +0900

    feat: 입력 기능을 지시문 아래로 이동
    * 실행 결과 예시처럼 지시문 아래에 사용자가 입력할 수 있도록 했습니다.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const userStr = await MissionUtils.Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요."
-    );
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.");
+
+    const userStr = await MissionUtils.Console.readLineAsync("");
     this.result(userStr);
   }
 
