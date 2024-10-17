78b0bc4c50ce11a9077b17badef2a2e19e217fea
Rigu1 <dlsrbtla@gmail.com>
2024-10-17 19:05:00 +0900

    feat(delimiter): add delimiter setting processing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ class App {
     return await MissionUtils.Console.readLineAsync();
   }
 
+  getDelimiter(str) {
+    let delimiter = /,|:/;
+    if (str.startsWith("//")) {
+      const [_, customDelimiter] = str.match(/\/\/(.*?)\\n/);
+      delimiter = new RegExp(/,|:/.source + '|' + customDelimiter);
+    }
+    return delimiter;
+  }
+  
   async run() {
     const str = await this.receiveInput();
+    const delimiter = this.getDelimiter(str);
+    MissionUtils.Console.print(delimiter);
   }
 }
 
