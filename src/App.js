afcc2823689249040c30ba3d7c5f279632069d0d
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-10-17 21:17:59 +0900

    feat: 커스텀구분자 지정하는 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
+import StringPlusCalculator from './calculator/StringPlusCalculator';
 
 class App {
   async run() {
     try {
       const input =
         await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+      const calculate = new StringPlusCalculator();
+      calculate.calculate(input);
     } catch (e) {
       throw new Error(e);
     }
--- src/calculator/StringPlusCalculator.js
+++ src/calculator/StringPlusCalculator.js
@@ -1,7 +1,28 @@
+import hasCustomDelimiter from './hasCustomDelimiter';
+
 class StringPlusCalculator {
   constructor() {
     this.number = 0;
     this.delimiter = [',', ':'];
+    this.numberStartIdx = 0;
+  }
+
+  calculate(input) {
+    if (hasCustomDelimiter(input)) {
+      this.addCustomDelimiter(input);
+    }
+  }
+
+  addCustomDelimiter(input) {
+    let customDelimiter = '';
+    const escapeInput = input.replace(/\\n/, '\n');
+    for (let idx = 2; idx < escapeInput.length; idx += 1) {
+      if (escapeInput[idx] === '\n') {
+        break;
+      }
+      customDelimiter += input[idx];
+    }
+    this.delimiter.push(customDelimiter);
   }
 }
 
