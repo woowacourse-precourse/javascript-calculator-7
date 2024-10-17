b55eefb82911ac51a06c1af6c9f66c326a7fdaf0
jeong922 <hwajeong922@naver.com>
2024-10-17 16:47:32 +0900

    feat(App): 입력값에서 숫자 추출하기 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  // 입력 문자열 받아오기
   async getString() {
     return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
   }
 
-  async run() {
-    try {
-      const input = await this.getString();
-      console.log(input);
-    } catch (error) {
-      console.error(error);
-    } finally {
+  async getNumbers(input) {
+    let string = input;
+    if (input === '') {
+      return [0];
     }
+
+    const match = input.match(/^\/\/(.)\\n/);
+    string = match ? string.replace(match[0], '') : string;
+
+    const regex = match ? new RegExp(`[:,${match[1]}]`) : new RegExp(`[:,]`);
+
+    const result = string.split(regex);
+
+    return result;
+  }
+
+  async run() {
+    const input = await this.getString();
+    const numbers = await this.getNumbers(input);
+    Console.print(numbers);
   }
 }
 
