3b92cd3883c29eb3b5cd00d4d40d378d2b50ecba
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 11:00:57 +0900

    :recycle: refactor: 종료 메시지 추가

--- src/models/validateinput.js
+++ src/models/validateinput.js
@@ -1,8 +1,10 @@
-import { ERROR_MESSAGE } from "../constant/messages.js";
+import { ERROR_MESSAGE, BASIC_MESSAGES } from "../constant/messages.js";
 
 function validateInput(input) {
   if (typeof input !== "string") {
-    throw new Error(ERROR_MESSAGE.invalidString);
+    const error = new Error(ERROR_MESSAGE.invalidString);
+    error.additionalMessage = BASIC_MESSAGES.exitMessage;
+    throw error;
   }
   //빈 공백만 포함하는지 확인
   if (input.trim() === "") {
@@ -12,20 +14,26 @@ function validateInput(input) {
   if (input.startsWith("//")) {
     const SEPARATORENDINDEX = input.indexOf("\n");
     if (SEPARATORENDINDEX === -1 || SEPARATORENDINDEX === 2) {
-      throw new Error(ERROR_MESSAGE.invalidCostom);
+      const error = new Error(ERROR_MESSAGE.invalidCostom);
+      error.additionalMessage = BASIC_MESSAGES.exitMessage;
+      throw error;
     }
 
     const SEPARATOR = input.substring(2, SEPARATORENDINDEX);
     //유효하지 않은 구분자 문자인지 확인
     const invalidChars = /[^a-zA-Z0-9,:\n]/;
     if (invalidChars.test(SEPARATOR)) {
-      throw new Error(ERROR_MESSAGE.invalidSeparator);
+      const error = new Error(ERROR_MESSAGE.invalidSeparator);
+      error.additionalMessage = BASIC_MESSAGES.exitMessage;
+      throw error;
     }
   }
   const numbers = extractNumbersFromInput(input);
   for (const num of numbers) {
     if (num < 0) {
-      throw new Error(ERROR_MESSAGE.invalidPositiveNumber);
+      const error = new Error(ERROR_MESSAGE.invalidPositiveNumber);
+      error.additionalMessage = BASIC_MESSAGES.exitMessage;
+      throw error;
     }
   }
   return true;
