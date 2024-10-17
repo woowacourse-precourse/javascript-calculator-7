ff3a6dcdecb3bdf02fccb1b9b0893d532185305c
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-10-17 15:05:05 +0900

    feat(App): 문자열 입력 및 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import {Console} from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+	async run() {
+		const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+		Console.print(input);
+	}
 }
 
 export default App;
