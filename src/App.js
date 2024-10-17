1c2dbaf5cf3ddca2a37c6414e343d6603528ca5b
Minje Sung <minje813@dgu.ac.kr>
2024-10-17 18:34:28 +0900

    feat(App): 사용자 입력부/결과 출력부 추가
    App.js
    - Console API import
    - 사용자 입력부 추가
    - 결과 출력부 추가
    README.md
    - 구현할 기능목록 5,6번 체크

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const UserInput = Console.readLineAsync()
+    Console.print("결과 : 1")
+  }
 }
 
 export default App;
