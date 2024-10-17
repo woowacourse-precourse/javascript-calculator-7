04bb4e2bfa10f6991d30294286c749d6689edd92
beomjo <dongjun1315@naver.com>
2024-10-17 19:57:07 +0900

    feat:ãããreceiveValue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputValue = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n'
+    );
+    Console.print(`입력된 값 : ${inputValue}`);
+  }
 }
 
 export default App;
