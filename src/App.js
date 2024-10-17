40a576f6d9b646d413157575d20c429c2d353d86
hyunyeee <5004ohy@naver.com>
2024-10-17 23:12:54 +0900

    feat(calculate): 커스텀 구분자를 사용한 계산인 지 여부에 따라 계산식을 구한다

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { InputHandler } from "./handler/InputHandler.js";
 import { CustomOperatorExtractor } from "./CustomOperatorExtractor.js";
+import { DEFAULT_OPERATOR, END_CUSTOM_OPERATOR } from "./constants/Constants.js";
 
 class App {
   #inputHandler;
   #customOperatorExtractor;
+  #isCustomCalculate;
 
   async run() {
     this.#inputHandler = new InputHandler();
     this.#customOperatorExtractor = new CustomOperatorExtractor();
     const input = await this.#inputHandler.getInput();
     const customOperator = this.#customOperatorExtractor.extractCustomStartOperator(input)
-    console.log(customOperator);
+    this.#isCustomCalculate = customOperator !== '';
+    const operatorList = this.#isCustomCalculate ? DEFAULT_OPERATOR : [customOperator];
+    const expression = this.#isCustomCalculate ? input.slice(input.indexOf(END_CUSTOM_OPERATOR) + END_CUSTOM_OPERATOR.length) : input;
   }
 }
 
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,3 @@
 export const START_CUSTOM_OPERATOR = '//';
 export const END_CUSTOM_OPERATOR = '\\n';
+export const DEFAULT_OPERATOR = [',', ':'];
