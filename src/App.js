4e5db2bb31e41ccac53d193b585d7a3ce7c43be4
chungyo <kimchungy98@gmail.com>
2024-10-17 14:55:57 +0900

    refactor: README 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import addCal from "./calculator.js";
 
 class App {
   async run() {
-    const result = addCal("1,2,3");
+    const result = addCal("//;\n 1;3;4;5");
     console.log(`Result: ${result}`);
   }
 }
--- src/calculator.js
+++ src/calculator.js
@@ -3,10 +3,10 @@
 
 ## 구현할 기능 목록
 
-1. 빈 문자열을 입력할 경우 0을 반환한다.
+1. 빈 문자열을 입력할 경우 0을 반환한다. 예외처리 우선
 2. 쉼표(,) 또는 콜론(:)을 구분자로 사용하여 문자열을 분리한 후 각 숫자의 합을 반환한다.
    - 예시: "" => 0, "1,2" => 3, "1,2,3" => 6, "1,2:3" => 6
-3. 커스텀 구분자를 지원하며, "//"와 "\n" 사이의 문자를 구분자로 지정할 수 있다.
+3. 2번에서 시험을 해봤으니, 커스텀 구분자를 만든다. 초기값은 기존의 쉽표와 콜론이며, "//"와 "\n" 사이의 문자를 구분자로 지정할 수 있다.
    - 예시: "//;\n1;2;3"은 세미콜론(;)을 구분자로 사용하고 결과는 6을 반환한다.
 4. 잘못된 입력 값이 들어올 경우, "[ERROR]"로 시작하는 메시지와 함께 Error를 발생시킨다.
 5. 입력된 문자열의 숫자들을 추출하여 덧셈 결과를 출력한다.
@@ -18,10 +18,19 @@ function addCal(input) {
     if (input === "") {
         return 0;
     }
-    // 2. 쉼표와 콜론을 구분자로 사용해 분리 후 각 숫자의 합을 반환.
-    const numbers = input.split(/[,|:]/);
-    const result = numbers.reduce((sum, num) => sum + Number(num), 0);
+    let delimiter = /[,|:]/;
 
+    // 3. 커스텀 구분자 생성
+    if(input.startsWith("//")){
+        const customDelimiterMatch = input.match(/^\/\/(.)\n/);
+        if(customDelimiterMatch){
+            delimiter = new RegExp(customDelimiterMatch[1]);
+            input = input.split("\n")[1];
+        }
+    }
+    const numbers = input.split(delimiter);
+    const result = numbers.reduce((sum, num) => sum + Number(num), 0);
+    return result;
 }
 
 export default addCal;
