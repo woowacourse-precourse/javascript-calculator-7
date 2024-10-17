5f2983f8d59c710375a04db870174c6c22b8d62f
Jang Eunhye <hh961130@naver.com>
2024-10-17 20:28:09 +0900

    feat: 구분자 설정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ class App {
   async run() {
     //입력받기
     let input = await Console.readLineAsync("사용자 입력을 해주세요 : ");
+
+    let div;
+    //구분자 설정
+    if (input.indexOf("\\n") - input.indexOf("//") == 3) {
+      // '//'와 '\n'이 존재한다면
+      div = [input.substr(2, 1)];
+    } else {
+      div = [",", ":"];
+    }
+    Console.print(div);
   }
 }
 export default App;
