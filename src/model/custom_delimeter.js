afde2db6a1fb29fe21f1436363f74c600bcc8075
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-10-17 23:00:44 +0900

    feat: 커스텀 구분자를 기준으로 구분 (배열화) 하는 기능

--- src/model/custom_delimeter.js
+++ src/model/custom_delimeter.js
@@ -4,3 +4,21 @@ export const isHaveCustomDelimiter = (input) => {
   }
   return false;
 };
+
+export const getDelimiter = (input) => {
+  const lastIndex = input.indexOf("\\n");
+  const delimiter = input.slice(2, lastIndex);
+  return delimiter;
+};
+// 구분자 찾기
+
+export const splitByDelimiter = (input, delimiter) => {
+  const formattedInput = input.split(delimiter);
+  return formattedInput;
+};
+
+export const getInputExecptDelimiter = (input) => {
+  const lastIndex = input.indexOf("\\n");
+  const inputExecptDelimiter = input.slice(lastIndex + 2);
+  return inputExecptDelimiter;
+};
