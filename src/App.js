edbde4409235e8a6c621209712bedf645b584ba9
jeong922 <hwajeong922@naver.com>
2024-10-17 15:39:13 +0900

    feat(App): 입력 문자열 받아오기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  // 입력 문자열 받아오기
+  async getString() {
+    return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+  }
+
+  async run() {
+    try {
+      const input = await this.getString();
+      console.log(input);
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  }
 }
 
 export default App;
