e2ab4f28c4d4a6bd9b406901d2ec6313ba67ba9b
정성엽 <yeop@jeongseong-yeob-ui-MacBookPro.local>
2024-10-17 21:34:07 +0900

    refactor(Util) : viewUtil.js 코드 개선 - Validate 로직을 추가하여 데이터를 입/출력시 유효성을 검사하는 로직 추가 - viewUtils를 함수에서 객체로 변환

--- src/util/ViewUtil.js
+++ src/util/ViewUtil.js
@@ -1,23 +1,25 @@
 import { Console } from "@woowacourse/mission-utils";
 import { messages } from "../ViewConstant/ViewConstants.js";
+import { Validator } from "../validate/Validate.js";
 
-export const viewUtils = () => {
+export const viewUtils = {
   // 입력 받은 데이터를 반환하는 함수
-  const inputData = async () => {
+  async inputData() {
+    const { validateEntireForm } = Validator();
     const input = await Console.readLineAsync(messages.inputMessage);
-    // TODO : InputForm Validate
+    validateEntireForm(input);
     return input;
-  };
+  },
 
   // 매개변수로 전달된 값을 콘솔에 출력하는 함수
-  const outputData = async (data) => {
-    // TODO : OutputForm Validate
-    return await Console.print(data);
-  };
+  async outputData(output) {
+    const { validateOutputForm } = Validator();
+    validateOutputForm(output);
+    return await Console.print(output);
+  },
 
   // 에러 메세지를 콘솔에 출력하는 함수
-  const errorMessage = async (err) => {
-    return await outputData(`${messages.errorMessage} ${err}`);
-  };
-  return { inputData, outputData, errorMessage };
+  async errorMessage(err) {
+    return await Console.print(`${messages.errorMessage} ${err}`);
+  },
 };
