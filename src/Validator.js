f9c30a3ae67d7a9c4cb987bddd6f303375b8270f
L2HYUNN <donghyuni98@gmail.com>
2024-10-18 00:26:01 +0900

    feat(validator): 구분자를 기준으로 분리된 문자가 양수가 아닌 경우 에러 메시지와 함께 에러를 발생시키는 기능 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,5 +1,5 @@
 import Delimiter from './Delimiter.js';
-import { isNumericString } from './utils.js';
+import { isNumericString, isPositiveNumber } from './utils.js';
 import SchemaValidator from './lib/SchemaValidator.js';
 
 class Validator {
@@ -12,6 +12,7 @@ class Validator {
   static ERROR_MESSAGE = Object.freeze({
     HAS_NOT_ALLOWED_DELIMITER:
       '[ERROR] 쉼표(,), 콜론(:) 또는 "//"와 "\n" 사이에 위치하는 문자만 구분자로 사용할 수 있어요.',
+    HAS_NEGATIVE_OR_ZERO_NUMBER: '[ERROR] 음수나 0은 사용할 수 없어요.',
   });
 
   constructor(delimiter, validator) {
@@ -23,12 +24,21 @@ class Validator {
     return value.every((aValue) => isNumericString(aValue));
   }
 
+  #hasPositiveNumber(value) {
+    return value.every((aValue) => isPositiveNumber(aValue));
+  }
+
   validate(value) {
     const delimitedString = this.#delimiter.getDelimitedString(value);
 
-    this.#validator.validate(delimitedString).with(this.#hasAllowedDelimiter, {
-      message: Validator.ERROR_MESSAGE.HAS_NOT_ALLOWED_DELIMITER,
-    });
+    this.#validator
+      .validate(delimitedString)
+      .with(this.#hasAllowedDelimiter, {
+        message: Validator.ERROR_MESSAGE.HAS_NOT_ALLOWED_DELIMITER,
+      })
+      .with(this.#hasPositiveNumber, {
+        message: Validator.ERROR_MESSAGE.HAS_NEGATIVE_OR_ZERO_NUMBER,
+      });
   }
 }
 
