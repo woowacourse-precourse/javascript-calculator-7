157772326644644b0b1b75cc2e5b27120f2f5cde
guesung <gueit214@naver.com>
2024-10-17 11:35:23 +0900

    refactor: `runCalculatorTestError`함수로 에러 테스트 케이스 모듈화

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -16,7 +16,7 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-const runCalculatorTest = async (input, output) => {
+const runCalculatorTestCorrect = async (input, output) => {
   const inputs = [input];
   const outputs = [`결과 : ${output}`];
   mockQuestions(inputs);
@@ -28,84 +28,73 @@ const runCalculatorTest = async (input, output) => {
   });
 };
 
+const runCalculatorTestError = async (input) => {
+  const inputs = [input];
+  mockQuestions(inputs);
+
+  const app = new App();
+
+  await expect(app.run()).rejects.toThrow("[ERROR]");
+};
+
 describe("문자열 계산기", () => {
   describe("커스텀 구분자 사용", () => {
     describe("커스텀 구분자 1개", () => {
       test("숫자 1개 입력", async () => {
-        await runCalculatorTest("//;\n1", "1");
+        await runCalculatorTestCorrect("//;\n1", "1");
       });
       test("숫자 3개 입력", async () => {
-        await runCalculatorTest("//v\n1v23v456", "480");
+        await runCalculatorTestCorrect("//v\n1v23v456", "480");
       });
     });
     describe("커스텀 구분자 2개", () => {
       test("숫자 1개 입력", async () => {
-        await runCalculatorTest("//;v\n1", "1");
+        await runCalculatorTestCorrect("//;v\n1", "1");
       });
       test("숫자 3개 입력", async () => {
-        await runCalculatorTest("//;v\n1v23;456", "480");
+        await runCalculatorTestCorrect("//;v\n1v23;456", "480");
       });
     });
     describe("특수한 구분자 2개", () => {
       test("숫자 1개 입력", async () => {
-        await runCalculatorTest("//;v\b\n1", "1");
+        await runCalculatorTestCorrect("//;v\b\n1", "1");
       });
       test("숫자 3개 입력", async () => {
-        await runCalculatorTest("//;v\b\n1\\23b456", "480");
+        await runCalculatorTestCorrect("//;v\b\n1\\23b456", "480");
       });
       test("중복된 특수한 구분자 & 숫자 3개 입력", async () => {
-        await runCalculatorTest("//;vv\n1v23v456", "480");
+        await runCalculatorTestCorrect("//;vv\n1v23v456", "480");
       });
     });
   });
 
   describe("기본 구분자 사용", () => {
     test("빈 문자열 입력", async () => {
-      await runCalculatorTest("", "0");
+      await runCalculatorTestCorrect("", "0");
     });
     test("숫자 1개 입력", async () => {
-      await runCalculatorTest("1", "1");
+      await runCalculatorTestCorrect("1", "1");
     });
     test("숫자 3개 입력", async () => {
-      await runCalculatorTest("1,2:3", "6");
+      await runCalculatorTestCorrect("1,2:3", "6");
     });
     test("숫자 10개 입력", async () => {
-      await runCalculatorTest("1,2:3,4:5,6:7,8:9,10", "55");
+      await runCalculatorTestCorrect("1,2:3,4:5,6:7,8:9,10", "55");
     });
   });
 
   describe("예외 테스트", () => {
     test("음수 입력", async () => {
-      const inputs = ["-1,2,3"];
-      mockQuestions(inputs);
-
-      const app = new App();
-
-      await expect(app.run()).rejects.toThrow("[ERROR]");
+      await runCalculatorTestError("-1,2,3");
     });
     test("기본 구분자 외의 구분자 사용", async () => {
-      const inputs = ["1\\2"];
-      mockQuestions(inputs);
-
-      const app = new App();
-
-      await expect(app.run()).rejects.toThrow("[ERROR]");
+      await runCalculatorTestError("1\\2");
     });
     test("커스텀 구분자 외의 구분자 사용", async () => {
-      const inputs = ["\\;\n1,2"];
-      mockQuestions(inputs);
-
-      const app = new App();
-
-      await expect(app.run()).rejects.toThrow("[ERROR]");
+      await runCalculatorTestError("\\;\n1,2");
     });
     test("숫자 외의 문자 입력", async () => {
-      const inputs = ["\\;\na"];
-      mockQuestions(inputs);
-
-      const app = new App();
-
-      await expect(app.run()).rejects.toThrow("[ERROR]");
+      await runCalculatorTestError("\\;\na");
     });
   });
 });
