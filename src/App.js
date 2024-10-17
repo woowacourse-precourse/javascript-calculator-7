c48546178708e889788c7b663b27a37cced1690d
sanghyeonLee <hyeon012366@gmail.com>
2024-10-17 17:10:38 +0900

    feat : 입력 메서드 추가

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ class App { //하나의 메소드는 하나의 역할
   sum = 0 
   customSeparator = "" //인스턴스 변수는 최대 2개
   async run() { //풀이 결과 반환 담당, 메소드명만 봐도 무슨역할인지 알 수 있게
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+    const input = await this.input()
     this.calculator([...this.isCustom(input)])
     this.resultOutput()
   }
+ //에러처리
 
+  async input(){ //입력 메서드
+    return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+  }
   isCustom(value){ //isCustom 을 무조건 거치게 만들자 조건 넣기
     const startIdx = value.indexOf("//")+2 //2
     const lastIdx = value.indexOf("\\n") //3
