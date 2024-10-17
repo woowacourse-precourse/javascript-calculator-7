d2ee3bae4c7e335e12989f73b6afcbdb9ce6fa2e
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-18 03:59:35 +0900

    feat :regex 함수 테스트코드 작성

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,14 @@
 export class Calculator {
-  calculate(input) {}
+  calculate(input) {
+    const sliceNumbers = this.regexTest(input);
+    if (sliceNumbers.length === 0) return `[결과 : 0]`;
+    if (sliceNumbers) {
+      return `[결과 : ${this.add(sliceNumbers)}]`;
+    }
+    return "[ERROR]";
+  }
 
   regexTest(input) {}
-
   // add 함수내 파라미터를 레스트 파라미터로 설정
   add(...numbers) {
     let result = 0;
--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,7 +1,7 @@
 import { Calculator } from "./Calculator";
 const calculator = new Calculator();
 
-// add 함수 테스트 코드 작성
+// add 함수 테스트 코드 정의
 describe("add test", () => {
   const a = 1,
     b = 2,
@@ -10,11 +10,23 @@ describe("add test", () => {
 
   // a와 b를 각각 add 함수에 전달하여 더하기
   test("a + b = 3", () => {
-    expect(calculator.add(a, b)).toBe(3);
+    expect(calculator.add(a, b)).toBe(3); // 두 인자를 전달
   });
 
   // a, b, c, d를 각각 add 함수에 전달하여 더하기
   test("a + b + c + d = 10", () => {
-    expect(calculator.add(a, b, c, d)).toBe(10);
+    expect(calculator.add(a, b, c, d)).toBe(10); // 네 인자를 전달
+  });
+});
+
+// regex 함수 테스트 코드 정의
+describe("regex test", () => {
+  const testInput = "//;\n1;2;3";
+  const noneCustomTestInput = "1;2;3";
+  test("//;\\n1;2;3", () => {
+    expect(calculator.regexTest(testInput)).toEqual([1, 2, 3]);
+  });
+  test("1;2;3", () => {
+    expect(calculator.regexTest(noneCustomTestInput)).toEqual([1, 2, 3]);
   });
 });
