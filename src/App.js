59b475d16736721dd2c1e55796aede1bf3ada98f
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-10-17 15:12:59 +0900

    feat(App): 구분자 할당 및 문자열 출력

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import {Console} from '@woowacourse/mission-utils';
 class App {
 	async run() {
 		const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-		Console.print(input);
+		const {values, delimiter} = this.handleString(input);
+		Console.print(`결과 값 : ${values} 구분자:${delimiter}`);
+	}
+
+	// 문자열을 처리함수
+	handleString(input) {
+		// 기본 구분자 할당
+		let delimiter = /,|:/;
+		let values = input;
+
+		// 커스텀 구분자 처리
+		const customDelimiterPattern = /^\/\/(.)\\n/;
+		const match = input.match(customDelimiterPattern);
+		if (match) {
+			delimiter = new RegExp(match[1]);
+			values = input.split('\\n')[1];
+		}
+		return {values, delimiter};
 	}
 }
 
