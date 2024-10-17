fd8819898b37460c725615a8c56bba9b41dc8062
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-10-17 11:43:57 +0900

    feat: 문자열 입력 받기 - 사용자로부터 문자열 입력받고, 비동기 처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n'); // 문자열 입력
+    } catch (error) {
+      this.handleError(); // 예외 처리
+    }
+  }
 
   // 1. 예외 처리 함수
   handleError() {
     Console.print('[ERROR]');
     throw new Error('[ERROR]');
   }
-  
+
 }
 
 export default App;
