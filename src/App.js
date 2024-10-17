c1866bf0833f2df47a0e82d4a522c9de324a8379
zzzzzuuuuu <agu0830@naver.com>
2024-10-17 23:38:27 +0900

    feat: 커스텀 구분자로 문자열 구분

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ class App {
   async run() {
     // 문자열 입력
     Console.print("덧셈할 문자열을 입력해 주세요.");
-    const result = await Console.readLineAsync("");
+    const input = await Console.readLineAsync("");
+
+    let words;
 
     // 문자열 구분
-    let words = result.split(/[,|:]/); // 콤마 또는 콜론
+    if (input.startsWith("//") && input.substring(3, 5) === "\\n") {
+      words = input.substring(5).split(input[2])
+    } else {
+      words = input.split(/[,|:]/);
+    }
     Console.print(words);
   }
 }
