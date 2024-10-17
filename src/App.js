f3edb08b5552d68aee90e72f43c081869f6e2c5e
Glyph8 <newdy0808@gmail.com>
2024-10-17 17:26:46 +0900

    feat(console) : 문자열 입출력 기능 추가
    Console.print를 이용한 안내 메세지 출력 및 Console.readLineAsync를 이용한 입력기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+//프로그래밍 요구 사항 - 라이브러리 Console API 사용할 것
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요 : ");
+    //비동기처리 입력을 위해 await을 사용.
+
+    Console.print(`결과 : ${input}`);
+  }
 }
 
 export default App;
