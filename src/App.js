73a4c0f8a2dbf2b9321e6974e6e6e3ef71ec38fa
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-10-17 23:16:10 +0900

    feat: App.js를 Controller로 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { formatInput } from "./model/formatter.js";
+import { getInput } from "./View/Input.js";
+import { printOutput } from "./View/Output.js";
+import { checkInputValidation } from "./validation/validation.js";
+import { plusCalculator } from "./utils/plus.calculator.js";
+
 class App {
-  async run() {}
+  async run() {
+    let input = await getInput();
+    input = formatInput(input);
+    checkInputValidation(input);
+    const additionResult = plusCalculator(input);
+    printOutput(additionResult);
+  }
 }
 
 export default App;
