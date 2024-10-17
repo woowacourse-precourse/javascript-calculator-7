da9bdbb5f0195bfbf2a737fc4eb6e35e8cfa93ed
mlnyx <nsm110@naver.com>
2024-10-18 01:28:06 +0900

    feat: 문자열 입력 기능 및 기본 구조 설정
    - 사용자로부터 덧셈할 문자열 입력받기 구현
    - 기본 변수(numbersArr, defaultSeparators, sum) 초기화
    - @woowacourse/mission-utils의 Console 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    Console.print("덧셈할 문자열을 입력해주세요.");
+    const inputStr = await Console.readLineAsync("");
+    let numbersArr = [];
+    const defaultSeparators = ",:"; // 기본 구분자 (쉼표와 콜론)
+    let sum = 0; // 최종 덧셈 결과
+  }
 }
 
 export default App;
