a66fbc3dc737307f073650ebbdbb72c1d4b07dfb
jungwho <jungwho0328@gmail.com>
2024-10-17 18:37:29 +0900

    feat(delimiter): add custom delimiter
    requirements2-2: confirm custom delimiter and reset delimiter, update input

--- src/functions.js
+++ src/functions.js
@@ -3,4 +3,14 @@ export const stringSumCalculator = (input) => {
 
   //basic delimiter
   let delimiter = /,|:/;
+
+  //custom delimiter
+  if (input[0] === "/" && input[0] === "/") {
+    let nIndex = input.indexOf("\\n");
+    if (nIndex === -1) throw new Error("[ERROR]잘못된 형식입니다.");
+    else {
+      delimiter = new RegExp(`,|:|${input.slice(2, nIndex)}`);
+      input = input.slice(nIndex + 2);
+    }
+  }
 };
