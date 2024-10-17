8667c4cf65d8c78c6991bf5c3c42d5e6ffb80e82
chan8632 <duscks8632@gmail.com>
2024-10-17 21:55:55 +0900

    feat(App): 사용자 입력 처리 - MissionUtils 라이브러리 import - run 메서드에 사용자 입력 받기 및 처리 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.print("덧셈을 입력하세요");
+    const userInput = await MissionUtils.Console.readLineAsync("");
+    const divider = /[:,]/;
+    const numberList = userInput.split(divider).map(parseInt(x));
+  }
 }
 
 export default App;
