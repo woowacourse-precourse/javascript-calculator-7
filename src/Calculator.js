e6c151b5b64dc0b806872d443a15f0e25ca63746
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-10-18 05:13:31 +0900

    refactor :단일책임원칙 분리

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,6 @@
 export class Calculator {
   calculate(input) {
-    const sliceNumbers = this.regexTest(input);
+    const sliceNumbers = this.testRegex(input);
     if (sliceNumbers.length === 0) return `결과 : 0`;
     if (sliceNumbers.error) {
       return "[ERROR]";
@@ -8,29 +8,30 @@ export class Calculator {
     return `결과 : ${this.add(...sliceNumbers.numbers)}`;
   }
 
-  regexTest(input) {
+  testRegex(input) {
     if (input == "") return 0;
     if (input.length > 10000) return { error: true };
-
     let regexText = /[,:]/; // 기본 구분자 설정
     let result = "";
+    if (checkCustom) result = input.split(regexText);
+    // result type :  array<string>
+    if (this.checkMin(result)) return { error: true };
+    result = changeFloat(result);
+    return { error: false, numbers: result };
+  }
 
-    let customInput = input.match(/^\/\/(.)\\n/); // ; 추출
+  checkCustom(string) {
+    let customInput = string.match(/^\/\/(.)\\n/); // ; 추출
     // 커스텀 구분자가 있는 케이스
     if (customInput) {
       let sliceInput = input.split("\\n")[1];
       regexText = new RegExp(`[${customInput[1]},:]`); // 정규식 생성
-      result = sliceInput.split(regexText);
-    } else {
-      result = input.split(regexText);
+      const result = sliceInput.split(regexText);
+      return result;
     }
-    // result type :  array<string>
-    if (this.minCheck(result)) return { error: true };
-    result = floatChange(result);
-
-    return { error: false, numbers: result };
   }
-  floatChange(result) {
+
+  changeFloat(result) {
     const newResult = result.map((number) => {
       let preNum = parseFloat(number);
       if ((preNum * 10) % 10 == 0) {
@@ -41,7 +42,7 @@ export class Calculator {
     return newResult;
   }
 
-  minCheck(result) {
+  checkMin(result) {
     for (let index = 0; index < result.length; index++) {
       let preNum = parseFloat(result[index]);
       if (preNum < 0 || isNaN(preNum)) return false;
