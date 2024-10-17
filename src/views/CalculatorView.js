b968b1cd4cc42ec7c7182b287cf5d87463ddff55
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-17 13:29:28 +0900

    feat: 사용자 입력 받는 기능 추가

--- src/views/CalculatorView.js
+++ src/views/CalculatorView.js
@@ -3,3 +3,13 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export function printInputMessage() {
   MissionUtils.Console.print('덧셈할 문자열을 입력해 주세요.');
 }
+
+export function getUserInput(callback) {
+  MissionUtils.Console.readLineAsync('')
+    .then((input) => {
+      callback(input);
+    }) // 입력된 값 callback 전달
+    .catch((error) => {
+      MissionUtils.Console.print(error);
+    }); // 에러처리
+}
