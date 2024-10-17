c7bc11858fd8325bca3412ce96e332759e7532b6
jeonsm22 <jeonsm22@daum.net>
2024-10-17 15:23:35 +0900

    입력 함수 생성 및 입력 값을 통한 인스턴스 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { getInput } from "./inputFunctions";
+
 class App {
   async run() {
-    
+    const INPUT_STRING = await getInput();
+    const inputData = new Input(INPUT_STRING);
   }
 }
 
---
+++ src/InputFunctions.js
@@ -0,0 +1,5 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export function getInput() {
+  return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+}
