00a20de60f8564fed7c7fdbf8622948683bdcf8f
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-10-17 19:52:24 +0900

    feat(app): 문자열 입력받기 기능추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input =
+        await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
 }
 
 export default App;
