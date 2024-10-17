240125eeffa4ef16ed2ff46113b7dd63007d6aa2
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 21:39:56 +0900

    :sparkles: feat: 출력 창

--- src/view.js
+++ src/view.js
@@ -1,3 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 
-class CalculatorView {}
+class CalculatorView {
+  async inputView() {
+    return await Console.readLineAsync("문자열을 입력하세요:\n");
+  }
+
+  async outputView() {
+    return await Console.readLineAsync(`결과: ${RESULT}`);
+  }
+}
