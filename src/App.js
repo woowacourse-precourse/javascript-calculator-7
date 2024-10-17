c6b1c9a734d69e7135d5847efda6a0f9c4ea4e21
eunji kang <miki2323@naver.com>
2024-10-17 19:01:40 +0900

    feat: 사용자 입출력 함수 구현
    - Console로 입력받은 사용자 Data 출력
    - woowacourse mission utils 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import { Console } from "@woowacourse/mission-utils";
 
 // 사용자 입력
-function getUserInputData() {
+async function getUserInputData() {
+  const userInputData = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");  
+  return userInputData;
 }
 
 // 숫자 추출
-function extractNumber(formula) {
+function extractNumber(userInputData) {
+  return userInputData
 }
 
 // 숫자 연산
 function calculator(numberArr) {
+  return numberArr
 }
 
 // 예외 처리
 function errorHandling() {
+
 }
 
-
 class App {
-  async run() {}
+  async run() {
+    const userInput = await getUserInputData();
+    const result = calculator(extractNumber(userInput));
+
+    Console.print("결과 : " + result.toString())
+  }
 }
 
 export default App;
