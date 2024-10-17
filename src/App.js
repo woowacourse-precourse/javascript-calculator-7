4c15106aaaed2962fe4485e6e3dbe0e4959c88cc
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-10-17 09:17:21 +0900

    feat: 기본 구분자 배열 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync();
+
+    const seperator = [":", ","];
+  }
 }
 
 export default App;
