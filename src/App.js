7a922699abaae388a07b07d6dcb9718680f466a4
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-17 17:43:15 +0900

    refact(App.js) : 입력 받은 값을 상수에 받는 것으로 변경

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ import Errors from "./constants/Errors.js";
 class App {
   async run() {
     // 입력 메세지 출력 및 입력 받기
-    let input = await Console.readLineAsync(Messages.INPUT_MESSAGE);
+    const INPUT = await Console.readLineAsync(Messages.INPUT_MESSAGE);
 
     // 커스텀 구분자가 있는지 확인
-    const CUSTOM_DELIMITER = this.checkCustomDelimiter(input);
+    const CUSTOM_DELIMITER = this.checkCustomDelimiter(INPUT);
 
-    //커스텀 구분자가 있다면 커스텀 구분자 이후로 문자열을 자름
-    if (CUSTOM_DELIMITER) {
-      input = input.slice(input.indexOf("\\n") + 2);
-    }
+    // 커스텀 구분자가 있다면 커스텀 구분자 이후로 문자열을 자름
+    const PROCESSED_INPUT = CUSTOM_DELIMITER
+      ? INPUT.slice(INPUT.indexOf("\\n") + 2)
+      : INPUT;
 
     // 정규식을 동적으로 생성
     const DELIMITER_REGEX = new RegExp(`[,:${CUSTOM_DELIMITER}]`);
 
     // 구분자에 따라 입력 받은 문자열을 숫자로 변환
-    const numbers = input.split(DELIMITER_REGEX).map(Number);
+    const NUMBERS_ARRAY = PROCESSED_INPUT.split(DELIMITER_REGEX).map(Number);
 
     // 에러처리
     // 숫자가 아닌 문자열이 포함되어 있는 경우
-    if (numbers.some((number) => isNaN(Number(number)))) {
+    if (NUMBERS_ARRAY.some((number) => isNaN(Number(number)))) {
       throw new Error(Errors.NOT_NUMBER);
     }
 
     // 음수가 포함되어 있는 경우
-    if (numbers.some((number) => number < 0)) {
+    if (NUMBERS_ARRAY.some((number) => number < 0)) {
       throw new Error(Errors.HAS_NEGATIVE);
     }
 
     // 숫자의 합 계산
-    const SUM = numbers.reduce((acc, cur) => acc + cur, 0);
+    const SUM = NUMBERS_ARRAY.reduce((acc, cur) => acc + cur, 0);
     Console.print(`${Messages.OUTPUT_MESSAGE}${SUM}`);
   }
 
