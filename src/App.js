c14f325b0723c218e5758b505ec446688826f4e0
Yubeenpark <yoobinpark@gmail.com>
2024-10-17 22:49:11 +0900

    feat(calculator): add positive number validation and update error message

--- src/App.js
+++ src/App.js
@@ -8,24 +8,31 @@ class App {
       let result = 0;
       let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
       let separator = "";
+
       if (input.indexOf("//") === 0 && input.indexOf("\\n") > 2) {
         separator = input.slice(2, input.indexOf("\\n"));
         input = input.slice(input.indexOf("\\n") + 2);
         input = input.replaceAll(separator, ",");
       }
+
       input = input.replaceAll(":", ",");
       const arr = input.split(",");
+
       for (const el of arr) {
-        const number = +el;
-        if (Number.isNaN(number)) {
-          throw Error("[ERROR]처리할 수 없는 입력입니다.");
+        if (el === "") {
+          continue;
         } else {
-          result += number;
+          const number = +el;
+          if (Number.isNaN(number) || number < 0) {
+            throw new Error("[ERROR]"); // 예외를 명확하게 throw 합니다.
+          } else {
+            result += number;
+          }
         }
       }
-      console.log(`결과 : ${result}`);
+      Console.print(`결과 : ${result}`);
     } catch (err) {
-      throw Error(`[ERROR]${err}`);
+      throw new Error("[ERROR]");
     }
   }
 }
