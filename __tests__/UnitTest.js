c4b5c91e62cd5c2036144a317c8d49d7b259feb7
wo-o29 <woogur29@gmail.com>
2024-10-17 19:23:14 +0900

    test: 유효성 검사 테스트 코드 작성

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,4 +1,12 @@
-import { parseUserInput, extractCustomSeparators, replaceAllPattern } from "../src/utils";
+import {
+  parseUserInput,
+  extractCustomSeparators,
+  replaceAllPattern,
+  assertCondition,
+  isValidCustomSeparator,
+  isAllPositive,
+} from "../src/utils";
+import { MESSAGES } from "../src/constants";
 
 describe("문자열의 특정 패턴을 바꾸고 싶은 문자열로 모두 바꾼다.", () => {
   test("\\n를 모두 \n로 변환한다.", () => {
@@ -62,5 +70,48 @@ describe("커스텀 문자열에서 커스텀 구분자를 추출한다.", () =>
     const customString2 = "///n";
     const customSeparators2 = extractCustomSeparators(customString2);
     expect(customSeparators2).toEqual([]);
+  });
+});
+
+describe("유효성 검사들을 테스트한다.", () => {
+  test("커스텀 문자열이 커스텀 형식에 맞지 않다면 false를 반환한다.", () => {
+    const customString = "//1\n";
+    const customSeparators = extractCustomSeparators(customString);
+
+    expect(isValidCustomSeparator(customString, customSeparators)).toBeFalsy();
+  });
+
+  test("커스텀 문자열이 커스텀 형식에 맞다면 true를 반환한다.", () => {
+    const customString = "//a\n";
+    const customSeparators = extractCustomSeparators(customString);
+
+    expect(isValidCustomSeparator(customString, customSeparators)).toBeTruthy();
+  });
+
+  test("숫자 배열에 음수가 포함되어 있다면 false를 반환한다.", () => {
+    const numbers = [0, 1, 2, 3];
+
+    expect(isAllPositive(numbers)).toBeFalsy();
+  });
+
+  test("숫자 배열이 모두 양수라면 true를 반환한다.", () => {
+    const numbers = [1, 2, 3];
+
+    expect(isAllPositive(numbers)).toBeTruthy();
+  });
+
+  test("인자로 받은 조건식이 true라면 예외 처리가 발생한다.", () => {
+    const numbers = [0, 1, 2, 3];
+
+    expect(() => {
+      assertCondition(!isAllPositive(numbers), MESSAGES.INVALID_ALL_POSITIVE);
+    }).toThrow(MESSAGES.INVALID_ALL_POSITIVE);
+
+    const customString = "//123\n";
+    const customSeparators = extractCustomSeparators(customString);
+
+    expect(() => {
+      assertCondition(!isValidCustomSeparator(customString, customSeparators), MESSAGES.INVALID_CUSTOM_SEPARATOR);
+    }).toThrow(MESSAGES.INVALID_CUSTOM_SEPARATOR);
   });
 });
