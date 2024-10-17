60f38e3ac6506722af5261810a1aedba73cded03
dawn0314 <sdawn0314@gmail.com>
2024-10-17 15:40:08 +0900

    refactor: split logics

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
 import { Console } from "@woowacourse/mission-utils";
+import {
+  ERROR_EMPTY_STRING,
+  ERROR_INVALID_DELIMITER,
+  CUSTOM_DELIMITER,
+} from "./Constants.js";
 
 class App {
   async run() {
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
-    if (input === "") {
-      throw new Error("[ERROR] 문자열을 입력해주세요");
+    try {
+      const result = this.calculateSum(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  calculateSum(input) {
+    if (!input) {
+      throw new Error(ERROR_EMPTY_STRING);
     }
 
-    let delimiters = /,|:/;
-    let customDelimiter = input.match(/\/\/(.*?)\\n/);
+    const numbers = this.splitInput(input);
+    return this.sumNumbers(numbers);
+  }
 
-    if (customDelimiter) {
+  splitInput(input) {
+    let delimiter = /,|:/;
+
+    const customDelimiterMatch = input.match(CUSTOM_DELIMITER);
+
+    if (customDelimiterMatch) {
       const slicedInput = input.slice(5);
-      const reg = new RegExp(`[^0-9${customDelimiter[1]}]`);
-
-      if (reg.test(slicedInput)) {
-        throw new Error("[ERROR] 올바른 커스텀 구분자를 입력하세요.");
-      }
-
-      delimiters = new RegExp(customDelimiter[1]);
+      delimiter = new RegExp(customDelimiterMatch[1]);
       input = slicedInput;
     }
-    let numArray = input.split(delimiters);
 
+    const numbers = input.split(delimiter);
+    return numbers;
+  }
+
+  sumNumbers(numbers) {
     let sum = 0;
 
-    for (let num of numArray) {
+    for (let num of numbers) {
       if (isNaN(Number(num)) || Number(num) < 0) {
-        throw new Error("[ERROR] 올바른 구분자를 입력하세요.");
+        throw new Error(ERROR_INVALID_DELIMITER);
       }
 
       sum += parseInt(num);
     }
 
-    return Console.print(`결과 : ${sum}`);
+    return sum;
   }
 }
 
