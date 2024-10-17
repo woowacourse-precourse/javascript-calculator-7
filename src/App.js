f58007e860c852b56465bec1a2788cd8507c7e0b
이서현 <mellifluous3380@gmail.com>
2024-10-17 14:49:02 +0900

    feat: 문자열 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+    async run() {
+        Console.print('덧셈할 문자열을 입력해 주세요.');
+        const input = await Console.readLineAsync('');
+    }
 }
 
 export default App;
