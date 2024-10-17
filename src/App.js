edbecc0cc8c459523d35b9a92ca6029c018549b5
KimPuro <puro@protonmail.ch>
2024-10-17 19:52:51 +0900

    feat(App): 문자열 입력 기능을 추가
    @woowacourse/mission-utils의 Console.readLineAsync를 이용해 사용자로부터 문자열을 입력받고 이를 console.log로 출력합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.")
+    console.log(str);
+  }
 }
 
 export default App;
