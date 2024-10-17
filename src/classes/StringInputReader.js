7d080c992f6ae5ce10961040277cd5ceb768f51f
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-17 19:07:45 +0900

    chore: 입력 프롬프트 수정
    - 덧셈할 문자열을 입력하는 프롬프트에 줄바꿈 추가

--- src/classes/StringInputReader.js
+++ src/classes/StringInputReader.js
@@ -3,7 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 class StringInputReader {
   async getInput() {
     const input = await Console.readLineAsync(
-      '덧셈할 문자열을 입력해 주세요: '
+      '덧셈할 문자열을 입력해 주세요.\n'
     );
     return input;
   }
