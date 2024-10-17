54bafc106f4eda4673489d2a2b17f84f335503bb
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-10-18 03:19:29 +0900

    feat: add feature to split numbers by default delimiters and calculate sum

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    const numbers = this.splitNumbersByDelimiter(input);
+    const result = this.calculateSum(numbers);
+
+    Console.print(`결과 : ${result}`);
+  }
+
+  splitNumbersByDelimiter(input) {
+    const DELIMITERS = /[,:]/;
+    const stringNumbers = input.split(DELIMITERS);
+
+    return stringNumbers.map(num => parseInt(num));
+  }
+
+  calculateSum(numbers) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
   }
 }
 
