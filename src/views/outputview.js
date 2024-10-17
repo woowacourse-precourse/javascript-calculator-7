3aa8f367a413cb02d356769f8e50071d6a9bd9e0
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 13:37:51 +0900

    :recycle: refector: 오류수정

--- src/views/outputview.js
+++ src/views/outputview.js
@@ -3,11 +3,11 @@ import { BASIC_MESSAGES } from "../constant/messages.js";
 
 class OutputView {
   printCalcsum(message) {
-    Console.print(BASIC_MESSAGES.calcOutputMessage(message));
+    Console.print(BASIC_MESSAGES.calcOutputMessage);
   }
 
   printExit(message) {
-    Console.print(BASIC_MESSAGES.exitMessage(message));
+    Console.print(BASIC_MESSAGES.exitMessage);
   }
 }
 
