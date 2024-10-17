28e53996900d0149ceb1e4b6c0cbb37ae57dbdaf
rhtjddls123 <jjjk0605@naver.com>
2024-10-17 20:06:37 +0900

    fix: 구분자를 통해 문자열 분리하는 부분 오류 해결
    입력된 값을 구분자를 통해 분리하는 과정에서 커스텀 구분자로 |를 생성하지
    않고 사용해도 통과하던 오류 해결
    정규식의 메타문자나 이스케이프 시퀀스가 커스텀 구분자로 쓰일 경우
    발생하던 오류 해결

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App {
   extractInput(str, sep) {
     // 입력된 값을 구분자를 통해 분리
     // 구분자를 이용하여 정규 표현식을 만들어준뒤 split함수를 통해 분리
-    const sepRegex = new RegExp(`[${sep.join("|")}]+`);
+    const sepRegex = new RegExp(`[${sep.map((s) => s.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&"))}]`);
     return str.split(sepRegex).filter((v) => v != "");
   }
 
