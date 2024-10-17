ef3b565dfa52cb3307d183cede48165079b3b555
jin123457 <jin_ox@naver.com>
2024-10-17 15:06:16 +0900

    refactor (CheckDelimiter) : update regex patterns in delimiter function

--- src/Utils/CheckDelimiter.js
+++ src/Utils/CheckDelimiter.js
@@ -1,31 +1,14 @@
 const CheckDelimiter = str => {
-  const delimiterSet = new Set();
-  const custArr = ['/', '/', '\\', 'n'];
-  let custIdx = 0;
-  let custIdxPos = [0, 0];
+  const findDelimiter = str.match(
+    /(?<=\d+)(,)(?=\d+)|(?<=\d+)(:)(?=\d+)|(?<=\/\/)(.*?)(?=\\n)/g,
+  );
 
-  str.split('').map((v, i) => {
-    if ((v === ',' || v === ':') && !delimiterSet.has(v)) {
-      return delimiterSet.add(v);
-    }
-
-    if (custArr[custIdx] === v && custIdx === 0) {
-      custIdxPos[0] = i;
-      custIdx += 1;
-    } else if (custArr[custIdx] === v && custIdx === 3) {
-      custIdxPos[1] = i;
-      custIdx = 0;
-      custIdxPos = [0, 0];
-      return delimiterSet.add(str.slice(custIdxPos[0] + 2, custIdxPos[1] - 1));
-    } else if (custArr[custIdx] === v) {
-      custIdx += 1;
-    }
-  });
-
-  if (!delimiterSet.size) {
-    throw new Error(`[ERROR] 구분자 혹은 커스텀 구분자가 존재하지 않습니다.`);
+  if (findDelimiter.length > 0) {
+    const delimiterSet = new Set([...findDelimiter]);
+    return delimiterSet;
   }
-  return delimiterSet;
+
+  throw new Error(`[ERROR] 구분자 혹은 커스텀 구분자가 존재하지 않습니다.`);
 };
 
 export default CheckDelimiter;
