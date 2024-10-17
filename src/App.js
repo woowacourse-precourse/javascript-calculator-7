17382f830618db7d5c0c1ba07202455098c8c01e
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-17 11:47:06 +0900

    :sparkles: feat: 구동점

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import CalculatorController from "./controllers/calculatorcontroller";
+
 class App {
-  async run() {}
+  #controller;
+
+  constructor() {
+    this.#controller = new CalculatorController();
+  }
+
+  async run() {
+    await this.#controller.startRun();
+  }
 }
+
+const app = new App();
+app.run();
 
 export default App;
