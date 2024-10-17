1c7987c205aadbda1e1c1674a51d843c6cd94037
eunoia-jaxson <eunoia@handong.ac.kr>
2024-10-17 13:43:17 +0900

    refactor: 입력 처리 및 계산 로직 리팩토링
    airbnb 스타일 가이드를 따르기 위해 코드를 리팩토링 했습니다.
    코드를 리팩토링 할수록, 점점 깔끔해지는 것을 볼 수 있었습니다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Console } from "@woowacourse/mission-utils";
-import { CALCULATOR_MESSAGE, ERROR_MESSAGE } from "./constants.js";
+import { Console } from '@woowacourse/mission-utils';
+import { CALCULATOR_MESSAGE, ERROR_MESSAGE } from './constants.js';
 import {
   failIsNumbers,
   failVaildSeparator,
   failNumberRange,
-} from "./validation.js";
+} from './validation.js';
 
 class App {
   async run() {
     await this.enterInput();
+
     try {
       this.calculateResult();
       this.printResult();
@@ -18,27 +19,30 @@ class App {
   }
 
   async enterInput() {
-    Console.print(CALCULATOR_MESSAGE.CALCULATOR_START);
-    this.input = await Console.readLineAsync("");
-  }
-
-  splitInput() {
-    if (this.input.startsWith("//")) {
-      const [customDelimiter, separatedInput] = this.input
-        .slice(2)
-        .split("\\n");
-      return separatedInput.split(customDelimiter);
-    }
-
-    return this.input.split(/,|:/);
+    this.input = await Console.readLineAsync(
+      CALCULATOR_MESSAGE.CALCULATOR_START
+    );
   }
 
   calculateResult() {
     const numbers = this.splitInput().map((number) =>
-      number === "" ? 0 : number
+      number === '' ? 0 : number
     );
+
     this.checkInput(numbers);
     this.sum = numbers.reduce((acc, cur) => acc + Number(cur), 0);
+  }
+
+  splitInput() {
+    if (this.input.startsWith('//')) {
+      const [customDelimiter, separatedInput] = this.input
+        .slice(2)
+        .split('\\n');
+
+      return separatedInput.split(customDelimiter);
+    }
+
+    return this.input.split(/,|:/);
   }
 
   printResult() {
