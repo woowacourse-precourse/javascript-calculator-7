bdde7c6a6e46dc79510cec87390e3360cb9fe0fa
정해성 <gotjd0230@naver.com>
2024-10-17 09:03:24 +0900

    feat: 커스텀 구분자 유무 판단 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const DEFAULT_SEPARATORS = [',', ':'];
+const DEFAULT_ELEMENTS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ...DEFAULT_SEPARATORS];
 
 const sum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
 
 const separate = (str, sep) => sep.reduce((acc, cur) => acc.replaceAll(cur, ','), str).split(',').map(Number);
 
-const customParse = (str) => str.includes('//') ? [str.substring(5), str.substr(2, 1)] : [str, null];
+const customParse = (str, isCustomed) => isCustomed ? [str.substring(5), str.substr(2, 1)] : [str, null];
 
 const checkMaximumLength = (line) => {
   if (line.length > 15) throw('[Error] 최대 글자 제한인 15자를 초과하였습니다.');
 }
 
+const checkCustom = (line) => line.split('').some((char) => !DEFAULT_ELEMENTS.includes(char));
 
 const check = (line) => {
   checkMaximumLength(line);
@@ -22,9 +24,10 @@ class App {
     const separators = [...DEFAULT_SEPARATORS];
 
     const line = await Console.readLineAsync('');
+    const isCustomed = checkCustom(line);
     check(line);
 
-    const [parsedLine, customSeparator] = customParse(line);
+    const [parsedLine, customSeparator] = customParse(line, isCustomed);
     separators.push(customSeparator);
 
     const numbers = separate(parsedLine, separators);
