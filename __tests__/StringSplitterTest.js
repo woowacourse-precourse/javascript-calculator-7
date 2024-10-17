a60e3a56dca66524e5179533409f4b7b088aba0d
Seio924 <seio9241@gmail.com>
2024-10-17 20:08:53 +0900

    refactor(StringSplitterTest): 커스텀 구분자 확인 방식 수정

--- __tests__/StringSplitterTest.js
+++ __tests__/StringSplitterTest.js
@@ -1,34 +1,27 @@
-import App from "../src/App";
-import { getLogSpy, mockQuestions } from "./ApplicationTest";
+import Calculator from "../src/Calculator";
+import { mockQuestions } from "./ApplicationTest";
 
 describe("문자열 구분", () => {
   test("커스텀 구분자 설정 부분이 없을 때 false를 반환하는 지 확인", async () => {
-    const inputs = ["//;\\1"];
-    mockQuestions(inputs);
+    const input = "//;\\1";
+    mockQuestions(input);
 
-    const logSpy = getLogSpy();
     const output = false;
 
-    const app = new App();
-    await app.run();
+    const cal = new Calculator(input);
 
-    inputs.forEach(() => {
-      expect(logSpy).toBe(output);
-    });
+    const result = cal.hasCustomDelimiter();
+    expect(result).toBe(output);
   });
 
   test("커스텀 구분자 설정 부분이 있을 때 true를 반환하는 지 확인", async () => {
-    const inputs = ["//;\\n1"];
-    mockQuestions(inputs);
+    const input = "//;\\n1";
+    mockQuestions(input);
 
-    const logSpy = getLogSpy();
     const output = true;
+    const cal = new Calculator(input);
 
-    const app = new App();
-    await app.run();
-
-    inputs.forEach(() => {
-      expect(logSpy).toBe(output);
-    });
+    const result = cal.hasCustomDelimiter();
+    expect(result).toBe(output);
   });
 });
