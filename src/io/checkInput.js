f0d0416b1d5cf60a157b7574fbde90bf17c160d8
JaiIn <di0308@naver.com>
2024-10-17 19:35:40 +0900

    feat: input이 비어있을 경우 0 출력

---
+++ src/io/checkInput.js
@@ -0,0 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const checkInput = (input) => {
+
+    let sum = 0;
+
+    if(input.length === 0) sum = 0;
+
+    // Console.print("문자열 : " + input);
+    Console.print("결과 : " + sum);
+};
+
+export default checkInput;
