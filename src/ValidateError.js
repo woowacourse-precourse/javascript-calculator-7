afaca71ec5ecd1776376e157c4006afa4791ed7a
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-17 21:26:22 +0900

    feat: 커스텀구분자 지정 형식 확인 기능 - 커스텀 구분자가 //와 \n 사이에 지정되어 있지 않으면 오류를 발생시킵니다.

--- src/ValidateError.js
+++ src/ValidateError.js
@@ -1,4 +1,10 @@
 export class ValidateError {
+  static validateCustomDelimiterFormat(input) {
+    const customDelimiterPattern = /^\/\/.\n/;
+    if (!customDelimiterPattern.test(input)) {
+      throw new Error('커스텀 구분자 지정 형식이 잘못되었습니다');
+    }
+  }
   static validateNoWhitespace(input) {
     if (input.includes(' ')) throw new Error('공백이 포함되어 있습니다.');
   }
