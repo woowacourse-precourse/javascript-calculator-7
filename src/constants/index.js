d0e06d139f1480effb7cb2f1c9a538d512db95d2
ryanbae94 <ryanbae94@gmail.com>
2024-10-17 12:06:43 +0900

    refactor (constants): 에러 메세지 수정
    - ERROR의 notNumber 에러 코드에 해당하는 메세지를 좀 더 정확하게 수정
    	- 기존: 계산할 값은 0~9 사이의 숫자만 입력할 수 있습니다.
    	- 변경: 계산할 값은 0~9 사이의 정수만 입력할 수 있습니다.
    
    숫자라는 표현은 모호할 수 있으므로, 정수만 입력하도록 안내하여 3.2와 같은 입력 방지

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const MESSAGE = {
 
 export const ERROR = {
 	empty: '데이터를 입력하지 않았습니다.',
-	notNumber: '계산할 값은 0~9 사이의 숫자만 입력할 수 있습니다.',
+	notNumber: '계산할 값은 0~9 사이의 정수만 입력할 수 있습니다.',
 	whitespace: '공백을 사용할 수 없습니다.',
 	negative: '음수를 입력할 수 없습니다.',
 };
