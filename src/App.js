439efbf319a726e0b8d5f5e9cbf1edcdc79c013d
Yeongi Park <yeongi0111@naver.com>
2024-10-17 22:18:15 +0900

    refactor : 한가지 일을 않은 입력값 검증 함수 분리
    사용자 입력값 검증하는 메서드가 검증과 입력값 추출후 반환, 2가지 일을
    하고 있었다. 이를 각각 다른 일을 하는 메서드로 분리하여 한가지 일을 하는
    함수로 분리

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ class App {
 
   async run() {
     const userInput = await this.#inputText();
+    // 사용자 입력값 검증하기
+    this.#validateUserInput(userInput);
 
+    // 검증된 사용자 입력값 커스텀 구분자 추출하기
     const { customSeparator, processedUserInput } =
-      this.#validateUserInput(userInput);
+      this.#exportUserInput(userInput);
 
-    const sumResult = this.#sumUserInput(
-      processedUserInput || userInput,
-      customSeparator,
-    );
+    const sumResult = this.#sumUserInput(processedUserInput, customSeparator);
     this.#printSumResult(sumResult);
   }
 
@@ -30,23 +30,31 @@ class App {
 
   // 입력값 검증하기
   #validateUserInput(input) {
+    // 더블슬래시와 숫자로 시작하는지 검증
+    this.#validator.validateStartsWith(input);
+
+    // 커스텀 구분자와 정제된 입력값 추출
+    const { customSeparator, processedUserInput } =
+      this.#exportUserInput(input);
+
+    // 구분자를 제대로 사용했는지 검증
+    this.#validator.validateUsedSeparator(processedUserInput, customSeparator);
+  }
+
+  #exportUserInput(input) {
     let customSeparator = null;
     let processedUserInput = null;
 
-    this.#validator.validateStartsWith(input);
-
+    // 더블슬래시로 시작하는 경우만 추출
     if (this.#validator.checkStartWidthDubbleSlash(input)) {
+      // 사용 가능한 커스텀 구분자인지 검증
       this.#validator.validateCustomSeparator(input);
+
       customSeparator = UserInputHandler.getCustomSeparator(input);
       processedUserInput = UserInputHandler.getRemovedCustomSeparator(input);
     }
 
-    this.#validator.validateUsedSeparator(
-      processedUserInput || input,
-      customSeparator,
-    );
-
-    return { customSeparator, processedUserInput };
+    return { customSeparator, processedUserInput: processedUserInput || input };
   }
 
   #sumUserInput(input, customSeparator) {
