c0d53f9dabf89886f4924db4a17c6ac32eff00f0
Xen-alpha <hky7548@naver.com>
2024-10-17 18:35:19 +0900

    feat(App): add input parsing logic

--- src/App.js
+++ src/App.js
@@ -10,11 +10,32 @@ class App {
     ); // 1단계: 그대로 출력
   }
   processInput(inputstr) {
-    const condCustomInput = /(\/\/.{1}\\n.+)/;
+    const condCustomInput = /\/\/[^0-9]+\\n.+/g;
+    const condCustomSep = /[^\/\\n]+/g; // exec()[0]의 결과가 seperator
+    const condSepValid = /[^0-9]+/g;
+    const condAbnormalFormat = /[^0-9,:]+/g;
     // 커스텀 구분자가 있는지 확인
-    // 커스텀 구분자가 있을 경우
-    // 커스텀 구분자가 없을 경우
-    return inputstr;
+    if (
+      condCustomInput.test(inputstr) &&
+      condSepValid.test(inputstr.match(condCustomSep)[0]) // Note:이건 구분자가 적절한지(숫자 미포함인지) 확인하는 코드
+    ) {
+      // 커스텀 구분자가 있을 경우
+      const expList = inputstr.match(condCustomSep);
+      if (expList.length !== 2) throw new Error("[ERROR]: 잘못된 포맷입니다.");
+      return expList[1]
+        .split(expList[0])
+        .map((value) => value.split(/(:|,)/g).filter((value) => !isNaN(value)))
+        .reduce((prev, curr) => prev.concat(curr), []);
+    } else if (!condAbnormalFormat.test(inputstr)) {
+      // 커스텀 구분자가 없으며 포맷에 맞는 경우 정해진 값대로 split
+      return inputstr
+        .split(/(:|,)/g)
+        .filter((value) => !isNaN(value))
+        .map((value) => parseInt(value));
+    } else {
+      // 잘못된 입력
+      throw new Error("[ERROR]: 잘못된 포맷입니다.");
+    }
   }
 }
 
