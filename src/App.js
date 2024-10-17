25accb47b1ddc7403cec7b08ad0611cf238da22a
zzzzzuuuuu <agu0830@naver.com>
2024-10-17 22:47:00 +0900

    feat: 쉼표(,) 또는 콜론(:)으로 문자열 구분

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App {
     // 문자열 입력
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const result = await Console.readLineAsync("");
-    Console.print(result);
+
+    // 문자열 구분
+    let words = result.split(/[,|:]/); // 콤마 또는 콜론
+    Console.print(words);
   }
 }
 
