3ca27a05c02d1164b093292cf0e888a05876c724
Lee <dltmdgns0508@gmail.com>
2024-10-17 16:04:23 +0900

    feat(App): 문자열 입력 기능 추가
    App.js 변경
    - Console.readLineAsync 함수를 이용해 문자열 입력 받음
    - 입력받은 문자열의 값이 없을시 error 반환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    Console.print('계산할 문자열을 입력해 주세요');
+    const input = await Console.readLineAsync('');
+    if (!input) {
+      throw new Error('[Error] 문자열이 입력되지 않았습니다.');
+    }
+  }
 }
 
 export default App;
