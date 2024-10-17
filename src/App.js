bf1c623fc192543a2351af7e66e171b12916ec65
K0seoyoung <koseo12@hufs.ac.kr>
2024-10-18 03:51:49 +0900

    fix:커스텀 구분자 사용 버그 해결

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ class App {
     if (input === '') {
       return 0;
     }
-  
+
     let delimiters = [',', ':'];
     let numbersStr = input;
 
+    // 커스텀 구분자 체크
     if (input.startsWith('//')) {
       const delimiterEndIndex = input.indexOf('\\n');
       if (delimiterEndIndex === -1) {
@@ -30,11 +31,13 @@ class App {
       numbersStr = input.substring(delimiterEndIndex + 2); // '\\n'의 길이는 2
     }
 
-    const tokens = this.splitNumbers(input, delimiters);
-  
+    // 구분자로 숫자 분리
+    const tokens = this.splitNumbers(numbersStr, delimiters);
+
+    // 문자열을 숫자로 변환하고 합계 계산
     const numbers = tokens.map((token) => {
-      if (token === ''){
-        throw new Error(`[ERROR] 잘못된 숫자입니다.`);
+      if (token === '') {
+        throw new Error('[ERROR] 잘못된 숫자입니다.');
       }
       const num = Number(token);
       if (isNaN(num) || num <= 0) {
@@ -42,12 +45,13 @@ class App {
       }
       return num;
     });
-  
+
     const sum = numbers.reduce((acc, curr) => acc + curr, 0);
     return sum;
   }
-  
+
   splitNumbers(numbersStr, delimiters) {
+    // 구분자에 특수 문자가 있는 경우 이스케이프 처리
     const escapedDelimiters = delimiters.map((delim) =>
       delim.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
     );
