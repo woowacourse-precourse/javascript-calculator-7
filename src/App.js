8a3bbe3d28c216678e2a190e4c07970585c0412f
KimYoungWoo <rladuddn3924@gmail.com>
2024-10-17 21:48:01 +0900

    feat: 구분자 리스트 선언 및 분리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  // 클래스 내 변수 선언
+  constructor() {
+    this.separators = [",", ":"];
+  }
+
   async run() {
     // 입력 받기
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
@@ -8,6 +13,11 @@ class App {
     // 빈 문자열 처리
     if (input === "") {
       return Console.print("결과 : 0");
+    }
+
+    // 구분자 처리
+    for (const separator of this.separators) {
+      input = input.split(separator).join(",");
     }
   }
 }
