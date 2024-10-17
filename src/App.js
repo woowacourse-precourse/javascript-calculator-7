1a9cce46ddecedca05ba2feea9c9e4261a94f2bb
eunji kang <miki2323@naver.com>
2024-10-17 18:55:34 +0900

    feat: 기능 별 함수 골격 생성
    - 기능 요구 사항에 1:1 매핑되는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+
+// 사용자 입력
+function getUserInputData() {
+}
+
+// 숫자 추출
+function extractNumber(formula) {
+}
+
+// 숫자 연산
+function calculator(numberArr) {
+}
+
+// 예외 처리
+function errorHandling() {
+}
+
+
 class App {
   async run() {}
 }
