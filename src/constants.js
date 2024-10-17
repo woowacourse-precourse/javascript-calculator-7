f9fdb812726ba054e32985d86941616b2acd582b
seonae-j <wkdtjsdo135@naver.com>
2024-10-17 23:20:42 +0900

    feat: ERROR 메시지 문구 상수화
    예외 처리 전 사용하게 될 에러문구를 constants 파일을 만들어서 모아두었습니다.
    - constants.js : 파일 추가 및 ERROR 상수 선언

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const ERROR = {
+  NUMBER_ERROR: '[ERROR] 올바른 숫자 형식이 아닙니다.',
+  SEPARATOR_ERROR: '[ERROR] 올바른 구분자 형식이 아닙니다.',
+  CUSTOM_SEPERATOR_ERROR:
+    '[ERROR] 커스텀 구분자 생성 기호가 올바르지 않습니다.',
+  CUSTOM_ORDER_ERROR: '[ERROR] 커스텀 구분자 생성 순서가 올바르지 않습니다.',
+  CUSTOM_INPUT_ERROR:
+    '[ERROR] 커스텀 구분자 생성 기호 위치가 올바르지 않습니다.',
+};
