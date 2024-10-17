17a6c6e6219e1f9cf0090da8c1120abd46861f4f
Jang Eunhye <hh961130@naver.com>
2024-10-17 22:20:41 +0900

    feat: 구분자 기준으로 숫자 분리

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ class App {
     //입력받기
     let input = await Console.readLineAsync("사용자 입력을 해주세요 : ");
 
-    let div;
+    let delimiter;
     //구분자 설정
     if (input.indexOf("\\n") - input.indexOf("//") == 3) {
       // '//'와 '\n'이 존재한다면
-      div = [input.substr(2, 1)];
+      delimiter = [input.substr(2, 1)];
+      input = input.substr(5);
     } else {
-      div = [",", ":"];
+      delimiter = /[:,]/; // 정규식 : 콜론(:)과 쉽표(,)중 하나와 일치하는 패턴
     }
-    Console.print(div);
+    let splitInputNumber = input.split(delimiter); // 문자열을 구분자 기준으로 구분하여 저장
+    Console.print(splitInputNumber);
   }
 }
 export default App;
