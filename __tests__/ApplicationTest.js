ca76920c5aac2fe12b167966d5cb2524e07acf51
rhtjddls123 <jjjk0605@naver.com>
2024-10-17 20:04:57 +0900

    test: 테스트 항목 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,6 +32,21 @@ describe("문자열 계산기", () => {
     });
   });
 
+  test("커스텀 구분자 사용2", async () => {
+    const inputs = ["//.\\n1.2.3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", async () => {
     const inputs = ["-1,2,3"];
     mockQuestions(inputs);
@@ -39,5 +54,145 @@ describe("문자열 계산기", () => {
     const app = new App();
 
     await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자 생성이 문자열 맨 앞이 아닐경우 예외 테스트", async () => {
+    const inputs = ["//.\\n1,2,3.4//;\\n5"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자가 여러개일 경우", async () => {
+    const inputs = ["//;\\n//hi\\n1,2:3hi4;5hi6"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 21"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("입력값이 없는 경우", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자에 이스케이프 시퀀스가 들어가는 경우", async () => {
+    const inputs = ["//\\t\\n1\\t2"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 3"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자에 이스케이프 시퀀스가 여러개 들어가는 경우", async () => {
+    const inputs = ["//\\t\\a\\n1\\t\\a2\\t\\a3,4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("이스케이프 시퀀스를 가진 커스텀 구분자가 여러개 들어가는 경우", async () => {
+    const inputs = ["//\\t\\n//\\a\\n1\\a2\\t,3:4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자에 특수문자가 들어가는 경우", async () => {
+    const meta = [
+      "!",
+      "@",
+      "#",
+      "$",
+      "%",
+      "^",
+      "&",
+      "*",
+      "(",
+      ")",
+      "()",
+      "`",
+      "~",
+      "-",
+      "_",
+      "+",
+      "=",
+      "[",
+      "]",
+      "[]",
+      "{",
+      "}",
+      "{}",
+      ";",
+      ":",
+      "'",
+      '"',
+      "''",
+      '""',
+      "<",
+      ",",
+      ".",
+      ">",
+      "/",
+      "?",
+      "\\",
+      "|"
+    ];
+    for (const m of meta) {
+      const inputs = [`//${m}\\n1${m}2,3`];
+      mockQuestions(inputs);
+
+      const logSpy = getLogSpy();
+      const outputs = ["결과 : 6"];
+
+      const app = new App();
+      await app.run();
+
+      outputs.forEach((output) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      });
+    }
   });
 });
