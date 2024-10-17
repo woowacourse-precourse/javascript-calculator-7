470718ab74a0fd6cb1408947fa29ac0465810317
kochungwon <yud01063@gmail.com>
2024-10-17 09:12:32 +0900

    feat(계산기): 문자열 덧셈 계산기 입력값 받기
    - 문자열 입력값을 MissionUtils모듈을 통해 받아왔습니다.
    - MissionUtils모듈을 통해 출력값을 확인했습니다.
    - 유효한 입력인지 검사하는 isValid 함수 로직을 설계했습니다.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    Console.readLineAsync()
-    // 를 이용하여 문자열 입력값을 받는다.
-    // 입력값을 입력하지 않은경우 "[ERROR] 입력값을 입력하지 않았습니다." 라는 메시지와 함께 앱을 종료시킨다.
-    // 사용자가 잘못된값을 입력한경우 "[ERROR] 잘못된 값을 입력하셨습니다."라는 메시지와 함께 앱을 종료시킨다
-    // 유효한 입력값일 경우, 계산하는 함수를 실행한다.
-    calculate()
-    // 계산 결과를 출력한다.
-    Console.print()
-  }
+    // Console.readLineAsync를 이용하여 문자열 입력값을 받는다.
+    try {
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
 
-  // 입력값을 계산하는 함수
-  calculate() {
+      // 입력값을 입력하지 않은경우 "[ERROR] 입력값을 입력하지 않았습니다." 라는 메시지와 함께 앱을 종료시킨다.
+      if (input.length===0){
+        MissionUtils.Console.print('[ERROR] 입력값을 입력하지 않았습니다.');
+        return
+      }
+      //유효한 입력인지 검사
+      if (isValid(input)){
+        // 유효한 입력값일 경우, 계산하는 함수를 실행한다.
+        calculate()
+      }else{
+        // 사용자가 잘못된값을 입력한경우 "[ERROR] 잘못된 값을 입력하셨습니다."라는 메시지와 함께 앱을 종료시킨다
+        MissionUtils.Console.print('[ERROR] 잘못된 값을 입력하셨습니다.');
+        return
+      }
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR]:${error}`);
+    }
+    
+    // 계산 결과를 출력한다.
+    MissionUtils.Console.print('계산성공')
+  }
+  //유효값을 계산하는함수
+  isValid(){
     // 구분자 배열을 만든다. [',', ':']
     // 커스텀 구분자가 있는 지 확인한다.
     // 커스텀 구분자가 있을시  (//)와 (\n) 사이에 있는 커스텀 구분자를 구분자 배열에 추가한다.
     // 입력값에서 커스텀 구분자를 제거한다.
     //[',', ':', '(//)와 (\n) 사이에 있는 커스텀 구분자']를 제외한 구분자를 입력한 경우 "[ERROR] 구분자를 잘못 입력하셨습니다."라는 메시지와 함께 앱을 종료시킨다.
+    MissionUtils.Console.print('[ERROR] 잘못된 값을 입력하셨습니다.');
+  }
+  
+  // 입력값을 계산하는 함수
+  calculate() {
     // 구분자를 기준으로 숫자들을 분리한다.
     // 숫자 중 음수가 있는지 체크한다.
     // 입력값중 숫자가 음수일경우 "[ERROR] 음수의 값을 입력하셨습니다."라는 메시지와 함께 앱을 종료시킨다.
