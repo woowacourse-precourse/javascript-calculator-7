5763b84a76841d2f70de2d1f08af4f537ce32a79
jin123457 <jin_ox@naver.com>
2024-10-17 15:28:41 +0900

    fix (CheckDelimiter) : add null value handling

--- src/Utils/CheckDelimiter.js
+++ src/Utils/CheckDelimiter.js
@@ -3,12 +3,13 @@ const CheckDelimiter = str => {
     /(?<=\d+)(,)(?=\d+)|(?<=\d+)(:)(?=\d+)|(?<=\/\/)(.*?)(?=\\n)/g,
   );
 
-  if (findDelimiter.length > 0) {
-    const delimiterSet = new Set([...findDelimiter]);
-    return delimiterSet;
+  if (findDelimiter == null) {
+    throw new Error(`[ERROR] 구분자 혹은 커스텀 구분자가 존재하지 않습니다.`);
   }
 
-  throw new Error(`[ERROR] 구분자 혹은 커스텀 구분자가 존재하지 않습니다.`);
+  const delimiterSet = new Set([...findDelimiter]);
+
+  return delimiterSet;
 };
 
 export default CheckDelimiter;
