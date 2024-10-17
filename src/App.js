400eda25f64adcc10b30671422ecc257295666b5
rhtjddls123 <jjjk0605@naver.com>
2024-10-17 23:06:25 +0900

    fix: 길이가 긴 구분자를 사용할때 예상과 다르게 동작하는 부분 해결
    커스텀 구분자가 123이라고 할 때 1312323의 경우 13, 23으로 분리되어 36을
    반환해야 하지만 구분자중 한 개의 문자만 포함되어있어도 구분자로 사용되던
    오류 해결

--- src/App.js
+++ src/App.js
@@ -42,7 +42,10 @@ class App {
     // 입력된 값을 구분자를 통해 분리
     // 구분자를 이용하여 정규 표현식을 만들어준뒤 split함수를 통해 분리
     const sepRegex = new RegExp(
-      `[${this.separator.map(s => s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'))}]`,
+      this.separator
+        .map(s => s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'))
+        .join('|'),
+      'g',
     );
     return this.input.split(sepRegex).filter(v => v !== '');
   }
