72bf6df9f29e210ab552a2908879aa83e791c316
ddozza54 <bananamayong@gmail.com>
2024-10-18 01:27:54 +0900

    feat: 각 숫자의 합 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+const { Console } = MissionUtils;
+
 class App {
-  async run() {}
+  async run() {
+    Console.readLine(
+      '덧셈할 문자열을 입력해 주세요.',
+      (answer) => {
+        const sum = answer
+          .split(/[:,]/g)
+          .reduce((a, c) => Number(a) + Number(c));
+        console.log(`결과: ${sum}`);
+      }
+    );
+  }
 }
 
 export default App;
