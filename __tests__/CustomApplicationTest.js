0e41edf0df4fe28a5f742b3072de3be1af4ee2fe
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-10-18 00:00:21 +0900

    refactor: 하나의 테스트 안에서 여러 테스트 케이스를 시도해볼 수 있도록 개선

--- __tests__/CustomApplicationTest.js
+++ __tests__/CustomApplicationTest.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
-
+  
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
     const input = inputs.shift();
     return Promise.resolve(input);
@@ -33,18 +33,19 @@ describe("문자열 계산기", () => {
   });
 
   test("-(하이픈)을 구분자로 가지는 문자열을 전달한 경우, 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.", async () => {
-    const inputs = ["//-\\n1,2-3"];
-    mockQuestions(inputs);
+    const inputs = ["//-\\n1,2-3", "//-\\n1-2-30"];
+    
+    for(let i = 0; i < inputs.length; i++) {
+      mockQuestions(inputs);
+      const logSpy = getLogSpy();
+      const outputs = ["결과 : 6", "결과 : 33"];
+    
+      const app = new App();
+      await app.run();  
+  
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(outputs[i]));
+    }
 
-    const logSpy = getLogSpy();
-    const outputs = ["결과 : 6"];
-
-    const app = new App();
-    await app.run();
-
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
   });
 
   test("2자리 이상 숫자는 2개의 수가 아니라 하나의 수로 처리한다.", async () => {
