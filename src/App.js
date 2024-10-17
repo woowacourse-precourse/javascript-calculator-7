a4be1c777e81534e319a1349e5266b85c8a56742
youngsu <everdudtn@gmail.com>
2024-10-18 00:07:05 +0900

    feat(app): 예외처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    Console.print("문자열을 입력해 주세요");
-    const input = await Console.readLineAsync("");
+    try {
+      Console.print("문자열을 입력해 주세요");
+      const input = await Console.readLineAsync("");
 
-    let numbers;
-    let sum = 0;
+      let numbers;
+      let sum = 0;
 
-    numbers = input.split(/,|:|\/\/.\\n/);
-    Console.print(numbers);
+      numbers = input.split(/,|:|\/\/.\\n/);
+      Console.print(numbers);
 
-    numbers.map((n) => {
-      sum += Number(n);
-    });
+      numbers.map((n) => {
+        if (Number(n) < 0) {
+          throw new Error("[ERROR]");
+        }
+        sum += Number(n);
+      });
 
-    Console.print(sum);
+      if (isNaN(sum)) {
+        throw new Error("[ERROR]");
+      } else {
+        Console.print("결과 : " + sum);
+      }
+    } catch (e) {
+      Console.print("[ERROR]");
+      throw e;
+    }
   }
 }
 
