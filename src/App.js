4fa9082e80f0f0be71728f96175c777377711768
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-17 22:07:01 +0900

    feat(app): mission-utils를 사용한 실행 및 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+	async run() {
+		try {
+			const res = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+
+			Console.print(`결과 : ${res}`);
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 }
 
 export default App;
