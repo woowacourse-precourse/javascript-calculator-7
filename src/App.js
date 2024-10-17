647a8e93254467bacb718d81cecda930eb2f5580
Jang Eunhye <hh961130@naver.com>
2024-10-17 23:31:12 +0900

    feat: 분리된 숫자 덧셈 및 출력

--- src/App.js
+++ src/App.js
@@ -15,7 +15,12 @@ class App {
       delimiter = /[:,]/; // 정규식 : 콜론(:)과 쉽표(,)중 하나와 일치하는 패턴
     }
     let splitInputNumber = input.split(delimiter); // 문자열을 구분자 기준으로 구분하여 저장
-    Console.print(splitInputNumber);
+
+    let sum = 0;
+    splitInputNumber.forEach((num) => {
+      sum += Number(num); // 구분된 문자형 숫자를 숫자 타입으로 변환하여 덧셈
+    });
+    Console.print("결과 : " + sum);
   }
 }
 export default App;
