6477e396f64cb5b455bb552a85cd95ab34a0d13d
hyunyeee <5004ohy@naver.com>
2024-10-17 23:39:02 +0900

    feat(calculate): 사용자가 잘못된 값을 입력할 경우 에러를 던진다.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,27 @@
+import { INPUT_ERROR_MESSAGE } from "./constants/Messages.js";
+
 export class Calculator {
   calculate(operatorList, expression) {
     const op = this.#makeRegularExpression(operatorList);
-    return expression.split(op);
+    const splitNumbers = expression.split(op);
+    splitNumbers.some((number) => this.#validateNumber(number));
   }
 
   #makeRegularExpression(operatorList) {
     const escapedOperators = operatorList.map(operator => `\\${operator}`).join('|');
     return new RegExp(`[${escapedOperators}]`);
+  }
+
+  #validateNumber(number){
+    this.#validatePositiveNumber(number);
+    this.#validateIsNaN(number);
+  }
+
+  #validatePositiveNumber(number) {
+    if (number <= 0) throw new Error(INPUT_ERROR_MESSAGE);
+  }
+
+  #validateIsNaN(number) {
+    if (isNaN(Number(number))) throw new Error(INPUT_ERROR_MESSAGE);
   }
 }
