e2ebbbeddfd24db4f74951217c210d26b379b157
Jang Eunhye <hh961130@naver.com>
2024-10-17 17:59:23 +0900

    feat: 모듈참조 및 사용자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+//모듈 사용
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    //입력받기
+    let input = await Console.readLineAsync("사용자 입력을 해주세요 : ");
+  }
 }
-
 export default App;
