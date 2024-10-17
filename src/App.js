6a321e3a2928c5dd1df8ea759f172e4309716b65
gumbi <gusn9719@gmail.com>
2024-10-17 19:24:24 +0900

    feat: 빈 문자열 및 null 입력 시 0 반환 기능 추가
    입력이 빈 문자열("")이거나 null일 경우 결과로 0을 반환하는 기능을
    구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ class App {
     async run() {
         try {
             const input = await MissionUtils.Console.readLineAsync('덧셉할 문자열을 입력해 주세요.\n');
-            const result = calculate(input);
+            const result = this.calculate(input);
 
-            Console.print(`결과 : ${result}`);
+            MissionUtils.Console.print(`결과 : ${result}`);
         } catch (e) {
             MissionUtils.Console.print(e.message);
         }
+    }
+
+    calculate(input) {
+        if (!input) return 0;
     }
 }
 
