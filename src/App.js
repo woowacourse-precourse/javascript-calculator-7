9e68dca00953095ec6d8cf4336d4e3046a0b3090
gumbi <gusn9719@gmail.com>
2024-10-17 19:00:23 +0900

    feat: 문자열 덧셈 계산기 기본 입력 기능 추가
    MissionUtils.Console.readLineAsync()를 사용하여 사용자로부터 문자열 입력을 받는 기능을 구현했습니다.
    MissionUtils.Console.print()를 사용하여 결과를 출력하게 설정해 뒀습니다.
    기본 구조를 잡았습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+    async run() {
+        try {
+            const input = await MissionUtils.Console.readLineAsync('덧셉할 문자열을 입력해 주세요.\n');
+            const result = calculate(input);
+
+            Console.print(`결과 : ${result}`);
+        } catch (e) {
+            MissionUtils.Console.print(e.message);
+        }
+    }
 }
 
 export default App;
