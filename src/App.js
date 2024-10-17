5b605058bdd05143bb250464460f0abec161a319
정해성 <gotjd0230@naver.com>
2024-10-17 15:18:14 +0900

    refactor: 에러 체크 관심사 분리 및 주요 함수 수정
    - 전체 문자열, 커스텀 절, 문자열 절 세 가지로 구분
    - checkCustom -> checkIsNormal: 커스텀 문자가 있는 지가 아닌 기본 문자열인지 판단
    - 전체 문자열에서 커스텀 구분자를 추출하는 것에서 커스텀 절과 문자열 절로 분리하는 것으로 변경
    - 커스텀 구분자 에러 메시지를 구분할 수 있도록 변경

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import { Console } from "@woowacourse/mission-utils";
 const DEFAULT_SEPARATORS = [',', ':'];
 const DEFAULT_ELEMENTS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ...DEFAULT_SEPARATORS];
 
-const checkCustom = (line) => line.split('').some((char) => !DEFAULT_ELEMENTS.includes(char));
-
 const checkMaximumLength = (line) => {
   if (line.length > 15) throw('[Error] 최대 글자 제한인 15자를 초과하였습니다.');
 }
 
-const checkSlashStart = (line) => {
-  if (!line.startsWith('//')) throw('[Error] 커스텀 구분자를 잘못 사용하였습니다.');
+const checkDoubleSlashStart = (str) => {
+  if (!str.startsWith('//')) throw('[Error] 커스텀 구분자를 잘못 사용하였습니다. (//로 시작해야 함)');
 }
 
-const checkBackslashN = (line) => {
-  if (!line.includes('\\n')) throw('[Error] 커스텀 구분자를 잘못 사용하였습니다.');
+const checkBackslashNEnd = (str) => {
+  if (!str.endsWith('\\n')) throw('[Error] 커스텀 구분자를 잘못 사용하였습니다. (\\n으로 끝나야 함)');
 }
 
-const check = (line, isCustomed) => {
+const checkLine = (line) => {
   checkMaximumLength(line);
-
-  if (isCustomed) {
-    checkSlashStart(line);
-    checkBackslashN(line);
-  }
 }
 
-const customParse = (str, isCustomed) => isCustomed ? [str.substring(5), str.substr(2, 1)] : [str, null];
+const checkCustomPart = (str) => {
+  checkDoubleSlashStart(str);
+  checkBackslashNEnd(str);
+}
+
+const checkLinePart = (str, sep) => {
+}
+
+const checkIsNormal = (line) => !line.split('').some((char) => !DEFAULT_ELEMENTS.includes(char));
+
+const divideCustom = (str) => [str.substring(0, 5), str.substring(5)];
 
 const separate = (str, sep) => sep.reduce((acc, cur) => acc.replaceAll(cur, ','), str).split(',').map(Number);
 
@@ -37,16 +40,27 @@ class App {
     const separators = [...DEFAULT_SEPARATORS];
 
     const line = await Console.readLineAsync('');
-    const isCustomed = checkCustom(line);
-    check(line, isCustomed);
+    checkLine(line);
+    
+    if (checkIsNormal(line)) {
+      const numbers = separate(line, separators);
+      const result = sum(numbers);
 
-    const [parsedLine, customSeparator] = customParse(line, isCustomed);
-    separators.push(customSeparator);
+      Console.print(result);
+      return;
+    }
 
-    const numbers = separate(parsedLine, separators);
+    const [customPart, linePart] = divideCustom(line);
+    checkCustomPart(customPart);
+
+    separators.push(customPart[2]);
+    checkLinePart(linePart, separators);
+
+    const numbers = separate(linePart, separators);
     const result = sum(numbers);
 
     Console.print(result);
+    return;
   }
 }
 
