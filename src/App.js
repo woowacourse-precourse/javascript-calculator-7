c3a4033f0b7309988dc6e456739f0ad77a43d459
songhj10207@naver.com <songhj10207@naver.com>
2024-10-18 23:24:12 +0900

    feat: 사용자 문자열 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  constructor() {}
+
+  async getUserinput() {
+    const userinput = MissionUtils.Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+    return userinput;
+  }
+
+  async run() {
+    const userinput = await this.getUserinput();
+  }
 }
 
 export default App;
