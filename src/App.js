b57d13ba5423e6e11495aa548e319ddabec441ab
wnsxk2 <wnsxk206@gmail.com>
2024-10-19 00:53:35 +0900

    feat: 문자열을 숫자로 변환하는 기능 추가
    문자열로 이루어진 배열을 숫자로 변환하고 숫자의 총합을 반환하는 코드를 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ class App {
   async run() {
     try {
       const regex = /[,:]/;
+
       const answer = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-      Console.print(`결과 : ${answer.split(regex)}`);
+
+      const sum = answer.split(regex)
+        .reduce((total, num) => total + Number(num), 0 );
+        
+      Console.print(`결과 : ${sum}`);
     } catch (error) {
       // reject 되는 경우
       Console.print(`에러 발생: ${error.message}`);
