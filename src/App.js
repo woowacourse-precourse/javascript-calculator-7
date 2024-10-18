8dd57167982f758260081f4547d44779f4501d06
AlgeMoya <kjhobd2@naver.com>
2024-10-18 21:21:00 +0900

    feat: 사용자로부터 값 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     Console.print("덧셈할 문자열을 입력해 주세요.");
-    // Console.readLineAsync();
+    const input = await Console.readLineAsync("");
   }
 }
 
