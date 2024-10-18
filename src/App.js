675ac1c0b78cb5403d5ebd5ad3ea89fa4599decb
yeeun426 <thsudkcla7@naver.com>
2024-10-18 19:54:45 +0900

    feat : 커스텀 구분자 구별

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    let separator = [];
+
     try {
       let string = await Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n"
       );
+      separator = string.match(/\/\/(.*?)\\n(.*)/);
     } catch (error) {}
+
+    // Console.print(separator);
   }
 }
 
