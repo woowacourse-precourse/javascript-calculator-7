89714bdbeec6a9b87be3a9e2257a113424c4bcc6
thumbthing <ilscc0902@gmail.com>
2024-10-18 17:07:05 +0900

    feat: 구현 기능 App에 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import getAddInput from "./UI/getAddInput.js";
+import separator from "./feature/converter/stringConverter.js";
+import getNumberArray from "./feature/converter/arrayConverter.js";
+import errorHandler from "./feature/error/errorHandler.js";
+import inputAdder from "./feature/caculate/inputAdder.js";
+import resultUI from "./UI/resultUI.js";
+
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      let inputText = await getAddInput();
+      let calulateList = separator(inputText);
+      let numberList = getNumberArray(calulateList);
+      let totalSum = inputAdder(numberList);
+      resultUI(totalSum);
+    } catch (error) {
+      errorHandler(error);
+    }
+  }
 }
 
 export default App;
