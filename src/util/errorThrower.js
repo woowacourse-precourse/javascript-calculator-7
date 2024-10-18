70769e271830b88e59352c755bf907fc5ff48f2b
장정안 <wjddks96@naver.com>
2024-10-18 17:47:21 +0900

    refactor: 에러를 throw하는 함수 분리
    기존 validation에 있던 throwError 함수는 관심사에 맞지 않다고 판단하여 따로 분리하였습니다.

---
+++ src/util/errorThrower.js
@@ -0,0 +1,8 @@
+import { ERROR_MESSAGE } from '../constants/errorMessages.js';
+
+/**@param {string} message */
+const throwError = (message) => {
+  throw new Error(`${ERROR_MESSAGE.PREFIX} ${message}`);
+};
+
+export default throwError;
