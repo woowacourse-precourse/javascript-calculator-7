672a3da4c4f034b1f76419a69993db35f8e7f617
leechangsuuu <sunnckd@naver.com>
2024-10-18 20:14:14 +0900

    feat: 사용자로부터 입력받고 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    try{
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하여 주세요.\n");
+      const result = `${input}`
+      MissionUtils.Console.print(`결과: ${result}`);
+    }catch(error){
+      console.error("ERROR");
+    }
+  }
 }
 
 export default App;
