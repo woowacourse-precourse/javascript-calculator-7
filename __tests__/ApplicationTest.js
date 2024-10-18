fbeaa8d8179c038c07edba071d4074cc3186ca40
Yusi <yusi4781@naver.com>
2024-10-19 04:30:40 +0900

    test : add test code

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,8 +32,83 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
+  test("다수의 커스텀 구분자 정의", async () => {
+    const inputs = ["//;\\n//.\\n1;2.3,4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    })
+  })
+
+  test("- 커스텀 구분자 사용", async () => {
+    const inputs = ["//-\\n1-2-3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    })
+  })
+
+  test("음수 입력", async () => {
+    const inputs = ["1,-2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("피연산자 사이에 구분자를 두 개 입력", async () => {
+    const inputs = ["1::2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("하나의 커스텀 구분자를 정의할 때 두 개의 문자를 입력", async () => {
+    const inputs = ["//!@\\n1!2"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 문자를 정의할 때 특수문자가 아닌 다른 문자를 입력", async () => {
+    const inputs = ["//ㅁ\\n1ㅁ2"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 정의를 시작했으나 \\n을 사용하지 않은 경우", async () => {
+    const inputs = ["//.1.2"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("첫 문자가 숫자 혹은 /(슬래쉬)가 아닌 경우", async () => {
+    const inputs = [",1"];
     mockQuestions(inputs);
 
     const app = new App();
