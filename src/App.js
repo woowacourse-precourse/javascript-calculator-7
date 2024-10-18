15ee2e27f599dc405f994caa083475b3720b24a1
SeokHun <tommya98@naver.com>
2024-10-18 01:40:51 +0000

    feat(App): 입력 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+    if (str.trim() === "") {
+      Console.print(0);
+      return;
+    }
+
     Console.print(str);
   }
 }
