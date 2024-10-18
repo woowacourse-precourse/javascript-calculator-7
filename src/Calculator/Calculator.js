aae6a55148dfc588f918897cec854806cf952dbb
haesa <xdhaesa@gmail.com>
2024-10-18 17:46:08 +0900

    feat(): convertArray

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -7,4 +7,9 @@ export default class Calculator {
     const seperators = string.split(/\/\/|\\n/);
     return seperators[1];
   }
+
+  convertArray(string, separator) {
+    const defaultArray = string.split(/,|;/);
+    return defaultArray.flatMap((item) => item.split(separator));
+  }
 }
