8cc7a214c60a6a9103cb1b29af667443c1c84fff
leechangsuuu <sunnckd@naver.com>
2024-10-18 23:03:58 +0900

    feat: 기본 구분자로 입력문 중 숫자 추출,파싱  후 덧셈계산

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-class App {
-  async run() {
+class StringCalculator{ // 계산을 위한 클래스
+  add(inputs){
     try{
-      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하여 주세요.\n");
-      const result = `${input}`
-      MissionUtils.Console.print(`결과: ${result}`);
+      let delimiter = /[,:]/;// 기본 구분자를 쉼표(,) 또는 콜론(:)으로 설정
+                            // let을 사용한 이유는 나중에 커스텀 구분자로 변경하여 사용하기 위함
+      // 입력 문자열을 구분자로 분리한 후, 
+      // 숫자로 변환(입력값을 split으로 구분자로 나누고 map을 돌아 숫자마다 십진수값으로 변환)
+      const numbers = inputs.split(delimiter).map((num)=>{
+        const parsedNum = parseInt(num,10);//십진수
+        return parsedNum;
+      })
+      return numbers.reduce((sum, num)=> sum + num, 0); //배열 numbers의 모든 요소를 더해 합산
+      //reduce(누적값, 현재 요소) => {처리 로직} , 초기값)
     }catch(error){
-      console.error("ERROR");
+      throw new Error("잘못된 입력 형식입니다.");
     }
+  }
+}
+class App { // 입력과 출력하는 클래스
+  constructor(){ // StringCalculator 인스턴스를 생성하여 저장
+    this.calculator = new StringCalculator();
+  }
+  async run() {
+      try{
+        // 사용자로부터 문자열 입력을 비동기적으로 받음
+        const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하여 주세요.\n");
+        // 입력된 문자열을 계산하여 결과를 얻음
+        const result = this.calculator.add(input);
+        MissionUtils.Console.print(`결과: ${result}\n`);
+      }catch(error){
+        console.error(`ERROR - ${error.message}`);//에러에 맞게 메시지 출력
+        
+      }
   }
 }
 export default App;
