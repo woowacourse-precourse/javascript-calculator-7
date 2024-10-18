1f6560e2ea7ae9f24133776469a808512d63094a
SNXWXH <snxwxh@gmail.com>
2024-10-18 18:12:21 +0900

    feat(app): add user input handling feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const INPUT_MESSAGE = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n',
+    );
+    const TRIM_INPUT_MESSAGE = INPUT_MESSAGE.trim();
+
+    if (TRIM_INPUT_MESSAGE === '' || typeof TRIM_INPUT_MESSAGE !== 'string') {
+      throw new Error('[ERROR] 입력값이 올바르지 않습니다.');
+    }
+  }
 }
 
 export default App;
