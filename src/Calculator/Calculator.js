cf498e2ef8351b7bd9e2076fd1ba466c65187b4d
haesa <xdhaesa@gmail.com>
2024-10-18 17:45:47 +0900

    feat(): extractSeparator

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -2,4 +2,9 @@ export default class Calculator {
   hasCustomPrefixSeparator(string) {
     return /^\/\/.+\\n.*/.test(string);
   }
+
+  extractSeparator(string) {
+    const seperators = string.split(/\/\/|\\n/);
+    return seperators[1];
+  }
 }
