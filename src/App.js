8e4e3d7e9dc30d3b2753115e2a2904ffacad14cf
Yusi <yusi4781@naver.com>
2024-10-19 03:33:59 +0900

    feat : extract operands

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App {
       formula = this.extractionCustomDelimiter(input, delimiters);
     }
     //수식 유효성 검사
-    this.validateFormula(formula, delimiters)
+    if (this.validateFormula(formula, delimiters)) {
+
+    }
   }
 
   extractionCustomDelimiter(input, delimiters) {
@@ -38,20 +40,32 @@ class App {
   };
 
   validateFormula(formula, delimiters) {
+    const delimiterRegex = new RegExp(`[${delimiters.join('')}]`);
+    //-를 구분자로 지정하지 않았을 때 음수 처리
     if (delimiters.join('').indexOf('-') === -1 && /-(\d+)/.test(formula)) {
       throw new Error("[ERROR] 음수는 허용되지 않습니다.");
-    }
-
-    // 숫자 유효성 검사 (숫자와 구분자만 존재하는지 확인)
-    const validFormulaRegex = new RegExp(`^([0-9]+|[${delimiters.join('')}])+$`);
-    if (!validFormulaRegex.test(formula)) {
-      throw new Error("[ERROR] 유효하지 않은 형식입니다.");
     }
 
     // 연속된 구분자 방지
     if (/([,;:!@#$%^&*()_\-+=\[\]{};:'",.<>?/\\|~`]){2,}/.test(formula)) {
       throw new Error("[ERROR] 구분자가 연속으로 사용되었습니다.");
     }
+
+    //구분자와 숫자 이외의 문자가 들어갔는지 검사
+    for (const char of formula) {
+      if (!char.match(/\d/) && !delimiterRegex.test(char)) {
+        throw new Error("[ERROR] 유효하지 않은 문자가 들어갔습니다.");
+      }
+    }
+
+    return true;
+  }
+
+  calculateSum(formula, delimiters) {
+    //구분자를 기준으로 split
+    const delimitersRegex = new RegExp(`[${delimiters.join('')}]`);
+    const numbers = formula.split(delimitersRegex);
+    return numbers;
   }
 };
 
