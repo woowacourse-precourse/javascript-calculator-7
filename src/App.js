690d1dfc6cff9bde8789de20a703f76a5c2aa0ec
mynameSumin <msm4167@naver.com>
2024-10-18 14:17:54 +0900

    feat: 잘못된 문자열 입력에 따른 에러 처리
    다음은 처리된 에러의 목록이다.
    - 숫자, 구분자가 아닌 문자를 사용함
    - 문자열을 커스텀 구분자로 사용함
    - 숫자를 커스텀 구분자로 사용함

--- src/App.js
+++ src/App.js
@@ -6,35 +6,59 @@ class App {
     let number = [];
     let sum = 0;
 
+    function checkIsCorrectChar(data) {
+      for(let i = 0; i < data.length; i++){
+        if(!parseInt(data[i], 10) && !separator.includes(data[i])){
+          throw new Error('[ERROR] 숫자 및 구분자가 아닌 값을 사용하고 있습니다');
+        }
+      }
+      return true;
+    };
+
     function getCustomSeparator(data) {
-      if (!data.startsWith('//')) return;
+
+      // \n이 커스텀 구분자 설정이 아닌 용도로 사용된 경우
+      if (!data.startsWith('//')){
+        throw new Error('[ERROR] 숫자 및 구분자가 아닌 값을 사용하고 있습니다');
+      };
+
+      // 커스텀 문자가 한 글자가 아닌 경우
+      if(data.length != 3){
+        throw new Error('[ERROR] 잘못된 커스텀 구분자입니다. 커스텀 구분자는 한 개의 문자만 설정 가능합니다.');
+      }
+
+      // 숫자를 커스텀 문자로 사용하려는 경우
+      if(parseInt(data[2], 10)){
+        throw new Error('[ERROR] 숫자는 커스텀 구분자로 사용할 수 없습니다.');
+      }
+
       separator += data[2]; 
-    }
+      return true;
+    };
 
     function getNumberFromData (data) {
       const regex = new RegExp(`[${separator}]`);
       number = data.split(regex);
 
       for(let i = 0; i < number.length; i++){
-        sum += parseInt(number[i], 10);
+        if(checkIsCorrectChar(number[i])){
+          sum += parseInt(number[i], 10);
+        }
       }
-    }
+    };
 
-    Console.print('덧셈할 문자열을 입력해 주세요.');
-    const data = await Console.readLineAsync('');
+    const data = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
     if (data.includes('\\n')) {
       const partsOfData = data.split('\\n');
-      getCustomSeparator(partsOfData[0]);
+      getCustomSeparator(partsOfData[0]);     
       getNumberFromData(partsOfData[1]);
     }
     else {
       getNumberFromData(data);
     }
 
-    Console.print(`결과 : ${sum}`);
-
-    
+      Console.print(`결과 : ${sum}`);
   }
 }
 
