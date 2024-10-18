1a3c772754ed453b90d5d77519982e331cbd0fcc
Yusi <yusi4781@naver.com>
2024-10-19 04:28:19 +0900

    feat : add exception handling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const delimiters = [',', ':'];
+    const firstCharRegex = /^[\d\/]/
     //사용자의 입력값 받기
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    //사용자의 입력값이 "/" 혹은 숫자로 시작하는지 검사
+    if (!firstCharRegex.test(input)) {
+      throw new Error('[ERROR] 첫 문자는 숫자 혹은 커스텀 정의 구문이어야 합니다.');
+    }
     let formula = input;
     //사용자의 입력값이 커스텀 구분자를 정의하며 시작하는지 검사
     if (input.startsWith("//")) {
