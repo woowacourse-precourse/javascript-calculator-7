a0af4f17f468924e5e6acb1a2238e9dcdbb59439
parkyugyeong <qkrb8019@gmai.com>
2024-10-18 22:52:03 +0900

    feat: 1. 빈 문자열일 경우 0을 반환 기능구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run(input = "") {
+    try {
+      const result = this.calculate(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  async readInput() {
+    return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요:\n");
+  }
+
+  calculate(input) {
+    // 1번 기능: 빈 문자열일 경우 0을 반환
+    if (input === "") {
+      return 0;
+    }
+  }
 }
 
 export default App;
