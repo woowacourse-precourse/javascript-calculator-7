ed95a34c5c8338beeadeb2598942061a76fec5d4
L2HYUNN <donghyuni98@gmail.com>
2024-10-18 23:14:12 +0900

    fix(validator): 허가되지 않은 구분자를 사용했을때 에러 메시지가 제대로 출력되지 않던 문제 해결

--- src/Validator.js
+++ src/Validator.js
@@ -7,7 +7,7 @@ class Validator {
 
   static ERROR_MESSAGE = Object.freeze({
     HAS_NOT_ALLOWED_DELIMITER:
-      '[ERROR] 쉼표(,), 콜론(:) 또는 "//"와 "\n" 사이에 위치하는 문자만 구분자로 사용할 수 있어요.',
+      '[ERROR] 쉼표(,), 콜론(:) 또는 "//"와 "\\n" 사이에 위치하는 문자만 구분자로 사용할 수 있어요.',
     HAS_NEGATIVE_OR_ZERO_NUMBER: '[ERROR] 음수나 0은 사용할 수 없어요.',
   });
 
