89815b3e2c4b4cafa7a2e43e7afca6c859f43a1a
Jun Hee Jung <wjdtka1325@naver.com>
2024-10-19 05:55:06 +0900

    feat(calculator): 구분자를 통한  계산기 기능 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,20 @@
 export class Calculator {
-  static calculate(input){
-    if(input ==='') return 0
+  static SEPARATOR_REGEX = /[,:]/;
+
+  static calculate(input) {
+    if (input === '') return 0;
+
+    const numbers = this.parseNumbers(input);
+    const sum = this.sumNumbers(numbers)
+
+    return sum
+  }
+
+  static parseNumbers(input) {
+    return input.split(this.SEPARATOR_REGEX).map((num) => Number(num.trim()));
+  }
+
+  static sumNumbers(numbers) {
+    return numbers.reduce((acc, cur) => acc + cur, 0);
   }
 }
