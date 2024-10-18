15ff422af45adb9a4fd321cce78c06660394ee4f
Lee <dltmdgns0508@gmail.com>
2024-10-18 21:35:54 +0900

    feat(App): 에러 메세지 출력 기능 추가
    App.js에 기능 추가
    - try catch 문을 이용해 Error 발생시 에러메세지 출력

--- src/App.js
+++ src/App.js
@@ -2,45 +2,49 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    Console.print('계산할 문자열을 입력해 주세요');
-    const input = await Console.readLineAsync('');
-    if (!input) {
-      throw new Error('[Error] 문자열이 입력되지 않았습니다.');
-    }
-
-    const createSeparatorPattern = customSeparator => {
-      const separator = [',', ':'];
-      if (customSeparator) {
-        separator.push(customSeparator);
+    try {
+      Console.print('계산할 문자열을 입력해 주세요');
+      const input = await Console.readLineAsync('');
+      if (!input) {
+        throw new Error('[Error] 문자열이 입력되지 않았습니다.');
       }
-      return new RegExp(separator.join('|'));
-    };
-    let nums = [];
 
-    if (input.match(/^\/\//)) {
-      const customSeparator = input.match(/^\/\/(.+?)\\n/);
-
-      if (!customSeparator) {
-        if (!input.match(/\\n/)) {
-          throw new Error('[Error] \\n이 입력되지 않았습니다.');
+      const createSeparatorPattern = customSeparator => {
+        const separator = [',', ':'];
+        if (customSeparator) {
+          separator.push(customSeparator);
         }
-        throw new Error('[Error] 커스텀 구분자가 입력되지 않았습니다.');
-      }
+        return new RegExp(separator.join('|'));
+      };
+      let nums = [];
 
-      nums = input
-        .split('\\n')[1]
-        .split(createSeparatorPattern(customSeparator[1]));
-    } else {
-      nums = input.split(createSeparatorPattern());
-    }
-    const result = nums.reduce((hap, num) => {
-      if (isNaN(num)) {
-        throw new Error('[Error] 잘못된 값이 입력되었습니다.');
+      if (input.match(/^\/\//)) {
+        const customSeparator = input.match(/^\/\/(.+?)\\n/);
+
+        if (!customSeparator) {
+          if (!input.match(/\\n/)) {
+            throw new Error('[Error] \\n이 입력되지 않았습니다.');
+          }
+          throw new Error('[Error] 커스텀 구분자가 입력되지 않았습니다.');
+        }
+
+        nums = input
+          .split('\\n')[1]
+          .split(createSeparatorPattern(customSeparator[1]));
+      } else {
+        nums = input.split(createSeparatorPattern());
       }
-      hap += Number(num);
-      return hap;
-    }, 0);
-    Console.print(result);
+      const result = nums.reduce((hap, num) => {
+        if (isNaN(num)) {
+          throw new Error('[Error] 잘못된 값이 입력되었습니다.');
+        }
+        hap += Number(num);
+        return hap;
+      }, 0);
+      Console.print(result);
+    } catch (err) {
+      Console.print(err.message);
+    }
   }
 }
 
