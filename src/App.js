ec4836f0a260d4a6811aa5a800371e6f92a92a61
ahhyun <wndkgus0225@naver.com>
2024-10-18 16:01:26 +0900

    입력 전 문구 출력 및 입력값 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('입력 값을 입력하세요: ');
+    var number = input.split(',').map(Number);
+    var sum = 0;
+    for (let i=0; i<number.length; i++){
+      sum += number[i];
+    }
+    console.log(sum);
+  }
 }
 
 export default App;
