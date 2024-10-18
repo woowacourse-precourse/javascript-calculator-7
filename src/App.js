d14a9fbdd3c5d4d59cf4278ccc148c6abc084826
SWARVY <swarvy0826@naver.com>
2024-10-19 02:43:27 +0900

    refactor: `App` 클래스 파이프라인 로직 수정

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { enter, print } from './utils/console.js';
 class App {
   async run() {
     const input = await enter('덧셈할 문자열을 입력해 주세요.\n');
-
-    this.#calculate(input);
+    this.#tokenizer(input);
   }
 
-  #calculate(input) {
+  #tokenizer(input) {
     const numbers = splitWithCustomDelimiter(input);
-    const sum = numbers.reduce((acc, cur) => acc + cur, BigInt(0));
+    this.#calculate(numbers);
+  }
 
+  #calculate(numbers) {
+    const sum = numbers.reduce((acc, cur) => acc + cur, BigInt(0));
+    this.#result(sum);
+  }
+
+  #result(sum) {
     print(`결과 : ${sum}`);
   }
 }
