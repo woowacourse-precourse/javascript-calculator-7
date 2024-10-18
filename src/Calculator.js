d3beb31363b0734ce9f2344392631df566eb159d
juhui88 <sand8594@naver.com>
2024-10-18 10:58:28 +0900

    feat: splitByDelimiters - 문자열을 구분자로 분리하는 함수 구현 완료

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,14 +3,28 @@ export default class Calculator {
     calculate(input) {
         if (!input) return 0
 
-        const deliters = this.getDelimiters(input)
-        console.log(deliters)
+        const delimiters = this.getDelimiters(input)
+        const tokens = this.splitByDelimiters(input, delimiters)
+
+
     }
 
     getDelimiters(input) {
         const defaulDelimiters = [",", ":"]
-        const customDeliter = input.match(/\/\/(.*?)\\n/)
+        const customDelimiter = input.match(/\/\/(.*?)\\n/)
 
-        return customDeliter ? [...defaulDelimiters, customDeliter[1]] : defaulDelimiters
+        return customDelimiter ? [...defaulDelimiters, customDelimiter[1]] : defaulDelimiters
+    }
+
+    splitByDelimiters(input, delimiters) {
+        input = input.replace(/\/\/.*?\\n/g, ''); // 커스텀부분 문자열 부분 삭제
+
+        let tokens = []
+        for (let item of input) {
+            if (!delimiters.includes(item)) {
+                tokens.push(item)
+            }
+        }
+        return tokens
     }
 }
