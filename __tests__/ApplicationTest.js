a408ca2c056ad4324dc377c168831debc425db1d
luminox93 <memoji1236@gmail.com>
2024-10-19 00:42:02 +0900

    style: prettier 수정
    1. prettier에서 tab 사용 true로 수정
    2. 해당 style ApplicationTest에 적용

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -3,44 +3,44 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import App from '../src/App';
 
 const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = global.jest.fn();
+	MissionUtils.Console.readLineAsync = global.jest.fn();
 
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
+	MissionUtils.Console.readLineAsync.mockImplementation(() => {
+		const input = inputs.shift();
+		return Promise.resolve(input);
+	});
 };
 
 const getLogSpy = () => {
-  const logSpy = global.jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
+	const logSpy = global.jest.spyOn(MissionUtils.Console, 'print');
+	logSpy.mockClear();
+	return logSpy;
 };
 
 describe('문자열 계산기', () => {
-  test('커스텀 구분자 사용', async () => {
-    const inputs = ['//;\\n1'];
-    mockQuestions(inputs);
+	test('커스텀 구분자 사용', async () => {
+		const inputs = ['//;\\n1'];
+		mockQuestions(inputs);
 
-    const logSpy = getLogSpy();
-    const outputs = ['결과 : 1'];
+		const logSpy = getLogSpy();
+		const outputs = ['결과 : 1'];
 
-    const app = new App();
-    await app.run();
+		const app = new App();
+		await app.run();
 
-    outputs.forEach((output) => {
-      global
-        .expect(logSpy)
-        .toHaveBeenCalledWith(global.expect.stringContaining(output));
-    });
-  });
+		outputs.forEach((output) => {
+			global
+				.expect(logSpy)
+				.toHaveBeenCalledWith(global.expect.stringContaining(output));
+		});
+	});
 
-  test('예외 테스트', async () => {
-    const inputs = ['-1,2,3'];
-    mockQuestions(inputs);
+	test('예외 테스트', async () => {
+		const inputs = ['-1,2,3'];
+		mockQuestions(inputs);
 
-    const app = new App();
+		const app = new App();
 
-    await global.expect(app.run()).rejects.toThrow('[ERROR]');
-  });
+		await global.expect(app.run()).rejects.toThrow('[ERROR]');
+	});
 });
