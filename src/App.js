81698816191d21de917122c705ae71943f925e22
seunghyeon77 <jdyjsh77@naver.com>
2024-10-18 17:38:43 +0900

    feat: 문자열 입력받고 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+import { StrSumFunc } from "./StrSumFunc";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync("문자열을 입력하세요: ");
+      const result = StrSumFunc(input);
+      Console.print(`결과: ${result}`);
+    } catch (err) {
+      Console.print(`오류: ${err.message}`);
+    }
+  }
 }
 
 export default App;
