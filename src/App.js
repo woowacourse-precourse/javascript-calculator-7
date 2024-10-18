57c514bdab1d75bf1c66f94d036079f680f1e653
AlgeMoya <kjhobd2@naver.com>
2024-10-18 23:56:15 +0900

    feat: 구분자에 따라 숫자 분리 후 배열 저장 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     let separator = "";
+    let splitedstring = "";
 
     Console.print("덧셈할 문자열을 입력해 주세요.");
-    const input = await Console.readLineAsync("");
+    let input = await Console.readLineAsync("");
 
-    if (isNaN(input) && input.slice(0, 2) !== "//") {
+    if (input.length !== 0 && isNaN(input[0]) && input.slice(0, 2) !== "//") {
       throw new Error("[ERROR] 잘못된 입력입니다.");
     }
 
     if (input.slice(0, 2) === "//") {
       separator = input.slice(2, 3);
+      input = input.slice(5);
       Console.print("커스텀 구분자: " + separator);
+      Console.print("문자열: " + splitedstring);
     }
+
+    // 쉼표(,)와 콜론(:)은 기본적으로 포함하고, 추가로 사용자 입력을 포함한 정규 표현식 생성
+    const regex = new RegExp(`[${separator},:]+`);
+    const result = input.split(regex);
+
+    Console.print(result);
 
     // //;\n1;2;3
   }
