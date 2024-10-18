3bc341ad99d3aa1cc298401c17cc93584ef4427f
MERx88 <wjang07@gmail.com>
2024-10-18 14:41:22 +0900

    feat(App): add feature to parse input with default delimeter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+
+
   async run() {
+    //get user input
     const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
-    Console.print(userInput)
+    Console.print(`raw input : ${userInput}`)
+    //parse input with default delimiter
+    if(userInput){
+      const parsedInput=userInput.split(/[,:]/)
+      Console.print(parsedInput)
+    }else{
+      Console.print(0)
+    }
   }
 }
 
