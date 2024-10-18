61ef256e36a87d6a642070ef126068379632214c
haesa <xdhaesa@gmail.com>
2024-10-18 17:47:19 +0900

    feat(): complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import Calculator from './Calculator/Calculator.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      `덧셈할 문자열을 입력해 주세요.\n`
+    );
+    const calculator = new Calculator();
+
+    const extraSeperator = calculator.hasCustomPrefixSeparator(input)
+      ? calculator.extractSeparator(input)
+      : undefined;
+
+    const string = input.replace(/^\/\/.+\\n/, '');
+    const array = calculator.convertArray(string, extraSeperator);
+
+    calculator.validate(array);
+
+    const result = array.reduce((acc, num) => acc + Number(num), 0);
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
