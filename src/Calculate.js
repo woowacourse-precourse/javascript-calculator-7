97f472c38276369dcad37fc41dcaec99d44ca14e
zelkovaria <hyein396@gmail.com>
2024-10-19 01:53:38 +0900

    feat: splitByCustom 함수 구현

--- src/Calculate.js
+++ src/Calculate.js
@@ -9,12 +9,32 @@ class Calculate {
 
   async getUserInput() {
     const userInput = await InputView.getUserInput();
-    return this.splitByCommaColon(userInput);
+    return this.splitByCustom(userInput);
   }
 
   // 콤마, 콜론 기준으로 Input을 분리
   splitByCommaColon(userInput) {
     return userInput.split(/[,:]/).map(Number);
+  }
+
+  splitByCustom(userInput) {
+    // //로 시작하는 커스텀 구분자의 경우
+    if (userInput.startsWith("//")) {
+      const splitedInput = userInput.split("\\n");
+      const customSplited = splitedInput[0].substring(2);
+
+      userInput = splitedInput[1];
+      return userInput.split(customSplited).map(Number);
+    }
+
+    // \n로 시작하는 커스텀 구분자의 경우
+    if (userInput.startsWith("\\n")) {
+      const splitedInput = userInput.split("//");
+      const customSplited = splitedInput[0].substring(2);
+
+      userInput = splitedInput[1];
+      return userInput.split(customSplited).map(Number);
+    }
   }
 
   validateUserInput(userInput) {}
