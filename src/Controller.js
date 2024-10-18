7e164521ab78380d0fc3a7e7e28f7a8b336f9630
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-18 21:06:24 +0900

    feat: 입력한 문자열 포맷 형식 확인 후 구분자와 값 문자열로 분리 - 커스텀 구분자로 시작하는 입력 문자열의 형식을 검증하고, 구분자와 값을 분리하는 기능으로 전달 - 커스텀구분자를 받아 기본 구분자 배열에 추가하고, 남은 문자열을 valueString에 저장 - 커스텀 구분자가 없으면 입력 문자열 전체를 valueString에 저장

--- src/Controller.js
+++ src/Controller.js
@@ -1,12 +1,28 @@
+import { StringCalculator } from './StringCalculator.js';
 import { UserInterface } from './UserInterface.js';
+import { ValidateError } from './ValidateError.js';
 
 const user = new UserInterface();
+const calculator = new StringCalculator();
 
 export class Controller {
   constructor() {
-    const IntputString = '';
+    this.inputString;
+    this.delimiter = [',', ':'];
+    this.valueString;
   }
   async getUserInput() {
     this.inputString = await user.getInputString();
+  }
+  separateInput() {
+    if (this.inputString.startsWith('//')) {
+      ValidateError.validateCustomDelimiterFormat(this.inputString);
+      const { customDelimiter, valueString } =
+        calculator.separateCustomDelimiter(this.inputString);
+      this.delimiter.push(customDelimiter);
+      this.valueString = valueString;
+    } else {
+      this.valueString = this.inputString;
+    }
   }
 }
