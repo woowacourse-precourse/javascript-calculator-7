f973912f178902eb6462551e5dad69cecf2b3edd
haesa <xdhaesa@gmail.com>
2024-10-18 17:46:26 +0900

    feat(): validate

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -12,4 +12,18 @@ export default class Calculator {
     const defaultArray = string.split(/,|;/);
     return defaultArray.flatMap((item) => item.split(separator));
   }
+
+  validate(array) {
+    if (array.some((item) => item === '')) {
+      throw Error('[ERROR] 구분자 사이에 숫자가 없습니다.');
+    }
+
+    if (array.some((item) => Number(item) <= 0)) {
+      throw Error('[ERROR] 숫자는 양수만 허용합니다.');
+    }
+
+    if (array.some((item) => Number.isNaN(Number(item)))) {
+      throw Error('[ERROR] 구분자가 아닌 문자가 포함되어 있습니다.');
+    }
+  }
 }
