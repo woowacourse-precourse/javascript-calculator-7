b1e1055e98a66ed20607e15f28f6131e1710f136
DongjaJ <jdh981118@naver.com>
2024-10-18 15:31:25 +0900

    feat: 사용자에게 문자열을 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  #input;
+
   async run() {
-    Console.print("덧셈할 문자열을 입력해 주세요.");
+    await this.readInput();
+  }
+
+  async readInput() {
+    this.#input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
   }
 }
 
