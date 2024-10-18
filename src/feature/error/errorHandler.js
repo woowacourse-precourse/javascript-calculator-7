93b6abe4e2d73a3d40327fb470ee680a4ab8f671
thumbthing <ilscc0902@gmail.com>
2024-10-18 17:06:14 +0900

    feat: 에러 출력 메시지 변경 및 reject 상태가 되도록 변경

--- src/feature/error/errorHandler.js
+++ src/feature/error/errorHandler.js
@@ -4,9 +4,10 @@ function errorHandler(occurredError) {
   const DEFAULT_ERROR_MESSAGE = '[ERROR]';
   
   let errorMessage = occurredError.message;
-  let finalMessage = `${DEFAULT_ERROR_MESSAGE} ${errorMessage}`;
+  let finalMessage = `${errorMessage}`;
   
   Console.print(finalMessage);
+  throw new Error(DEFAULT_ERROR_MESSAGE)
 }
 
 export default errorHandler;
