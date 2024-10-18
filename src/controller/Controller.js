b5eb09dcfe97723ad533aa00e7fd70434a173c10
sooyeoniya <tndus1306403@naver.com>
2024-10-18 19:22:26 +0900

    feat: 구분자 추출 및 덧셈 계산 기능 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -5,24 +5,16 @@ import Validation from "../domain/StringValidation.js";
 import { ERROR_CODES, VALID_STRING_CODE } from "../constants/constants.js";
 
 class Controller {
-  constructor() {
-    // this.calculator = new Calculator();
-  }
-
   async start() {
-    // 사용자 입력
     const inputValue = await InputView.readStringInput();
-    
-    // 예외 처리 검사
     const code = Validation.validate(inputValue);
 
     if (code === ERROR_CODES.EMPTY_STRING) OutputView.printResultValue(0);
     else if (code !== VALID_STRING_CODE) OutputView.printErrorMessage(code);
     else {
-      // const resultValue = this.calculator.getCalculatorResult(inputValue);
-
-      // 화면 출력
-      // OutputView.printResultValue(resultValue);
+      const calculator = new Calculator(inputValue);
+      const resultValue = calculator.getCalculatorResult();
+      OutputView.printResultValue(resultValue);
     }
   }
 }
--- src/domain/StringCalculator.js
+++ src/domain/StringCalculator.js
@@ -1,15 +1,38 @@
-
-
 class StringCalculator {
-  // 컨트롤러에 의해 1번이 실행되면 마지막 4번 함수에 의해 반환될 때까지 내부에서만 작업하면 됨
+  #delimiters;
+  #numbers;
 
-  // 1. 커스텀 구분자로 시작하는지 아닌지 판단하여 커스텀 구분자가 있는 경우 2번, 아닌 경우 3번 호출
+  constructor(inputValue) {
+    this.inputValue = inputValue;
+    this.#delimiters = [',', ':'];
+    this.#numbers = [];
+  }
 
-  // 2. 커스텀 구분자 분리하여 구분자 추출하는 함수
+  getCalculatorResult() {
+    this.#extractCustomDelimiter();
+    this.#extractNumbers();
+    return this.#sumNumbers();
+  }
 
-  // 3. 기본 구분자를 포함한 커스텀 구분자를 통해 숫자 추출하는 함수
+  #extractCustomDelimiter() {
+    if (this.inputValue.startsWith('//')) {
+      const customDelimiter = this.inputValue.substring(2, this.inputValue.indexOf('\\n'));
+      this.#delimiters.push(customDelimiter);
+      this.inputValue = this.inputValue.substring(this.inputValue.indexOf('\\n') + 2);
+      // console.log(this.inputValue);
+    }
+  }
 
-  // 4. 추출되어 저장된 각 숫자들을 모두 더하여 반환하는 함수 (큰 수도 고려)
+  #extractNumbers() {
+    const regex = new RegExp(`[${this.#delimiters.join('')}]`, 'g');
+    // console.log(regex);
+    this.#numbers = this.inputValue.split(regex).map(Number);
+    // console.log(this.#numbers);
+  }
+
+  #sumNumbers() {
+    return this.#numbers.reduce((acc, cur) => acc + cur, 0);
+  }
 }
 
 export default StringCalculator;
