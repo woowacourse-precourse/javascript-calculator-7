6b761d4913de6c94c7c4ab9752316c98121ff6a3
misung.dev <misung.dev@gmail.com>
2024-10-18 22:57:59 +0900

    refactor: 첫 번째 요구사항 코드 리팩터링
    - 입력 받은 문자열의 split 처리 모듈화
    - 입력 받은 문자열의 split 처리 방식 변경

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   static DEFAULT_SEPARATOR_LIST = [",", ":"];
 
+  static splitInputString(
+    inputString,
+    separatorList = App.DEFAULT_SEPARATOR_LIST
+  ) {
+    return inputString.split(new RegExp(`[${separatorList.join("")}]`));
+  }
+
   static sumNumericStringList(numericStringList) {
     return numericStringList.reduce(
       (accumulator, currentValue) => accumulator + Number(currentValue),
@@ -14,20 +21,9 @@ class App {
     const inputString = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    let splitResultList = [inputString];
+    const splitResultList = App.splitInputString(inputString);
 
-    for (const separator of App.DEFAULT_SEPARATOR_LIST) {
-      let tempSplitResultList = [];
-
-      for (const splitResult of splitResultList) {
-        tempSplitResultList = [
-          ...tempSplitResultList,
-          ...splitResult.split(separator),
-        ];
-      }
-
-      splitResultList = tempSplitResultList;
-    }
+    console.log(App.sumNumericStringList(splitResultList));
 
     return App.sumNumericStringList(splitResultList);
   }
