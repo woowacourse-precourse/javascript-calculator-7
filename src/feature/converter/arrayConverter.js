32c8fc12b1ab5684a7449fb57f25491078266b5c
thumbthing <ilscc0902@gmail.com>
2024-10-18 16:11:36 +0900

    feat: 함수명 변경, isNaN 판별 후 에러 처리 기능 추가

--- src/feature/converter/arrayConverter.js
+++ src/feature/converter/arrayConverter.js
@@ -1,5 +1,17 @@
-function getNumberString(stringArray) {
-  let result = stringArray.map((calculateNumber) => Number(calculateNumber));
+function getNumberArray(stringArray) {
+  let result = stringArray.map((calculateNumber) => {
+    let singleValue = Number(calculateNumber);
+
+    if(Number.isNaN(singleValue)) {
+      const ERROR_MESSAGE = '입력하신 값 중에 숫자가 아닌 값이 존재합니다.';
+      const ERROR = new Error(ERROR_MESSAGE);
+      throw ERROR;
+    }
+
+    return singleValue;
+  });
 
   return result;
-}
+};
+
+export default getNumberArray;
