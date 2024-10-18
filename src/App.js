37d1dff27e1372242d88f03a02436d3690efc735
ChanWoo Yun <yunchanwo@naver.com>
2024-10-18 23:27:28 +0900

    코드 제출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+      const result = this.calculate(input);
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
+  }
+
+  calculate(input) {
+    if (!input) return 0;
+
+    const customDelimiterMatch = input.match(/^\/\/(.+)\n/);
+    let numbersString = input;
+    let delimiter = /,|:/;
+
+    if (customDelimiterMatch) {
+      delimiter = customDelimiterMatch[1];
+      numbersString = input.split("\n")[1];
+    }
+
+    const numbers = numbersString.split(new RegExp(delimiter)).map(Number);
+
+    this.validateNumbers(numbers);
+
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  validateNumbers(numbers) {
+    numbers.forEach((num) => {
+      if (num < 0) {
+        throw new Error("[ERROR] 음수는 허용되지 않습니다.");
+      }
+    });
+  }
 }
 
 export default App;
