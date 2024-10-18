42c158a25f2046e30d44e2e05375c271cbebd8b6
dev-dino22 <wlsskdud02@naver.com>
2024-10-19 00:55:50 +0900

    feat(parseInput.js): 기본 및 커스텀 구분자를 사용하는 문자열 파싱 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
+import parseInput from './parseInput.js';
 
 class App {
   async run() {
     Console.print('문자열 덧셈 계산기가 실행되었습니다.');
 
     try {
-      const inputNum = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n> ');
-      if (inputNum.length <= 0 || inputNum == null) {
+      const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n> ');
+      if (inputStr.length <= 0 || inputStr == null) {
         throw new Error('[ERROR] 입력값이 비었습니다.');
       }
 
-      Console.print(inputNum);
+      const test = await parseInput(inputStr);
+
+      Console.print(test);
 
     } catch (error) {
       Console.print(error.message);
---
+++ src/parseInput.js
@@ -0,0 +1,42 @@
+const parseInput = (inputStr) => {
+    try {
+        // 기본 구분자 설정 및 숫자부분을 반환할 변수에 일단 입력값 정의
+        let defaultDlm = [',', ':'];
+        let numbersPart = inputStr;
+
+        // 커스텀 구분자가 있는지 체크
+        if (inputStr.startsWith('//') && inputStr.includes('\\n')) {
+            const firstDlm = inputStr.indexOf('//');
+            const lastDlm = inputStr.indexOf('\\n');
+            const customDlm = inputStr.substring(firstDlm + 2, lastDlm);
+
+            defaultDlm.push(customDlm);
+            numbersPart = inputStr.substring(lastDlm + 2);
+        }
+
+        // 여러 구분자를 포함한 정규표현식 생성 (커스텀 구분자 포함)
+        const finalDlm = new RegExp(`[${defaultDlm.join('')}]`, 'g');
+
+        // 구분자에 의해 숫자 문자열을 분리
+        const resultStr = numbersPart.split(finalDlm);
+
+        // 각각 분리된 문자열의 타입을 숫자열로 변환
+        const resultNum = resultStr.map(Number);
+
+        // 숫자로 변환되지 않은 배열값이 있는지 검사
+        if (!resultNum.every(x => !isNaN(x))) {
+            throw new Error('[ERROR] 숫자열로 변환되지 않는 입력이 포함됐습니다.');
+        }
+
+        // 음수가 있는지 검사
+        if (resultNum.every(x => x < 0)) {
+            throw new Error('[ERROR] 음수가 포함되어있습니다. 양수를 입력해주세요.');
+        }
+
+        return resultNum;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+export default parseInput;
