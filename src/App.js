efb3135d6d3f8db83bad6cefe186c8d3184c0b68
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-10-18 21:48:08 +0900

    refactor: 문자열 분류 로직 변경
    classifyCharacters에서 findAndRemoveCustomDelimiters,splitAndAddNumbers로 변경
    기존의 classifyCharacters는 여러자리의 숫자를 처리하지 못했다. 커스텀 구분자를 분류하는 로직을 분리해 구현했다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const CUSTOM_DELIMITER_PATTERN = /^\/\/(.)\\n/;
+
 class App {
   async run() {
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
 
     let numbers = [];
     let delimiters = [",", ":"];
@@ -11,49 +15,64 @@ class App {
       Console.print("결과 : 0");
       return;
     }
-    this.classifyCharacters(input, numbers, delimiters);
+
+    const modifiedInput = this.findAndRemoveCustomDelimiters(input, delimiters);
+
+    this.splitAndAddNumbers(modifiedInput, delimiters, numbers);
 
     const sum = this.calculator(numbers);
 
     Console.print(`결과 : ${sum}`);
   }
 
-  classifyCharacters(input, numbers, delimiters) {
-    const chars = input.split("");
-
-    for (let i = 0; i < chars.length; i++) {
-      if (this.isNaturalNumber(Number(chars[i]))) {
-        numbers.push(Number(chars[i]));
-      } else if (delimiters.includes(chars[i])) {
-        continue;
-      } else if (chars[i] === "/") {
-        this.extrackDelimiter(chars.slice(i, i + 5), delimiters);
-        i += 4;
-      } else {
-        throw new Error("[ERROR]: 유효하지 않은 문자열 입력");
-      }
-    }
-  }
-
   isNaturalNumber(value) {
     return value > 0 && Number.isInteger(value);
-  }
-
-  extrackDelimiter(slicedchars, delimiters) {
-    console.log(slicedchars);
-    if (
-      slicedchars[0] == "/" &&
-      slicedchars[1] == "/" &&
-      slicedchars[3] == "\\" &&
-      slicedchars[4] == "n"
-    ) {
-      delimiters.push(slicedchars[2]);
-    }
   }
 
   calculator(numbers) {
     return numbers.reduce((acc, cur) => acc + cur, 0);
   }
-}
 
+  findAndRemoveCustomDelimiters(input, delimiters) {
+    let modifiedInput = input;
+
+    while (true) {
+      let match = modifiedInput.match(CUSTOM_DELIMITER_PATTERN);
+
+      if (!match) {
+        break;
+      }
+
+      const customDelimiter = match[1];
+      delimiters.push(customDelimiter);
+
+      const matchIndex = match.index;
+      const beforePattern = modifiedInput.slice(0, matchIndex);
+      const afterPattern = modifiedInput.slice(matchIndex + 5);
+      modifiedInput = beforePattern + afterPattern;
+    }
+
+    return modifiedInput;
+  }
+
+  splitAndAddNumbers(modifiedInput, delimiters, numbers) {
+    let splitedInput = [modifiedInput];
+
+    delimiters.forEach((delimiter) => {
+        splitedInput = splitedInput.flatMap((part) => part.split(delimiter));
+    });
+
+    splitedInput.forEach((numStr) => {
+        if (numStr === "") {
+            throw new Error("[ERROR]: 구분자는 연속으로 올 수 없습니다.");
+        }
+        const num = Number(numStr);
+        if (this.isNaturalNumber(num)) {
+            numbers.push(num);
+        } else if (numStr !== "") {
+            throw new Error("[ERROR]: 유효하지 않은 문자열 입력입니다.");
+        }
+    });
+  }
+}
 export default App;
