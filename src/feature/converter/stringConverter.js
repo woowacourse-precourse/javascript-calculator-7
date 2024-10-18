0915a31818f797993417956eef19c781ca052566
thumbthing <ilscc0902@gmail.com>
2024-10-18 14:45:46 +0900

    feat: 함수의 선언부 위치 변경, 초기 변수값 null로 변경, 커스텀 구분자 제외한 문자열 반환

--- src/feature/converter/stringConverter.js
+++ src/feature/converter/stringConverter.js
@@ -7,32 +7,37 @@ function defaultSeparator(text) {
   return convertedText;
 };
 
+function getCustomSeparator(customText) {
+  const CUSTOM_TEXT_LENGTH = customText.length;
+  const CUSTOM_TEXT_ACTUAL_RANGE = CUSTOM_TEXT_LENGTH - 2;
+  
+  let resultSeparator = customText.slice(2, CUSTOM_TEXT_ACTUAL_RANGE);
+  return resultSeparator;
+}
+
 function customSeparator(text) {
   // FIXME: 배열을 사용해서 정규식을 생성할지 literal에 작성된 것을 사용할지 정해야함
   const CUSTOM_SEPERATOR = ['//', '\\n'];
   const CUSTOM_REGEX = /\/\/\w+\n/g;
   const IS_NOT_CUSTOM = -1;
-
+  
   const RESULT = new Map([
     ['customSeparatorString', null],
-    ['text', text]
+    ['text', null]
   ])
-
+  
   if(text.search(CUSTOM_REGEX) === IS_NOT_CUSTOM) {
+    RESULT.set('text', text);
     return RESULT;
   } else {
     let customText = text.match(CUSTOM_REGEX);
     let resultCustomSeparator = getCustomSeparator(customText[0]);
+    
+    let calculateString = text.replace(CUSTOM_REGEX, '');
+    
     RESULT.set('customSeparatorString', resultCustomSeparator);
+    RESULT.set('text', calculateString);
     
     return RESULT;
   }
-}
-
-function getCustomSeparator(customText) {
-  const CUSTOM_TEXT_LENGTH = customText.length;
-  const CUSTOM_TEXT_ACTUAL_RANGE = CUSTOM_TEXT_LENGTH - 2;
-
-  let resultSeparator = customText.slice(2, CUSTOM_TEXT_ACTUAL_RANGE);
-  return resultSeparator;
 }
