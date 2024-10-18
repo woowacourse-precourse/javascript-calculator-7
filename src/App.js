2c8a5b67dcd4d0460175fca745384dcd3a9f6489
Parksangwoo <psu8430@gmail.com>
2024-10-18 19:18:57 +0900

    feat(user_input) : Added the ability to parse a string to return a numeric array
    - Implementing parsing for strings with commas(,) or colon(:) as separator
    - Implementing a custom separator-specified string parsing feature
    - Type parsed character arrays into integer arrays

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const ORIGIN_REGEX = /,|:/;
+const CUSTOM_REGEX = /\/\/(.*?)\\n.*/;
+const CUSTOM_DIVIDER_REGEX = /\/\/(.*?)\\n/;
+
 class App {
+  #get_number_list(user_string) {
+    let num_list;
+    if (CUSTOM_REGEX.test(user_string)) {
+      const divider = user_string.match(CUSTOM_DIVIDER_REGEX)[1];
+      const number_area = user_string.replace(CUSTOM_DIVIDER_REGEX, "");
+      num_list = this.#get_numbers_from_string(number_area, divider);
+    } else if (ORIGIN_REGEX.test(user_string)) {
+      num_list = this.#get_numbers_from_string(user_string, ORIGIN_REGEX);
+    } else {
+      throw new Error(`[ERROR] 잘못된 값이 입력되었습니다.`);
+    }
+
+    return num_list;
+  }
+
+  #get_numbers_from_string(string, divider = ORIGIN_REGEX) {
+    const divided_numbers = string.split(divider);
+    const formatted_numbers = divided_numbers.map((num) => parseInt(num));
+
+    return formatted_numbers;
+  }
+
   async run() {
-    const user_string = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
+    try {
+      const user_string = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      const num_list = this.#get_number_list(user_string);
+    } catch (error) {
+      return error;
+    }
   }
 }
 
