2a843f55bbd49b3a4df1965fec67ef83252e09bd
Yurin Wang <king_1994@naver.com>
2024-10-18 16:14:07 +0900

    feat(delimiterHandler): 커스텀 구분자 처리 기능 구현 - 커스텀 구분자를 기준으로 문자열을 분리한다. - 커스텀 구분자는 문자열 앞에 위치해야 한다. - //와 n 사이에 위치하는 문자를 커스텀 구분자로 사용한다.

--- src/delimiterHandler.js
+++ src/delimiterHandler.js
@@ -1,6 +1,28 @@
-export function parseDefaultDelimiters(userInput) {
+function parseDefaultDelimiters(userInput) {
   const defaultDelimiterPattern = /[,:]/g;
   const delimiterSeparatedArray = userInput.split(defaultDelimiterPattern);
 
   return delimiterSeparatedArray;
+}
+
+function parseCustomDelimiters(userInput) {
+  const customDelimitersPattern = /^\/\/(.*?)\\n/;
+  const match = userInput.match(customDelimitersPattern);
+
+  if (!match) {
+    throw new Error('[ERROR] 커스텀 구분자는 맨 앞에 위치해야 합니다.');
+  }
+
+  const customDelimiter = match[1];
+  const delimiterSeparatedArray = userInput
+    .split('\\n')[1]
+    .split(customDelimiter);
+
+  return delimiterSeparatedArray;
+}
+
+export default function parseInput(userInput) {
+  return userInput.startsWith('//')
+    ? parseCustomDelimiters(userInput)
+    : parseDefaultDelimiters(userInput);
 }
