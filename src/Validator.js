22d18164c45fd71c5813a292f3a7e70ab3df6228
L2HYUNN <donghyuni98@gmail.com>
2024-10-18 23:24:57 +0900

    refactor(validator): validator에서 Error 부분을 객체로 분리

--- src/Validator.js
+++ src/Validator.js
@@ -1,15 +1,10 @@
 import { isNumericString, isPositiveNumber } from './lib/utils.js';
 import SchemaValidator from './lib/SchemaValidator.js';
+import Error from './Error.js';
 
 class Validator {
   /** @type {SchemaValidator} */
   #validator;
-
-  static ERROR_MESSAGE = Object.freeze({
-    HAS_NOT_ALLOWED_DELIMITER:
-      '[ERROR] 쉼표(,), 콜론(:) 또는 "//"와 "\\n" 사이에 위치하는 문자만 구분자로 사용할 수 있어요.',
-    HAS_NEGATIVE_OR_ZERO_NUMBER: '[ERROR] 음수나 0은 사용할 수 없어요.',
-  });
 
   constructor(validator) {
     this.#validator = validator;
@@ -42,10 +37,10 @@ class Validator {
     this.#validator
       .validate(values)
       .with(this.#hasAllowedDelimiters, {
-        message: Validator.ERROR_MESSAGE.HAS_NOT_ALLOWED_DELIMITER,
+        message: Error.MESSAGE.HAS_NOT_ALLOWED_DELIMITER,
       })
       .with(this.#hasPositiveNumbers, {
-        message: Validator.ERROR_MESSAGE.HAS_NEGATIVE_OR_ZERO_NUMBER,
+        message: Error.MESSAGE.HAS_NEGATIVE_OR_ZERO_NUMBER,
       });
   }
 }
