0e9e1d7341a86a77b830a777ad03612936ed6595
chaeseungyun <thslr57@naver.com>
2024-10-18 16:35:20 +0900

    feat: 커스텀 구분자 추출하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
-  async run() {}
+  constructor() {
+    this.sum = 0;
+    this.separatorArray = [",", ":"];
+  }
+  async run() {
+    let userInput = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    const customSeparator = userInput.search(customSeparatorReg);
+    if (customSeparator >= 0) {
+      this.separatorArray.push(userInput[2]);
+      userInput = userInput.slice(5);
+    }
+  }
 }
 
 export default App;
