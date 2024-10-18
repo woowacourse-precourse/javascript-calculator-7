1049df7d57fbbdcde6112cce070d52ba8931fc2d
yoouyeon <jyeon.yoon59@gmail.com>
2024-10-19 00:25:13 +0900

    feat(view): 결과 출력에 prefix 추가

--- src/View.js
+++ src/View.js
@@ -3,13 +3,15 @@ import { Console } from "@woowacourse/mission-utils";
 class View {
   static #USER_INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n";
 
+  static #RESULT_PREFIX = "결과 : ";
+
   static async readUserInput() {
     const line = await Console.readLineAsync(View.#USER_INPUT_MESSAGE);
     return line;
   }
 
   static printResult(result) {
-    Console.print(result);
+    Console.print(View.#RESULT_PREFIX + result);
   }
 }
 
