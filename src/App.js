374618704cbb8d66b2a44eca0e9bcba1660a9eae
Yeongi Park <yeongi0111@naver.com>
2024-10-18 16:16:20 +0900

    chore : 주석 제거
    명확한 함수명으로 주석이 필요없어져 코드내 모든 주석 제거

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import { inputText, printSumResult } from './util.js';
 class App {
   async run() {
     const userInput = await inputText();
-    // 사용자 입력값 검증하기
+
     CalculatorService.validateUserInput(userInput);
 
-    // 검증된 사용자 입력값 커스텀 구분자 추출하기
     const { customSeparator, processedUserInput } =
       CalculatorService.parseUserInput(userInput);
 
-    // 합 구하기
     const sumResult = CalculatorService.sumUserInput(
       processedUserInput,
       customSeparator,
     );
+
     printSumResult(sumResult);
   }
 }
--- src/CalculatorService.js
+++ src/CalculatorService.js
@@ -11,13 +11,8 @@ export default class CalculatorService {
   }
 
   static validateUserInput(input) {
-    // 더블슬래시와 숫자로 시작하는지 검증
     Validator.validateStartsWith(input);
-
-    // 커스텀 구분자와 정제된 입력값 추출
     const { customSeparator, processedUserInput } = this.parseUserInput(input);
-
-    // 구분자를 제대로 사용했는지 검증
     Validator.validateUsedSeparator(processedUserInput, customSeparator);
   }
 
@@ -25,12 +20,8 @@ export default class CalculatorService {
     let customSeparator = null;
     let processedUserInput = null;
 
-    // 더블슬래시로 시작하는 경우만 추출
     if (Validator.checkStartWidthDubbleSlash(input)) {
-      // 사용 가능한 커스텀 구분자인지 검증
       Validator.validateCustomSeparator(input);
-
-      // 사용 가능하면 커스텀 구분자 추출하기
       customSeparator = UserInputHandler.getCustomSeparator(input);
       processedUserInput = UserInputHandler.getRemovedCustomSeparator(input);
     }
