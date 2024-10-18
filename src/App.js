aafa8c886fb33e85e4491ed395934679f52ae6df
DodoJBNU <dh723@jbnu.ac.kr>
2024-10-18 23:59:39 +0900

    feat: add function to print sum result

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const [seperatorSet, filteredInput] = this.handleCustomSeperator(input);    
+
+    const [seperatorSet, filteredInput] = this.handleCustomSeperator(input);
+
     const extractedNumbers = this.extractPositiveNumbers(filteredInput, ...seperatorSet);
+
     const sum = this.sumArray(...extractedNumbers);
-    /**
-     * @TODO 덧셈 결과 출력
-     */
+    
+    this.print(sum);
   }
 
   handleCustomSeperator(input){
@@ -55,6 +57,10 @@ class App {
 
   sumArray(...numbers){
     return numbers.reduce((acc, cur) => acc + cur, 0);
+  }
+
+  print(value){
+    MissionUtils.Console.print(`결과 : ${value}`);
   }
 }
 
