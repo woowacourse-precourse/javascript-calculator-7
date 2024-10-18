bb1b836952088ef66ac861116d416d4c2af4d975
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-18 22:26:38 +0900

    test: 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -17,6 +17,51 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
+  test("입력이 없을 경우", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("기본 구분자 사용1", async () => {
+    const inputs = ["1,2"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 3"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("기본 구분자 사용2", async () => {
+    const inputs = ["1,2:3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("커스텀 구분자 사용", async () => {
     const inputs = ["//;\\n1"];
     mockQuestions(inputs);
@@ -34,6 +79,15 @@ describe("문자열 계산기", () => {
 
   test("예외 테스트", async () => {
     const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("잘못된 구분자 사용", async () => {
+    const inputs = ["//$\n3$3:2;"];
     mockQuestions(inputs);
 
     const app = new App();
