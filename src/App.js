915fda695907875e44b5acd630a1802652e446f2
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-18 23:41:20 +0900

    refactor: StringInputProcessor 구분자 처리 개선
    특정 테스트 케이스에서 발생한 커스텀 구분자 처리 문제를 해결하기 위해
    StringInputProcessor 클래스를 리팩토링.
    
    - 구분자를 단순히 `split`으로 하는 것이 아니라 집합으로 구성하여 처리
    - 구분자 추출 및 숫자 파싱 로직 개선
    - 정적 메서드를 인스턴스 메서드로 변경하여 각 인스턴스가 고유한 상태를 유지할 수 있도록 개선

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import StringOutputWriter from './classes/StringOutputWriter.js';
 class App {
   async run() {
     const inputReader = new StringInputReader();
+    const processor = new StringInputProcessor();
+
     const inputString = await inputReader.getInput();
-    const processedInput = StringInputProcessor.processInput(inputString);
+    const processedInput = processor.processInput(inputString);
+
     StringCalculator.validateNumbersArray(processedInput);
     const result = StringCalculator.sum(processedInput);
+
     StringOutputWriter.printResult(result);
   }
 }
--- src/classes/StringInputProcessor.js
+++ src/classes/StringInputProcessor.js
@@ -1,38 +1,75 @@
 class StringInputProcessor {
-  // 입력 처리
-  static processInput(input) {
+  constructor() {
+    this.customDelimiterRegEx = /\/\/(.*?)\r?\n/g;
+    this.defaultDelimiters = new Set([',', ':']);
+    this.customDelimiters = new Set();
+  }
+
+  processInput(input) {
     if (!input) {
-      return 0;
+      return [];
     }
 
-    let delimiter = /[,:]/;
-    let numbersString = input;
+    input = input.replace(/\\n/g, '\n');
 
-    if (input.startsWith('//')) {
-      const parts = input.split('\\n');
-      if (parts.length < 2) {
-        throw new Error('[ERROR] 잘못된 구분자 설정입니다.');
+    // 커스텀 구분자 추출
+    this.customDelimiters.clear();
+    let match;
+    while ((match = this.customDelimiterRegEx.exec(input)) !== null) {
+      this.customDelimiters.add(match[1]);
+    }
+
+    const combinedDelimiters = new Set([
+      ...this.defaultDelimiters,
+      ...this.customDelimiters,
+    ]);
+
+    const numbersStartIndex = input.lastIndexOf('\n') + 1;
+    const numbersString = input.slice(numbersStartIndex);
+
+    return this.extractNumbersFromString(numbersString, combinedDelimiters);
+  }
+
+  extractNumbersFromString(numbersString, allDelimiters) {
+    const numbersArray = [];
+    let currentNumber = '';
+    let currentDelimiter = '';
+
+    for (let char of numbersString) {
+      if (/[0-9]/.test(char)) {
+        this.validateAccumulatedDelimiter(currentDelimiter, allDelimiters);
+        currentDelimiter = '';
+        currentNumber += char;
+      } else {
+        this.addAccumulatedNumber(numbersArray, currentNumber);
+        currentDelimiter += char;
+        currentNumber = '';
       }
-
-      delimiter = this.addDelimiter(delimiter, parts[0][2]);
-      numbersString = parts[1];
     }
 
-    if (!numbersString.trim()) {
-      return 0;
+    if (currentNumber) {
+      numbersArray.push(Number(currentNumber));
     }
 
-    const numbersArray = numbersString.split(delimiter).map(Number);
     return numbersArray;
   }
 
-  // 커스텀 구분자 추가
-  static addDelimiter(existingRegExp, newDelimiter) {
-    const pattern = `${existingRegExp.source.replace(
-      /[\[\]]/g,
-      ''
-    )}${newDelimiter}`;
-    return new RegExp(`[${pattern}]`);
+  validateAccumulatedDelimiter(accumulatingChar, allDelimiters) {
+    if (accumulatingChar) {
+      this.validateDelimiter(accumulatingChar, allDelimiters);
+    }
+  }
+
+  addAccumulatedNumber(numbersArray, accumulatingNumber) {
+    if (accumulatingNumber) {
+      numbersArray.push(Number(accumulatingNumber));
+    }
+  }
+
+  validateDelimiter(char, allDelimiters) {
+    if (!allDelimiters.has(char)) {
+      throw new Error(`[ERROR] 허용되지 않은 구분자: ${char} 가 입력되었습니다. 입력 문자열을 확인해주세요.`);
+    }
   }
 }
 
