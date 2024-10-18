bd65c54beb034fab4c96272e0ed14213095b6974
eunji kang <miki2323@naver.com>
2024-10-18 10:43:58 +0900

    refactor: 개별 함수를 기능 별 class에 통합
    - 기능 별 Class 생성
    - Extractor, Calculator 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,43 +8,92 @@ async function getUserInputData() {
 
 // 숫자 추출
 function extractNumber(userInputData) {
-  const splitReg = /[,|:]|\/\/.\\n/g;
-  const numberArr = userInputData.split(splitReg);
 
-  if (isError(numberArr) == true) {
-    // todo: throw error 
-    return null
+}
+
+class NumberExtractor {
+  userInputData
+  numberData
+  delemeter
+  customDelemter
+
+  NUM_REG = /[^0-9]/
+  CUSTOM_DELI_START = '//'
+  CUSTOM_DELI_END = '\\\\n'
+  CUSTOM_DELI_REG = new RegExp(`^${this.CUSTOM_DELI_START}.${this.CUSTOM_DELI_END}`)
+
+  constructor(userInputData) {
+    this.userInputData = userInputData
+    this.delemeter = [',', ':']
   }
 
-  return numberArr
+  extractNumber() {
+    if (this.findCustomDelemeter() == true) {
+      addDelemeter();
+    }
+
+  }
+
+  findCustomDelemeter() {
+    console.log(this.CUSTOM_DELI_REG)
+
+    const customDeliString = this.userInputData.match(this.CUSTOM_DELI_REG);
+    if (customDeliString == undefined) return false
+
+    this.customDelemter = customDeliString[0].charAt(2)
+    // todo : Error Handling
+    // 1. custom 구분자 설정은 \n 으로 끝나야 합니다.
+    // 2. custom 구분자는 '하나의 문자'로 설정되어야 합니다.
+  }
+
+  addDelemeter() {
+    this.delemeter.push(this.customDelemter)
+  }
+
+  splitInputDate(inputData) {
+    this.splitReg = "/[,|:]|\/\/.\\n/g;"
+
+    const numberArr = userInputData.split(splitReg);
+
+    if (isError(numberArr) == true) {
+      // todo: throw error 
+      Console.print("error")
+      return null
+    }
+    return numberArr
+  }
+
+  checkError() {
+    const findChar = numberArr.find((element) => {
+      return reg.test(element);
+    })
+    return findChar != undefined
+  }
 }
 
-function isError(numberArr) {
-  const reg = /[^0-9]/
+class Calculator {
 
-  const findChar = numberArr.find((element) => {
-    return reg.test(element);
-  })
+  addNumber() {
+    let result = BigInt(0)
 
-  return findChar != undefined
+    for (let i=0; i < numberArr.length; i++) {
+      result += BigInt(numberArr[i])
+    }
+  
+    return result
+  }
+
 }
 
-// 숫자 연산
-function calculator(numberArr) {
-  return numberArr
-}
 
-// 예외 처리
-function errorHandling() {
-
+class ErrorHandler {
 }
 
 class App {
   async run() {
     const userInput = await getUserInputData();
-    const result = calculator(extractNumber(userInput));
-
-    Console.print("결과 : " + result.toString())
+    const numberextractor = new NumberExtractor(userInput);
+    numberextractor.findCustomDelemeter();
   }
 }
 
