dfd0a829ff7d9143c816c0f70adcbe80abcc0dd6
ahhyun <wndkgus0225@naver.com>
2024-10-18 23:57:12 +0900

    커스텀과 일반 구분자를 if문으로 구분

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const input = await Console.readLineAsync('입력 값을 입력하세요: ');
-    var number = input.split(/[,|:]/).map(Number);
-    var sum = 0;
-    for (let i=0; i<number.length; i++){
-      sum += number[i];
-    }
-    console.log(sum);
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+    const result = sumNumbers(input);
+    Console.print(`결과 : ${result}`);
   }
 }
+
+    function sumNumbers(input){
+
+      if (input === "") {
+        return 0;
+    }
+
+    let customPattern = /\/\/(.+)\n/;
+      if(customPattern.test(input)){
+        let match = input.match(customPattern);
+        let customWord = match[1]; 
+        let numberPart = input.split('\n')[1];
+        let numbers =  numberPart.split(new RegExp(customWord)).map(Number)
+        return numbers.reduce((sum, num) => sum + num, 0);
+      } else if(input.includes(",") || input.includes(":")){
+        let numbers = input.split(/[,|:]/).map(Number);
+        return numbers.reduce((sum, num) => sum + num, 0);
+      }
+      return 0;
+  }
 
 export default App;
