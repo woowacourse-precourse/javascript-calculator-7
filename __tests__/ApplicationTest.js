782e8ab0837e8a351ad1ab728f1f90530dbe18a1
DodoJBNU <dh723@jbnu.ac.kr>
2024-10-18 18:50:21 +0900

    test: add decimal number and custom seperator test

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -40,4 +40,58 @@ describe("문자열 계산기", () => {
 
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
+
+  test("양의 소수 테스트", async () => {
+    const inputs = ["1.5,2.5,3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 7"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("음의 소수 예외 테스트", async () => {
+    const inputs = ["-1.5,-2.5,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자 사용, 양의 소수 사용 테스트", async () => {
+    const inputs = ["//;\\n1;10;3,4.5"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 18.5"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자 . 테스트", async () => {
+    const inputs = ["//.\\n1.22.345.4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 372"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
