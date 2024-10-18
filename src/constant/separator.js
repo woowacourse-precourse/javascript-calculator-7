9dd2337f809f4287416a6abeb62e6b9dfc6db6b6
ChanLee_KR <cl700120@gmail.com>
2024-10-18 15:58:02 +0900

    feat: 커스텀 구분자 추출 기능 구현

---
+++ src/constant/separator.js
@@ -0,0 +1,5 @@
+export const DEFAULT_SEPARATORS = [',', ':'];
+
+export const CUSTOM_SEPARATOR_START = '//'
+
+export const CUSTOM_SEPARATOR_END = '\\n'
--- src/model/Calculator.js
+++ src/model/Calculator.js
@@ -1,4 +1,40 @@
+import {
+  DEFAULT_SEPARATORS,
+  CUSTOM_SEPARATOR_START,
+  CUSTOM_SEPARATOR_END,
+} from '../constant/separator.js'
+
 export default class Calculator {
+  /**
+   * @type {Array<string>}
+   */
+  separators = [];
+  value = "";
+
+  /**
+   * @param {string} input 
+   */
+  constructor(input) {
+    this.setSeparatorAndValue(input);
+  }
+
+  /**
+   * @param {string} input
+   */
+  setSeparatorAndValue(input) {
+    const prefixMatch = input.startsWith(CUSTOM_SEPARATOR_START);
+    const splitText = input.split(CUSTOM_SEPARATOR_END);
+    if (prefixMatch && splitText.length === 2) {
+      const customSeparator = splitText[0].replace(CUSTOM_SEPARATOR_START, '');
+      this.separators = [...DEFAULT_SEPARATORS, customSeparator];
+      this.value = splitText[1];
+      return
+    }
+    this.separators = [...DEFAULT_SEPARATORS];
+    this.value = input;
+    return
+  }
+
   /**
    * @param {number} value
    * @returns {boolean}
