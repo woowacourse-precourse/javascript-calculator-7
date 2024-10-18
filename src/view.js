46b86bf59af4e674430bd7827f973e8a2a6fc3c8
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-18 11:32:33 +0900

    :recycle: refactor: 출력 비동기 삭제

--- src/view.js
+++ src/view.js
@@ -5,17 +5,13 @@ class CalculatorView {
     return await Console.readLineAsync("문자열을 입력하세요:\n");
   }
 
-  async outputView() {
-    return await Console.print(`결과: ${RESULT}`);
+  outputView() {
+    Console.print(`결과: ${RESULT}`);
   }
 
   errorView(error) {
     console.error(error.message);
   }
-
-  /*close() {
-    this.rl.close();
-  }*/
 }
 
 export default CalculatorView;
