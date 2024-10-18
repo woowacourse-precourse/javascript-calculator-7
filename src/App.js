354efa04ff302d21974764c5f1e61332e2e523f8
Dongubak <91736442+Dongubak@users.noreply.github.com>
2024-10-18 23:24:27 +0900

    feat(debug): testcase 추가
    debug 가능한 테스트 케이스 추가
    convention에 맞게 변수 이름 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
     this.seps = [',', ':'];
     this.result = 0;
     this.inputs = "";
-    this.DEBUG = false;
+    this.debugFlag = false;
     this.customeFlag = false;
     this.error = false;
   }
@@ -14,11 +14,14 @@ class App {
   }
 
   isCustomSep() {
-    if(this.inputs[0] === '/') {
+    if(
+      this.inputs.slice(0, 2) === "//" && 
+      this.inputs.slice(3, 5) === "\\n") {
       //// TODO //X\n 명확한 순서로 입력받은 경우로 수정할 필요있음
       this.seps.push(this.inputs[2]);
       this.customeFlag = true;
     } else {
+    
       this.customeFlag = false;
     }
   }
@@ -53,21 +56,40 @@ class App {
     await Console.print("결과 : " + sum);
   }
 
-  async debug(inputString) {
+  errorHandling() {
+    throw Error("[ERROR]");
+  }
+
+  debug(inputString, testCount) {
+    Console.print("-------------------------------");
+
+    Console.print(`${testCount + 1}번째 테스트 케이스 : ${inputString}`);
+
     let val = "";
     let sum = 0;
 
-    for(let i = this.customeFlag ? 5 : 0; i < inputString; i++) {
+    this.error = false;
+    this.inputs = inputString;
+    this.customeFlag = false;
+    this.seps = ['.', ','];
+
+    this.isCustomSep();
+
+    if(this.customeFlag) Console.print('custom flag on');
+    else Console.print('custome flag off');
+
+
+    for(let i = this.customeFlag ? 5 : 0; i < inputString.length; i++) {
       //this.inputs[i]
       //   //^\n1^23^4,6_10
       if(!isNaN(Number(inputString[i]))) { /// 숫자인 경우
-        Console.print(i + "번째 시행");
-        Console.print(i + "번째 값 : " + inputString[i]);
+        // Console.print(i + "번째 시행");
+        // Console.print(i + "번째 값 : " + inputString[i]);
         val += inputString[i];
       } else { /// 구분자인 경우
         if(val === "") {
           this.error = true;
-          return;
+          break;
         }
 
         if(this.seps.includes(inputString[i])) {
@@ -75,34 +97,43 @@ class App {
           val = "";
         } else {
           this.error = true;
-          return;
+          break;
         }
       }
-      Console.print("tmp 값 : " + val);
-      Console.print(sum);
+      // Console.print("tmp 값 : " + val);
+      // Console.print(sum);
     }
 
     sum += Number(val);
+    if(this.error) {
+      Console.print(`[Error]발생`);
+    } else {
+      Console.print(`${testCount + 1}번째 테스트 케이스 결과 : ${sum}`);
+    }
 
-    await Console.print(sum);
+
+    Console.print("-------------------------------");
   }
 
   async run() {
-    await this.read();
-    this.isCustomSep();
+    this.debugFlag = true;
 
-    const test_input = ["//^\n1^23^4,6,10", "1,2,3,4,5", "//^\n-1^3,1,2"];
+    const testInput = 
+    ["//^\\n1^23^4,6,10", "1,2,3,4,5", "//^\\n-1^3,1,2",
+      "//&\\n11&2&3,5,7", "1/2/3/4/5", "//(\\n1(2(3(4",
+      "//-\\n1-2-34"
+    ];
 
-    if(this.DEBUG) {
-      test_input.forEach(e => {
-        this.debug(e);
-      })
+    if(this.debugFlag) {
+      testInput.forEach((e, i) => {
+        this.debug(e, i);
+      });
     } else {
+      await this.read();
+      this.isCustomSep();
       this.sol();
 
-      if(this.error) {
-        throw Error("[ERROR] : 잘 못된 문자열을 입력받았습니다");
-      }
+      if(this.error) throw Error("[ERROR] : 잘 못된 문자열을 입력받았습니다");
     }
   }
 }
