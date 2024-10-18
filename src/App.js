0ad1ff75bc016240cae439dfdcde96792bb56086
gumbi <gusn9719@gmail.com>
2024-10-18 23:33:48 +0900

    feat: 커스텀 구분자 이스케이프 처리 함수에 유효성 검사 추가
    - 기존 escapeRegExp() 함수에 커스텀 구분자에 대한 유효성 검사 기능을 추가하고 함수명을 validateRegExp()로 변경했습니다.
    - 커스텀 구분자에 숫자를 사용할 수 없도록 제한하고, 잘못된 입력 시 예외를 발생시키도록 구현하였습니다.
    - 기존 기능과 함께 확장된 기능을 구현하여 커스텀 구분자의 안전한 사용을
      보장합니다.

--- src/App.js
+++ src/App.js
@@ -15,45 +15,42 @@ class App {
     calculate(input) {
         if (!input) return 0;
 
-        let delimiter = '[,:]'; // 기본 구분자를 문자열로 정의 (정규식의 문자열 표현)
+        let delimiter = '[,:]';
 
-        // 커스텀 구분자가 있는지 확인
         if (input.startsWith('//')) {
             const customDelimiterIndex = input.indexOf('\\n');
 
-            // 커스텀 구분자를 닫는 \n 가 없으면 에러 처리
             if (customDelimiterIndex === -1) {
                 throw new Error('[ERROR] 올바른 형식의 커스텀 구분자 생성이 아닙니다.');
             }
 
-            // 커스텀 구분자 추출
             let customDelimiter = input.slice(2, customDelimiterIndex);
 
-            // 커스텀 구분자를 이스케이프 처리하여 정규식에 안전하게 사용
-            customDelimiter = this.escapeRegExp(customDelimiter);
+            // 커스텀 구분자를 유효성 검사 후 이스케이프 처리
+            customDelimiter = this.validateRegExp(customDelimiter);
 
-            // 기본 구분자에 커스텀 구분자 추가
             delimiter = `[,:${customDelimiter}]`;
-            console.log('구분자', delimiter);
 
-            // 커스텀 구분자 추가 후 입력 값
             input = input.slice(customDelimiterIndex + 2);
-            console.log('줄바꿈 이후의 input', input);
         }
 
-        // 정규식 형태로 delimiter 사용
         delimiter = new RegExp(delimiter);
 
         let numbers = input.split(delimiter).map(Number);
 
-        console.log('숫자 배열', numbers);
+        const result = numbers.reduce((acc, number) => acc + number, 0);
 
-        // 숫자들의 합을 계산하여 반환
-        return numbers.reduce((sum, num) => sum + num, 0);
+        return result;
     }
 
-    // 정규식 메타문자를 이스케이프 처리하는 함수
-    escapeRegExp(char) {
+    // 예외를 막을 유효성 검사 함수
+    validateRegExp(char) {
+        // 잘못된 형식의 숫자 입력을 방지
+        if (/\d/.test(char)) {
+            throw new Error('[ERROR] 숫자는 커스텀 구분자로 사용할 수 없습니다.');
+        }
+
+        // 정규식 메타문자를 이스케이프 처리
         return char.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 }
