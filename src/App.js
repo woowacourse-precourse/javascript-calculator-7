c7a47acde33c31cbcf0689663ba31a22cc38c3bd
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-10-18 23:08:21 +0900

    feat: 프로그램 흐름 제어 기능 - 프로그램 시작 시 Controller 인스턴스를 생성하고 사용자 입력을 받아   입력을 구분하여 값 문자열을 추출하고 유효성을 검증한 후   최종 합계를 계산하여 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Controller } from './Controller.js';
+
 class App {
-  async run() {}
+  async run() {
+    const controller = new Controller();
+    controller.getUserInput().then(() => {
+      controller.separateInput();
+      controller.extractValues();
+      controller.validateValues();
+      controller.getTotalSum();
+      controller.showResult();
+    });
+  }
 }
 
 export default App;
