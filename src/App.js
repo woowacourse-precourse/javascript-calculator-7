98116bfef5cebd4fa39bfb66249134baba1b87ad
DodoJBNU <dh723@jbnu.ac.kr>
2024-10-18 23:50:44 +0900

    feat: add function to sum numbers

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ class App {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const [seperatorSet, filteredInput] = this.handleCustomSeperator(input);    
     const extractedNumbers = this.extractPositiveNumbers(filteredInput, ...seperatorSet);
-    
-    /**
-     * @TODO 덧셈 로직 구현
-     */
-
+    const sum = this.sumArray(...extractedNumbers);
     /**
      * @TODO 덧셈 결과 출력
      */
@@ -55,6 +51,10 @@ class App {
     }
 
     return extractedNumbers;
+  }
+
+  sumArray(...numbers){
+    return numbers.reduce((acc, cur) => acc + cur, 0);
   }
 }
 
