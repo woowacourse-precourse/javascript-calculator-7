bb78d2424ea72757bfbe339d0bc1e1624b4a76ca
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-10-19 03:18:06 +0900

    feat : 입력기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+const { Console } = MissionUtils;
+
 class App {
-  async run() {}
+  constructor() {
+    this.answer = "";
+  }
+
+  async run() {
+    await this.input();
+  }
+
+  // "덧셈할 문자열을 입력해 주세요."란 문장의 출력과 함께 문자열을 입력 받습니다.
+  input() {
+    Console.readLine("덧셈할 문자열을 입력해 주세요.\n", (answer) => {
+      this.answer = answer;
+    });
+  }
 }
 
 export default App;
