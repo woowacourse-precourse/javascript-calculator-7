3f6985640a103ca48a8500f1de1625fdd9d04fab
mynameSumin <msm4167@naver.com>
2024-10-18 10:10:53 +0900

    feat: 커스텀 구분자 판별 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    let separator = [',',':',];
+    let separator = ',:';
     let number = [];
     let sum = 0;
+
+    function getCustomSeparator(data) {
+      if (!data.startsWith('//')) return;
+      separator += data[2]; 
+    }
+
 
     Console.print('덧셈할 문자열을 입력해 주세요.');
     const data = await Console.readLineAsync('');
 
+    if (data.includes('\\n')) {
+      const partsOfData = data.split('\\n');
+      getCustomSeparator(partsOfData[0]);
+      
+      
+    }
+    else {
+    }
 
     Console.print(`결과 : ${sum}`);
-    function getCustomSeparator(data) {
-      if(data.startsWith('//')){ 
-        if(data.include('\n')){
-          if(data.substring(3, 5) == "\n"){
-            separator.push(data[2]); 
-          } 
-          else{
-            Console.print('[ERROR] 잘못된 커스텀 구분자입니다. 커스텀 구분자는 한 개의 문자만 설정 가능합니다.');
-          }
-        }
-        else{
-          Console.print('[ERROR] 잘못된 문자열입니다. 문자열에는 구분자, 숫자만 입력 가능합니다.');
-        }
-      }
-    }
   }
 }
 
