8fb7a649e7e74a73d1c0cf12092343a85261fb3f
hwiy01 <vdocom02@naver.com>
2024-10-18 21:50:49 +0900

    feat: 문자열 입력받아 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
+    Console.print(`입력값 : ${input}`);
+  }
 }
 
 export default App;
