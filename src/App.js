2d632bd574b558695ef4d3c5bb683c58ac180dac
jjang-aaa <jadoit473@gmail.com>
2024-10-18 20:39:41 +0900

    feat(App): 받아온 문자열 앞부분에 //와 \n이 있는지 판단하는 조건문 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ class App {
     const inputString = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    if (inputString.startsWith("//")) {
+      if (inputString.search(/\n/)) {
+        console.log("커스텀 구분자가 있다");
+      } else {
+        console.log("커스텀 구분자가 없다");
+      }
+    } else {
+      console.log("커스텀 구분자가 없다");
+    }
   }
 }
 
