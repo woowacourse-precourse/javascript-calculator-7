50a5fdd96da49c32d42cd62f690aaf26d24bfb61
seunghyeon77 <jdyjsh77@naver.com>
2024-10-18 19:17:18 +0900

    feat: ,: 구분자를 추출하는 기능 구현

--- src/StrSumFunc.js
+++ src/StrSumFunc.js
@@ -1,9 +1,10 @@
 // 문자열을 나누는 StrSumFunc 함수를 따로 만들었다. 왜냐하면 관심사를 분리해 가독성과 부담을 줄이기 위해서이다.
 
 export function StrSumFunc(input) {
-  let divisionStr = "/[,:]/";
+  let divisionStr = "/[,:]/"; // ,:을 나타내는 정규식
   let inputValueStr = input;
 
+  // 커스텀 구분자 추출
   if (inputValueStr.startsWith("//")) {
     const CUSTOMDIVISIONVALUE = inputValueStr.split("\n");
     if (CUSTOMDIVISIONVALUE.length < 2) {
@@ -12,6 +13,9 @@ export function StrSumFunc(input) {
     divisionStr = new RegExp(`[${CUSTOMDIVISIONVALUE[0][2]}]`);
     inputValueStr = CUSTOMDIVISIONVALUE[1];
   }
+
+  // ,: 구분자 추출
+  const DIVISIONVALUE = inputValueStr.split(divisionStr);
 
   return;
 }
