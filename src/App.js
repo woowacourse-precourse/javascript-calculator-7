0bca5b2c60ff0548428bd5860d04c3b07b47d21c
silver <ekdldkaa@naver.com>
2024-10-18 10:06:39 +0900

    refactor : rearrange fuctions in run method
    Ordered all methods in run method, which is starting point
    of the program

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ const REGEX2 = /^\/{2}.+\d*(\.\d*)?$/;
 const REGEX4 = /\,|\:/;
 
 class App {
-  getStr = async () => {
+  async run() {
+    // 문자열 입력
     const userStr = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요.\n'
     );
 
-    //빈 문자열인 경우
+    let result;
+
     if (userStr == '') {
-      Console.print(`결과 : 0`);
+      // 빈 문자열인 경우
+      result = 0;
     } else if (REGEX1.test(userStr)) {
       //, 또는 : 구분자 문자열
-      this.add_num(userStr);
+      result = this.add_num(userStr);
     } else if (REGEX2.test(userStr)) {
       // 커스텀 구분자 문자열
       const params = this.extract_delimiter(userStr);
-      this.add_num(params[0], params[1]);
+      result = this.add_num(params[0], params[1]);
     } else {
       //에러
     }
-  };
+
+    //결과 출력
+    Console.print(`결과 : ${result}`);
+  }
 
   //커스텀 구분자 추출
-  extract_delimiter = (str) => {
+  extract_delimiter(str) {
     const delimiter = str.slice(2, str.indexOf('\\n'));
     const extractedStr = str.slice(str.indexOf('\\n') + 2);
 
@@ -41,24 +47,15 @@ class App {
     } else {
       //Err
     }
-  };
+  }
 
   //숫자 더하기
-  add_num = (str, delimiter = REGEX4) => {
+  add_num(str, delimiter = REGEX4) {
     let numArr = [];
+    numArr = str.split(delimiter); //숫자 추출하기
+    numArr = numArr.map((x) => Number(x)); //숫자로 전환
 
-    //숫자 추출하기
-    numArr = str.split(delimiter);
-
-    //숫자로 전환
-    numArr = numArr.map((x) => Number(x));
-
-    //더한 결과 값 반환
-    return numArr.reduce((a, b) => a + b);
-  };
-
-  async run() {
-    this.getStr();
+    return numArr.reduce((a, b) => a + b); //더한 결과 값 반환
   }
 }
 
