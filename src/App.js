5e5d2dcd9e4a37984373077c701868b2411a63a0
rwaeng <rwaeng0421@gmail.com>
2024-10-18 17:26:58 +0900

    feat: 구분자 찾기 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.userInput = "";
+    this.separator = null;
   }
 
   async getUserInput() {
@@ -11,8 +12,21 @@ class App {
     );
   }
 
+  // 구분자 찾기 기능
+  async getSeparator() {
+    if (this.userInput.startsWith("//")) {
+      // 구분자 획득
+      this.separator = this.userInput[2];
+
+      // 입력값에서 커스텀 구분자 관련 문자 제거
+      const indexOfn = this.userInput.indexOf("n");
+      this.userInput = this.userInput.slice(indexOfn + 1);
+    }
+  }
+
   async run() {
     await this.getUserInput();
+    await this.getSeparator();
   }
 }
 
