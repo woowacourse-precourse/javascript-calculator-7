1dcf50703d76aa3bcc005d1081bf20e8924e76a0
goodbyebada <goodbyebada@naver.com>
2024-10-18 09:00:13 +0900

    feat: 기본 구분자(: ,) 기준으로 문자열 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 class App {
   async run() {}
+  splitByComma(input) {
+    return splitBySeperaterChar(":", input);
+  }
+
+  splitBySemiColumn(input) {
+    return splitBySeperaterChar(";", input);
+  }
+
+  splitBySeperaterChar(sepChar, input) {
+    let result;
+    if (!!input) {
+      const numbers = input.split(sepChar);
+      result = this.sum(numbers);
+      Console.print(result);
+      return result;
+    }
+  }
 }
 
 export default App;
