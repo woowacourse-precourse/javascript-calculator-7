9fc7b2373ed77312019e3d5a3f0bace8d1d28b01
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-18 21:07:40 +0900

    feat: 커스텀 구분자 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
     const defaultRegExp = /[:,]/g;
-    const numbers = input.split(defaultRegExp);
-    const result = numbers.map(Number).reduce((acc, cur) => acc + cur, 0);
-    MissionUtils.Console.print("결과 : " + result);
+    const customRegExp = /^\/\/(.*?)\\n/;
+    const customMatch = input.match(customRegExp);
+    let numbers;
+
+    if (customMatch === null) {
+      numbers = input.split(defaultRegExp);
+    } else {
+      numbers = input.slice(customMatch[0].length).split(customMatch[1]);
+    }
+    const sumResult = numbers.map(Number).reduce((acc, cur) => acc + cur, 0);
+    MissionUtils.Console.print("결과 : " + sumResult);
   }
 }
 
