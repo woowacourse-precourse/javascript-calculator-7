c779fe2e5d5d62a7c214c23cb626a44763824851
AlgeMoya <kjhobd2@naver.com>
2024-10-18 23:39:13 +0900

    feat: 사용자로부터의 입력 값 검증 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ class App {
   async run() {
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const input = await Console.readLineAsync("");
+
+    Console.print(input.slice(0, 2));
+    Console.print(isNaN(input));
+
+    if (isNaN(input) && input.slice(0, 2) !== "//") {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
+
+    // if (decision !== "1" && decision !== "2") {
+    //   throw new Error("[ERROR] 잘못된 입력입니다.");
+    // }
+
+    // //;\n1;2;3
+
+    // if (input.slice(0, 1)
   }
 }
 
