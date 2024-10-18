40e533aa428e925d8d86e0ee64562df9942b1729
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-10-19 03:25:59 +0900

    feat : 입력값이 커스텀 구문자인지를 확인하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ const { Console } = MissionUtils;
 
 class App {
   constructor() {
+    // 입력 값을 저장
     this.answer = "";
+
+    // 구분자를 저장하는 배열
+    this.delimiter = [",", ":"];
   }
 
   async run() {
@@ -14,7 +18,28 @@ class App {
   input() {
     Console.readLine("덧셈할 문자열을 입력해 주세요.\n", (answer) => {
       this.answer = answer;
+      this.isCustom(this.answer);
     });
+  }
+
+  // 사용자의 값이 커스텀 구분자인지 확인한다.
+  isCustom(answer) {
+    // 커스텀 구분자의 접두어를 확인하기 위한 변수
+    let customDelimiterPrefix = answer.slice(0, 2);
+    // 커스텀 구분자의 접미사를 확인하기 위한 변수
+    let customDelimiterSuffix = answer.slice(3, 5);
+    //커스텀 구분자를 추출하기 위한 변수
+    let customDelimiter = answer.slice(2, 3);
+
+    if (customDelimiterPrefix === "//" && customDelimiterSuffix === "\\n") {
+      // 커스텀 구분자를 구분자 배열에 저장한다.
+      this.delimiter.push(customDelimiter);
+
+      // 커스텀 구분자를 제거한다.
+      this.answer = this.answer.replaceAll(customDelimiterPrefix, "");
+      this.answer = this.answer.replaceAll(customDelimiterSuffix, "");
+      this.answer = this.answer.replace(customDelimiter, "");
+    }
   }
 }
 
