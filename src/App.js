711b0a925c624864ef78333dde3d968f89bd850a
jennaaaaaaaaa <ggm4954@gmail.com>
2024-10-18 14:59:40 +0900

    feat: 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async run() {}
-}
+// import { Console } from '@woowacourse/mission-utils';
+// class App {
 
+// }
+// export default App;
+
+import { Console } from '@woowacourse/mission-utils';
+class App {
+   async run() {
+      Console.print('덧셈할 문자열을 입력해 주세요:');
+      const input = await Console.readLineAsync(''); // 사용자 입력을 비동기적으로 받기
+      this.calculateSum(input);
+   }
+}
 export default App;
