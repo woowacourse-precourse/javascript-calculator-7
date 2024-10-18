f27aac3dd68a5742c9be2e58884f33281d7c2b5f
Minje Sung <minje813@dgu.ac.kr>
2024-10-19 03:13:58 +0900

    feat(utils): 오류 검사 구체화
    3가지 경우의 오류 검사 후 안내 문구 제공
    
    utils/checkError.js
    - 커스텀 구분자 지정문 오류 검사
    - 미지정 구분자 사용 오류 검사
    - 숫자배열 입력 오류 검사
    data/
    - 오류에 따른 검사 정규표현식 및 안내문구 추가
    README.md
    - 구현할 기능 목록 4번 체크

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,6 @@
-const PRE_SEPERATORS = [",", ":"];
-const SELECTOR_REGEX = /^\/\/(.+)\\n(.*)/;
-const REGEX_META_LETTER = /[\\\^\$\.\|\[\]\(\)\*\+\?\{\}]/;
-
-export {PRE_SEPERATORS, SELECTOR_REGEX, REGEX_META_LETTER}
+export const PRE_SEPERATORS = [",", ":"];
+export const SELECTOR_REGEX = /^\/\/(.+)\\n(.*)/;
+export const REGEX_META_LETTER = /[\\\^\$\.\|\[\]\(\)\*\+\?\{\}]/;
+export const IS_SELECTOR_WRONG = /^[^\d]/
+export const IS_WRONG_SEPERATOR = /^\d+(.\d+)+$/
+export const IS_WRONG_NUMBERS = /^\d+(.+)+$/
--- src/utils/checkError.js
+++ src/utils/checkError.js
@@ -1,14 +1,32 @@
-import { WRONG_INPUT_ERROR } from "../data/errorMessage.js";
+import { IS_SELECTOR_WRONG, IS_WRONG_NUMBERS, IS_WRONG_SEPERATOR } from "../data/constants.js";
+import { WRONG_INPUT_ERROR, WRONG_NUMBERS_ERROR, WRONG_SELECTOR_ERROR, WRONG_SEPERATOR_ERROR } from "../data/errorMessage.js";
 
 // string => Error?
 // 입력받은 값 유효성 검사
 const checkError = (restString, seperatorJoinString) => {
-  // 숫자배열이 잘 입력되었는지 에러체크
   const numbersRegexString = `^\\d+((${seperatorJoinString})\\d+)*$`;
   const numbersRegex = new RegExp(numbersRegexString);
-  if (!numbersRegex.test(restString)) {
-    throw new Error(`[ERROR] ${WRONG_INPUT_ERROR}`);
+
+  // 에러 없음
+  if (numbersRegex.test(restString)) {
+    return;
   }
+
+  // 커스텀 구분자 지정문 에러
+  if (IS_SELECTOR_WRONG.test(restString)) {
+    throw new Error(`[ERROR] ${WRONG_SELECTOR_ERROR}`);
+  }
+  // 지정되지 않은 구분자 사용 에러
+  if (IS_WRONG_SEPERATOR.test(restString)) {
+    throw new Error(`[ERROR] ${WRONG_SEPERATOR_ERROR}`);
+  }
+  // 숫자배열 입력 에러
+  if (IS_WRONG_NUMBERS.test(restString)) {
+    throw new Error(`[ERROR] ${WRONG_NUMBERS_ERROR}`);
+  }
+
+  // 입력 에러
+  throw new Error(`[ERROR] ${WRONG_INPUT_ERROR}`);
 };
 
 export default checkError;
