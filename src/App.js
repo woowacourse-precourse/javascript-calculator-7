db63bf3e4d7d3e410f66799aa8826bffce11fc0f
yusuShin <tlsdbtn0507@naver.com>
2024-10-18 21:33:51 +0900

    feat(app): isValidSeperator 함수 작성 - run 함수에서 입력값 전달 - every와 some함수를 사용하여 커스텀 구분자가 있는지 일반 ,와:구분자만 있는지 체크 - 그 외의 경우에 에러 발생

--- src/App.js
+++ src/App.js
@@ -8,8 +8,33 @@ class App {
 
   async run() {
     const userInput = await Console.readLineAsync('문자열 덧셈을 해보아요:');
-
+    this.isValidSeperator(userInput);
   };
+
+  isValidSeperator(str) {
+    // 아무 구분자가 없을 경우
+    if (
+      this.normalSeperators.every(e => !str.includes(e)) &&
+      this.customSeperators.every(e => !str.includes(e))
+    ) {
+      this.printError('구분자("," , ":" , "//", "\\n")를 넣어주세요');
+      return
+    };
+
+    // "//"와 "\\n"사이에 커스텀 구분자가 있을 경우
+    if (this.customSeperators.every(e => str.includes(e))) {
+      return
+    };
+
+    // ','와 ":" 만 있을 경우 
+    if (this.normalSeperators.some(e => str.includes(e))) {
+      return
+    }
+
+    // 그 외의 경우 예외처리
+    this.printError('알 수 없는 오류가 발생했습니다 앱을 다시 실행해주세요');
+  }
+
 
   printError(msg) {
     Console.print('[ERROR]'+msg);
