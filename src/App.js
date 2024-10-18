2ff8341e5066c4d62187703ea7ba9e98de4f5c6f
hyunyeee <5004ohy@naver.com>
2024-10-18 18:03:02 +0900

    refactor: 객체 생성을 constructor에서 하도록 변경

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import { OutputHandler } from "./handler/OutputHandler.js";
 import { DEFAULT_OPERATOR, END_CUSTOM_OPERATOR } from "./constants/Constants.js";
 
 class App {
-  #inputHandler;
-  #outputHandler;
-  #customOperatorExtractor;
   #isCustomCalculate;
+  #inputHandler;
+  #customOperatorExtractor;
   #calculate;
+  #outputHandler;
 
-  async run() {
+  constructor() {
     this.#inputHandler = new InputHandler();
     this.#customOperatorExtractor = new CustomOperatorExtractor();
+    this.#calculate = new Calculator();
+    this.#outputHandler = new OutputHandler();
+  }
+
+  async run() {
     const input = await this.#inputHandler.getInput();
     const customOperator = this.#customOperatorExtractor.extractCustomStartOperator(input)
     this.#isCustomCalculate = customOperator !== '';
     const operatorList = this.#isCustomCalculate ? [customOperator] : DEFAULT_OPERATOR;
     const expression = this.#isCustomCalculate ? input.slice(input.indexOf(END_CUSTOM_OPERATOR) + END_CUSTOM_OPERATOR.length) : input;
-    this.#calculate = new Calculator();
     const result = this.#calculate.calculate(operatorList, expression);
-    this.#outputHandler = new OutputHandler();
     this.#outputHandler.print(result);
   }
 }
