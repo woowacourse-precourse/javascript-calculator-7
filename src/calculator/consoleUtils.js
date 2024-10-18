1b33b96791f58ac24c841eeab4e37086ac04b785
kch6396 <cjfghks6396@gmail.com>
2024-10-18 17:48:59 +0900

    feat(calculator): 문자열 입력 기능 구현

--- src/calculator/consoleUtils.js
+++ src/calculator/consoleUtils.js
@@ -3,6 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 const consoleUtils = async () => {
   try {
     Console.print("덧셈할 문자열을 입력해 주세요.");
+    await Console.readLineAsync("");
   } catch (error) {}
 };
 
