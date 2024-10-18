851f0870e36440d727cfe210caf3d7b70790e1e0
Yurin Wang <king_1994@naver.com>
2024-10-19 00:42:06 +0900

    refactor(validator): 음수가 입력된 경우 에러 발생 로직 분리

--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,7 @@
+function validateNegativeNumber(number) {
+  if (number < 0) throw new Error('[ERROR] 음수는 포함할 수 없습니다.');
+}
+
 export function validateNumbers(parsedArray) {
   return parsedArray.map((value) => {
     const number = Number(value);
@@ -5,8 +9,7 @@ export function validateNumbers(parsedArray) {
     if (Number.isNaN(number))
       throw new Error('[ERROR] 구분자 이외의 문자열이 포함되어 있습니다.');
 
-    if (number < 0) throw new Error('[ERROR] 음수는 포함할 수 없습니다.');
-
+    validateNegativeNumber(number);
     return number;
   });
 }
