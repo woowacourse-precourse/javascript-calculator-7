1429e1976f5c52135c5a0aa0f5b91409f1ba0303
AlgeMoya <kjhobd2@naver.com>
2024-10-18 21:11:50 +0900

    feat: 시작 문구 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    // console.log("Hello, World!");
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    // Console.readLineAsync();
   }
 }
 
