4b261252089b41640bcd19f2d6084d9e9e975284
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-18 21:14:46 +0900

    feat: 에러 핸들링 추가

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-    const defaultRegExp = /[:,]/g;
-    const customRegExp = /^\/\/(.*?)\\n/;
-    const customMatch = input.match(customRegExp);
-    let numbers;
+    try {
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+      const defaultRegExp = /[:,]/g;
+      const customRegExp = /^\/\/(.*?)\\n/;
+      const customMatch = input.match(customRegExp);
+      let numbers;
 
-    if (customMatch === null) {
-      numbers = input.split(defaultRegExp);
-    } else {
-      numbers = input.slice(customMatch[0].length).split(customMatch[1]);
+      if (customMatch === null) {
+        numbers = input.split(defaultRegExp);
+      } else {
+        numbers = input.slice(customMatch[0].length).split(customMatch[1]);
+      }
+      const sumResult = numbers
+        .map((number) => {
+          const formatNumber = Number(number);
+          if (formatNumber >= 0) {
+            return formatNumber;
+          }
+          throw new Error("유효하지 않은 숫자입니다.");
+        })
+        .reduce((acc, cur) => acc + cur, 0);
+      MissionUtils.Console.print("결과 : " + sumResult);
+    } catch (error) {
+      throw new Error("[ERROR]" + error);
     }
-    const sumResult = numbers.map(Number).reduce((acc, cur) => acc + cur, 0);
-    MissionUtils.Console.print("결과 : " + sumResult);
   }
 }
 
