d819fab61c5591fd5edf684a632b69599d272c5f
ChanLee_KR <cl700120@gmail.com>
2024-10-18 12:52:12 +0900

    feat: 유효성 검사 기능 구현

--- src/model/Calculator.js
+++ src/model/Calculator.js
@@ -1,5 +1,13 @@
 export default class Calculator {
   /**
+   * @param {number} value
+   * @returns {boolean}
+   */
+  validator(value) {
+    return Number.isSafeInteger(value) && value > 0;
+  }
+
+  /**
    * @param {number} a
    * @param {number} b
    * @returns {number}
