a30f537f82c4ad2fd83b2638454466af122c4412
daun-up <awhekdns@kookmin.ac.kr>
2024-10-18 20:19:26 +0900

    refactor: 디버깅 코드 제거

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     let input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+
     try {
       let result = this.caculate(input);
       MissionUtils.Console.print(`결과 : ${result}`);
@@ -23,29 +24,21 @@ class App {
     if (input.startsWith("//")) {
       const match = input.match(/^\/\/(.)\\n(.*)$/);
       if (match) {
-        // delimiter = new RegExp(`[${match[1]}]`);
         const customDelimiter = match[1];  // 커스텀 구분자 추출
-        // delimiter = new RegExp(`[${delimiter.source}${customDelimiter}]`);
         delimiter.push(customDelimiter);
-        // MissionUtils.Console.print(delimiter)
         input = match[2]; // \n 이후의 문자열을 처리
       } else {
-        // MissionUtils.Console.print(delimiter)
         throw new Error("[ERROR] 잘못된 형식의 입력입니다.");
       }
     }
 
     let pattern = new RegExp(`[${delimiter.join('')}]`);
-    MissionUtils.Console.print(pattern)
     let numbers = input.split(pattern);
-    MissionUtils.Console.print(numbers)
-    MissionUtils.Console.print(delimiter)
 
     let sum  = 0
 
     for (let number of numbers) {
       MissionUtils.Console.print(number)
-      // 음수 에러 처리
       if (parseInt(number) < 0) {
         throw new Error("[ERROR] 음수는 입력할 수 없습니다.")
       }
