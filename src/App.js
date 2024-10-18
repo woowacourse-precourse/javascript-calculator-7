e21a257982152670271a4502b13a38dafafb5c6f
yechan <kevin2695@naver.com>
2024-10-18 15:34:33 +0900

    [k1my3ch4n] 문자열 분리 - 기본, custom 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const FIND_CUSTOM_REGEX = /^\/\/(.*?)\\n/;
+
 class App {
+  // 기본 구분자
+  static REGEX = [",", ":"];
+
   async run() {
     const input = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요. \n"
     );
 
-    console.log("결과 : ", input);
+    // custom 구분자가 있는지 확인
+    if (FIND_CUSTOM_REGEX.test(input)) {
+      // 있다면 추출 후 REGEX 에 추가
+      const customRegex = FIND_CUSTOM_REGEX.exec(input);
+
+      App.REGEX.push(customRegex[1]);
+    }
+
+    console.log(App.REGEX.join("    "));
   }
 }
 
