8eeed9e1c2d830b232f287f7f765e34f12e51190
parkyugyeong <qkrb8019@gmai.com>
2024-10-18 23:28:06 +0900

    feat: 3. 커스텀 구분자 지정 기능구현

--- src/App.js
+++ src/App.js
@@ -19,8 +19,26 @@ class App {
     if (input === "") {
       return 0;
     }
-    // 2번 기능: 쉼표(,) 또는 콜론(:) 구분자로 숫자 더하기
-    cnumbers = input.split(/[,|:]/).map(Number);
+
+    let numbers = [];
+
+    // 3번 기능: 커스텀 구분자가 있는지 확인
+    const customSeparatorFormat = input.match(/^\/\/(.)\n(.*)/); // 커스텀 구분자가 있는지 match 메서드로 확인
+
+    if (customSeparatorFormat) {
+      const customDelimiter = customSeparatorFormat[1]; // 커스텀 구분자를 추출
+      if (customDelimiter === "," || customDelimiter === ":") {
+        throw new Error(
+          "[ERROR] 커스텀 구분자는 기본 구분자와 겹칠 수 없습니다."
+        );
+      }
+      numbers = customSeparatorFormat[2].split(customDelimiter).map(Number); // 커스텀 구분자로 숫자를 분리
+    } else {
+      // 2번 기능: 쉼표(,) 또는 콜론(:) 구분자로 숫자 더하기
+      numbers = input.split(/[,|:]/).map(Number);
+    }
+
+    // 합산 결과 반환
     return numbers.reduce((sum, num) => sum + num, 0);
   }
 }
