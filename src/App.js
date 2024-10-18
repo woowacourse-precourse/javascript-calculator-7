1006fff36e21700522821b669cabf3eee68841ac
taeyomi <terry11ty@naver.com>
2024-10-19 03:10:24 +0900

    feat: 분리 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const INPUT = await this.userInput();
+      const USER = this.userSeparator(INPUT);
+      const NUMBERS = this.parseNumbers(USER);
+      this.userOutput(NUMBERS);
+    } catch (error) {}
+  }
 }
 
 export default App;
