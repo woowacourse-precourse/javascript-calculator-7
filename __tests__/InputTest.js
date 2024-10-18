77956791475f66e0ec330349cda0063aa18b8689
hamo-o <yheel369@gmail.com>
2024-10-18 12:32:29 +0900

    test(InputTest): 모킹 함수의 매개변수를 배열로 변경

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -2,9 +2,17 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import Input from "../src/Input";
 
 const { Console } = MissionUtils;
-const mockQuestions = (mockInput) => {
+
+/**
+ *
+ * @param {string[]} mockInputs
+ */
+const mockQuestions = (mockInputs) => {
   const spy = jest.spyOn(Console, "readLineAsync");
-  spy.mockResolvedValue(mockInput);
+  spy.mockImplementation(() => {
+    const mockInput = mockInputs.shift();
+    return Promise.resolve(mockInput);
+  });
 };
 
 describe("getPlusString()", () => {
@@ -17,10 +25,10 @@ describe("getPlusString()", () => {
   afterEach(jest.restoreAllMocks);
 
   test("사용자가 한 번 입력 시 입력을 올바르게 반환해야 함", async () => {
-    const mockInput = "1,2,3";
+    const mockInput = ["1,2,3"];
     mockQuestions(mockInput);
 
     const result = await input.getPlusString();
-    expect(result).toBe(mockInput);
+    expect(result).toEqual("1,2,3");
   });
 });
