9afede0eb2133a84ed477be3e9bde2875d3161db
MERx88 <wjang07@gmail.com>
2024-10-18 23:26:03 +0900

    feat(App): add feature to parse input with custom delimeter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  //parse user input using both custom delimiters and default delimiters
+  parseInput(userInput) {
+    let delimetersPattern = "[,:]";
+    let input = userInput;
+    let customDelimetersArray = [];
 
+    while (input.startsWith("//")) {
+      const customDelimeter = input.slice(2, input.indexOf("\\n"));
+      customDelimetersArray.push(customDelimeter);
+      input = input.slice(input.indexOf("\\n") + 2);
+    }
 
+    if (customDelimetersArray.length) {
+      delimetersPattern = `[,:${customDelimetersArray.join("")}]`;
+    }
+
+    const delimetersRegExp = new RegExp(delimetersPattern);
+    const parsedInput = input.split(delimetersRegExp);
+
+    return parsedInput;
+  }
   async run() {
     //get user input
-    const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
-    Console.print(`raw input : ${userInput}`)
-    //parse input with default delimiter
-    if(userInput){
-      const parsedInput=userInput.split(/[,:]/)
-      Console.print(parsedInput)
-    }else{
-      Console.print(0)
+    const userInput = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    Console.print(`raw input : ${userInput}`);
+    //parse user input
+    if (userInput) {
+      const parsedInput = this.parseInput(userInput);
+      Console.print(parsedInput);
+    } else {
+      Console.print(0);
     }
   }
 }
