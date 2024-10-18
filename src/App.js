fb23753e6edd0aafb4d190360d37f463586d0c64
hwiy01 <vdocom02@naver.com>
2024-10-19 02:07:08 +0900

    feat: 커스텀 구분자 포함 문자열 입력받아 이로 구분하여 각 숫자 합을 출력

--- src/App.js
+++ src/App.js
@@ -13,23 +13,41 @@ function adder(numList) {
 function divideWithDefault(inputStr){
   let strList = inputStr.split(/[, :]/);
 
-  // 각 문자열에 대해 int type으로 변경
   let numList = strList.map(element => {
       return parseInt(element, 10);
   });
 
-  // 숫자 리스트에 대해 합 구하여 반환
+  let sum = adder(numList);
+  return sum;
+}
+
+// 커스텀 구분자 추출하여 커스텀 구분자로 구분하여 더하는 함수
+function divideWithCustom(inputStr){
+  // //와 문자열 \n 사이의 캡처 그룹을 추출하여 customDelimeter에 저장
+  const DELIMETER = inputStr.match(/\/\/(.+)\\n/)[1];
+  const STR = inputStr.split("\\n")[1];
+
+  let strList = STR.split(DELIMETER);
+
+  let numList = strList.map(element => {
+      return parseInt(element, 10);
+  });
+
   let sum = adder(numList);
   return sum;
 }
 
 class App {
   async run() {
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
+    const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
     let answer = 0;
-    // 기본 구분자 (,와 :) 에 대한 결과
-    answer = divideWithDefault(input);
 
+    if(INPUT.startsWith('//')){
+      answer = divideWithCustom(INPUT);
+    }
+    else{
+      answer = divideWithDefault(INPUT);
+    }
     Console.print(`결과 : ${answer}`);
   }
 }
