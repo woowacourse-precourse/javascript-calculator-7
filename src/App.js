75b5fdae18317c85c6bcf17769f079c18bbcd4b5
Jang Eunhye <hh961130@naver.com>
2024-10-18 23:06:26 +0900

    feat: 예외처리 분리(총 7가지)

--- src/App.js
+++ src/App.js
@@ -32,15 +32,47 @@ function validate(inputString) {
   //유효성 검사
   try {
     const regex = /^[0-9,:]+$/; // 정규식 : 전부 숫자,쉼표,콜론으로 구성된 패턴
-    if (regex.test(inputString)) {
+    if (regex.test(inputString) || inputString == "") {
       return 1; // case1 : 커스텀 구분자 없음
     } else if (
       inputString.indexOf("//") == 0 &&
       inputString.indexOf("\\n") == 3
     ) {
-      return 2; // case2 : 커스텀 구분자 있음
+      const customDelimiter = inputString[2];
+      const regexCustom = new RegExp(`^[0-9${customDelimiter}]+$`);
+      if (regexCustom.test(inputString.substr(5))) {
+        return 2; // case2 : 커스텀 구분자 있음
+      } else throw new Error("[Error] 커스텀 구분자로만 구분해 주세요");
     } else {
-      throw new Error("[Error] 잘못된 입력 입니다.");
+      // 예외
+      // 커스텀 구분자 형식 오류
+      if (inputString.includes("//") || inputString.includes("\\n")) {
+        if (inputString.includes("//") == false)
+          // "//"빠진 경우
+          throw new Error("[Error] 커스텀 구분자 앞에 '//'를 추가해 주세요");
+        if (inputString.indexOf("//") > 0)
+          // "//"의 위치가 잘못된 경우
+          throw new Error("[Error] '//'의 위치는 맨 앞이에요");
+        if (inputString.indexOf("//") == 0) {
+          // "//"의 위치는 맞음
+          if (inputString.includes("\\n") == false)
+            // "\n"이 빠진 경우
+            throw new Error("[Error] 커스텀 구분자 뒤에 '\\n'을 추가해 주세요");
+          if (inputString.indexOf("\\n") == 2)
+            // 커스텀 구분자가 없는 경우
+            throw new Error(
+              "[Error] '//'와 '\\n'사이에 커스텀 구분자를 추가하세요"
+            );
+          if (inputString.indexOf("\\n") > 3)
+            // 커스텀 구분자가 여러 글자인 경우
+            throw new Error("[Error] 커스텀 구분자는 한글자만 가능해요");
+        }
+      }
+
+      //기본 구분자 형식 오류
+      else {
+        throw new Error("[Error] 기본 구분자는 쉼표(,)와 콜론(:)만 가능해요");
+      }
     }
   } catch (error) {
     console.error(error.message);
