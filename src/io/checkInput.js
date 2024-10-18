a00c8024995d872044a192dd30fcbc2ba52ca5e8
JaiIn <di0308@naver.com>
2024-10-18 20:01:56 +0900

    feat: 음수, 구분자 공백 처리

--- src/io/checkInput.js
+++ src/io/checkInput.js
@@ -4,7 +4,11 @@ const checkInput = (input) => {
 
     let sum = 0;
 
+    // 문자열에 음수가 있으면 x
+    // 배열에 some이라는 메서드가 있다네요
+
     if(input.length === 0) sum = 0;
+    
     else if(input.slice(0,2) === '//'){
         const endPoint = input.indexOf('\\n');
         if(endPoint === -1){
@@ -21,13 +25,48 @@ const checkInput = (input) => {
             input = input.split(',')
             .flatMap(value => value.split(':'))
             .flatMap(value => value.split(custom))
-            .map((a) => Number(a));
             
+            if (input.some(value => value === '')) {
+                Console.print('\"[ERROR]\" 구분자 사이에 값이 없습니다.');
+                return;
+            }
+
+            input = input.map(a => Number(a));
+
+            if(input.some(value => value < 0)){
+                Console.print('\"[ERROR]\"양수만 입력하세요');
+                return;
+            } 
+
             sum = input.reduce((sum,current) => sum +current, 0);
+            
+            if(isNaN(sum)){
+                Console.print('\"[ERROR]\"잘못 입력했습니다.');
+                return;
+            }
         }
     }
+    else{
+        input = input.split(',')
+                .flatMap(value => value.split(':'))
 
-    
+        if (input.some(value => value === '')) {
+            Console.print('\"[ERROR]\" 구분자 사이에 값이 없습니다.');
+            return;
+        }
+
+        input = input.map(a => Number(a));
+
+        if(input.some(value => value < 0)){
+            Console.print('\"[ERROR]\"양수만 입력하세요');
+            return;
+        }   
+        sum = input.reduce((sum,current) => sum +current, 0);
+        if(isNaN(sum)){
+            Console.print('\"[ERROR]\"잘못 입력했습니다.');
+            return;
+        }
+    };
     Console.print("결과 : " + sum);
 };
 
--- src/io/io.js
+++ src/io/io.js
@@ -3,7 +3,7 @@ import checkInput from "./checkInput.js";
 
 const io = async() => {
     try {
-        const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요. : ");
+        const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
         checkInput(input);
     } catch (error) {
         
