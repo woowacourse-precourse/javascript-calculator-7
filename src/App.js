789d0de5545eed0c8f6dddd7d8b61199e53987e7
eungyo <eungyo0965@gmail.com>
2024-10-18 11:08:55 +0200

    feat(calculator): add custom delimiter support for string addition

--- src/App.js
+++ src/App.js
@@ -24,6 +24,20 @@ class App {
     // 1-1. 기본 구분자 설정
     let delimiter = /[,:]/; 
 
+    // 1-2. 커스텀 delim 설정
+    if (input.startsWith('//')) {
+      const delimiterEnd = input.indexOf('\n');
+      if (delimiterEnd === -1) {
+        throw new Error('[ERROR] Invalid custom delimiter format.');
+      }
+    
+      const customDelimiter = input.substring(2, delimiterEnd);
+      //RegExp : 정규표현식 객체 생성 
+      //escapeRegExp : 이스케이프 문자열 -> 정규표현식 객체 변환
+      delimiter = new RegExp(`${this.escapeRegExp(customDelimiter)}`);
+      input = input.substring(delimiterEnd + 1);
+    }
+
     // 2. 구분자를 기준으로 나누고 숫자를 numbers배열에 저장
     const numbers = input.split(delimiter).map((num) => {
       if (num.trim() === '') return 0;
