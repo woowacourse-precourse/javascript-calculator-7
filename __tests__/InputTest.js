f0949014a442f067c0866963278280cf2d0d2ea6
hamo-o <yheel369@gmail.com>
2024-10-18 12:57:57 +0900

    test(InputTest): 입력 안내 메시지 출력

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -3,9 +3,12 @@ import Input from "../src/Input";
 
 const { Console } = MissionUtils;
 
+const getConsoleSpy = () => jest.spyOn(Console, "readLineAsync");
+
 /**
  *
  * @param {string[]} mockInputs
+ * @param {jest.SpyInstance} spyFunc
  */
 const mockQuestions = (mockInputs) => {
   const spy = jest.spyOn(Console, "readLineAsync");
@@ -23,6 +26,15 @@ describe("getPlusString()", () => {
   });
 
   afterEach(jest.restoreAllMocks);
+
+  test("입력 안내 메시지 출력", async () => {
+    const mockInput = ["1,2,3"];
+    const spy = getConsoleSpy();
+    mockQuestions(mockInput, spy);
+
+    await input.getPlusString();
+    expect(spy).toHaveBeenCalledWith("덧셈할 문자열을 입력해 주세요.\n");
+  });
 
   test("사용자가 한 번 입력 시 입력을 올바르게 반환해야 함", async () => {
     const mockInput = ["1,2,3"];
