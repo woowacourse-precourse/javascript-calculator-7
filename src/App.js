83ffc1130cb51e2c0a3f31ed124a07f0b813cb1d
SWARVY <swarvy0826@naver.com>
2024-10-18 23:06:22 +0900

    feat: `App` 클래스에 문제 요구 사항 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import splitWithCustomDelimiter from './utils/split.js';
+import { enter, print } from './utils/console.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await enter('덧셈할 문자열을 입력해 주세요.\n');
+
+    this.calculate(input);
+  }
+
+  calculate(input) {
+    const numbers = splitWithCustomDelimiter(input);
+    const sum = numbers.reduce((acc, cur) => acc + cur, BigInt(0));
+
+    print(`결과 : ${sum}`);
+  }
 }
 
 export default App;
