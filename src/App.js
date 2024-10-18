7c50ce375eb88ec3d0fe6e9fd8e9d19abfa224b9
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-18 20:53:28 +0900

    feat: 기본 구분자 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-    MissionUtils.Console.print("결과 : " + input);
+    const defaultRegExp = /[:,]/g;
+    const numbers = input.split(defaultRegExp);
+    const result = numbers.map(Number).reduce((acc, cur) => acc + cur, 0);
+    MissionUtils.Console.print("결과 : " + result);
   }
 }
 
