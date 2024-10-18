a240ab31b4f0fc34fc412f25cf23d58934e57fa6
jennaaaaaaaaa <ggm4954@gmail.com>
2024-10-18 18:41:18 +0900

    feat: 입력값에서 구분자 확인

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-// import { Console } from '@woowacourse/mission-utils';
-// class App {
-
-// }
-// export default App;
-
 import { Console } from '@woowacourse/mission-utils';
 class App {
    async run() {
-      Console.print('덧셈할 문자열을 입력해 주세요:');
-      const input = await Console.readLineAsync(''); // 사용자 입력을 비동기적으로 받기
-      this.calculateSum(input);
+      let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const result = this.calculateSum(input);
+      Console.print(`결과 ${result}`);
+   }
+
+   calculateSum(input) {
+      if (!input) {
+         throw new Error('[ERROR] 입력해주세요.');
+      }
+      const delimiterMatch = input.match(/^\/\/(.+)\\n(.+)/);
+      console.log('delimiterMatch', delimiterMatch);
    }
 }
 export default App;
