725f6e60c716b75fd313016a749ececaaeaac0da
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-18 15:56:59 +0900

    style: 코드 가독성 증가

--- src/model.js
+++ src/model.js
@@ -4,12 +4,10 @@ class CalculatorModel {
   }
 
   calculate(input) {
-    console.log("Calculating with input:", input); // 삭제할것
     return this.parseInput(input); // parseInput 호출
   }
 
   parseInput(input) {
-    console.log("parsing input:", input); //삭제할것
     let separators = [...this.defaultSeparators];
     let numbersString = input;
 
@@ -18,16 +16,13 @@ class CalculatorModel {
     if (customSeparatorMatch) {
       const customSeparator = customSeparatorMatch[1].trim();
       separators.push(customSeparator);
-      console.log("custom separator found:", customSeparator); //삭제할것
       numbersString = input.slice(customSeparatorMatch[0].length);
     }
 
-    console.log("Using separators:", separators); //삭제할것
     const regex = new RegExp(`[${separators.join("")}]+`);
     const numbers = numbersString.split(regex).map((num) => {
       const trimmedNum = num.trim();
       const parsedNum = parseFloat(trimmedNum);
-      console.log("parsed number:", parsedNum); //삭제할것
 
       if (isNaN(parsedNum)) {
         throw new Error("[ERROR] 숫자가 아닌 값이 포함되었습니다.");
@@ -39,9 +34,9 @@ class CalculatorModel {
 
       return parsedNum;
     });
-    console.log("Parsed numbers:", numbers); //삭제할것
+
     const sum = numbers.reduce((acc, cur) => acc + cur, 0);
-    console.log("sum of numbers:", sum); //삭제할것
+
     return sum;
   }
 }
