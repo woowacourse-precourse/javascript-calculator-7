f8abe008ddad72a21b85cdd4053bc5c4ba597923
wnsxk2 <wnsxk206@gmail.com>
2024-10-19 00:30:16 +0900

    feat: 쉼표와 콜론으로 구분하는 기능 추가
    입력받은 문자열을 쉼표와 콜론으로 구분하여 배열로 반환하는 코드를 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
+      const regex = /[,:]/;
       const answer = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-      Console.print(`결과 : ${answer}`);
+      Console.print(`결과 : ${answer.split(regex)}`);
     } catch (error) {
       // reject 되는 경우
       Console.print(`에러 발생: ${error.message}`);
