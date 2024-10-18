56bb821674cf6c3797fb7cd28869bad0077f917e
seungboshim <fodo9898@gmail.com>
2024-10-18 10:18:16 +0900

    feat: 구분자 배열 저장

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
   
-    let customDelimiter;
+    let delimiters = ['.', ':'];
     let inputString = input;
     if (this.isCustom(input)) {
-      [customDelimiter, inputString] = this.splitCustom(input);
+      const [customDelimiter, newInputString] = this.splitCustom(input);
+      delimiters.push(customDelimiter);
+      inputString = newInputString;
     }
+
     
-    MissionUtils.Console.print(`결과 : ${inputString}`);
+    Console.print(`결과 : ${inputString}`);
+  }
+
+  /** 커스텀 구분자 스트링인지 확인 */
+  isCustom(input) {
+    return input.startsWith('//');
   }
 
   /** \n 기준으로 나누고 구분자 추출 */
@@ -18,10 +26,6 @@ class App {
     const [customDelimiterString, inputString] = input.split('\\n');
     const customDelimiter = customDelimiterString.slice(2);
     return [customDelimiter, inputString];
-  }
-
-  isCustom(input) {
-    return input.startsWith('//');
   }
 
   
