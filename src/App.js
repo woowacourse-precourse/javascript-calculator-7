3bae0a04356011cd2056600758928bf1107da49a
DodoJBNU <dh723@jbnu.ac.kr>
2024-10-18 17:28:06 +0900

    Revert "feat: validate input string and handle errors"
    This reverts commit 2b9c89b606f0dd3ddb2625fdd404f1cb2832ad87.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ class App {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     
     const [seperatorSet, filteredInput] = this.handleCustomSeperator(input);
-    const [isValidInput, errorMessage] = this.validateInputWithErrorMessage(filteredInput, ...seperatorSet);
-
-    try {
-      if(isValidInput){
-        // 양수 추출 후 덧셈 로직 추가
-      } else {
-        throw new Error(errorMessage);
-      }
-    } catch(error){
-      MissionUtils.Console.print(error.message);
-    }
   }
 
   handleCustomSeperator(input){
@@ -35,46 +24,6 @@ class App {
     
     return [seperatorSet, filteredInput];
   }
-
-  setErrorMessage(index, char){
-    let errorMessage = `[ERROR] `;
-
-    if(this.isEven(index)){
-      errorMessage += `${char}는 양의 정수가 아닙니다.`;
-    } else if(this.isOdd(index)){
-      errorMessage += `${char}는 유효한 구분자가 아닙니다.`
-    }
-    
-    return errorMessage;
-  }
-
-  validateInputWithErrorMessage(input, ...seperators){
-    const seperatorSet = new Set(seperators);
-    let errorMessage;
-
-    const isValidInput = [...input].every((char, index) => {
-      let isEvenIndexPositiveNumber = this.isEven(index) && this.isPositiveNumber(char);
-      let isOddIndexInSeperatorSet = this.isOdd(index) && seperatorSet.has(char);
-      
-      let isValidInput = isEvenIndexPositiveNumber || isOddIndexInSeperatorSet;
-
-      if(!isValidInput){
-        errorMessage = this.setErrorMessage(index, char);
-        return false;
-      }
-      return true;
-    })
-
-    return [isValidInput, errorMessage];
-  }
-
-  isPositiveNumber(char){
-    return isNaN(char) === false && Number(char) > 0;
-  }
-
-  isEven = (num) => num % 2 === 0;
-  isOdd = (num) => num % 2 === 1;
-
 }
 
 export default App;
