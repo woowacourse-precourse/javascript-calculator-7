1837a48c9dd6a16efed87099ee0fda9a4ce6035d
JaiIn <di0308@naver.com>
2024-10-18 23:02:19 +0900

    test: test케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,12 +32,79 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("음수 입력", async () => {
     const inputs = ["-1,2,3"];
     mockQuestions(inputs);
 
-    const app = new App();
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] : 양수만 입력하세요"];
 
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
+
+  test("여러개의 커스텀 문자열 사용", async () => {
+    const inputs = ["//;;;\\n1"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] : 커스텀 구분자는 한 글자만 사용할 수 있습니다."];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("구분자 사이에 값이 없음", async () => {
+    const inputs = [",,,,,,1"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] : 구분자 사이에 값이 없습니다"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("입력값 없음", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("구분자가 숫자", async () => {
+    const inputs = ["//2\\n1"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["[ERROR] : 커스텀 문자에 숫자는 사용할 수 없습니다."];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
 });
