79c1854943b38e2b306e65cfa3b2bf80ae87bc97
RAKRAK21 <94699316+RAKRAK21@users.noreply.github.com>
2024-10-18 16:37:39 +0900

    feat: Add custom delimiter support and mixed delimiter handling
    - Implement custom delimiter parsing
    - Allow mixed use of custom and basic delimiters

--- src/App.js
+++ src/App.js
@@ -11,8 +11,22 @@ class App {
     }
   }
 
+  getCustomDelimiter(input) { // 커스텀 구분자를 찾는 과정. 찾아서 저장
+    const customDelimiterMatch = input.match(/^\/\/(.)\\n/);
+    return customDelimiterMatch ? customDelimiterMatch[1] : null;
+  }
+  
+
   calculateSum(input) {
-    const numbers = input.split(/[,:]/).map(Number); //정규표현식으로 구분자를 찾음
+    let delimiter = ',|:'
+    const custom = this.getCustomDelimiter(input); // 커스텀 구분자 찾음
+
+    if (custom) {
+      delimiter += '|\\' + custom; // 커스텀 구분자 추가
+      input = input.replace(/^\/\/.\\n/, ''); // 커스텀 구분자 선언부 제거
+    }
+
+    const numbers = input.split(new RegExp(delimiter)).map(Number); //정규표현식으로 구분자를 찾음
     let sum1 = 0; //기본을 0으로 두고 for문을 통해 숫자 합산
     for (let i = 0; i < numbers.length; i++) {
       sum1 += numbers[i];
