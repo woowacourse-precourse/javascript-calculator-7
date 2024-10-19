c8bc1b092b2e7c62bf05fd9361ff816472aa62a8
김나연 <apinksky00@gmail.com>
2024-10-19 21:02:35 +0900

    feat(feat1): 기본 기능
    구분자가 쉼표(,) 또는 콜론(:)일 때 숫자 합 구하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      /** 구분자 목록 */
+      const SEPARATORS = [",", ":"];
+
+      /** 입력 값 */
+      const input = await Console.readLineAsync("문자열을 입력해주세요.\n");
+
+      /** 구분자 정규표현식 */
+      const separatorRegex = new RegExp(
+        SEPARATORS.map((s) => `\\${s}`).join("|")
+      );
+
+      const numberArray = input
+        .split(separatorRegex)
+        .map(Number)
+        .filter((n) => !isNaN(n));
+
+      /** 출력 값 */
+      const sum = numberArray.reduce((acc, curr) => acc + curr, 0);
+
+      Console.print(sum);
+    } catch (error) {}
+  }
 }
 
 export default App;
