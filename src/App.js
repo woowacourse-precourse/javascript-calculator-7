54039a58344dd9391cddb9275d11aab0deed6f2b
elect10 <wlgnsakswp@naver.com>
2024-10-19 20:57:52 +0900

    feat: manage empty string

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    Console.print('덧셈할 문자열을 입력해 주세요.');
+    const input = await Console.readLineAsync('');
+
+    if (input === "") { //빈문자열 처리
+      return 0;
+    }
+
+    try {
+      this.sum(input);
+    }
+    catch (error) {
+      Console.error(error);
+    }
+  }
+
+  sum(input) { //덧셈 함수
+
+  }
 }
 
 export default App;
