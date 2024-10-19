18a360198ef22860e6cfed130f4bb77202f74be6
Kim_Yjoo <pas4976@gmail.com>
2024-10-19 17:27:03 +0900

    feat(input) : 사용자 입력 기능 추가
    App클래스 내 run() 에 사용자 입력 받는 로직 추가
    - Console.readLineAsync(''); 로 userInput 변수에 사용자 입력을 반복적으로 받는 루프 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    
+    let userInput;
+    while(true){
+      userInput = await Console.readLineAsync('');
+
+
+    }
+
+  }
 }
 
 export default App;
