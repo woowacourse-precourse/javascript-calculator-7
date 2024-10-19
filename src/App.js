d45b99b40eebcb40a392156b9943f84704d0dbee
ZUITOPIA <korhwngml123@naver.com>
2024-10-20 01:35:05 +0900

    feat(App): 문자열 입력받는 함수 따로 빼내기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
     async run() {
-        // Console.readLineAsync()와 Console.print()를 활용하여 사용자의 값 입력, 출력
-
-        const inputText = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+        const inputText = await this.getInputText();
 
         Console.print(inputText); // input 값 확인
+    }
+
+    getInputText() {
+        return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
     }
 }
 
