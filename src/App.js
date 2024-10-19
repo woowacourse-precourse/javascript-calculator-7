0ab80a4f1bc27ebae3799460485b07e7fbb0fb7b
yuri295 <jin_astro0302@naver.com>
2024-10-19 20:43:15 +0900

    feat: 숫자 문자열을 더하는 계산기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+
+      const result = this.calculateSum(input);
+      MissionUtils.Console.print(`결과: ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR] ${error.message}`);
+    }
+  }
+  calculateSum(input) {
+    const numbers = input.split("").map(Number);
+    return numbers.reduce((sum, number) => sum + number, 0);
+  }
 }
 
 export default App;
