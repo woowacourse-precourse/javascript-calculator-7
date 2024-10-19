866068582ae4576f467f5db6fabcce43b2fd2719
jsy <suyeonb1db@gmail.com>
2024-10-19 20:34:36 +0900

    feat: 구분자 처리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+function getSplitunit(input){
+  if(input.indexOf("//") != -1 && input.indexOf("\\n") != -1){
+    let start = input.indexOf("//") + 2;
+    let end = input.indexOf("\\n");
+    return input.substring(start, end);
+  }
+  return ",:";
+};
+
 async function getText() {
   try {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-    console.log(input);
     return input;
   } catch (error) {
   }
@@ -12,6 +20,7 @@ async function getText() {
 class App {
   async run() {
     const input = await getText();
+    let splitunit = getSplitunit(input);
   }
 }
 
