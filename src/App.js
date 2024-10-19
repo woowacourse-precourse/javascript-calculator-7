82dcfc6bf90e3cc1695b408e0e150c9c3d259be2
Ko Daeun <drechsu@gmail.com>
2024-10-19 22:31:42 +0900

    feat(App): 커스텀 구분자 지정해서 더하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  getNumBeforeSeparator(input, separator = []) {
+  getNumBeforeSeparator(input, customSeparator = "") {
     let num = "";
     for (let i = 0; i < input.length; i++) {
       if (input[i] === "," || input[i] === ":") {
         break;
       }
+      if (customSeparator !== "" && input[i] === customSeparator) {
+        break;
+      }
+
       num += input[i];
     }
     return { num, nextInput: input.slice(num.length + 1) };
   }
 
-  getNumSum(input) {
+  getNumSum(input, customSeparator) {
     let sum = 0;
     while (input.length > 0) {
-      const { num, nextInput } = this.getNumBeforeSeparator(input);
+      const { num, nextInput } = this.getNumBeforeSeparator(
+        input,
+        customSeparator
+      );
       sum += Number(num);
       input = nextInput;
     }
     return sum;
   }
 
+  checkCustomSeparator(input) {
+    if (input.startsWith("//") && input.includes("\\n")) {
+      const parts = input.split("\\n");
+      const customSeparator = parts[0].slice(2);
+      const remainingInput = parts[1];
+      return { customSeparator, remainingInput };
+    }
+    return { customSeparator: "", remainingInput: input };
+  }
   async run() {
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-    const sum = this.getNumSum(input);
+    let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    const { customSeparator, remainingInput } =
+      this.checkCustomSeparator(input);
+    const sum = this.getNumSum(remainingInput, customSeparator);
     Console.print(`결과: ${sum}`);
   }
 }
