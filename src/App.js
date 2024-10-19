1e00d95dd53f4127b236b0d96496bc2b4753a51c
Kim_Yjoo <pas4976@gmail.com>
2024-10-19 18:22:13 +0900

    feat(checkCustomSpliter) : 사용자 입력에서 커스텀 문자 정의 여부를 확인
    //로 시작하는지 확인 한 후, 해당된다면 \n의 인덱스 위치를 찾은 다음 그 사이에 있는 문자들을 한 개씩 추가(구분자는 오직 하나의 문자라고 가정)
    - checkCustomSpliter 메서드에 유저 입력과 구분자 set 객체를 인자로 넘긴다.
    - 구분자는 set 객체로 하여 자동으로 중복을 제거하게 한다.
    - 메서드는 커스텀 구분자 선언문을 잘라낸 나머지 문자열을 반환한다.
    - 커스텀 구분자 선언의 규칙이 정확하지 않다면 오류를 발생시킨다.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,33 @@ class App {
     let userInput;
     let result = 0;
     while(true){
+      let spliter = new Set([",",":"]);
       userInput = await Console.readLineAsync('');
 
       if(!this.isEmpty(userInput)) {
+        userInput = this.checkCustomSpliter(userInput, spliter);
         
       }
-      
+
     }
 
   }
 
   isEmpty(userInput) {
     return userInput === '';
+  }
+  checkCustomSpliter(userInput, spliter) {
+    if(userInput.substring(0,2) === "//"){
+      let indexOfEnd = userInput.indexOf('\\n');
+      if(indexOfEnd === -1) {
+        throw new Error("[ERROR] 잘못된 문장. (커스텀 문장 추가 에러)"); 
+      }
+      for (let newSpliters of userInput.substring(2, indexOfEnd)) {
+        spliter.add(newSpliters);
+      }
+      return userInput.substring(indexOfEnd + 2, userInput.length);
+    }
+    return userInput;
   }
 }
 
