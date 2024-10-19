7cb3c6f713876497c3601b57126808cb259cf1f9
siggu <m0_w_0m@naver.com>
2024-10-19 22:33:12 +0900

    refactor: 에러 메세지 및 허용 커스텀 구분자 삭제

--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,9 @@ const MESSAGES = {
   INPUT_PROMPT: '덧셈할 문자열을 입력해 주세요.',
   ERROR: {
     INPUT_NEGATIVE_NUMBER: '[ERROR] 양수를 입력해야 합니다.',
-    INPUT_DASH_SEPARATOR: '[ERROR] 구분자로 "-"는 사용할 수 없습니다.',
     INPUT_NUMBER_SEPARATOR: '[ERROR] 커스텀 구분자를 숫자로 지정할 수 없습니다.',
     INPUT_BASIC_SEPARATOR: '[ERROR] 이미 있는 구분자를 커스텀 구분자로 지정할 수 없습니다.',
     INPUT_BASIC_OR_CUSTOM_SEPARATOR: '[ERROR] 기본 구분자 혹은 커스텀 구분자를 사용해야 합니다.',
-    INPUT_CONTINUOUS_DOTS: '[ERROR] 연속된 소수점은 입력할 수 없습니다.',
     INPUT_SPACE: '[ERROR] 문자열에 공백을 입력할 수 없습니다.',
     START_TO_SEPARATOR: '[ERROR] 구분자로 시작하는 문자열은 입력할 수 없습니다.',
     END_TO_SEPARATOR: '[ERROR] 구분자로 끝나는 문자열은 입력할 수 없습니다.',
@@ -17,11 +15,9 @@ const MESSAGES = {
 
 const SEPARATOR = {
   ALLOWED_SEPARATOR: [',', ':'],
-  ASSIGN_CUSTOM_SEPARATOR: ['/', '\\', 'n', '.', '"'],
+  ASSIGN_CUSTOM_SEPARATOR: ['/', '\\', 'n'],
 };
-
-const ROUND_NUMBER = 1e10;
 
 const MAX_NUM = Number.MAX_VALUE;
 
-export { MESSAGES, SEPARATOR, ROUND_NUMBER, MAX_NUM };
+export { MESSAGES, SEPARATOR, MAX_NUM };
