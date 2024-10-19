d7eef1cb74df384c997a8176d77db632f246423d
dmson1218 <dmson1218@naver.com>
2024-10-19 12:38:55 +0900

    feat(App): 사용자의 문자열을 입력받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    async run() {
+        const input = await this.getUserInput();
+    }
+
+    async getUserInput() {
+        try {
+            const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+            return userInput;
+        } catch (error) {
+            throw new Error("[Error]");
+        }
+    }
 }
 
 export default App;
