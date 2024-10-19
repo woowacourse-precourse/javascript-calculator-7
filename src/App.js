c1c4ece3e11d437762d070877ac8800581e06637
hummingbbird <pop9814@kookmin.ac.kr>
2024-10-19 14:58:26 +0900

    feat($validateInput): 음수 에러 처리 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import validateInput from "./validateInput.js";
+
 class App {
   async run() {
     let separator = ":,";
-    let numbers=[];
+    let numbers = [];
     const ERRORMESSAGE = "[ERROR]";
     const TARGET_STRING = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    if (validateInput(TARGET_STRING)) {
-      MissionUtils.Console.print(ERRORMESSAGE);
+
+    if (!validateInput(TARGET_STRING)) {
+      throw new Error(ERRORMESSAGE);
+      // MissionUtils.Console.print(`${ERRORMESSAGE}`);
     } else {
       // 커스텀한 구분자 저장
       if (TARGET_STRING.startsWith("//")) {
-        separator = TARGET_STRING[2];
-        numbers = TARGET_STRING.slice(5).split(separator);
+        const NINDEX = TARGET_STRING.indexOf("\n");
+        separator = TARGET_STRING.slice(2, NINDEX);
+        
+        // 특수 문자를 이스케이프 처리
+        const escapedSeparator = separator.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+
+        const regexSeparator =
+          escapedSeparator.length > 1 ? `[${escapedSeparator}]` : escapedSeparator; // 여러 개일 경우 정규 표현식 생성
+        
+        numbers = TARGET_STRING.slice(NINDEX + 1).split(
+          new RegExp(regexSeparator)
+        );
       } else {
         // 커스텀한 구분자가 없을 경우(가장 basic한)
         numbers = TARGET_STRING.split(/[:\,]/);
       }
-      const SUM = numbers.reduce((sum, n) => sum + parseInt(n), 0);
-      MissionUtils.Console.print(`출력 : ${SUM}`);
+
+      const SUM = numbers.reduce((sum, n) => {
+        const parsedNum = parseInt(n); // 문자열을 숫자로 변환
+        return isNaN(parsedNum) ? sum : sum + parsedNum; // NaN일 경우 현재 sum을 반환
+      }, 0);
+      
+      MissionUtils.Console.print(`결과 : ${SUM}`);
     }
   }
 }
--- src/validateInput.js
+++ src/validateInput.js
@@ -7,18 +7,14 @@ function validateInput(targetNumber) {
   if (CUSTOMWORD && (CUSTOMWORD[1] === "" || /^\d+$/.test(CUSTOMWORD[1])))
     return false;
 
-  // 3. targetNumber가 커스텀 구분자 형식일 때만 해당 조건 적용
-  if (!targetNumber.startsWith("//") || /^\d+\n/.test(targetNumber)) return false;
-
-  // 4. 0이 있는 경우
-  if (targetNumber.includes("0")) return false;
-
   // 5. 아무것도 안 들어온 경우
   if (!targetNumber) return false;
 
   // 6. 끝에 문자가 있을 경우
   if (!/\d$/.test(targetNumber)) return false;
 
+  // 7. 커스텀 구분자가 없고 음수가 들어온 경우
+  if (!CUSTOMWORD && /-\d+/.test(targetNumber)) return false;
   return true;
 }
 
