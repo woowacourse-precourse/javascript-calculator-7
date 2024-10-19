df7154876da1297cebe41f8ab450242a56bea028
zzz664 <zzz664>
2024-10-19 12:54:42 +0900

    feat(App): splitDelim 메서드  구현
    Readme.md에 작성한 기능 중 첫번째 기능인 쉼표(,)와 콜론(:)으로 구분하여 숫자를 추출하는 메서드를 구현했다.
    javascript String객체의 내장메서드 split과 여러 개의 구분자를 위해 정규표현식을 활용했다.
    또한 +단항연산자를 사용해서 문자열->숫자의 변환을 했다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 class App {
-  async run() {}
+  splitDelim(str) {
+    // /,|:/ 정규식을 통해 split메서드로 여러개의 구분자를 사용하여 문자열을 가공한다
+    let processedStr = str.split((/,|:/));
+
+    // + 단항연산자를 이용해서 문자열 -> 숫자 변환을 한다
+    for(let i = 0; i<splitStr.length; ++i) {
+      processedStr[i] = +processedStr[i];
+    }
+
+    return processedStr;
+  }
+
+  async run() {
+    console.log(this.splitDelim("1,23:5,7:8:22"));
+  }
 }
 
 export default App;
