c4beeab72d6b2b5594e2a9e17204699f9ed3e16a
jissssu <leeland21@naver.com>
2024-10-19 20:12:09 +0900

    feat: 커스텀 구분자 추출 및 기본 구분자 추가

--- src/App.js
+++ src/App.js
@@ -12,24 +12,40 @@ class App {
 
     // 기본 구분자 
     const separators = [",", ":"];
-    for (const separator of separators) {
-      input = input.split(separator).join(",");
+
+    // 커스텀 구분자 처리
+    let inputString = input;
+    let customSeparator = null;
+    if (this.isCustom(input)) {
+      [customSeparator, inputString] = this.splitCustom(input);
+      separators.push(customSeparator); // 커스텀 구분자를 기본 구분자 배열에 추가
     }
 
-    // 커스텀 구분자 
-    if (input.startsWith("//")) {
-      const customSeparator = input.charAt(2); 
-      input = input.split(/\n/)[1]; 
-      input = input.split(customSeparator).join(","); 
+    // 기본 구분자 처리
+    for (const separator of separators) {
+      inputString = inputString.split(separator).join(",");
     }
 
     // 잘못된 입력값 처리
-    const numbers = input.split(",").map(num => Number(num.trim()));
+    const numbers = inputString.split(",").map(num => Number(num.trim()));
     if (numbers.some(num => isNaN(num) || num < 0)) {
       return Console.print("[ERROR] 음수 또는 잘못된 값을 입력했습니다.");
     }
 
-    Console.print(input);
+    Console.print(inputString); 
+  }
+
+  // 커스텀 구분자 스트링인지 확인 
+  isCustom(input) {
+    return input.startsWith("//");
+  }
+
+  //커스텀 구분자를 추출하고 새로운 입력 문자열을 반환 
+  splitCustom(input) {
+    const parts = input.split("\n");
+    const customSeparator = parts[0].slice(2); 
+    const newInputString = parts[1]; 
+    return [customSeparator, newInputString];
   }
 }
 
