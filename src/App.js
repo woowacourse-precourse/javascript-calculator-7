3053fdd457f6aec522408cd7c76422064e929026
Sohee <sh11govlday@gmail.com>
2024-10-19 10:11:29 +0900

    feat : string input functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n'
+      );
+      Console.print(input);
+    } catch (error) {
+      Console.print('에러:', error);
+    }
+  }
 }
 
 export default App;
