90cfb9ecf233cba1e2fea815bffbd62ad007d262
leechangsuuu <sunnckd@naver.com>
2024-10-19 15:41:06 +0900

    feat: 커스텀 구분자 확인 및 덧셈으로 출력 가능하게 함

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ class StringCalculator{ // 계산을 위한 클래스
     try{
       let DELIMITER = /[,:]/;// 기본 구분자를 쉼표(,) 또는 콜론(:)으로 설정
                             // let을 사용한 이유는 나중에 커스텀 구분자로 변경하여 사용하기 위함
+      
+     // 입력 문자열이 커스텀 구분자를 가지고 있을 경우
+      if(inputs.startsWith("//")){ // 입력이 "//"으로 시작되었을 때에만 실행
+        const CUSTOM = inputs.match(/^\/\/(.)\\n/);// 커스텀 구분자를 찾기 위한 정규표현식. 
+                                                   // "//" 뒤의 문자와 "\n" 사이를 구분자로 설정
+        if(CUSTOM){// 커스텀 구분자를 찾았을 때만 실행
+          DELIMITER = CUSTOM[1]; // 구분자를 정규표현식에서 추출한 커스텀 구분자로 변경
+                                 // 예: 세미콜론(;)을 구분자로 설정
+          inputs = inputs.slice(CUSTOM[0].length); // "1;2;3"과 같이 구분자 뒤의 숫자만 남김
+        }else{
+          throw new Error("잘못된 입력 형식입니다.");
+        }
+      }
       // 입력 문자열을 구분자로 분리한 후, 
       // 숫자로 변환(입력값을 split으로 구분자로 나누고 map을 돌아 숫자마다 십진수값으로 변환)
       const NUMBERS = inputs.split(DELIMITER).map((num)=>{
@@ -24,10 +37,10 @@ class App { // 입력과 출력하는 클래스
   async run() {
       try{
         // 사용자로부터 문자열 입력을 비동기적으로 받음
-        const INPUT = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하여 주세요.\n");
+        const INPUT = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하여 주세요.");
         // 입력된 문자열을 계산하여 결과를 얻음
         const RESULT = this.calculator.add(INPUT);
-        MissionUtils.Console.print(`결과: ${RESULT}\n`);
+        MissionUtils.Console.print(`결과 : ${RESULT}`);
       }catch(error){
         console.error(`ERROR - ${error.message}`);//에러에 맞게 메시지 출력
         
