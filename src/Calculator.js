fa349d72b14e5e02a342bb3fc3a3d7cf01b5631d
Hyori <hyoring030@gmail.com>
2024-10-19 19:12:41 +0900

    feat: add basic case calculate

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,14 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Calculator {
-    calculate(string) {
-      try {
-        return string+string;
-      } catch (error) {
-      }
-    }
-  }
-  
-export default Calculator;
-  
+  calculate(string) {
+    try {
+      let NUMS = string.split(/,|:/);
+      let ANSWER = 0;
 
+      for (let i = 0; i < NUMS.length; i++) {
+        ANSWER = ANSWER + parseInt(NUMS[i], 10);
+      }
+      if(isNaN(ANSWER)){
+        ANSWER = 0;
+      }
+      return ANSWER;
+    } catch (error) {}
+  }
+}
+
+export default Calculator;
