a8ffceb8ba8e9ac7989213edc6d34588aecdafa4
yheseul <kangyheseul@naver.com>
2024-10-19 14:41:56 +0900

    feat: 커스텀 구분자가 없는 경우 쉼표와 콜론 기준 문자열 분리 및 예외 처리 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ class App {
     const numbersString = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    const isCustomSeparator = numbersString.includes("//") && numbersString.includes("\\n");
+    const isCustomSeparator =
+      numbersString.includes("//") && numbersString.includes("\\n");
     if (isCustomSeparator) {
       Console.print("둘다있음");
       const start = numbersString.indexOf("//");
@@ -14,6 +15,23 @@ class App {
       const stringToSeparate = numbersString.replace(custom, "");
       const separatedString = stringToSeparate.split(customSeparator);
       Console.print(separatedString);
+    } else {
+      if (numbersString.includes(",") && numbersString.includes(";")) {
+        const semicolonSeparatedString = numbersString.split(";");
+        const commaSeparatedString = semicolonSeparatedString.map((el) =>
+          el.split(",")
+        );
+        const lastSeparatedString = commaSeparatedString.join(",").split(",");
+        Console.print(lastSeparatedString);
+      } else if (numbersString.includes(",")) {
+        const separatedString = numbersString.split(",");
+        Console.print(separatedString);
+      } else if (numbersString.includes(";")) {
+        const separatedString = numbersString.split(";");
+        Console.print(separatedString);
+      } else {
+        Console.print("[ERROR]");
+      }
     }
   }
 }
