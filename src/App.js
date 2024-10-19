719d9f0ec0b62057080669029bbfc53980fb1b4a
Ko Daeun <drechsu@gmail.com>
2024-10-19 21:49:03 +0900

    feat(App): 숫자 추출 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  getNumBeforeSeparator(input, separator = []) {
+    let num = "";
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] === "," || ":") {
+        break;
+      }
+      num += input[i];
+    }
+    return { num, nextInput: input.slice(num.length + 1) };
+  }
+
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
     Console.print(`결과: ${input}`);
