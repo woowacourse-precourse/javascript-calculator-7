afdd45f72e61e5eefe949303f24a9c0ae464a697
Jinny <ggab.challenge@gmail.com>
2024-10-19 15:49:58 +0900

    refactor(const): 상수 이름을 camelCase에서 SNAKE_CASE로 변경 (중복 아님)
    - numberSum.js파일 내에 상수 이름 변경
      1) 기존: const regularExpression
         변경: const REGULAR_EXPRESSION
      2) 기존: const removeStr
         변경: const REMOVE_STR
      3) 기존: const splitNumbers
         변경: const SPLIT_NUMBERS

--- src/Model/numberSum.js
+++ src/Model/numberSum.js
@@ -4,15 +4,15 @@ import UserInput from '../View/UserInput.js';
 // 구분자 쉼표나 콜론 기준으로 숫자 추출
 
 const NumberSum = userInput => {
-  const regularExpression = /[^0-9]/g;
-  const removeStr = userInput.replace(regularExpression, '');
+  const REGULAR_EXPRESSION = /[^0-9]/g;
+  const REMOVE_STR = userInput.replace(REGULAR_EXPRESSION, '');
 
   // 각 자리 숫자 더하는 함수 만들기
-  const splitNumbers = removeStr.split('');
+  const SPLIT_NUMBERS = REMOVE_STR.split('');
   let totalNumberSum = 0;
 
-  for (let i = 0; i < splitNumbers.length; i += 1) {
-    totalNumberSum += parseInt(splitNumbers[i], 10);
+  for (let i = 0; i < SPLIT_NUMBERS.length; i += 1) {
+    totalNumberSum += parseInt(SPLIT_NUMBERS[i], 10);
   }
   Console.print('결과 : ' + totalNumberSum);
 };
