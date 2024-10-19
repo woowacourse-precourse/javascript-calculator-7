25715f3d2f5b92ea49d105b9fa2a7204dd38e7e9
subsub-e <ab13696802@gmail.com>
2024-10-19 16:59:17 +0900

    feat(app) : 결과 출력 및 기능 합치기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // 기능 구현 리스트
 // 1. 입력하라고 알려주는 문구 출력하고 입력받은 문자열 저장하기 [O]
-// 2. 맨 앞에 / 로 시작할 경우 커스텀 구분자 추출해서 저장하기 [X]
-// 3. 구분자를 기준으로 숫자 나누기 [X]
-// 4. 나눈 숫자 더하기 [X]
-// 5. 결과값 출력하고 프로그램 종료하기 [X]
+// 2. 맨 앞에 / 로 시작할 경우 커스텀 구분자 추출해서 저장하기 [O]
+// 3. 구분자를 기준으로 숫자 나누기 [O]
+// 4. 나눈 숫자 더하기 [O]
+// 5. 결과값 출력하고 프로그램 종료하기 [O]
 // 6. 예외 처리 [X]
 
 import { readInput, printOutput } from './utils/inputOutputHandler.js';
+import { findDelimiters } from './utils/findDelimiters.js';
+import { extractNumbers } from './utils/extractNumbers.js';
+import { calculateNumbers } from './utils/calculateNumbers.js';
 
 
 class App {
@@ -15,9 +18,18 @@ class App {
     // 입력 받기
     let input = await readInput();
 
+    // 구분자 + 커스텀 구분자 추출
+    const delimiters = findDelimiters(input);
+
+    // 구분자 배열을 토대로 숫자 추출
+    const numberArray = extractNumbers(input, delimiters);
+
+    // 추출된 숫자 계산
+    const result = calculateNumbers(numberArray);
+
     // 출력 하기
-    printOutput(input);
-    
+    printOutput(result);
+
   }
 }
 
--- src/utils/inputOutputHandler.js
+++ src/utils/inputOutputHandler.js
@@ -3,6 +3,9 @@ import { Console}  from "@woowacourse/mission-utils";
 // 입력 메세지
 const INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n";
 
+// 출력 메세지
+const OUTPUT_MESSAGE = "결과 : "
+
 
 // 입력 함수
 export function readInput() {
@@ -13,5 +16,5 @@ export function readInput() {
 
 // 출력 함수
 export function printOutput(output) {
-    Console.print(output);
+    Console.print(OUTPUT_MESSAGE + output);
 }
