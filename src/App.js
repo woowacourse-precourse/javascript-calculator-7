2d4c66103ad1dfc35185b2491bd34932eacc4a95
DongjaJ <jdh981118@naver.com>
2024-10-19 21:08:48 +0900

    feat: 숫자들의 구분자가 구분자의 조합(예시 ,: )으로 이루어져 있을 때 에러를 반환한다.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   #input;
+  separatorList = [",", ":"];
 
   async run() {
     await this.readInput();
@@ -25,12 +26,68 @@ class App {
         return true;
       }
 
-      if (Number.isInteger(Number(this.#input[2]))) {
+      const customSeparator = this.#input[2];
+
+      if (
+        this.separatorList.includes(customSeparator) ||
+        Number.isInteger(Number(customSeparator))
+      ) {
         return true;
       }
+
+      this.separatorList.push(this.#input[2]);
+
+      this.#input = this.#input.substring(5);
     }
 
-    return false;
+    if (
+      this.#input.replaceAll(
+        new RegExp(`[${this.separatorList.join("")}0-9]`, "g"),
+        ""
+      ).length !== 0
+    ) {
+      return true;
+    }
+
+    function generateDelimiterCombinations(
+      delimiters,
+      minLength = 2,
+      maxLength = 3
+    ) {
+      const result = [];
+
+      function backtrack(combination, start) {
+        if (combination.length >= minLength) {
+          result.push(combination.join(""));
+        }
+
+        if (combination.length === maxLength) {
+          return;
+        }
+
+        for (let i = 0; i < delimiters.length; i++) {
+          if (i !== start) {
+            // 연속된 같은 구분자 방지
+            backtrack([...combination, delimiters[i]], i);
+          }
+        }
+      }
+
+      backtrack([], -1);
+      return result;
+    }
+
+    const combinations = generateDelimiterCombinations(this.separatorList);
+
+    let isInValid = false;
+    combinations.forEach((combination) => {
+      if (this.#input.includes(combination)) {
+        isInValid = true;
+        return;
+      }
+    });
+
+    return isInValid;
   }
 }
 
