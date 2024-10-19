e17a123f9370e5318698878dfa81b72ad5ac840c
SungSion <sionsbed2@gmail.com>
2024-10-19 16:33:55 +0900

    feat: implement basic string addition feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class Calulator {
+  add(input) {
+    if (input === "") {
+      return 0;
+    }
+
+    const delimiters = /[,:]/;
+    const numbers = input.split(delimiters);
+
+    const sum = numbers.reduce((total, current) => {
+      const number = parseInt(current, 10);
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 잘못된 입력값이 포함되어 있습니다.");
+      }
+      return total + number;
+    }, 0);
+
+    return sum;
+  }
+}
+
 class App {
-  async run() {}
+  constructor() {
+    this.calulator = new Calulator();
+  }
+
+  async run() {
+    try {
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+      const result = this.calulator.add(input);
+      Console.print(`결과 : ${result}`);
+
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
