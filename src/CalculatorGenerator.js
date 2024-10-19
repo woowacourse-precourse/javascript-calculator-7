ec9e7b86b39f7bdafab0d4efd38e5429c6fcbbb6
singyuKang <rkdtlscks123@naver.com>
2024-10-19 18:48:16 +0900

    feat : CalculatorGenerator 추가

---
+++ src/CalculatorGenerator.js
@@ -0,0 +1,44 @@
+import { DEFAULT_DELIMITER, ERROR_MESSAGE } from "./constants.js";
+
+const CalculatorGenerator = {
+  async getResult(inputString) {
+    if (this.checkCustomDelimiter(inputString)) {
+      return this.calculateCustomDelimiter(inputString);
+    } else {
+      return this.calculatDelimiter(inputString);
+    }
+  },
+
+  checkCustomDelimiter(inputString) {
+    return inputString.startsWith("//");
+  },
+
+  calculatDelimiter(inputString) {
+    if (inputString === "") {
+      return 0;
+    }
+    let numbers = [];
+    numbers = inputString.split(DEFAULT_DELIMITER);
+    return this.calculateNumber(numbers);
+  },
+
+  calculateCustomDelimiter(inputString) {
+    let intStr = inputString;
+    let customDelimeterEndIndex = intStr.indexOf("\\n");
+    let customDelimeter = intStr.substring(2, customDelimeterEndIndex);
+    let numbers = [];
+    intStr = intStr.substring(customDelimeterEndIndex + 2);
+    numbers = intStr.split(customDelimeter);
+    return this.calculateNumber(numbers);
+  },
+
+  calculateNumber(numberArr) {
+    let sum = numberArr.reduce((acc, cur) => {
+      let parsedNumber = cur === "" ? 0 : parseFloat(cur, 10);
+      return acc + parsedNumber;
+    }, 0);
+    return sum;
+  },
+};
+
+export default CalculatorGenerator;
