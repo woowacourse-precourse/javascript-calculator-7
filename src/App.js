5a6f53c1830bdec6bb16d477a6deeb02ec845b73
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-19 10:10:36 +0900

    feat : 초기 문자열출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputStr = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    Console.print(inputStr);
+  }
 }
 
 export default App;
