56d3a204d21a512b65669ca3f12d39709f454a36
kochungwon <yud01063@gmail.com>
2024-10-19 22:08:58 +0900

    fix(입력): 입력 프롬프트에 줄바꿈 추가
    - 사용자가 입력할 때 더 명확하게 보이도록 "덧셈할 문자열을 입력해 주세요." 프롬프트에 줄바꿈을 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
       if (input.length === 0) {
         MissionUtils.Console.print('결과 : 0');
