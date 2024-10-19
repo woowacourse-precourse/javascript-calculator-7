ea204afd07bf74e10f7fb13b0e278b36cb88ba36
yena <rladpsk1117@gmail.com>
2024-10-19 18:59:29 +0900

    feat: 기본 구분자 분리 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ class App {
       '덧셈할 문자열을 입력해 주세요.\n'
     );
     try {
+      // 빈 문자열일 경우
+      if (input == '') {
+        Console.print('결과 : 0 ');
+      }
+
+      // 기본 구분자
+      let sum = 0;
+      const result = input.split(/,|;/g);
+      result.forEach((item) => {
+        sum += parseInt(item);
+      });
+      Console.print(`결과 : ${sum}`);
+
+      // 커스텀 구분자
     } catch (error) {
       Console.print(`[ERROR] ${error}`);
     }
