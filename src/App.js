ad3afe7f87c73a1960b28a73b4a218c3e05fc7af
choiihwa <chldlghk@gmail.com>
2024-10-19 15:52:06 +0900

    feat: 문자열 입력 기능 생성 - @woowacourse/mission-utils 라이브러리 추가 - 결과값 변수 생성 - 라이브러리를 이용한 문자열 입력 요청

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 결과값 변수
+    let result = 0;
+
+    // 사용자 입력값을 받는다.
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+   
+  }
+  
 }
 
 export default App;
