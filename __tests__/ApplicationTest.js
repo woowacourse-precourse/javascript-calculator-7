bfbd3e47062429f94151b83a827b88a3b6819d22
dongmin <dongmin11566@gmail.com>
2024-10-19 22:55:12 +0900

    test: 기본 구분자와 커스텀 구분자 동시 사용 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,6 +32,21 @@ describe("문자열 계산기", () => {
     });
   });
 
+  test("기본 구분자와 커스텀 구분자 동시 사용", async () => {
+    const inputs = ["//;\\n1;2,3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", async () => {
     const inputs = ["-1,2,3"];
     mockQuestions(inputs);
