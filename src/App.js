153db9e5a3f6e93364e5bc23479971d312257cf7
hyeonjin6530 <cola1876@naver.com>
2024-10-19 22:27:06 +0900

    feat: 쉼표와 콜론을 구분자로 하는 계산기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const result = this.calculateSum(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print('[ERROR]');
+    }
+  }
+
+  calculateSum(input) {
+    if (!input) return 0;
+    const numbers = input.split(/[,|:]/).map(Number);
+    return numbers.reduce((sum, number) => sum + number, 0);
+  }
 }
 
 export default App;
