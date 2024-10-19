677994ac8a28c3aca5e2767aa527908ac7bb8a76
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-19 17:05:01 +0900

    feat: 커스텀일 경우, 아닐경우 구분

--- src/utils/splitNumber.js
+++ src/utils/splitNumber.js
@@ -1,9 +1,26 @@
 import { ERROR_MESSAGES } from "../constants/errorMessages.js";
+
 const splitNumber = (input) => {
   // 공백일 경우 0을 반환
   if (!input || input.trim() === "") {
     return [0];
   }
+
+  const customDelimiterRegex = /^\/\/(.*)\\n(.*)$/;
+  const match = input.match(customDelimiterRegex);
+
+  let numbers;
+
+  // 커스텀이 아닐 경우
+  if (!match) {
+    numbers = input.split(/[,:]/);
+  }
+
+  // 커스텀일 경우
+  if (match) {
+  }
+
+  return numbers;
 };
 
 export default splitNumber;
