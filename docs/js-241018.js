d3539ddd22752d17c5ec9972c06a39961fe24d68
Eastyn <eastynwork@gmail.com>
2024-10-19 16:29:57 +0900

    Revert "docs : add notes of 18th of October, 2024"
    This reverts commit fc1077d399aafee7887cf9b36b320b671dd5b018.

--- docs/js-241018.js
+++ docs/js-241018.js
@@ -1,25 +1,20 @@
-let tester1 = "//;\\n1";
-let tester2 = "//;\n1;2;3;";
-let tester3 = "1,2:3";
-let tester4 = "-1,2,3";
-
 let input;
 
-function checkNumberSign(number) {
-	return Math.sign(number);
-}
+// 특수문자 색출 함수
+function checkInputDelimiter(input) {}
 
 function sumNumbers(array) {
 	let result = 0;
 	for (let i = 0; i < array.length; i++) {
-		if (checkNumberSign(array[i]) >= 0) {
-			result = Number(array[i]) + result;
-		} else {
-			throw new Error("양수가 아닙니다.");
-		}
+		result = Number(array[i]) + result;
 	}
 	return result;
 }
+
+let tester1 = "//;\\n1";
+let tester2 = "//;\n1;2;3;";
+let tester3 = "1,2:3";
+let tester4 = "-1,2,3";
 
 function testerFn(input) {
 	let regexes = {
@@ -33,6 +28,7 @@ function testerFn(input) {
 	let delimiter;
 	let reg;
 	let input_array;
+	let result = 0;
 
 	try {
 		if (input.match(regexes.check_special_chracter) || input.match(regexes.check_empty_string)) {
@@ -43,16 +39,18 @@ function testerFn(input) {
 				reg = RegExp(`[\\\\/\\sA-Za-z${delimiter}]`, "g");
 
 				input_array = input.split(reg);
-				/* 	for (let i = 0; i < input_array.length; i++) {
+				for (let i = 0; i < input_array.length; i++) {
 					result = Number(input_array[i]) + result;
-				} */
+				}
 			} else {
 				input_array = input.split(regexes.normal_delimiter);
+				for (let i = 0; i < input_array.length; i++) {
+					result = Number(input_array[i]) + result;
+				}
 			}
-			const answer = sumNumbers(input_array);
-			console.log(`결과 : ${answer}`);
+			console.log(`결과 : ${result}`);
 		}
 	} catch (e) {
-		console.error(e);
+		throw new Error("[ERROR] 잘못된 문자열을 입력하셨습니다.");
 	}
 }
--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-	checkNumberSign(number) {
-		return Math.sign(number);
-	}
-
-	sumNumbers(array) {
-		let result = 0;
-		for (let i = 0; i < array.length; i++) {
-			if (this.checkNumberSign(array[i]) >= 0) {
-				result = Number(array[i]) + result;
-			} else {
-				throw new Error("양수가 아닙니다");
-			}
-		}
-		return result;
-	}
-
 	async run() {
 		let regexes = {
-			check_special_chracter: RegExp(/[,;/\n]/, "g"),
-			check_empty_string: RegExp(/^$/),
+			check_special_chracter: RegExp(/[,;/\n]/, "g"), // 특수 문자 확인
+			check_empty_string: RegExp(/^$/), // 빈 문자열
 			check_front: /^\/\//,
 			normal_delimiter: RegExp(/[,:]/, "g"),
 			custom_delimiter: /[^//]/,
 		};
 
 		let input;
+
+		input = await Console.readLineAsync(",(쉼표)와 ;(세미 콜론)와 숫자를 조합한 문자열을 입력해주세요. ex)1,2:3 ");
+
 		let delimiter;
 		let reg;
 		let input_array;
-
-		input = await Console.readLineAsync(",(쉼표)와 ;(세미 콜론)와 숫자를 조합한 문자열을 입력해주세요. ex)1,2:3 ");
+		let result = 0;
 
 		try {
 			if (input.match(regexes.check_special_chracter) || input.match(regexes.check_empty_string)) {
@@ -40,15 +26,21 @@ class App {
 				if (is_using_custom_reg) {
 					delimiter = input.match(regexes.custom_delimiter)[0];
 					reg = RegExp(`[\\\\/\\sA-Za-z${delimiter}]`, "g");
+
 					input_array = input.split(reg);
+					for (let i = 0; i < input_array.length; i++) {
+						result = Number(input_array[i]) + result;
+					}
 				} else {
 					input_array = input.split(regexes.normal_delimiter);
+					for (let i = 0; i < input_array.length; i++) {
+						result = Number(input_array[i]) + result;
+					}
 				}
-				const answer = this.sumNumbers(input_array);
-				Console.print(`결과 : ${answer}`);
+				Console.print(`결과 : ${result}`);
 			}
-		} catch (error) {
-			throw new Error(`[ERROR] ${error.message}`);
+		} catch (e) {
+			throw new Error("[ERROR] 잘못된 문자열을 입력하셨습니다.");
 		}
 	}
 }
