7f30b1a70710a6c9cf633b32869c2e131f51f144
codingsiro <yunjin2768@naver.com>
2024-10-19 16:16:03 +0900

    feat: check and handle a input value

--- src/App.js
+++ src/App.js
@@ -2,35 +2,59 @@ import {readLineAsync, print} from "./MissionUtils.js";
 
 class App {
   async run() {
-    const answer = await readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    try {
+      const answer = await readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
-    if(answer === "") {
-      this.printResult(0);
+      if(answer === "") {
+        this.printResult(0);
+        return;
+      }
+
+      const isCustom = this.isCustomDelimiter(answer);
+      let customDelimiter = "";
+
+      if(isCustom) {
+        customDelimiter = this.getCustomDelimiter(answer);
+      } 
+
+      const delimiters = ":|,|" + customDelimiter;
+
+      if(this.checkErrorOfAnswer(answer, delimiters)) {
+        throw new Error("잘못된 값을 입력하였습니다. 다시 입력해주세요.");
+      }
+
+    } catch (error) {
+      print('[ERROR] : ' + error.message);
       return;
     }
-
-    const isCustom = this.isCustomDelimiter(answer);
-    let customDelimiter = "";
-
-    if(isCustom) {
-      customDelimiter = this.getCustomDelimiter(answer);
-    } 
-
-  }
-  
-  isCustomDelimiter(str) { 
-    const test = new RegExp(/^\/\/(.*?)\\n/,'g').test(str);
-    return str.includes("//") && str.includes("\\n") && test; 
-  }
-
-  getCustomDelimiter(str) {
-    return str.slice(str.indexOf("//") + 2, str.indexOf("\\n"));
+    
   }
 
   printResult(result) {
     print('결과 : ' + result);
   }
+
+  // 커스텀 여부 확인
+  isCustomDelimiter(str) { 
+    const test = new RegExp(/^\/\/(.*?)\\n/,'g').test(str);
+    return str.includes("//") && str.includes("\\n") && test; 
+  }
+
+  // 커스텀 구분자 추출
+  getCustomDelimiter(str) {
+    return str.slice(str.indexOf("//") + 2, str.indexOf("\\n"));
+  }
+
+
+  // 입력값 오류 확인
+  checkErrorOfAnswer(str, delimiters) {
+    let rst = str.replace(new RegExp(/^\/\/(.*?)\\n/,'g'), "");
+    rst = rst.replaceAll(new RegExp(`[0-9]|${delimiters}`, 'g'), "");
+    
+    return rst === "" ? false : true;
+  }
 }
+
 
 
 export default App;
