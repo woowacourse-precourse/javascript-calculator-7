f8eaad5b98d93604448c4befb047ec61fc1c3bae
2wndrhs <2wndrhs@gmail.com>
2024-10-19 21:42:08 +0900

    feat: 사용자에게 덧셈할 문자열을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const INPUT_MESSAGE = '덧셈할 문자열을 입력해 주세요.\n';
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE);
+    Console.print(input);
+  }
 }
 
 export default App;
