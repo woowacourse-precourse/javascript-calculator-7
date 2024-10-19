0f7ca7a4ea1cdaca58c78f1081eacc17a8dd50b5
eunji kang <miki2323@naver.com>
2024-10-19 20:02:33 +0900

    refactor: class 이름 뒤에 Api 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 
-async function getUserInputData() {
-  const userInputData = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n"); 
-  if (isEmpty(userInputData) === true) makeError("입력 오류가 발생했습니다.")
-  return userInputData;
-}
 
-function isEmpty(string) {
-  return string === null || string === undefined ? true : false;
-}
-
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
-
-function makeError(errorMsg) {
+function generateError(errorMsg) {
   throw new Error("[ERROR] " + errorMsg);
 }
 
 class DelimeterApi {
   delimeter = []
-  customFormatStart
-  customFormatEnd
+  customFormat = []
 
   constructor (delimeter1, delimeter2) {
     this.delimeter.push(delimeter1)
@@ -29,33 +15,39 @@ class DelimeterApi {
   }
 
   setCustomFormat(start, end) {
-    this.customFormatStart = start
-    this.customFormatEnd = end
+    this.customFormat.push(start)
+    this.customFormat.push(end)
   }
 
-  parseCustomDelimeter(userInputData) {
-    const customRegExp = new RegExp(`^${escapeRegExp(this.customFormatStart)}.${escapeRegExp(this.customFormatEnd)}`)
-    const parsedData = userInputData.match(customRegExp);
-    if (parsedData === null) return userInputData
+  parseCustomDelimeter(string) {
+    const escapedCustomFormat = this.customFormat.map(customFormat => this.escapeRegExp(customFormat));
+    const customRegExp = new RegExp(`^${escapedCustomFormat[0]}.${escapedCustomFormat[1]}`)
+
+    const parsedData = string.match(customRegExp);
+    if (parsedData === null) return string
   
     const customDelimeter = parsedData[0].charAt(2)
     if (isNaN(customDelimeter) === false) {
-      makeError("커스텀 구분자는 숫자가 아닌 문자로 설정되어야 합니다.")
+      generateError("커스텀 구분자는 숫자가 아닌 문자로 설정되어야 합니다.")
     }
 
     this.delimeter.push(customDelimeter);
-    return userInputData.replace(customRegExp, '')
+    return string.replace(customRegExp, '')
   }
 
+  escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+  
   getregExp() {
-    const escapedDelimeter = this.delimeter.map(delimeter => escapeRegExp(delimeter));
+    const escapedDelimeter = this.delimeter.map(delimeter => this.escapeRegExp(delimeter));
     const regExpString = escapedDelimeter.join('|');
 
     return new RegExp(regExpString, 'g')
   }
 }
 
-class NumberExtractor {
+class NumberExtractorApi {
   delimeterApi
   orgUserInputData
   userInputData
@@ -110,14 +102,21 @@ class CalculatorApi {
   }
 }
 
-function generateError(errorMsg) {
-  throw new Error("[ERROR] " + errorMsg);
+async function getUserInputData() {
+  const userInputData = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n"); 
+  if (isEmpty(userInputData) === true) generateError("입력 오류가 발생했습니다.")
+  return userInputData;
 }
+
+function isEmpty(string) {
+  return string === null || string === undefined ? true : false;
+}
+
 class App {
   async run() {
     const userInput = await getUserInputData();
 
-    const numberExtractor = new NumberExtractor(userInput);
+    const numberExtractor = new NumberExtractorApi(userInput);
     const numArr = numberExtractor.extractNumber();
 
     const calcultor = new CalculatorApi(numArr);
