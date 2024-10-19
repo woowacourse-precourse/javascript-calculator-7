8e55b8f9cc6f0be8cfe26dc7db431b640c708571
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-10-19 11:29:30 +0900

    feat: 문자열 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const USERINFO = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 }
 
 export default App;
