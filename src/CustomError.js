4d83a66bfa63efe7f49984214059e0595746ab61
DongjaJ <jdh981118@naver.com>
2024-10-20 00:53:30 +0900

    refactor: Error를 상속한 CustomError 객체의 생성자에서 부모클래스의 생성자를 호출하도록 수정

--- src/CustomError.js
+++ src/CustomError.js
@@ -2,7 +2,7 @@ import { ERROR_PREFIX } from "./constants.js";
 
 class CustomError extends Error {
   constructor(message) {
-    throw new Error(`${ERROR_PREFIX}${message}`);
+    super(`${ERROR_PREFIX}${message}`);
   }
 }
 
