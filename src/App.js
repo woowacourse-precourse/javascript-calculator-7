118aa9b2dcc68535131489eddaed4ea5ec68e070
CJY <cjy3458@naver.com>
2024-10-20 05:32:58 +0900

    fix: test code 형식에 맞게 에러 처리 방식 변경

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { Console } from "@woowacourse/mission-utils";
 const ERROR = {
   INVALID_INPUT: {
     name: "InvalidInputError",
-    message: "유효하지 않은 입력입니다. 기본 구분자가 필요합니다.",
+    message: "[ERROR] 유효하지 않은 입력입니다. 기본 구분자가 필요합니다.",
   },
   EMPTY_STRING: {
     name: "EmptyStringError",
-    message: "빈 문자열을 입력할 수 없습니다.",
+    message: "[ERROR] 빈 문자열을 입력할 수 없습니다.",
   },
   MISMATCHED_DELIMITER: {
     name: "MismatchedDelimiterError",
-    message: "입력한 구분자와 일치하지 않는 구분자가 포함되어 있습니다.",
+    message:
+      "[ERROR] 입력한 구분자와 일치하지 않는 구분자가 포함되어 있습니다.",
   },
   NOT_A_NUMBER: {
     name: "NotANumberError",
-    message: "숫자가 아닌 값이 포함되어 있습니다.",
+    message: "[ERROR] 숫자가 아닌 값이 포함되어 있습니다.",
   },
   NEGATIVE_NUMBER: {
     name: "NegativeNumberError",
-    message: "음수는 입력할 수 없습니다.",
+    message: "[ERROR] 음수는 입력할 수 없습니다.",
   },
 };
 
@@ -39,7 +40,8 @@ class App {
       const result = this.calculateSum(input);
       Console.print(`결과 : ${result}`);
     } catch (error) {
-      Console.print(`[ERROR] ${error.name}: ${error.message}`);
+      Console.print(`${error.message}(${error.name})`);
+      throw error;
     }
   }
 
