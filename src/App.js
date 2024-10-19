d6e315219bb9bdd08362cdbbdaba618d9ce58f35
young24n <won8560@gmail.com>
2024-10-19 20:15:51 +0900

    feat: 문자열을 구분자와 숫자 라인으로 분리하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
+  splitLine(text) {
+    const IS_HAVE_DELIMITER = /^\/\/.+\\n/;
+    let delimiter_line = '';
+    let number_line = '';
+
+    if (IS_HAVE_DELIMITER.test(text)) {
+      [delimiter_line, number_line] = text.split('\\n');
+      
+      return { delimiter_line, number_line };
+    }
+    number_line = text;
+
+    return { delimiter_line, number_line };
+  }
+  
   async run() {
-    try{
+    try {
       const INPUT_DATA = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-      // MissionUtils.Console.print(INPUT_DATA)
+      const result = this.splitLine(INPUT_DATA);
+      MissionUtils.Console.print(result);
       
     } catch (error) {
-
+      MissionUtils.Console.print(error);  
     }  
   }
 }
