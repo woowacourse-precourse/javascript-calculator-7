1cfdec3eee182e804828751041b94275d0a2c058
DodoJBNU <dh723@jbnu.ac.kr>
2024-10-19 20:01:11 +0900

    style: apply Prettier formatting based on 우아한 테크코스 Java Style Guide

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
-    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const {seperatorSet, filteredInput} = this.handleCustomSeperator(input);
-    const extractedNumbers = this.extractPositiveNumbers(filteredInput, ...seperatorSet);
-    const sum = this.sumArray(...extractedNumbers);
-    
-    this.print(sum);
-  }
+    async run() {
+        const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+        const { seperatorSet, filteredInput } = this.handleCustomSeperator(input);
+        const extractedNumbers = this.extractPositiveNumbers(filteredInput, ...seperatorSet);
+        const sum = this.sumArray(...extractedNumbers);
 
-  extractCustomSeperator(input){
-    const customSeperatorChunksRegex = /^(\/\/.\\n)+/g;
-    const customSeperatorRegex = /\/\/(.)\\n/g;
-
-    const customSeperatorChunks = customSeperatorChunksRegex.exec(input)?.[0];
-
-    if(customSeperatorChunks === null || customSeperatorChunks === undefined){
-      return {customSeperator: [], filteredInput: input};
+        this.print(sum);
     }
 
-    const customSeperatorMatch = customSeperatorChunks.match(customSeperatorRegex);
-    const customSeperator = customSeperatorMatch ? customSeperatorMatch.map((match) => match[2]) : [];
-    const filteredInput = input.slice(customSeperatorChunks.length);
+    extractCustomSeperator(input) {
+        const customSeperatorChunksRegex = /^(\/\/.\\n)+/g;
+        const customSeperatorRegex = /\/\/(.)\\n/g;
 
-    return {customSeperator, filteredInput};
-  }
+        const customSeperatorChunks = customSeperatorChunksRegex.exec(input)?.[0];
 
-  handleCustomSeperator(input){
-    const defaultSeperator = [':', ','];
-    const {customSeperator, filteredInput} = this.extractCustomSeperator(input);
-    const seperatorSet = new Set([...defaultSeperator, ...customSeperator]);
+        if (customSeperatorChunks === null || customSeperatorChunks === undefined) {
+            return { customSeperator: [], filteredInput: input };
+        }
 
-    return {seperatorSet, filteredInput};
-  }
+        const customSeperatorMatch = customSeperatorChunks.match(customSeperatorRegex);
+        const customSeperator = customSeperatorMatch ? customSeperatorMatch.map((match) => match[2]) : [];
+        const filteredInput = input.slice(customSeperatorChunks.length);
 
-  extractPositiveNumbers(input, ...seperatorArray){
-    const seperatorRegex = new RegExp(`[${seperatorArray.join('|')}]`, 'g');
-    const words = input.split(seperatorRegex);
+        return { customSeperator, filteredInput };
+    }
 
-    const extractedNumbers = words.map((word) => {
-      const number = Number(word);
+    handleCustomSeperator(input) {
+        const defaultSeperator = [":", ","];
+        const { customSeperator, filteredInput } = this.extractCustomSeperator(input);
+        const seperatorSet = new Set([...defaultSeperator, ...customSeperator]);
 
-      if(isNaN(number) || number <= 0){
-        throw new Error(`[ERROR] ${word}는 잘못된 값입니다.`);
-      }
-      return number;
-    })
+        return { seperatorSet, filteredInput };
+    }
 
-    return extractedNumbers;
-  }
+    extractPositiveNumbers(input, ...seperatorArray) {
+        const seperatorRegex = new RegExp(`[${seperatorArray.join("|")}]`, "g");
+        const words = input.split(seperatorRegex);
 
-  sumArray(...numbers){
-    return numbers.reduce((acc, cur) => acc + cur, 0);
-  }
+        const extractedNumbers = words.map((word) => {
+            const number = Number(word);
 
-  print(value){
-    MissionUtils.Console.print(`결과 : ${value}`);
-  }
+            if (isNaN(number) || number <= 0) {
+                throw new Error(`[ERROR] ${word}는 잘못된 값입니다.`);
+            }
+            return number;
+        });
+
+        return extractedNumbers;
+    }
+
+    sumArray(...numbers) {
+        return numbers.reduce((acc, cur) => acc + cur, 0);
+    }
+
+    print(value) {
+        MissionUtils.Console.print(`결과 : ${value}`);
+    }
 }
 
 export default App;
