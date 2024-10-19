7791289803ddb83ad81484cf3d03e00ad1afba50
@u_limit_ <yurim@ajou.ac.kr>
2024-10-19 17:55:33 +0900

    refactor: 연산자 주변에 띄어쓰기 추가

--- src/App.js
+++ src/App.js
@@ -1,66 +1,65 @@
-import {Console} from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 
 let separator = [",",":"];
 function findSeparator(input){
-  const INPUT_ARRAY=input.split("");
-  if(input.slice(0,2)==="//"){
-    const START = input.indexOf("//")+2;
+  const INPUT_ARRAY = input.split("");
+  if (input.slice(0,2) === "//") {
+    const START = input.indexOf("//") + 2;
     const END = input.indexOf("\\n");
-    if(START===1||END===-1){
+    if (START === 1 || END === -1) {
       throw new Error("[ERROR] 커스텀 구분자 추가를 위해선 //와 \\n 사이에 추가하고자 하는 구분자를 입력해주세요.")
-    }else{
+    } else {
       const customSeparator = input.slice(START, END);
       separator.push(customSeparator);
-      if(customSeparator.length>1||!isNaN(customSeparator)){
+      if (customSeparator.length > 1 || !isNaN(customSeparator)){
         throw new Error("[ERROR] 커스텀 구분자의 입력이 잘못됐습니다.")
-      }else{
-        separator.push(input.slice(customSeparator))
-        return INPUT_ARRAY.slice(END+2);
       }
+      separator.push(input.slice(customSeparator))
+      return INPUT_ARRAY.slice(END + 2);
     }
-  }else{
-    return INPUT_ARRAY;
   }
+  return INPUT_ARRAY;
 }
 
 function getNumber(inputArray) {
   let currentNum = '';
-  const RESULT=[];
-  let isContinuous=0;
+  const RESULT = [];
+  let isContinuous = 0;
   inputArray.forEach(string=>{
-    if(separator.includes(string)){
-      isContinuous+=1;
-      if(isContinuous>1){
+    if (separator.includes(string)) {
+      isContinuous += 1;
+      if (isContinuous > 1) {
         throw new Error("[ERROR] 구분자가 연속으로 사용됐습니다.")
       }
-      if(currentNum.length>0){
+      if (currentNum.length > 0) {
         RESULT.push(currentNum);
-        currentNum='';
+        currentNum = '';
       }
-    }else{
-      isContinuous=0;
+    } else {
+      isContinuous = 0;
       currentNum += string;
     }
-  })
-  if(currentNum !== ''){
+  });
+
+  if (currentNum !== '') {
     RESULT.push(currentNum);
   }
   return RESULT;
 }
 
 function calculate(input){
-  if(isNaN(input[input.length-1])){
+  if (isNaN(input[input.length - 1])) {
     throw new Error("[ERROR] 더할 숫자 없이 구분자만 입력됐습니다.")
   }
   let result = 0;
   getNumber(input).map((number)=>{
-    if(Number(number)<0){
+    if (Number(number) < 0) {
       throw new Error("[ERROR] 양수를 입력해주세요.")
     }
-    if(isNaN(number)){
+    if (isNaN(number)) {
       throw new Error("[ERROR] 올바른 숫자를 입력해주세요.")
     }
-    result+=Number(number);
+    result += Number(number);
   })
   return result;
 }
@@ -70,8 +69,8 @@ class App {
   ) {
     let input="";
     Console.print("덧셈할 문자열을 입력해 주세요.");
-    input=await Console.readLineAsync(input);
-    Console.print("결과 : "+calculate(findSeparator(input)));
+    input = await Console.readLineAsync(input);
+    Console.print("결과 : " + calculate(findSeparator(input)));
   }
 }
 
