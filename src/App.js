7a12d2dc178354cf04c37d34ee54661b85d2b9e5
jsy <suyeonb1db@gmail.com>
2024-10-19 20:05:09 +0900

    feat: 문자열 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+async function getText() {
+  try {
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+    console.log(input);
+    return input;
+  } catch (error) {
+  }
+};
+
 class App {
-  async run() {}
+  async run() {
+    const input = await getText();
+  }
 }
 
 export default App;
