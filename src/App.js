1ab0e53eed8aa77d8cdde7c39946abc886b5710f
xxxjinn <tlsguswls319@naver.com>
2024-10-20 01:25:47 +0900

    refactor(App): 변수명 수정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const INPUT_MESSAGE = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    let ANSWER=999999;
-    if(INPUT_MESSAGE==="") ANSWER=0;
+    let answer=999999;
+    if(INPUT_MESSAGE==="") answer=0;
 
-    Console.print(`결과 : ${ANSWER}`)
+    Console.print(`결과 : ${answer}`)
   }
 }
 
