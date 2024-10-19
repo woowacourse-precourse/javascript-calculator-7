58c2ceee05229abe9ce423b57c44a58c7938f2eb
Eunjin Cho <whdmswls9207@gmail.com>
2024-10-19 23:03:01 +0900

    feat: 문자열 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    //문자열 입력받기
+    const inputString = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+  }
 }
 
 export default App;
