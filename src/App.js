d4e59d667f0a31d2d71bba1dcf480b2c9ef57cad
@u_limit_ <yurim@ajou.ac.kr>
2024-10-19 18:21:40 +0900

    refactor: 화살표 함수 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 
 let separator = [",",":"];
-function findSeparator(input){
+
+const findSeparator = (input) => {
   const INPUT_ARRAY = input.split("");
   if (input.slice(0,2) === "//") {
-    const START = input.indexOf("//") + 2;
+    const START = input.indexOf("//");
     const END = input.indexOf("\\n");
     if (START === 1 || END === -1) {
-      throw new Error("[ERROR] 커스텀 구분자 추가를 위해선 //와 \\n 사이에 추가하고자 하는 구분자를 입력해주세요.")
+      throw new Error("[ERROR] 커스텀 구분자 추가를 위해선 //와 \\n 사이에 추가하고자 하는 구분자를 입력해주세요.");
     } else {
       const customSeparator = input.slice(START, END);
       separator.push(customSeparator);
@@ -21,15 +22,15 @@ function findSeparator(input){
   return INPUT_ARRAY;
 }
 
-function getNumber(inputArray) {
+const getNumber = (inputArray) => {
   let currentNum = '';
   const RESULT = [];
   let isContinuous = 0;
-  inputArray.forEach(string=>{
+  inputArray.forEach((string) => {
     if (separator.includes(string)) {
       isContinuous += 1;
       if (isContinuous > 1) {
-        throw new Error("[ERROR] 구분자가 연속으로 사용됐습니다.")
+        throw new Error("[ERROR] 구분자가 연속으로 사용됐습니다.");
       }
       if (currentNum.length > 0) {
         RESULT.push(currentNum);
@@ -47,17 +48,17 @@ function getNumber(inputArray) {
   return RESULT;
 }
 
-function calculate(input){
+const calculate = (input) => {
   if (isNaN(input[input.length - 1])) {
-    throw new Error("[ERROR] 더할 숫자 없이 구분자만 입력됐습니다.")
+    throw new Error("[ERROR] 더할 숫자 없이 구분자만 입력됐습니다.");
   }
   let result = 0;
   getNumber(input).map((number)=>{
     if (Number(number) < 0) {
-      throw new Error("[ERROR] 양수를 입력해주세요.")
+      throw new Error("[ERROR] 양수를 입력해주세요.");
     }
     if (isNaN(number)) {
-      throw new Error("[ERROR] 올바른 숫자를 입력해주세요.")
+      throw new Error("[ERROR] 올바른 숫자를 입력해주세요.");
     }
     result += Number(number);
   })
