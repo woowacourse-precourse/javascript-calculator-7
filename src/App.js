638a692133942e97e52dd7c3174a6296b9520d94
Mina Lee <mal0070@naver.com>
2024-10-19 11:24:41 +0900

    feat: 문자열 분리: 커스텀 구분자

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ class App {
   
     try{
       //문자열 분리
-      const sep = input.split((/,|:/));
-      Console.print(sep);
+      //쉼표 또는 콜론
+      if((/,|:/).test(input)){
+        const sep = input.split((/,|:/));
+        Console.print(sep);
+      } 
+
+      //커스텀 구분자
+      else if((/\/\/(.*?)\\n/).test(input)){
+        //커스텀 구분자 추출
+        const matchStr = input.match(/\/\/(.*?)\\n/);
+        const customChar = matchStr[1];
+
+        //분리할 문자열
+        const str = input.replace(/\/\/.*?\\n/,'');
+        const sep = str.split(customChar);
+        Console.print(sep);
+      } 
+      
+      else {
+        throw new Error();
+      }
     } catch (err) {
       Console.print(err);
       Console.print("[ERROR] 잘못된 값을 입력하셨습니다. 앱을 다시 실행해주세요.");
