ebdaa185334c65c1f31ccf12753b4cde55930ea0
seungJoo <sj10230506@gmail.com>
2024-10-19 20:33:33 +0900

    feat: 구분자 정하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,12 +7,32 @@ class App {
 
       let input = await Console.readLineAsync("");
 
+      if(input.length === 0){ //구분자 구할필요 없이 그냥 정답이 0임 
+        return 0;
+      }
+      let seperator = this.getSeperator(input);
+
+      Console.print(seperator);
+      
     }
     catch(err){
       Console.print(err.message);
       return;
     }
 
+  }
+
+  getSeperator(input){
+    if (input.startsWith("//")) {
+      const match = input.match(/\/\/(.+)\\n/);  // "//"와 "\n" 사이의 커스텀 구분자 추출
+      if (match === null) {
+        throw new Error("[ERROR] 잘못된 입력입니다. 커스텀 구분자를 읽을 수 없습니다.");
+      }
+      return new RegExp(`[${match[1]}]`); // 커스텀 구분자를 정규식으로 변환
+    }
+
+    // 커스텀 구분자가 없는 경우, 기본 구분자를 반환
+    return /[,:]/;
   }
 }
 
