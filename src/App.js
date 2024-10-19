bf34da23072241a6411f2c892d3ca37bd6ff052c
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-19 22:56:52 +0900

    refactor(app): Calculator 클래스 분리로 코드 간소화 및 App 클래스는 호출과 출력만 담당하도록 개선

--- src/App.js
+++ src/App.js
@@ -1,76 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
-import { checkForNaN } from './utils/checkForNaN.js';
-import { hasNegative } from './utils/hasNegative.js';
-import { isEmptyOrNull } from './utils/isEmptyOrNull.js';
-import { splitByCustomSeparator } from './utils/splitByCustomSeparator.js';
-import { splitByDefaultSeparators } from './utils/splitByDefaultSeparators.js';
+import Calculator from './Calculator.js';
+import { throwError } from './utils/throwError.js';
 
 class App {
-	#input = '';
-	#result = 0;
-
 	async run() {
 		try {
-			await this.#getUserInput();
-			this.#validateInput();
-			this.#parseAndSplitInput();
-			this.#validateSplitResult();
-			this.#result = this.#calculateSum(this.#result);
-			this.#printResult();
+			const input = await this.#getUserInput();
+			const calculator = new Calculator(input);
+			const sum = calculator.calculate();
+			this.#printResult(sum);
 		} catch (err) {
-			this.#throwError(err);
+			throwError(err);
 		}
 	}
 
 	async #getUserInput() {
-		this.#input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+		return Console.readLineAsync(`덧셈할 문자열을 입력해 주세요.\n`);
 	}
 
-	#validateInput() {
-		if (isEmptyOrNull(this.#input)) {
-			this.#result = 0;
-			return;
-		}
-		if (hasNegative(this.#input)) {
-			this.#throwError('[ERROR] : 양수만 입력할 수 있어요.');
-		}
-	}
-
-	#validateSplitResult() {
-		if (this.#result[0] === this.#input) {
-			this.#throwError('[ERROR] : 구분자와 양수로 이루어진 값을 입력해주세요.');
-		}
-		if (checkForNaN(this.#result)) {
-			this.#throwError('[ERROR] : 입력값에 숫자 이외의 값이 포함되어 있어요.');
-		}
-	}
-
-	#parseAndSplitInput() {
-		if (this.#input.startsWith('//')) {
-			const result = splitByCustomSeparator(this.#input);
-			if (result === null) {
-				this.#throwError('[ERROR] : 커스텀 구분자를 확인할 수 없어요.');
-			}
-			this.#result = result;
-		} else {
-			this.#result = splitByDefaultSeparators(this.#input);
-		}
-	}
-
-	#calculateSum(result) {
-		let sum = 0;
-		for (const v of result) {
-			sum += +v;
-		}
-		return sum;
-	}
-
-	#printResult() {
-		Console.print(`결과 : ${this.#result}`);
-	}
-
-	#throwError(err) {
-		throw new Error(err);
+	#printResult(result) {
+		Console.print(`결과 : ${result}`);
 	}
 }
 
