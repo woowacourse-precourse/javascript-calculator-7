4896c47b1d2a543552231e68eee45c4f172d8b86
gunyongbok <qhrrjsdyd123@gmail.com>
2024-10-19 16:13:35 +0900

    fix: 올바르지 않은 구분자 사용시 예외 처리 수정 커스텀 구분자를 선언할때 사용하는 // 는 예외 처리하여 에러에 걸리지 않게 처리

--- src/common/message.js
+++ src/common/message.js
@@ -1,7 +1,6 @@
 export const StartMessage = "덧셈할 문자열을 입력해 주세요.";
 
 export const ErrorMessage = {
-  NOT_CORRECT_INPUT_MESSAGE:
-    "[ERROR] 입력값은 구분자와 양수로만 이루어져 있어야합니다.",
-  HAS_MORE_THAN_TWO_NON_NUMBERS_MESSAGE: "[ERROR] 잘못된 구분자 사용입니다.",
+  NOT_CORRECT_INPUT_MESSAGE: "[ERROR] 올바르지 못한 입력값 입니다.",
+  NOT_CORRECT_SEPARATOR_USE_MESSAGE: "[ERROR] 잘못된 구분자 사용입니다.",
 };
--- src/controller/input/validateInput.js
+++ src/controller/input/validateInput.js
@@ -1,5 +1,6 @@
 import { ErrorMessage } from "../../common/message.js";
 import validateCorrectInput from "../../validator/validateCorrectInput.js";
+import validateSeparteUse from "../../validator/validateSeparateUse.js";
 import extractNumbersFromInput from "./extractNumbersFromInput.js";
 
 const validateInput = (input) => {
@@ -7,6 +8,9 @@ const validateInput = (input) => {
 
   if (!validateCorrectInput(numbers))
     throw new Error(ErrorMessage.NOT_CORRECT_INPUT_MESSAGE);
+
+  if (!validateSeparteUse(input))
+    throw new Error(ErrorMessage.NOT_CORRECT_SEPARATOR_USE_MESSAGE);
 };
 
 // validateInput("1:2,0");
