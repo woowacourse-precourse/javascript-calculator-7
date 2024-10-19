ecf55d3517c237733fe2e005f0701b4cd185726c
singyuKang <rkdtlscks123@naver.com>
2024-10-19 18:49:05 +0900

    feat : InputView , CalculatorGenerator, OutputView 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import CalculatorGenerator from "./CalculatorGenerator.js";
+import OutputView from "./OutputView.js";
+import InputView from "./InputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const userInputString = await InputView.getUserInput();
+      const result = await CalculatorGenerator.getResult(userInputString);
+      OutputView.printResult(result);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
