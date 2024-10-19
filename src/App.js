3474cd9f8fbfc5ca8e4d3c11f906e7a5e8019beb
neaimt <pas120344@gmail.com>
2024-10-19 17:02:54 +0900

    feat: 프롬프트 문자열 입력 기능
     '덧셈할 문자열을 입력해 주세요.' 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// Console API 
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n'); // 1.프롬프트 입력 기능
+      Console.print(`결과 : ${input}`); 
+    } catch(err) {
+      throw new Error('[ERROR]')
+    }
+  }
 }
 
 export default App;
