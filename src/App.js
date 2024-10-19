ca768cbdead3392562382b9375763d0ac43d71dd
jeonhyeonseon <goodpretty0325@gmail.com>
2024-10-19 14:35:19 +0900

    feat: 문자열 입력 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+
+    // 1. 문자열 입력받기
+    Console.print("덧셈할 문자열을 입력해주세요.");
+    const strInput = await Console.readLineAsync(" ");
+    
+    // 1-2. 입력된 문자열이 null 또는 빈문자열이라면, 0을 반환한다.
+    if (strInput === "" || strInput === null) {
+      console.log(0);
+      return;
+    }
+  }
 }
 
 export default App;
