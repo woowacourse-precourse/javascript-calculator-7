5d4b22a3ebc0986a079336adf3445f9a89e7b2e2
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-19 14:00:49 +0900

    feat : 입력한 값에서 기본연산자를 제외하고 숫자 추출하고 출력하는 기능추가, 기본구분자에 해당하는 문자가 있는 경우 오류출력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  
   async run() {
+    //기본연산자
+    const basicOperators = [",",":"];
     const inputStr = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    Console.print(inputStr);
+    
+    //문자열에서 기본연산자 추출 => 기댓값 : [1,2,3]
+    const nums = extractNums(inputStr,basicOperators);
+    
+    Console.print(nums);
   }
+}
+
+function extractNums(inputStr,basicOperators){
+  const nums = [];
+  for (const item of inputStr){
+    if (basicOperators.includes(item)){
+      continue;
+    }
+    if (!isNaN(item)){
+      nums.push(Number(item));
+    }
+    else{
+       throw Error('[ERROR] : 잘못된 문자가 입력되었습니다.');;
+    }
+  }
+  return nums;
 }
 
 export default App;
