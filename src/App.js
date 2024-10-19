b1ef2a49bd1ea490ed8280d6555e822ed517193e
ljs831 <ljsljs2580@gmail.com>
2024-10-20 01:23:43 +0900

    feat : 입력받은 값을 구분자를 기준으로 숫자 분리해 저장하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
-
+const separators = [",", ":"];
+const regex = new RegExp(separators.map((s) => `\\${s}`).join("|"), "g");
 class App {
   async run() {
     try {
       const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+      const numArr = input.split(regex).map(Number);
+      const sum = 0;
+      Console.print(sum);
     } catch (error) {
       Console.print(error);
     }
