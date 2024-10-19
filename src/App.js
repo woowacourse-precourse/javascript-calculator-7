9ae310ef435e2863fb568e9548a9b38d2f5760bf
seungJoo <sj10230506@gmail.com>
2024-10-19 20:58:38 +0900

    feat: 커스텀 구분자 설정 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,37 +3,36 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
 
-    try{
+    try {
 
       let input = await Console.readLineAsync("");
 
-      if(input.length === 0){ //구분자 구할필요 없이 그냥 정답이 0임 
-        return 0;
+      let delimiter;
+      if (input.length > 0 && input.startsWith('//')) { //커스텀 구분자가 지정된 경우
+        delimiter = this.getDelimiter(input);
+        input = input.split('\\n')[1];
       }
-      let seperator = this.getSeperator(input);
+      else{  //기본 구분자를 사용하는 경우
+        delimiter = /[,:]/;
+      }
 
-      Console.print(seperator);
-      
+
     }
-    catch(err){
+    catch (err) {
       Console.print(err.message);
       return;
     }
 
   }
 
-  getSeperator(input){
-    if (input.startsWith("//")) {
-      const match = input.match(/\/\/(.+)\\n/);  // "//"와 "\n" 사이의 커스텀 구분자 추출
-      if (match === null) {
-        throw new Error("[ERROR] 잘못된 입력입니다. 커스텀 구분자를 읽을 수 없습니다.");
-      }
-      return new RegExp(`[${match[1]}]`); // 커스텀 구분자를 정규식으로 변환
+  getDelimiter(input) {
+    const match = input.match(/\/\/(.+)\\n/);  // "//"와 "\n" 사이의 커스텀 구분자 추출
+    if (match === null) {
+      throw new Error("[ERROR] 잘못된 입력입니다. 커스텀 구분자를 읽을 수 없습니다.");
     }
-
-    // 커스텀 구분자가 없는 경우, 기본 구분자를 반환
-    return /[,:]/;
+    return new RegExp(`[${match[1]}]`); // 커스텀 구분자를 정규식으로 변환
   }
+
 }
 
 export default App;
