5ae711384dbaa4dbea8134166a95830cc72834ce
Eunicekk <cho_lyn@naver.com>
2024-10-19 21:48:49 +0900

    [feat] 문자열 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+	async run() {
+		// 1. 문자열 입력 기능 구현
+		Console.print("덧셈할 문자열을 입력해 주세요.");
+		const input = await Console.readLineAsync("");
+		Console.print(input);
+	}
 }
 
 export default App;
