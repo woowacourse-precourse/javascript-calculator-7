c340acb6b0e7d2f1a2227cd017c64cd6248e68c9
codingsiro <yunjin2768@naver.com>
2024-10-19 15:34:12 +0900

    feat: add basic featrue for input and output

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import {readLineAsync, print} from "./MissionUtils.js";
+
 class App {
-  async run() {}
+  async run() {
+    const answer = await readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    if(answer === "") {
+      this.printResult(0);
+      return;
+    }
+  }
+
+  printResult(result) {
+    print('결과 : ' + result);
+  }
 }
+
 
 export default App;
