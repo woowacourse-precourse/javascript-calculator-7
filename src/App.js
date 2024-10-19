7be26631cb2f948cceda491f0d6c9a55e0563b7a
yena <rladpsk1117@gmail.com>
2024-10-19 17:06:04 +0900

    feat: 문자열 입출력 기능 구현
    @woowacourse/mission-utils 라이브러리 import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-class App {
-  async run() {}
-}
+import { Console } from '@woowacourse/mission-utils';
 
+class App {
+  async run() {
+    const input = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+    try {
+    } catch (error) {
+      Console.print(`[ERROR] ${error}`);
+    }
+  }
+}
 export default App;
