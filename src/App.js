b3042d640c8d5ee69d8746e1b8a192d7e79710d8
dongmin <dongmin11566@gmail.com>
2024-10-19 21:46:10 +0900

    feat(run): 합산 결과를 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const result = this.add(input);
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const result = this.add(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+
   }
 
   add(numbers) {
