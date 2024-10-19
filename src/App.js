2c8d585776ac1360f4dc3f93460223fc154a8ef8
Lim-shweety <smile70200@naver.com>
2024-10-20 00:57:14 +0900

    feat(app.js): 입력받기
    입력 전 문구 출력 후 입력받아서 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import {Console, MissionUtils} from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    let inputStr= await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+  }
 }
 
 export default App;
+
