331381681a50ddcd6fd5b393ce24649321bac4f8
Jinny <ggab.challenge@gmail.com>
2024-10-20 03:02:52 +0900

    refactor(string): 결과 출력 값을 template strings 형식으로 변경

--- src/View/exportUserOutput.js
+++ src/View/exportUserOutput.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 // TO DO: userOutput -> exportUserOutput / 함수명은 동작 + ~ /
 const exportUserOutput = totalNumberSum => {
-  Console.print('결과 : ' + totalNumberSum);
+  Console.print(`결과 : ${totalNumberSum}`);
 };
 
 export default exportUserOutput;
