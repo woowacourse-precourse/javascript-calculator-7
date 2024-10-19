458e574c19f806dbcbcd69fab9a80f7ad5c461c4
yena <rladpsk1117@gmail.com>
2024-10-19 20:39:16 +0900

    feat: 커스텀 구분자 분리 기능 구현

--- src/App.js
+++ src/App.js
@@ -11,15 +11,25 @@ class App {
         Console.print('결과 : 0 ');
       }
 
+      // 커스텀 구분자
+      if (input.startsWith('//') && input.substring(3, 5) == '\\n') {
+        const delimiter = input.charAt(2);
+        const result = input.substring(5).split(delimiter);
+        let hap = 0;
+        result.forEach((item) => {
+          hap += parseInt(item);
+        });
+
+        Console.print(`결과 : ${hap}`);
+      }
+
       // 기본 구분자
-      let sum = 0;
       const result = input.split(/,|;/g);
+      let sum = 0;
       result.forEach((item) => {
         sum += parseInt(item);
       });
       Console.print(`결과 : ${sum}`);
-
-      // 커스텀 구분자
     } catch (error) {
       Console.print(`[ERROR] ${error}`);
     }
