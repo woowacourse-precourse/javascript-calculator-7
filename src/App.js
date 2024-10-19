483c4a45e87e48501b331bb0c163ce8d6b67a0aa
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-20 04:52:31 +0900

    feat: 문자열 입력 검증을 위한 validateInput 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  validateInput(input) {
+    const sep = /[^0-9,:]/;
+    const customSep = /^\/\/\D\\n/;
+
+    if (!sep.test(input)) {
+      return 0;
+    } else if (customSep.test(input)) {
+      const pattern = new RegExp(`[^0-9${input[2]}]`);
+      if (!pattern.test(input.slice(5))) {
+        return 1;
+      }
+    }
+
+    return -1;
+  }
+
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
   }
