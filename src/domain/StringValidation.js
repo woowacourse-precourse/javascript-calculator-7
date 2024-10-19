5d03935a4aaab198638849281909aca2b7992c9a
sooyeoniya <tndus1306403@naver.com>
2024-10-19 19:00:06 +0900

    refactor: StringValidation 클래스 내부 함수 분리

--- src/domain/StringValidation.js
+++ src/domain/StringValidation.js
@@ -2,47 +2,72 @@ import { ERROR_CODES, VALID_STRING_CODE } from "../constants/constants.js";
 
 class StringValidation {
   static validate(inputValue) {
-    if (inputValue === '')
-      return ERROR_CODES.EMPTY_STRING;
+    if (this.#isEmpty(inputValue)) return ERROR_CODES.EMPTY_STRING;
+    if (this.#hasLeadingOrTrailingInvalidCharacter(inputValue)) return ERROR_CODES.LEADING_TRAILING_CHARACTER;
+    if (this.#hasCustomDelimiter(inputValue)) {
+      const customDelimiterValidationResult = this.#customDelimiterValidationResult(inputValue);
+      if (customDelimiterValidationResult) return customDelimiterValidationResult;
+    }
 
-    if ((!inputValue.startsWith('//') && (isNaN(inputValue[0])) || inputValue[0] === ' ') 
-      || (isNaN(inputValue[inputValue.length - 1]) || inputValue[inputValue.length - 1] === ' '))
-      return ERROR_CODES.LEADING_TRAILING_CHARACTER;
+    const { parsedValue, delimiters } = this.#extractParsedValueAndDelimiters(inputValue);
+    if (this.#hasUndefinedDelimiter(parsedValue, delimiters)) return ERROR_CODES.UNDEFINED_DELIMITER;
+    if (this.#hasConsecutiveDelimiters(parsedValue, delimiters)) return ERROR_CODES.CONSECUTIVE_DELIMITERS;
 
-    if (inputValue.startsWith('//') && !inputValue.includes('\\n'))
-      return ERROR_CODES.INVALID_CUSTOM_DELIMITER;
+    return VALID_STRING_CODE;
+  }
 
-    if (inputValue.startsWith('//') && inputValue.substring(2, inputValue.indexOf('\\n')) === '')
-      return ERROR_CODES.EMPTY_CUSTOM_DELIMITER;
+  static #isEmpty(inputValue) {
+    return inputValue === '';
+  }
 
-    if (inputValue.startsWith('//') && inputValue.substring(2, inputValue.indexOf('\\n')).length >= 2)
-      return ERROR_CODES.MULTIPLE_CUSTOM_DELIMITERS;
+  static #hasCustomDelimiter(inputValue) {
+    return inputValue.startsWith('//');
+  }
 
-    if (inputValue.startsWith('//') && (!isNaN(inputValue.substring(2, inputValue.indexOf('\\n'))) 
-      && inputValue.substring(2, inputValue.indexOf('\\n')) !== ' '))
-      return ERROR_CODES.NUMERIC_CUSTOM_DELIMITER;
+  static #hasLeadingOrTrailingInvalidCharacter(inputValue) {
+    const firstChar = inputValue[0];
+    const lastChar = inputValue[inputValue.length - 1];
+    return (!this.#hasCustomDelimiter(inputValue) 
+    && (isNaN(firstChar) || firstChar === ' ')) 
+    || (isNaN(lastChar) || lastChar === ' ');
+  }
 
-    if (inputValue.startsWith('//') && (isNaN(inputValue[inputValue.indexOf('\\n') + 2])) 
-      || inputValue[inputValue.indexOf('\\n') + 2] === ' ')
-      return ERROR_CODES.INVALID_CHARACTER_AFTER_CUSTOM_DELIMITER;
+  static #customDelimiterValidationResult(inputValue) {
+    const customDelimiter = inputValue.substring(2, inputValue.indexOf('\\n'));
+    const afterDelimiterChar = inputValue[inputValue.indexOf('\\n') + 2];
 
+    if (!inputValue.includes('\\n')) return ERROR_CODES.INVALID_CUSTOM_DELIMITER;
+    if (customDelimiter === '') return ERROR_CODES.EMPTY_CUSTOM_DELIMITER;
+    if (customDelimiter.length >= 2) return ERROR_CODES.MULTIPLE_CUSTOM_DELIMITERS;
+    if (!isNaN(customDelimiter) && customDelimiter !== ' ') return ERROR_CODES.NUMERIC_CUSTOM_DELIMITER;
+    if (isNaN(afterDelimiterChar) || afterDelimiterChar === ' ') return ERROR_CODES.INVALID_CHARACTER_AFTER_CUSTOM_DELIMITER;
+    return null;
+  }
+
+  static #extractParsedValueAndDelimiters(inputValue) {
     let parsedValue = inputValue;
     const delimiters = [',', ':'];
 
-    if (inputValue.startsWith('//')) {
+    if (this.#hasCustomDelimiter(inputValue)) {
       parsedValue = inputValue.substring(inputValue.indexOf('\\n') + 2);
-      delimiters.push(inputValue.substring(2, inputValue.indexOf('\\n')));
+      const customDelimiter = inputValue.substring(2, inputValue.indexOf('\\n'));
+      delimiters.push(customDelimiter);
     }
+    return { parsedValue, delimiters: this.#escapeDelimiters(delimiters) };
+  }
 
-    const escapedDelimiters = delimiters.map(delimiter => delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+  static #escapeDelimiters(delimiters) {
+    return delimiters.map(delimiter => delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+  }
 
-    const invalidCharacterRegex = new RegExp(`[^0-9${escapedDelimiters.join('')}]`);
-    if (invalidCharacterRegex.test(parsedValue)) return ERROR_CODES.UNDEFINED_DELIMITER;
+  static #hasUndefinedDelimiter(parsedValue, delimiters) {
+    const invalidCharacterRegex = new RegExp(`[^0-9${delimiters.join('')}]`);
+    return invalidCharacterRegex.test(parsedValue);
+  }
 
-    const consecutiveDelimiterRegex = new RegExp(`([${escapedDelimiters.join('')}]){2,}`);
-    if (consecutiveDelimiterRegex.test(parsedValue)) return ERROR_CODES.CONSECUTIVE_DELIMITERS;
-
-    return VALID_STRING_CODE;
+  static #hasConsecutiveDelimiters(parsedValue, delimiters) {
+    const consecutiveDelimitersRegex = new RegExp(`([${delimiters.join('')}]){2,}`);
+    return consecutiveDelimitersRegex.test(parsedValue);
   }
 }
 
