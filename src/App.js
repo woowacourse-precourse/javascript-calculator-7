0b6547d5f89febd7d521d6a29af6479fc53a6526
Hyori <hyoring030@gmail.com>
2024-10-19 18:45:38 +0900

    feat: add basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-class App {
-  async run() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Calculator from "./Calculator.js";
 
+class App {
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+  
+    const calculator = new Calculator();
+    const result = calculator.calculate(input);
+    
+    MissionUtils.Console.print(`결과 : ${result}`);
+  }
+}
 export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Calculator {
+    calculate(string) {
+      try {
+        return string+string;
+      } catch (error) {
+      }
+    }
+  }
+  
+export default Calculator;
+  
+
