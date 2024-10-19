2ae60915b87cdf425432b949bb112a5137ff35eb
ShinjungOh <tlswjd9260@gmail.com>
2024-10-19 17:58:16 +0900

    refactor: 입력값 파싱 및 계산 로직을 메소드로 분리

--- src/App.js
+++ src/App.js
@@ -1,59 +1,70 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import {MissionUtils} from "@woowacourse/mission-utils";
 
 class App {
-  async run() {
-      const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-      let inputString = input.toString();
-      const separators = [",", ":"];
+    constructor() {
+        this.separators = [",", ":"];
+    }
 
-      // 아무 것도 입력하지 않았을 경우(""), 0으로 처리하기
-      if (input === "") {
-          return MissionUtils.Console.print('결과 : 0');
-      }
+    async run() {
+        const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+        let inputString = input.toString().trim();
 
-      //  문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 지정하기
-      if (inputString[0] === '/' && inputString[1] === '/') {
-          const searchLastWord = inputString.indexOf('\\n');
-          if (searchLastWord > 0) {
-              const customSeparator = inputString.slice(2, searchLastWord);
-              separators.push(customSeparator);
-              inputString = inputString.substring(searchLastWord + 2);
-          }
-      }
+        const {value, separators} = this.parseInput(inputString);
+        const result = this.calculate(value, separators);
+        return MissionUtils.Console.print(`결과 : ${result}`);
+    }
 
-      const inputArray = [...inputString];
-      const numArray = [];
+    parseInput(input) {
+        if (input[0] === '/' && input[1] === '/') {
+            const searchLastWord = input.indexOf('\\n');
+            if (searchLastWord > 0) {
+                const customSeparator = input.slice(2, searchLastWord);
+                const newSeparator = [...this.separators, customSeparator];
+                return {
+                    value: input.substring(searchLastWord + 2),
+                    separators: newSeparator,
+                }
+            }
+        }
+        return {
+            value: input,
+            separators: this.separators,
+        }
+    }
 
-      let string = '';
-      let isPrevSeparator = true;
-      let numArrayIndex = 0;
+    calculate(value, separators) {
+        if (value === '') {
+            return 0;
+        }
 
-      // 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 경우, 구분자를 기준으로 숫자 추출하기
-      inputArray.forEach((char, index) => {
-          if (!(isNaN(char))) { // 숫자
-              isPrevSeparator = false;
-              string = string + char;
-              numArrayIndex++;
-              if (index === inputArray.length -1) {
-                  numArray.push(string);
-              }
-          }
-          if (separators.includes(char)) { // 구분자
-              numArray.push(string);
-              string = '';
-              isPrevSeparator = true;
-          } else if (!separators.includes(char) && isNaN(char)) {
-              throw Error('[ERROR] 올바르지 않은 연산자를 사용했습니다.');
-          }
-      });
+        const valueArray = [...value];
+        let currentNum = '';
+        let isPrevSeparator = true;
+        let numArrayIndex = 0;
+        const numArray = [];
 
-      // 분리한 각 숫자의 합을 반환하기
-      const result = numArray
-          .map((str) => parseInt(str,10))
-          .reduce((acc, curr) => acc+ curr, 0);
+        valueArray.forEach((char, index) => {
+            if (!(isNaN(char))) {
+                isPrevSeparator = false;
+                currentNum += char;
+                numArrayIndex++;
+                if (index === valueArray.length - 1) {
+                    numArray.push(currentNum);
+                }
+            }
+            if (separators.includes(char)) {
+                numArray.push(currentNum);
+                currentNum = '';
+                isPrevSeparator = true;
+            } else if (!separators.includes(char) && isNaN(char)) {
+                throw Error('[ERROR] 올바르지 않은 연산자를 사용했습니다.');
+            }
+        });
 
-      return MissionUtils.Console.print(`결과 : ${result}`);
-  }
+        return numArray
+            .map((str) => parseInt(str, 10))
+            .reduce((acc, curr) => acc + curr, 0);
+    }
 }
 
 export default App;
