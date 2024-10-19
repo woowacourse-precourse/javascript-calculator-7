7624871a7d59ff75501c861396590dd9176ff7f1
hamo-o <yheel369@gmail.com>
2024-10-19 21:22:22 +0900

    feat(InvalidCustomSeparatorError,InputTest): 에러메시지 출력

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -156,4 +156,12 @@ describe("에러메시지 출력", () => {
       expect(error.message).toBe("[ERROR] 입력 값에 등록되지 않은 구분자가 포함되어 있습니다.\n 문자옆 앞부분의 '//'와 '\\n' 사이에 커스텀 구분자를 입력하거나, 덧셈할 문자열에 구분자와 양수만 포함되도록 입력해주세요.");
     }
   });
+
+  test("InvalidCustomSeparatorError 에러 발생 시", () => {
+    try {
+      throw new InvalidCustomSeparatorError();
+    } catch (error) {
+      expect(error.message).toBe("[ERROR] 커스텀 구분자는 문자로 입력해주세요.");
+    }
+  });
 });
--- src/Error/InvalidCustomSeparatorError.js
+++ src/Error/InvalidCustomSeparatorError.js
@@ -4,6 +4,7 @@ class InvalidCustomSeparatorError extends ValidationError {
   constructor() {
     super();
     this.name = "InvalidCustomSeparatorError";
+    this.message = "[ERROR] 커스텀 구분자는 문자로 입력해주세요.";
   }
 }
 
