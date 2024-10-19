eb3fa115decc5b3b8c50968ff81760e40d9300c5
neaimt <pas120344@gmail.com>
2024-10-19 17:06:26 +0900

    feat: 커스텀 구분자 확인 기능
    문자열이 '//'로 시작하는지 확인
    '\n'을 기준으로 문자 분리
    커스텀 구분자 반환

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ class App {
   async run() {
     try {
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n'); // 1.프롬프트 입력 기능
+      const result = this.calculate(input);
       Console.print(`결과 : ${input}`); 
     } catch(err) {
       throw new Error('[ERROR]')
     }
+  }
+
+  // 문자열 덧셈 계산기
+  calculate(input) {
+    if (!input) {
+      return 0;
+    }
+
+    const customDelimiter = this.getCustomDelimiter(input); // 2.커스텀 구분자 확인 기능
+    Console.print(customDelimiter);
+
+    return 0;
+  }
+
+  // 2.커스텀 구분자 확인 기능
+  getCustomDelimiter(input) {
+    if (input.startsWith('//')) {
+      const delimiterSection = input.split('\\n')[0];
+      return delimiterSection.substring(2);
+    }
+    return;
   }
 }
 
