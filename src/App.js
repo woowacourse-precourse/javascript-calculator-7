026e66d63cb8d837e1a61387dc90715c61e636d7
SNXWXH <snxwxh@gmail.com>
2024-10-20 00:52:11 +0900

    feat(app): add handling of numbers, separators, and sum

--- src/App.js
+++ src/App.js
@@ -6,16 +6,41 @@ class App {
     const INPUT_MESSAGE = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요.\n',
     );
-    const TRIM_INPUT_MESSAGE = INPUT_MESSAGE.trim();
+    let message = INPUT_MESSAGE.trim();
     let separators = /[,:]/;
 
-    if (TRIM_INPUT_MESSAGE === '' || typeof TRIM_INPUT_MESSAGE !== 'string') {
+    if (message === '') {
+      Console.print('결과 : 0');
+      return;
+    }
+
+    if (typeof message !== 'string') {
       throw new Error('[ERROR] 입력값이 올바르지 않습니다.');
     }
 
-    if (TRIM_INPUT_MESSAGE.startsWith('//')) {
-      separators = checkSeparator(TRIM_INPUT_MESSAGE);
+    if (message.startsWith('//')) {
+      separators = checkSeparator(message);
+      message = message.replace(/\/\/.*?\\n/, '');
+    } else {
+      const INVALID_SEPARATORS_REGEX = /[^\d,:\n]/;
+
+      if (INVALID_SEPARATORS_REGEX.test(message)) {
+        throw new Error('[ERROR] 유효하지 않은 구분자입니다.');
+      }
     }
+
+    let numbers = message.split(separators).filter(v => v);
+
+    numbers.forEach(v => {
+      const NUM = parseFloat(v);
+      if (NUM <= 0 || Number.isNaN(NUM)) {
+        throw new Error(
+          '[ERROR] 음수, 0 또는 비정상적인 값이 포함되어 있습니다.',
+        );
+      }
+    });
+
+    const ANSWER = numbers.reduce((acc, cur) => acc + +cur, 0);
   }
 }
 
--- src/checkSeparators.js
+++ src/checkSeparators.js
@@ -5,7 +5,7 @@ const checkSeparators = input => {
     throw new Error('[ERROR] 유효하지 않은 구분자입니다.');
   }
 
-  return new RegExp(`[${match[1]},;]`);
+  return new RegExp(`${match[1]}`);
 };
 
 export default checkSeparators;
