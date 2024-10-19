ee3cbb31d0c694523c9d064af75dfd5b26e2c538
jjang-aaa <jadoit473@gmail.com>
2024-10-19 18:46:42 +0900

    feat(App): 커스텀 구분자가 있는 경우 구현
    커스텀 구분자를 가져오는 extractCustomDelimiter함수와 커스텀 구분자와 기존 구분자로 문자열을 나누는 splitCustomDelimiter함수 구현

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ class App {
       "덧셈할 문자열을 입력해 주세요.\n"
     );
 
+    let splitArr;
     if (inputString.startsWith("//")) {
-      if (inputString.search(/\n/)) {
-        console.log("커스텀 구분자가 있다");
-      } else {
-        console.log("커스텀 구분자가 없다");
-      }
-    } else {
-      console.log("커스텀 구분자가 없다");
+      let customDelimiter = extractCustomDelimiter(inputString);
+      splitArr = splitCustomDelimiter(inputString, customDelimiter);
     }
   }
+}
+
+function extractCustomDelimiter(inputString) {
+  if (inputString.search(/\\n/) !== -1) {
+    return inputString.substring(2, inputString.search(/\\n/));
+  } else {
+    throw new Error("[ERROR]: 커스텀 구분자를 정확히 입력해주세요.");
+  }
+}
+
+function splitCustomDelimiter(inputString, customDelimiter) {
+  let remainString = inputString.substring(
+    inputString.search(/\\n/) + 2,
+    inputString.length
+  );
+  const regex = new RegExp(`${customDelimiter}|,|:`);
+  return remainString.split(regex);
 }
 
 export default App;
