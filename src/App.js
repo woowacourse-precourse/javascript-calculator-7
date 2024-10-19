90f330b806a8b4bb8a46592dcf9d1921e99d50ef
ChaDongmin <cdm1263@gmail.com>
2024-10-19 22:53:40 +0900

    refactor(App): run 메서드에 try catch 구문 적용

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ class App {
   }
 
   async run() {
-    const userInput = await this.console.read(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
-    this.calculator.extractCustomSeperator(userInput);
-    const processedInput = this.calculator.replaceAllSeperators(userInput);
-    const validInputArray = this.calculator.validateInput(processedInput);
+    try {
+      const userInput = await this.console.read(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      this.calculator.extractCustomSeperator(userInput);
+      const processedInput = this.calculator.replaceAllSeperators(userInput);
+      const validInputArray = this.calculator.validateInput(processedInput);
 
-    const sum = this.calculator.sumAll(validInputArray);
-    this.console.print(`결과 : ${sum}`);
+      const sum = this.calculator.sumAll(validInputArray);
+      this.console.print(`결과 : ${sum}`);
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
 
