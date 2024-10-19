b57ef83aaafa5c04747433ff83b4e09b98fca64b
parkseyoung <hellosy76@naver.com>
2024-10-19 18:08:07 +0900

    feat: try, catch 를 활용한 결과 도출 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await this.getUserInput(); //사용자 입력
+      const result = this.calculateSum(input); //결과값 계산
+      Console.print(`결과: ${result}`); //결과 출력
+    } catch (error) {
+      Console.print(error.message); //에러처리
+      throw error;
+    }
+  }
 }
 export default App;
