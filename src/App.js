32115b402ca2850f80d24c59fced7b105aa06a32
gyur1kim <qri1114@gmail.com>
2024-10-19 18:38:00 +0900

    feat: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 구분자를 기준으로 분리한 각 숫자의 합을 반환

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  constructor () {
+    this.input = ''
+    this.delimeter = [',', ':']
+    this.output = 0
+  }
+
   async getUserInput() {
     try {
       const userInput = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-
       if (userInput === '') throw new Error('[Error]');
-      return userInput;
+
+      this.input = userInput;
     } catch (error) {
+      throw error;
+    }
+  }
+
+  checkIsValidNumber(n) {
+    try {
+      MissionUtils.Console.print(`n : ${n}`);
+      const num = Number(n);
+      if (Number.isNaN(num) || num < 0 ) throw new Error('[Error]')
+      return num;
+    } catch(error) {
+      throw error;
+    } 
+  }
+
+  async add() {
+    try {
+      const reg = new RegExp(this.delimeter.join('|'))
+      this.output = this.input.split(reg)
+        .map(this.checkIsValidNumber)
+        .reduce((acc, cur) => acc + cur, 0);
+    } catch(error) {
       throw error;
     }
   }
 
   async run() {
     try {
-      const userInput = await this.getUserInput();
-      MissionUtils.Console.print(`사용자가 입력한 값 : ${userInput}`);
+      await this.getUserInput();
+      await this.add();
+
+      MissionUtils.Console.print(`결과값 : ${this.output}`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
     }
