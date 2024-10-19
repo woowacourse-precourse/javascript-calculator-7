71923f7db5faa23ea4455bf0cb356bca0d522135
jin123457 <jin_ox@naver.com>
2024-10-19 19:13:06 +0900

    fix (checkDelimiter) : add condition for negative function execution

--- src/Utils/CheckDelimiter.js
+++ src/Utils/CheckDelimiter.js
@@ -9,7 +9,7 @@ const CheckDelimiter = (str) => {
     throw new Error(`[ERROR] 구분자 혹은 커스텀 구분자가 존재하지 않습니다.`);
   }
 
-  if (FIND_DELIMITER.indexOf('-') === -1) {
+  if (FIND_DELIMITER.indexOf('-') === -1 || /-{2}\d+/.test(str)) {
     ContainsNegative(str);
   }
   const DELIMITER_SET = new Set([...FIND_DELIMITER]);
