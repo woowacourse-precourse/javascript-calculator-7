3a3b8276e39a2e4914bd6e606d37af4deeff1e47
songhj10207@naver.com <songhj10207@naver.com>
2024-10-19 18:32:39 +0900

    feat: 구분자와 계산할 값 분리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
-  constructor() {}
+  constructor() {
+    this.separator = ',|:';
+    this.initValue = '';
+  }
 
   async getUserinput() {
     const userinput = MissionUtils.Console.readLineAsync(
@@ -10,8 +13,35 @@ class App {
     return userinput;
   }
 
+  getCustomSeparator(userinput) {
+    const isCustomSeparator =
+      userinput.slice(0, 2) === '//' && userinput.slice(3, 5) === '\\n';
+    if (isCustomSeparator) {
+      return userinput[2];
+    }
+    return '';
+  }
+
+  updateSeparator(customSeparator) {
+    if (customSeparator) {
+      this.separator = `${this.separator}|${customSeparator}`;
+    }
+  }
+
+  updateInputValue(userinput, customSeparator) {
+    if (customSeparator) {
+      this.initValue = userinput.slice(5);
+    }
+    if (!customSeparator) {
+      this.initValue = userinput;
+    }
+  }
+
   async run() {
     const userinput = await this.getUserinput();
+    const customSeparator = this.getCustomSeparator(userinput);
+    this.updateSeparator(customSeparator);
+    this.updateInputValue(userinput, customSeparator);
   }
 }
 
