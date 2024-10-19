f90d2b8cb617e6e1748d75f7b31b46358cbd88bc
Lim-shweety <smile70200@naver.com>
2024-10-20 01:02:52 +0900

    feat(app.js): get empty input
    빈 문자열 입력 시 result값 0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import {Console, MissionUtils} from "@woowacourse/mission-utils";
 class App {
   async run() {
     let inputStr= await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+    let result;
+    if (inputStr===""){
+      result=0;
+    }
+    Console.print("결과 : "+result);
   }
 }
 
