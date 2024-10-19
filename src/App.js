f233a2dc6c7ab863712326b04b77b6819cca4466
yheseul <kangyheseul@naver.com>
2024-10-19 22:40:27 +0900

    chore: Prettier 적용하여 코드 스타일 통일

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
-    const INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n"
-    const RESULT = "결과 : "
-    const ERROR = "[ERROR]"
+    const INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n";
+    const RESULT = "결과 : ";
+    const ERROR = "[ERROR]";
 
     const input = await Console.readLineAsync(INPUT_MESSAGE);
     const numberArray = this.extractNumbers(input);
     const hasInvalidValue = numberArray.includes(NaN);
-    const hasNegativeNumber = numberArray.filter(el => el < 0).length
+    const hasNegativeNumber = numberArray.filter((el) => el < 0).length;
 
     if (hasInvalidValue || hasNegativeNumber) throw new Error(ERROR);
     if (!hasInvalidValue && !hasNegativeNumber) {
@@ -25,14 +25,25 @@ class App {
 
     const hasComma = this.hasSeparator(input, COMMA);
     const hasSemicolon = this.hasSeparator(input, SEMICOLON);
-    const hasCustomDelimiterPrefix = this.hasSeparator(input, CUSTOM_DELIMITER_PREFIX);
-    const hasCustomDelimiterSuffix = this.hasSeparator(input, CUSTOM_DELIMITER_SUFFIX);
+    const hasCustomDelimiterPrefix = this.hasSeparator(
+      input,
+      CUSTOM_DELIMITER_PREFIX
+    );
+    const hasCustomDelimiterSuffix = this.hasSeparator(
+      input,
+      CUSTOM_DELIMITER_SUFFIX
+    );
 
     const hasCommaAndSemicolon = hasComma && hasSemicolon;
-    const hasCustomSeparator = hasCustomDelimiterPrefix && hasCustomDelimiterSuffix;
+    const hasCustomSeparator =
+      hasCustomDelimiterPrefix && hasCustomDelimiterSuffix;
 
     if (hasCustomSeparator)
-      return this.splitUsingCustomDelimiterAndConvertToNumbers(input, CUSTOM_DELIMITER_PREFIX, CUSTOM_DELIMITER_SUFFIX);
+      return this.splitUsingCustomDelimiterAndConvertToNumbers(
+        input,
+        CUSTOM_DELIMITER_PREFIX,
+        CUSTOM_DELIMITER_SUFFIX
+      );
     if (hasCommaAndSemicolon)
       return this.splitAndConvertToNumbers(input, [COMMA, SEMICOLON]);
     if (hasComma) return this.splitAndConvertToNumbers(input, COMMA);
@@ -40,11 +51,21 @@ class App {
     return [NaN];
   }
 
-  splitUsingCustomDelimiterAndConvertToNumbers(input, customDelimiterPrefix, customDelimiterSuffix) {
+  splitUsingCustomDelimiterAndConvertToNumbers(
+    input,
+    customDelimiterPrefix,
+    customDelimiterSuffix
+  ) {
     const customDelimiterPrefixIndex = input.indexOf(customDelimiterPrefix);
     const customDelimiterSuffixIndex = input.indexOf(customDelimiterSuffix);
-    const customDelimiter = input.slice(customDelimiterPrefixIndex + 2, customDelimiterSuffixIndex);
-    const stringToSeparate = input.replace(input.slice(customDelimiterPrefixIndex, customDelimiterSuffixIndex + 2), "");
+    const customDelimiter = input.slice(
+      customDelimiterPrefixIndex + 2,
+      customDelimiterSuffixIndex
+    );
+    const stringToSeparate = input.replace(
+      input.slice(customDelimiterPrefixIndex, customDelimiterSuffixIndex + 2),
+      ""
+    );
     return this.splitAndConvertToNumbers(stringToSeparate, customDelimiter);
   }
 
