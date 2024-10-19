1ea9222b13568f8f6a80ee8e83fed47ffbbf7e25
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-20 00:15:24 +0900

    feat: 커스텀 구분자 체크 및 문자열에서 구분자 분리하기 (빈 문자열 처리)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await this.getInput();
+    const { inputString, delimiters } = this.splitInput(input);
+    if (inputString === "0") {
+      MissionUtils.Console.print("결과 : 0");
+      return;
+    }
   }
 
   async getInput() {
     return await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요."
     );
+  }
+
+  splitInput(inputString) {
+    if (!inputString) return { inputString: "0", delimiters: [] };
+    return { inputString, delimiters };
   }
 }
 
