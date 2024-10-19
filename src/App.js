1bb50929e1fbbe98912ca2aa5dc00038247a9697
zzz664 <zzz664>
2024-10-20 04:05:26 +0900

    feat(App): getCustomDelim 메서드 구현
    Readme.md에 작성한 기능 중 두번째 기능인 커스텀 구분자를 인식하는  메서드를 구현했다.
    커스텀 구분자를 포함하지 않는 문자열과 커스텀 구분자를 포함하는 문자열의 처리를 다르게 하는 코드를 추가했다.
    - 19 ~ 28줄의 코드: 커스텀 구분자를 포함하는 문자열은 커스텀 구분자를 쉼표(,)로 치환하여 처리함

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 class App {
+  constructor() {
+    // 커스텀 구분자
+    this.customDelim = null;
+  }
+
+  getCustomDelim(str) {
+    let start = str.indexOf("//");
+    let end = str.indexOf("\n");
+
+    // "//"와 "\n" 사이의 커스텀 구분자 문자를 customDelim 멤버변수에 저장한다.
+    this.customDelim = str.slice(start + 2, end);
+  }
+
   splitDelim(str) {
     // /,|:/ 정규식을 통해 split메서드로 여러개의 구분자를 사용하여 문자열을 가공한다
-    let processedStr = str.split((/,|:/));
+    let processedStr = null;
+    
+    if(str.indexOf("//") === -1) {
+      // 커스텀 구분자의 시작인 "//"가 존재하지 않으면 ",",":"를 사용한 추출을 진행한다.
+      processedStr = str.split((/,|:/));
+    } else {
+      // 커스텀 구분자가 있다면 커스텀 구분자를 추출한다.
+      this.getCustomDelim(str);
+
+      // 커스텀 구분자의 끝인 "\n"의 위치까지 slice한 후 커스텀 구분자를 기본 구분자인 쉼표(,)로 치환한 후 숫자를 추출한다.
+      processedStr = str.slice(str.indexOf("\n")).replace(this.customDelim, ",").split((/,|:/));
+    }
 
     // + 단항연산자를 이용해서 문자열 -> 숫자 변환을 한다
-    for(let i = 0; i<splitStr.length; ++i) {
+    for(let i = 0; i<processedStr.length; ++i) {
       processedStr[i] = +processedStr[i];
     }
 
@@ -12,7 +36,7 @@ class App {
   }
 
   async run() {
-    console.log(this.splitDelim("1,23:5,7:8:22"));
+    console.log(this.splitDelim("//&\n1,23:5,7&8:22"));
   }
 }
 
