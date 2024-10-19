cebc5fb6bb9b7c561ca8e7320fc80e97ae31c4d2
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-20 03:40:51 +0900

    feat: 상수 추가
    - 입출력, 에러, 구분자 관련 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const INPUT_MESSAGE = '덧셈할 문자열을 입력해 주세요.';
+export const RESULT_MESSAGE = '결과 : ';
+export const ERROR_MESSAGE = '[ERROR]';
+
+export const DEFAULT_DELIMITER = /,|:/;
+export const CUSTOM_DELIMITER = /^\/\/(.*?)\\n/;
