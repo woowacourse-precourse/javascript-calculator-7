b694687a271edb9eb952cfba7fd9220f7e4d72ed
dongmin <dongmin11566@gmail.com>
2024-10-19 19:23:10 +0900

    feat(add): 빈 문자열 입력 시 0 반환 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const result = this.add(input);
+  }
+
+  add(numbers) {
+    if(numbers === '') return 0;
+  }
 }
 
 export default App;
