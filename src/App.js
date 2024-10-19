a0649eb54462a4bd1017e7f31e9ee105ff8eebdb
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-19 16:11:06 +0900

    feat: App.js 전체 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-class App {
-  async run() {}
-}
+import { calculate } from './models/CalculatorModel.js';
+import {
+  getUserInput,
+  displayErrorMessage,
+  resultOutput,
+} from './views/CalculatorView.js';
 
+class App {
+  async run() {
+    try {
+      const input = await getUserInput(); // 입력을 비동기로 받기
+      const result = calculate(input); // Model에서 에러 발생 가능
+      resultOutput(result);
+    } catch (error) {
+      displayErrorMessage(error.message); // View에서 에러 출력
+      throw error; // 에러를 다시 던지기
+    }
+  }
+}
 export default App;
