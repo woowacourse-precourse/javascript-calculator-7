62952309667f539d34aabc10e2b5261552505db0
0Jaemin0 <jaemin2167@naver.com>
2024-10-19 19:30:49 +0900

    feat: 입력한 문자열의 길이에 따른 조건 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+    let result = 0;
+
+    if (input.length === 0) {
+      Console.print(`결과 : ${result}`);
+      return;
+    }
 
     Console.print(`입력 문자열 확인: ${input}`);
   }
