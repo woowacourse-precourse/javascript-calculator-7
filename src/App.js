0a5eecf0514c3433223178007550757da1d692db
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-19 19:29:28 +0900

    feat: 문자열 입력 문구 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.");
+  }
 }
-
 export default App;
