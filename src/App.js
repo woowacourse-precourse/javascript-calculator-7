67591027822624e9bb3de4b5002f79fce8a17ae8
@u_limit_ <yurim@ajou.ac.kr>
2024-10-20 00:01:58 +0900

    refactor: calculate 함수 불필요한 변수 삭제 및 에러 메세지 변경

--- src/App.js
+++ src/App.js
@@ -35,19 +35,22 @@ const getNumber = (input) => {
 
 const calculate = (input) => {
   let total = 0;
-
   getNumber(input).forEach((number)=>{
     let numValue = Number(number);
 
     if (numValue < 0) {
-      throw new Error("[ERROR] 양수를 입력해주세요.");
+      throw new Error("[ERROR] 양수(정수)를 입력해주세요.");
     }
 
-    if (isNaN(number)) {
-      throw new Error("[ERROR] 올바른 구분자를 입력해주세요.");
+    if(number.length === 0) {
+      throw new Error("[ERROR] 구분자 뒤에 더할 숫자를 입력해주세요.")
     }
 
-    total += Number(number);
+    if (isNaN(numValue)) {
+      throw new Error("[ERROR] 디폴트 구분자(, 또는 :) 또는 커스텀 구분자를 사용해주세요.");
+    }
+
+    total += numValue;
   })
 
   return total;
