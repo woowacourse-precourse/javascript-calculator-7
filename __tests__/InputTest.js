e9740de5ed226367d077d4da9297b134cf335a96
hamo-o <yheel369@gmail.com>
2024-10-19 21:13:50 +0900

    feat(InvalidSeparatorError,InputTest): 에러메시지 출력

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -148,4 +148,12 @@ describe("에러메시지 출력", () => {
       expect(error.message).toBe("[ERROR] 입력 값에 0이 포함되어 있습니다.\n 구분자와 양수로 구성된 문자열을 입력해주세요.");
     }
   });
+
+  test("InvalidSeparatorError 에러 발생 시", () => {
+    try {
+      throw new InvalidSeparatorError();
+    } catch (error) {
+      expect(error.message).toBe("[ERROR] 입력 값에 등록되지 않은 구분자가 포함되어 있습니다.\n 문자옆 앞부분의 '//'와 '\\n' 사이에 커스텀 구분자를 입력하거나, 덧셈할 문자열에 구분자와 양수만 포함되도록 입력해주세요.");
+    }
+  });
 });
