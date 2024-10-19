1736c95e2b3414c8d8763632411f8865d6faca9d
Lim-shweety <smile70200@naver.com>
2024-10-20 01:18:16 +0900

    feat(app.js): update split str and calculate sum

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import {Console, MissionUtils} from "@woowacourse/mission-utils";
+
 class App {
-  async run() {
-    let inputStr= await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
-    let result;
-    if (inputStr===""){
-      result=0;
+    async run() {
+        let inputStr = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+        let result = 0;
+        if (inputStr === "") {
+            result = 0;
+        } else {
+            let nums = inputStr.split(/[:,]/);
+            for (const num of nums) {
+                result += Number(num);
+            }
+        }
+        Console.print("결과 : " + result);
     }
-    Console.print("결과 : "+result);
-  }
 }
 
 export default App;
