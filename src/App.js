56c0e3d5f6929be2a017a9fe84d2dd7473c9d1a3
Joohyun Kim <blaireinse@gmail.com>
2024-10-19 18:30:34 +0900

    feat(getSum): add sum function for numbers separated by comma or colon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const inputValue = await this.getInputValue();
-    let result = inputValue;
+    const parsedInputValue = this.parseInputValueToNumber(inputValue);
+
+    let result = this.getSum(parsedInputValue);
 
     try {
       if (!inputValue) {
@@ -18,6 +20,18 @@ class App {
 
   async getInputValue() {
     return await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+  }
+
+  parseInputValueToNumber(inputValue) {
+    const defaultDelimiter = /,|:/;
+    return inputValue.split(defaultDelimiter).map(Number);
+  }
+
+  getSum(numbers) {
+    return numbers.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
   }
 }
 
