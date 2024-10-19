70525edc76d0d28b9f288e571c2801a411da14f0
kwonsoonook <sunoogy99@naver.com>
2024-10-20 04:05:39 +0900

    feat: add splitDelimeter and escapedSpecialChars functions Implemented splitDelimeter function to split a string based on custom and default delimeters. Also added escapedSpecialChars function to handle special characters in the delimeters to avoid regex conflicts.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,36 @@ class App {
     return MATH_EXPRESSION;
   }
 
+  // 3. 구분자 기반 분리하기
+  splitDelimeter(expression, delimeters) {
+    // 기본 구분자 정의
+    const DEFAULT_DELIMETERS = [':', ','];
+
+    // 커스텀 구분자 이스케이프 처리
+    const ESCAPED_DELIMETERS = this.escapeSpecialChars(delimeters);
+
+    // 모든 구분자
+    const ALL_DELIMETER = ESCAPED_DELIMETERS.concat(DEFAULT_DELIMETERS);
+
+    // 모든 구분자를 split에 들어갈 표현식으로 만들기
+    const ALL_DELIMETER_REGEX = ALL_DELIMETER.join('|');
+
+    // 구분자 기반 분리하여 숫자 리스트 반환하기
+    const NUMBERS = expression.split(new RegExp(ALL_DELIMETER_REGEX));
+
+    return NUMBERS;
+  }
+
+  // 3-1. 커스텀 구분자 전처리하기
+  escapeSpecialChars(delimeters) {
+    // 이스케이프 처리가 필요한 특수 문자 목록
+    const EXCAPE_CHARS = /([.*+?^${}()|\[\]\\/])/g;
+
+    const ESCAPED_DELIMETERS = delimeters.map(del => del.replace(EXCAPE_CHARS, '\\$1'));
+
+    return ESCAPED_DELIMETERS;
+  }
+
   async run() {}
 }
 
