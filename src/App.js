702b7e1d1bbba92b1c1fc519cf78320c4a11e0b8
ongheong <pudding4271@gmail.com>
2024-10-19 18:22:51 +0900

    ✨ Feat(App): add mission module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈 계산을 위해 원하는 문자열을 입력해 주세요: "
+    );
+    Console.print("계산 결과: " + input);
+  }
 }
 
 export default App;
