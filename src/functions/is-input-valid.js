6e0e10ac83686a40f5cc89fbfa520d263be76cb1
meteorqz6 <nys8765@naver.com>
2024-10-20 02:59:08 +0900

    refactor(validation) : isInputValid 함수 리팩토링

--- src/functions/is-input-valid.js
+++ src/functions/is-input-valid.js
@@ -1,35 +1,42 @@
 function isInputValid(input) {
     const positiveIntegerPattern = /^\d+$/;
 
-    if(input.startsWith('//')){
-        if(input.includes('\\n')){
-            // 커스텀 구분자 추출
-            const customDelimiterStartIndex = input.indexOf('\\n');
-            const delimiter = input.slice(2,customDelimiterStartIndex);
-            const numberString = input.substring(customDelimiterStartIndex + 2);
-            const str = numberString.split(new RegExp(`[${delimiter},:]`));
-
-            if(str === ''){
-                throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
-            }
-            for(const char of str){
-                if(!positiveIntegerPattern.test(char)){
-                    throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
-                }
-            }
-        } else {
-            throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
-        }
-
+    if(input.startsWith('//')) {
+        validateCustomDelimiterInput(input, positiveIntegerPattern);
     } else {
-        const str = input.split(/[, :]/);
+        validateDefaultInput(input, positiveIntegerPattern);
+    }
+}
 
-        for(const char of str) {
-            if(!positiveIntegerPattern.test(char)){
-                throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
-            }
-        }
+function validateCustomDelimiterInput(input, pattern) {
+    const customDelimiterStartIndex = input.indexOf('\\n');
+    if(customDelimiterStartIndex === -1){
+        throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
+    }
+
+    const delimiter = input.slice(2, customDelimiterStartIndex);
+    const string = input.slice(customDelimiterStartIndex + 2);
+    validateString(string, delimiter, pattern);
+}
+
+function validateDefaultInput(input, pattern) {
+    const arr = input.split(/[,:]/);
+    validateArray(arr, pattern);
+
+}
+
+function validateString(string, delimiter, pattern) {
+    const arr = string.split(new RegExp(`[${delimiter},:]`));
+    if(arr.length === 0 || arr.some(value => !pattern.test(value))) {
+        throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
+    }
+}
+
+function validateArray(arr, pattern) {
+    if(arr.some(value=> !pattern.test(value))) {
+        throw new Error('[ERROR] 잘못된 값을 입력했습니다.');
     }
 }
 
 export default isInputValid;
+
