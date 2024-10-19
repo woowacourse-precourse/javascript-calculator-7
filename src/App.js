b929e826687147082a5defef7ea237815e6c2db9
인호현 <inhohyun36@gmail.com>
2024-10-20 01:05:38 +0900

    fix: MissionUtils import 방식으로 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     // 문자열 입력
     Console.print("덧셈할 문자열을 입력해 주세요.");
-    const input = await Console.readLineAsync("");
+    const input = await MissionUtils.Console.readLineAsync("");
 
     // 입력값이 비어 있는 경우 0 출력
     if (input === "") {
