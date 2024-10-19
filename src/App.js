eda00067a162e0933517bdd94f16be6e45c556b7
sky121016 <sky121016@naver.com>
2024-10-20 00:49:20 +0900

    feat: 1. 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import {Console} from "@woowacourse/mission-utils";
+
+
 class App {
-  async run() {}
+  async run() {
+
+    try{
+      // 입력
+      const input = await this.getInput();
+      Console.print(input);
+
+      // 커스텀 구분자 파싱
+
+      // 숫자 파싱
+
+      // 합계 연산
+
+      // 결과 출력
+
+
+    }catch(error){
+
+    }
+  }
+
+  async getInput(){
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    return input;
+  }
 }
 
 export default App;
