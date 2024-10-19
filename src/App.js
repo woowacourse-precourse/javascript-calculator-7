6730b6f4d3b18618d5f7e1b6b9c7ba40a6b57296
CJY <cjy3458@naver.com>
2024-10-19 16:39:53 +0900

    feat: 문자열 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      Console.print(`결과: ${input}`);
+    } catch (error) {
+      Console.print(`[ERROR] 에러 발생`);
+    }
+  }
 }
 
 export default App;
