87d12f7277ef070a6bff57899243ee77c76f02c3
spoyodevelop <kjy9505@gmail.com>
2024-10-20 06:18:50 +0900

    fix: fix error message

--- src/Constraints/Constraints.js
+++ src/Constraints/Constraints.js
@@ -2,7 +2,7 @@ const ERROR_PREFIX = '[ERROR]: ';
 
 const ERROR_MESSAGES = {
   INVALID_INPUT_FORMAT:
-    '입력은 반드시 "//"로 시작하고, "\\n"이 포함되어야 합니다.',
+    '커스텀 구분자 입력시 입력은 반드시 "//"로 시작하고, "\\n"이 포함되어야 합니다.',
   INVALID_CUSTOM_INPUT:
     'delimiter와 숫자 이외의 문자가 포함되었거나, 입력 순서가 잘못되었습니다.',
   INVALID_NUMBER_INPUT: '문자열에 포멧이 올바르지 않거나, 음수를 입력했습니다.',
