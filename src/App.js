70526afc6c98fe9266fc5abe1be70acf9a224bbb
Lim-shweety <smile70200@naver.com>
2024-10-20 02:11:47 +0900

    feat(app.js): update new std split
    커스텀 구분자 입력 시 처리

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import {Console, MissionUtils} from "@woowacourse/mission-utils";
 class App {
     async run() {
         let inputStr = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
-        let result = 0;
+        let result = -1;
         if (inputStr === "") {
             result = 0;
+        } else if (inputStr.slice(0,2) === '//') {
+            let endIndex = inputStr.indexOf("\\n");
+            const std = inputStr.slice(2, endIndex);
+            let nums = inputStr.slice(endIndex+2).split(std);
+            result = this.#SUM(nums)
         } else {
             let nums = inputStr.split(/[:,]/);
-            for (const num of nums) {
-                result += Number(num);
-            }
+            result = this.#SUM(nums)
         }
-        Console.print("결과 : " + result);
+        if (result !== -1) {
+            Console.print("결과 : " + result);
+        }
+    }
+
+    #SUM = (nums) => {
+        let result = 0
+        for (const num of nums) {
+              result += Number(num);
+        }
+        return result
     }
 }
+
 
 export default App;
 
