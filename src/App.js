41800796429fc38461a501fb74e6a91c9ad55b3f
hyomin <leehm123456@gmail.com>
2024-10-19 18:26:33 +0900

    feat: add string split function

--- src/App.js
+++ src/App.js
@@ -7,8 +7,25 @@ class App {
     return inputString;
   }
 
+  stringSplitDelimiter(inputString) {
+    let str = [];
+    // 커스텀 구분자 사용 시
+    if (inputString[0] === "/" && inputString[1] === "/") {
+      const index = inputString.indexOf("\\n");
+      const delimiter = inputString.substring(2, index);
+      const stringToSplit = inputString.substring(index + 2);
+      str = stringToSplit.split(delimiter).map(Number);
+    } else {
+      // 기본 구분자(쉼표, 콜론) 사용 시
+      str = inputString.split(/,|:/).map(Number);
+    }
+
+    return str;
+  }
+
   async run() {
     const inputString = await this.getInputString();
+    const splitedString = this.stringSplitDelimiter(inputString);
   }
 }
 
