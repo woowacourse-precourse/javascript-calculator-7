ab62e922bf3f0179dd1a2f322d8dc65a672c5c79
neaimt <pas120344@gmail.com>
2024-10-19 17:11:48 +0900

    feat: 정해진 구분자, 숫자 외 다른 문자 있는지 확인하는 기능
    숫자, 구분자 이외의 문자가 포함되었는지 확인
    사용자가 잘못된 값을 입력할 경우 Error 발생시킴

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     try {
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n'); // 1.프롬프트 입력 기능
       const result = this.calculate(input);
-      Console.print(`결과 : ${input}`); 
+      Console.print(`결과 : ${result}`); 
     } catch(err) {
       throw new Error('[ERROR]')
     }
@@ -21,7 +21,7 @@ class App {
     const customDelimiter = this.getCustomDelimiter(input); // 2.커스텀 구분자 확인 기능
     const allDelimiter = this.addDelimiter(customDelimiter); // 3.모든 구분자 ( 기본 구분자 배열에 커스컴 구분자 추가 ) 반환 기능
     const delimiterRegex = this.regexDelimiters(allDelimiter); // 4.구분자를 정규 표현식으로 바꾸는 기능
-    Console.print(delimiterRegex);
+    this.checkForInvalidCharacters(input, delimiterRegex);  // 5.정해진 구분자, 숫자 외 다른 문자 있는지 확인하는 기능 - 유효성 체크
 
     return 0;
   }
@@ -50,6 +50,22 @@ class App {
   regexDelimiters(delimiters) {
     const pattern = delimiters.join('|'); 
     return new RegExp(pattern);
+  }
+
+  // 5.정해진 구분자, 숫자 외 다른 문자 있는지 확인하는 기능
+  checkForInvalidCharacters(input, regexDelimiters) {
+    if (input.startsWith('//')) {
+      input = input.split('\\n')[1];
+    }
+
+    const invalidCharacterPattern = /[^0-9]/;
+  
+    // 모든 문자가 숫자이거나 구분자에 포함된 경우에만 통과
+    for (let char of input) {
+      if (invalidCharacterPattern.test(char) && !regexDelimiters.test(char)) {
+        throw new Error(); // 허용되지 않는 문자가 있을 경우 에러 발생
+      }
+    }
   }
 }
 
