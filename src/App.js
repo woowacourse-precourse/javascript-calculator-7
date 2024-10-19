cd8f9a95960cde5ee9f575bf71be9682334a716f
parkseyoung <hellosy76@naver.com>
2024-10-19 19:13:15 +0900

    feat: ,: 구분의 합 구하기

--- src/App.js
+++ src/App.js
@@ -11,5 +11,26 @@ class App {
       throw error;
     }
   }
+
+  async getUserInput() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    return input;
+  }
+  calculateSum(input) {
+    let numbers = input;
+    let delimiter = /[,:]/;
+
+    const parsedNumbers = numbers.split(delimiter).map((num) => {
+      const trimmed = num.trim();
+      if (trimmed === "") return 0;
+      const parsed = parseInt(trimmed, 10);
+      return parsed;
+    });
+
+    return parsedNumbers.reduce((sum, num) => sum + num, 0);
+  }
 }
+
 export default App;
