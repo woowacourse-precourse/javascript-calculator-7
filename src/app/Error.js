e69f5d89e68bb14ae95b19a4704d4923020747d0
L2HYUNN <donghyuni98@gmail.com>
2024-10-19 17:52:24 +0900

    refactor(error): 허가되지 않은 구분자를 사용했을때 에러 메시지 개선

--- src/app/Error.js
+++ src/app/Error.js
@@ -1,7 +1,7 @@
 class Error {
   static MESSAGE = Object.freeze({
     HAS_NOT_ALLOWED_DELIMITER:
-      '[ERROR] 쉼표(,), 콜론(:) 또는 "//"와 "\\n" 사이에 위치하는 문자만 구분자로 사용할 수 있어요.',
+      '[ERROR] 쉼표(,), 콜론(:) 또는 문자열 앞부분의 "//"와 "\\n" 사이에 위치하는 문자만 구분자로 사용할 수 있어요.',
     HAS_NEGATIVE_OR_ZERO_NUMBER: '[ERROR] 음수나 0은 사용할 수 없어요.',
   });
 }
