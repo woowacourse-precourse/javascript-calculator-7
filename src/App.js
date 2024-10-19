df714b467df302786dfc5661ba629eecea7483a5
eunji kang <miki2323@naver.com>
2024-10-19 19:00:49 +0900

    refactor: 구분자 class 분리
    - DelimeterApi : 구분자 class 분리
    - escapeRegExp 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,78 +1,96 @@
 import { Console } from "@woowacourse/mission-utils";
 
-// 사용자 입력
 async function getUserInputData() {
   const userInputData = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");  
   return userInputData;
 }
 
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+function makeError(errorMsg) {
+  throw new Error("[ERROR] " + errorMsg);
+}
+
+class DelimeterApi {
+  delimeter = []
+  
+  customFormatStart
+  customFormatEnd
+
+  constructor (delimeter1, delimeter2) {
+    this.delimeter.push(delimeter1)
+    this.delimeter.push(delimeter2)
+  }
+
+  setCustomFormat(start, end) {
+    this.customFormatStart = start
+    this.customFormatEnd = end
+  }
+
+  parseCustomDelimeter(userInputData) {
+    const customRegExp = new RegExp(`^${escapeRegExp(this.customFormatStart)}.${escapeRegExp(this.customFormatEnd)}`)
+    const parsedData = userInputData.match(customRegExp);
+    if (parsedData === null) return userInputData
+  
+    const customDelimeter = parsedData[0].charAt(2)
+    if (isNaN(customDelimeter) === false) {
+      makeError("커스텀 구분자는 숫자가 아닌 문자로 설정되어야 합니다.")
+    }
+
+    this.delimeter.push(customDelimeter);
+    return userInputData.replace(customRegExp, '')
+  }
+
+  getregExp() {
+    console.log(this.delimeter)
+
+    const escapedDelimeter = this.delimeter.map(delimeter => escapeRegExp(delimeter));
+    const regExpString = escapedDelimeter.join('|');
+
+    console.log(regExpString);
+
+    return new RegExp(regExpString, 'g')
+  }
+}
+
 class NumberExtractor {
+  delimeterApi
+
   orgUserInputData
   userInputData
-
-  delemeter
-  customDelemter
-
-  NUM_REG = /\D/
-  CUSTOM_DELI_START = '//'
-  CUSTOM_DELI_END = '\\\\n'
 
   constructor(userInputData) {
     this.orgUserInputData = userInputData
     this.userInputData = userInputData
 
-    this.delemeter = [',', ':']
+    this.delimeterApi = new DelimeterApi(',', ':')
+    this.delimeterApi.setCustomFormat('//', '\\n')
   }
 
   extractNumber() {
-    if (this.findCustomDelemeter() === true) {
-      this.addDelemeter();
-    }
+    this.userInputData = this.delimeterApi.parseCustomDelimeter(this.orgUserInputData)
+
+    console.log(this.userInputData)
 
     this.splitByDelimeter()
 
     return this.userInputData;
   }
 
-  findCustomDelemeter() {
-    const customDeliRegExp = new RegExp(`^${this.CUSTOM_DELI_START}.${this.CUSTOM_DELI_END}`)
-    const customDeliString = this.userInputData.match(customDeliRegExp);
-
-    if (customDeliString == undefined) return false
-  
-    this.customDelemter = customDeliString[0].charAt(2)
-
-    if (isNaN(this.customDelemter) == false) {
-      throw new Error("[ERROR] - 커스텀 구분자는 '문자'로 설정되어야 합니다.");
-    }
-
-    this.userInputData = this.userInputData.replace(customDeliRegExp, '')
-
-    return true
-    // todo : Error Handling
-    // 1. custom 구분자 설정은 \n 으로 끝나야 합니다.
-    // 2. custom 구분자는 '하나의 문자'로 설정되어야 합니다.
-  }
-
-  addDelemeter() {
-    this.delemeter.push(this.customDelemter)
-  }
-
   splitByDelimeter() {
-    // todo: delemeter[2] undefined처리
-    const deliRegExp = new RegExp(`${this.delemeter[0]}|${this.delemeter[1]}|${this.delemeter[2]}`, 'g')
-    this.userInputData = this.userInputData.split(deliRegExp);
-
-    if (this.checkError() == true) {
-      throw new Error("[ERROR] - 구분자에 해당하지 않는 문자가 존재합니다.");
-    }
+    this.userInputData = this.userInputData.split(this.delimeterApi.getregExp());
+    this.errorHandling();
   }
 
-  checkError() {
+  errorHandling() {
+    const numRegExp = /\D/
     const findChar = this.userInputData.find((element) => {
-      return this.NUM_REG.test(element);
+      return numRegExp.test(element);
     })
-    return findChar != undefined
+
+    if(findChar != undefined) generateError("구분자에 해당하지 않는 문자가 존재합니다.")
   }
 }
 
@@ -97,12 +115,9 @@ class Calculator {
   }
 }
 
-class ErrorHandler {
-  constructor() {
-
-  }
+function generateError(errorMsg) {
+  throw new Error("[ERROR] " + errorMsg);
 }
-
 class App {
   async run() {
     const userInput = await getUserInputData();
