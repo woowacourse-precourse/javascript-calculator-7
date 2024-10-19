940fcaa9f0a56ead1e3cf307747e7eae93c8a1f6
jllee000 <jllee000@naver.com>
2024-10-19 19:40:03 +0900

    feat : 기본 구분자 지정 후 합 출력

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const inputContents = await Console.readLineAsync("입력값을 입력해주세요.");
-    console.log(inputContents);
+    const USER_INPUT = await Console.readLineAsync("입력값을 입력해주세요.");
+    // :라는 기호가 있다면 무조건 ,로 대체하기
+    const NUMBERS = USER_INPUT.replace(/:/g, ",").split(",");
+    // 우선 NUMBERS 문자열을 정수로 바꿔서 더해줘야함
+    const TOTAL_SUM = NUMBERS.reduce((sum, num) => sum + parseInt(num, 10), 0);
+    Console.print(TOTAL_SUM);
   }
 }
 
