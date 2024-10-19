229a806fb36266761e0daf1f7390865f5241d91e
rwaeng <rwaeng0421@gmail.com>
2024-10-19 19:47:39 +0900

    refactor: 중복된 코드 삭제 및 userInput 변수 private으로 변경

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  #userInput = "";
+
   constructor() {
     this.separators = new RegExp(`[,:]`);
-    this.userInput = "";
     this.numbers = [];
   }
 
   async getUserInput() {
-    this.userInput = await Console.readLineAsync(
+    this.#userInput = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
   }
 
   // 구분자 찾기 기능
   async getSeparator() {
-    if (this.userInput.startsWith("//")) {
+    if (this.#userInput.startsWith("//")) {
       // 구분자 획득
-      this.separator = this.userInput[2];
-      this.separators = new RegExp(new RegExp(`[,:${this.separator}]`));
+      this.separator = this.#userInput[2];
+      this.separators = new RegExp(`[,:${this.separator}]`);
 
       // 입력값에서 커스텀 구분자 관련 문자 제거
-      const indexOfn = this.userInput.indexOf("n");
-      this.userInput = this.userInput.slice(indexOfn + 1);
+      const indexOfn = this.#userInput.indexOf("n");
+      this.#userInput = this.#userInput.slice(indexOfn + 1);
     }
   }
 
@@ -34,8 +35,8 @@ class App {
   }
 
   // 입력한 문자열 유효성 검사
-  async validateInput() {
-    const chars = this.userInput.split(this.separators);
+  validateInput() {
+    const chars = this.#userInput.split(this.separators);
 
     for (const char of chars) {
       const isValid = this.isValidChar(char);
@@ -67,7 +68,7 @@ class App {
     try {
       await this.getUserInput();
       await this.getSeparator();
-      await this.validateInput();
+      this.validateInput();
       this.calculateSum();
       this.printResult();
     } catch (error) {
