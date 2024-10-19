9040f1216f67e9ec480402eb07d5839488e696e1
이동현 <idonghyeon@idonghyeon-ui-MacBookAir.local>
2024-10-19 23:54:14 +0900

    :sparkles: feat: 입력이 끝단이 구분자일 때 에러처리

--- src/model.js
+++ src/model.js
@@ -19,6 +19,11 @@ class CalculatorModel {
       numbersString = input.slice(customSeparatorMatch[0].length);
     }
 
+    //입력의 끝단이 구분자인지 확인하는 에러 처리
+    if (this.endWithSeparators(numbersString, separators)) {
+      throw new Error("[ERROR] 입력의 마지막은 숫자여야합니다.");
+    }
+
     const regex = new RegExp(`[${separators.join("")}]+`);
     const numbers = numbersString.split(regex).map((num) => {
       const trimmedNum = num.trim();
@@ -38,6 +43,12 @@ class CalculatorModel {
     const sum = numbers.reduce((acc, cur) => acc + cur, 0);
 
     return sum;
+  }
+
+  //입력의 끝단이 구분자인지 확인하는 함수
+  endWithSeparators(input, separators) {
+    const lastChar = input.trim().slice(-1);
+    return separators.some((sep) => sep === lastChar);
   }
 }
 export default CalculatorModel;
