dd1c8ed283e7443d7215d69548683c23c32b0544
kimyou1102 <yoosk5485@naver.com>
2024-10-19 20:09:34 +0900

    feat: 커스텀 구분자 확인을 위한 조건문 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { validation } from './utils/validate.js';
 import { getInput } from './utils/getInput.js';
+import { findCustomSeparator } from './utils/separator.js';
 
 class App {
   async run() {
     const receivedInput = await getInput();
 
     validation(receivedInput);
+
+    if (findCustomSeparator(receivedInput)) {
+    } else {
+    }
   }
 }
 
