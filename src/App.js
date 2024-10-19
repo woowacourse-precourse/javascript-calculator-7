f6b4eefd084807b6dce96621aa145ebaae198262
Ko Daeun <drechsu@gmail.com>
2024-10-19 22:02:42 +0900

    feat(App): 추출한 문자열을 더해서 출력

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   getNumBeforeSeparator(input, separator = []) {
     let num = "";
     for (let i = 0; i < input.length; i++) {
-      if (input[i] === "," || ":") {
+      if (input[i] === "," || input[i] === ":") {
         break;
       }
       num += input[i];
@@ -12,9 +12,20 @@ class App {
     return { num, nextInput: input.slice(num.length + 1) };
   }
 
+  getNumSum(input) {
+    let sum = 0;
+    while (input.length > 0) {
+      const { num, nextInput } = this.getNumBeforeSeparator(input);
+      sum += Number(num);
+      input = nextInput;
+    }
+    return sum;
+  }
+
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-    Console.print(`결과: ${input}`);
+    const sum = this.getNumSum(input);
+    Console.print(`결과: ${sum}`);
   }
 }
 
