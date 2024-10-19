ff8368b66d54077bd220c50b74cf0691ba332d9d
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-19 20:44:36 +0900

    refactor(app): private 필드를 활용하여 은닉화

--- src/App.js
+++ src/App.js
@@ -5,51 +5,49 @@ import { splitByCustomSeparator } from './utils/splitByCustomSeparator.js';
 import { splitByDefaultSeparators } from './utils/splitByDefaultSeparators.js';
 
 class App {
-	constructor() {
-		this.input = '';
-		this.result = 0;
-	}
+	#input = '';
+	#result = 0;
 
 	async run() {
 		try {
-			await this.getUserInput();
-			this.checkForNegativeInput();
-			this.parseAndSplitInput();
-			this.handleArrayResult();
-			this.printResult();
+			await this.#getUserInput();
+			this.#checkForNegativeInput();
+			this.#parseAndSplitInput();
+			this.#handleArrayResult();
+			this.#printResult();
 		} catch (err) {
-			this.handleError(err);
+			this.#handleError(err);
 		}
 	}
 
-	async getUserInput() {
-		this.input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+	async #getUserInput() {
+		this.#input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
 	}
 
-	checkForNegativeInput() {
-		hasNegative(this.input);
+	#checkForNegativeInput() {
+		hasNegative(this.#input);
 	}
 
-	parseAndSplitInput() {
-		if (isEmptyOrNull(this.input)) {
-			this.result = 0;
-		} else if (this.input.startsWith('//')) {
-			this.result = splitByCustomSeparator(this.input);
+	#parseAndSplitInput() {
+		if (isEmptyOrNull(this.#input)) {
+			this.#result = 0;
+		} else if (this.#input.startsWith('//')) {
+			this.#result = splitByCustomSeparator(this.#input);
 		} else {
-			this.result = splitByDefaultSeparators(this.input);
+			this.#result = splitByDefaultSeparators(this.#input);
 		}
 	}
 
-	handleArrayResult() {
-		if (Array.isArray(this.result)) {
-			if (this.result[0] === this.input) {
+	#handleArrayResult() {
+		if (Array.isArray(this.#result)) {
+			if (this.#result[0] === this.#input) {
 				throw new Error('[ERROR]: 구분자와 양수로 이루어진 값을 입력해주세요.');
 			}
-			this.result = this.calculateSum(this.result);
+			this.#result = this.#calculateSum(this.#result);
 		}
 	}
 
-	calculateSum(result) {
+	#calculateSum(result) {
 		let sum = 0;
 		for (const v of result) {
 			if (isNaN(v)) {
@@ -60,11 +58,11 @@ class App {
 		return sum;
 	}
 
-	printResult() {
-		Console.print(`결과 : ${this.result}`);
+	#printResult() {
+		Console.print(`결과 : ${this.#result}`);
 	}
 
-	handleError(err) {
+	#handleError(err) {
 		throw new Error(err);
 	}
 }
