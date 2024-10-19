7c6029f8f66ff588bc0dd428b6fa89d1ecc4ae80
jissssu <leeland21@naver.com>
2024-10-19 17:56:19 +0900

    feat: 사용자 입력 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    Consle.print(input);
+  }
+}
+
+export default App;class App {
   async run() {}
 }
 
