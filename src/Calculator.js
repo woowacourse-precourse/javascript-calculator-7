0dae7c6cbed0c691c7a84de7e25adb94a915f3a5
siggu <m0_w_0m@naver.com>
2024-10-19 21:18:58 +0900

    feat: 문자열 계산 방식 변경
    문자열 검사 핸들러에게 커스텀 구분자 사용 여부를 넘김

--- src/Calculator.js
+++ src/Calculator.js
@@ -11,52 +11,39 @@ class Calculator {
     this.output = new Output();
   }
 
-  // 커스텀 구분자 계산
-  calcCustomSeparator(userInput) {
-    let customSep = '';
-
-    if (userInput[0] === userInput[1] && userInput[0] === '/' && userInput[3] === '\\' && userInput[4] === 'n') {
-      customSep = userInput[2];
-    }
-
-    return customSep;
-  }
-
   // 유저 입력 문자열 계산
   calcUserInput(userInput) {
     let numString = '';
     let numSum = 0;
-    let hasDecimalPoint = false;
-
-    const customSep = this.calcCustomSeparator(userInput);
-    const allowedSeps = [...SEPARATOR.ALLOWED_SEPARATOR, customSep];
+    const useCustomSep = this.validator.checkCustomSeparator(userInput).useCustomSep;
+    const customSep = this.validator.checkCustomSeparator(userInput).customSep;
 
     this.validator.checkValidCustomSeparator(customSep);
 
-    userInput.forEach((input, idx) => {
-      this.validator.checkValidSeparator(input);
+    const allowedSep = [...SEPARATOR.ALLOWED_SEPARATOR, customSep];
 
+    userInput.forEach((input, idx) => {
       const calcResult = this.handler.handleUserInput(
+        useCustomSep,
+        customSep,
+        allowedSep,
         input,
         userInput,
-        numString,
         idx,
-        hasDecimalPoint,
-        allowedSeps,
-        SEPARATOR.ASSIGN_CUSTOM_SEPARATOR,
+        numString,
         numSum
       );
 
       numString = calcResult.numString;
       numSum = calcResult.numSum;
-      hasDecimalPoint = calcResult.hasDecimalPoint;
-      this.validator.checkValidInput(userInput, idx, customSep);
     });
 
     if (numString !== '') {
       numSum += Number(numString);
       this.validator.checkValidCalculateResult(numSum);
     } else if (numString === '' && numSum === 0) {
+    } else if (numString[0] === '0') {
+      throw new Error(MESSAGES.ERROR.INPUT_NEGATIVE_NUMBER);
     } else {
       throw new Error(MESSAGES.ERROR.END_TO_SEPARATOR);
     }
