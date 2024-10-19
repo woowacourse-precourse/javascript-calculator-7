f59c230a2b456fd66646c3e41458e3f173cd93e9
Jeong-Ag <kjungu1105@gmail.com>
2024-10-19 21:11:21 +0900

    feat(App): add string input and output
    형식에 맞추어 문자열을 입력받고 계산 결과를 출력합니다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import calculator from "./calculator.js";
+
 class App {
-  async run() {}
+  async run() {
+    const operand =
+      await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해주세요\n"
+      );
+    const result = calculator(operand);
+    MissionUtils.Console.print("결과 : " + result);
+  }
 }
 
 export default App;
