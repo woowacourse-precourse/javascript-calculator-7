21368be2256bb574c61a4e7dae90efbf98f9441a
YeWon Seo <syw1236@naver.com>
2024-10-19 19:11:14 +0900

    feat: 사용자 입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import {Console} from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+
+  async run() {
+    const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+
+  }
 }
 
 export default App;
