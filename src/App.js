62ad78f1044317c3a599cefbb5a51b99fc681d59
COMPOSEDKIM <composedkim@naver.com>
2024-10-19 17:11:12 +0900

    feat: 사용자에게 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userString = await this.getUserString();
+    // MissionUtils.Console.print(userString);
+  }
+
+  //문자열 입력 받기
+  async getUserString() {
+    try {
+      const userString = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요 \n"
+      );
+      return userString;
+    } catch (error) {}
+  }
 }
 
 export default App;
