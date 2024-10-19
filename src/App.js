b50673361234565ddfd071eb8011f40e33c9b1e1
hyomin <leehm123456@gmail.com>
2024-10-19 18:24:07 +0900

    feat: add string input function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  getInputString() {
+    const inputString =
+      Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
+    return inputString;
+  }
+
+  async run() {
+    const inputString = await this.getInputString();
+  }
 }
 
 export default App;
