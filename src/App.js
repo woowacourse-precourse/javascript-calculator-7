6b19869c131d405e2c67e5d759311674d449183e
leejin_rho <tbdpapdl@gmail.com>
2024-10-20 00:58:01 +0900

    feat: 덧셈 계산기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console } from "@woowacourse/mission-utils";
+
+
 class App {
-  async run() {}
+  async run() {
+    Console.print("덧셈할 문자열을 입력해주세요.")
+    const userInput = await this.getUserInput();
+    const result = this.sumNumbers(userInput);
+    Console.print(`결과 : ${result}`);
+
+  }
+
+  async getUserInput() {
+    return await Console.readLineAsync("");
+  }  
+
+  sumNumbers(input) {
+  if (input === "") {
+    return 0;
+  }
+
+  const numbers = input.split(/[,:]/).map(Number);
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+
 }
 
 export default App;
+
+
+// 덧셈할 문자열을 입력해 주세요.
+// 1,2:3
+// 결과 : 6
