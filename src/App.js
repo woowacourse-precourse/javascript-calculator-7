4b352bd744299b5f8da73976a4fef1a5a7716ad9
dmson1218 <dmson1218@naver.com>
2024-10-19 13:15:51 +0900

    feat(App): 에러 발생 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
 
+// 요구 사항에 기재되지 않은 내용 판단
+// 1. 구분자로 시작하거나 끝나는 경우 -> False
+// 2. 앞에 무의미한 0이 붙는 경우 -> 일단.. True
+
 class App {
     async run() {
-        const input = await this.getUserInput();
-        Console.print(this.isValidInput(input));
+        try {
+            const input = await this.getUserInput();
+            this.validateInput(input);
+        } catch (error) {
+            Console.print(error.message);
+        }
     }
 
     async getUserInput() {
@@ -11,17 +19,21 @@ class App {
             const userInput = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
             return userInput;
         } catch (error) {
-            throw new Error("[Error]");
+            this.throwError("입력값을 받아오는 데 실패했습니다.");
         }
     }
 
-    isValidInput(input) {
-        if (input === null || input === undefined || input === "") {
-            return "0";
-        }
+    validateInput(input) {
         // 구분자와 양수로 구성된 문자열인지 검증하는 정규표현식
         const regex = /^[0-9]+([,:][0-9]+)*$/;
-        return regex.test(input);
+        if (input !== "" && !regex.test(input)) {
+            this.throwError("입력값이 유효하지 않습니다.");
+        }
+    }
+
+    throwError(message) {
+        // 굳이 함수화가 필요할까?
+        throw new Error(`[ERROR] ${message}`);
     }
 }
 
