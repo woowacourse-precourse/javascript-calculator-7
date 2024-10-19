acc1e1160f3ac54d1151a51a38a8ff5c634fad2a
kimkyuhong <bobo3766@naver.com>
2024-10-20 01:35:15 +0900

    feat(StringStorage.js): separator, string 관리하고 유효성 검사 실행하는 StringStorage Class구현

--- src/StringStorage/StringStorage.js
+++ src/StringStorage/StringStorage.js
@@ -3,4 +3,30 @@ export class StringStorage {
     this.separator = separator;
     this.string = string;
   }
+
+  get separator() {
+    return this._separator
+  }
+
+  get string() {
+    return this._string
+  }
+
+  set separator(separator) {
+    this._separator = separator;
+  }
+
+  set string(string) {
+    const validate =
+      /^\s*$/.test(string) || /(?<!\d)-[1-9]\d*|\b-?0+\b/g.test(string);
+
+    if (validate) {
+      throw new Error("[ERROR]");
+    }
+    this._string = string;
+  }
+
+  getSeparatorArray() {
+    return this._separator.split("");
+  }
 }
