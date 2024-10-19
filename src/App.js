202e3d686bca5a6125bfe9db8be1177ca837b99b
Kim_Yjoo <pas4976@gmail.com>
2024-10-19 19:43:23 +0900

    feat(doSum) : 구분자로 구분되는 숫자들의 합을 구하는 함수 구현
    인자로 유저 입력과 커스텀 구분자를 포함하는 구분자 집합을 넘겨준다.
    - 유저 입력 문자열을 문자 하나씩 순회하면서 숫자인지 구분자인지 검사
    - 숫자일 경우 문자열 변수 주머니 말단에 합친다.
    - 문자일 경우 구분자인지 확인하여, 구분자이면 현재까지 모인 문자열 주머니에 담긴 숫자를 결과 변수에 더한다.
    - 구분자가 아니면 에러 발생

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ class App {
   async run() {
     
     let userInput;
-    let result = 0;
+    let result;
     while(true){
       let spliter = new Set([",",":"]);
-      userInput = await Console.readLineAsync('');
+      userInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
       if(!this.isEmpty(userInput)) {
         userInput = this.checkCustomSpliter(userInput, spliter);
-        
+        this.doSum(userInput, spliter);
       }
 
     }
@@ -21,8 +21,9 @@ class App {
   isEmpty(userInput) {
     return userInput === '';
   }
+
   checkCustomSpliter(userInput, spliter) {
-    if(userInput.substring(0,2) === "//"){
+    if(userInput.includes("//")){
       let indexOfEnd = userInput.indexOf('\\n');
       if(indexOfEnd === -1) {
         throw new Error("[ERROR] 잘못된 문장. (커스텀 문장 추가 에러)"); 
@@ -33,6 +34,29 @@ class App {
       return userInput.substring(indexOfEnd + 2, userInput.length);
     }
     return userInput;
+  }
+
+  doSum(userInput, spliter) {
+    let result = 0;
+    let tmp = "";
+    for(let idx of userInput) {
+      
+      
+      if(!isNaN(idx)) {
+        tmp += idx;
+      }
+      else{
+        if(!spliter.has(idx)) {
+          Console.print(idx in spliter);
+          throw new Error("[ERROR] 잘못된 문장. (구분자가 아닌 문자 사용)");
+        } 
+        result += Number(tmp)
+        tmp = "";
+      }
+      
+    }
+    result += Number(tmp);
+    Console.print("결과 : " + result);
   }
 }
 
