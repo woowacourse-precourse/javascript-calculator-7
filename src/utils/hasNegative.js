0ef52cb8afa957af15f620327a99428321f2a47a
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-19 20:59:32 +0900

    refactor(utils): hasNegative 유틸 함수가 boolean 타입을 반환하도록 수정

--- src/utils/hasNegative.js
+++ src/utils/hasNegative.js
@@ -1,8 +1,4 @@
 export function hasNegative(str) {
 	const regex = /-\d+/;
-	const hasNegative = regex.test(str);
-	if (hasNegative) {
-		throw new Error('[ERROR]: 양수만 입력할 수 있어요.');
-	}
-	return;
+	return regex.test(str);
 }
