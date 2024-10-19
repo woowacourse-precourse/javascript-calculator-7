47347f1dae86ce4a34661d5acc3fd00a51422e49
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-10-19 22:20:52 +0900

    feat: 문자열을 구분자를 기준으로 나누고, 더한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해주세요\n");
+      this.calculate(input);
+    } catch(error) {
+      MissionUtils.Console.print(`Error: ${error.message}`);
+    }
+  }
+
+  calculate = (text) => {
+    let answer = 0;
+    const splited = text.split(/,|:/);
+    for(let num of splited){
+      if(isNaN(Number(num))){
+        throw new Error(`'${num}' is not a valid number.`);
+      }
+      answer += Number(num);
+    }
+    MissionUtils.Console.print(answer);
+  }
 }
 
 export default App;
