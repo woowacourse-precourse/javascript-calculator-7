aab92084297e9f1bfd0b1f670f879f248cfa1815
Sohee <sh11govlday@gmail.com>
2024-10-19 10:15:26 +0900

    feat : add function to split string by default delimiter

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ class App {
       const input = await Console.readLineAsync(
         '덧셈할 문자열을 입력해 주세요.\n'
       );
-      Console.print(input);
+
+      let numArr = [];
+      numArr = this.splitByDelimiter(input);
+      Console.print(numArr);
     } catch (error) {
       Console.print('에러:', error);
     }
+  }
+
+  splitByDelimiter(input) {
+    return input.split(/,|:/).map(Number);
   }
 }
 
