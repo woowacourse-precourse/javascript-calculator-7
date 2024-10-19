68e2ac5e90f3e809b7d1beaa3426dba9a34faffa
kmk01 <144207120+KMKang01@users.noreply.github.com>
2024-10-19 18:23:44 +0900

    feat(기본 구분자): 사용자에게 문자열을 입력 받고 기본 구분자로 나눈 뒤 결과를 출력

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
-async function getInput() {
-  return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-}
+const getInput = async () =>
+  Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
-function splitInput(str) {
-  let arr = "";
-  if (str.charAt(0) == "/") {
-    // 문장 시작이 / 인 경우 - 커스텀 구분자
-    const customSep = str.charAt(2); // 커스텀 구분자 뽑아내기
-    const newStr = str.slice(5); // 문자 5개 스킵하고 새로운 문자열 (예시 형태: //;\n1;2;3 => 1;2;3)
-    arr = [...newStr];
+const processInput = async (str) => {
+  if (str === '') return 0;
+  const sepArr = [',', ':'];
 
-    console.log(arr);
-  } else {
-    // 문장 시작이 /가 아닌 경우
-    arr = str.split(/[,:]/g);
+  const arr = splitAndValidateNumbers(str, sepArr);
+  if (arr !== null) return sumNumbers(arr);
+  throw new Error('[ERROR]');
+};
+
+const splitAndValidateNumbers = (str, sepArr) => {
+  const regex = new RegExp(`[${sepArr.join('')}]`);
+  const checkExclude = new RegExp(`[^0-9${sepArr.join('')}]`);
+  const inputStr = sepArr.length === 3 ? str.slice(5) : str;
+
+  if (!checkExclude.test(inputStr)) {
+    return inputStr.split(regex);
   }
+  throw new Error('[ERROR]');
+};
 
-  return arr;
-}
+const sumNumbers = (arr) => arr.reduce((result, e) => result + parseInt(e), 0);
 
 class App {
   async run() {
     const input = await getInput();
-    console.log(input);
-    console.log(splitInput(input));
+    const result = await processInput(input);
+    Console.print('결과 : ' + result);
   }
 }
 
