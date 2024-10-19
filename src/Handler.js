683c83a0607b5ee4ed4ea10ed9648f30ad808c5a
siggu <m0_w_0m@naver.com>
2024-10-19 21:40:47 +0900

    feat: 문자열 검사 로직 변경
    - 부동 소수점 판별 삭제
    - 구분자 핸들러 로직 변경
      - 인자 변경
      - 양수 검사 로직 추가

--- src/Handler.js
+++ src/Handler.js
@@ -8,44 +8,43 @@ class Handler {
 
   // 숫자일 때
   handleNumberInput(input, userInput, numString, idx) {
-    if (input === '0' && numString === '' && userInput[idx + 1] !== '.') {
-      throw new Error(MESSAGES.ERROR.INPUT_NEGATIVE_NUMBER);
-    }
     numString += String(input);
     return { numString };
   }
 
   // 숫자가 아닐 때
-  handleSeparatorInput(input, hasDecimalPoint, numString, separator, assignCustomSep, numSum) {
-    if (input === '.' && !hasDecimalPoint) {
-      hasDecimalPoint = true;
-      numString += '.';
-    } else if (!separator.includes(input) && !assignCustomSep.includes(input)) {
+  handleSeparatorInput(input, userInput, numString, useCustomSep, allowedSep, numSum, idx) {
+    if (this.validator.checkSeparator(useCustomSep, allowedSep, input, userInput, idx)) {
       throw new Error(MESSAGES.ERROR.INPUT_BASIC_OR_CUSTOM_SEPARATOR);
-    } else if (input === '.' && hasDecimalPoint) {
-      throw new Error(MESSAGES.ERROR.INPUT_CONTINUOUS_DOTS);
-    } else {
-      numSum += Number(numString);
-      numString = '';
-      hasDecimalPoint = false;
     }
-    return { numSum, numString, hasDecimalPoint };
+
+    if (numString[0] === '0') {
+      throw new Error(MESSAGES.ERROR.INPUT_NEGATIVE_NUMBER);
+    }
+
+    numSum += Number(numString);
+    numString = '';
+
+    if (numSum === 0 && !useCustomSep) {
+      throw new Error(MESSAGES.ERROR.INPUT_NEGATIVE_NUMBER);
+    }
+
+    return { numSum, numString };
   }
 
   // 전체 문자열 검사
-  handleUserInput(input, userInput, numString, idx, hasDecimalPoint, separator, allowedSeps, numSum) {
+  handleUserInput(useCustomSep, customSep, allowedSep, input, userInput, idx, numString, numSum) {
     if (!isNaN(input) && input.trim() !== '') {
       const numResult = this.handleNumberInput(input, userInput, numString, idx);
       numString = numResult.numString;
     } else {
-      const sepResult = this.handleSeparatorInput(input, hasDecimalPoint, numString, separator, allowedSeps, numSum);
+      const sepResult = this.handleSeparatorInput(input, userInput, numString, useCustomSep, allowedSep, numSum, idx);
       numSum = sepResult.numSum;
       this.validator.checkValidCalculateResult(numSum);
       numString = sepResult.numString;
-      hasDecimalPoint = sepResult.hasDecimalPoint;
     }
 
-    return { numSum, numString, hasDecimalPoint };
+    return { numSum, numString };
   }
 }
 
