25429e93ec116d3cf1c5144dd3c4fe9d25f83844
jeongwoo903 <www8565@naver.com>
2024-10-19 09:44:23 +0900

    feat: 기본적인 MVC 구조 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Model from './Model.js';
+import ViewIn from './ViewIn.js';
+import ViewOut from './ViewOut.js';
+import Controller from './Controller.js';
+
 class App {
-  async run() {}
+  async run() {
+    const model = new Model();
+    const viewIn = new ViewIn();
+    const viewOut = new ViewOut();
+    const controller = new Controller();
+
+    await controller.control(model, viewIn, viewOut);
+  }
 }
 
 export default App;
---
+++ src/Controller.js
@@ -0,0 +1,8 @@
+export default class Controller {
+  async control(model, viewIn, viewOut) {
+    // Todo: 사용자의 입력값 받기
+    // Todo: 입력값으로 부터 숫자 추출하기
+    // Todo: 결과값 구하기
+    // Todo: 사용자에게 결과값 보여주기
+  }
+}
