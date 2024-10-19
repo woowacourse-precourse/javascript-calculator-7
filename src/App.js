348f00212826d18d3de0f6051286bc37671de529
jjeongsu <jeongsu.dev@gmail.com>
2024-10-19 21:11:42 +0900

    feat: 문자열 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import CALCULATOR_MESSAGE from '../src/constants/CalculatorMessage.js';
+import { Console } from '@woowacourse/mission-utils';
+
+class Io {
+  async getString() {
+    return await Console.readLineAsync(CALCULATOR_MESSAGE.start);
+  }
+  printResult(value) {
+    return Console.print(CALCULATOR_MESSAGE.result + value);
+  }
+}
+
 class App {
-  async run() {}
+  constructor() {
+    this.ioHandler = new Io();
+  }
+  async run() {
+    let input = await this.ioHandler.getString();
+  }
 }
 
 export default App;
