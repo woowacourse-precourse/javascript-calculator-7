21e697f1679dc28afc8a5fd71870325aa5ddab50
erica-jeong <wjddbwjd1205@gmail.com>
2024-10-20 02:01:27 +0900

    feat: 구분자를 기준으로 숫자 추출 기능 추가
    - getNumbers 함수에서 구분자로 문자열을 분리해 숫자를 추출
    - 연속된 구분자로 인한 빈 문자열 제거
    - validateNumbers 함수에서 예외상황 처리
    - 숫자가 없는 경우, 소숫점 숫자가 추출된 경우, 구분자 외의 잘못된 문자가 포함된 경우 예외 처리 추가

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,20 +6,26 @@ class Calculator {
   constructor() {
     this.outputHandler = new OutputHandler();
   }
-  
   calculate(inputString) {
-      const isCustomerSeparator = this.determineSeparator(inputString);
-      if (isCustomerSeparator) {
-        console.log(isCustomerSeparator);
-        const customSeparator = DEFAULT_SEPARATOR.push(this.getCustomSeparator(inputString));
-      } else {
-        console.log(isCustomerSeparator);
-      }
+    const isCustomerSeparator = this.determineSeparator(inputString);
+    let numbers;
+
+    if (isCustomerSeparator) {
+      const separator = this.getCustomSeparator(inputString);
+      const customSeparators = [separator, ...DEFAULT_SEPARATOR];
+      const newString = inputString.substring(5); //커스텀 구분자를 표시하는 부분 제거
+
+      numbers = this.getNumbers(newString, customSeparators);
+
+    } else {
+      numbers = this.getNumbers(inputString, DEFAULT_SEPARATOR);
+
+    }
   }
 
   determineSeparator(inputString = '') {
     if (inputString.startsWith('//') && inputString.indexOf('\\n') > 2) {
-      return true; 
+      return true;
     } else if (DEFAULT_SEPARATOR.some(separator => inputString.includes(separator))) {
       return false
     } else {
@@ -30,8 +36,9 @@ class Calculator {
   getCustomSeparator(inputString = '') {
     const newIndex = inputString.indexOf('\\n');
     const customSeparator = inputString.substring(2, newIndex);
-    console.log(customSeparator);
-    this.validateCustomSeparator(customSeparator); 
+
+    this.validateCustomSeparator(customSeparator);
+
     return customSeparator;
   }
 
@@ -39,6 +46,47 @@ class Calculator {
     if (separator.length !== 1) {
       throw new Error("커스텀 구분자는 하나의 문자여야 합니다.");
     }
+  }
+
+  getNumbers(inputString = '', separators = []) {
+    let splitResults = [inputString];
+
+    separators.forEach(separator => {
+      splitResults = splitResults.flatMap(part => part.split(separator));
+    });
+
+    // 빈 문자열 제거
+    splitResults = splitResults.filter(item => item.trim() !== '');
+    this.validateNumbers(splitResults);
+
+    return splitResults
+  }
+
+  validateNumbers(splitResults) {
+    // 숫자가 없는 경우
+    if (splitResults.length === 0) {
+      throw new Error("숫자가 포함되어야 합니다.");
+    }
+
+    // 소숫점 숫자를 포함하는지 검증하고 예외 처리
+    splitResults.forEach(item => {
+      const number = Number(item);
+
+      // 숫자로 변환할 수 없는 값이 있으면 예외 처리
+      if (isNaN(number)) {
+        throw new Error("잘못된 문자가 포함되었습니다.");
+      }
+
+      // 소숫점 숫자 예외 처리
+      if (item.includes('.')) {
+        throw new Error("소숫점 숫자는 허용되지 않습니다.");
+      }
+    });
+  }
+
+
+  doAddition() {
+
   }
 }
 
