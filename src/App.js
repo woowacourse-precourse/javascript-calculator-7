ff192250bb1e5db797fb5b086181baca08c9e192
ThinkMuk <b2000kang@gmail.com>
2024-10-19 19:36:55 +0900

    feat(input): implement input string for number extraction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    try {
+      const result = this.add(input);
+      Console.print(`결과 : ${result}`);
+    } catch (err) {
+      Console.print(error.message);
+    }
+  }
+  add(input) {}
 }
 
 export default App;
