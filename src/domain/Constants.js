64787046cd08d05bd4b97672e7551c4f63cc9323
ho8ae <xogh2242@gmail.com>
2024-10-19 23:14:09 +0900

    refactor : 상수 객체에 Object.freeze 적용

--- src/domain/Constants.js
+++ src/domain/Constants.js
@@ -1,23 +1,22 @@
-export const ErrorMessage = {
+export const ErrorMessage = Object.freeze({
     NEGATIVE_NUMBER: '[ERROR] 음수는 허용되지 않습니다.',
     EMPTY_NUMBER: '[ERROR] 빈 숫자는 허용되지 않습니다.',
     INVALID_NUMBER: '[ERROR] 유효하지 않은 숫자입니다.',
     INVALID_CUSTOM_DELIMITER_FORMAT: '[ERROR] 올바르지 않은 커스텀 구분자 형식입니다.',
-};
+});
 
-export const GuideMessage = {
+export const GuideMessage = Object.freeze({
     START_CALCULATION: '덧셈할 문자열을 입력해 주세요.',
     RESULT: '결과 : ',
-};
+});
 
-
-export const Delimiter = {
+export const Delimiter = Object.freeze({
     DEFAULT: /[,:]/,
     CUSTOM_PREFIX: '//',
     CUSTOM_SUFFIX: '\\n',
-};
+});
 
-export const StaticNumber = {
+export const StaticNumber = Object.freeze({
     INITIAL_SUM: 0,
     RADIX: 10,
-};
+});
