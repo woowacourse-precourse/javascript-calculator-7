2f200d5c9c4c67a9236d156cf6768763284138e6
Eunicekk <cho_lyn@naver.com>
2024-10-19 22:06:09 +0900

    [feat] 빈 문자열 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
 	async run() {
-		// 1. 문자열 입력 기능 구현
+		// 문자열 입력 기능 구현
 		Console.print("덧셈할 문자열을 입력해 주세요.");
 		const input = await Console.readLineAsync("");
-		Console.print(input);
+	}
+
+	// add: 입력 문자열에서 숫자를 분리하고 합산하는 메서드
+	add(input) {
+		// 빈 문자열 처리 기능 구현
+		if (input === "") return 0;
+
+		// 구분자 기준 문자열 처리 기능 구현
+
+		// 숫자 유효성 검사 기능 구현
+
+		// 숫자 합산 기능 구현
 	}
 }
 
