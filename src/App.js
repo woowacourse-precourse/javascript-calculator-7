7ee1ad7a3af4f4f75de5a5bfa14bb877bb1261ca
SNXWXH <snxwxh@gmail.com>
2024-10-19 23:47:18 +0900

    feat(app): add separator validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import checkSeparator from './checkSeparators.js';
 
 class App {
   async run() {
@@ -6,9 +7,14 @@ class App {
       '덧셈할 문자열을 입력해 주세요.\n',
     );
     const TRIM_INPUT_MESSAGE = INPUT_MESSAGE.trim();
+    let separators = /[,:]/;
 
     if (TRIM_INPUT_MESSAGE === '' || typeof TRIM_INPUT_MESSAGE !== 'string') {
       throw new Error('[ERROR] 입력값이 올바르지 않습니다.');
+    }
+
+    if (TRIM_INPUT_MESSAGE.startsWith('//')) {
+      separators = checkSeparator(TRIM_INPUT_MESSAGE);
     }
   }
 }
---
+++ src/checkSeparators.js
@@ -0,0 +1,11 @@
+const checkSeparators = input => {
+  const match = input.match(/\/\/(.+)\\n/);
+
+  if (match === null) {
+    throw new Error('[ERROR] 유효하지 않은 구분자입니다.');
+  }
+
+  return new RegExp(`[${match[1]},;]`);
+};
+
+export default checkSeparators;
