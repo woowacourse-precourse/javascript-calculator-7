7cbeb6f3434176b508cb52bc2df69995c5419a7a
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-19 19:39:42 +0900

    feat(method): extract custom separator and numString in input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import { Console } from '@woowacourse/mission-utils'
 class App {
   async run() {
     const inputs = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    this.extractSeparatorsAndNumString(inputs);
+  }
+
+  extractSeparatorsAndNumString(inputs) {
+    const separatorRegex = /\/\/(.*?)\\n(.*)/;
+    const matched = inputs.match(separatorRegex);
+
+    return {
+      separator: matched?.[1] ?? null,
+      numString: matched?.[2] ?? inputs
+    }
   }
 }
 
