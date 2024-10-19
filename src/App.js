c24894201d3b0b6288119570af5677efd5bafa5f
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-19 17:02:43 +0900

    feat: 입력값이 공백일 경우

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 import { GAME_MESSAGES } from "./constants/gameMessages.js";
+import splitNumber from "./utils/splitNumber.js";
+import validateNumber from "./utils/validateNumber.js";
 
 class App {
   async run() {
     const userInput = await Console.readLineAsync(GAME_MESSAGES.START);
+
+    const numbers = splitNumber(userInput);
+
     Console.print(GAME_MESSAGES.END + result);
   }
 }
---
+++ src/utils/splitNumber.js
@@ -0,0 +1,9 @@
+import { ERROR_MESSAGES } from "../constants/errorMessages.js";
+const splitNumber = (input) => {
+  // 공백일 경우 0을 반환
+  if (!input || input.trim() === "") {
+    return [0];
+  }
+};
+
+export default splitNumber;
