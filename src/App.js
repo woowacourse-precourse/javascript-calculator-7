932bd0bb066c98f19ec29a247b11f38018f20927
hummingbbird <pop9814@kookmin.ac.kr>
2024-10-19 12:27:01 +0900

    feat: 사용자 입력 받는 기능 추가
    Added new event to $run:
    - MissonUtils 라이브러리를 통해 콘솔 입력 받음
    - 입력 받은 문자열은 상수로 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const TARGET_STRING = MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    
+  }
 }
 
 export default App;
