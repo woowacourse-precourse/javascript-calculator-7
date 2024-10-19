7e632fe9e8b80dc4eb02f9a3a04fee6982c89860
jeongwoo903 <www8565@naver.com>
2024-10-19 10:22:16 +0900

    feat: 유저의 입력값을 받아오는 `getInput()`매서드 구현

--- src/Controller.js
+++ src/Controller.js
@@ -1,6 +1,7 @@
 export default class Controller {
   async control(model, viewIn, viewOut) {
-    // Todo: 사용자의 입력값 받기
+    const input = await viewIn.getInput();
+
     // Todo: 입력값으로 부터 숫자 추출하기
     // Todo: 결과값 구하기
     // Todo: 사용자에게 결과값 보여주기
--- src/ViewIn.js
+++ src/ViewIn.js
@@ -1,5 +1,9 @@
-export default class ViewIn {
-   getInput() {
+import { Console } from '@woowacourse/mission-utils';
 
+export default class ViewIn {
+
+   async getInput() {
+      Console.print('덧셈할 문자열을 입력해 주세요.');
+      return await Console.readLineAsync('');
    }
 }
