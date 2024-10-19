355446e903772a059979b0aa1e99341f47e132fc
hyeonjin6530 <cola1876@naver.com>
2024-10-19 23:04:43 +0900

    feat: 커스텀 지정자 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,21 +8,38 @@ class App {
       Console.print(`결과 : ${result}`);
     } catch (error) {
       Console.print(error.message);
+      throw error;
     }
   }
 
   calculateSum(input) {
     if (!input) return 0;
 
-    const numbers = input.split(/[,|:]/).map((value) => {
-      const number = Number(value);
-      if (isNaN(number)) {
+    const { numbers } = this.parseInput(input);
+
+    return numbers.map((value) => {
+      const number = Number(value.trim());
+      if (isNaN(number) || number < 0) {  // 잘못된 입력(음수) 처리
         throw new Error('[ERROR] 입력이 잘못되었습니다.');
       }
       return number;
-    });
+    }).reduce((sum, number) => sum + number, 0);
+  }
 
-    return numbers.reduce((sum, number) => sum + number, 0);
+  parseInput(input) {
+    let delimiter = /[,:]/; // 기본 구분자
+    let numbersPart = input;
+
+    // 커스텀 구분자 처리
+    if (input.startsWith('//')) {
+      const customDelimiter = input[2]; // 구분자가 될 3번째 문자 추출
+      const escapedDelimiter = customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // 특수 문자 이스케이프(하지 않으면 오류 발생)
+      delimiter = new RegExp(`[${escapedDelimiter}]`);
+      numbersPart = input.slice(5); // 계산될 숫자 부분 추출
+    }
+
+    const numbers = numbersPart.split(delimiter);
+    return { numbers };
   }
 }
 
