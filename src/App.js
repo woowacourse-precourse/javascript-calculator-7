31cc0b23a13be1838096dbb30a203db277e037d2
Sohee <sh11govlday@gmail.com>
2024-10-19 19:02:49 +0900

    fix : add exception handling for empty input string

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      const input = await Console.readLineAsync(
+      let input = await Console.readLineAsync(
         '덧셈할 문자열을 입력해 주세요.\n'
       );
+      input = input.trim();
 
       let numArr = [];
-      if (input.startsWith('//')) {
+      if (input.length === 0) {
+        this.throwError('빈 문자열');
+      } else if (input.startsWith('//')) {
         numArr = this.splitByCustom(input);
       } else {
         numArr = this.splitByDelimiter(input);
@@ -25,6 +28,8 @@ class App {
     switch (message) {
       case '음수':
         Console.print(`[ERROR] : 구분자와 양수로 구성된 문자열이어야 합니다.`);
+      case '빈 문자열':
+        Console.print(`[ERROR] : 빈 문자열입니다.`);
     }
     throw new Error('[ERROR]');
   }
