56633a7a338e815a29079c1cefb5151086b72483
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-10-20 02:18:19 +0900

    :sparkles: feat(App): 커스텀 구분자 추출
    커스텀 구분자를 추출하고 구분자 배열에 추가한다.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
-    this.separators = [',', ':']; // App.js 실행시 생성자를 통해 ,와 :를 구분자 배열에 추가한다.
+    this.defaultSeparators = [',', ':']; // App.js 실행시 생성자를 통해 ,와 :를 구분자 배열에 추가한다.
   }
 
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+  }
+
+  // 입력값을 파싱하여 구분자와 숫자 추출
+  parseInput(input) {
+    const separators = [...this.defaultSeparators]; // 기본 구분자 추가
+
+    // 커스텀 구분자 처리
+    if (input.startsWith('//')) {
+      const customIndex = input.indexOf('\n');
+      if (customIndex !== 3) {
+        throw new Error('[ERROR] 커스텀 구분자를 잘못 입력했습니다.');
+      }
+      // 커스텀 구분자 추출
+      const customSeparator = input.substring(2, 3);
+      separators.push(customSeparator);
+      // 커스텀 구분자 이후 문자열 추출
+      mainString = input.substring(customIndex + 1);
+    }
+    return { separators, mainString };
   }
 }
 
