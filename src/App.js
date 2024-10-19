09710181d84c8cb6d4f0f756c7f47c7999a10808
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-19 16:15:38 +0900

    feat: 사용자 입출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GAME_MESSAGES } from "./constants/gameMessages.js";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await Console.readLineAsync(GAME_MESSAGES.START);
+    Console.print(GAME_MESSAGES.END + result);
+  }
 }
 
 export default App;
