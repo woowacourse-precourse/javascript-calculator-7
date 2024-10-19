f3367f2dd9a3f6127adb8e55507d59db52b68467
최영빈 <bin778@naver.com>
2024-10-19 15:46:03 +0900

    refactor: 커스텀 구분자 추가, 구분자 확인 부분을 메서드로 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
+const ERR_MESSAGE = "[ERROR]";
 
-class strToArr {
-  async seperatorAddPart(str, idx) {
-    const SEPERATOR_ADD_STR = str.substring(0, idx + 1);
+class stringCalculate {
+  // "//"와 "\n" 사이에 위치하는 문자열끼리 배열로 분리하는 함수
+  seperatorAddPart(plus_op_str, n_idx) {
+    const SEPERATOR_ADD_STR = plus_op_str.substring(0, n_idx + 1);
 
     // 문자열에서 숫자와 영문자를 제외한 문자를 분리하고 추출
     const SPLIT_STR = [SEPERATOR_ADD_STR, "\n"];
@@ -20,19 +22,43 @@ class strToArr {
     return REPLACED_STR.split(DYNAMIC_REGEX).filter(Boolean);
   }
 
-  async calculateParts(str, idx) {
-    const CALCULATE_STR = str.substring(idx + 1);
+  // 실제 연산을 진행하는 문자열을 배열로 분리하는 함수
+  calculateParts(plus_op_str, n_idx) {
+    const CALCULATE_STR = plus_op_str.substring(n_idx + 1);
     return CALCULATE_STR.match(/\d+|[^\w\s]+/g) === null
       ? [0]
       : CALCULATE_STR.match(/\d+|[^\w\s]+/g).map((item) =>
           item.match(/\d+/) ? Number(item) : item
         );
   }
+
+  // 커스텀 구분자를 추가하는 함수
+  addSeperatorChar(added_sep, seperator_arr) {
+    for (let i = 0; i < added_sep.length; i += 3) {
+      if (
+        added_sep[i] === "//" &&
+        isNaN(Number(added_sep[i + 1])) &&
+        added_sep[i + 2] === "\n"
+      )
+        seperator_arr.push(added_sep[i + 1]);
+      else throw new Error(ERR_MESSAGE);
+    }
+  }
+
+  // 모든 문자 요소가 구분자 배열에 포함되있는 지 확인하는 함수
+  seperateCheck(calculate_part, seperator_arr) {
+    for (let PART = 0; PART < calculate_part.length; PART++) {
+      if (PART % 2 === 1 && !seperator_arr.includes(calculate_part[PART])) {
+        throw new Error(ERR_MESSAGE);
+      } else if (PART % 2 === 0 && typeof calculate_part[PART] !== "number") {
+        throw new Error(ERR_MESSAGE);
+      }
+    }
+  }
 }
 
 class App {
   async run() {
-    const ERR_MESSAGE = "[ERROR]";
     try {
       const PLUS_OP_STR = await Console.readLineAsync(
         "덧셈할 문자열을 입력해주세요.\n"
@@ -43,51 +69,31 @@ class App {
 
       // 구분자, 숫자 배열
       const SEPARATOR_ARR = [",", ":"];
-      const NUM_ARR =
-        PLUS_OP_STR.match(/\d+/g) === null
-          ? [0]
-          : PLUS_OP_STR.match(/\d+/g).map(Number);
+      const NUM_MATCH = PLUS_OP_STR.match(/\d+/g);
+      const NUM_ARR = NUM_MATCH === null ? [0] : NUM_MATCH.map(Number);
 
       // 'n' 기준 문자 인덱스 생성
       const LAST_N_CHAR_INDEX = PLUS_OP_STR.lastIndexOf("n");
 
       // '\n'를 기준으로 문자열을 나눠서 배열로 분리하기
-      const strDivisionParts = new strToArr();
-      let SEPERATOR_ADD_PART = [];
+      const strCalculate = new stringCalculate();
+      let ADDED_SEPERATORS = [];
       if (LAST_N_CHAR_INDEX > 0) {
-        SEPERATOR_ADD_PART = await strDivisionParts.seperatorAddPart(
+        ADDED_SEPERATORS = await strCalculate.seperatorAddPart(
           PLUS_OP_STR,
           LAST_N_CHAR_INDEX
         );
 
-        // '//'와 '\n' 사이에 있는 문자 추가하기
-        for (let i = 0; i < SEPERATOR_ADD_PART.length; i += 3) {
-          if (
-            SEPERATOR_ADD_PART[i] === "//" &&
-            isNaN(Number(SEPERATOR_ADD_PART[i + 1])) &&
-            SEPERATOR_ADD_PART[i + 2] === "\n"
-          )
-            SEPARATOR_ARR.push(SEPERATOR_ADD_PART[i + 1]);
-          else throw new Error(ERR_MESSAGE);
-        }
+        strCalculate.addSeperatorChar(ADDED_SEPERATORS, SEPARATOR_ARR);
       }
 
-      const CALCULATE_PART = await strDivisionParts.calculateParts(
+      const CALCULATE_PART = await strCalculate.calculateParts(
         PLUS_OP_STR,
         LAST_N_CHAR_INDEX
       );
 
-      // 마지막 배열의 모든 문자 요소가 구분자 배열에 포함되있는 지 확인
-      for (let PARTS = 0; PARTS < CALCULATE_PART.length; PARTS++) {
-        if (PARTS % 2 === 1 && !SEPARATOR_ARR.includes(CALCULATE_PART[PARTS])) {
-          throw new Error(ERR_MESSAGE);
-        } else if (
-          PARTS % 2 === 0 &&
-          typeof CALCULATE_PART[PARTS] !== "number"
-        ) {
-          throw new Error(ERR_MESSAGE);
-        }
-      }
+      // 모든 문자가 구분자 배열에 포함되있는지 확인
+      strCalculate.seperateCheck(CALCULATE_PART, SEPARATOR_ARR);
 
       // 모두 일치하면 문자열에서 추출한 모든 숫자의 합 출력
       const OP_RESULT = NUM_ARR.reduce((acc, val) => acc + val);
