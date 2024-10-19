faff926df8a9ee1f05720fb004cdeddba8be9e42
san1313 <skan134679@gmail.com>
2024-10-19 15:24:57 +0900

    refactor(perfomance): 성능향상을 위한 리팩토링
    코드 전체적으로 성능 향상을 위한 리팩토링을 진행했습니다.
    현재 테스트 케이스 상에서는 극적인 효과는 기대하기 힘든 것
    같습니다.
    - Jest 테스트 10회 평균 -
    이전 : 0.3876 s
    이후 : 0.3857 s

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
-    this.separator = [':', ','];
-    this.numbers = [];
+    this.separator = new Set([':', ',']);
   }
 
   async run() {
     // 1. 사용자에게 문자열을 입력 받는다.
     const STR = await this.readLine('덧셈할 문자열을 입력해 주세요.\n');
     // 2. 입력받은 문자열을 처리한다.
-    this.processString(STR);
-    // 3. 배열의 숫자를 모두 더해 반환한다.
-    const RESULT = this.numbers.reduce((sum, cur) => sum + cur, 0);
-    // 4. 반환된 숫자를 출력한다.
-    this.printResult(this.parseResult(RESULT));
+    // 3. 숫자를 모두 더해 저장한다.
+    const RESULT = this.processString(STR);
+    // 4. 저장된 숫자를 출력한다.
+    this.printResult(RESULT);
   }
 
   /**
@@ -28,25 +26,30 @@ class App {
 
   /**
    * 문자열을 처리하여 커스텀 구분자를 this.separator에 push하고
-   * this.separator에 저장된 구분자들을 이용해 숫자들을 분리하여 배열에 저장한다.
+   * this.separator에 저장된 구분자들을 이용해 숫자들을 분리하여 더한 후 반환한다.
    * @param str
+   * @return {number} 계산된 값
    */
   processString(str) {
     // 커스텀 구분자를 배열에 저장하고 문자열 앞부분의 //와 \n을 제거한 문자열을 반환한다.
     const STRING_AFTER_PROCESS = this.processCustomSeparator(str);
     // 커스텀 구분자를 처리 한 후 빈 문자열이라면 결과 : 0을 출력한다.
     if (STRING_AFTER_PROCESS === '') {
-      this.printResult(0);
-      return;
+      return 0;
     }
-    this.separateNumbers(STRING_AFTER_PROCESS); // 배열에 있는 모든 구분자를 통해 숫자를 분리해 배열에 저장한다.
+    // 배열에 있는 모든 구분자를 통해 숫자를 분리, 더한 후 반환.
+    const NUMBER_ARR = this.separateNumbers(STRING_AFTER_PROCESS);
+    // 일반적으로 반복은 for문이 가장 빠르다.
+    let sum = 0;
+    for (let i = 0; i < NUMBER_ARR.length; i++) sum += NUMBER_ARR[i];
+    return sum;
   }
 
   /**
    * Error를 지정된 양식으로 throw 한다.
    */
   throwError() {
-    throw new Error('[ERROR] 입력 값이 잘못되었습니다.');
+    throw new Error('[ERROR] 입력 값이 잘못되었습니다. 구분자와 양수로 이루어진 문자열을 입력해주십시오.');
   }
 
   /**
@@ -59,27 +62,33 @@ class App {
    * return = "1;2-3"
    */
   processCustomSeparator(str) {
-    const REGEX = /^\/\/.\\n/; // 문자열 앞부분에 //.\n 형식이 있는 경우
-    while (str.match(REGEX)) {
-      this.separator.push(str[2]);
-      str = str.substring(5);
+    const REGEX = /^(\/\/.\\n)+/g; // 문자열 앞부분에 //.\n 형식이 있는 경우
+    const MATCHED = str.match(REGEX);
+    let matchedString = '';
+    if (MATCHED) matchedString = MATCHED[0];
+    for (let i = 2; i < matchedString.length; i += 5) {
+      this.separator.add(matchedString[i]);
     }
-    return str;
+    return str.substring(matchedString.length);
   }
 
   /**
    * 문자열을 this.separator에 있는 구분자를 사용하여 split한다.
    * split된 배열을 검사하여 양수인지 확인하고 아닌경우 throw한다.
+   * 배열을 Float형으로 파싱하여 return한다.
    * @param str
+   * @return {number[]}
    * @throws {Error} 배열에 양수가 아닌 문자열이 포함된 경우
    */
   separateNumbers(str) {
-    const REGEX = new RegExp(`[${this.separator.join('')}]`, 'g');
+    const REGEX = new RegExp(`[${[...this.separator].join('')}]`, 'g');
     const SPLITTED_STR = str.split(REGEX);
-    SPLITTED_STR.forEach(string => {
-      this.checkValidNumber(string);
-      this.numbers.push(parseFloat(string));
-    });
+    const NUMBER_ARR = [];
+    for (let i = 0; i < SPLITTED_STR.length; i++) {
+      this.checkValidNumber(SPLITTED_STR[i]);
+      NUMBER_ARR.push(parseFloat(SPLITTED_STR[i]));
+    }
+    return NUMBER_ARR;
   }
 
   /**
@@ -87,26 +96,22 @@ class App {
    * @throws {Error} 숫자가 양수가 아닐 때, 숫자가 아닌 문자가 포함된 경우
    */
   checkValidNumber(str) {
-    const REGEX = /[^0-9.\-+]/
+    const REGEX = /[^0-9.\-+]/;
     /*
     javascript의 parse함수는 앞에서부터 parse가 가능한 부분까지 처리 후 반환한다. ex) 3a4 -> 3
     따라서 별도의 문자열 검사가 필요함.
      */
-    if (REGEX.test(str)) {
-      this.throwError();
-    }
+    if (REGEX.test(str)) this.throwError();
     const NUM = parseFloat(str);
-    if (isNaN(NUM) || NUM <= 0) {
-      this.throwError();
-    }
+    if (Number.isNaN(NUM) || NUM <= 0) this.throwError();
   }
 
   /**
    * message를 콘솔에 출력한다.
-   * @param message
+   * @param result
    */
-  printResult(message) {
-    Console.print(`결과 : ${message}`);
+  printResult(result) {
+    Console.print(`결과 : ${this.parseResult(result)}`);
   }
 
   /**
