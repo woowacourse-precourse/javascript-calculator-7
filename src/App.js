960f7c717c5eb19e83df1590b8f91465663c9ae0
Eunjin Cho <whdmswls9207@gmail.com>
2024-10-20 02:44:54 +0900

    feat: 숫자의 합 계산

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    let customSeperator = "";
+    let sum = 0;
+
     //문자열 입력받기
     let inputString = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
 
     //커스텀 구분자 저장
-    let customSeperator = "";
     if (inputString.startsWith("//") && inputString.includes("\\n")) {
       inputString = inputString.split("\\n");
       customSeperator = inputString[0].substring(2);
@@ -18,6 +20,11 @@ class App {
     //문자열에서 숫자 추출하여 배열로 저장
     const seperatorRegex = new RegExp(`[${customSeperator}:,]`);
     const numbers = inputString.split(seperatorRegex).map(Number);
+
+    //숫자의 합 계산
+    numbers.forEach((num) => {
+      sum += num;
+    });
   }
 }
 
