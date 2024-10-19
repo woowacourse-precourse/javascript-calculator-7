92b80e7b98bc012840bee23a5b19d8933de7aed8
김나연 <apinksky00@gmail.com>
2024-10-19 21:34:19 +0900

    feat(feat2): 커스텀 구분자 사용시 숫자 합 계산

--- src/App.js
+++ src/App.js
@@ -3,27 +3,48 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      /** 구분자 목록 */
-      const SEPARATORS = [",", ":"];
+      /** 기본 구분자 목록 */
+      const DEFAULT_SEPARATORS = [",", ":"];
 
       /** 입력 값 */
       const input = await Console.readLineAsync("문자열을 입력해주세요.\n");
 
+      /** 커스텀 구분자 목록 */
+      let separators = DEFAULT_SEPARATORS;
+      /** 숫자 값들 */
+      let numbers = input;
+
+      // 커스텀 구분자 처리
+      if (input.startsWith("//")) {
+        const newLineIndex = input.toString().indexOf("\\n");
+        if (newLineIndex === -1) {
+          throw new Error("[ERROR] 커스텀 구분자 형식이 올바르지 않습니다.");
+        }
+        const customSeparator = input.substring(2, newLineIndex);
+        separators = [customSeparator];
+        numbers = input.substring(newLineIndex + 2);
+      }
+
       /** 구분자 정규표현식 */
       const separatorRegex = new RegExp(
-        SEPARATORS.map((s) => `\\${s}`).join("|")
+        separators.map((s) => `\\${s}`).join("|")
       );
 
-      const numberArray = input
-        .split(separatorRegex)
-        .map(Number)
-        .filter((n) => !isNaN(n));
+      const numberArray = numbers.split(separatorRegex).map((n) => {
+        const num = Number(n);
+        if (isNaN(num)) {
+          throw new Error(`[ERROR] 유효하지 않은 입력값입니다: ${n}`);
+        }
+        return num;
+      });
 
       /** 출력 값 */
       const sum = numberArray.reduce((acc, curr) => acc + curr, 0);
 
-      Console.print(sum);
-    } catch (error) {}
+      Console.print(`결과 : ${sum}`);
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
 
