8c4f627263237bd676d9cc9352779498733f82a5
mynameSumin <msm4167@naver.com>
2024-10-19 14:22:05 +0900

    refactor: 코드 컨벤션에 따른 코드 수정

--- src/App.js
+++ src/App.js
@@ -7,28 +7,29 @@ class App {
     let sum = 0;
 
     function checkIsCorrectChar(data) {
-      for(let i = 0; i < data.length; i++){
-        if(!parseInt(data[i], 10) && !separator.includes(data[i])){
+      for (let i = 0; i < data.length; i++) {
+        if (!parseInt(data[i], 10) && !separator.includes(data[i])) {
           throw new Error('[ERROR] 숫자 및 구분자가 아닌 값을 사용하고 있습니다');
         }
       }
+
       return true;
     };
 
     function getCustomSeparator(data) {
 
       // \n이 커스텀 구분자 설정이 아닌 용도로 사용된 경우
-      if (!data.startsWith('//')){
+      if (!data.startsWith('//')) {
         throw new Error('[ERROR] 숫자 및 구분자가 아닌 값을 사용하고 있습니다');
       };
 
       // 커스텀 문자가 한 글자가 아닌 경우
-      if(data.length != 3){
+      if (data.length != 3) {
         throw new Error('[ERROR] 잘못된 커스텀 구분자입니다. 커스텀 구분자는 한 개의 문자만 설정 가능합니다.');
       }
 
       // 숫자를 커스텀 문자로 사용하려는 경우
-      if(parseInt(data[2], 10)){
+      if (parseInt(data[2], 10)) {
         throw new Error('[ERROR] 숫자는 커스텀 구분자로 사용할 수 없습니다.');
       }
 
@@ -60,12 +61,11 @@ class App {
       const partsOfData = data.split('\\n');
       getCustomSeparator(partsOfData[0]);     
       getNumberFromData(partsOfData[1]);
-    }
-    else {
+    } else {
       getNumberFromData(data);
     }
 
-      Console.print(`결과 : ${sum}`);
+    Console.print(`결과 : ${sum}`);
   }
 }
 
