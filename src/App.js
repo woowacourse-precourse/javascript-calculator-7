caf848568d52c54c1d59e36f0d8542de3233f0a7
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-19 20:42:26 +0900

    feat: 문자열을 기본구분자(, :)로 나누어 숫자 합산 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ class App {
     let input_string = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+    // 배열을 돌며 sum변수에 누적합 계산
+    input_string = input_string.split(/[,:]/);
+    let sum = 0;
+    input_string.forEach((n) => {
+      sum += Number(n);
+    });
   }
 }
 export default App;
