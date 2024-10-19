2e729dd32045c8de911ffe3b6c7828d945c1f4a7
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-10-20 06:41:16 +0900

    :sparkles: feat(App): 숫자와 구분자 분리

--- src/App.js
+++ src/App.js
@@ -27,6 +27,20 @@ class App {
     }
     return { separators, mainString };
   }
+
+  // 구분자와 숫자를 분리한다
+  static splitNumbers(mainString, separators) {
+    const separatorRegex = new RegExp(`[${separators.join('')}]`); // 구분자들을 정규형으로 나타낸다
+    const numArray = mainString.split(separatorRegex); // 숫자를 분리한다
+
+    return numArray.map(num => {
+      const number = parseInt(num, 10);
+      if (Number.isNaN(number) || number < 0) {
+        throw new Error('[ERROR] 잘못된 입력입니다. 양수를 입력해 주세요.');
+      }
+      return number;
+    });
+  }
 }
 
 export default App;
