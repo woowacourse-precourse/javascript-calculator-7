433e4f4c5548ffd901740a96b62569b50aa5dc00
DongjaJ <jdh981118@naver.com>
2024-10-19 21:47:18 +0900

    refactor: validator 클래스 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
+import Validator from "./Validator.js";
 
 class App {
   #input;
   separatorList = [",", ":"];
+  #validator;
+
+  constructor() {
+    this.#validator = new Validator();
+  }
 
   async run() {
     await this.readInput();
@@ -13,81 +19,11 @@ class App {
       "덧셈할 문자열을 입력해 주세요.\n"
     );
 
-    if (this.isInValidInput()) {
+    if (Validator.isInValid(this.#input, this.separatorList)) {
       throw new Error("[ERROR]: 에러입니다.");
     }
-  }
 
-  isInValidInput() {
-    if (this.#input.startsWith("//")) {
-      const lineBreakStringIndex = this.#input.indexOf("\\n");
-
-      if (lineBreakStringIndex !== 3) {
-        return true;
-      }
-
-      const customSeparator = this.#input[2];
-
-      if (
-        this.separatorList.includes(customSeparator) ||
-        Number.isInteger(Number(customSeparator))
-      ) {
-        return true;
-      }
-
-      this.separatorList.push(this.#input[2]);
-
-      this.#input = this.#input.substring(5);
-    }
-
-    if (
-      this.#input.replaceAll(
-        new RegExp(`[${this.separatorList.join("")}0-9]`, "g"),
-        ""
-      ).length !== 0
-    ) {
-      return true;
-    }
-
-    function generateDelimiterCombinations(
-      delimiters,
-      minLength = 2,
-      maxLength = 3
-    ) {
-      const result = [];
-
-      function backtrack(combination, start) {
-        if (combination.length >= minLength) {
-          result.push(combination.join(""));
-        }
-
-        if (combination.length === maxLength) {
-          return;
-        }
-
-        for (let i = 0; i < delimiters.length; i++) {
-          if (i !== start) {
-            // 연속된 같은 구분자 방지
-            backtrack([...combination, delimiters[i]], i);
-          }
-        }
-      }
-
-      backtrack([], -1);
-      return result;
-    }
-
-    const combinations = generateDelimiterCombinations(this.separatorList);
-
-    let isInValid = false;
-    combinations.forEach((combination) => {
-      if (this.#input.includes(combination)) {
-        isInValid = true;
-        return;
-      }
-    });
-
-    return isInValid;
+    Console.print(this.#input);
   }
 }
 
---
+++ src/Validator.js
@@ -0,0 +1,70 @@
+class Validator {
+  static isInValid(input, separatorList) {
+    if (input.startsWith("//")) {
+      const lineBreakStringIndex = input.indexOf("\\n");
+
+      if (lineBreakStringIndex !== 3) {
+        return true;
+      }
+
+      const customSeparator = input[2];
+
+      if (
+        separatorList.includes(customSeparator) ||
+        Number.isInteger(Number(customSeparator))
+      ) {
+        return true;
+      }
+
+      separatorList.push(input[2]);
+
+      input = input.substring(5);
+    }
+
+    if (
+      input.replaceAll(new RegExp(`[${separatorList.join("")}0-9]`, "g"), "")
+        .length !== 0
+    ) {
+      return true;
+    }
+
+    function generateDelimiterCombinations(
+      delimiters,
+      minLength = 2,
+      maxLength = 3
+    ) {
+      const result = [];
+
+      function backtrack(combination, start) {
+        if (combination.length >= minLength) {
+          result.push(combination.join(""));
+        }
+
+        if (combination.length === maxLength) {
+          return;
+        }
+
+        for (let i = 0; i < delimiters.length; i++) {
+          backtrack([...combination, delimiters[i]], i);
+        }
+      }
+
+      backtrack([], -1);
+      return result;
+    }
+
+    const combinations = generateDelimiterCombinations(separatorList);
+
+    let isInValid = false;
+    combinations.forEach((combination) => {
+      if (input.includes(combination)) {
+        isInValid = true;
+        return;
+      }
+    });
+
+    return isInValid;
+  }
+}
+
+export default Validator;
