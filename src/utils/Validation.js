24f2fcdcc699f5cbbdc504ea28a644622a37b6dd
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-20 04:43:46 +0900

    feat(utils) : checkErrors 함수 추가

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,1 +1,13 @@
+import Errors from "../constants/Errors";
 
+const checkErrors = (numbersArray) => {
+  if (numbersArray.some((number) => isNaN(Number(number)))) {
+    throw new Error(Errors.NOT_NUMBER);
+  }
+
+  if (numbersArray.some((number) => number < 0)) {
+    throw new Error(Errors.HAS_NEGATIVE);
+  }
+};
+
+export default checkErrors;
