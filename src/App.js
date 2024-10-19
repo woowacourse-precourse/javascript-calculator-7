425ba5b8da7427090ba3ea34d89a8bf77e650f7d
MinGu Jeong <mingu830@gmail.com>
2024-10-19 17:24:22 +0900

    feat: 문자열 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+    console.log(input);
+    
+  }
 }
 
 export default App;
