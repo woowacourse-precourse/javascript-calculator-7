3cd337b8c93bef39c4e769aa7d683e7c55e527f4
xxxjinn <tlsguswls319@naver.com>
2024-10-20 01:52:33 +0900

    feat(App): 결과 값 출력하기

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const INPUT_MESSAGE = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     let answer=999999;
-    if(INPUT_MESSAGE==="") answer=0;
+
+    if(input==="") answer=0;
+
+    const REGEX = new RegExp('[,:]');
+    answer=input.split(REGEX).map(Number).reduce((acc,cur)=>{return acc+cur},0);
 
     Console.print(`결과 : ${answer}`)
   }
