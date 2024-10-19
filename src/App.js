3acbf02543c29e3c14f870a72d81f151e4496d0f
causyj <jainayj804998@gmail.com>
2024-10-19 14:48:02 +0900

    feat(cli): add user input handling for caluator
    add feacture to handle use inuput:
    - use @woowacourse/mission-utils
    - use Console.print() for input verification
    - use Console.readLineAsync() for user input
    
    Breaks the calculator now expects string input from the user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n',
+      );
+      Console.print(`\n입력한 문자열 : ${input}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
