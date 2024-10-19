889c2f03e19056047bb7fe20c99413b0a1ff25f0
aydenote <dslgpgh@gmail.com>
2024-10-20 03:24:01 +0900

    feat(Calculator): 문자열에서 숫자 분리 기능 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,6 +8,7 @@ class Calculator {
 			const userOriginalInputValue = await this.getUserInput();
 			await validateSeparatorFormat(userOriginalInputValue);
 			await validatePositiveNumber(userOriginalInputValue);
+			const numbers = await this.getNumbers(userOriginalInputValue);
 		} catch (error) {
 			console.error(error.message);
 		}
@@ -16,6 +17,22 @@ class Calculator {
 	async getUserInput() {
 		const USER_INPUT = await userInput("덧셈할 문자열을 입력해 주세요.\n");
 		return USER_INPUT;
+	}
+
+	async getNumbers(userInputValue) {
+		const customDelimiterPattern = /^\/\/(.)\\n/;
+		let separators = [",", ":"];
+
+		const customDelimiterMatch = userInputValue.match(customDelimiterPattern);
+		if (customDelimiterMatch) {
+			separators.push(customDelimiterMatch[1]);
+			userInputValue = userInputValue.substring(customDelimiterMatch[0].length);
+		}
+
+		const delimiterRegex = new RegExp(`[${separators.join("")}]`);
+		const numbers = userInputValue.split(delimiterRegex).filter(Boolean);
+
+		return numbers;
 	}
 }
 
