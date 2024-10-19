7723db62b893e56ba925f49500c61ecd3b01b9ab
yheseul <kangyheseul@naver.com>
2024-10-19 14:13:13 +0900

    feat: 커스텀 구분자를 이용한 문자열 분리 기능 추가 (//와 n 사이 문자)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const numbersString = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    const isCustomSeparator = numbersString.includes("//") && numbersString.includes("\\n");
+    if (isCustomSeparator) {
+      Console.print("둘다있음");
+      const start = numbersString.indexOf("//");
+      const end = numbersString.indexOf("\\n") + 2;
+      const custom = numbersString.slice(start, end);
+      const customSeparator = custom.replace("//", "").replace("\\n", "");
+      const stringToSeparate = numbersString.replace(custom, "");
+      const separatedString = stringToSeparate.split(customSeparator);
+      Console.print(separatedString);
+    }
+  }
 }
 
 export default App;
