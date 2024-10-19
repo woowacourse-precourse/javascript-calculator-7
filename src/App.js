2418bd6a671f4abe7657942ae67b011ea6759527
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-10-19 19:32:23 +0900

    feat: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달한 경우
    구분자(쉼표(,), 콜론(:))를 기준으로 숫자를 분리
    분리한 각 숫자의 합을 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+    const SPLITS = INPUT.split(/,|:/);
+    Console.print(SPLITS);
+    let sum = 0;
+    for (let i = 0; i < SPLITS.length; i++) {
+      sum += parseInt(SPLITS[i]);
+    }
+    Console.print("결과 : " + sum);
+  }
 }
 
 export default App;
