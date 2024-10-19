912b19e5c2f33a0ea913dc0a772f5cc00df96fc6
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-10-19 23:45:52 +0900

    :sparkles: feat: 기본 구분자 배열에 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  constructor() {
+    this.separators = [',', ':']; // App.js 실행시 생성자를 통해 ,와 :를 구분자 배열에 추가한다.
+  }
+
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+  }
 }
 
 export default App;
