54539b1ece149ef1244ad573e8428be1a9d6d1d8
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-10-19 18:29:20 +0900

    feat: 정규표현식을 활용한 구분자 추가 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("문자열을 입력해주세요: ");
+    const regexp = /\/\/.\\n/;
+    const seperators = [",", ":"];
+    let inputString = input;
+
+    while (true) {
+      const regexpArray = regexp.exec(inputString);
+      if (regexpArray === null) {
+        break;
+      }
+      if (regexpArray.index !== 0) {
+        break;
+      }
+      seperators.push(regexpArray[0][2]);
+      inputString = inputString.replace(regexp, "");
+    }
+  }
 }
 
 export default App;
