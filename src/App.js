945def24b5ce3fd3558c2c7b499aaf73bf99317f
인호현 <inhohyun36@gmail.com>
2024-10-20 01:03:00 +0900

    feat: 문자열 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    // 문자열 입력
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    const input = await Console.readLineAsync("");
+
+    // 입력값이 비어 있는 경우 0 출력
+    if (input === "") {
+      Console.print(0);
+      return;
+    }
+  }
 }
 
 export default App;
