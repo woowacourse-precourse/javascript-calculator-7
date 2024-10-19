cf4c50b8f98d1d3f0b5f23c29359aea4b6a5da90
ThinkMuk <b2000kang@gmail.com>
2024-10-19 20:15:00 +0900

    feat(calculator): add calSum and temporary isValidNum for input summation

--- src/App.js
+++ src/App.js
@@ -9,9 +9,31 @@ class App {
       Console.print(err.message);
     }
   }
+
   add(input) {
     if (!input) {
-      return 0;
+      return 0; //빈 문자열 처리
+    }
+    const seperator = [',', ':'];
+    const numArr = input.split(new RegExp(`[${seperator.join('')}]`));
+
+    return this.calSum(numArr);
+  }
+
+  calSum(numbers) {
+    return numbers.reduce((sum, num) => {
+      const value = Number(num);
+      this.isValidNum(value); //입력값 예외처리
+      return sum + value;
+    }, 0);
+  }
+
+  isValidNum(value) {
+    if (isNaN(value)) {
+      //숫자가 아닐시 예외처리
+    }
+    if (value < 0) {
+      //음수일시 예외처리
     }
   }
 }
