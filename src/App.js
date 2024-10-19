d52ca6ff37d6e6f6f4d8f4f9cc2b7e961f2abbe9
jissssu <leeland21@naver.com>
2024-10-19 18:33:28 +0900

    feat: 기본 구분자 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    //입력받기
+    // 입력받기
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
     // 빈 문자열 처리
     if (input === "") {
       return Console.print("결과 : 0");
+    }
+
+    // 구분자 처리
+    const separators = [",", ":"]; 
+    for (const separator of separators) {
+      input = input.split(separator).join(","); 
     }
 
     Console.print(input);
