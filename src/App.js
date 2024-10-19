d42744168a5978ff150f2eceee8e80c02312a53f
ZUITOPIA <korhwngml123@naver.com>
2024-10-19 17:36:12 +0900

    feat(run): mission-utils를 사용하여 사용자 입력과 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const Console = MissionUtils.Console;
+
 class App {
-  async run() {}
+    async run() {
+        // Console.readLineAsync()와 Console.print()를 활용하여 사용자의 값 입력, 출력
+
+        const inputText = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+
+        Console.print(inputText); // input 값 확인
+    }
 }
 
 export default App;
