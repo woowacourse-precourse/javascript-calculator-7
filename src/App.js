dbc970f6a2f55232773ac268bcd53fdf0f71cb5a
kwonsoonook <sunoogy99@naver.com>
2024-10-20 03:55:37 +0900

    feat: add extractMathExpression function Implemented the extractMathExpression function to extract mathematical expressions from a given string.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,17 @@ class App {
     return DELIMETERS;
   }
 
+  // 2. 커스텀 구분자 부분 제거하기
+  extractMathExpression(inputString) {
+    // 커스텀 구분자 추출하기 위한 캡처 정의
+    const CAPTURE = new RegExp('//([^0-9])\\n', 'g');
+
+    // 커스텀 구분자 부분 제거하기
+    const MATH_EXPRESSION = inputString.replace(CAPTURE, '');
+
+    return MATH_EXPRESSION;
+  }
+
   async run() {}
 }
 
