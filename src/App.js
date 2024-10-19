6a7cdf2549b523e0287b97786e2f585f9459a9d8
심우석 <dntjr5741@naver.com>
2024-10-20 00:15:31 +0900

    feat : 구분자 및 커스텀 구분자로 문자열 분리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ class App {
     const input = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    if (input === "") {
+      MissionUtils.Console.print("결과 : 0");
+      return;
+    }
+
+    let numbers = [];
+    const customDelimiterMatch = input.match(/^\/\/(.)\\n(.*)/);
+
+    if (customDelimiterMatch) {
+      const customDelimiter = customDelimiterMatch[1];
+      const numberPart = customDelimiterMatch[2];
+      numbers = numberPart.split(new RegExp(`[${customDelimiter}]`));
+    } else {
+      numbers = input.split(/[,:]/);
+    }
+
+    console.log("분리된 숫자:", numbers);
   }
 }
 
