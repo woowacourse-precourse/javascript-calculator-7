d2d261921b3a2abf7c1c68acd4c49f39140d9c2f
jiyun1006 <jiyun4251@gmail.com>
2024-10-20 14:20:42 +0900

    ✨ feat : 문자열 검증 메서드 추가.
    문자열이 숫자인지 양수인지 구분하는 메서드 추가
    - isNumber 메서드로 숫자인지 확인, 양수인지 확인.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { MissionUtils, Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const tmp = await getInputStr();
-    console.log(tmp);
+    console.log(tmp, "마지막 콘솔");
   }
 }
 
@@ -15,16 +15,32 @@ async function getInputStr() {
     // 커스텀 구분자를 가지는지 확인.
     customBoolean = isCustom(inputStr);
 
+    if (customBoolean) { // 커스텀 구분자를 가졌을 때,
+      const tmpSep = inputStr.slice(2, 3); // 커스텀 구분자
+      inputStr = inputStr.slice(5,); // 커스텀 구분자를 제외한 문자열
+      separator.push(tmpSep);
+    }
+
+    // 구분자를 이용한 분리
+    separator.forEach((sep) => {
+      inputStr = inputStr.split(sep).join('');
+    })
+    inputStr = inputStr.split('');
+
+    console.log(inputStr);
+    // 분리된 문자열에서 조건에 맞는지 검증
+    isNumber(inputStr);
+    isNotNegative(inputStr);
     // 구분자를 이용해 문자열 구분.
     return inputStr;
   } catch (error) {
      return error;
-    // reject 되는 경우
   }
 }
 
 // 입력문에서의 에러 상황 체크
 
+// 1. 커스텀 구분자를 가지는지.
 const isCustom = (inputStr) => {
   const regExp = /(\/\/.{1}\\n){1}.{0,}/; 
   const regExp2 = /(\/\/\D{1}\\n){1}.{0,}/;
@@ -35,6 +51,21 @@ const isCustom = (inputStr) => {
     return true;
   }
   return false;
+}
+
+
+// 2. 문자열이 숫자 인지
+const isNumber = (inputStr) => {
+  for (let i = 0; i < inputStr.length; i += 1){
+    if(isNaN(inputStr[i])) throw new Error("[ERROR] 숫자가 아닙니다. or 양수가 아닙니다.")
+  }
+}
+
+// 3. 문자열에 구분자를 제외하면 양수만 있는지.
+const isNotNegative = (inputStr) => {
+  for (let i = 0; i < inputStr.length; i += 1){
+    if(0 > inputStr[i]) throw new Error("[ERROR] 양수만 사용할 수 있습니다.")
+  }
 }
 
 
