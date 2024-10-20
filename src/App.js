c8b58fd5d05aa66ddb6b157166c99965d1543cfb
RAKRAK21 <94699316+RAKRAK21@users.noreply.github.com>
2024-10-20 23:38:20 +0900

    docs: Refine test cases and error messages in README.md
    - Remove test case 6 for invalid custom delimiter format
    - Update error message descriptions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App {
       const result = this.calculateSum(input);
       Console.print(`결과 : ${result}`);
     } catch (error) {
-      Console.print("[ERROR]" + error.message);
+      Console.print(error.message);
+      throw error;
     }
   }
 
@@ -15,24 +16,34 @@ class App {
     const customDelimiterMatch = input.match(/^\/\/(.)\\n/);
     return customDelimiterMatch ? customDelimiterMatch[1] : null;
   }
-  
 
   calculateSum(input) {
     let delimiter = ',|:'
     const custom = this.getCustomDelimiter(input); // 커스텀 구분자 찾음
 
     if (custom) {
-      delimiter += '|\\' + custom; // 커스텀 구분자 추가
+      delimiter += '|' + custom; // 커스텀 구분자 추가
       input = input.replace(/^\/\/.\\n/, ''); // 커스텀 구분자 선언부 제거
     }
 
-    const numbers = input.split(new RegExp(delimiter)).map(Number); //정규표현식으로 구분자를 찾음
-    let sum1 = 0; //기본을 0으로 두고 for문을 통해 숫자 합산
+    const numbers = input.split(new RegExp(delimiter)).map(num => { //정규표현식으로 구분자를 찾음
+      const parsed = parseInt(num, 10);
+      if (isNaN(parsed)){
+        throw new Error("[ERROR] 숫자가 아닙니다.");
+      }
+      else if (parsed < 0) {
+        throw new Error("[ERROR] 음수는 허용되지 않습니다.");
+      }
+      return parsed;
+    }); 
+
+    let sum = 0; //기본을 0으로 두고 for문을 통해 숫자 합산
     for (let i = 0; i < numbers.length; i++) {
-      sum1 += numbers[i];
+      sum += numbers[i];
     }
-    return sum1;
+    return sum;
   }
+
 }
 
 export default App;
