16b601a229bb36cb83deedc30db4306c985cd2e7
luii0415 <becky45210193@gmail.com>
2024-10-21 01:03:51 +0900

    기능 2. 구분자 없이 숫자만 있는 경우, 계산된 문자열 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
+    // 사용할 변수
+    let result = 0;
     //입력 처리
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const addNumber = await Console.readLineAsync("");
+
+    // 기능 1. 구분자 없이 숫자만 입력된 경우
+    const onlyNum = /^\d+$/.test(addNumber);
+    if (onlyNum) {
+      result = addNumber;
+    }
+    Console.print(`결과 : ${result}`);
   }
 }
 
