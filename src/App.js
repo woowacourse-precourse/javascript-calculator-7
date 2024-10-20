44783e5782083cac690f45f7b6f806640dbb75bf
hwangheetae <tejaia@naver.com>
2024-10-21 01:38:16 +0900

    feat: 유저의 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
+  async getUserInput() {
+    try {
+      const userInput = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      return userInput;
+    } catch (error) {
+      Console.print(`[ERROR]`);
+    }
+  }
+
   async run() {}
 }
 
