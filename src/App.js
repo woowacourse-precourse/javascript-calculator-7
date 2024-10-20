5a9c13f074055eb9ca5bda3c46223324d8446161
luii0415 <becky45210193@gmail.com>
2024-10-21 00:46:08 +0900

    기능 1. 문자열 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    //입력 처리
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    const addNumber = await Console.readLineAsync("");
+  }
 }
 
 export default App;
