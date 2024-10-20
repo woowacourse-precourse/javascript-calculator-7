353373009f04a298aa07fe5ff4956db28e35808d
yiyujin <yujin94@gmail.com>
2024-10-20 03:09:32 -0400

    add result print format

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ class App {
   async run() {
     try {
       const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-      const result = this.calculate(input);
+      const result = this.calculate( input );
       Console.print(`결과 : ${result}`);
-    } catch (error) {
+    } catch ( error ) {
       if (!error.message.startsWith("[ERROR]")) {
         error.message = `[ERROR] ${error.message}`;
       }
