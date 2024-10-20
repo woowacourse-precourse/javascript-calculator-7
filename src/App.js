f1385153812a7b9cc1e7503ad5bdc63240e50e4f
Junseo Kim <semiwest2@gmail.com>
2024-10-21 00:52:34 +0900

    feat: App.js에서 모듈을 활용한 문자열 덧셈 계산기 로직 구현 완료
    각 모듈을 활용하여 입력 처리, 커스텀 구분자 검출, 유효성 검증, 숫자 배열 생성, 덧셈 계산 및 결과 출력 로직을 App.js에 연결.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import getInputString from './input/getUserInput.js';
+import handleCustomDelimiter from './input/CustomDelimiterHandler.js';
+import splitByDelimiters from './parser/splitByDelimiters.js';
+import isValidInput from './input/isValidInput.js';
+import calculateSum from './calculater/calculateSum.js';
+import printResult from './output/output.js';
+
 class App {
-  async run() {}
+  async run() {
+    let input = await getInputString();
+    const { cleanedString, customDelimiter } = handleCustomDelimiter(input);
+    this.checkValidInput(cleanedString, customDelimiter);
+    const parsedNumberList = splitByDelimiters(cleanedString, customDelimiter);
+    const sum = calculateSum(parsedNumberList);
+    printResult(sum);
+  }
+
+  checkValidInput(cleanedString, customDelimiter) {
+    if (!isValidInput(cleanedString, customDelimiter)) {
+      throw new Error("[ERROR] : [,][;][커스텀 구분자][숫자 문자]를 제외한 입력이 존재합니다!");
+    }
+  }
 }
 
 export default App;
