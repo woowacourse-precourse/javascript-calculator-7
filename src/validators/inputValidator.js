f0e53a79721559da86ba115e88a1cfc8aad2f3d0
silver0108 <kimsoeun0108@gmail.com>
2024-10-21 02:43:32 +0900

    feat: 입력값의 모든 유효성 검증 실행 함수

--- src/validators/inputValidator.js
+++ src/validators/inputValidator.js
@@ -1,19 +1,30 @@
 import throwError from "../utils/throwError";
 
 // 입력값이 없는 경우
-export const emptyInput = (input) => {
+const emptyInput = (input) => {
   if (!input) throwError("input is not found");
   return input;
 };
 
 // 입력값에 음수가 있는 경우
-export const negativeInput = (input) => {
+const negativeInput = (input) => {
   if (/-\d/.test(input)) throwError("input is nagative number");
   return input;
 };
 
 // "//"와 "\n" 사이에 문자가 없는 경우
-export const nullCustomDelimiter = (input) => {
+const nullCustomDelimiter = (input) => {
   if (/^\/\/\\n/.test(input)) throwError("custom delimiter is null");
+  return input;
+};
+
+// 모든 검증 실행
+export const validateInput = (input) => {
+  const validators = [emptyInput, negativeInput, nullCustomDelimiter];
+
+  validators.forEach((validator) => {
+    validator(input);
+  });
+
   return input;
 };
