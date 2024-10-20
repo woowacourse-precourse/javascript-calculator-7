a6c2a7ba7e69acaa6f182b394e20a6c73ece6690
Yoonjeong Ko <jejukyj@naver.com>
2024-10-20 22:02:21 +0900

    feat : 사용자 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      Console.print(`결과 : ${input}`);
+    } catch (error) {
+      // reject 되는 경우
+    }
+  }
 }
 
 export default App;
