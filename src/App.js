1b5dd542d74fad0e61413efdaa10f7395ca481b2
leedohyung28 <dhleee0123@gmail.com>
2024-10-20 19:21:25 +0900

    ⚡쉼표 구분 구현

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ class App {
     Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n").then(function (
       result
     ) {
-      Console.print("결과 : " + result);
+      const SEPARATED = separate(result);
+      Console.print("결과 : " + SEPARATED);
     });
   }
 }
+
+const separate = function separateCommas(input) {
+  const SEPARATED_COMMAS = input.split(",");
+  return SEPARATED_COMMAS[0];
+};
 
 export default App;
