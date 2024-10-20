f815023c68d2378a48a06b71adb8961368455e02
Osw0 <5196ksy@gmail.com>
2024-10-20 19:40:05 +0900

    feat: 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Calculator from './calculator.js';
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  constructor() {
+    this.calculator = new Calculator();
+  }
+
+  async run() {
+    const input = await Console.readLineAsync(
+      `덧셈할 문자열을 입력해 주세요.\n`
+    );
+
+    try {
+      const result = this.calculator.add(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
