ff49a3e82caf727206470d162f6f9538694bab7f
jiucchu <jiucchu@gmail.com>
2024-10-20 17:40:03 +0900

    feat: 입출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    Console.readLine('계산기 값을 입력해주세요.', (inputNumber) => {
+      Console.print(`${inputNumber}`);
+      Console.close()
+    })
+  }
 }
+
 
 export default App;
