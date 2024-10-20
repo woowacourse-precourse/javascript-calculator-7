ab09d8611eb36e95276b1d6a89ec9b633c20ea62
dmson1218 <dmson1218@naver.com>
2024-10-20 16:20:58 +0900

    test(ApplicationTest): 기본 동작 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -2,42 +2,123 @@ import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
+    MissionUtils.Console.readLineAsync = jest.fn();
 
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
+    MissionUtils.Console.readLineAsync.mockImplementation(() => {
+        const input = inputs.shift();
+        return Promise.resolve(input);
+    });
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
 };
 
 describe("문자열 계산기", () => {
-  test("커스텀 구분자 사용", async () => {
-    const inputs = ["//;\\n1"];
-    mockQuestions(inputs);
+    test("빈 문자열 입력", async () => {
+        const inputs = [""];
+        mockQuestions(inputs);
 
-    const logSpy = getLogSpy();
-    const outputs = ["결과 : 1"];
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 0"];
 
-    const app = new App();
-    await app.run();
+        const app = new App();
+        await app.run();
 
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
     });
-  });
 
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
-    mockQuestions(inputs);
+    test("쉼표 구분자 사용", async () => {
+        const inputs = ["1,2"];
+        mockQuestions(inputs);
 
-    const app = new App();
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 3"];
 
-    await expect(app.run()).rejects.toThrow("[ERROR]");
-  });
+        const app = new App();
+        await app.run();
+
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
+
+    test("콜론 구분자 사용", async () => {
+        const inputs = ["1:2"];
+        mockQuestions(inputs);
+
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 3"];
+
+        const app = new App();
+        await app.run();
+
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
+
+    test("쉼표 구분자 연속 사용", async () => {
+        const inputs = ["1,2,3"];
+        mockQuestions(inputs);
+
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 6"];
+
+        const app = new App();
+        await app.run();
+
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
+
+    test("콜론 구분자 연속 사용", async () => {
+        const inputs = ["1:2:3"];
+        mockQuestions(inputs);
+
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 6"];
+
+        const app = new App();
+        await app.run();
+
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
+
+    test("쉼표 구분자 및 콜론 구분자 동시 사용", async () => {
+        const inputs = ["1,2:3"];
+        mockQuestions(inputs);
+
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 6"];
+
+        const app = new App();
+        await app.run();
+
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
+
+    test("커스텀 구분자 사용", async () => {
+        const inputs = ["//;\\n1"];
+        mockQuestions(inputs);
+
+        const logSpy = getLogSpy();
+        const outputs = ["결과 : 1"];
+
+        const app = new App();
+        await app.run();
+
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
 });
