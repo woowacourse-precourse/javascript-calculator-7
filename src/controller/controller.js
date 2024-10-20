7b348a43c08785395cda1c9341c057e29159f19f
pjc0179 <pjc0179@gmail.com>
2024-10-20 19:57:35 +0900

    feat: "커스텀문자와 나머지를 나눠주는 함수 작성"

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,11 +1,20 @@
 import INPUT from "../view/input.js";
+import reg from "../constants/reg.js";
 
 class Controller {
   async start() {
     const userInput = await INPUT.getUserInput();
+
+    const [custom, other] = this.findCustomDelimiter(userInput);
   }
 
-  checkUserInput() {}
+  findCustomDelimiter(string) {
+    const str = string.match(reg);
+
+    return [str[1], str[2]];
+  }
+
+  checkCustomDelimiter(string) {}
 }
 
 export default Controller;
