69dcfcdc2c50c635c6b20fe0adc8d23f29fc8219
jiyeeeah <sije3757@snu.ac.kr>
2024-10-20 19:52:07 +0900

    [feat] 추가 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -41,3 +41,80 @@ describe("문자열 계산기", () => {
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
 });
+
+// 추가 테스트
+describe("추가 테스트", () => {
+  test("공백이 숫자로 주어진 경우 0으로 판단", async () => {
+    const inputs = ["1,2,,3"];
+    mockQuestions(inputs);
+
+    logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자에 문자 사용", async () => {
+    const inputs = ["//g\\n1g3:5"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 9"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("공백 무시", async () => {
+    const inputs = ["1,2, 3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
+
+describe("추가 예외 테스트", () => {
+  test("입력값에 커스텀 아닌 특수 기호 포함된 경우", async () => {
+    const inputs = ["1,2,3;5"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("숫자나 구분자 외의 문자가 포함된 경우", async () => {
+    const inputs = ["1,2,hi:3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자가 숫자인 경우", async () => {
+    const inputs = ["//3\\n1"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+});
