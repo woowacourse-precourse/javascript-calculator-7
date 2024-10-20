3f0ebcc68a321082cc168fafb6606238f4001363
salmonco <xkqjsslsek80@naver.com>
2024-10-20 18:16:43 +0900

    refactor(app): 가독성을 위해 run 메서드 내부에 정의한 함수를 클래스 메서스로 변경

--- src/App.js
+++ src/App.js
@@ -11,59 +11,58 @@ class App {
       return;
     }
 
-    const isCustomSeparator = (str) => {
-      return str.includes("//") && str.includes("\\n");
-    };
-
-    const getCustomSeparator = (str) => {
-      return str.split("//")[1].split("\\n")[0];
-    };
-
-    const parseStrWithCustomSeparator = (str) => {
-      return str.split("\\n")[1];
-    };
-
     const separators = [",", ":"];
     let str = input;
 
-    if (isCustomSeparator(input)) {
-      const customSeparator = getCustomSeparator(input);
+    if (this.isCustomSeparator(input)) {
+      const customSeparator = this.getCustomSeparator(input);
       separators.push(customSeparator);
-      str = parseStrWithCustomSeparator(input);
+      str = this.parseStrWithCustomSeparator(input);
     }
 
-    const separateStr = (str, separators) => {
-      let newStr = str;
-      separators.forEach((separator) => {
-        const regex = new RegExp(`[${separator}]`, "g");
-        newStr = newStr.replace(regex, ",");
-      });
-      const arr = newStr.split(",");
-      return arr;
-    };
+    const separatedArr = this.separateStr(str, separators);
 
-    const separatedArr = separateStr(str, separators);
-
-    const isNegativeValue = (arr) => {
-      return arr.some((value) => +value < 0);
-    };
-
-    if (isNegativeValue(separatedArr)) {
+    if (this.isNegativeValue(separatedArr)) {
       throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
     }
 
-    const sum = (arr) => {
-      return arr.reduce((acc, cur) => acc + +cur, 0);
-    };
-
-    // Console.print(separateStr(str, separators));
-    const result = sum(separatedArr);
+    const result = this.sum(separatedArr);
 
     if (Number.isNaN(result) || Number.isInteger(result) === false) {
       throw new Error("[ERROR] 잘못된 입력입니다.");
     }
 
     Console.print(`결과 : ${result}`);
+  }
+
+  isCustomSeparator(str) {
+    return str.includes("//") && str.includes("\\n");
+  }
+
+  getCustomSeparator(str) {
+    return str.split("//")[1].split("\\n")[0];
+  }
+
+  parseStrWithCustomSeparator(str) {
+    return str.split("\\n")[1];
+  }
+
+  isNegativeValue(arr) {
+    return arr.some((value) => +value < 0);
+  }
+
+  separateStr(str, separators) {
+    let newStr = str;
+    separators.forEach((separator) => {
+      const regex = new RegExp(`[${separator}]`, "g");
+      newStr = newStr.replace(regex, ",");
+    });
+    const arr = newStr.split(",");
+    return arr;
+  }
+
+  sum(arr) {
+    return arr.reduce((acc, cur) => acc + +cur, 0);
   }
 }
 
