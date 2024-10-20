bbe7127f7a9bee3295404e6d9ec7de4effb5cc4f
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-20 23:01:08 +0900

    fix: 사용자 입력 반환 방식 수정

--- src/views/CalculatorView.js
+++ src/views/CalculatorView.js
@@ -1,23 +1,16 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-
-
-export function getUserInput(callback) {
-  MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
-    .then((input) => {
-      callback(input);
-    }) // 입력된 값 callback 전달
-    .catch((error) => {
-      MissionUtils.Console.print(error);
-    }); // 에러처리
+// --- 사용자 입력 받기 ---
+export function getUserInput() {
+  return MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 }
 
-// 에러 메시지 출력 함수
+// --- 에러 메시지 출력  ---
 export function displayErrorMessage(message) {
-  MissionUtils.Console.print(`[ERROR]: ${message}`);
+  MissionUtils.Console.print(`${message}`);
 }
 
-// 결과 출력 함수
+// --- 결과 출력 ---
 export function resultOutput(result) {
   MissionUtils.Console.print(`결과 : ${result}`);
 }
