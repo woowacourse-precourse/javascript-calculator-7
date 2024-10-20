96cfab5b4c309efee4e0e007b95d0cde6c194dd6
luminox93 <memoji1236@gmail.com>
2024-10-20 22:30:19 +0900

    feat: constants 생성
    1. 에러 메세지를 담을 ERROR_MESSAGES 상수 생성
    2. 커스텀 구분자 선언부를 정규표현식화 하여 PATTERN 상수 생성

---
+++ src/constants/contants.js
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGES = {
+	INVALID_INPUT: '잘못된 입력입니다.',
+	NEGATIVE_NUMBER: '음수는 입력할 수 없습니다.',
+	INVALID_NUMBER: '숫자가 아닙니다.',
+	INVALID_SEPARATER: '잘못된 구분자입니다.',
+};
+
+export const PATTERN = /\/\/(.*?)\n/;
