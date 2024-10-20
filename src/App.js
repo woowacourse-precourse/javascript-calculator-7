bc38804b75722fd6eb691201ec9cc17d094aa190
kr-younghoon <sullanta0802@gmail.com>
2024-10-20 20:25:31 +0900

    fix(calculator): 빈 문자열 입력 시 0 반환 기능 추가
    빈 문자열이 입력된 경우 0을 반환하도록 기능을 수정했어요.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
     async run() {
         /* 
-            1. 기본 구문자를 사용하여 숫자 더하기 
-            - 쉼표와 콜론을 구분자로 사용하는 문자열에서 숫자를 추출하고 합산합니다.
-            - 예 : "1,2,3" -> 6
+            2. 빈 문자열 처리
+            - 빈 문자열이 입력된 경우 `0`을 반환합니다.
+            - 예 : "" -> 0
          */
 
         // 사용자로부터 입력(문자열)을 받음
         const INPUT = await Console.readLineAsync(
             '덧셈할 문자열을 입력해주세요.\n'
         );
+
+        // 압력이 빈 문자열인 경우, 0을 반환
+        if (INPUT.trim() === '') {
+            return Console.print('0');
+        }
 
         // 문자열을 쉼표와 콜론으로 분리하여 숫자 배열로 변환
         const NUMBERS = INPUT.split(/[,|:]/).map(Number);
