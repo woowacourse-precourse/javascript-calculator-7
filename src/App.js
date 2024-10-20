87fd3513e68c0fb3047c4dbc7961ebadda4c4957
minbo <smb5174@naver.com>
2024-10-20 15:15:55 +0900

    feat: 입력 받은 문자열의 양 끝 공백 제거

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class App {
     // 입력 받는 메세지를 먼저 출력
     Console.print("덧셈할 문자열을 입력해 주세요.");
 
-    // 다음 줄에서 입력을 받음
-    const input = await Console.readLineAsync("");
+    // 다음 줄에서 입력을 받음 + 입력 받은 문자열의 양 끝 공백 제거
+    const input = await Console.readLineAsync("").then((input) => input.trim());
   }
 }
 
