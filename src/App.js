86b088fece660caa6bc67ff0ace9b52b26ef64d0
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-20 21:34:35 +0900

    ✨feat: 입력받은 문자열을 기본 구분자를 사용해 분리하기

--- src/App.js
+++ src/App.js
@@ -6,8 +6,36 @@ class App {
       '덧셈할 문자열을 입력해 주세요.\n',
     );
 
+    if (userInputValue.trim() === '') {
+      Console.print(0);
+      return;
+    }
+
     const defaultDelimiter = /,|:/;
     const separatedValue = userInputValue.split(defaultDelimiter);
+
+    if (separatedValue.includes('')) {
+      throw new Error('[ERROR] 구분자 사이에 입력된 값이 없습니다.');
+    }
+
+    separatedValue.forEach(value => {
+      if (value !== value.trim()) {
+        throw new Error(
+          '[ERROR] 문자열을 입력하실 때 공백을 추가하시면 안됩니다.',
+        );
+      }
+
+      const number = Number(value.trim());
+
+      if (Number.isNaN(number)) {
+        throw new Error('[ERROR] 숫자가 아닌 값을 입력하시면 안됩니다.');
+      }
+
+      if (number <= 0) {
+        throw new Error('[ERROR] 양수만 입력할 수 있습니다.');
+      }
+    });
+
     const convertedNumbers = separatedValue.map(Number);
   }
 }
