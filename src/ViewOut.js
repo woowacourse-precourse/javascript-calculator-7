340e40fb247f3f173781426a3b47ba4a27c73caf
jeongwoo903 <www8565@naver.com>
2024-10-20 15:25:45 +0900

    chore: 스타일 준수

--- src/ViewOut.js
+++ src/ViewOut.js
@@ -2,6 +2,6 @@ import { Console } from '@woowacourse/mission-utils';
 
 export default class ViewOut {
    async showResult(result) {
-      await Console.print('결과: ' + result);
+      await Console.print('결과 : ' + result);
    }
 }
--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,13 +1,13 @@
 export default class Validator {
   static validateEmptyInput(text) {
     if (!text) {
-      throw new Error("[ERROR] 값이 입력되지 않았습니다. 다시 확인해주세요.");
+      throw new Error('[ERROR] 값이 입력되지 않았습니다. 다시 확인해주세요.');
     }
   }
 
   static validateNegative(numbers) {
     if (numbers.some(number => parseInt(number, 10) < 0)) {
-      throw new Error("[ERROR] 음수인 숫자는 허용되지 않습니다. 다시 확인해주세요.");
+      throw new Error('[ERROR] 음수인 숫자는 허용되지 않습니다. 다시 확인해주세요.');
     }
   }
 
@@ -20,7 +20,7 @@ export default class Validator {
   static validateIsNumber(numbers) {
     numbers.forEach((number) => {
       if (isNaN(Number(number))) {
-        throw new Error(`[ERROR] 입력된 값이 구분자로 올바르게 나눠지지 않았습니다. 다시 확인해주세요.`);
+        throw new Error('[ERROR] 입력된 값이 구분자로 올바르게 나눠지지 않았습니다. 다시 확인해주세요.');
       }
     });
   }
