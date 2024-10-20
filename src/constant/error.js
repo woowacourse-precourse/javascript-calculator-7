c201d12f4570ce703fdd6b1b5b557db303b549fc
ChanLee_KR <cl700120@gmail.com>
2024-10-20 13:24:55 +0900

    refactor: 커스텀 에러메시지 출력 함수 추가

--- src/constant/error.js
+++ src/constant/error.js
@@ -1,6 +1,14 @@
-export const ERROR_PREFIX = '[ERROR]';
+const ERROR_PREFIX = '[ERROR]';
+
+/**
+ * @param {string} message 
+ * @returns {string}
+ */
+function getErrorMessage(message) {
+  return [ERROR_PREFIX, message].join(' ');
+}
 
 export const ERROR_MESSAGE = Object.freeze({
-  FAILED_READ_LINE: `${ERROR_PREFIX} 문자를 읽어오는데 실패하였습니다.`,
-  INVALID_INPUT: `${ERROR_PREFIX} 유효하지 않은 입력입니다.`,
+  FAILED_READ_LINE: getErrorMessage('문자를 읽어오는데 실패하였습니다.'),
+  INVALID_INPUT: getErrorMessage('유효하지 않은 입력입니다.'),
 });
