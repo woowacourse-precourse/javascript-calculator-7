3eb071cbca0ed0a6d4942046e80081ad5371537a
jeonsm22 <jeonsm22@daum.net>
2024-10-20 19:18:52 +0900

    파일명과 import에서 파일 명 수정

--- src/InputFunctions.js
+++ src/InputFunctions.js
@@ -1,7 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 
-export async function getInput() {
-  return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+export function getInput() {
+  return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 }
 
 function isPositiveNumber(numberList) {
