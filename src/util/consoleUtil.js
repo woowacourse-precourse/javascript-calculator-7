8fc8027d4d5a000c2078636be2f53d37dbc132c1
Dabin Jeong <allempti@gmail.com>
2024-10-20 15:34:01 +0900

    feat: consoleUtil 정의

---
+++ src/util/consoleUtil.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export const readLineAsync = (query) => {
+  Console.readLineAsync(query);
+};
+
+export const print = (message) => {
+  Console.print(message);
+};
