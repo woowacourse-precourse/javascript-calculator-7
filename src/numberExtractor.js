8ecbd72f51e9ff951f52e9c05c063b0276b434e4
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-20 19:50:06 +0900

    refactor:` extractNumbers` 함수에서 구분자 처리 기능 분리 구현
    - 기본 구분자 처리와 커스텀 구분자 처리를 분리하여 책임을 분리함

--- src/numberExtractor.js
+++ src/numberExtractor.js
@@ -1,20 +1,30 @@
 import { CUSTOM_DELIMITER, DEFAULT_DELIMITER } from './constants.js';
 
-// 숫자를 추출하는 함수
+// 커스텀 구분자를 사용하여 숫자를 추출하는 함수
+function extractWithCustomDelimiter(input, customDelimiterMatch) {
+  const customDelimiter = new RegExp(customDelimiterMatch[1], 'g');
+  return input
+    .split(customDelimiterMatch[0])[1]
+    .split(customDelimiter)
+    .map(Number);
+}
+
+// 기본 구분자를 사용하여 숫자를 추출하는 함수
+function extractWithDefaultDelimiter(input) {
+  return input.split(DEFAULT_DELIMITER).map(Number);
+}
+
+// 전체 숫자를 추출하는 함수
 function extractNumbers(input) {
   if (!input) return [0];
 
   const customDelimiterMatch = input.match(CUSTOM_DELIMITER);
-  let numbers;
 
   if (customDelimiterMatch) {
-    const customDelimiter = new RegExp(customDelimiterMatch[1], 'g');
-    numbers = input.split(customDelimiterMatch[0])[1].split(customDelimiter);
+    return extractWithCustomDelimiter(input, customDelimiterMatch);
   } else {
-    numbers = input.split(DEFAULT_DELIMITER);
+    return extractWithDefaultDelimiter(input);
   }
-
-  return numbers.map(Number);
 }
 
 export default extractNumbers;
