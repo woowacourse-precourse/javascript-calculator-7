eb0de274c91c655d785196c6986708af46a3bd08
wowalswjd <alswjd5491@naver.com>
2024-10-21 02:46:40 +0900

    feat(App): 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해주세요.\n"
+      );
+      const result = input; // 계산 메서드 호출
+      Console.print(`결과: ${result}`);
+    } catch (error) {
+      Console.print(`[ERROR] ${error}`);
+    }
+  }
 }
 
 export default App;
