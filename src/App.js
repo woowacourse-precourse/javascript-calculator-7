6c423aab35ae25d97335bc3bb82efa9e5703637f
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2024-10-21 05:09:22 +0900

    feat#3 : 커스텀 구분자 처리 기능 추가
    문자열 앞부분에 //와 n사이에 위치한 커스텀 구분자를 사용하여 숫자를 추출하는 기능을 구현
    구분자가 없는 경우 쉼표와 콜론을 기본 구분자로 처리.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
     /**
+     * 프로그램 실행의 시작점
      */
     async run() {
         const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
@@ -11,15 +12,25 @@ class App {
 
     /**
      * 입력받은 문자열을 파싱하고, 숫자들을 더한 값을 반환
+     * 커스텀 구분자가 있으면 이를 사용하여 파싱
      * @param {string} input - 사용자 입력 문자열
      * @returns {number} - 계산된 합
      */
     calculateSum(input) {
         if (input === '') return 0; // 빈 문자열일 경우 0 반환
 
-        // 쉼표(,)와 콜론(:)으로 구분하여 숫자 배열로 변환 후 합산
-        const tokens = input.split(/[,|:]/).map(Number);
-        return tokens.reduce((acc, curr) => acc + curr, 0); // 배열의 숫자 합산
+        let delimiter = /[,|:]/; // 기본 구분자
+        let numbersString = input;
+
+        // 커스텀 구분자가 있는 경우 처리
+        if (input.startsWith('//')) {
+            const delimiterEndIndex = input.indexOf('\n');
+            delimiter = new RegExp(input.substring(2, delimiterEndIndex)); // 커스텀 구분자 추출
+            numbersString = input.substring(delimiterEndIndex + 1); // 구분자 이후의 숫자 문자열 추출
+        }
+
+        const tokens = numbersString.split(delimiter).map(Number); // 숫자 변환
+        return tokens.reduce((acc, curr) => acc + curr, 0); // 숫자 합산
     }
 }
 
