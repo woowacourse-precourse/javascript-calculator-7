938b9c5532765ec49a8710616a4b196cf4412598
silver93hq <gkagjsrb@gmail.com>
2024-10-20 13:19:45 +0900

    fix: 커스텀 구분자 구하는 기능 수정

--- src/handleCustomDelimiter.js
+++ src/handleCustomDelimiter.js
@@ -5,7 +5,7 @@ export const seperateCustomDelimiter = (string) => {
   validator.validateString(string);
   const startIndex = string.lastIndexOf(CUSTOM_DELIMITER_INPUT.start);
   const endIndex = string.lastIndexOf(CUSTOM_DELIMITER_INPUT.end);
-  if (startIndex !== 0 && endIndex !== 3) return { customDelimiter: [], numbersString: string };
+  if (startIndex !== 0 && endIndex !== 3) return { customDelimiter: "", numbersString: string };
   const numbersString = string.slice(endIndex + CUSTOM_DELIMITER_INPUT.end.length);
   const customDelimiterInput = string.slice(
     startIndex,
