4ac238dcb41ffa37b75718dbc18e2644f093d0da
H0ngJu <hongju4385@gmail.com>
2024-10-20 13:17:16 +0900

    feat: 커스텀 구분자 정의

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+    let formattedInput = input.replace(/\\n/g, "\n");
+    let delimiter = /,|:/;
+    let nums = [];
 
-    if (!input) {
+    if (!formattedInput) {
       Console.print("결과 : 0");
       return;
+    }
+
+    if (formattedInput.startsWith("//")) {
+      const parts = formattedInput.split("\n");
+      Console.print(parts);
+
+      if (parts.length < 2) {
+        Console.print("[ERROR] 구분자 형식을 확인해주세요.");
+        return;
+      }
+
+      const customDelimiter = parts[0].slice(2);
+      delimiter = new RegExp(`[${customDelimiter},:]`);
+
+      formattedInput = parts[1];
     }
   }
 }
