5923fa593234a459b84046878d07f6c61d38f1c6
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-20 22:52:32 +0900

    refactor: 입력값 없을 경우 메시지 삭제

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -3,7 +3,6 @@ export const ERROR_MESSAGES = Object.freeze({
   NOT_A_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
   NOT_POSITIVE_NUMBER: "[ERROR] 양수만 입력 가능합니다.",
   INVALID_INPUT: "[ERROR] 유효하지 않은 입력입니다.",
-  EMTPY_INPUT: "[ERROR] 입력하지 않았습니다.",
   INVALID_SPLITTER: "[ERROR] 숫자는 구분자로 사용할 수 없습니다.",
   EMPTY_DELIMITER: "[ERROR] 구분자를 입력해주세요.",
 });
