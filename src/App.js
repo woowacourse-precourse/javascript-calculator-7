0ce79e91175ca6de19a9edc45d061177a6cd91be
mun-kyeong <horse6953@naver.com>
2024-10-20 15:50:23 +0900

    [FIX] (App.js) : 함수 실행 방법 변경 - (커스텀 구분자 test 통과)
    - Test 환경에서 돌아갈 수 있는 코드로 수정
    - async 중첩되어있던 코드 수정
    - while 반복문 삭제
    
    Test환경에서 코드가 작동되지 않던 원인을 발견하여 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -4,29 +4,16 @@ import renderError from "./views/ErrorView.js";
 
 class App {
   async run() {
-    async function getInputString() {
-      while (true) {
-        try {
-          const input = await Console.readLineAsync(
-            "덧셈할 문자열을 입력해 주세요.\n"
-          );
-
-          if (input.trim() === "") {
-            renderError("입력오류 : 빈 문자열입니다. 다시 입력해주세요\n\n ");
-            continue;
-          }
-          return input;
-        } catch (error) {
-          renderError(
-            "입력 오류 \n 유효하지 않은 입력입니다. 다시 시도해 주세요.\n"
-          );
-          continue;
-        }
-      }
-    }
-    getInputString().then((input) => {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
       handleCalculation(input);
-    });
+    } catch (error) {
+      renderError(
+        "입력 오류 \n 유효하지 않은 입력입니다. 다시 시도해 주세요.\n"
+      );
+    }
   }
 }
 
