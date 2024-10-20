de0e9048b05168f38149231b8192270bf3c2856c
이창준 <2018110021@dongguk.edu>
2024-10-20 17:17:06 +0900

    feat: 사용자 입출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      const userinput = await Console.readLineAsync(`덧셈할 문자열을 입력해 주세요.\n`);
+      Console.print(userinput);
+      const result = userinput.split(',').reduce((acc, cur) => acc + parseInt(cur), 0);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+
+  }
 }
 
 export default App;
