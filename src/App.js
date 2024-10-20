79990fef42fe0de60dea6ce436c309eaf47b2ad3
sanghyeonLee <hyeon012366@gmail.com>
2024-10-20 22:05:47 +0900

    refactor : toArray 메소드 인자 배열 -> 문자열로 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App { //하나의 메소드는 하나의 역할
     customSeparator = ""//인스턴스 변수는 최대 2개
     async run() { //풀이 결과 반환 담당, 메소드명만 봐도 무슨역할인지 알 수 있게
         const input = await this.input()
-        input.startsWith("//") ? this.isCustom(input) : this.calculator(this.toArray([...input]))
+        input.startsWith("//") ? this.isCustom(input) : this.calculator(this.toArray(input))
         this.resultOutput()
     }
 
@@ -17,7 +17,7 @@ class App { //하나의 메소드는 하나의 역할
         const lastIdx = value.indexOf("\\n") // "\n" 가 없는 경우
         if (lastIdx !== -1) {
             this.customSeparator = value.slice(2, lastIdx)
-            return this.calculator(this.toArray([...value.slice(lastIdx + 2)]))
+            return this.calculator(this.toArray(value.slice(lastIdx + 2)))
         }
         throw new Error("[Error]: 에러발생")
     }
@@ -36,7 +36,7 @@ class App { //하나의 메소드는 하나의 역할
     }
 
     toArray(param) { //10이상 수 삽입 : 추후 인덴트 줄이기
-        let arr = []
+        let arr = [...param]
         let tmpValue = "";
         param.forEach((element, idx) => { //구분자가 숫자 값이면? 문자 값이라 했으니 문자만?
                 if (!isNaN(element)) {
