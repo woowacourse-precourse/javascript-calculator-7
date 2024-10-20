b36562ad6e59098fd5259b745375b8a7cf82b0c9
Dabin Jeong <allempti@gmail.com>
2024-10-20 18:12:25 +0900

    feat: 사용자 입력 검증 및 숫자 추출

--- src/model/Calculator.js
+++ src/model/Calculator.js
@@ -3,7 +3,6 @@ import { ERROR_MESSAGES } from '../constant/message.js';
 // 계산기 데이터 및 비즈니스 로직 처리
 class Calculater {
   validateString(input) {
-    // 입력이 아예 없는 경우
     if (!input) {
       throw new Error(ERROR_MESSAGES.empty);
     }
@@ -11,23 +10,30 @@ class Calculater {
     // 커스텀 구분자를 사용하는 경우
     if (input.startsWith('//')) {
       const customDelimiter = this.extractCustomDelimiter(input);
-      this.validateCustomDelimiter(customDelimiter);
+
+      const stringPart = input.split('\\n')[1];
+      this.validateNumbers(stringPart, new RegExp(`[${customDelimiter}]`));
     }
 
     // 기본 구분자를 사용하는 경우
+    else {
+      if (isNaN(Number(input[0]))) {
+        throw new Error(ERROR_MESSAGES.defaultStart);
+      }
+
+      this.validateNumbers(input, /[,|:]/);
+    }
   }
 
-  extractCustomDelimiter(input) {
-    const inputMatch = input.match(/^\/\/(.+)\n/);
+  extractCustomDelimiter(data) {
+    const inputMatch = data.match(/^\/\/(.+)\\n/);
 
     if (!inputMatch) {
       throw new Error(ERROR_MESSAGES.customForamt);
     }
 
-    return inputMatch[1];
-  }
+    const customDelimiter = inputMatch[1];
 
-  validateCustomDelimiter(customDelimiter) {
     if (customDelimiter.length > 1) {
       throw new Error(ERROR_MESSAGES.length);
     }
@@ -35,6 +41,28 @@ class Calculater {
     if (customDelimiter === '.') {
       throw new Error(ERROR_MESSAGES.period);
     }
+
+    return customDelimiter;
+  }
+
+  validateNumbers(string, delimiter) {
+    const stringNumbers = string.split(delimiter);
+
+    stringNumbers.forEach((stringNumber) => {
+      if (!stringNumber) {
+        throw new Error(ERROR_MESSAGES.missingNumber);
+      }
+
+      const number = Number(stringNumber);
+
+      if (isNaN(number)) {
+        throw new Error(ERROR_MESSAGES.invalidDelimiter);
+      }
+
+      if (number <= 0) {
+        throw new Error(ERROR_MESSAGES.negativeNumber);
+      }
+    });
   }
 }
 
