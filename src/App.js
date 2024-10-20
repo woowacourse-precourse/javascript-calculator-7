59d57d09dbf7ce8aff832f658468597780565b17
solmee <kii5541@naver.com>
2024-10-21 05:15:57 +0900

    feat: 결과 출력 및 합산 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
+import Calculator from "./calculator.js";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
 
-    Console.print(`입력받은 문자열: ${input}`);
+      const calculator = new Calculator();
+      const result = calculator.run(input);
+
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
   }
 }
 
