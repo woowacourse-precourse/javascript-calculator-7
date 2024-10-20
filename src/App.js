c13f02dc2e434c78f4055e02a1d36a5f5b905a3e
Joy <jungheeyoo.joy@gmail.com>
2024-10-20 19:45:13 +0900

    쉼표(,) 또는 콜론(:)을 구분자로 문자열 분리 후 합산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.print('덧셈할 문자열을 입력해 주세요.');
+
+    const input = await MissionUtils.Console.readLineAsync();
+    const result = this.calculate(input);
+    MissionUtils.Console.print(`결과 : ${result}`);
+  }
+
+  calculate(input) {
+    if (input === '') return 0;
+
+    const numbers = input.split(/[,|:]/).map(Number);
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
 }
 
 export default App;
