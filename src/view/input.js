4052439db4179061465d0839a77ec129dc22e769
pjc0179 <pjc0179@gmail.com>
2024-10-20 19:58:13 +0900

    fix: "input 함수로 변경"

--- src/view/input.js
+++ src/view/input.js
@@ -1,7 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const INPUT = {
-  getUserInput: Console.readLineAsync("덧셈할 문자열을 입력해 주세요"),
+  getUserInput: () => Console.readLineAsync("덧셈할 문자열을 입력해 주세요"),
 };
 
 export default INPUT;
