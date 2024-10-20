1c873e8d4c10bb710fe1074911a2032c556f1eed
kim jihwan <kjhwan0802@naver.com>
2024-10-20 16:36:10 +0900

    test: add new test case

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -95,6 +95,21 @@ describe("문자열 계산기", () => {
     });
   });
 
+  test("커스텀 구분자 사용 공백", async () => {
+    const inputs = ["// \\n1 2 3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("커스텀 구분자 없는 예외", async () => {
     const inputs = ["//\\n1;2;3"];
     mockQuestions(inputs);
