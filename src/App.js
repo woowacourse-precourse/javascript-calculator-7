70bdd3817bd63995cb146fd0072062f2c6e7e1ca
immaemi <nam14785@soongsil.ac.kr>
2024-10-20 18:20:49 +0900

    feat: calculator with fixed delimiters , and :

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const delimiters = [',', ':'];
+
+    let str = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    let sum = 0;
+    let numberTmp = '';
+
+    for (let i = 0; i < str.length; i += 1) {
+      if (delimiters.includes(str[i])) {
+        sum += Number(numberTmp);
+        numberTmp = '';
+        continue;
+      }
+
+      numberTmp += str[i];
+    }
+
+    sum += Number(numberTmp);
+    Console.print(`결과 : ${sum}`);
+  }
 }
 
 export default App;
