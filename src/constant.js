e31ab9361d6849ee415876418411a8c6eb5f7e82
oko_jin <dswvgw6412@naver.com>
2024-10-20 20:22:34 +0900

    feat(errorMessages): 각 에러에 해당하는 에러 메시지 등록

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,10 @@
 export const errorMessages = {
   unexpectedError: '예상치 못한 에러가 발생했습니다.',
+  invalidDelimiter: '구분자 입력이 잘못되었습니다.',
+  invalidNumber: '유효하지 않은 숫자입니다.',
+  invalidCustomDelimiter: '유효한 커스텀 구분자가 아닙니다.("1-9", "//", ".", "\n", 공백)',
+  notInputCustomDelimiter: '커스텀 구분자가 입력되지 않았습니다.',
+  duplicatedDelimiter: '이미 등록된 구분자입니다.',
 };
 
 export const Regex = {
