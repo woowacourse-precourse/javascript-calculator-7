0e2f4f65a433f109ba18badfb101074ec2f5d96a
Jestin-FE <jestinfe@naver.com>
2024-10-21 01:18:50 +0900

    feat(App): 계산 기능 구현
    App.js 파일에 문자열을 입력받아 계산하여 결과를 반환하는 기능을 구현한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const givenNum = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+      let custom = '';
+      let isCustom = false;
+      if (givenNum[0] === '/' && givenNum[1] === '/' && givenNum[3] === '\\' && givenNum[4] === 'n') {
+        custom = givenNum[2];
+        isCustom = true;
+      }
+
+      const splitGivenNum = (arr) => {
+        let splitNum = [];
+        if (isCustom) {
+          splitNum = arr.slice(5).split(new RegExp(`[${custom},:]`));
+          return splitNum;
+        }
+        splitNum = arr.split(new RegExp(`[,:]`));
+        return splitNum;
+      }
+
+      const getSum = (arr) => {
+        return arr.reduce((a, c) => {
+          return a + Number(c);
+        }, 0);
+      }
+
+      const sum = getSum(splitGivenNum(givenNum));
+      MissionUtils.Console.print(`결과 : ${sum}`);
+    } catch {
+      MissionUtils.Console.print('[ERROR]');
+      throw new Error('[ERROR]');
+    }
+  }
 }
 
 export default App;
