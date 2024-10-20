7768f8a86975311817dcab13bebbdcde1f1d93cf
salmonco <xkqjsslsek80@naver.com>
2024-10-20 16:53:08 +0900

    feat(app): 커스텀 구분자를 갖는 문자열인지 여부 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+
+    const isCustomSeparator = (str) => {
+      return str.includes("//") && str.includes("\\n");
+    };
+
+    Console.print(isCustomSeparator(input));
+  }
 }
 
 export default App;
