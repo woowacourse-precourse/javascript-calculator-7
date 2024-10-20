77cb693357a33951ccdd7e255ba6d7f7f8105ad4
geunhwa37 <jacika37@naver.com>
2024-10-21 04:23:57 +0900

    Merge branch 'main' of https://github.com/geunhwa37/javascript-calculator-7

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App {
   async run() {
     const input = await this.getInput(); // 입력 메소드 사용
     try {
-      const numbers = this.extractNumbers(input); // 숫자 추출 메소드 사용
-      const result = this.calculateSum(numbers); // 합 계산 메소드 사용
+      const result = this.extractNumbers(input); // 숫자 추출 메소드 사용
       this.printResult(result); // 결과 출력 메소드 사용
     } catch (error) {
       this.handleError(error); // 에러 처리 메소드 사용
@@ -19,37 +18,27 @@ async getInput() {
   return await Console.readLineAsync('');
 }
 
-// 기본 구분자 기준 분리: 쉼표(,) 또는 콜론(:)을 구분자
-splitNumbers(numbers, delimiter) {
-  return numbers.split(new RegExp(`[${delimiter}]`));
-}
+// 숫자 추출: 구분자 분리 + 숫자 변환 메소드
+extractNumbers(input) {
+let custom = ',:'; 
+let sum = 0; 
 
-// 커스텀 구분자 기준 분리: "//"와 "\n" 사이에 위치하는 문자인 커스텀 구분자를 기준
-extractDelimiter(input) {
-  if (input.startsWith("//")) { 
-    const [delimiterLine, ...numberLines] = input.split('\n'); 
-    const delimiter = delimiterLine.substring(2); 
-    const numbers = numberLines.join('\n'); 
-    return [delimiter, numbers]; 
+if(input.slice(0,2) === "//"){
+  custom=input.slice(2,3);
+  input=input.slice(5);
+}
+const delimiter = new RegExp(`[${custom},:]+`);
+const result = input.split(delimiter);
+
+for (let i = 0; i < result.length; i++) {
+  if (result[i] === "" || isNaN(result[i])) {
+    sum += 0; 
+  } else {
+    sum += Number(result[i]);
   }
-  return [',:', input]; 
 }
 
-// 숫자 변환: 분리한 문자를 숫자로 변환하는 메소드
-convertToNumbers(numbers) {
-  return numbers.map(num => Number(num)); 
-}
-
-// 숫자 추출: 구분자 분리 + 숫자 변환
- extractNumbers(input) {
-  const [delimiter, numbers] = this.extractDelimiter(input); 
-  const numberArray = this.splitNumbers(numbers, delimiter); 
-  return this.convertToNumbers(numberArray);
-}
-
-// 합 계산: 분리한 각 숫자의 합을 반환하는 메소드
-calculateSum(numbers) {
-  return numbers.reduce((sum, num) => sum + num, 0); 
+return sum; 
 }
 
 // 출력: 덧셈 결과를 출력하는 메소드
