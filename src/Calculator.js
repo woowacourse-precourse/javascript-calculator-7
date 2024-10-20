3ab9cfe6dbb5ab191d690d03ba2c079a13b89477
Daeuning <happyjde3@gmail.com>
2024-10-21 05:01:06 +0900

    feat(Calculator): 커스텀 구분자를 가져오는 getCustomDelimiter 메소드 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,7 +3,14 @@ import { Console } from '@woowacourse/mission-utils';
 function getInput() {
   Console.readLine('덧셈할 문자열을 입력해 주세요.', (answer) => {
     console.log(answer);
+    const customDelimiter = getCustomDelimiter(answer);
+    console.log(customDelimiter);
   });
+}
+
+function getCustomDelimiter(input) {
+  const customDelimiterPattern = /^\/\/(.+)\\n/;
+  return input.match(customDelimiterPattern);
 }
 
 export function calculator() {
