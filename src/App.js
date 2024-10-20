58f105cd2429cd4a19d577a0f0fc4b913abc5380
boxi <boxiyoun@gmail.com>
2024-10-21 06:14:36 +0900

    숫자 입력 받는 함수 수정3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  #startCalculator() { // 숫자 입력받는 함수
-    Console.readLineAsync('덧셈할 문자열을 입력해 주세요.', (userInput) => {
-      console.log(`입력: ${userInput}`);
-    });
+  startCalculator() { // 숫자 입력받는 함수
+    const userInput = Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    return userInput;
   }
 
 
