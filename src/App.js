49c928be36ef737073b1fc5c558d499ec9004735
youdame <112458620+youdame@users.noreply.github.com>
2024-10-20 19:59:41 +0900

    feat(계산기): 커스텀 구분자를 처리하고 구분자 정의 부분 제거 후 문자열을 구분하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+    // 커스텀 문자열을 찾는 정규표현식
+    const regex = /^\/\/(.+)\\n/;
 
-    const StringNumbers = input.split(/[,|:]/);
+    // 커스텀 문자열과 match한 결과를 배열로
+    const match = input.match(regex);
 
-    const sum = StringNumbers.reduce((acc, number) => {
+    let stringNumbers;
+    if (match) {
+      console.log('match', match, match[1]);
+      // 커스텀 문자열 찾기
+      const customDelimiter = match[1];
+
+      // 정규표현식으로 커스텀 문자열을 정의한 부분을 제거
+      const newInput = input.replace(regex, '');
+      // 커스텀 문자열로 split
+      stringNumbers = newInput.split(customDelimiter);
+    } else {
+      stringNumbers = input.split(/[,|:]/);
+    }
+    const sum = stringNumbers.reduce((acc, number) => {
       // 이 에러 처리 시점을 어디로 해야할까?
       if (number.includes('-')) {
         throw new Error('[ERROR] 음수는 입력할 수 없습니다');
       }
+
       return acc + Number(number);
     }, 0);
-
-    Console.print(sum);
+    Console.print('결과 : ' + sum);
   }
 }
 
