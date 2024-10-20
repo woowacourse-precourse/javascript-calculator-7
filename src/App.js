f11e32068c134dd7d4c3527dde52ba8e427bfb3b
gumbi <gusn9719@gmail.com>
2024-10-20 15:22:39 +0900

    refactor: App.js 파일 내 불필요한 주석 제거
    - 코드 작성 중 참고용으로 사용되었던 불필요한 주석들을 제거하여 코드의 가독성을 높였습니다.
      - 함수명과 변수명이 기능을 명확히 설명하므로, 주석 없이도 코드가 이해될 수 있습니다.

--- src/App.js
+++ src/App.js
@@ -16,28 +16,19 @@ class App {
             }
 
             try {
-                // answer에 undefined가 들어올 경우, toUpperCase() 호출 시 오류가 발생할 수 있으므로 기본값으로 빈 문자열을 할당합니다.
                 let answer = (await MissionUtils.Console.readLineAsync('다시 한번 계산하시겠습니까? (Y/N)\n')) || '';
-
-                // 다시 계산을 묻고 y를 입력시 appContinue에 true를 할당해서 계산을 다시 실행
-                // 사용자 편의성을 위해 소문자 'y'나 'n'을 입력해도 올바르게 처리될 수 있도록 toUpperCase()를 적용합니다.
 
                 if (answer.toUpperCase() === 'Y') {
                     appContinue = true;
                 } else if (answer.toUpperCase() === 'N') {
-                    // n을 입력시 appContinue에 false를 할당해서 while문을 종료, 프로그램 또한 종료
                     appContinue = false;
                     MissionUtils.Console.print('계산을 종료합니다. 😊');
                 } else {
-                    // y, n, Y, N 이외의 값이 들어 왔을 때 y 또는 n만 입력 가능하다 알려주고 프로그램을 종료
-                    // 이 부분에서 프로그램을 다시 실행을 할까 고민을 했지만 [ERROR]로 시작하는 예외 처리이기에 프로그램 종료
                     appContinue = false;
                     throw new Error('[ERROR] Y또는 N만 입력할 수 있습니다.');
                 }
             } catch (e) {
                 MissionUtils.Console.print(e.message);
-                // throw e를 사용하면 테스트 프로그램에서 예외가 다시 던져져 프로그램이 비정상적으로 종료됩니다.
-                // 따라서 정상적으로 종료되도록 이 부분에서 예외를 다시 던지지 않습니다.
             }
         } while (appContinue);
     }
