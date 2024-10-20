b11c039ff8feda2acc21578bf48300dbf09c01ff
aydenote <dslgpgh@gmail.com>
2024-10-20 21:56:20 +0900

    refactor(Calculator): 클래스 내부 함수 캡슐화

--- src/Calculator.js
+++ src/Calculator.js
@@ -11,29 +11,29 @@ import { getSum } from "./utils/calculate.js";
 class Calculator {
 	async runCalculator() {
 		try {
-			const userOriginalInputValue = await this.getUserInput();
+			const userOriginalInputValue = await this.#getUserInput();
 			await validateSeparatorFormat(userOriginalInputValue);
 			await validatePositiveNumber(userOriginalInputValue);
-			const numbers = await this.getNumbers(userOriginalInputValue);
+			const numbers = await this.#getNumbers(userOriginalInputValue);
 			const sumNumber = await getSum(numbers);
-			await this.printOutputNumber(sumNumber);
+			await this.#printOutputNumber(sumNumber);
 		} catch (error) {
 			console.error(error.message);
 			throw new Error(`${error.message}`);
 		}
 	}
 
-	async getUserInput() {
+	async #getUserInput() {
 		const USER_INPUT = await userInput("덧셈할 문자열을 입력해 주세요.\n");
 		return USER_INPUT;
 	}
 
-	async getNumbers(userInputValue) {
+	async #getNumbers(userInputValue) {
 		const { separators, inputWithoutSeparator } = await extractCustomSeparator(userInputValue);
 		return splitNumbers(separators, inputWithoutSeparator);
 	}
 
-	async printOutputNumber(outputValue) {
+	async #printOutputNumber(outputValue) {
 		printOutput(`결과 : ${outputValue}`);
 	}
 }
