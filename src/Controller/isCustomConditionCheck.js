9b49d7cb4ff40d5c7c8150dff0a2e4be3d948758
Jinny <ggab.challenge@gmail.com>
2024-10-20 19:28:28 +0900

    refactor(function): 구분자 사용 입력을 체크하는 함수를 기능별로 3개로 분리

--- src/Controller/isCustomConditionCheck.js
+++ src/Controller/isCustomConditionCheck.js
@@ -6,8 +6,8 @@ const isConditionTextInputCheck = userInput => {
   return false;
 };
 
-// 2. 구분자 사용 입력 체크
-const isSeparatorTextInputCheck = userInput => {
+// 2. 구분자 사용 입력 횟수, 반복문 횟수
+const separatorTextInputAndLoofCount = userInput => {
   let allSpeparatorTrueCheck = 0;
   let loofCountForallSpeparatorTrueCheck = 0;
   for (let i = 6; i < userInput.length; i += 1) {
@@ -18,9 +18,22 @@ const isSeparatorTextInputCheck = userInput => {
       allSpeparatorTrueCheck += 1;
     }
   }
-  if (allSpeparatorTrueCheck === loofCountForallSpeparatorTrueCheck) {
+  return [allSpeparatorTrueCheck, loofCountForallSpeparatorTrueCheck];
+};
+
+// 2. 구분자 사용 입력횟수, 반복문 횟수 같은지 확인
+const isSeparatorTextInputAndLoofCountEqualCheck = countForSeparatorTextAndLoof => {
+  if (countForSeparatorTextAndLoof[0] === countForSeparatorTextAndLoof[1]) {
     return true;
   }
+  return false;
+};
+
+const isSeparatorTextInputCountCheck = userInput => {
+  if (isSeparatorTextInputAndLoofCountEqualCheck(separatorTextInputAndLoofCount(userInput)) === true) {
+    return true;
+  }
+  return false;
 };
 
 // 3. 숫자가 양수로 이루어진지 체크하기
@@ -33,7 +46,6 @@ const isPositiveNumberCheck = userInput => {
       positiveNumberCheckCount += 1;
     }
   }
-
   if (positiveNumberCheckCount === NUMBER_COUNT_FOR_INT) {
     return true;
   }
@@ -41,7 +53,7 @@ const isPositiveNumberCheck = userInput => {
 };
 
 const isCustomConditionCheck = userInput => {
-  if (isConditionTextInputCheck(userInput) && isSeparatorTextInputCheck(userInput) && isPositiveNumberCheck(userInput)) {
+  if (isConditionTextInputCheck(userInput) && isSeparatorTextInputCountCheck(userInput) && isPositiveNumberCheck(userInput)) {
     return true;
   }
   return false;
