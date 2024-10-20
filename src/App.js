8c6f72550df89244c56962b393601af53a4564d0
kr-younghoon <sullanta0802@gmail.com>
2024-10-21 00:32:24 +0900

    feat(calculator): 커스텀 구분자 지원 기능 추가
    문자열의 앞 부분에서 `//`와 `\n` 사이에 커스텀 구분자를 지정할 수 있는 기능을 추가했어요.

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
     async run() {
         /* 
-            2. 빈 문자열 처리
-            - 빈 문자열이 입력된 경우 `0`을 반환합니다.
-            - 예 : "" -> 0
+            3. 커스텀 구분자를 지원하는 기능
+            - 문자열의 앞 부분에서 `//` 와 `\n` 사이에 커스텀 구분자를 지정할 수 있습니다.
+            - 예 : "//;\n1;2;3" -> 6
          */
 
         // 사용자로부터 입력(문자열)을 받음
-        const INPUT = await Console.readLineAsync(
+        let input = await Console.readLineAsync(
             '덧셈할 문자열을 입력해주세요.\n'
         );
+        let delimiter = /[,|:]/;
 
         // 압력이 빈 문자열인 경우, 0을 반환
-        if (INPUT.trim() === '') {
+        if (input.trim() === '') {
             return Console.print('0');
         }
 
-        // 문자열을 쉼표와 콜론으로 분리하여 숫자 배열로 변환
-        const NUMBERS = INPUT.split(/[,|:]/).map(Number);
+        // 커스텀 구분자가 있는지 확인
+        if (input.startsWith('//')) {
+            const end = input.indexOf('\\n');
+            delimiter = input.substring(2, end);
+            input = input.substring(end + 2);
+        }
 
-        // 숫자를 합산
-        const RESULT = NUMBERS.reduce(
-            (acc, curr) => acc + curr,
-            0
-        );
+        // 문자열을 쉼표,콜론 혹은 커스텀 구분자로 분리하여 숫자 배열로 변환 숫자를 합산
+        const result = input
+            .split(delimiter)
+            .map(Number)
+            .reduce((acc, curr) => acc + curr, 0);
 
         // 결과 출력
-        Console.print(`${RESULT}`);
+        Console.print(`${result}`);
     }
 }
 
