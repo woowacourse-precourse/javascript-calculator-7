bf04f845091abea2fa03f13142693143d7edd842
youdame <112458620+youdame@users.noreply.github.com>
2024-10-20 16:48:07 +0900

    feat(에러 처리): 음수 값 입력 시 '[ERROR] 음수는 입력할 수 없습니다.' 에러 발생

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    try {
-      const input = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
 
-      const StringNumbers = input.split(/[,|:]/);
+    const StringNumbers = input.split(/[,|:]/);
 
-      const sum = StringNumbers.reduce((acc, number) => {
-        return acc + Number(number);
-      }, 0);
+    const sum = StringNumbers.reduce((acc, number) => {
+      // 이 에러 처리 시점을 어디로 해야할까?
+      if (number.includes('-')) {
+        throw new Error('[ERROR] 음수는 입력할 수 없습니다');
+      }
+      return acc + Number(number);
+    }, 0);
 
-      Console.print(sum);
-    } catch {}
+    Console.print(sum);
   }
 }
 
