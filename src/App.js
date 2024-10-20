0081875eebe2c1cd78ae3b0d3d9484976014bd15
kim-museong <antjd0419@gmail.com>
2024-10-21 03:17:08 +0900

    feat(input): 입력받은 문자열에서 숫자를 추출하여 반환하는 기능 구현
    - 계산 함수를 만들어서 유저가 입력한 값에 숫자를 추출하여 반환하는 기능을 구현했습니다.
    - 정규식을 사용하여 숫자를 추출하였습니다.
    - 정규식을 사용한 이유는 정규식을 사용한 경험이 없어서 이번 과제에서 공부하면서 사용해보았습니다.
    - 추출한 값을 숫자로 변경했을 때 숫자가 아닌 값이 있으면 오류를 반환하는 추가기능을 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,28 @@ class App {
     async readInput() {
         try {
             const VALUE = await Console.readLineAsync('');
-            Console.print(`결과 : ${VALUE}`);
+            const RESULT = this.calculate(VALUE);
+
+            Console.print(`결과 : ${RESULT}`);
         } catch (error) {
             Console.print(`[ERROR] : ${error.message}`);
         }
+    }
+
+    calculate(input) {
+        if (input.trim() === '') throw new Error('입력된 값이 없습니다.');
+
+        const REGEXP = /[,:]/;
+
+        const numbers = input.split(REGEXP).map(Number);
+
+        if (numbers.some(isNaN)) {
+            throw new Error('숫자가 아닌 값이 포함되었습니다.');
+        }
+
+        Console.print(numbers);
+
+        return numbers;
     }
 }
 
