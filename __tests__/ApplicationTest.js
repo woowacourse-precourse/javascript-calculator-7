e7680aec0efe9ceb77d9f6d8aa805a584928f939
Rigu1 <dlsrbtla@gmail.com>
2024-10-20 20:25:39 +0900

    test: add test cases for custom delimiter and validation

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -18,11 +18,11 @@ const getLogSpy = () => {
 
 describe("문자열 계산기", () => {
   test("커스텀 구분자 사용", async () => {
-    const inputs = ["//;\\n1"];
+    const inputs = ["//;\\n1;2,3"];
     mockQuestions(inputs);
 
     const logSpy = getLogSpy();
-    const outputs = ["결과 : 1"];
+    const outputs = ["결과 : 6"];
 
     const app = new App();
     await app.run();
@@ -31,6 +31,22 @@ describe("문자열 계산기", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  test("기본 구분자 사용", async () => {
+    const inputs = ["1:2,3:4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
 
   test("예외 테스트 음수", async () => {
     const inputs = ["-1,2,3"];
@@ -52,6 +68,24 @@ describe("문자열 계산기", () => {
 
   test("예외 테스트 \\n의 반복", async () => {
     const inputs = ["//;\\n1\\n1\\n1\\n1"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  
+  test("예외 테스트 공백", async () => {
+    const inputs = ["//;\\n1    2"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트 이중 커스텀 문자", async () => {
+    const inputs = ["//;\\n1;2//^\\n1^2"];
     mockQuestions(inputs);
 
     const app = new App();
