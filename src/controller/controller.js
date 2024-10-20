08400de75812ba2c4a1ecc1b11817e6a9dd34817
pjc0179 <pjc0179@gmail.com>
2024-10-20 20:47:10 +0900

    feat:"컨트롤러 구현"

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,23 +1,35 @@
 import INPUT from "../view/input.js";
 import { splitReg } from "../constants/reg.js";
+import Model from "../model/model.js";
 
 class Controller {
+  model = new Model();
+
   async start() {
     const userInput = await INPUT.getUserInput();
 
     const [custom, other] = this.findCustomDelimiter(userInput);
-    console.log(this.checkCustomDelimiter(custom.split("")));
+
+    this.checkCustomDelimiter(custom);
+
+    this.model.addDelimiter(custom);
+
+    console.log(this.model.calculate(other));
   }
 
   findCustomDelimiter(string) {
     const str = string.match(splitReg);
-    if (str === null) return ["", str];
 
-    return [str[1], str[2]];
+    if (str === null) return [[""], string];
+
+    return [str[1].split(""), str[2]];
   }
 
   checkCustomDelimiter(stringList) {
-    return stringList.filter((string) => !isNaN(+string)).length === 0;
+    if (stringList[0] === "") return;
+    if (stringList.filter((string) => !isNaN(+string)).length > 0) {
+      throwError("숫자는 커스텀문자에 들어갈 수 없습니다.");
+    }
   }
 }
 
