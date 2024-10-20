c25496029923f8cca7c454ed0913a0ad398b19b7
“HYUNJIN105” <hyunjin.011005@gmail.com>
2024-10-20 20:58:55 +0900

    feat: 사용자 입력값 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n',);
+    Console.print(input);
   }
 }
 
