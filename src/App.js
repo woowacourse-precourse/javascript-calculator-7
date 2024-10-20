fa2afed831dff378bc69a6e52730c291cf689d2f
leedohyung28 <dhleee0123@gmail.com>
2024-10-20 19:10:43 +0900

    ⚡입력 부분 구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    Console.print("덧셈할 문자열을 입력해 주세요.");
+    Console.readLineAsync("덧셈할 문자열을 입력해 주세요.").then(function (
+      result
+    ) {
+      Console.print(result);
+    });
   }
 }
 
