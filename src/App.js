7bb41a4a6d41ef44495b718995146be4606656ad
immaemi <nam14785@soongsil.ac.kr>
2024-10-20 18:58:11 +0900

    feat: add readCustomDelimiters member function

--- src/App.js
+++ src/App.js
@@ -5,8 +5,29 @@ class App {
     this.delimiters = [',', ':'];
   }
 
+  async readCustomDelimiters(str) {
+    if (str[0] === '/' && str[1] === '/') {
+      let endOfCustomDelimeters = 0;
+
+      for (let i = 2; i < str.length; i += 1) {
+        if (str[i] === '\\' && str[i + 1] === 'n') {
+          endOfCustomDelimeters = i + 2;
+          break;
+        }
+
+        this.delimiters.push(str[i]);
+      }
+
+      return str.slice(endOfCustomDelimeters, str.length);
+    }
+
+    return str;
+  }
+
   async run() {
     let str = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+    str = await this.readCustomDelimiters(str);
 
     let sum = 0;
     let numberTmp = '';
