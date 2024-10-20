117a8f32313c38bf2d9f73f2c191d986628d5092
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-10-20 18:47:12 +0900

    feat: Handling Different Types between default and custom Delimeter

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App {
       }
       return num;
     });
+
     var num = parsingNumbers.reduce((a, b) => a + b);
     return num;
   }
@@ -20,6 +21,25 @@ class App {
       "덧셈할 문자열을 입력해 주세요.\n "
     );
 
+    if (input.startsWith("//")) {
+      //커스텀 구분자일 가능성이 있는 경우
+      const endOfDelimeter = input.indexOf("\\n");
+
+      const customDelimeter = input.slice(2, endOfDelimeter);
+
+      const inputSlice = input.slice(endOfDelimeter + 2); //숫자 부분 추출
+
+      result = this.parsingInput(
+        inputSlice,
+        new RegExp(`[${customDelimeter},:]`)
+      );
+    } else if (!isNaN(Number(input[0]))) {
+      //만약에 Number(!)하면 isNaN나옴
+      result = this.parsingInput(input, /[,:]/);
+    
+    } 
+
+    MissionUtils.Console.print(`"결과 : ${result}"`);
 
   }
 }
