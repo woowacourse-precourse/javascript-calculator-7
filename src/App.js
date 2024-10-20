dd8e2a589df6ea4d895de14730a02bf86e55ebe3
luii0415 <becky45210193@gmail.com>
2024-10-21 02:51:17 +0900

    feat : create and change
    create 목록
    - 오류 및 예외  처리
    1. 문자열 내 숫자가 없고, 빈 문자열이 아닌 경우.
    2. 빈 문자열인 경우
    
    change 목록
    1. 코드 내 주석 수정

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ class App {
     //기능 1. 입력 처리
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const addNumber = await Console.readLineAsync("");
-
-    // 기능 2. 구분자 없이 숫자만 입력된 경우
     const onlyNum = /^\d+$/.test(addNumber);
+    if (addNumber == "") {
+      //기능 5. "" 등의 빈 문자열 입력시
+      Console.print(`결과 : ${result}`);
+      return;
+    }
     if (onlyNum) {
+      // 기능 2. 구분자 없이 숫자만 입력된 경우
       result = addNumber;
     } else if (addNumber.match(basic)) {
       // 기능 3. 기본 구분자로 구분된 숫자 계산
@@ -22,6 +26,12 @@ class App {
         .substring(5)
         .split(addNumber[2])
         .map((num) => Number(num));
+    } else if (!/\d/.test(str) && str !== "") {
+      // 기능 5. 문자열 내 숫자가 없고, 빈 문자열이 아닌 경우
+      throw new Error("[ERROR]");
+    } else {
+      // 기능 6. 그 외 모든 오류 사항 (제시된 기능에서 벗어나는 경우)
+      throw new Error("[ERROR]");
     }
 
     if (list.length > 0) {
@@ -29,7 +39,7 @@ class App {
         result += list[i];
       }
     }
-    //기능 2 계산된 문자열 출력
+    // 문자열 출력
     Console.print(`결과 : ${result}`);
   }
 }
