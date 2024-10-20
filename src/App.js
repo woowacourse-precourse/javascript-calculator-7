46c783a6ef1ecd6150c69edbb4c01e1d565bbc44
geunhwa37 <jacika37@naver.com>
2024-10-20 23:04:02 +0900

    feat: run 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 class App {
-  async run() {}
+  async run() {
+    const input = await this.getInput(); // 입력 메소드 사용
+    try {
+      const numbers = this.extractNumbers(input); // 숫자 추출 메소드 사용
+      const result = this.calculateSum(numbers); // 합 계산 메소드 사용
+      this.printResult(result); // 결과 출력 메소드 사용
+    } catch (error) {
+      this.handleError(error); // 에러 처리 메소드 사용
+    }
+  }
 }
 
 // 입력: 사용자에게 덧셈할 문자열을 입력받는 메소드
@@ -43,7 +52,7 @@ printResult(result) {
   console.log(`결과 : ${result}`); 
 }
 
-// 에러 처리: 사용자가 잘못된 값을 입력할 경우, "[ERROR]"로 시작하는 메시지와 함께 Error를 발생시키는 메서드
+// 에러 처리: 사용자가 잘못된 값을 입력할 경우, "[ERROR]"로 시작하는 메시지와 함께 Error를 발생시키는 메소드
 handleError(error) {
   console.log(`[ERROR] ${error.message}`); // 오류 메시지 출력
   process.exit(1); // 애플리케이션 종료
