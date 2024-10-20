f81224e8dbb1587a3846e3aa207516006af6b068
YongHyunKing <yhkim8246@gmail.com>
2024-10-21 04:35:01 +0900

    feat(Calculator): 커스텀 구분자 입력부가 "//"로 시작하는지 확인

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,18 @@
 class Calculator {
   constructor(input) {
     this.input = input;
+    this.customDelimiterSection = null;
+    this.numberSection = null;
   }
 
   checkInputValidity() {
     const splitInput = this.input.split("\\n");
     if (splitInput.length > 2) return [false, "[ERROR] 입력 문자열에 \'\\n\'이 2개 이상입니다."];
+
+    this.customDelimiterSection = splitInput[0];
+    this.numberSection = splitInput[1];
+
+    if (!this.customDelimiterSection.startsWith("//")) return [false, "[ERROR] 커스텀 구분자 입력부는 문자열 앞에 위치하여 \"//\"로 시작하고 \"\\n\"으로 끝나야 합니다."];
 
     return [true, "유효한 문자열"];
   }
