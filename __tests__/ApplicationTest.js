4cf9ec70f126023e1845920d842ecc24d9bb423b
kim jihwan <kjhwan0802@naver.com>
2024-10-20 16:15:01 +0900

    test: add new test cases

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -17,6 +17,54 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
+  test("음수 예외", async () => {
+    const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("문자 예외", async () => {
+    const inputs = ["-1,test2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("기본 구분자 사용", async () => {
+    const inputs = ["1,2:3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("빈 문자열", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("커스텀 구분자 사용", async () => {
     const inputs = ["//;\\n1"];
     mockQuestions(inputs);
@@ -32,8 +80,23 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
+  test("커스텀 구분자 사용 여러 문자", async () => {
+    const inputs = ["//;\\n1;2;3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자 없는 예외", async () => {
+    const inputs = ["//\\n1;2;3"];
     mockQuestions(inputs);
 
     const app = new App();
