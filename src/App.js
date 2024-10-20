faac36b988430fa83c6a681b9bfe7b40573da1ab
hyunow <112617546+hyunow@users.noreply.github.com>
2024-10-21 02:42:04 +0900

    feat: 문자열 덧셈 계산기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    const numbers = input.split(/[,:]/);
+    let answer = 0;
+    for (let n of numbers) {
+      answer += Number(n);
+    }
+    if (typeof answer !== NaN) {
+      Console.print("[ERROR]");
+      throw new Error();
+    } else {
+      Console.print(`결과 : ${answer}`);
+    }
+  }
 }
 
 export default App;
