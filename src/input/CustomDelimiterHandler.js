87efbf21ecc120efd6e559065e2ddbff54a01c93
Junseo Kim <semiwest2@gmail.com>
2024-10-20 19:36:33 +0900

    feat: 커스텀 구분자와 정리된 문자열을 반환하는 함수 추가 (handleCustomDelimiter)

--- src/input/CustomDelimiterHandler.js
+++ src/input/CustomDelimiterHandler.js
@@ -1,5 +1,19 @@
 import { CUSTOM_DELIMITER_REGEX } from '../constant/constant.js';
 
+//커스텀 구분자를 쓰는지 확인하는 함수
+export default function handleCustomDelimiter(input) {
+  if (!hasCustomDelimiter(input)) {
+    return input;
+  }
+  const customDelimiter = getCustomDelimiter(input);
+  const cleanString = cleanStringAfterDelimiter(input);
+
+  return {
+    customDelimiter,
+    cleanString,
+  }
+}
+
 //커스텀 구분자를 사용하는 문자열인지 확인
 function hasCustomDelimiter(input) {
   return CUSTOM_DELIMITER_REGEX.test(input);
