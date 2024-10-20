0e07d15b28fc992ecda7858b5e6d10354af838b2
Lee <dltmdgns0508@gmail.com>
2024-10-20 18:08:55 +0900

    style(App): 입력 메세지 변경
    App.js 수정
    - 문자열 입력 받을시 메세지 변경

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import numberCalculator from './NumberCalculator.js';
 class App {
   async run() {
     const input =
-      await Console.readLineAsync('계산할 문자열을 입력해 주세요\n');
+      await Console.readLineAsync('덧셈할 문자열을 입력해 주세요\n');
     if (!input) {
       throw new Error('[ERROR] 문자열이 입력되지 않았습니다.');
     }
