5fafb6a77c1e296095bf3b54547be3cbf92354b5
yena <rladpsk1117@gmail.com>
2024-10-21 06:50:22 +0900

    feat: 커스텀 구분자 예외처리
    입력은 구분자로 시작하거나 끝날 수 없음
    구분자 연속 사용 불가
    커스텀 구분자 지정 문자열과 구분자를 포함한 양의 정수만 입력 가능

--- src/App.js
+++ src/App.js
@@ -43,8 +43,28 @@ class App {
 
       // 커스텀 구분자
       if (input.startsWith('//') && input.substring(3, 5) == '\\n') {
+        const inputCustomNumbers = input.substring(5);
         const delimiter = input.charAt(2);
-        const result = input.substring(5).split(delimiter);
+
+        // 예외처리
+        if (
+          inputCustomNumbers.startsWith(delimiter) |
+          inputCustomNumbers.endsWith(delimiter)
+        ) {
+          throw new Error('구분자로 시작하거나 끝날 수 없습니다.');
+        }
+
+        const delimiterReg = new RegExp(`([${delimiter}]){2,}`);
+        if (delimiterReg.test(inputCustomNumbers)) {
+          throw new Error('구분자는 2번 이상 사용할 수 없습니다.');
+        }
+        const inputReg = new RegExp(`^[1-9]\\d*([${delimiter}][1-9]\\d*)*$`);
+        if (!inputReg.test(inputCustomNumbers)) {
+          throw new Error('입력값이 유효하지 않습니다.');
+        }
+
+        // 합 구하기
+        const result = inputCustomNumbers.split(delimiter);
         let hap = 0;
         result.forEach((item) => {
           hap += parseInt(item);
