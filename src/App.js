41fea2b517003584303cc4861c7435a4d99245cf
jiyun1006 <jiyun4251@gmail.com>
2024-10-20 13:04:48 +0900

    🐛 fix : 커스텀 구분자 메서드 수정.
    커스텀 구분자 메서드(isCustom)에서 모든 문자열에 커스텀 구분자를 가지도록 걸러내고 있어서
    커스텀 구분자의 형식을 갖춘 문자열만 검증하도록 변경.

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ async function getInputStr() {
   try {
     const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const separator = [":", ","];
-    if (isCustom(inputStr)) {
-      const tmpSep = inputStr.slice(2, 3);
-      console.log(tmpSep);
-    } else {
-    }
+    // 커스텀 구분자를 가지는지 확인.
+    isCustom(inputStr)
+    const tmpSep = inputStr.slice(2, 3);
+    console.log(tmpSep);
+    
     return inputStr;
   } catch (error) {
+     return error;
     // reject 되는 경우
   }
 }
@@ -25,8 +26,13 @@ async function getInputStr() {
 // 입력문에서의 에러 상황 체크
 
 const isCustom = (inputStr) => {
-  const regExp = /(\/\/.{1}\\n).{0,}/;
-  return regExp.test(inputStr);
+  const regExp = /(\/\/.{1}\\n){1}.{0,}/; 
+  const regExp2 = /(\/\/.{1}\\n).{0,}/;
+  if (regExp.test(inputStr)) { // 커스텀 구분자의 형식을 가진 문자열 이라면,
+    if (!regExp2.test(inputStr)) {
+      throw new Error("[ERROR] 잘못된 입력 형식 입니다.(구분자 길이 오류 or 잘못된 커스텀 구분자 형식)");
+    }
+  }
 }
 
 
