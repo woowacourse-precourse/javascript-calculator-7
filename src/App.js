474fbbdfc7a98287a5e30fc240805c487b6a6ea7
yiju <yiju1755@naver.com>
2024-10-21 00:31:59 +0900

    feat: 정규식 이스케이프 처리를 위한 함수 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ const NUMBER_CHECK_REGEX = /^[0-9]+$/;
 class CalculatorApp {
   #delimiters = [",", ":"];  // 기본 구분자
   #totalSum = 0;
+
+  // 정규식 이스케이프 처리
+  #escapeDelimiterRegExp() {
+    return this.#delimiters
+      .map((delimiter) => delimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))
+      .join("|");
+  }
   async run() {}
 }
 
