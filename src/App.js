599239ffb245b07fc499f234433cf73101deb34d
jiyeeeah <sije3757@snu.ac.kr>
2024-10-20 19:47:19 +0900

    [feat] 구분자 예외 처리
    - 구분자가 숫자인 경우, 구분자의 역할을 할 수 없을 것이라고 판단하여
      이 경우 예외가 발생하도록 함

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import InterfaceUtil from "./InterfaceUtil.js";
 import CalculateUtil from "./CalculateUtil.js";
-import { isInputValid } from "./validation.js";
+import { isExpressionValid, isSeparatorValid } from "./validation.js";
 import MESSAGES from "./messages.js";
 
 class App {
@@ -8,7 +8,10 @@ class App {
     const input = await InterfaceUtil.inputString();
 
     const { separator, expression } = CalculateUtil.parseInput(input);
-    if (!isInputValid(separator, expression)) throw new Error(MESSAGES.ERROR);
+    for (const sep of separator) {
+      if (!isSeparatorValid(sep)) throw new Error(MESSAGES.ERROR);
+    }
+    if (!isExpressionValid(separator, expression)) throw new Error(MESSAGES.ERROR);
 
     const sum = CalculateUtil.sumExpression(separator, expression);
 
--- src/validation.js
+++ src/validation.js
@@ -1,4 +1,9 @@
-export function isInputValid(separator, expression) {
+export function isSeparatorValid(separator) {
+  if (!isNaN(separator)) return false;
+  return true;
+}
+
+export function isExpressionValid(separator, expression) {
   const expressionArr = expression.split("").map((val) => val.trim());
   for (const num of expressionArr) {
     if (!separator.includes(num) && isNaN(num)) return false;
