69be7ad2f42fda5517731914277074f283323b4e
daun-up <awhekdns@kookmin.ac.kr>
2024-10-20 19:04:00 +0900

    refactor : SRP 기준으로 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
@@ -14,10 +13,20 @@ class App {
   }
 
   calculate(input) {
-    if (input === '') {
+    if (this.isEmptyInput(input)) {
       return 0;
     }
 
+    const { delimiters, updatedInput } = this.delimiterHandler(input); // 변경된 input을 반영
+    const numbers = this.splitInput(updatedInput, delimiters);
+    return this.sumNumbers(numbers);
+  }
+
+  isEmptyInput(input) {
+    return input === '';
+  }
+
+  delimiterHandler(input) {
     const delimiter = [',', ':'];
 
     if (input.startsWith('//')) {
@@ -31,11 +40,17 @@ class App {
       }
     }
 
-    const pattern = new RegExp(`[${delimiter.join('')}]`);
-    const numbers = input.split(pattern);
+    return { delimiters: delimiter, updatedInput: input }; // delimiter와 수정된 input을 반환
+  }
 
+  splitInput(input, delimiters) {
+    const pattern = new RegExp(`[${delimiters.join('')}]`); // 구분자를 합쳐서 정규식으로 생성
+    return input.split(pattern);
+  }
+
+  sumNumbers(numbers) {
     return numbers.reduce((sum, number) => {
-      const parsedNumber = parseInt(number, 10);
+      const parsedNumber = parseInt(number, 10); // 문자열을 10 진수 숫자로 변환
       if (parsedNumber < 0) {
         throw new Error('[ERROR] 음수는 입력할 수 없습니다.');
       }
