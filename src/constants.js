3fb5c67bf39a60e786f376fe089ac358157db97a
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-20 19:45:39 +0900

    fix: INPUT_MESSAGE에 줄바꿈 추가

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const INPUT_MESSAGE = '덧셈할 문자열을 입력해 주세요.';
+export const INPUT_MESSAGE = '덧셈할 문자열을 입력해 주세요.\n';
 export const RESULT_MESSAGE = '결과 : ';
 export const ERROR_MESSAGE = '[ERROR]';
 
