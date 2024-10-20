ee9eb00c4a6feb64f738ef3510eba4e7f2839ca6
boxi <boxiyoun@gmail.com>
2024-10-21 05:34:50 +0900

    숫자 입력 부분 수정2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
-Console.print(1);
-
 
 class App {
-  #startCalculator() {
-    Console.readLineAsync('덧셈할 문자열을 입력해 주세요.', (num) => {
-      console.log(`숫자: ${num}`);
+  #startCalculator() { // 숫자 입력받는 함수
+    Console.readLineAsync('덧셈할 문자열을 입력해 주세요.', (userInput) => {
+      console.log(`입력: ${userInput}`);
     });
   }
-
 
   async run() {
     await this.#startCalculator();
   }
 };
+
 
 export default App;
