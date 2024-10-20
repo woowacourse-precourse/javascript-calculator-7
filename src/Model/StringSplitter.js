331a56a7296dad00b8ebefbdf2bb9f8499e1dbce
2wndrhs <2wndrhs@gmail.com>
2024-10-20 18:23:42 +0900

    feat: 문자열에서 커스텀 구분자를 기준으로 숫자를 분리하는 기능 구현

--- src/Model/StringSplitter.js
+++ src/Model/StringSplitter.js
@@ -1,6 +1,8 @@
 export default class StringSplitter {
   #DEFAULT_VALID_PATTERN = /^\d+([,:]\d+)*$/;
 
+  #CUSTOM_DELIMITER_PATTERN = /^\/\/(.)\\n/;
+
   #delimiters;
 
   #string;
@@ -8,11 +10,20 @@ export default class StringSplitter {
   constructor(input) {
     if (input.length === 0) {
       this.#string = '';
+      return;
     }
 
     if (this.#DEFAULT_VALID_PATTERN.test(input)) {
-      this.#delimiters = /[,:]/;
       this.#string = input;
+      this.#delimiters = /[,:]/;
+      return;
+    }
+
+    const found = input.match(this.#CUSTOM_DELIMITER_PATTERN);
+    if (found) {
+      const [fullMatch, delimiter] = found;
+      this.#string = input.slice(fullMatch.length);
+      this.#delimiters = delimiter;
     }
   }
 
