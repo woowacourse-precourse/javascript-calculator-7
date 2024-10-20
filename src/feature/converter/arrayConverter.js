ade249eb6e1fd5b47ae94bb010b6f2a529e50731
thumbthing <ilscc0902@gmail.com>
2024-10-20 15:04:37 +0900

    feat: 소수값에 대한 에러처리 추가

--- src/feature/converter/arrayConverter.js
+++ src/feature/converter/arrayConverter.js
@@ -14,12 +14,10 @@ function getNumberArray(stringArray) {
       throw ERROR;
     }
 
-    /** TODO : 소수인 경우 판별
-     *  에러 메시지는 '소수가 포함되어 있습니다. 커스텀 구분자가 . 으로 설정될 경우 원하는 계산값을 얻을 수 없을 수 있습니다.
-     */
-
     if(Math.floor(singleValue) !== singleValue) {
-      const ERROR_MESSAGE = ''
+      const ERROR_MESSAGE = '입력하신 값 중에 소수가 존재합니다.';
+      const ERROR = new Error(ERROR_MESSAGE);
+      throw ERROR;
     }
 
     return singleValue;
