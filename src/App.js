95d541936fe4010bb98c642e031213d077e801c3
jiyun1006 <jiyun4251@gmail.com>
2024-10-20 11:43:03 +0900

    ✨ feat : 커스텀 구분자를 가지는 체크
    isCustom 메서드를 이용해 입력된 문자열이 커스텀 구분자를 가지는지 확인.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const tmp = await getInputStr();
+    console.log(tmp);
+  }
 }
+
+async function getInputStr() {
+  try {
+    const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+    if(isCustom(inputStr))
+    return inputStr;
+  } catch (error) {
+    // reject 되는 경우
+  }
+}
+
+// 입력문에서의 에러 상황
+
+const isCustom = (inputStr) => {
+  const regExp = /(\/\/.{1}\n).{1,}/;
+  return regExp.test(inputStr);
+}
+
 
 export default App;
