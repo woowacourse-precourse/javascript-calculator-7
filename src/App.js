aa37990f6eea8cb023f496dbfb4c3f738b4a077d
Ray <rlawlsdn9583@gmail.com>
2024-10-21 01:33:06 +0900

    feat: 커스텀 구분자를 확인하고 추출하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ class App {
   async run() {
       Console.print('덧셈할 문자열을 입력해주세요.');
       const INPUT_DATA = await Console.readLineAsync("");
+
+      // 커스텀 구분자 추출 및 제거
+      const [EXTRACT_DATA, SEPARATORS] = this.extractSeparator(INPUT_DATA, this.defaultSeparator); 
   };
+
+  extractSeparator(inputString, separators) {
+    if (inputString.slice(0, 2) === "//" && inputString.slice(3, 5) === "\\n") {
+      separators.push(inputString[2]);
+      inputString = inputString.slice(5);
+    }
+    return [inputString, separators];
+  }
 }
 export default App;
