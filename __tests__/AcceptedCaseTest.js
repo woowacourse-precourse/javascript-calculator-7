4fb1b643c52563674a501214f72d706f9499cf73
silver93hq <gkagjsrb@gmail.com>
2024-10-20 13:20:25 +0900

    test: 정상 테스트 케이스 추가

--- __tests__/AcceptedCaseTest.js
+++ __tests__/AcceptedCaseTest.js
@@ -31,4 +31,14 @@ describe("허용되는 예외상황 테스트", () => {
     const app = new App();
     await expectResults(app, outputs);
   });
+
+  test.only("숫자와 구분자만 입력된 경우", async () => {
+    const inputs = ["1,2,3,4", "1:2:3:4"];
+    mockMultipleQuestions(inputs);
+
+    const outputs = ["결과 : 10", "결과 : 10"];
+
+    const app = new App();
+    await expectResults(app, outputs);
+  });
 });
