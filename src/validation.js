eafdecbfd4bf35157118055309c72f38e1b129d8
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-20 20:42:44 +0900

    refactor:` isValidateNumbers` 함수에서 유효성 검사 기능 분리 구현
    - 숫자 유효성 검사와, 음수 검사 기능을 각각 분리함

--- src/validation.js
+++ src/validation.js
@@ -1,11 +1,27 @@
 import { ERROR_MESSAGE } from './constants.js';
 
-function isValidateNumbers(numbers) {
+// 잘못된 문자를 검사하는 함수
+function isNumberValid(numbers) {
   for (const num of numbers) {
-    if (isNaN(num) || Number(num) < 0) {
-      throw new Error(ERROR_MESSAGE + '잘못 입력하였습니다.');
+    if (isNaN(num)) {
+      throw new Error(ERROR_MESSAGE + '잘못된 입력입니다.');
     }
   }
+}
+
+// 음수 검사 함수
+function isNonNegative(numbers) {
+  for (const num of numbers) {
+    if (Number(num) < 0) {
+      throw new Error(ERROR_MESSAGE + '음수는 입력할 수 없습니다.');
+    }
+  }
+}
+
+// 전체 유효성 검사를 하는 함수
+function isValidateNumbers(numbers) {
+  isNumberValid(numbers);
+  isNonNegative(numbers);
 }
 
 export default isValidateNumbers;
