25f60306e4fcc029b9826c234c6cdf471542cf33
guddnboy <hung3018@naver.com>
2024-10-20 18:31:45 +0900

    feat: 음수, 커스텀 구분자 체크 후 숫자 합산

--- src/App.js
+++ src/App.js
@@ -3,41 +3,56 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     let userInput = await Console.readLineAsync("문자열을 입력해주세요: ");
+    let result = 0;
 
-    function customIdentifer(userInput) {
-      const customRegex = /\/\/(.)\\n/;
-      let customString = userInput.match(customRegex);
+    function checkCustomIdentifier(userInput) {
+      const customRegex = /\/\/(.*)\\n/;
+      const customString = userInput.match(customRegex);
+      let customIdentifier = "";
 
       if (customString === null) {
         return userInput;
       } else {
-        const targetCustom = customString[1];
+        customIdentifier = customString[1];
 
-        let filteredUserInput = userInput.replaceAll(targetCustom, ",");
+        if (customIdentifier.length > 1) {
+          throw new Error("[ERROR] 커스텀 구분자는 1자리여야 합니다.");
+        }
+        if (!isNaN(customIdentifier)) {
+          throw new Error("[ERROR] 커스텀 구분자는 문자여야 합니다.");
+        }
 
-        return filteredUserInput;
+        if (customIdentifier === ":" || customIdentifier === ",") {
+          throw new Error("[ERROR] 기본 구분자와 중복됩니다.");
+        }
+      }
+      return customIdentifier;
+    }
+
+    function checkMinus(userInput) {
+      const minusRegex = /-\d+/g;
+      let minusString = userInput.match(minusRegex);
+      if (minusString === null) {
+        return userInput;
+      } else {
+        throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
       }
     }
 
-    function findMinus(userInput) {
-      const minusRegex = /-\d+/g;
-      let minusString = userInput.match(minusRegex);
-      return minusString;
-    }
-
-    function getSum(userInput) {
-      let sum = 0;
+    function calculateSum(userInput) {
       const targetRegex = /[^0-9:,]/g;
-      let targetString = userInput.replace(targetRegex, "");
-      let splitedString = targetString.split(/[:,]/);
+      let sum = 0;
 
-      splitedString.forEach((element) => {
-        let number = parseInt(element);
-        if (isNaN(number)) {
-          number = 0;
+      userInput = userInput.replace(targetRegex, "");
+
+      const splitedInput = userInput.split(/[:,]/);
+      splitedInput.map((element) => {
+        if (element !== "") {
+          element = parseInt(element);
+          sum += element;
         }
-        sum += number;
       });
+
       return sum;
     }
 
@@ -45,13 +60,15 @@ class App {
       Console.print(0);
       return;
     } else {
-      let result_custom = customIdentifer(userInput);
-      if (findMinus(userInput) !== null) {
-        throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
+      checkMinus(userInput);
+      if (userInput === checkCustomIdentifier(userInput)) {
+        result = calculateSum(userInput);
+        Console.print(`결과 : ${result}`);
+      } else {
+        userInput = userInput.replaceAll(checkCustomIdentifier(userInput), ",");
+        result = calculateSum(userInput);
+        Console.print(`결과 : ${result}`);
       }
-      let sum = getSum(result_custom);
-      Console.print(`결과 : ${sum}`);
-      return;
     }
   }
 }
