554443c57e1678a2fb104ce0af736de0baa9e0e2
rubberchin <88rubber@gmail.com>
2024-10-20 16:41:26 +0900

    fix($error) :대신 ;를 기본 구분자로 인식하던 문제 수정 - 기본 구분자 목록을 [,;]에서 [,:]로 변경 Update - (JS 컨벤션) 문자열에 쓰이는 ""를 ''로 변경

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ class App {
       let customSeparator;
       let finalNumbers;
 
-      const CUSTOM_START = userInput.indexOf("//");
-      const CUSTOM_END = userInput.indexOf("\\n");
+      const CUSTOM_START = userInput.indexOf('//');
+      const CUSTOM_END = userInput.indexOf('\\n');
      
       if(CUSTOM_START !== -1 && CUSTOM_END !== -1 && CUSTOM_START === 0) {
         customSeparator = userInput.substring(CUSTOM_START+2, CUSTOM_END);
         userInput = userInput.slice(CUSTOM_END+2);
         //커스텀 구분자로 절단 후 기본 구분자로 절단
         numbers = userInput.split(customSeparator);
-        finalNumbers = numbers.map(num => num.split(/[,;]/));
+        finalNumbers = numbers.map(num => num.split(/[,:]/));
       } else {
         //기본 구분자로 절단
-        finalNumbers = numbers.split(/[,;]/);
+        finalNumbers = numbers.split(/[,:]/);
       }
       
       //2차원 배열을 1차원 배열로 변환
@@ -29,15 +29,15 @@ class App {
     
     const isAllNum = function isAllNumericStrings(array) {
       return array.every(element => 
-        typeof element === "string" && /^\d+$/.test(element)
+        typeof element === 'string' && /^\d+$/.test(element)
       );
     }
   
     try {
       //유저에게 입력받기
-      let userInput = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
-      if (userInput.includes(" ")) {
-        throw new Error("spaceInInput");
+      let userInput = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+      if (userInput.includes(' ')) {
+        throw new Error('spaceInInput');
       } else {
         userInput = separateInput(userInput);
       }
@@ -47,13 +47,13 @@ class App {
         const inputSum = userInput.reduce((sum, num) => sum + Number(num), 0);
         Console.print(`결과 : ${inputSum}`);
       } else {
-        throw new Error("notAllNum");
+        throw new Error('notAllNum');
      }  
     } catch(error) {
-      if (error.message === "spaceInInput") {
-        throw new Error("[ERROR] 공백 없이 입력해 주세요!");
-      } else if (error.message === "notAllNum") {
-        throw new Error("[ERROR] 구분자와 숫자만 입력해주세요!");
+      if (error.message === 'spaceInInput') {
+        throw new Error('[ERROR] 공백 없이 입력해 주세요!');
+      } else if (error.message === 'notAllNum') {
+        throw new Error('[ERROR] 구분자와 숫자만 입력해주세요!');
       } else {
         throw new Error(`[ERROR] 알 수 없는 에러 발생: ${error.message}`);
       }  
