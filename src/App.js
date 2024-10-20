fee0c88564eab04c67588def51bbbc4266afb824
youdame <112458620+youdame@users.noreply.github.com>
2024-10-20 16:29:10 +0900

    feat(계산기): 쉼표와 콜론을 구분자로 사용하는 문자열의 숫자 합산 기능 추가
    쉼표(,)와 콜론(:)을 구분자로 가지는 문자열을 전달할 때, 이를 기준으로 숫자를 분리하고 그 합을 계산하는 기능을 구현
    - 입력 문자열을 구분자로 분리
    - 분리된 문자열을 숫자로 변환 후 합산

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+
+      const StringNumbers = input.split(/[,|:]/);
+
+      const sum = StringNumbers.reduce((acc, number) => {
+        return acc + Number(number);
+      }, 0);
+
+      Console.print(sum);
+    } catch {}
+  }
 }
 
 export default App;
