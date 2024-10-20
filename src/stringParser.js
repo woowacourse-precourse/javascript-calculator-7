e294b71c11fc833387541fdc38bc1e54cf9feb6b
khwww <khw4420@knu.ac.kr>
2024-10-21 05:20:56 +0900

    feat(stringParser): 숫자 유효성 검사 기능 추가

--- src/stringParser.js
+++ src/stringParser.js
@@ -10,12 +10,10 @@ class StringProcessor {
   }
 
   processInput() {
-    // '0-empty'인 경우는 빈 입력에서 0으로 처리
     if (this.inputValue === "0-empty") {
       return [0];
     }
 
-    // 커스텀 구분자 처리
     if (this.inputValue.startsWith("//")) {
       return processCustomDelimiter(
         this.inputValue,
@@ -23,7 +21,6 @@ class StringProcessor {
       );
     }
 
-    // 기본 구분자 처리
     return processDefaultDelimiter(
       this.inputValue,
       this.processNumbers.bind(this)
@@ -31,18 +28,20 @@ class StringProcessor {
   }
 
   processNumbers(numbers) {
-    for (let num of numbers) {
+    numbers.forEach((num) => {
       const validatedNum = this.validateNumber(num);
       this.result.push(validatedNum);
-    }
+    });
     return this.result;
   }
 
   validateNumber(inputString) {
     const number = Number(inputString);
-    if (!/^\d+$/.test(inputString) || number <= 0) {
+
+    if (isNaN(number) || number <= 0) {
       throw new Error("유효하지 않은 숫자가 포함되었습니다.");
     }
+
     return number;
   }
 }
