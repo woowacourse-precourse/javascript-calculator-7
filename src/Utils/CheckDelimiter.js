3e2ec962c06b875ca4cb4e8db788584e2af7fdb0
jin123457 <jin_ox@naver.com>
2024-10-20 16:44:01 +0900

    fix (CheckDelimiter) : update check negatives with '-' delimiter

--- src/Utils/CheckDelimiter.js
+++ src/Utils/CheckDelimiter.js
@@ -9,7 +9,11 @@ const CheckDelimiter = (str) => {
     throw new Error(`[ERROR] 구분자 혹은 커스텀 구분자가 존재하지 않습니다.`);
   }
 
-  if (FIND_DELIMITER.indexOf('-') === -1 || /-{2}\d+/.test(str)) {
+  if (
+    FIND_DELIMITER.indexOf('-') === -1 ||
+    /\d+-{2}\d+/.test(str) ||
+    /[^\d+]-\d+/.test(str)
+  ) {
     ContainsNegative(str);
   }
   const DELIMITER_SET = new Set([...FIND_DELIMITER]);
