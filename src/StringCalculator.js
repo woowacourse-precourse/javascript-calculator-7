b4172e681ce25d599ed38f350035e74efe03ac2c
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-10-20 14:40:21 +0900

    feat: 기본구분자, 커스텀구분자 정의 및 추출기능 추가

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -28,6 +28,20 @@ class StringCalculator {
 
     return numbers.reduce((acc, curr) => acc + Number(curr), 0);
   }
+
+  extractDelimiters(input) {
+    const delimiterPattern = /^\/\/(.*?)\\n/; // //로 시작하고 \n로 끝나는 부분을 찾음
+    const match = input.match(delimiterPattern);
+    let delimiters = [",", ":"]; // 기본 구분자
+
+    if (match) {
+      const customDelimiter = match[1]; // 커스텀 구분자 추출 후 공백 제거
+      delimiters = [customDelimiter, ...delimiters]; // 커스텀 구분자 추가
+      input = input.slice(match[0].length); // 커스텀 구분자 부분 잘라내고 공백 제거
+    }
+
+    return { delimiters, trimmedInput: input }; // 이스케이프 문자를 포함하지 않도록 반환
+  }
 }
 
 export default StringCalculator;
