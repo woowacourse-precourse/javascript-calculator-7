c9beecae7091775753f826a46b96d2bfc14f9762
j0n0m2 <dbsk8223@naver.com>
2024-10-20 18:32:05 +0900

    구분자를 사용해 숫자를 분리한다

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n", (INPUT));
+    const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
     // 빈 문자열은 0을 반환한다.
     if (INPUT === "") {
-      Console.print(`결과 : 0`)
-    } else {
-      Console.print(`결과 : ${INPUT}`)
+      Console.print(`결과 : 0`);
+      return;
     }
+    //구분자를 사용해 숫자를 분리한다
+    let DELIMITER = /,|:/;
 
-  //g
+    const NUMBERS = INPUT.split(DELIMITER).map(Number)
   }
 }
 
