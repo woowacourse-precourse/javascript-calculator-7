3711124a33212ea3ad3a68766014ef9f84c2627d
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-10-20 14:41:53 +0900

    feat: 구분자를 기준으로 숫자를 더하는 기능 구현 및 예외처리

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ class App {
   async run() {
     try {
       const userInput = await getUserInput().then((result) => result.trim());
-      const separator = [':', ','];
+      const separators = [':', ','];
       let formula = '';
-      if (
-        !(
-          userInput.length > 0 &&
-          (userInput.slice(0, 2) === '//' || !isNaN(userInput[0]))
-        )
-      ) {
+      if (userInput.length === 0) {
+        Console.print(0);
+        return;
+      } else if (!(userInput.slice(0, 2) === '//' || !isNaN(userInput[0]))) {
         // 공백 제거후 값이 비어있거나 string이 '//' 혹은 숫자로 시작되지 않는경우 에러 반환
         throw '입력 형식이 올바르지 않습니다.';
       }
@@ -25,20 +23,36 @@ class App {
           // "//"와 "\n" 사이에 문자열이 있는지 확인
           const customSeprator = userInput.slice(2, endIndex);
 
-          if (!customSeparatorFormat(customSeprator)) {
+          if (!customSeparatorsFormat(customSeprator)) {
             // 커스텀 구분자에 숫자나 공백이 포함된 경우 에러반환
             throw '커스텀 구분자에 공백이나 숫자는 포함할 수 없습니다.';
           }
 
-          separator.push(customSeprator);
+          separators.push(customSeprator);
           formula = userInput.substr(endIndex + 2);
         } else {
           // \n이 없는 경우 에러 반환
           throw '커스텀 구분자를 사용하려면 \\n을 입력해주세요';
         }
+      } else {
+        formula = userInput;
       }
 
-      Console.print(userNumber);
+      const answer = separators
+        .reduce(
+          (number, separator) => {
+            return number.flatMap((part) => part.split(separator));
+          },
+          [formula],
+        )
+        .reduce((acc, current) => {
+          if (acc === '' || current === '') {
+            throw '구분자를 연속해서 입력하셨습니다.';
+          }
+          return Number(acc) + Number(current);
+        }, 0);
+
+      Console.print(answer);
     } catch (error) {
       // Console.print(userInput);
       Console.print(ERROR_MESSAGE + error);
@@ -46,7 +60,7 @@ class App {
   }
 }
 
-function customSeparatorFormat(str) {
+function customSeparatorsFormat(str) {
   for (let i = 0; i < str.length; i++) {
     if (!isNaN(str[i]) || str[i] === ' ') {
       // 문자가 숫자이거나 공백이면 true 반환
