6bd041fcf7e83552ebd208239482a41774b89520
minbo <smb5174@naver.com>
2024-10-20 16:41:28 +0900

    feat: 커스텀 구분자가 있는 경우, 커스텀 구분자를 포함해서 구분

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import BASIC_REGEXP from "./regExp.js";
 
 class App {
   async run() {
@@ -7,6 +8,24 @@ class App {
 
     // 다음 줄에서 입력을 받음 + 입력 받은 문자열의 양 끝 공백 제거
     const input = await Console.readLineAsync("").then((input) => input.trim());
+
+    // "//"를 확인하기 위해 첫 두 글자 가져오기
+    const startString = input.slice(0, 2);
+
+    if (startString === "//") {
+      const parts = input.indexOf("\\n");
+
+      if (parts !== -1) {
+        // "\n"이 있는 경우
+        const newSeparator = input.slice(2, parts);
+        const dynamicRegExp = new RegExp(`[,:]|${newSeparator}`);
+        const array = input.slice(parts + 2).split(dynamicRegExp);
+      } else {
+        // "\n"이 없는 경우
+        throw new Error(JSON.stringify("ERROR: //는 있는데 \n는 없습니다."));
+      }
+    } else {
+    }
   }
 }
 
