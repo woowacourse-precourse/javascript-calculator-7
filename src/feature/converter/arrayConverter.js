944d4885016809562b01eb3a34039f89eb399d6b
thumbthing <ilscc0902@gmail.com>
2024-10-20 15:01:45 +0900

    fix: 양수가 아닌 값에 대한 에러처리 메시지 수정

--- src/feature/converter/arrayConverter.js
+++ src/feature/converter/arrayConverter.js
@@ -9,9 +9,17 @@ function getNumberArray(stringArray) {
     }
 
     if(singleValue < 0) {
-      const ERROR_MESSAGE = '입력하신 값 중에 정수가 아닌 값이 존재합니다.';
+      const ERROR_MESSAGE = '입력하신 값 중에 양수가 아닌 값이 존재합니다.';
       const ERROR = new Error(ERROR_MESSAGE);
       throw ERROR;
+    }
+
+    /** TODO : 소수인 경우 판별
+     *  에러 메시지는 '소수가 포함되어 있습니다. 커스텀 구분자가 . 으로 설정될 경우 원하는 계산값을 얻을 수 없을 수 있습니다.
+     */
+
+    if(Math.floor(singleValue) !== singleValue) {
+      const ERROR_MESSAGE = ''
     }
 
     return singleValue;
