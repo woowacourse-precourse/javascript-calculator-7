ad4d5c76cca891bdfdeb7233e845eb960616e9b1
ParkjuYoung <kwn03111@gmail.com>
2024-10-20 17:19:43 +0900

    refactor:  rename variables to camelCase
    사용자 입력에 따라 값이 변하기 때문에 상수가 아니라고 판단하여 카멜케이스로 변경함

--- src/Model/NumberSumExtractor.js
+++ src/Model/NumberSumExtractor.js
@@ -1,6 +1,6 @@
 export const SumNumbersFromString = (input) => {
-  const NUMBER_LIST = (input.match(/\d+/g) || []).map(Number);
-  const RESULT = NUMBER_LIST.reduce((acc, num) => acc + num, 0);
+  const numberList = (input.match(/\d+/g) || []).map(Number);
+  const result = NUMBER_LIST.reduce((acc, num) => acc + num, 0);
 
-  return RESULT;
+  return result;
 };
