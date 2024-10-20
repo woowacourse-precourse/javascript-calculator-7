9bb0cce962ac1fa2fa0f11e4e52b1c4ec6bc9b71
jiyun1006 <jiyun4251@gmail.com>
2024-10-20 13:51:30 +0900

    🐛 fix : 커스텀 구분자 메서드 수정
    커스텀 구분자 메서드에 대한 수정사항
    - 메서드 내에서 문자열을 자르는 방식 제거.
    - 커스텀 구분자를 가지는지 아닌지에 대한 boolean 객체 생성.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App {
 
 async function getInputStr() {
   try {
-    const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    let inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const separator = [":", ","];
+    let customBoolean = false;
     // 커스텀 구분자를 가지는지 확인.
-    isCustom(inputStr)
-    const tmpSep = inputStr.slice(2, 3);
-    console.log(tmpSep);
-    
+    customBoolean = isCustom(inputStr);
+
+    // 구분자를 이용해 문자열 구분.
     return inputStr;
   } catch (error) {
      return error;
@@ -27,12 +27,14 @@ async function getInputStr() {
 
 const isCustom = (inputStr) => {
   const regExp = /(\/\/.{1}\\n){1}.{0,}/; 
-  const regExp2 = /(\/\/.{1}\\n).{0,}/;
+  const regExp2 = /(\/\/\D{1}\\n){1}.{0,}/;
   if (regExp.test(inputStr)) { // 커스텀 구분자의 형식을 가진 문자열 이라면,
     if (!regExp2.test(inputStr)) {
       throw new Error("[ERROR] 잘못된 입력 형식 입니다.(구분자 길이 오류 or 잘못된 커스텀 구분자 형식)");
     }
+    return true;
   }
+  return false;
 }
 
 
