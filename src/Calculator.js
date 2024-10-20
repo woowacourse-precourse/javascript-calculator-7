a44701b0b70e11672c1760560488e5d32fe4d126
mywhzy <cmj2393a@naver.com>
2024-10-20 22:27:30 +0900

    feat: 기본 구분자 기준 숫자 분리 기능 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,17 @@
+import { DEFAULT_SEPARATORS } from './constants.js';
+
 class Calculator {
   #inputText;
+  #numbers;
 
   constructor(input) {
     this.#inputText = input;
+    this.#numbers = 0;
+  }
+
+  splitText() {
+    const separatorsRegex = new RegExp(`${DEFAULT_SEPARATORS.join('|')}`, 'g');
+    this.#numbers = this.#inputText.split(separatorsRegex).map(Number);
   }
 }
 
