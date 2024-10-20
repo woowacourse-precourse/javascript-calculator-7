714ff7439094d84cd050ac26a72895ece0340df4
minbo <smb5174@naver.com>
2024-10-20 15:03:40 +0900

    feat: 사용자에게 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 입력 받는 메세지를 먼저 출력
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+
+    // 다음 줄에서 입력을 받음
+    const input = await Console.readLineAsync("");
+  }
 }
 
 export default App;
