b08bd27c9ae53380d18d46e808d3017bf0bf2983
JAESEUNGJAE <valkyrie9923@gmail.com>
2024-10-21 01:57:10 +0900

    feat(app.js): 계산기의 입력 값 받는 기능 및 덧겜 결과 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await this.getInput();
+    try {
+      const result = this.calculate(input);
+      console.log(`결과 : ${result}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async getInput() {
+    // MissionUtils.Console.readLineAsync를 사용하여 입력 받기
+    return await MissionUtils.Console.readLineAsync('입력: ');
+  }
+
+  calculate(input) {
+    if (!input) return 0;
+    let delimiter = /,|:/; // 기본 구분자
+    const numbers = input.split(delimiter).map(Number);
+    // 예외 처리 (음수 값 등)
+    const nan = numbers.filter(n => isNaN(n));
+    if (nan.length > 0) {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
+    const negatives = numbers.filter(n => n < 0);
+    if (negatives.length > 0) {
+      throw new Error("[ERROR] 음수는 사용할 수 없습니다.");
+    }
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
 }
 
 export default App;
