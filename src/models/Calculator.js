8ae6ee3da6c0134d68c992590a6f7cd49addc865
chungyo <kimchungy98@gmail.com>
2024-10-20 18:18:43 +0900

    refactor: Calculator 클래스 추가

--- src/models/Calculator.js
+++ src/models/Calculator.js
@@ -1,1 +1,21 @@
 import {validateInput, validateNumbers} from "../utils/Validator.js";
+import {add} from "../utils/add.js";
+import {extractCustomDelimiter, splitByCustomDelimiter, splitByDefaultDelimiters} from "../utils/DelimiterHandler.js";
+
+export class Calculator {
+    calculate(input){
+        validateInput(input);
+
+        let numbers;
+        const customDelimiterData = extractCustomDelimiter(input);
+        if(customDelimiterData){
+            numbers = splitByCustomDelimiter(customDelimiterData.numbers, customDelimiterData.delimiter);
+        }
+        else{
+            numbers = splitByDefaultDelimiters(input);
+        }
+
+        validateNumbers(numbers);
+        return add(numbers);
+    }
+}
