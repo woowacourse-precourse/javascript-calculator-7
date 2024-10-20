ce9fa6ef3b0fce24199792a05b09e06f99addd65
BoRamSon <onbrson1@gmail.com>
2024-10-20 14:50:27 +0900

    feat : add [5] 일반구분자를 처리하는 함수를 만듭니다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {}
@@ -6,21 +6,18 @@ class App {
 
 export default App;
 
-
-
 // [1] 사용자 입력값 임시테스트값 지정
 const inputValue = "//;\n2 ;4; 5; 7";
 // const inputValue = "1 ;2|3 :5, 8";
 
-
 // [2] 입력값 공백제거
 const INPUT_VALUE = inputValue.replace(/[^\S\n]/g, "");
-console.log('입력값이 무엇이죠? : ', INPUT_VALUE);
+console.log("입력값이 무엇이죠? : ", INPUT_VALUE);
 
 // [3] 커스텀구분자 여부를 확인합니다.
 const CUSTOM_PATTERN = /^\/\//;
 const IS_THIS_CUSTOM = CUSTOM_PATTERN.test(INPUT_VALUE); // true, flase 여부 반환
-console.log('커스텀 구분자인가요? : ', IS_THIS_CUSTOM);
+console.log("커스텀 구분자인가요? : ", IS_THIS_CUSTOM);
 
 // [4] 커스텀구분자를 처리하는 함수를 만듭니다.
 const EXTRACT_CUSTOM_DELI = (INPUT_VALUE) => {
@@ -30,4 +27,11 @@ const EXTRACT_CUSTOM_DELI = (INPUT_VALUE) => {
   const CUSTOM_DELI_COMP = EXTRACT_NUMBERS.split(CUSTOM_DELIMITER);
   const CUSTOM_DELI_NUM_ARR = CUSTOM_DELI_COMP.map(Number);
   return CUSTOM_DELI_NUM_ARR;
+};
+
+// [5] 일반구분자를 처리하는 함수를 만듭니다.
+const EXTRACT_GEN_DELI = (INPUT_VALUE) => {
+  const GEN_DELI_COMP = INPUT_VALUE.split(/[,|;: ]+/);
+  const GEN_DELI_NUM_ARR = GEN_DELI_COMP.map(Number);
+  return GEN_DELI_NUM_ARR;
 };
