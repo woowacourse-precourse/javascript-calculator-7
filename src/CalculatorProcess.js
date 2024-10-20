ffa53232fa50afb0325b722caa271683320d996c
조승우 <choseung97@gmail.com>
2024-10-21 03:33:48 +0900

    feat(calculator): 커스텀 구분자로 수가 구분된 문자열 중 숫자만을 배열에 할당하는 기능을 추가한다
    커스텀 구분자로 숫자가 구분될 때, 구분자를 기준으로 parsing하여 숫자를 배열에 할당한다.

--- src/CalculatorProcess.js
+++ src/CalculatorProcess.js
@@ -5,17 +5,25 @@ class CalculatorProcess {
 
   inputString = '';
 
-  seperator = undefined;
-
   async getString() {
     const INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
     this.inputString = INPUT;
   }
 
   doParsing() {
-    if (/^-?\d+([,:]-?\d+)*$/.test(this.inputString)) {
-      const parsedNums = this.inputString.match(/-?\d+/g);
-      this.inputNums = parsedNums.map((element) => Number(element));
+    if (/^\d+([,:]\d+)*$/.test(this.inputString)) {
+      const parsedNums = this.inputString.match(/\d+/g);
+      this.inputNums = parsedNums.map(Number);
+    } else {
+      const customDelimiterMatch = this.inputString.match(/^\/\/(.)\\n(.*)$/);
+      if (customDelimiterMatch) {
+        const customDelimiter = new RegExp(`[${customDelimiterMatch[1]},:]`);
+        this.inputString = customDelimiterMatch[2];
+        const parsedNums = this.inputString.split(customDelimiter).map(Number);
+        this.inputNums = parsedNums;
+      } else {
+        throw new Error('[ERROR] 잘못된 형식의 입력입니다.');
+      }
     }
   }
 
