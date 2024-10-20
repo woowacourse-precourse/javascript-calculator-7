7be16b938e2fb71da5d3bc6a808b07eee36fdc61
seungJoo <sj10230506@gmail.com>
2024-10-20 19:20:47 +0900

    fix: throw error 하도록 수정

--- src/App.js
+++ src/App.js
@@ -3,52 +3,44 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
 
-    try {
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    let input = await Console.readLineAsync("");
 
-      Console.print("덧셈할 문자열을 입력해 주세요.");
-      let input = await Console.readLineAsync("");
-
-      if(input.length === 0){
-        this.printResult(0);
-        return;
-      }
-
-      let delimiter;
-      if (input.length > 0 && input.startsWith('//')) { //커스텀 구분자가 지정된 경우
-        delimiter = this.getDelimiter(input);
-        input = input.split('\\n')[1];
-      }
-      else {  //기본 구분자를 사용하는 경우
-        delimiter = /[,:]/;
-      }
-
-      let numbers = input.split(delimiter);
-
-      numbers = numbers.map((v) => {
-        if (v.length === 0 || isNaN(+v))
-          throw new Error("[ERROR} 잘못된 입력입니다.");
-        if (+v < 0)
-          throw new Error("[ERROR] 잘못된 입력입니다. 음수가 입력에 포함됩니다.");
-        return +v;
-      })
-      let sum = numbers.reduce((acc, cur) => acc + cur, 0);
-
-      Console.print(`결과 : ${sum}`);
-
-    }
-    catch (err) {
-      Console.print(err.message);
+    if (input.length === 0) {
+      this.printResult(0);
       return;
     }
 
+    let delimiter;
+    if (input.length > 0 && input.startsWith('//')) { //커스텀 구분자가 지정된 경우
+      delimiter = this.getDelimiter(input);
+      input = input.split('\\n')[1];
+    }
+    else {  //기본 구분자를 사용하는 경우
+      delimiter = /[,:]/;
+    }
+
+    let numbers = input.split(delimiter);
+
+    numbers = numbers.map((v) => {
+      if (v.length === 0 || isNaN(+v))
+        throw new Error("[ERROR] 잘못된 입력입니다.");
+      if (+v < 0)
+        throw new Error("[ERROR] 잘못된 입력입니다. 음수가 입력에 포함됩니다.");
+      return +v;
+    })
+    let sum = numbers.reduce((acc, cur) => acc + cur, 0);
+
+    Console.print(`결과 : ${sum}`);
+    
   }
 
-  printResult(sum){      
+  printResult(sum) {
     Console.print(`결과 : ${sum}`);
   }
 
   getDelimiter(input) {
-    const match = input.match(/\/\/(.*)\\n/);  // "//"와 "\n" 사이의 커스텀 구분자 추출
+    const match = input.match(/\/\/(.)\\n(.*)/);  // "//"와 "\n" 사이의 커스텀 구분자 추출
     if (match === null) {
       throw new Error("[ERROR] 잘못된 입력입니다. 커스텀 구분자를 읽을 수 없습니다.");
     }
