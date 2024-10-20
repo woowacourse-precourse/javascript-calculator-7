ebc35b0ed0fae2e918ac183d469ba2010c19da4b
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-20 18:59:03 +0900

    feat: 입력 및 출력을 포함한 계산기 실행 로직 구현
    - 문자열 덧셈 계산기의 기능을 갖춘 App 클래스 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import calculate from './calculate.js';
+import DelimiterParser from './DelimiterParser.js';
+import printMessage from './utils/print-output.js';
+import readInput from './utils/read-input.js';
+import { RESULT_MESSAGE } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await readInput();
+
+    const separator = new DelimiterParser(input);
+    const numbers = separator.getNumbers();
+
+    const result = calculate(numbers);
+    printMessage(`${RESULT_MESSAGE}${result}`);
+  }
 }
 
 export default App;
