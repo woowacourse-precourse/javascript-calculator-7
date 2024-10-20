2891f8c9bd579dc8058812acc54a0c8f3306242c
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-10-20 18:45:22 +0900

    feat: 입력값이 인지 확인하는 기능

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ class App {
   async run() {
     Console.print('덧셈할 문자열을 입력해 주세요.')
     const input = await Console.readLineAsync('');
+
+    if (input === ""){
+      Console.print('0')
+    }
   }
 }
 
