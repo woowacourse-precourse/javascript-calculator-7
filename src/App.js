6ba3d56087ebb350cc1f60ea5e036821500fdbba
mun-kyeong <horse6953@naver.com>
2024-10-20 13:36:06 +0900

    [FEAT] (App.js) : 사용자로부터 입력받기
    - Console.readLineAsync() 함수를 사용해 사용자로부터 입력받기
    - try-catch문 사용
    - 빈 문자열일경우 값이 들어올 때 까지 반복 실행
    - 정상적인 입력이 들어왔을 경우 Controller로 input 전송
    - Controller에서 입력 들어왔는지 확인
    
    사용자로부터 입력을 받고 해당 입력을 Controller로 전송합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console } from "@woowacourse/mission-utils";
+import handleCalculation from "./controllers/CalculatorController.js";
+
 class App {
-  async run() {}
+  async run() {
+    async function getInputString() {
+      while (true) {
+        try {
+          const input = await Console.readLineAsync(
+            "덧셈할 문자열을 입력해 주세요.\n"
+          );
+
+          if (input.trim() === "") {
+            Console.print(
+              "[ERROR] 입력오류 : 빈 문자열입니다. 다시 입력해주세요\n\n "
+            );
+            continue;
+          }
+          return input;
+        } catch (error) {
+          Console.print(
+            "[ERROR] 입력 오류 \n 유효하지 않은 입력입니다. 다시 시도해 주세요.\n"
+          );
+          continue;
+        }
+      }
+    }
+    getInputString().then((input) => {
+      handleCalculation(input);
+    });
+  }
 }
 
 export default App;
---
+++ src/controllers/CalculatorController.js
@@ -0,0 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+
+function handleCalculation(input) {
+  Console.print(`입력받은 문자열: ${input}`);
+}
+
+export default handleCalculation;
