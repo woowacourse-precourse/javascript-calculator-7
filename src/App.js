f36b94234bb243dceda405eb970d6b226ab7f87d
silver0108 <kimsoeun0108@gmail.com>
2024-10-21 02:58:21 +0900

    refactor: 분리한 숫자들 검증 함수 분리

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import { inputUserString } from "./Input.js";
 import { getCustomDelimiter } from "./utils/getCustomDelimiter.js";
 import { getNumber } from "./utils/getNumber.js";
-import { isNumber } from "./validators/numberValidator.js";
+import { validateNumbers } from "./validators/numberValidator.js";
 import { validateInput } from "./validators/inputValidator.js";
-import { throwError } from "./utils/throwError.js";
 
 class App {
   async run() {
-    let nums = [];
     const customRegex = /^\/\/(.*?)\\n/;
     const userInput = await inputUserString();
+
     validateInput(userInput);
+
     const customDelimiter = getCustomDelimiter(userInput, customRegex);
+    const filterdInput = userInput.replace(customRegex, "");
+    const nums = getNumber(filterdInput, customDelimiter || /,|:/);
 
-    if (customDelimiter)
-      nums = getNumber(userInput.replace(customRegex, ""), customDelimiter);
-    else nums = getNumber(userInput, /,|:/);
-
-    for (const n of nums) {
-      if (!isNumber(n)) throwError("invalid number");
-    }
+    validateNumbers(nums);
 
     const sum = nums.reduce((acc, val) => acc + parseInt(val), 0);
     MissionUtils.Console.print(`결과 : ${sum}`);
--- src/validators/numberValidator.js
+++ src/validators/numberValidator.js
@@ -1,5 +1,13 @@
+import { throwError } from "../utils/throwError.js";
+
 // 숫자인지 검증
 export const isNumber = (value) => {
   if (isNaN(value)) return false;
   return true;
+};
+
+export const validateNumbers = (nums) => {
+  nums.forEach((n) => {
+    if (!isNumber(n)) throwError("invalid number");
+  });
 };
