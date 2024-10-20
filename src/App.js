b7a05bc578ed4480d95ed2f868986da3b4247598
MINJOO-KIM <gkminju2000@gmail.com>
2024-10-20 17:22:15 +0900

    feat: 유효한 입력값인지 확인하는 함수 추가 - isValidInput

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ class App {
       const input = await MissionUtils.Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요."
       );
+      if (this.isValidInput(input)) {
+        this.plus();
+      } else {
+      }
     } catch (error) {
-      MissionUtils.Console.print("[ERROR] ");
+      MissionUtils.Console.print(`[ERROR] ${error}`);
     }
   }
+
+  // 유효한 입력값인지 확인하는 함수
+  isValidInput(input) {
+    return true;
+  }
+
+  // 숫자의 합을 계산하는 함수
+  plus() {}
 }
 
 export default App;
