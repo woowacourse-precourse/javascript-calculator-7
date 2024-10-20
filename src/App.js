771a2ad9079fdfb65629e046acd3e2aafab44a70
oko_jin <dswvgw6412@naver.com>
2024-10-21 03:28:45 +0900

    chore(app): 예상치 못한 에러를 app에서 처리하도록 변경
    unexpectedError를 ioService => app으로 이동

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { errorMessages } from './constant.js';
-import { inputService } from './lib/IOService.js';
+import Calculator from '../Calculator.js';
+import Validator from '../Validator.js';
 
 class App {
   async run() {
     try {
-      inputService();
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+      if (!input) {
+        Console.print('결과 : 0');
+        return;
+      }
+
+      const calculator = new Calculator(input);
+      const validator = new Validator();
+
+      if (validator.valid(input)) {
+        Console.print(`결과 : ${calculator.add(input)}`);
+        return;
+      } else {
+        throw Error(errorMessages.unexpectedError);
+      }
     } catch (e) {
       throw Error(errorMessages.unexpectedError);
     }
