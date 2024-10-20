6d50e1b82501586a81af6875bc4f911e32a60b69
sky121016 <sky121016@naver.com>
2024-10-20 21:24:46 +0900

    feat: 합계 연산

--- src/App.js
+++ src/App.js
@@ -3,81 +3,84 @@ import {Console} from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-
-    try{
-      let delimiters = [',',':'];
-      let numbers = [];
+    try {
+      let DELIMITERS = [",", ":"];
+      let NUMBERS = [];
 
       // 입력
-      const input = await this.getInput();
-
+      const INPUT = await this.getInput();
 
       // 커스텀 구분자 파싱 후 숫자 입력만 반환
-      const inputNumbers = await this.customDelimiter(input, delimiters);
-
+      const INPUT_NUMBERS = await this.customDelimiter(INPUT, DELIMITERS);
 
       // 숫자 파싱
-      numbers = await this.getNumbers(inputNumbers, numbers, delimiters);
+      NUMBERS = await this.getNumbers(INPUT_NUMBERS, NUMBERS, DELIMITERS);
 
       // 합계 연산
+      const SUM = await this.getSum(NUMBERS);
 
       // 결과 출력
-
-
-    }catch(error){
-
-    }
+    } catch (error) {}
   }
 
   // 입력
-  async getInput(){
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    return input;
+  async getInput() {
+    const INPUT = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    return INPUT;
   }
 
-
   // 커스텀 구분자 파싱
-  async customDelimiter(input, delimiters){
-    
-    let s = 0;
-    let e = 2;
-    
+  async customDelimiter(INPUT, DELIMITERS) {
+    let START = 0;  
+    let END = 2;
+
     // 앞부분에서 // 와 \n 사이에 있으면 모두 구분자
-    while (input.substring(s, e) == "//" && e < input.length) {
-      while (e < input.length && input.substring(e, e + 2) !== "\\n") {
-        e++;
+    while (INPUT.substring(START, END) == "//" && END < INPUT.length) {
+      while (END < INPUT.length && INPUT.substring(END, END + 2) !== "\\n") {
+        END++;
       }
-      
-      const newDelimiter = input.substring(s + 2, e);
+
+      const NEW_DELIMITER = INPUT.substring(START + 2, END);
 
       // 구분자 배열에 push
-      delimiters.push(newDelimiter);
+      DELIMITERS.push(NEW_DELIMITER);
 
       // index 재설정
-      s = e + 2;
-      e = s + 2;
+      START = END + 2;
+      END = START + 2;
     }
 
-    const inputNumbers = input.substring(s);
-    return inputNumbers;
+    const INPUT_NUMBERS = INPUT.substring(START);
+    return INPUT_NUMBERS;
   }
 
   // 숫자 파싱
-  async getNumbers(input, numbers, delimiters){
+  async getNumbers(INPUT, NUMBERS, DELIMITERS) {
     // split을 위해 구분자 한 문자열로 병합
-    let splitD = delimiters.join('');
+    let splitD = DELIMITERS.join("");
 
-    const regex = new RegExp(`[${splitD}]`, 'g');
+    const REGEX = new RegExp(`[${splitD}]`, "g");
 
     // 숫자 split
-    const stringNumbers = input.split(regex);
+    const STRING_NUMBERS = INPUT.split(REGEX);
 
     // 숫자 형태로 배열에 저장
-    numbers = stringNumbers.map(Number);
+    NUMBERS = STRING_NUMBERS.map(Number);
 
-    return numbers;
+    return NUMBERS;
   }
 
+  async getSum(NUMBERS) {
+    let SUM = 0;
+
+    for (let i = 0; i < NUMBERS.length; i++) {
+      SUM += NUMBERS[i];
+    }
+
+    return SUM;
+  }
 }
 
 export default App;
