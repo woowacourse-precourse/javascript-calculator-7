ca901f187398f1a0591d62cefa82836e0bc99983
spoyodevelop <kjy9505@gmail.com>
2024-10-20 16:17:51 +0900

    fix: quick fix for checking where bug happens

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import { SYSTEM_MESSAGES } from './Constraints/Constraints.js';
 
 class App {
   async run() {
-    try {
-      const str = await Console.readLineAsync(SYSTEM_MESSAGES.ASK_USER_INPUT);
-      const result = parseString(str);
-      Console.print(`${SYSTEM_MESSAGES.PARSE_RESULT}${result}`);
-    } catch (error) {
-      Console.print(error.message);
-      throw error;
-    }
+    const str = await Console.readLineAsync(SYSTEM_MESSAGES.ASK_USER_INPUT);
+    const result = parseString(str);
+    Console.print(`${SYSTEM_MESSAGES.PARSE_RESULT}${result}`);
   }
 }
 
