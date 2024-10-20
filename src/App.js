47c591b88f85eea3926c6e424c0d70b98f02f566
BoRamSon <onbrson1@gmail.com>
2024-10-20 14:49:37 +0900

    feat : add [4] 커스텀구분자를 처리하는 함수를 만듭니다.

--- src/App.js
+++ src/App.js
@@ -21,3 +21,13 @@ console.log('입력값이 무엇이죠? : ', INPUT_VALUE);
 const CUSTOM_PATTERN = /^\/\//;
 const IS_THIS_CUSTOM = CUSTOM_PATTERN.test(INPUT_VALUE); // true, flase 여부 반환
 console.log('커스텀 구분자인가요? : ', IS_THIS_CUSTOM);
+
+// [4] 커스텀구분자를 처리하는 함수를 만듭니다.
+const EXTRACT_CUSTOM_DELI = (INPUT_VALUE) => {
+  const WHAT_CUSTOM_DELI = INPUT_VALUE.match(/^\/\/(.+)\n/);
+  const CUSTOM_DELIMITER = WHAT_CUSTOM_DELI[1];
+  const EXTRACT_NUMBERS = INPUT_VALUE.split("\n")[1];
+  const CUSTOM_DELI_COMP = EXTRACT_NUMBERS.split(CUSTOM_DELIMITER);
+  const CUSTOM_DELI_NUM_ARR = CUSTOM_DELI_COMP.map(Number);
+  return CUSTOM_DELI_NUM_ARR;
+};
