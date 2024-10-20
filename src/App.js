a60f7b91ebf64760b022e8f5112aa7f4b1281d6a
seunghyeon77 <jdyjsh77@naver.com>
2024-10-20 14:54:20 +0900

    refactor: 비동기 오류 처리를 위해 try catch문 삭제

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { StrSumFunc } from "./StrSumFunc";
 
 class App {
   async run() {
-    try {
-      const input = await Console.readLineAsync("문자열을 입력하세요: ");
-      const result = StrSumFunc(input);
-      Console.print(`결과: ${result}`);
-    } catch (err) {
-      Console.print(`오류: ${err.message}`);
-    }
+    const input = await Console.readLineAsync("문자열을 입력하세요: ");
+    const result = StrSumFunc(input);
+    Console.print(`결과: ${result}`);
   }
 }
 
