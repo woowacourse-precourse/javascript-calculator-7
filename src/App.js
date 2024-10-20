f0a9732ae27fb3589e8baee0ba36d2bc4d18204c
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-10-20 18:42:37 +0900

    feat: Parsing Function(parsingInput)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
+
+  parsingInput(input, delimiter) {
+    const parsingNumbers = input.split(delimiter).map((value) => {
+      var num = Number(value);
+      if (isNaN(num)) {
+        throw new Error("[ERROR]: 숫자가 아닌 값이 포함되어 있습니다.");
+      }
+      return num;
+    });
+    var num = parsingNumbers.reduce((a, b) => a + b);
+    return num;
+  }
+
+
   async run() {
     var result;
     const input = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n "
     );
+
+
   }
 }
 
