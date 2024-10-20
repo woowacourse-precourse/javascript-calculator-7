89e49c8358a413b6ff50c9b1062b6f8946758259
Yoonjeong Ko <jejukyj@naver.com>
2024-10-21 02:00:37 +0900

    feat : 커스텀 구분자 추출하기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
     try {
       // 사용자로부터 문자열 입력받기
-      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      let input = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n'
+      );
 
-      const numbers = this.parseInput(input);
+      let numbers = this.parseInput(input);
 
-      const sum = this.sumNumbers(numbers);
+      let sum = this.sumNumbers(numbers);
 
       Console.print(`결과 : ${sum}`);
-
     } catch (error) {
-      // reject 되는 경우
+      Console.print(`[ERROR] ${error.message}`);
+      throw error;
     }
   }
 
   // 문자열 파싱
-  parseInput(input){
+  parseInput(input) {
     let delimiter = /,|:/; // 정규식을 사용하여 구분자 여러개 설정
 
-    return input.split(delimiter).map((num) => parseInt(num));
+    if (input.startsWith('//') && input.includes('\\n')) {
+      let customDelimiter = input.slice(2, 3);
+      let string = input.slice(5);
+
+      let numbers = string.split(customDelimiter);
+      return numbers;
+    } else {
+      return input.split(delimiter).map((num) => parseInt(num));
+    }
   }
-  // 숫자의 합 계신
-  sumNumbers(numbers){
-    return numbers.reduce((acc, num) => acc + num, 0);
+
+  // 숫자의 합 계산
+  sumNumbers(numbers) {
+    return numbers.reduce((acc, num) => acc + parseInt(num), 0);
   }
 }
 
