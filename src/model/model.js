4336a70a0309cc4472fd65cd59eac239a7a0652d
pjc0179 <pjc0179@gmail.com>
2024-10-20 20:47:28 +0900

    feat:" 문자열나누기, 체크하기, 더하기 부분 구현"

--- src/model/model.js
+++ src/model/model.js
@@ -4,6 +4,24 @@ class Model {
   addDelimiter(stringArray) {
     stringArray.forEach((string) => this.delimiter.add(string));
   }
+
+  calculate(string) {
+    const stringArray = this.splitStringWithDelimiter(string);
+    this.checkStringChangeWithNumber(stringArray);
+    return stringArray.reduce((acc, cur) => +cur + +acc, 0);
+  }
+
+  splitStringWithDelimiter(string) {
+    const regex = new RegExp(`[${[...this.delimiter].join("")}]`);
+
+    return string.split(regex);
+  }
+
+  checkStringChangeWithNumber(stringArray) {
+    if (stringArray.filter((string) => isNaN(+string)).length > 0) {
+      throwError("구분자 외에 다른 문자열은 계산 부분에 들어올 수 없습니다.");
+    }
+  }
 }
 
 export default Model;
