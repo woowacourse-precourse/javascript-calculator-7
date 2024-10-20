601a51f774d0e8671fffcd1445c511023df18e53
yiyujin <yujin94@gmail.com>
2024-10-20 03:26:54 -0400

    add feature for delimiter , and ;

--- src/App.js
+++ src/App.js
@@ -16,7 +16,28 @@ class App {
 
   calculate(input) {
     if (!input) return 0;
+    
+    let numberString = input;
+    let delimiter = ',|:';
+    
+    const numbers = numberString.split(new RegExp(delimiter))
+      .map(num => {
+        const number = Number(num.trim());
+        
+        if (isNaN(number)) {
+          throw new Error("[ERROR] 숫자나 구분자 (, 혹은 :)가 아닌 값이 포함되어 있습니다.");
+        }
+        
+        if (number < 0) {
+          throw new Error("[ERROR] 음수를 입력할 수 없습니다. 양수를 입력해주세요.");
+        }
+        
+        return number;
+      });
+    
+    return numbers.reduce((sum, num) => sum + num, 0);
   }
+
 }
 
 export default App;
