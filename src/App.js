07ef4d3a9a7a9160f0880222056f691f682281b3
Minji Kim <minjikim2000@hotmail.com>
2024-10-21 02:59:28 +0900

    refactor: var에서 let으로 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      var inputNumbers;
+      let inputNumbers;
 
       const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
 
