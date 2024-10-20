8fcd19537ae19c19c1abd81e34ce571986b1b6df
kr-younghoon <sullanta0802@gmail.com>
2024-10-20 20:12:17 +0900

    feat(calculator): 기본 구분자를 사용하여 문자열 덧셈 기능 구현
    쉼표(,)와 콜론(:)을 구분자로 하여 문자열에서 숫자를 추출하고 합산하는 기능을 구현했습니다.
    - `@woowacourse/missionutils`모듈을 사용하여 사용자로부터 문자열을 입력받습니다.
    - 문자열을 쉼표, 콜론으로 분리하여 숫자 배열로 변환합니다.
    - 숫자 배열 안에 있는 숫자를 모두 합산합니다.
    - 합산한 결과들을 출력합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+    async run() {
+        /* 
+            1. 기본 구문자를 사용하여 숫자 더하기 
+            - 쉼표와 콜론을 구분자로 사용하는 문자열에서 숫자를 추출하고 합산합니다.
+            - 예 : "1,2,3" -> 6
+         */
+
+        // 사용자로부터 입력(문자열)을 받음
+        const INPUT = await Console.readLineAsync(
+            '덧셈할 문자열을 입력해주세요.\n'
+        );
+
+        // 문자열을 쉼표와 콜론으로 분리하여 숫자 배열로 변환
+        const NUMBERS = INPUT.split(/[,|:]/).map(Number);
+
+        // 숫자를 합산
+        const RESULT = NUMBERS.reduce(
+            (acc, curr) => acc + curr,
+            0
+        );
+
+        // 결과 출력
+        Console.print(`${RESULT}`);
+    }
 }
 
 export default App;
