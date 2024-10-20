c5cb9e471561dc75dacdae50e97196c1a1647e1d
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-10-20 16:35:58 +0900

    feat: 예외 처리 및 숫자 합 도출
    음수 또는 0이 있는 경우와 커스텀 구분자가 아닌 문자를 인식할 시 에러로 인지하고 그 외에는 각 숫자의 합을 구하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 class App {
+  constructor() {
+    this.isError = false;
+  }
+
   async checkSeparator(str) {
     const re = /\/\/[\D]+\\n/g;   // 커스텀 구분자 찾는 정규식
     const customSeperatorIndex = str.search(re);  // 커스텀 구분자 인덱스 찾기
@@ -20,10 +24,26 @@ class App {
     return numberArray;
   }
 
+  async getSum(strNumArr) {
+    let sum = 0;
+    strNumArr.map((str) => {
+      const strNum = Number(str);
+      if (strNum <= 0 || !isNaN(strNum)) {
+        if (!(str === '')) {
+          this.isError = true;
+          return -1;
+        }
+      }
+      sum += strNum;
+    });
+    return sum;
+  }
+
   async run() {
     const inputString = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요\n');
     const customSeperator = await this.checkSeparator(inputString);
     const separatedNumberArray = await this.separateNumber(inputString, customSeperator);
+    const sum = await this.getSum(separatedNumberArray);
   }
 }
 
