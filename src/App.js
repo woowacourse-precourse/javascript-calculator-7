44f643c0620ef392a3688a5da3e010d6bed7d7f9
sanghee01 <sang6299@naver.com>
2024-10-20 15:33:11 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 Error를 발생하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ class App {
 
       const splitInput = input.split(/[,:]/);
 
+      splitInput.forEach((stringNumber) => {
+        if (!Number(stringNumber)) {
+          throw new Error("[ERROR] 지정되지 않은 구분자를 입력했습니다.");
+        }
+        if (Number(stringNumber) < 0) {
+          throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
+        }
+      });
+
       MissionUtils.Console.print(`결과 : ${splitInput}`);
     } catch (error) {
       MissionUtils.Console.print(`[ERROR] ${error.message}`);
