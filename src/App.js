12febda818c2d99b2ec8b026e208961149ba0dcf
khwww <khw4420@knu.ac.kr>
2024-10-21 05:01:48 +0900

    feat(stringParser): 문자열 구분자 처리 및 파싱 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import inputHandler from "./inputHandler.js";
+import stringParser from "./stringParser.js";
 
 class App {
   constructor() {
     this.userInput = "";
+    this.parsedData = null;
   }
 
   setUserInput(value) {
     this.userInput = value.trim();
   }
 
+  async parseUserInput() {
+    try {
+      this.parsedData = stringParser(this.userInput);
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async run() {
     try {
       const inputValue = await inputHandler();
       this.setUserInput(inputValue);
+      await this.parseUserInput();
     } catch (error) {
       throw new Error(`[ERROR] ${error.message}`);
     }
