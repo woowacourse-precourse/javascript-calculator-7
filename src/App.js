750e9c673496cc55b8460595e253be755dd4c0f6
hihilong <hyelong20630@gmail.com>
2024-10-21 05:53:35 +0900

    오타수정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
     async run() {
         try {
-            const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요:");
+            const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
             const result = this.calculate_sum(input);
-            Console.print("결과: " + result);
-            this.run();
+            Console.print(`결과 : ${result}`);
         } catch (error) {
             Console.print(error.message);
         }
@@ -16,9 +15,6 @@ class App {
         if (input === "") {
             return 0;
         }
-
-        // '\n'을 실제 줄바꿈 문자로 변환
-        input = input.replace(/\\n/g, '\n');
 
         let numbers = [];
         let default_split_text = /[,|:]/;
@@ -38,19 +34,7 @@ class App {
         if (custom_split_text) {
             numbers = input.split(custom_split_text);
         } else {
-            // 기본 구분자만 사용하는지 확인
-            if (input.match(/[^0-9,:\s]/)) {
-                throw new Error("[ERROR] 기본구분자가 아닙니닷");
-            }
             numbers = input.split(default_split_text);
-        }
-
-        // 구분자 사용 여부 확인 
-        if (numbers.length === 1 && !custom_split_text && !input.match(default_split_text)) {
-            throw new Error("[ERROR] 구분자를 포함하여 입력해 주세요.");
-        }
-        else if (numbers.every(n => n.trim() === "") && (input.match(default_split_text) || custom_split_text)) {
-            throw new Error("[ERROR] 숫자를 입력해주세요.");
         }
 
         let sum = 0;
@@ -61,10 +45,10 @@ class App {
 
             const calculated_num = parseInt(num, 10);
             if (isNaN(calculated_num)) {
-                throw new Error("[ERROR] 유효하지 않은 입력: " + num);
+                throw new Error("[ERROR] 유효하지 않은 입력입니다.");
             }
             if (calculated_num < 0) {
-                throw new Error("[ERROR] 음수는 입력할 수 없습니다: " + num);
+                throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
             }
           
             sum += calculated_num;
