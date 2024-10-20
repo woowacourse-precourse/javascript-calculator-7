3e771a041551cac64619d5f125f3a2c9662049bb
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2024-10-21 05:05:33 +0900

    feat#2 : 쉼표, 콜론 구분자를 이용한 덧셈 기능 추가
    쉼표와 콜론을 구분자로 사용하여 문자열 내 숫자들을 더한 값을 반환하는 기능을 구현.
    빈 문자열일 경우 0을 리턴

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
     /**
-     * 프로그램 실행의 시작점
      */
     async run() {
         const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-        const result = this.parseInput(input); // 문자열 파싱
-        MissionUtils.Console.print(`파싱 결과 : ${result}`);
+        const result = this.calculateSum(input); // 숫자 계산
+        MissionUtils.Console.print(`결과 : ${result}`);
     }
 
     /**
-     * 입력받은 문자열을 파싱하여 숫자 문자열 배열 반환
+     * 입력받은 문자열을 파싱하고, 숫자들을 더한 값을 반환
      * @param {string} input - 사용자 입력 문자열
-     * @returns {string[]} - 파싱된 숫자 문자열 배열
+     * @returns {number} - 계산된 합
      */
-    parseInput(input) {
-        if (input === '') return [0]; // 빈 문자열일 경우 숫자 0 배열 반환
-        return input.split(/[,|:]/); // 쉼표(,)와 콜론(:)으로 구분하여 문자열 배열 반환
+    calculateSum(input) {
+        if (input === '') return 0; // 빈 문자열일 경우 0 반환
+        let delimiter = /[,|:]/; // 기본 구분자
+        // 쉼표(,)와 콜론(:)으로 구분하여 숫자 배열로 변환 후 합산
+        const tokens = input.split(/[,|:]/).map(Number);
+        return tokens.reduce((acc, curr) => acc + curr, 0); // 배열의 숫자 합산
     }
 }
 
