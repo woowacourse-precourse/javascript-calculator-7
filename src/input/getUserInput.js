30944c4a6277e0a9556f5e1b3129741a2171f660
Junseo Kim <semiwest2@gmail.com>
2024-10-21 00:54:43 +0900

    refactor: 문자열 입력 처리 기능 default export로 변경
    단일 기능을 export하는 모듈이므로, import 시 더 직관적이고 일관된 사용을 위해 default로 변경.

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -1,6 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-export async function getInputString() {
+export default async function getInputString() {
   const inputString = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
   return inputString;
 }
