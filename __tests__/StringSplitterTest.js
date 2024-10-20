3f14aa60576fb4251cb48ed4831080ad4f5a9011
2wndrhs <2wndrhs@gmail.com>
2024-10-20 17:22:07 +0900

    test: `split()` 메서드 변경으로 인한 테스트 코드 수정

--- __tests__/StringSplitterTest.js
+++ __tests__/StringSplitterTest.js
@@ -1,23 +1,20 @@
 import StringSplitter from '../src/Model/StringSplitter.js';
 
 describe('String Splitter 테스트', () => {
-  let stringSplitter;
-
-  beforeEach(() => {
-    stringSplitter = new StringSplitter();
+  describe('split() 메서드 테스트', () => {
+    test.each([
+      { input: '1,2,3', expectedOutput: ['1', '2', '3'] },
+      { input: '4:5:6', expectedOutput: ['4', '5', '6'] },
+      { input: '7,8:9,10', expectedOutput: ['7', '8', '9', '10'] },
+      { input: '11:12,13', expectedOutput: ['11', '12', '13'] },
+      { input: '14', expectedOutput: ['14'] },
+      { input: '', expectedOutput: [''] },
+    ])(
+      '입력 "$input"에 대해 쉼표와 콜론을 기준으로 문자열을 분리',
+      ({ input, expectedOutput }) => {
+        const stringSplitter = new StringSplitter(input);
+        expect(stringSplitter.split()).toEqual(expectedOutput);
+      },
+    );
   });
-
-  test.each([
-    { input: '1,2,3', expectedOutput: ['1', '2', '3'] },
-    { input: '4:5:6', expectedOutput: ['4', '5', '6'] },
-    { input: '7,8:9,10', expectedOutput: ['7', '8', '9', '10'] },
-    { input: '11:12,13', expectedOutput: ['11', '12', '13'] },
-    { input: '14', expectedOutput: ['14'] },
-    { input: '', expectedOutput: [''] },
-  ])(
-    '입력 "$input"에 대해 쉼표와 콜론을 기준으로 문자열을 분리',
-    ({ input, expectedOutput }) => {
-      expect(stringSplitter.split(input)).toEqual(expectedOutput);
-    },
-  );
 });
