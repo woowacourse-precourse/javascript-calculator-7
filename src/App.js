92b632223c23f8fbbdba719ee668640b9030426a
leesangjae0307 <lee4lee37@naver.com>
2024-10-20 23:06:59 +0900

    message

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App {
   calculateSum(input) {
     if (input === "") return 0;
 
-    let delimiter = [",", ":"];
+    let delimiters = [",", ":"];
 
     // 커스텀 구분자 처리
     if (input.startsWith("//")) {
@@ -25,18 +25,23 @@ class App {
       if (parts.length < 2) {
         throw new Error("커스텀 구분자 형식이 잘못되었습니다.");
       }
-      delimiter.push(parts[0].substring(2)); // "//" 이후 문자
+      delimiters.push(parts[0].substring(2)); // "//" 이후 문자
       input = parts[1]; // 첫 번째 줄 이후의 내용
     }
 
     // 숫자 추출 및 합산
-    const numbers = input.split(new RegExp(delimiter.join("|"))).map((num) => {
-      const parsedNum = parseInt(num.trim(), 10);
-      if (isNaN(parsedNum) || parsedNum < 0) {
-        throw new Error("음수를 입력할 수 없습니다.");
-      }
-      return parsedNum;
-    });
+    const numbers = input
+      .split(new RegExp(`[${delimiters.join("")}]`))
+      .map((num) => {
+        const parsedNum = parseInt(num.trim(), 10);
+        if (isNaN(parsedNum)) {
+          throw new Error("잘못된 숫자 형식이 포함되어 있습니다.");
+        }
+        if (parsedNum < 0) {
+          throw new Error("음수를 입력할 수 없습니다.");
+        }
+        return parsedNum;
+      });
 
     return numbers.reduce((acc, cur) => acc + cur, 0);
   }
