a4f34703e6d601b154bf2090ec43281a2bd85a00
ParkjuYoung <kwn03111@gmail.com>
2024-10-20 17:53:57 +0900

    feat: integrate input handling, validation, and result output
    구현한 로직들을 app.js에서 통합시킴

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { getInput } from './View/GetUserInput.js';
+import { inputNumberValidation } from './Model/InputValidator.js';
+import { sumNumbersFromString } from './Model/NumberSumExtractor.js';
+import { resultPrinter } from './View/SumResultPrinter.js';
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await getInput();
+    inputNumberValidation(userInput);
+    const result = sumNumbersFromString(userInput);
+    resultPrinter(result);
+  }
 }
 
 export default App;
