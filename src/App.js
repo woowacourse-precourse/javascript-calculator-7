0c468c727c6106b574c96675f35e24a043885f47
oko_jin <dswvgw6412@naver.com>
2024-10-20 15:00:49 +0900

    feat(app): 문자열 길이가 0인 경우 "결과 : 0" 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import { errorMessages } from "./constant.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const read = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+
+      if (!read) {
+        Console.print("결과 : 0");
+        return;
+      }
+    } catch (e) {
+      return new Error(errorMessages.unexpectedError);
+    }
+  }
 }
 
 export default App;
