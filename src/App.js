2382acb0477aa2c030b9d8a21da62d9e697866a2
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-10-20 18:35:24 +0900

    :recycle: refactor(App): 검증 로직 분리
    구분자와 숫자를 나눠주는 함수에 같이 있던 검증 로직을 분리해주었다

--- src/App.js
+++ src/App.js
@@ -41,18 +41,21 @@ class App {
     return { separators, mainString };
   }
 
-  // 구분자와 숫자를 분리한다
   static splitNumbers(mainString, separators) {
     const separatorRegex = new RegExp(`[${separators.join('')}]`); // 구분자들을 정규형으로 나타낸다
     const numArray = mainString.split(separatorRegex); // 숫자를 분리한다
 
     return numArray.map(num => {
       const number = parseInt(num, 10);
-      if (Number.isNaN(number) || number < 0) {
-        throw new Error('[ERROR] 잘못된 입력입니다. 양수를 입력해 주세요.');
-      }
+      App.validateNumber(number);
       return number;
     });
+  }
+
+  static validateNumber(number) {
+    if (Number.isNaN(number) || number < 0) {
+      throw new Error('[ERROR] 잘못된 입력입니다. 양수를 입력해 주세요.');
+    }
   }
 
   static sumNumbers(numbers) {
