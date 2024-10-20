3e92616486e0247e2922b1493c99ce6adc4cbb9d
yiyujin <yujin94@gmail.com>
2024-10-20 03:05:43 -0400

    add result print format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+      const result = this.calculate(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      if (!error.message.startsWith("[ERROR]")) {
+        error.message = `[ERROR] ${error.message}`;
+      }
+      throw error;
+    }
+  }
 }
 
 export default App;
