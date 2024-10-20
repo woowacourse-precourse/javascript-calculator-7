dd33e5a79bc733d6a5089acfb17a06ab758c8d70
yiju <yiju1755@naver.com>
2024-10-21 00:30:27 +0900

    feat: 커스텀 구분자 및 숫자 체크를 위한 정규식 정의

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+// 커스텀 구분자 정규식
+const CUSTOM_DELIMITER_REGEX = /^\/\/(.*?)\\n/;  
+// 숫자 체크 정규식
+const NUMBER_CHECK_REGEX = /^[0-9]+$/; 
+
 class App {
   async run() {}
 }
