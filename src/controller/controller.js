ec0d3e8928561c5566eaa976ae72bb0f7cdbd7a8
pjc0179 <pjc0179@gmail.com>
2024-10-20 20:06:21 +0900

    feat:"커스텀 문자 없을경우 에러나던 경우 해결"

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,20 +1,24 @@
 import INPUT from "../view/input.js";
-import reg from "../constants/reg.js";
+import { splitReg } from "../constants/reg.js";
 
 class Controller {
   async start() {
     const userInput = await INPUT.getUserInput();
 
     const [custom, other] = this.findCustomDelimiter(userInput);
+    console.log(this.checkCustomDelimiter(custom.split("")));
   }
 
   findCustomDelimiter(string) {
-    const str = string.match(reg);
+    const str = string.match(splitReg);
+    if (str === null) return ["", str];
 
     return [str[1], str[2]];
   }
 
-  checkCustomDelimiter(string) {}
+  checkCustomDelimiter(stringList) {
+    return stringList.filter((string) => !isNaN(+string)).length === 0;
+  }
 }
 
 export default Controller;
