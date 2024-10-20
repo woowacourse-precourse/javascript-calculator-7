06e39efe6317d2a1fd349c1112f3cedc74e0c548
geunhwa37 <jacika37@naver.com>
2024-10-20 20:59:19 +0900

    feat:  사용자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,4 +2,16 @@ class App {
   async run() {}
 }
 
+// 입력: 사용자에게 덧셈할 문자열을 입력받는 메소드
+getInput() {
+  return new Promise((resolve) => {
+    console.log('덧셈할 문자열을 입력해 주세요.'); 
+    process.stdin.once('data', (data) => {
+      resolve(data.toString().trim()); 
+    });
+  });
+}
+
+
+
 export default App;
