67f7486242ae0478cd8fb739ddb705fd0fc2e5b3
suubiin <127467411+Suubiin@users.noreply.github.com>
2024-10-21 04:51:50 +0900

    feat: throw error for invalid value

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    try {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      
+      if (this.isEmptyString(input)) {
+          Console.print('결과 : 0');
+          return;
+      }
 
-    if (this.isEmptyString(input)) {
-      Console.print('결과 : 0');
-      return;
+      const numbers = this.splitNumbersByDelimiter(input);
+      const result = this.calculateSum(numbers);
+
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
     }
-    
-    const numbers = this.splitNumbersByDelimiter(input);
-    const result = this.calculateSum(numbers);
-
-    Console.print(`결과 : ${result}`);
   }
 
   isEmptyString(input) {
@@ -20,18 +25,25 @@ class App {
   }
 
   splitNumbersByDelimiter(input) {
-    let delimiters;
+    let delimiter;
     
     if (input.startsWith('//') && input.slice(3, 5) === '\\n') {
-      delimiters = input[2];
+      delimiter = input[2];
       input = input.slice(5)
     } else {
-      delimiters = /[,:]/;
+      delimiter = /[,:]/;
     }
+    
+    const stringNumbers = input.split(delimiter);
+    
+    return stringNumbers.map(num => {
+      if (!/^\d+$/.test(num)) {
+        throw new Error('[ERROR] 구분자와 양수 이외의 문자는 입력할 수 없습니다.');
+      }
 
-    const stringNumbers = input.split(delimiters);
-
-    return stringNumbers.map(num => parseInt(num));
+      const parsedNum = parseInt(num);
+      return parsedNum;
+    });
   }
 
   calculateSum(numbers) {
