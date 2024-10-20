7761d4f6c7c2ed22eafd1c773a0411624dbe54b4
ejinn1 <ejinn1@naver.com>
2024-10-20 16:17:40 +0900

    feat: add input logic empty string

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ class App {
       const input = await Console.readLineAsync(
         '덧셈할 문자열을 입력해 주세요.\n',
       );
+
+      let nums = [];
+      if (input.length === 0) {
+        nums.push(0);
+      }
     } catch (error) {}
   }
 }
