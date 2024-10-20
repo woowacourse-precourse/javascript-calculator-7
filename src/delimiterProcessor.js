237fdc0c626cfa881cd6417153afbdbc5cc14b95
khwww <khw4420@knu.ac.kr>
2024-10-21 05:17:07 +0900

    feat(delimiterProcessor): 구분자 처리 로직 확장

--- src/delimiterProcessor.js
+++ src/delimiterProcessor.js
@@ -1,23 +1,26 @@
 export const processCustomDelimiter = (inputValue, processNumbers) => {
   const normalizedInput = inputValue.replace("\\n", "\n");
   const delimiterEndIndex = normalizedInput.indexOf("\n");
+
   if (delimiterEndIndex === -1) {
     throw new Error("구분자가 유효하지 않습니다.");
   }
 
-  const delimiter = normalizedInput.substring(2, delimiterEndIndex);
+  const delimiter = normalizedInput.slice(2, delimiterEndIndex);
   const splitValues = normalizedInput
-    .substring(delimiterEndIndex + 1)
+    .slice(delimiterEndIndex + 1)
     .split(delimiter);
+
   return processNumbers(splitValues);
 };
 
 export const processDefaultDelimiter = (inputValue, processNumbers) => {
-  if (!inputValue.includes(",") && !inputValue.includes(":")) {
+  const delimiters = [",", ":"];
+  const splitNumbers = inputValue.split(new RegExp(`[${delimiters.join("")}]`));
+
+  if (splitNumbers.length === 0) {
     throw new Error("구분자가 없거나 유효하지 않습니다.");
   }
 
-  const splitByComma = inputValue.split(",");
-  const splitNumbers = splitByComma.flatMap((item) => item.split(":"));
   return processNumbers(splitNumbers);
 };
