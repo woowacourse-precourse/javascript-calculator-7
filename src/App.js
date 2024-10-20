86174296301d7163ff7ddeeeb6b24940c94aa243
김아영 <thay123028@naver.com>
2024-10-21 00:26:47 +0900

    feat: 커스텀 구분자 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 class App {
   async run() {
 
-    const input = promt("덧셈할 문자열을 입력해 주세요.\n");
+    let input = promt("덧셈할 문자열을 입력해 주세요.\n");
 
     try {
       const result = this.add(input);
@@ -17,6 +17,12 @@ class App {
     }
 
     let delimiter = /,|:/; // 기본 구분자
+
+    if (input.startsWith("//")) {
+      const parts = input.split("\n");
+      let delimiter = new RegExp(parts[0].slice(2)); // 커스텀 구분자 추출
+      input = parts[1]; // 문자열 부분 추출
+    }
   }
 }
 export default App;
