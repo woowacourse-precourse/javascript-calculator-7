863692a8d0c6ca02981eccbb8466e1519159db78
Woo Jae Shim <shim@Wooui-MacBookAir.local>
2024-10-20 20:58:53 +0900

    chore(calc): avoid calling process.exit() on termination

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  async run() {
-    try {
-      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-      const result = this.calculateSum(input);
-      Console.print(`결과 : ${result}`);
-    } catch (error) {
-      Console.print(`[ERROR] ${error.message}`);
-    }
+  // process.exit()를 사용하지 않고 예외 처리
+async run() {
+  try {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const result = this.calculateSum(input);
+    Console.print(`결과 : ${result}`);
+  } catch (error) {
+    Console.print(`[ERROR] ${error.message}`);
   }
+}
+
 
   calculateSum(input) {
     if (input === '') return 0;
