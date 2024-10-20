8895d9f1c13ddd19f89d3ebff3b8ebc4a31aed78
YongHyunKing <yhkim8246@gmail.com>
2024-10-21 05:48:04 +0900

    feat(Calculator): 문자열을 "\n"로 나누어 커스텀 구분자 입력부와 계산할 숫자 입력부로 분리하기

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Calculator {
   constructor(input) {
     this.input = input;
@@ -9,20 +11,18 @@ class Calculator {
   checkInputValidity() {
     const splitInput = this.input.split("\\n");
 
-    if (splitInput.length > 2) return [false, "[ERROR] 입력 문자열에 \'\\n\'이 2개 이상입니다."];
-
     if (splitInput.length === 2) {
       this.customDelimiterSection = splitInput[0];
       this.numberSection = splitInput[1];
-    } else {
+    } else if (splitInput.length === 1) {
       this.customDelimiterSection = "";
       this.numberSection = splitInput[0];
-    }
+    } else return [false, "[ERROR] 입력 문자열에 \'\\n\'이 2개 이상입니다."];
 
-    console.log(this.customDelimiterSection, this.numberSection);
     if (this.customDelimiterSection !== "") {
       if (!this.customDelimiterSection.startsWith("//")) return [false, "[ERROR] 커스텀 구분자 입력부는 문자열 앞에 위치하여 \"//\"로 시작하고 \"\\n\"으로 끝나야 합니다."];
-    }
+      this.customDelimiter = this.customDelimiterSection.slice(2);
+    } else this.customDelimiter = "";
 
     return [true, "유효한 문자열"];
   }
