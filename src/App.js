bb43726c4bf964cd7ed7620e0fcf2497c68eb9ac
Lee <dltmdgns0508@gmail.com>
2024-10-20 17:48:47 +0900

    refactor(App): 기능별 함수 세분화 및 파일 분리.
    App.js 파일 수정
    - InputSplitter 파일 불러와서 numbers 생성
    - NumberCalculator 파일 불러와서 결과값 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
+import splitInput from './InputSplitter.js';
+import numberCalculator from './NumberCalculator.js';
 
 class App {
   async run() {
@@ -8,50 +10,9 @@ class App {
       throw new Error('[ERROR] 문자열이 입력되지 않았습니다.');
     }
 
-    const createSeparatorPattern = customSeparator => {
-      const separator = [',', ':'];
-      if (customSeparator) {
-        separator.push(
-          customSeparator.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'),
-        );
-      }
-      return new RegExp(separator.join('|'));
-    };
+    const numbers = splitInput(input);
+    const result = numberCalculator(numbers);
 
-    let nums = [];
-
-    if (input.match(/^\/\//)) {
-      const customSeparator = input.match(/^\/\/(.+?)\\n/);
-
-      if (!customSeparator) {
-        if (!input.match(/\\n/)) {
-          throw new Error('[ERROR] \\n이 입력되지 않았습니다.');
-        }
-        throw new Error('[ERROR] 커스텀 구분자가 입력되지 않았습니다.');
-      }
-      if (!isNaN(customSeparator[1])) {
-        throw new Error(
-          '[ERROR] 커스텀 구분자는 문자나 특수문자만 가능합니다.',
-        );
-      }
-
-      nums = input
-        .split('\\n')[1]
-        .split(createSeparatorPattern(customSeparator[1]));
-    } else {
-      nums = input.split(createSeparatorPattern());
-    }
-    const result = nums.reduce((hap, num) => {
-      const number = Number(num);
-      if (isNaN(number) || !number) {
-        throw new Error('[ERROR] 숫자를 입력해 주세요.');
-      }
-      if (number < 0) {
-        throw new Error('[ERROR] 음수가 아닌 양수를 입력해 주세요.');
-      }
-      hap += number;
-      return hap;
-    }, 0);
     Console.print(`결과 : ${result}`);
   }
 }
