15b4348222f28bc8a7dbf776089284d49fbb8146
dmson1218 <dmson1218@naver.com>
2024-10-20 18:48:13 +0900

    refactor(App): 주석 제거

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
 
-// 요구 사항에 기재되지 않은 내용 판단
-// 1. 구분자로 시작하거나 끝나는 경우 -> False
-// 2. 앞에 무의미한 0이 붙는 경우 -> False
-// 3. 커스텀 구분자가 숫자인 경우 -> False
-// 4. 커스텀 구분자가 문자 하나가 아닌 경우 -> False
-
 class App {
     async run() {
         const input = await this.getUserInput();
@@ -59,7 +53,6 @@ class App {
     }
 
     throwError(message) {
-        // 굳이 함수화가 필요할까?
         throw new Error(`[ERROR] ${message}`);
     }
 }
