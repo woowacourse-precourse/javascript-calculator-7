1a3bbf71823e5ef1b40c699a4f1c0cce2e3179c0
H0ngJu <hongju4385@gmail.com>
2024-10-20 14:35:13 +0900

    test: 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -17,6 +17,36 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
+  test("기본 구분자 사용", async () => {
+    const inputs = ["1,2,3,4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 1"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("기본 구분자 mix 사용", async () => {
+    const inputs = ["1,2:3:4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("커스텀 구분자 사용", async () => {
     const inputs = ["//;\\n1"];
     mockQuestions(inputs);
@@ -32,8 +62,50 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("기본&커스텀 구분자 사용", async () => {
+    const inputs = ["//#\n1#2#3#4,5:6,7#8#9"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 45"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("예외 테스트 - 음수", async () => {
     const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트 - 문자열", async () => {
+    const inputs = ["-1,2,3,abc"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트 - 커스텀 양식이 잘못된 경우 : /n", async () => {
+    const inputs = ["//#/n1#2#2#3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트 - 커스텀 양식이 잘못된 경우 : //", async () => {
+    const inputs = ["#\n1#2#2#3"];
     mockQuestions(inputs);
 
     const app = new App();
