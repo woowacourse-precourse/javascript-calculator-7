8c81b9c4eda2dc6f8fa87ca47d7ffb3c3c0114a3
jeonhyeonseon <goodpretty0325@gmail.com>
2024-10-20 18:08:22 +0900

    fix: 코드 출력 수정

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ class App {
   async run() {
 
     // 1. 문자열 입력받기
-    Console.print("덧셈할 문자열을 입력해주세요.");
-    const strInput = await Console.readLineAsync(" ");
+    const strInput = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
     
     // 1-2. 입력된 문자열이 null 또는 빈문자열이라면, 0을 반환한다.
     if (strInput === "" || strInput === null) {
-      console.log(0);
+      Console.print(0);
       return;
     }
 
