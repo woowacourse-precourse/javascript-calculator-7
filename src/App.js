568a7070717614dec69f427f083fa4e5d528d484
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2024-10-21 05:04:08 +0900

    feat#1 : 입력 문자열 파싱 기능 추가
    입력받은 문자열을 쉼표와 콜론을 기준으로 파싱하여 문자열 배열을 반환하는 기능을 구현
    빈 문자열일 경우, 0을 리턴하도록 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-    async run() {}
+    /**
+     * 프로그램 실행의 시작점
+     */
+    async run() {
+        const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+        const result = this.parseInput(input); // 문자열 파싱
+        MissionUtils.Console.print(`파싱 결과 : ${result}`);
+    }
+
+    /**
+     * 입력받은 문자열을 파싱하여 숫자 문자열 배열 반환
+     * @param {string} input - 사용자 입력 문자열
+     * @returns {string[]} - 파싱된 숫자 문자열 배열
+     */
+    parseInput(input) {
+        if (input === '') return [0]; // 빈 문자열일 경우 숫자 0 배열 반환
+        return input.split(/[,|:]/); // 쉼표(,)와 콜론(:)으로 구분하여 문자열 배열 반환
+    }
 }
 
 export default App;
