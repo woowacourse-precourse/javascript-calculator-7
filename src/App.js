7bb40b18c03100a1f823ca7ac58435e6a41c27c4
laire7 <syoungkwak@gmail.com>
2024-10-20 16:52:25 +0900

    feature() 2.outputs the sum of numbers from user input
    BREAKING CHANGE: change processing of user input.
    
    Before:
    
    scope:{
      separate numbers with a single str.split(':') method
    }
    
    After:
    
    scope:{
      separate numbers by iterating through a for loop with a if-else conditional statement
    }
    
    Also change user input str->int

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ class App {
       "덧셈할 문자열을 입력해 주세요.\n",
       (INPUT) => {
         var NUM_LIST = [];
-        NUM_LIST = INPUT.split(":");
-        console.log(`결과: ${NUM_LIST}`);
+        var NUM = "";
+        for (var i = 0; i <= INPUT.length; i++) {
+          if (
+            ((i != 0) & ((INPUT[i] == ",") | (INPUT[i] == ":"))) |
+            (i == INPUT.length)
+          ) {
+            NUM_LIST.push(parseInt(NUM));
+            NUM = "";
+          } else {
+            NUM += INPUT[i];
+          }
+          // console.log(`i:${i}, NUM:${NUM}, NUM_LIST: ${NUM_LIST}`);
+        }
+
+        const SUM = NUM_LIST.reduce((ACCU, CURR, IDX) => {
+          return (ACCU += CURR);
+        }, 0);
+
+        console.log(`결과: ${SUM}`);
+        // console.log(`결과: ${NUM_LIST}`);
       }
     );
   }
