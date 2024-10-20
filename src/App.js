3f60564cf02bbd036c335467967477d3c73dff82
dongmin <dongmin11566@gmail.com>
2024-10-20 20:58:45 +0900

    fix: 커스텀 구분자로 특수 문자 (\b, \t, \a) 사용 불가 문제 수정
    - 커스텀 구분자로 \b, \t, \a 등 특수 문자를 사용할 수 없던 문제를 해결
    - 정규 표현식에서 특수 문자가 올바르게 동작하도록 escape 처리 추가
    - separator 내 특수 문자를 escapeSpecialChars 함수로 변환하여 처리

--- src/App.js
+++ src/App.js
@@ -17,11 +17,13 @@ class App {
     if(inputStr === '') return 0;
 
     const { separator, numStr } = this.customSeparator(inputStr);
-    const numArr = numStr.split(new RegExp(`[${separator}]`)).map( num => {
+    const escapedSeparator = separator.split('|').map(sep => this.escapeSpecialChars(sep)).join('|');
+    const numArr = numStr.split(new RegExp(`[${escapedSeparator}]`)).map( num => {
       const parsedNum = Number(num);
 
       // 숫자가 아닌 값 처리
       if (isNaN(parsedNum)) {
+        console.log(num);
         throw new Error(ERROR_MESSAGES.NOT_A_NUMBER);
       }
 
@@ -35,9 +37,23 @@ class App {
     return numArr.reduce((acc, num) => acc + num, 0);
   }
 
+  // 특수 문자를 이스케이프 처리하는 함수
+  escapeSpecialChars(separator) {
+    return separator
+        .replace(/\\n/g, '\\n')  // 줄바꿈 문자 처리
+        .replace(/\\t/g, '\\t')  // 탭 문자 처리
+        .replace(/\\b/g, '\\\\b') // 백스페이스 문자 처리
+        .replace(/[.*+?^${}()|[\]\\]/g, '\\$&');  // 정규 표현식 특수문자 처리
+  }
+
   customSeparator(inputStr) {
     let defaultSeparator = ',|:';
     if(inputStr.startsWith('//')) {
+      if (inputStr.startsWith('//\\n\\n')) {
+        const numStr = inputStr.slice(5);
+        return { separator: '\\n', numStr };
+      }
+
       const endIndex = inputStr.indexOf('\\n');
 
       // 잘못된 구분자 형식 처리
@@ -46,6 +62,7 @@ class App {
       }
 
       const customSeparator = inputStr.substring(2, endIndex);
+      console.log(customSeparator);
       const numStr = inputStr.slice(endIndex + 2);
       const separator = `${defaultSeparator}|${customSeparator}`;
       return { separator, numStr }
