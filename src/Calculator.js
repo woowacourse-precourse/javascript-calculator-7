b01890754bf6a4b33f71d027bc62bd0169f4e017
hienjoy <wisdom16no@naver.com>
2024-10-21 02:43:13 +0900

    feat(Calculator): 문자열 유효성 검사 추가

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,7 +4,20 @@ class Calculator {
   }
 
   calculate(input) {
+    if (input === '') {
+      return 0;
+    }
+    this.isValidateInput(input);
     return input;
+  }
+
+  //문자열 유효성 검사
+  isValidateInput(text) {
+    const pattern = `^[${[...this.delimiter].join('')}\\d]+$`; //구분자와 숫자로만 이루어진 문자열인지
+    const regex = new RegExp(pattern);
+    if (!regex.test(text)) {
+      throw new Error('구분자와 양수가 아닌 다른 문자가 존재합니다');
+    }
   }
 }
 
