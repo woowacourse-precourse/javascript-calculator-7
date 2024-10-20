923c1b37f446fd4a2ca728353ebbeb50c02bbc4b
immaemi <nam14785@soongsil.ac.kr>
2024-10-20 18:26:38 +0900

    refactor: change delimiters array to member variable

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  async run() {
-    const delimiters = [',', ':'];
+  constructor() {
+    this.delimiters = [',', ':'];
+  }
 
+  async run() {
     let str = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
     let sum = 0;
     let numberTmp = '';
 
     for (let i = 0; i < str.length; i += 1) {
-      if (delimiters.includes(str[i])) {
+      if (this.delimiters.includes(str[i])) {
         sum += Number(numberTmp);
         numberTmp = '';
         continue;
