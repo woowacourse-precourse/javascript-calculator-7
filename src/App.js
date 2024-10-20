8dd343d3287fee36b91b7362932fb4f1b3a3510c
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-21 06:08:54 +0900

    fix: 예외 처리

--- src/App.js
+++ src/App.js
@@ -7,19 +7,58 @@ class App {
     const customSep = /^\/\/\D\\n/;
 
     if (!sep.test(input)) {
+      if (input.length !== 0 && !this.startsAndEndsWithNumber(input)) {
+        return VALIDATION_ERROR;
+      }
+      if (this.hasConsecutiveSeparator(input, /,:/)) {
+        return VALIDATION_ERROR;
+      }
       return VALIDATION_DEFAULT;
     } else if (customSep.test(input)) {
-      const pattern = new RegExp(`[^0-9,:${input[2]}]`);
-      if (!pattern.test(input.slice(5))) {
-        return VALIDATION_CUSTOM;
+      if (input.length > 5 && !this.startsAndEndsWithNumber(input.slice(5))) {
+        return VALIDATION_ERROR;
       }
+      if (this.hasConsecutiveSeparator(input.slice(5), /,:/)) {
+        return VALIDATION_ERROR;
+      }
+      if (input[2] !== '\\') {
+        const pattern = new RegExp(`[^0-9,:${input[2]}]`);
+        if (pattern.test(input.slice(5)) || this.hasConsecutiveSeparator(input.slice(5), input[2])) {
+          return VALIDATION_ERROR;
+        }
+      } else {
+        const pattern = new RegExp(/[^0-9,:\\]/);
+        if (pattern.test(input.slice(5)) || this.hasConsecutiveSeparator(input.slice(5), '\\\\')) {
+          return VALIDATION_ERROR;
+        }
+      }
+      return VALIDATION_CUSTOM;
     }
-
     return VALIDATION_ERROR;
+  }
+
+  startsAndEndsWithNumber(input) {
+    if (isNaN(input[0]) || isNaN(input[input.length - 1])) {
+      return false;
+    }
+    return true;
+  }
+
+  hasConsecutiveSeparator(input, sep) {
+    const pattern = new RegExp(`[${sep}]{2,}`);
+    return pattern.test(input);
   }
 
   convertToNumberArray(input, sep) {
     return input.split(sep).map(Number);
+  }
+
+  calculateSum(arr) {
+    let sum = 0;
+    arr.forEach((num) => {
+      sum += num;
+    });
+    return sum;
   }
 
   async run() {
@@ -29,14 +68,15 @@ class App {
     let sum = 0;
     if (validationResult === VALIDATION_DEFAULT) {
       const numArr = this.convertToNumberArray(input, /[,:]/);
-      numArr.forEach((num) => {
-        sum += num;
-      });
+      sum = this.calculateSum(numArr);
     } else if (validationResult === VALIDATION_CUSTOM) {
-      const numArr = this.convertToNumberArray(input.slice(5), input[2]);
-      numArr.forEach((num) => {
-        sum += num;
-      });
+      if (input[2] !== '\\') {
+        const numArr = this.convertToNumberArray(input.slice(5), new RegExp(`[,:${input[2]}]`));
+        sum = this.calculateSum(numArr);
+      } else {
+        const numArr = this.convertToNumberArray(input.slice(5), new RegExp(/[,:\\]/));
+        sum = this.calculateSum(numArr);
+      }
     } else {
       throw new Error('[ERROR]: 잘못된 값을 입력했습니다');
     }
