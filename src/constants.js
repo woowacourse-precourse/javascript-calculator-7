a68cd894fa9f4cfd248d7c7c5e2119ba55395665
mywhzy <cmj2393a@naver.com>
2024-10-21 01:57:53 +0900

    faet: 양수 아닌 수 입력 시 에러 메세지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
 export const DEFAULT_SEPARATORS = [',', ':'];
 export const CUSTOM_DESIGNATORS = { start: '//', end: '\\n' };
 export const CUSTOM_SEPARATOR_REGEX = /(?<=\/\/).+(?=\\n)/;
+
+export const ERROR_MESSAGE = {
+  positiveNum: '[ERROR] 문자열 덧셈은 1이상의 양수부터 가능합니다.',
+};
