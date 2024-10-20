d51f09497726a9b042f9c44a60920e91b1ec430b
j0n0m2 <dbsk8223@naver.com>
2024-10-20 23:22:17 +0900

    '//' 뒤에 '\\n'가 없는 경우 [ERROR]를 띄운다

--- src/App.js
+++ src/App.js
@@ -12,10 +12,17 @@ class App {
     let DELIMITER = /,|:/;
     let NUMBERSINPUT = INPUT
     // 커스텀 구분자를 사용한다.
+    // '//' 뒤에 '\\n'가 없는 경우
     if (INPUT.startsWith('//')) {
-      const CUSTOM = INPUT.substring(2,INPUT.indexOf('\\n'));
-      DELIMITER = new RegExp(`[${CUSTOM},:]`)
-      NUMBERSINPUT = INPUT.substring(INPUT.indexOf('\\n') + 2 );
+      const NEWLINE_INDEX = INPUT.indexOf('\\n');
+      if (NEWLINE_INDEX === -1) {
+        throw new Error("[ERROR]")
+      } else {
+        const CUSTOM = INPUT.substring(2,INPUT.indexOf('\\n'));
+        DELIMITER = new RegExp(`[${CUSTOM},:]`)
+        NUMBERSINPUT = INPUT.substring(INPUT.indexOf('\\n') + 2 );
+      }
+
     }
     const NUMBERS = NUMBERSINPUT.split(DELIMITER).map(Number);
     // 분리한 숫자의 합을 반환한다
@@ -25,7 +32,8 @@ class App {
     //음수를 입력하는 경우
     //'//'와 '\\n'사이가 공백일 경우
     const NEGATIVES = NUMBERS.filter(num => num < 0);
-    if (NEGATIVES.length > 0 || INPUT.indexOf('\\n') === 3 ) {
+
+    if (NEGATIVES.length > 0 || INPUT.indexOf('\\n') === 2) {
       throw new Error ("[ERROR]")
     } else {
       Console.print(`결과 : ${RESULT}`);
