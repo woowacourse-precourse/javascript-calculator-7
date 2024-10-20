27bdb2295cd8472c22e5f8b7a49b05df9a7ae02c
Minje Sung <minje813@dgu.ac.kr>
2024-10-20 20:23:37 +0900

    fix(data): 입력/출력 안내문구 contants로 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 import processInput from "./utils/processInput.js";
+import { INPUT_NOTICE, OUTPUT_NOTICE } from "./data/constants.js";
 
 class App {
   async run() {
     const UserInput = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
+      INPUT_NOTICE
     );
     const result = processInput(UserInput);
-    Console.print(`결과 : ${result}`);
+    Console.print(`${OUTPUT_NOTICE}${result}`);
   }
 }
 
--- src/data/constants.js
+++ src/data/constants.js
@@ -5,3 +5,5 @@ export const IS_WRONG_SEPERATOR = /^\d+(.\d+)+$/;
 export const IS_WRONG_NUMBERS = /^\d+(.+)+$/;
 export const START_LETTER = "//";
 export const END_LETTER = "\\n";
+export const INPUT_NOTICE = "덧셈할 문자열을 입력해 주세요.\n";
+export const OUTPUT_NOTICE = "결과 : ";
