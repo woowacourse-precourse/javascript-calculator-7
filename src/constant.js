05acdb600b9dd8bc8388a749d5a5318b91703734
oko_jin <dswvgw6412@naver.com>
2024-10-20 19:53:49 +0900

    feat(const): 커스텀 구분자를 인식하는 정규식 변경
    커스텀 구분자를 문자 1개 이상 인식하도록 변경

--- src/constant.js
+++ src/constant.js
@@ -1,8 +1,8 @@
 export const errorMessages = {
-  unexpectedError: '[ERROR]: 예상치 못한 에러가 발생했습니다.',
+  unexpectedError: '예상치 못한 에러가 발생했습니다.',
 };
 
 export const Regex = {
   customDelimiter: /^\/\/(.)+\\n/,
-  valid: /^\/\/(.)\n(?:\d+\1)*\d+$|^(?:\d+[,:])*\d+$/,
+  valid: /^\/\/(.)+\\n(?:\d+\1)*\d+$|^(?:\d+[,:])*\d+$/,
 };
