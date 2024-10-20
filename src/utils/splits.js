36da43589af0d1189db6d4fa177de7ee1cbaa062
ekkk1126 <cabi0203@naver.com>
2024-10-21 02:55:07 +0900

    feat: 입력값을 구분자와 숫자로 분리

---
+++ src/utils/splits.js
@@ -0,0 +1,31 @@
+import Errors from '../error/errors.js';
+
+/**
+ * 사용자 입력 문자열을 지정된 구분자로 나누고,
+ * 오류를 검토한 후, 오류가 없을 경우 숫자 배열을 반환하는 함수입니다.
+ */
+
+function Splits(input) {
+    let seperator = [',', ':'];
+
+    let newLineIdx = input.lastIndexOf('\\n');
+    let customs = newLineIdx !== -1 ? input.slice(2, newLineIdx) : null;
+    if (customs) {
+        seperator.push(customs);
+    }
+
+    // 문자열 구분자 기준으로 나누기
+    let inputNums = newLineIdx !== -1 ? input.slice(newLineIdx + 2) : input;
+    const regExp = new RegExp(
+        seperator.map((s) => s.replace(/([.*+?^${}()|\[\]\\])/g, '\\$1')).join('|'),
+        'g'
+    );
+    let numsArr = inputNums.split(regExp);
+
+    // 오류 검토
+    const validator = new Errors(input, customs, numsArr);
+    validator.run();
+
+    return numsArr.map(Number);
+}
+export default Splits;
