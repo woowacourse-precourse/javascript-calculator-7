664872248f2bebe83065f16860c6c0b377c7331e
spoyodevelop <kjy9505@gmail.com>
2024-10-20 17:00:42 +0900

    fix: fix more more testing

--- src/App.js
+++ src/App.js
@@ -7,13 +7,48 @@ import {
   SYSTEM_MESSAGES,
 } from './Constraints/Constraints.js';
 
-import {
-  validateCustomInput,
-  validateCustomInputFormat,
-  validateNormalInput,
-} from './Validator/validator.js';
 import sumAllNumbers from './Util/sumAllNumbers.js';
-import { escapeRegExp } from './Util/regex.js';
+const numberPattern = '\\d+(\\.\\d+)?';
+const specialCharsPattern = /[.*+?^${}()|[\]\\]/g;
+
+function escapeRegExp(string) {
+  // 정규식에 사용되는 모든 예약어들을 escape 처리한다. .*[]\ 등...
+  // 원본 문자를 남기고 ($&), 그 문자에 \를 더 붙인다.
+  // "Hello. How+are|you?" =>
+  // "Hello\. How\+are\|you\?"
+  return string.replace(specialCharsPattern, '\\$&');
+}
+
+function buildNormalInputValidationRegex() {
+  return new RegExp(`^${numberPattern}$`);
+}
+
+function buildCustomInputValidationRegex(escapedDelimiter) {
+  // escapedDelimiter를 포함하여 동적으로 정규식을 생성
+  return new RegExp(`^${numberPattern}(${escapedDelimiter}${numberPattern})*$`);
+}
+function validateCustomInputFormat(input) {
+  const isInvalidFormat = !input.startsWith('//') || !input.includes('\\n');
+  if (isInvalidFormat) {
+    throw new Error(`${ERROR_PREFIX}${ERROR_MESSAGES.INVALID_INPUT_FORMAT}`);
+  }
+}
+
+function validateCustomInput(inputString, escapedDelimiter) {
+  if (inputString === '') return;
+  const validationRegex = buildCustomInputValidationRegex(escapedDelimiter);
+  if (!validationRegex.test(inputString)) {
+    throw new Error(`${ERROR_PREFIX}${ERROR_MESSAGES.INVALID_CUSTOM_INPUT}`);
+  }
+}
+
+function validateNormalInput(splitValues) {
+  const validationRegex = buildNormalInputValidationRegex();
+  if (splitValues.some(value => !validationRegex.test(value))) {
+    throw new Error(`${ERROR_PREFIX}${ERROR_MESSAGES.INVALID_NUMBER_INPUT}`);
+  }
+}
+
 function parseCustomInput(input) {
   // 입력이 //로 시작하고 \n을 포함하는지 확인한다.
   validateCustomInputFormat(input);
