d2dd9c209c55d30507e811ff3fd4681439db8c37
ShinjungOh <tlswjd9260@gmail.com>
2024-10-21 02:10:40 +0900

    feat: 예외 케이스 처리하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
+import {ERROR_MESSAGES} from "./errorMessages.js";
 
 class App {
     constructor() {
@@ -6,22 +7,35 @@ class App {
     }
 
     async run() {
-        const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-        let inputString = input.toString().trim();
+        const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+        let inputString = input.toString();
 
+        if (inputString === '') {
+            return MissionUtils.Console.print(`결과 : 0`);
+        }
+
+        this.validate(inputString);
         const {value, separators} = this.parseInput(inputString);
         const result = this.calculate(value, separators);
+
         return MissionUtils.Console.print(`결과 : ${result}`);
     }
 
     parseInput(input) {
         if (input[0] === '/' && input[1] === '/') {
-            const searchLastWord = input.indexOf('\\n');
-            if (searchLastWord > 0) {
-                const customSeparator = input.slice(2, searchLastWord);
+            const searchLastWordIndex = input.indexOf('\\n');
+            const customSeparator = input.slice(2, searchLastWordIndex).trim();
+
+            if (customSeparator.length !== 1) {
+                throw Error(ERROR_MESSAGES.INVALID_CUSTOM_SEPARATOR);
+            }
+            if (this.isDigit(customSeparator)) {
+                throw Error(ERROR_MESSAGES.INVALID_CUSTOM_SEPARATOR);
+            }
+            if (searchLastWordIndex > 0) {
                 const newSeparator = [...this.separators, customSeparator];
                 return {
-                    value: input.substring(searchLastWord + 2),
+                    value: input.substring(searchLastWordIndex + 2),
                     separators: newSeparator,
                 }
             }
@@ -33,10 +47,6 @@ class App {
     }
 
     calculate(value, separators) {
-        if (value === '') {
-            return 0;
-        }
-
         const valueArray = [...value];
         let currentNum = '';
         let isPrevSeparator = true;
@@ -44,7 +54,7 @@ class App {
         const numArray = [];
 
         valueArray.forEach((char, index) => {
-            if (!(isNaN(char))) {
+            if (this.isDigit(char)) {
                 isPrevSeparator = false;
                 currentNum += char;
                 numArrayIndex++;
@@ -53,17 +63,36 @@ class App {
                 }
             }
             if (separators.includes(char)) {
+                if (isPrevSeparator) {
+                    throw Error(ERROR_MESSAGES.INVALID_SEPARATOR_USAGE);
+                }
                 numArray.push(currentNum);
                 currentNum = '';
                 isPrevSeparator = true;
             } else if (!separators.includes(char) && isNaN(char)) {
-                throw Error('[ERROR] 올바르지 않은 연산자를 사용했습니다.');
+                throw Error(ERROR_MESSAGES.INVALID_SEPARATOR_USAGE);
             }
         });
 
         return numArray
             .map((str) => parseInt(str, 10))
             .reduce((acc, curr) => acc + curr, 0);
+    }
+
+    validate(value) {
+        if (value.length < 3) {
+            throw Error(ERROR_MESSAGES.INVALID_EXPRESSION_RULE);
+        }
+        if (!isNaN(value)) { // 입력값이 양수만으로 구성된 경우
+            throw Error(ERROR_MESSAGES.INVALID_EXPRESSION_RULE);
+        }
+        if (!this.isDigit(value.at(-1))) { // 마지막 문자가 양수가 아닌 경우
+            throw Error(ERROR_MESSAGES.INVALID_EXPRESSION_RULE);
+        }
+    }
+
+    isDigit(value) {
+        return value >= '0' && value <= '9'
     }
 }
 
---
+++ src/errorMessages.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+    INVALID_EXPRESSION_RULE: '[ERROR] 연산 규칙이 잘못되었습니다.',
+    INVALID_CUSTOM_SEPARATOR: '[ERROR] 커스텀 구분자의 지정값이 올바르지 않습니다.',
+    INVALID_SEPARATOR_USAGE: '[ERROR] 구분자의 사용이 잘못되었습니다.',
+}
