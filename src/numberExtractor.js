72b5f8a8aa3afbcf29f47ecf1ebdde7cc664d3fa
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-10-20 18:51:04 +0900

    feat: 숫자를 추출하는 기능 구현
    - 입력받은 값에서 숫자를 추출하는 extractNumbers함수 구현

---
+++ src/numberExtractor.js
@@ -0,0 +1,22 @@
+import { CUSTOM_DELIMITER, DEFAULT_DELIMITER } from './constants.js';
+import isValidateNumbers from './validation.js';
+
+// 숫자를 추출하는 함수
+function extractNumbers(input) {
+  if (!input) return [0];
+
+  const customDelimiterMatch = input.match(CUSTOM_DELIMITER);
+  let numbers;
+
+  if (customDelimiterMatch) {
+    const customDelimiter = new RegExp(customDelimiterMatch[1], 'g');
+    numbers = input.split(customDelimiterMatch[0])[1].split(customDelimiter);
+  } else {
+    numbers = input.split(DEFAULT_DELIMITER);
+  }
+
+  isValidateNumbers(numbers); // 유효성 검사
+  return numbers.map(Number);
+}
+
+export default extractNumbers;
