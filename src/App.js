e84ac40ed5c07f6a93c9d08c160dd5561ee569ca
H0ngJu <hongju4385@gmail.com>
2024-10-20 10:47:26 +0900

    feat: 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+
+    if (!input) {
+      Console.print("결과 : 0");
+      return;
+    }
+  }
 }
 
 export default App;
