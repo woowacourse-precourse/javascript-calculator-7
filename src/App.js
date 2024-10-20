f75e2f0bfa466f181e867d6c3087b06fa6cfd54c
sanghee01 <sang6299@naver.com>
2024-10-20 15:32:17 +0900

    feat: 기본 제공되는 구분자로 나누어 배열에 저장하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      let input = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+
+      const splitInput = input.split(/[,:]/);
+
+      MissionUtils.Console.print(`결과 : ${splitInput}`);
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR] ${error.message}`);
+      throw error;
+    }
+  }
 }
 
 export default App;
