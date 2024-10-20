1c70e8d85ee733429de7b912166b7e3e2ffce20e
moonseonghui <msh9980@naver.com>
2024-10-20 19:42:02 +0900

    feat: “//” “\n”안에있는  커스텀 구분자 추출하기
    - // 와 \n 사이의 문자열만 slice로 추출

--- src/functions/getCustomDelimiter.js
+++ src/functions/getCustomDelimiter.js
@@ -1,3 +1,7 @@
 const isValidDelimiterFormat = function isValidDelimiterFormatFunc(input) {
   return input.startsWith('//') && input.indexOf('\\n') !== -1;
 };
+
+const extractDelimiter = function extractDelimiterFunc(input) {
+  return input.slice(2, input.indexOf('\\n') - 1);
+};
