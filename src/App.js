fbee89ffc5f27abb606b2f1b9a7c93531ffd74f1
soulchicken <soulfever01@naver.com>
2024-10-20 17:53:25 +0900

    feat: 결과 출력 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   input = "";
+  result = 0;
 
   async run() {
     this.start();
+    this.resultPrint();
   }
   
   async start() {
     this.input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+  }
+
+  async resultPrint() {
+    Console.print(`결과 : ${this.result}`);
   }
 }
 
