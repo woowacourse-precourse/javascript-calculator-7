885258e7873c1d1f825ad344126bca61a779da37
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-20 17:42:54 +0900

    feat : 입력받은 문자열 중 숫자가 있는지 검사

--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,8 @@
 export const inputValidation = (content) => {
   const IS_EMPTY = content.length === 0;
+  const IS_NOT_INCLUDE_NUMBER = content.split('').every((char) => isNaN(char));
 
   if (IS_EMPTY) throw new Error('[ERROR] 입력하신 문자열이 없습니다.');
+  if (IS_NOT_INCLUDE_NUMBER)
+    throw new Error('[ERROR] 입력하신 문자열 중 숫자가 없습니다.');
 };
