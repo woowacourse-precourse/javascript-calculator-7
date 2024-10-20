8c84debe03faab1941443050f9ed106b8641e918
ongheong <pudding4271@gmail.com>
2024-10-21 02:33:00 +0900

    🧪 Feat(App): add mission module demo on error testing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,32 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+    
+    let delimiter = new RegExp(`[,:]`);
+    const customDelimiter = /\/\/(.)\\n/;
+    let parseTarget = input;
+
+    if (input.match(customDelimiter)){
+      const matched = input.match(customDelimiter);
+      const newDelimiter = matched[1];
+      delimiter = new RegExp(`[${newDelimiter},:]`);
+      parseTarget = input.slice(5);
+    }
     //문자열에서 정규표현식으로 구분자 찾아 숫자 배열로 파싱
-    const parsedInput = input.trim().split(/,|:/g).map(Number);
+    const parsedList = parseTarget.trim().split(delimiter).map(Number);
     //숫자 배열 돌면서 값 더하기
-    const result = parsedInput.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    const result = parsedList.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    try {
+      if (isNaN(result) || parsedList.some((prev) => prev < 0) ) {
+        throw new Error("[ERROR]")
+      }
+      Console.print("결과 : " + result);
+    } catch (error) {
+      throw error;
+    }
     // 결과 출력
-    Console.print("계산 결과: " + result);
+    
   }
 }
 
