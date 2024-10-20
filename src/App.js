aa5d4fc68b5db52194e59395463975d4e3ae7559
HW-K <rlaql99@gmail.com>
2024-10-20 17:39:47 +0900

    문자열 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+
+  async run() {
+    // 문자열 입력
+    Console.print("덧셈할 문자열을 입력해 주세요."); 
+    let Input = await Console.readLineAsync("");
+
+    
+  }
 }
 
 export default App;
