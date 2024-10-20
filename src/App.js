e0f983dd894b1aba72b939ea6c4a1ea12290a9a0
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2024-10-20 20:55:54 +0900

    ✨feat: 사용자로부터 계산을 위한 문자열 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const userInputValue = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n',
+    );
+
+    const defaultDelimiter = /,|:/;
+    const separatedValue = userInputValue.split(defaultDelimiter);
+    const convertedNumbers = separatedValue.map(Number);
+  }
 }
 
 export default App;
