0d9423c50386b683af5e97e9805096baf56753e1
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-10-20 16:34:13 +0900

    feat: 구분자로 숫자 분리
    기존 구분자 및 커스텀 구분자를 통해 숫자를 분리하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ class App {
     }
     return str[customSeperatorIndex + 2]; // 커스텀 구분자 반환
   }
-  
+
+  async separateNumber(str, customSeperator) {
+    const refactoredStr = str.replace('\/\/', '').replace('\\n', '');  // 문자열에서 '//', '\n' 제거
+
+    const existingSeperators = [',', ':'];
+    existingSeperators.push(customSeperator);
+
+    const regex = new RegExp(existingSeperators.join('|'), 'g');  // 숫자로 나눌 정규식 생성
+
+    const numberArray = refactoredStr.split(regex);
+    return numberArray;
+  }
+
   async run() {
     const inputString = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요\n');
     const customSeperator = await this.checkSeparator(inputString);
+    const separatedNumberArray = await this.separateNumber(inputString, customSeperator);
   }
 }
 
