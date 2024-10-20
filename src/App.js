266dfda151dd99133af6ff4448db524ab7ca7cb1
hidanbeing <nd6162bv@gmail.com>
2024-10-20 16:10:17 +0900

    feat: 입력값이 커스텀 구분자인지, 기본 구분자인지 구별

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    if (
+      input.length >= 5 &&
+      input.startsWith("//") &&
+      input[3] === "\\" &&
+      input[4] === "n"
+    ) {
+      // 커스텀 구분자 일 경우
+    } else {
+      // 기본 구분자일 경우
+    }
   }
 }
 
