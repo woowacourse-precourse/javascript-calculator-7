16d8291da8aac4a148fcd6fc6582c4ce8f522fdb
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-10-20 11:33:59 +0900

    feat: 구분자 제외 후 인풋에 대해 문자,숫자 구별하여 합 구하기

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App {
     const seperators = [",", ":"];
     let inputString = input;
 
+    //정규표현식을 통한 구분자 추출
     while (true) {
       const regexpArray = regexp.exec(inputString);
       if (regexpArray === null) {
@@ -18,6 +19,23 @@ class App {
       seperators.push(regexpArray[0][2]);
       inputString = inputString.replace(regexp, "");
     }
+
+    // 구분자를 제외한 인풋스트링에 대해 문자와 숫자 판별
+    let currentValue = "";
+    let sum = 0;
+    const inputArray = inputString.split("");
+    inputArray.forEach((element) => {
+      if (!!parseInt(element)) {
+        currentValue += element;
+      } else {
+        sum += parseInt(currentValue);
+        currentValue = "";
+      }
+    });
+    if (!!parseInt(currentValue)) {
+      sum += parseInt(currentValue);
+    }
+    Console.print(sum);
   }
 }
 
