ca6e5ad73a26f01476811f76519a673d97bf8558
Eunmi Lee <eunoia0235@gmail.com>
2024-10-20 23:17:09 +0900

    feat(app): modify the result part

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync(
-      `구분자와 양수로 된 문자열을 입력해주세요.`
+      `덧셈할 문자열을 입력해 주세요.\n`
     );
     try {
-      const result = sumNumFromString(input);
-      Console.print(result);
+      const result = this.sumNumExceptString(input);
+      Console.print(`결과 : ${result}`);
     } catch (error) {
       Console.print(`[ERROR] ${error.message}`);
     }
