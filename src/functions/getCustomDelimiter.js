f07e207d22698e732a1f01e820f8583c28a553d7
moonseonghui <msh9980@naver.com>
2024-10-20 19:55:22 +0900

    feat(vaildDelimiter): 정규식 패턴을 통한 커스텀 구문자 유효성 확인
    - 유효한 특수 문자는 / ? . , ; : | * ~ ! ^ - _ + < > @ # % & \ = ' "

--- src/functions/getCustomDelimiter.js
+++ src/functions/getCustomDelimiter.js
@@ -1,7 +1,18 @@
+import handleError from '../utils/handleError.js';
+
 const isValidDelimiterFormat = function isValidDelimiterFormatFunc(input) {
   return input.startsWith('//') && input.indexOf('\\n') !== -1;
 };
 
 const extractDelimiter = function extractDelimiterFunc(input) {
   return input.slice(2, input.indexOf('\\n') - 1);
+};
+
+const validateDelimiter = function validateDelimiterFunc(delimiter) {
+  const alphaSpecialCharRegex = /^[a-zA-Z/?.,;:|*~!^\-_+<>@#%&\\='"]+$/;
+
+  if (!alphaSpecialCharRegex.test(delimiter))
+    handleError(
+      '유효하지 않은 커스텀 구문자: 알파벳 대소문자 또는 허용된 특수 문자만 가능합니다.',
+    );
 };
