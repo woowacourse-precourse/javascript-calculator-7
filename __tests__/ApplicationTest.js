a809ebd1ac2c68a34abd2c3d6609436067621dcf
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-20 18:17:31 +0900

    [test]: 테스트 코드 로직 일부 수정 - 여러 케이스를 순차적으로 테스트 가능하게 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,11 +1,10 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const mockQuestions = (inputs) => {
+const mockQuestions = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
     return Promise.resolve(input);
   });
 };
@@ -18,26 +17,29 @@ const getLogSpy = () => {
 
 describe('문자열 계산기', () => {
   test('커스텀 구분자 사용', async () => {
-    const inputs = ['//;\\n1'];
-    mockQuestions(inputs);
-
+    const inputs = ['//;\\n1', '//<\\n1<2'];
+    const outputs = ['결과 : 1', '결과 : 3'];
     const logSpy = getLogSpy();
-    const outputs = ['결과 : 1'];
 
-    const app = new App();
-    await app.run();
+    inputs.forEach(async (input) => {
+      const output = outputs.shift();
+      mockQuestions(input);
 
-    outputs.forEach((output) => {
+      const app = new App();
+      await app.run();
+
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
 
   test('예외 테스트', async () => {
     const inputs = ['-1,2,3'];
-    mockQuestions(inputs);
 
-    const app = new App();
+    inputs.forEach(async (input) => {
+      mockQuestions(input);
 
-    await expect(app.run()).rejects.toThrow('[ERROR]');
+      const app = new App();
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
   });
 });
