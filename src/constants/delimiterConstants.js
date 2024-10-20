89a501f5981d89ef55fc301ca6fd4a72eb11bfb6
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-20 23:06:46 +0900

    fix: custom delimiter

--- src/constants/delimiterConstants.js
+++ src/constants/delimiterConstants.js
@@ -1,5 +1,5 @@
 export const EMPTY_INPUT_DEFAULT = [0];
 export const DEFAULT_DELIMITERS = /[,:]/;
-export const CUSTOM_DELIMITER_REGEX = /^\/\/([^\n]*)\n(.*)$/;
+export const CUSTOM_DELIMITER_REGEX = /^\/\/([^\\n]*)\\n(.*)$/;
 export const ESCAPE_REGEX_PATTERN = /[.*+?^${}()|[\]\\]/g;
-export const ESCAPE_REGEX_REPLACEMENT = "\\";
+export const ESCAPE_REGEX_REPLACEMENT = "\\$&";
--- src/utils/splitNumber.js
+++ src/utils/splitNumber.js
@@ -24,7 +24,7 @@ export const validateCustomDelimiter = (delimiter) => {
 };
 
 const escapeRegExp = (string) => {
-  return string.replace(ESCAPE_REGEX_PATTERN, ESCAPE_REGEX_REPLACEMENT);
+  return string.replace(ESCAPE_REGEX_PATTERN, { ESCAPE_REGEX_REPLACEMENT });
 };
 
 export const handleCustomDelimiter = (match) => {
