fe7259c33cc288bf0038e9216a1dec3575e9dbbf
nayeong lee <lny021102@gmail.com>
2024-10-20 19:09:45 +0900

    feat: 빈 문자열 입력 시 0 반환 기능 추가
    Closes #3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 class App {
-  async run() {}
+  async run() {
+    const input = await this.getInput();
+    const result = this.add(input);
+    this.printResult(result);
+  }
+
+  async getInput() {
+    const { Console } = require('@woowacourse/mission-utils');
+    return new Promise((resolve) => {
+      Console.readLine('덧셈할 문자열을 입력해 주세요.\n', resolve);
+    });
+  }
+
+  add(input) {
+    // 빈 문자열이면 0을 반환
+    if (input === "") {
+      return 0;
+    }
+    // 커스텀구분자, 잘못된입력 시 대응을 구현할 자리
+  }
+
+  printResult(result) {
+    const { Console } = require('@woowacourse/mission-utils');
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
