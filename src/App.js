e09ac12ceb7024ce7f068dfbe45b9ec7c7c79a6a
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-10-20 18:40:42 +0900

    feat: Handling Input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-
-
   async run() {
     var result;
     const input = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n "
     );
-
-
   }
 }
 
