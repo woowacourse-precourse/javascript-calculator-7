fa099714560554e3a954d3a4aa9003cbfeef2c34
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-20 15:28:17 +0900

    feat(App): 문자열 입출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const result = 0;
+
+      Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
