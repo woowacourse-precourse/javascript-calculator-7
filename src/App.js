2c874187a9e52fb84faa3e223619e52659ca8214
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-20 15:43:06 +0900

    feat(App): 구분자 기준 문자열 분리 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-      const result = 0;
-
+      const result = this.addNumbers(input);
       Console.print(`결과 : ${result}`);
+  }
+  addNumbers(input) {
+      const delimiters = [',', ':']; // 기본 구분자 배열
+      const numbers = delimiters.reduce((acc, delimiter) => {
+          return acc.flatMap(str => str.split(delimiter));
+      }, [input]); // 구분자를 기준으로 문자열 분리
+
+      Console.print(numbers); // numbers 출력
+      
+      return numbers;
   }
 }
 
