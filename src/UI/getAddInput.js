882e4aece2d56b724ad701e4a9d274500ff5694a
thumbthing <ilscc0902@gmail.com>
2024-10-20 14:17:13 +0900

    refactor: 불필요한 import 삭제, 에러 전달 방식 변경

--- src/UI/getAddInput.js
+++ src/UI/getAddInput.js
@@ -1,12 +1,11 @@
-import { Console } from "@woowacourse/mission-utils";
-
 async function getAddInput() {
   try {
     let inputText = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     return inputText;
   } catch (error) {
-    let errorMessage = `[ERROR] ${error.message}`
-    Console.print(errorMessage);
+    let errorMessage = '입력중에 오류가 발생했습니다.'
+    const ERROR = new Error(errorMessage);
+    throw ERROR;
   }
 }
 
