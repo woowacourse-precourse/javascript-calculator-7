1f8d75966d15ebae1ae9fcc0d0f6314d90147f91
ejinn1 <ejinn1@naver.com>
2024-10-20 14:41:23 +0900

    chore: airbnb code convention setting

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', { max: 15 }],
+  },
+};
