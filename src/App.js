cd881f3f62cd1a9680cd25799c0987ac4521c876
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-10-21 06:51:36 +0900

    feat: 사용자가 잘못된 값을 입력한 경우
    [ERROR]로 시작하는 메시지와 함께 Error를 발생
    애플리케이션 종료

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
     // 입력값을 구분자와 숫자로 파싱
-    const { delimiter, numbers } = this.parseInput(INPUT);
+    const { delimiter, numbers } = this.parseInput(input);
+    
     // 구분자를 기준으로 숫자를 나누어 배열로 저장
-    const SPLITS = numbers.split(new RegExp(`${delimiter}|,|:`));
-    // Console.print(SPLITS);
+    const splits = this.splitNumbers(numbers, delimiter);
     
     // 배열의 각 요소를 숫자로 변환하여 더하기
     let sum = 0;
-    for (let i = 0; i < SPLITS.length; i++) {
-      sum += parseInt(SPLITS[i]) || 0;
+    for (let i = 0; i < splits.length; i++) {
+      const num = parseInt(splits[i]);
+      // 숫자가 아니거나 음수이면 에러 발생
+      if (isNaN(num)) {
+        throw new Error(`[ERROR] 유효하지 않은 입력값입니다: ${splits[i]}`);
+      }
+      if (num < 0) {
+        throw new Error(`[ERROR] 음수는 허용되지 않습니다: ${num}`);
+      }
+      sum += num;
     }
     Console.print("결과 : " + sum);
   }
 
   parseInput(input) {
+    // 입력값이 없으면 0을 반환
+    if (input.trim() === '') {
+      return { delimiter: '', numbers: '0' };
+    }
+    
     // 입력값이 커스텀 구분자를 가지고 있는지 확인
-    const customDelimiterRegex = /^\/\/(.)\n(.*)/;
+    const customDelimiterRegex = /^\/\/(.)\\n(.*)/;
     const match = input.match(customDelimiterRegex);
 
     // 커스텀 구분자가 있으면 구분자와 숫자를 반환
@@ -30,11 +43,20 @@ class App {
       };
     }
 
-    // 커스텀 구분자가 없으면 빈 문자열과 숫자를 반환
+    // 커스텀 구분자가 없으면 기본 구분자와 숫자를 반환
     return {
-      delimiter: '',
+      delimiter: ',|:',
       numbers: input
     };
+  }
+
+  // 구분자를 기준으로 숫자를 나누어 배열로 저장
+  splitNumbers(numbers, delimiter) {
+    // 구분자가 없으면 그대로 반환
+    if (delimiter === '') {
+      return [numbers];
+    }
+    return numbers.split(new RegExp(delimiter));
   }
 }
 
