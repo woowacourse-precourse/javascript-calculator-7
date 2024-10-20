32a795d434de1de238a07b61dbbaf6ad3f9dc3ae
jin123457 <jin_ox@naver.com>
2024-10-20 16:54:41 +0900

    fix (FindDigitCount) : add condition check for non-numeric strings

--- src/Utils/FindDigitCount.js
+++ src/Utils/FindDigitCount.js
@@ -1,5 +1,6 @@
 const FindDigitCount = (str) => {
   const DIGITS = str.match(/\d+/g);
+  const NOT_DIGITS = str.match(/[^\d+]/g);
 
   if (DIGITS == null) {
     throw new Error(`[ERROR] 숫자가 최소 한 개 이상 필요합니다.`);
@@ -9,7 +10,9 @@ const FindDigitCount = (str) => {
     throw new Error(`[ERROR] 음수는 문자열에 포함될 수 없습니다.`);
   }
 
-  return DIGITS.length;
+  return NOT_DIGITS !== null
+    ? DIGITS.length + NOT_DIGITS.length
+    : DIGITS.length;
 };
 
 export default FindDigitCount;
