0fa9a412e9075459961eb9749a532218cb926241
KimPuro <puro@protonmail.ch>
2024-10-20 17:53:39 +0900

    feat(App): 구분자를 사용하여 숫자를 더하는 기능 추가
    쉼표와 클론을 구분자로 사용하여 숫자를 더하고 이를 출력합니다.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import { Console } from "@woowacourse/mission-utils";
+
 class App {
+  constructor() {
+    this.separator = [',', ':'];
+  }
+
   async run() {
     try {
       const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
       if (this.hasWhitespace(str)) {
         throw new Error("[ERROR] 입력에 공백이 포함되어 있습니다.");
       }
-      console.log(str);
+      const numbers = this.extractNumbers(str);
+      const sum = this.sumNumbers(numbers);
+      Console.print(`결과 : ${sum}`);
     } catch (error) {
-      console.error(error.message);
+      Console.print(error.message);
     }
   }
-
 
   hasWhitespace(str) {
     const pattern = /\s/;
     return pattern.test(str);
+  }
+
+  extractNumbers(str) {
+    const regex = new RegExp(`[${this.separator.join('')}]`);
+    return str.split(regex).map(Number);
+  }
+
+  sumNumbers(numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
   }
 }
 
