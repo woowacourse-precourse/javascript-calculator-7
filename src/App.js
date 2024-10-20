2c7afe03fedfa2499a9b487896b02ffc53234989
KimPuro <puro@protonmail.ch>
2024-10-20 17:26:35 +0900

    feat(App): 입력 공백 처리 추가
    입력에 공백이 존재한다면 오류로 판단합니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
-    const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.")
-    console.log(str);
+    try {
+      const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+      if (this.hasWhitespace(str)) {
+        throw new Error("[ERROR] 입력에 공백이 포함되어 있습니다.");
+      }
+      console.log(str);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+
+  hasWhitespace(str) {
+    const pattern = /\s/;
+    return pattern.test(str);
   }
 }
 
