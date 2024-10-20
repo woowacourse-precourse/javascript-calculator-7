35b8d45c7122dbeba791a6532f5a8b92e98dd4b0
sanghyeonLee <hyeon012366@gmail.com>
2024-10-21 06:19:40 +0900

    refactor : 문자열 전처리 메소드 기존 반복문 및 조건문 사용로직을 정규표현식으로 변경

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import {Console} from "@woowacourse/mission-utils";
 
 class App { //하나의 메소드는 하나의 역할
     sum = 0
-    customSeparator = ""//인스턴스 변수는 최대 2개
+    customSeparator = null//인스턴스 변수는 최대 2개
+
     // 변환이 먼전가? 커스텀 구분이 먼전가
-    async run() { //풀이 결과 반환 담당, 메소드명만 봐도 무슨역할인지 알 수 있게 
+    async run() { //풀이 결과 반환 담당, 메소드명만 봐도 무슨역할인지 알 수 있게
         const input = await this.input()
         const argument = this.isCustom(input) ? this.sliceString(input) : input
         this.calculator(argument)
@@ -44,27 +45,11 @@ class App { //하나의 메소드는 하나의 역할
         throw new Error("[Error]: 에러발생")
     }
 
-    preprocessing(param) { //10이상 수 삽입 : 추후 인덴트 줄이기, 클래스는 하나의 역할만
-        let arr = [...param]
-        let tmpValue = "";
-        param.forEach((element, idx) => { //구분자가 숫자 값이면? 문자 값이라 했으니 문자만?
-                if (!isNaN(element)) {
-                    tmpValue += element
-                }
-                if (isNaN(element) && element !== this.customSeparator && element !== ":" && element !== ",") {
-                    throw new Error("[Error]: 에러발생")
-                }
-                if (isNaN(param[idx + 1]) || idx === param.length - 1) {
-                    arr.push(tmpValue)
-                    tmpValue = ""
-                }
-            }
-        )
-        if (arr[arr.length - 1] === "") {
-            throw new Error("[Error]: 에러발생")
-        }
-        console.log(arr)
-        return arr
+    preprocessing(param) {
+        const argument = this.customSeparator || /[:,]/g
+        const splitArray = param.split(argument)
+        return splitArray.map((elem) =>
+            isNaN(elem) ? new Error("[Error]: 에러발생") : Number(elem))
     }
 
     resultOutput() {
@@ -72,7 +57,7 @@ class App { //하나의 메소드는 하나의 역할
     }
 
     calculator(param) { //계산 담당
-        const numberArray = this.preprocessing([...param])
+        const numberArray = this.preprocessing(param)
         for (const element of numberArray) {
             this.sum += Number(this.isSeparator(element))
         }
