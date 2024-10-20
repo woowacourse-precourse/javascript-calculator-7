eebe51d23c46b0dd85b738d40aca1f251003e70a
kimgho <kgh67811@naver.com>
2024-10-20 16:51:55 +0900

    feat(app): 계산기 파이프라인을 위한 App 클래스 구현
    - 파이프라인 로더로 계산기를 초기화하기 위한 App 클래스를 생성
    - init 메서드는 비동기로 파이프라인을 로드하고, run 메서드는 사용자 입력을 처리하여 결과를 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import pipelineLoader from "./loader/index.js";
+import Calculator from "./template/Calculator.js";
+import { View } from './view/index.js';
+
 class App {
-  async run() {}
+  async init() {
+    this.pipeline = await pipelineLoader();
+    this.calculator = new Calculator(this.pipeline);
+  }
+
+  async run() {
+    await this.init();
+
+    const command = await View.input()
+    const { result } = await this.calculator.calculate(command);
+    View.printResult(result)
+  }
 }
 
 export default App;
