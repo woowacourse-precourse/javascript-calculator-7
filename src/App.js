6f3a5df15ca728712c06e194b20080b659ee0242
heeyoung123 <heeyoung321@naver.com>
2024-10-20 23:55:13 +0900

    feat(App): 커스텀 구분자를 기준으로 합 반환 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ class App {
         "덧셈할 문자열 입력: "
       );
       let delimiter = /[,:]/;
-      const sum = input.split(delimiter).reduce((acc, cur) => {
-        return acc + Number(cur);
-      }, 0);
+      let sum = 0;
+      let customDelimiter;
+      if (input.startsWith("//") && input.slice(3, 5) === "\\n") {
+        customDelimiter = input[2];
+        let numbers = input.substr(5);
+        sum = numbers
+          .split(customDelimiter)
+          .reduce((acc, cur) => acc + Number(cur), 0);
+      } else {
+        sum = input.split(delimiter).reduce((acc, cur) => acc + Number(cur), 0);
+      }
 
       MissionUtils.Console.print(`값: ${sum}`);
     } catch (error) {
