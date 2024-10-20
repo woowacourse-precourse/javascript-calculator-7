b3490daf8a6994a81c08b69b66eec857af2586ef
Hyori <hyoring030@gmail.com>
2024-10-20 21:56:57 +0900

    feat: add ERROR case

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Calculator from './Calculator.js';
 class App {
   async run() {
     try {
-      const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+      const input =
+        await MissionUtils.Console.readLineAsync(
+          '덧셈할 문자열을 입력해 주세요.'
+        );
       const calculator = new Calculator();
       const result = calculator.calculate(input);
       MissionUtils.Console.print(`결과 : ${result}`);
     } catch (error) {
-      MissionUtils.Console.print(error.message);
+      MissionUtils.Console.print(`${error.message}`); 
+      throw error;
     }
   }
 }
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,36 +2,47 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Calculator {
   calculate(string) {
-    try {
-      let answer = 0;
-      let nums;
+    let answer = 0;
+    let nums;
 
-      let custom = string.match(/\/\/(.*?)\\n/);
+    let custom = string.match(/\/\/(.*?)\\n/);
+    const numRegex = new RegExp(/[0-9]/);
 
-      if (custom) {
-        const customDelimiter = custom[1];
-        const splitString = string.split(custom[0]);
+    if (string === '') {
+      return 0;
+    }
 
-        if (splitString.length > 1) {
-          const regex = new RegExp(`${customDelimiter}|,|:`);
-          nums = splitString[1].split(regex);
-        }
+    if (custom) {
+      const customDelimiter = custom[1];
+      const splitString = string.split(custom[0]);
+      if (customDelimiter.length !== 1) {
+        throw new Error('[ERROR]');
+      }
+      if (numRegex.test(customDelimiter)) {
+        throw new Error('[ERROR]');
+      }
+      const regex = new RegExp(`${customDelimiter}|,|:`);
+      nums = splitString[1].split(regex);
+    } else {
+      nums = string.split(/,|:/);
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      
+      let num = parseInt(nums[i], 10);
+      if (num < 0) {
+        throw new Error('[ERROR]');
+      }
+      if (numRegex.test(nums[i])) {
+        answer += num;
       } else {
-        nums = string.split(/,|:/);
+        throw new Error('[ERROR]');
       }
-
-      for (let i = 0; i < nums.length; i++) {
-        let num = parseInt(nums[i], 10);
-        
-        if (!isNaN(num)) {
-          answer += num;
-        }
-      }
-      if (isNaN(answer)) {
-        answer = 0;
-      }
-      return answer;
-    } catch (error) {}
+    }
+    if (isNaN(answer)) {
+      answer = 0;
+    }
+    return answer;
   }
 }
 
