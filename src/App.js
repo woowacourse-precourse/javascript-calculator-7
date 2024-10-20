4c832a657fd5f2abe99fdb846e48c337bae3e6bf
wnsxk2 <wnsxk206@gmail.com>
2024-10-21 01:07:26 +0900

    feat: 문자열 검사 예외 추가
    입력받은 문자열이 숫자와 구분자로 이루어진 문자열이 아니면 Error를 발생시키는 코드를 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import { Console } from '@woowacourse/mission-utils';
 
+// 커스텀 구분자 확인 regex
+const CUSTOM_DELIMITER_REGEX = /^\/\/(\D)\\n/;
+
 class App {
   async run() {
     try {
-       const CUSTOM_DELIMITER_REGEX = /^\/\/(\D)\\n/;
-      
-       let delimiter;
-       let str;
-      const answer = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+      // 변수 선언
+      let delimiter;
+      // 문자열 입력
+      let str = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+      // 커스텀 구분자 확인
+      const isCustomDelimiter = CUSTOM_DELIMITER_REGEX.test(str);
 
-      const match = answer.match(CUSTOM_DELIMITER_REGEX);
-
-      if(match) {
-        str = answer.replace(CUSTOM_DELIMITER_REGEX, '');
+      if (isCustomDelimiter) {
+        const match = str.match(CUSTOM_DELIMITER_REGEX);
         delimiter = match[1];
+        str = str.replace(CUSTOM_DELIMITER_REGEX, '');
       } else {
-        str = answer;
         delimiter = /[,:]/;
       }
 
-      Console.print(`delimiter : =${delimiter}= | str : ${str}`);
-      const sum = str.split(delimiter)
-        .reduce((total, num) => total + Number(num), 0 );
+      // 문자열을 구분자로 구분한 배열 리턴
+      const numberArr = str.split(delimiter);
+      // 배열의 총합을 리턴
+      const sum = numberArr.reduce((total, num) => {
+        const a = Number(num);
+        // 예외 처리
+        if (isNaN(a) || num < 0) {
+          throw new Error('문자열은 양수과 구분자로만 구성되어야합니다.');
+        }
+        return total + a;
+      }, 0);
 
       Console.print(`결과 : ${sum}`);
     } catch (error) {
-      // reject 되는 경우
-      Console.print(`에러 발생: ${error.message}`);
+      throw new Error(`[ERROR] ${error.message}`);
     }
   }
 }
