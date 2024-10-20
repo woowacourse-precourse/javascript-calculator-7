ef4c034611aeffa9bae7ae07d59130b0b68dcf42
laire7 <syoungkwak@gmail.com>
2024-10-20 15:29:51 +0900

    feat(): displays user input
    Complete program setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.readLine(
+      "덧셈할 문자열을 입력해 주세요.\n",
+      (answer) => {
+        console.log(`결과: ${answer}`);
+      }
+    );
+  }
 }
 
 export default App;
