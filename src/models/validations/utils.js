01c4482a8c65faa78b593e6c0d827f5eb711630c
luminox93 <memoji1236@gmail.com>
2024-10-20 22:33:48 +0900

    refactor: utils.js 함수 변경

--- src/models/validations/utils.js
+++
@@ -1,8 +0,0 @@
-export const extractCustomSeperators = (input) => {
-	const customSeperator = input.match(/\/\/(.*)\n/);
-	if (customSeperator) {
-		return customSeperator[1];
-	} else {
-		return '';
-	}
-};
