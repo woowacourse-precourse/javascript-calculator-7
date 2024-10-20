c5c11e5febff18235e6770004dc33c63db2e0f91
songudeum <songudeum@gmail.com>
2024-10-21 02:15:16 +0900

    feat: seperatorParser 객체 구현 완료

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,13 @@
 const INPUT_ERROR = "[ERROR]잘못된 입력입니다.";
+const PLEASE_INPUT_VALUE = "덧셈할 문자열을 입력해 주세요.\n";
+const RESULT = "결과 : ";
+const SEPERATOR_REGEXP = /(?<=^\/\/)(.*?)(?=\\n)/;
+const INPUT_FILTER_REGEXP = /^\/\/.+\\n/;
 
-export { INPUT_ERROR };
+export {
+    INPUT_ERROR,
+    PLEASE_INPUT_VALUE,
+    RESULT,
+    SEPERATOR_REGEXP,
+    INPUT_FILTER_REGEXP,
+};
