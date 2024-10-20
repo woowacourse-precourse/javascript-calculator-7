600d58247b71054f8a3e35cbbb0138cbffbfa41b
Eunmi Lee <eunoia0235@gmail.com>
2024-10-20 15:56:06 +0900

    feat(app): add user input as a result

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      `구분자와 양수로 된 문자열을 입력해주세요.`
+    );
+    try {
+      const result = sumNumFromString(input);
+      Console.print(result);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
