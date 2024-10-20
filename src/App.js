9a930507be1d0c2051c0a76d11d4c7860bd8a70a
j0n0m2 <dbsk8223@naver.com>
2024-10-20 18:10:10 +0900

    빈 문자열은 '0'을 반환한다

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n", (INPUT));
+    // 빈 문자열은 0을 반환한다.
+    if (INPUT === "") {
+      Console.print(`결과 : 0`)
+    } else {
+      Console.print(`결과 : ${INPUT}`)
+    }
+
+  //g
   }
 }
 
