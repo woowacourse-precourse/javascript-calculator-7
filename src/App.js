d33a740a59f80e575b0da06dd13e79aea90f16b0
heeyoung123 <heeyoung321@naver.com>
2024-10-21 00:33:32 +0900

    docs : 오류케이스 세분화

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
       const input = await MissionUtils.Console.readLineAsync(
         "덧셈할 문자열 입력: "
       );
+
       let delimiter = /[,:]/;
       let sum = 0;
       let customDelimiter;
@@ -18,7 +19,6 @@ class App {
       } else {
         sum = input.split(delimiter).reduce((acc, cur) => acc + Number(cur), 0);
       }
-
       MissionUtils.Console.print(`값: ${sum}`);
     } catch (error) {
       MissionUtils.Console.print(`[ERROR] ${error.message}`);
