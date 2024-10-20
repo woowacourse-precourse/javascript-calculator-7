50cf70996652ef32897835d00f5d1f9a8fc1117b
Minji Kim <minjikim2000@hotmail.com>
2024-10-20 17:44:16 +0900

    feat: 숫자 추출하여 배열에 저장

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
+    const inputNumbers = input.split(/[,|:]/);
 
-    Console.print("결과 : " + input);
+    Console.print("결과 : " + inputNumbers);
   }
 }
 
