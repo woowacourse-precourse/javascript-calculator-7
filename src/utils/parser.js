72cf3041406a71f269519c26fe53f1e5731734dc
mun-kyeong <horse6953@naver.com>
2024-10-20 14:54:58 +0900

    [FEAT] (View, utils) : 커스텀 구분자 사용
    - "//"와 "\n" 사이에 위치하는 커스텀 문자 구분
    - 커스텀 구분자로 문자열 구분하여 retrun
    
    커스텀 구분자를 추출하여 해당 구분자 기준으로 문자열을 구분합니다.

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,8 +1,10 @@
-/**
- *
- * @todo 커스텀 구분자 처리 필요
- */
 function parseInput(input) {
+  if (input.startsWith("//")) {
+    const delimiterEndIndex = input.indexOf("\\n");
+    const delimiter = input.substring(2, delimiterEndIndex);
+    const numbers = input.substring(delimiterEndIndex + 2);
+    return numbers.split(delimiter).map(Number);
+  }
   return input.split(/[,:]/).map(Number);
 }
 
--- src/views/CalculatorView.js
+++ src/views/CalculatorView.js
@@ -1,5 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 
+/**
+ *
+ * @todo 입력값 띄워쓰기 수정 필요
+ */
 function renderResult(result) {
   Console.print(`결과: ${result}`);
 }
