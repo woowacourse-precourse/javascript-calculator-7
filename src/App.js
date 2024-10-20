150b7686960c10dc7cd1b89d781d7422eb44213c
BoRamSon <onbrson1@gmail.com>
2024-10-20 14:47:49 +0900

    feat : add [2] 입력값 공백제거

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  run() {}
+  async run() {}
 }
 
 export default App;
 
 
+
+// [1] 사용자 입력값 임시테스트값 지정
+const inputValue = "//;\n2 ;4; 5; 7";
+// const inputValue = "1 ;2|3 :5, 8";
+
+
+// [2] 입력값 공백제거
+const INPUT_VALUE = inputValue.replace(/[^\S\n]/g, "");
+console.log('입력값이 무엇이죠? : ', INPUT_VALUE);
 
