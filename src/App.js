2f7dbd3b3d67eac310a7a0b7ed7bd15ecc8688fa
Ray <rlawlsdn9583@gmail.com>
2024-10-21 01:33:42 +0900

    feat: 구분자를 기준으로 문자열을 분리하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App {
 
       // 커스텀 구분자 추출 및 제거
       const [EXTRACT_DATA, SEPARATORS] = this.extractSeparator(INPUT_DATA, this.defaultSeparator); 
+
+      // 구분자를 기준으로 분리
+      const SPLIT_DATA = this.splitString(EXTRACT_DATA, SEPARATORS);    
   };
 
   extractSeparator(inputString, separators) {
@@ -19,6 +22,26 @@ class App {
       inputString = inputString.slice(5);
     }
     return [inputString, separators];
+  }
+  
+  splitString(inputString, separators) {
+    let splitData = [inputString];
+    console.log(separators)
+    for (const separator of separators) {
+      let temp = [];
+      for (const separatorString of splitData) {
+        if (separatorString.indexOf(separator) !== -1) {
+          temp = [...temp, ...separatorString.split(separator)];
+        } else {
+          temp = [...temp, separatorString];
+        }
+      }
+      if (temp.length > 0) {
+        splitData = [...temp];
+      }
+    }
+  
+    return splitData.map(Number);
   }
 }
 export default App;
