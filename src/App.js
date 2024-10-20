8ee925d0b335baf5a02e702b5ebb2a3a8027393a
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-20 22:53:48 +0900

    feat: 잘못된 입력값 처리 기능 구현
    잘못된 값을 입력받았을 시 "[ERROR]"로 시작하는 메시지와 함께 Error를 발생시켜 애플리케이션을 종료시키는 기능을 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ class App {
     let inputString = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    if (
+      !inputString ||
+      inputString.trim() === "" ||
+      inputString.split(/[,:]/).length === 1
+    ) {
+      throw new Error(
+        "[ERROR] 입력값이 없거나 구분자가 없습니다. 프로그램을 종료합니다."
+      );
+    }
+
     inputString = inputString.split(/[,:]/);
-    let sum = 0;
-    sum = inputString.reduce(
+
+    for (let value of inputString) {
+      value = Number(value);
+      if (Number.isNaN(value)) {
+        throw new Error(
+          "[ERROR] 입력값이 숫자가 아닙니다. 프로그램을 종료합니다."
+        );
+      } else if (value < 0) {
+        throw new Error("[ERROR] 입력값이 음수입니다. 프로그램을 종료합니다.");
+      }
+    }
+
+    const sum = inputString.reduce(
       (accumulator, currentValue) => accumulator + Number(currentValue),
       0
     );
-    MissionUtils.Console.print(sum);
+    MissionUtils.Console.print(`결과 : ${sum}`);
   }
 }
 export default App;
