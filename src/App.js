127513725ff78c67a3b9e7f587141fc798d86a17
ingyuLim <123928311+ingyuLim@users.noreply.github.com>
2024-10-20 18:56:43 +0900

    feat: 사용자 입력과 출력을 처리하는 Console API 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  run() {
+    Console.readLine('문자열을 입력하세요: ', (input) => {
+      try {
+        // 나중에 입력값을 처리할 로직이 추가될 예정
+        const result = input; // 일단 입력값 그대로 출력
+        Console.print(`=> ${result}`);
+      } catch (error) {
+        Console.print(error.message);
+      } finally {
+        Console.close();  // 콘솔 인터페이스 종료
+      }
+    });
+  }
 }
 
 export default App;
