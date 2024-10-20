fc3eaa21e136e09c6b601b67dc37b5e7f5a5c743
deokbin <deokbin@naver.com>
2024-10-20 22:28:33 +0900

    test: 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -34,8 +34,48 @@ describe('문자열 계산기', () => {
         });
     });
 
-    test('예외 테스트', async () => {
+    test('예외 테스트1', async () => {
         const inputs = ['-1,2,3'];
+        mockQuestions(inputs);
+
+        const app = new App();
+
+        await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+    test('예외 테스트2', async () => {
+        const inputs = ['1,2,a'];
+        mockQuestions(inputs);
+
+        const app = new App();
+
+        await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+    test('예외 테스트3', async () => {
+        const inputs = ['//;\\n1;2;3;'];
+        mockQuestions(inputs);
+
+        const app = new App();
+
+        await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+    test('예외 테스트4', async () => {
+        const inputs = ['//;\\n1;2;3:4'];
+        mockQuestions(inputs);
+
+        const app = new App();
+
+        await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+    test('예외 테스트5', async () => {
+        const inputs = ['1,,2'];
+        mockQuestions(inputs);
+
+        const app = new App();
+
+        await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+    test('예외 테스트6', async () => {
+        const inputs = ['//;\\n'];
         mockQuestions(inputs);
 
         const app = new App();
