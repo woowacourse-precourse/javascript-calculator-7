36407b1e9dc0a672357f536b44f23441a01b6f69
최하은 <haeun4345@gmail.com>
2024-10-20 12:11:24 +0900

    feat: 사용자 입출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+
+async function getUserInput() {
+  try {
+    const input = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+    return input;
+  } catch (e) {
+    throw new Error('[ERROR] 입력값을 받아오는데 실패했습니다.');
+  }
+}
+
 class App {
-  async run() {}
+  async run() {
+    const input = await getUserInput();
+    Console.print(`결과 : ${input}`);
+  }
 }
 
 export default App;
