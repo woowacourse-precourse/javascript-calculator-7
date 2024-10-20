0d2b4083e885d4f3bf5094d528e60a435058d309
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-20 17:20:51 +0900

    feat : 기본구분자 지정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 class App {
   #inputString;
-
+  #delimiter;
   #resultNumber;
   async run() {
     const INPUT_STRING = await this.userInput(
       '덧셈할 문자열을 입력해 주세요.\n'
     );
     this.#inputString = INPUT_STRING;
-
+    this.separateNumbers();
     await this.printOutput(`결과 : ${this.#resultNumber}`);
   }
 
@@ -18,6 +18,9 @@ class App {
 
   async printOutput(content) {
     return await Console.print(content);
+  }
+  separateNumbers() {
+    this.#delimiter = [',', ':'];
   }
 }
 
