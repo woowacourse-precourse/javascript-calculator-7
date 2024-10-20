a23c34d4bc4ff0fc11b81ea6bd2d5da911c82f07
oko_jin <dswvgw6412@naver.com>
2024-10-21 03:17:45 +0900

    test(error): Validator 기능 테스트 코드 구현

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,42 +1,52 @@
-import App from '../src/App.js';
-import { MissionUtils } from '@woowacourse/mission-utils';
 import Validator from '../src/Validator.js';
+import { errorMessages } from '../src/constant.js';
+import Calculator from '../src/Calculator.js';
 
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
-
-describe('문자열 계산기', () => {
-  test('커스텀 구분자 사용', async () => {
-    const inputs = ['//;\\n1'];
-    mockQuestions(inputs);
-
-    const logSpy = getLogSpy();
-    const outputs = ['결과 : 1'];
-
-    const app = new App();
-    await app.run();
-
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
-  });
-
-  test('음수 예외처리', async () => {
+describe('Validator 테스트', () => {
+  test('음수 예외 처리', async () => {
     const input = '//;\n-1;2,-3:4.44,-5.12';
     const validator = new Validator();
 
     await expect(validator.valid(input)).toEqual(false);
+  });
+
+  test('커스텀 구분자 사용', async () => {
+    const input = '//;\\n1;2.2;3.33,4.444';
+    const validator = new Validator();
+
+    await expect(validator.valid(input)).toEqual(true);
+  });
+
+  test('기본 구분자 사용', async () => {
+    const input = '1;2.2;3.33,4.444';
+    const validator = new Validator();
+
+    await expect(validator.valid(input)).toEqual(true);
+  });
+
+  test('구분자 오류 예외 처리', async () => {
+    const input = '//;\\n1;2.2;3.33!4.444';
+
+    await expect(() => new Calculator(input).add()).toThrow(errorMessages.invalidDelimiter);
+  });
+
+  test('커스텀 구분자 오류 예외 처리', async () => {
+    const input = '//.\\n1;2.2;3.33,4.444';
+    const validator = new Validator();
+
+    await expect(() => validator.valid(input)).toThrow(errorMessages.invalidCustomDelimiter);
+  });
+
+  test('커스텀 구분자 미입력 예외 처리', async () => {
+    const input = '//\\n1;2.2;3.33,4.444';
+    const validator = new Validator();
+
+    await expect(() => validator.valid(input)).toThrow(errorMessages.notInputCustomDelimiter);
+  });
+
+  test('커스텀 구분자 중복 입력 예외 처리', async () => {
+    const input = '//,\\n1;2.2;3.33,4.444';
+
+    await expect(() => new Calculator(input)).toThrow(errorMessages.duplicatedDelimiter);
   });
 });
