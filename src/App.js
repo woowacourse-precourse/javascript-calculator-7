8fdbe8f2004e3ccc5d90eefb9867161df4331904
Minji Kim <minjikim2000@hotmail.com>
2024-10-20 19:26:55 +0900

    feat: 커스텀 구분자 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    var inputNumbers;
+
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
-    const inputNumbers = input.split(/[,|:]/);
+
+    const customSeparator = input.match(/\/\/(.)\\n/);
+
+    if (customSeparator != null) {
+      inputNumbers = input.split(customSeparator[1]);
+      inputNumbers.splice(0, 2);
+    } else {
+      inputNumbers = input.split(/[,|:]/);
+    }
 
     Console.print("결과 : " + inputNumbers);
   }
