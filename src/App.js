9e69260e17ac7bf8fce65a6f9f8b3afbdb6b7cc9
MINJOO-KIM <gkminju2000@gmail.com>
2024-10-20 17:02:09 +0900

    feat: 입력을 받는다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 입력을 받는다.
+      const input = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요."
+      );
+    } catch (error) {
+      MissionUtils.Console.print("[ERROR] ");
+    }
+  }
 }
 
 export default App;
