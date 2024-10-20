6d0b8dc42269c7235b07a7ca049d8292eca6eec0
Daeuning <happyjde3@gmail.com>
2024-10-21 05:03:53 +0900

    feat(Calculator): 주어진 구분자로 문자열을 나누는 splitByDelimiter 메소드 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,13 +4,31 @@ function getInput() {
   Console.readLine('덧셈할 문자열을 입력해 주세요.', (answer) => {
     console.log(answer);
     const customDelimiter = getCustomDelimiter(answer);
-    console.log(customDelimiter);
+    const result = splitByDelimiter(answer, customDelimiter);
+    console.log(result);
   });
 }
 
 function getCustomDelimiter(input) {
   const customDelimiterPattern = /^\/\/(.+)\\n/;
   return input.match(customDelimiterPattern);
+}
+
+function splitByDelimiter (input, customdelimiter) {
+  const defaultDelimiters = [':', ','];
+  let remainingInput = input;
+
+  if (customdelimiter) {
+    const customDelimiter = customdelimiter[1];
+    remainingInput = input.slice(customdelimiter[0].length);
+    defaultDelimiters.push(customDelimiter);
+  }
+
+  const escapedDelimiters = defaultDelimiters.map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+
+  const regex = new RegExp(escapedDelimiters.join('|'), 'g');
+
+  return remainingInput.split(regex);
 }
 
 export function calculator() {
