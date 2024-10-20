75f2bff06ac7b9c30415c2ca8f5bb7562637c149
김아영 <thay123028@naver.com>
2024-10-21 00:17:35 +0900

    feat: 빈 문자열 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 class App {
   async run() {
-    
+
     const input = promt("덧셈할 문자열을 입력해 주세요.\n");
 
     try {
@@ -8,6 +8,12 @@ class App {
       console.log(`결과 : ${result}`);
     } catch (error) {
       console.error(`[ERROR] ${error.message}`);
+    }
+  }
+
+  add(input) {
+    if (input == "") {
+      return 0; // 빈 문자열 처리
     }
   }
 }
