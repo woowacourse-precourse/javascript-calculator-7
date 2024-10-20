75775eda2d99280c777e2efcc99b5d7810c3dd49
hyeongdong <khd941018@gmail.com>
2024-10-20 21:33:33 +0900

    test(calculatorFnTest): 구분자를 기준으로 문자열에서 숫자를 분리하는 기능 테스트 코드 구현

--- __tests__/calculatorFnTest.js
+++ __tests__/calculatorFnTest.js
@@ -1,13 +1,23 @@
 import Calculator from "../src/Calculator";
 
 describe("문자열 계산기 함수 테스트", () => {
+  const calculator = new Calculator();
+
   test("커스텀 구분자를 문자열로부터 구분할 수 있다.", () => {
     const inputs = ["//;\\n12345", "//.\\n12345", "//-\\n12345", "12345k", ";\\n1234"];
     const outputs = [";", ".", "-", 0, 0];
-    const calculator = new Calculator();
 
     outputs.forEach((output, index) => {
       expect(calculator.getSeperatorFrom(inputs[index])).toBe(output);
+    });
+  });
+
+  test("문자열에서 구분자들로 숫자들만 따로 추출할 수 있다.", () => {
+    const inputs = ["1", "1,2,3", "1,2:3", "1:2:3", "1"];
+    const outputs = [["1"], ["1", "2", "3"], ["1", "2", "3"], ["1", "2", "3"]];
+
+    outputs.forEach((output, index) => {
+      expect(calculator.seperateBy(inputs[index])).toEqual(output);
     });
   });
 });
