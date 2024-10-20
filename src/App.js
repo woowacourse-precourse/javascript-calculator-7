70a8e7ff42fe95f047d1ba791b3863ae50b6c91f
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-20 17:10:14 +0900

    feat : 입출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  #inputString;
+
+  #resultNumber;
+  async run() {
+    const INPUT_STRING = await this.userInput(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+    this.#inputString = INPUT_STRING;
+
+    await this.printOutput(`결과 : ${this.#resultNumber}`);
+  }
+
+  async userInput(content) {
+    return await Console.readLineAsync(content);
+  }
+
+  async printOutput(content) {
+    return await Console.print(content);
+  }
 }
 
 export default App;
