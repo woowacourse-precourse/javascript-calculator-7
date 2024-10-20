54fdcaad866f6d2033f349cb2c2b6b9fa02b64f7
Joohyun Kim <blaireinse@gmail.com>
2024-10-21 00:42:18 +0900

    refactor(calculator): move calculator logic to separate calculator class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import Input from './utils/input.js';
-import Parser from './utils/parser.js';
 import Calculator from './utils/calculator.js';
-import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
     try {
-      const inputValue = await Input.getInputValue();
-
-      const { parsedString, delimiterRegex } = Parser.parseDelimiter(inputValue);
-      const operands = Parser.parseOperands(parsedString, delimiterRegex);
-
-      const result = Calculator.getSum(operands);
-      Console.print(`결과 : ${result}`);
+      await Calculator.calculate();
     } catch (err) {
       throw err;
     }
--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,4 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+import Input from './input.js';
+import Parser from './parser.js';
+
 class Calculator {
+  static async calculate() {
+    const inputValue = await Input.getInputValue();
+
+    const { parsedString, delimiterRegex } = Parser.parseDelimiter(inputValue);
+    const operands = Parser.parseOperands(parsedString, delimiterRegex);
+
+    const result = Calculator.getSum(operands);
+    Console.print(`결과 : ${result}`);
+  }
+
   static getSum(numbers) {
     return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   }
