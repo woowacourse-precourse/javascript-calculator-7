aa56783c7ff84d6371c4ce286b095decd2504baa
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-20 22:22:08 +0900

    refactor: 변수명, 함수명 수정

--- src/models/CalculatorModel.js
+++ src/models/CalculatorModel.js
@@ -4,14 +4,15 @@ const ERROR_MESSAGES = {
   ZERO_NOT_ALLOWED: '[ERROR] : 0은 입력할 수 없습니다.',
 };
 
+const DEFAULT_DELIMITER = /,|:/;
+
 // --- 에러 처리 함수 ---
-export function throwError(code) {
-  throw new Error(ERROR_MESSAGES[code]);
+export function throwError(errorCode) {
+  throw new Error(ERROR_MESSAGES[errorCode]);
 }
 
-// --- 예외처리 함수 ---
-// 양수가 아닌 수가 입력되거나, 구분자가 아닌 문자가 입력 된 경우
-export function checkForErrors(input) {
+// --- 입력 값 검증 함수 ---
+export function validateInput(input) {
   const hasNonNumeric = input.map(Number).some((char) => !/^\d+$/.test(char));
   const hasZero = input.some((char) => /^0+$/.test(char));
 
@@ -19,8 +20,8 @@ export function checkForErrors(input) {
   if (hasZero) throwError('ZERO_NOT_ALLOWED');
 }
 
-// --- 구분자 추출 함수 ---
-export function extractDelimiter(input) {
+// --- 구분자 추출 ---
+export function getCustomDelimiter(input) {
   return input.startsWith('//')
     ? input.substring(2, input.indexOf('\\n'))
     : null;
@@ -32,48 +33,39 @@ export function removeDelimiterSection(customDelimiter, input) {
 }
 
 // --- 문자열을 구분자 기준으로 나누기 ---
-export function splitString(customDelimiter, processedInput, defaultDelimiter) {
-  // 커스텀 문자로 나누기
+export function splitString(customDelimiter, processedInput) {
   const splitByCustomDelimiter = customDelimiter
     ? processedInput.split(customDelimiter)
     : [processedInput];
 
-  // 기본 구분자로 나누기
   const splitByDefaultDelimiters = splitByCustomDelimiter.flatMap((part) =>
-    part.split(defaultDelimiter),
+    part.split(DEFAULT_DELIMITER),
   );
 
   return splitByDefaultDelimiters;
 }
 
 // --- 빈 문자열은 0 반환 ---
-export function cleanAndConvertToNumbers(inputArr) {
+export function replaceEmptyWithZero(inputArr) {
   const cleanedNumbers = inputArr.map((value) => (value === '' ? '0' : value));
   return cleanedNumbers.map(Number);
 }
 
 // --- 숫자 파싱 로직 ---
 export function parseNumbers(input) {
-  const dafaultDelimiter = /,|:/; // 기본구분자
-  const customDelimiter = extractDelimiter(input); // 커스터 구분자 추출
-  const processedInput = removeDelimiterSection(customDelimiter, input); // 커스텀 부분 제외한 문자열
+  const customDelimiter = getCustomDelimiter(input);
+  const processedInput = removeDelimiterSection(customDelimiter, input);
 
-  // 구분자로 문자열 나누기
-  const splitByDelimiters = splitString(
-    customDelimiter,
-    processedInput,
-    dafaultDelimiter,
-  );
+  const splitByDelimiters = splitString(customDelimiter, processedInput);
 
-  checkForErrors(splitByDelimiters);
+  validateInput(splitByDelimiters);
 
-  // 빈 문자열이 있으면 '0'으로 대체
-  return cleanAndConvertToNumbers(splitByDelimiters);
+  return replaceEmptyWithZero(splitByDelimiters);
 }
 
 // --- 계산 ---
 export function calculate(input) {
   const numArr = parseNumbers(input);
-  const sum = numArr.reduce((acc, current) => acc + current, 0); // 배열의 합 계산
+  const sum = numArr.reduce((acc, current) => acc + current, 0);
   return sum;
 }
