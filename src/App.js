641d647ea46d5550b110d994f192e7c0297144d7
kanghaeun <xgkg0330@jnu.ac.kr>
2024-10-21 05:28:29 +0900

    feat: 공백 포함 시 예외 처리 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    input = this.validateInitialInput(input);
+  }
+
+  validateInitialInput(input) {
+    if (/\s/.test(input)) {
+      throw new Error("[ERROR] 공백이 포함될 수 없습니다.");
+    }
   }
 }
 
