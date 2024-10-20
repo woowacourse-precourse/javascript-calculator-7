482241e1dbde18a610598d5ce43b2cec23415aa7
Yoonjeong Ko <jejukyj@naver.com>
2024-10-20 22:31:04 +0900

    feat : 쉼표(,) 또는 콜론(:) 구분자를 기준으로 숫자 분리하기

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
+      // 사용자로부터 문자열 입력받기
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-      Console.print(`결과 : ${input}`);
+
+      const numbers = this.parseInput(input);
+
+      Console.print(`결과 : ${numbers}`);
+
     } catch (error) {
       // reject 되는 경우
     }
+  }
+
+  // 문자열 파싱
+  parseInput(input){
+    let delimiter = /,|:/; // 정규식을 사용하여 구분자 여러개 설정
+
+    return input.split(delimiter).map((num) => parseInt(num));
   }
 }
 
