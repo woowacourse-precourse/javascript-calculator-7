390f28fb5443ec038d6e411ccafd4c6031c52622
j0n0m2 <dbsk8223@naver.com>
2024-10-21 00:02:26 +0900

    '\\n'앞에 '//'가 없는 경우 [ERROR]를 띄운다

--- src/App.js
+++ src/App.js
@@ -11,26 +11,33 @@ class App {
     //구분자를 사용해 숫자를 분리한다
     let DELIMITER = /,|:/;
     let NUMBERSINPUT = INPUT
-    // 커스텀 구분자를 사용한다.
-    // '//' 뒤에 '\\n'가 없는 경우
+    
+    // '//' 뒤에 '\\n'가 없는 경우 error
     if (INPUT.startsWith('//')) {
-      const NEWLINE_INDEX = INPUT.indexOf('\\n');
-      if (NEWLINE_INDEX === -1) {
+      const CUSTOM_CLOSING = INPUT.indexOf('\\n');
+      if (CUSTOM_CLOSING === -1 ) {
         throw new Error("[ERROR]")
-      } else {
+      } 
+      // 커스텀 구분자를 사용한다. 
+      else {
         const CUSTOM = INPUT.substring(2,INPUT.indexOf('\\n'));
         DELIMITER = new RegExp(`[${CUSTOM},:]`)
         NUMBERSINPUT = INPUT.substring(INPUT.indexOf('\\n') + 2 );
       }
 
+    } 
+    // '\\n' 앞에 '//'가 없는 경우 error
+    else {
+      if (INPUT.indexOf('\\n') > -1) {
+        throw new Error("[ERROR]")
+      }
     }
     const NUMBERS = NUMBERSINPUT.split(DELIMITER).map(Number);
     // 분리한 숫자의 합을 반환한다
     const RESULT = NUMBERS.reduce((sum, num) => sum + num, 0);
-  
-    //예외 처리
-    //음수를 입력하는 경우
-    //'//'와 '\\n'사이가 공백일 경우
+
+    //음수를 입력하는 경우 error
+    //'//'와 '\\n'사이가 공백일 경우 error
     const NEGATIVES = NUMBERS.filter(num => num < 0);
 
     if (NEGATIVES.length > 0 || INPUT.indexOf('\\n') === 2) {
