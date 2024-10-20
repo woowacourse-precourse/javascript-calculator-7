1acaa52007be7cf2ecb189108dbd837559977839
boxi <boxiyoun@gmail.com>
2024-10-21 00:26:44 +0900

    실행시 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 class App {
-  async run() {}
+  async run() {
+    function startCal() {
+      console.log('덧셈할 문자열을 입력해 주세요.');
+    }
+    startCal();
+  }
 }
+
+let APP = new App();
+APP.run();
+
+
 
 export default App;
