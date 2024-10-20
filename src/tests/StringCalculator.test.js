33f0135f0b7600554e859147884d48aee792543c
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-20 17:59:22 +0900

    chore: 불필요한 테스트 코드 삭제

--- src/tests/StringCalculator.test.js
+++ src/tests/StringCalculator.test.js
@@ -7,12 +7,6 @@ describe('StringAddCalculator', () => {
     expect(result).toBe(6);
   });
 
-  test('정상 케이스. 모든 숫자가 양의 정수일 때 합 반환', () => {
-    const numbersArray = [100, 200, 300];
-    const result = StringAddCalculator.sum(numbersArray);
-    expect(result).toBe(600);
-  });
-
   test('정상 케이스. 하나의 숫자만 있을 때 그 숫자 그대로 반환', () => {
     const numbersArray = [42];
     const result = StringAddCalculator.sum(numbersArray);
