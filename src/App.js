23396d14c483239c816748c4586ca3ae3814ed69
inaemin <inaemin@gmail.com>
2024-10-21 01:24:06 +0900

    update: 함수별 로직 주석 작성

--- src/App.js
+++ src/App.js
@@ -18,14 +18,28 @@ class App {
   }
 
   // string인 input을 구분자 기준으로 쪼개서 숫자로 변환할 배열에 담는다.
-  parseString(input) {}
+  parseString(input) {
+    // 커스텀 구분자가 존재하는지 정규식으로 검사
+    // 있다면 커스텀 구분자 설정 뒤를 input으로 설정
+    const numStrs = [];
+    // 구분자를 기준으로 나뉜 문자열을 numStrs에 추가
+
+    return this.verifyNumber(numStrs);
+  }
 
   // 구분자를 제외한 문자열형식의 숫자들이 든 배열을 input으로 받아, 실제 숫자인지, 음수인지 검사한다.
   // 숫자가 아니거나 음수이면 예외 처리를 한다.
-  verifyNumber(numbers) {}
+  verifyNumber(numbers) {
+    // 배열의 요소가 NaN이거나 음수이면 에러를 리턴한다.
+    const verifiedNumbers = [];
+    // 숫자로 변환된 요소들을 더해준다.
+    return this.calculateSum(verifiedNumbers);
+  }
 
   // 파싱한 숫자를 더한다.
-  calculateSum(numbers) {}
+  calculateSum(numbers) {
+    return numbers.reduce((res, ele) => res + ele, 0);
+  }
 }
 
 export default App;
