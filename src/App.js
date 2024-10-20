665db58e0801b21fdbd5aecff5e2fbb7c17df491
kim-museong <antjd0419@gmail.com>
2024-10-21 03:09:01 +0900

    feat(console): 라이브러리를 사용하여 입출력 기능 구현
    - 유저가 입력한 문자열을 받아서 콘솔에 반환하는 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+// @ts-nocheck
+
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+    async run() {
+        Console.print('덧셈할 문자열을 입력해 주세요.');
+        await this.readInput();
+    }
+
+    async readInput() {
+        try {
+            const VALUE = await Console.readLineAsync('');
+            Console.print(`결과 : ${VALUE}`);
+        } catch (error) {
+            Console.print(`[ERROR] : ${error.message}`);
+        }
+    }
 }
 
 export default App;
