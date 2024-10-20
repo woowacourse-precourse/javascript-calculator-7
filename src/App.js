897664a497360957aba58649b9e552c7451e3545
sanghyeonLee <hyeon012366@gmail.com>
2024-10-20 22:05:47 +0900

    fix : isCustom 커스텀 구분자 유무만 판단하는 메서드로 수정, 다른 로직 제거

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App { //하나의 메소드는 하나의 역할
     customSeparator = ""//인스턴스 변수는 최대 2개
     async run() { //풀이 결과 반환 담당, 메소드명만 봐도 무슨역할인지 알 수 있게
         const input = await this.input()
-        input.startsWith("//") ? this.isCustom(input) : this.calculator(this.toArray([...input]))
+        this.isCustom(input) ? this.isCustom(input) : this.calculator(this.toArray(input))
         this.resultOutput()
     }
 
@@ -13,17 +13,13 @@ class App { //하나의 메소드는 하나의 역할
         return await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
     }
 
-    isCustom(value) {
-        const lastIdx = value.indexOf("\\n") // "\n" 가 없는 경우
-        if (lastIdx !== -1) {
-            this.customSeparator = value.slice(2, lastIdx)
-            return this.calculator(this.toArray([...value.slice(lastIdx + 2)]))
-        }
-        throw new Error("[Error]: 에러발생")
+    isCustom(param) {
+        return param.startsWith("//") && param.includes("\\n")
     }
 
+
     isSeparator(value) {//구분자 판단
-        if (!isNaN(value)) {
+        if (!isNaN(value)) { //부정 조건 지양하자
             return value
         }
         /*if (value === this.customSeparator) {
@@ -35,8 +31,8 @@ class App { //하나의 메소드는 하나의 역할
         throw new Error("[Error]: 에러발생")
     }
 
-    toArray(param) { //10이상 수 삽입 : 추후 인덴트 줄이기
-        let arr = []
+    toArray(param) { //10이상 수 삽입 : 추후 인덴트 줄이기, 클래스는 하나의 역할만
+        let arr = [...param]
         let tmpValue = "";
         param.forEach((element, idx) => { //구분자가 숫자 값이면? 문자 값이라 했으니 문자만?
                 if (!isNaN(element)) {
