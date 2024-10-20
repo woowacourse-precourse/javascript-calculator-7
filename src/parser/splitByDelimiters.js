70e51e8bcb4df058c6fb87e9d4cdbb294b548b5c
Junseo Kim <semiwest2@gmail.com>
2024-10-21 00:56:33 +0900

    refactor: splitByDelimiters 함수에서 불필요한 템플릿 리터럴 제거
    customDelimiter를 템플릿 리터럴(``${})로 감쌀 필요가 없음을 인지하고 이를 제거한 수정

--- src/parser/splitByDelimiters.js
+++ src/parser/splitByDelimiters.js
@@ -1,7 +1,7 @@
 // 문자열을 구분자에 따라 나누고, 숫자 배열로 변환하는 함수
 
 export default function splitByDelimiters(cleanedString, customDelimiter) {
-  const delimeters = [",", ";", `${customDelimiter}`].filter(d => d);
+  const delimeters = [",", ";", customDelimiter].filter(Boolean);
   const regex = new RegExp(`[${delimeters.join("")}]`);
   const parsedNumberList = cleanedString.split(regex).map(Number);
   return parsedNumberList;
