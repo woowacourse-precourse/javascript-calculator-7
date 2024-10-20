cba8fd27557e6f0072f25c8f02af8c3be0a15b73
spoyodevelop <kjy9505@gmail.com>
2024-10-20 10:36:40 +0900

    refactor: refactor regex for more reusability

--- src/Util/regex.js
+++ src/Util/regex.js
@@ -1,11 +1,12 @@
 const numberPattern = '\\d+(\\.\\d+)?';
+const specialCharsPattern = /[.*+?^${}()|[\]\\]/g;
 
 function escapeRegExp(string) {
   // 정규식에 사용되는 모든 예약어들을 escape 처리한다. .*[]\ 등...
   // 원본 문자를 남기고 ($&), 그 문자에 \를 더 붙인다.
   // "Hello. How+are|you?" =>
   // "Hello\. How\+are\|you\?"
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  return string.replace(specialCharsPattern, '\\$&');
 }
 
 function buildNormalInputValidationRegex() {
