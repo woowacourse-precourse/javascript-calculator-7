43f2a83cc99e1cbc7cfe209b045d8e713d3e4fe9
ekkk1126 <cabi0203@naver.com>
2024-10-21 03:57:22 +0900

    feat: 계산기 앱 실행 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Calculator from './calculator.js';
+import Input from './io-system/input.js';
+import Output from './io-system/output.js';
+import Splits from './utils/splits.js';
+
 class App {
-  async run() {}
+    async run() {
+        // 1️⃣ 사용자 입력
+        const userInput = await Input();
+
+        // 2️⃣ 입력값 검증
+        const validNums = Splits(userInput);
+
+        // 3️⃣ 계산기 실행
+        const result = Calculator(validNums);
+
+        // 4️⃣ 결과 출력
+        Output(result);
+    }
 }
 
 export default App;
