b5eefd5ace7d8b1de18a4e159f92822640b0d748
Jiny <moc24@naver.com>
2024-10-20 13:29:33 +0900

    docs(.js) : 입력받기 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  
+  async run() { // 비동기 동작. 
+    const getResult= (answer) => {
+      Console.print(`결과 : ${answer}`);
+      return;
+    }
+
+    Console.readLine('덧셈할 문자열을 입력해 주세요.\n', (answer) => {getResult(answer)});
+
+
+  }
 }
 
 export default App;
