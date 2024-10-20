c8cfff83c58a49c265b672e5f6ea7061a2ba46af
jiyun1006 <jiyun4251@gmail.com>
2024-10-20 12:36:09 +0900

    🐛 fix : 특수문자 검증하기 위한 표현식 수정
    \n 를 검증하지 못하는 문자 표현식을 수정 (\n --> \\n)

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ class App {
 
 async function getInputStr() {
   try {
-    const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-    if(isCustom(inputStr))
+    const inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const separator = [":", ","];
+    if (isCustom(inputStr)) {
+      const tmpSep = inputStr.slice(2, 3);
+      console.log(tmpSep);
+    } else {
+    }
     return inputStr;
   } catch (error) {
     // reject 되는 경우
   }
 }
 
-// 입력문에서의 에러 상황
+// 입력문에서의 에러 상황 체크
 
 const isCustom = (inputStr) => {
-  const regExp = /(\/\/.{1}\n).{1,}/;
+  const regExp = /(\/\/.{1}\\n).{1,}/;
   return regExp.test(inputStr);
 }
 
