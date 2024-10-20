3defcdcb23a31475b75d896f77ef0dfd2fe6872e
wnsxk2 <wnsxk206@gmail.com>
2024-10-20 10:28:58 +0900

    feat: 커스텀 문자열로 구분하는 기능 추가
    입력받은 문자열에서 커스텀 구분자를 확인하고 커스텀 구분자를 기준으로 구분하여 배열로 반환하는 코드 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      const regex = /[,:]/;
-
+       const CUSTOM_DELIMITER_REGEX = /^\/\/(\D)\\n/;
+      
+       let delimiter;
+       let str;
       const answer = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
 
-      const sum = answer.split(regex)
+      const match = answer.match(CUSTOM_DELIMITER_REGEX);
+
+      if(match) {
+        str = answer.replace(CUSTOM_DELIMITER_REGEX, '');
+        delimiter = match[1];
+      } else {
+        str = answer;
+        delimiter = /[,:]/;
+      }
+
+      Console.print(`delimiter : =${delimiter}= | str : ${str}`);
+      const sum = str.split(delimiter)
         .reduce((total, num) => total + Number(num), 0 );
-        
+
       Console.print(`결과 : ${sum}`);
     } catch (error) {
       // reject 되는 경우
