607e252bb4393ecd5b3f7bbf6d49ffbf5ef2c74c
jsy <suyeonb1db@gmail.com>
2024-10-20 23:21:26 +0900

    style: 큰따옴표로 통일

--- src/App.js
+++ src/App.js
@@ -26,28 +26,28 @@ function getSplitunit(input){
 
 async function getText() {
   try {
-    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요. \n');
+    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요. \n");
 
     // 문자열이 아닌 입력
-    if(typeof input !== 'string') throw new Error('[ERROR] 입력 형태가 문자열이 아닙니다.');
+    if(typeof input !== "string") throw new Error("[ERROR] 입력 형태가 문자열이 아닙니다.");
 
     // 음수가 포함된 입력
     let noNumber = input.match(/-\d+/);
     if(noNumber && noNumber.length > 0){
-      throw new Error('[ERROR] 음수가 포함되었습니다.');
+      throw new Error("[ERROR] 음수가 포함되었습니다.");
     }
     // 숫자로만 구성된 입력
-    if(/^\d+$/.test(input.replace(/\s+/g, ''))) throw new Error('[ERROR] 구분자 없이 숫자만 입력했습니다.');
+    if(/^\d+$/.test(input.replace(/\s+/g, ""))) throw new Error("[ERROR] 구분자 없이 숫자만 입력했습니다.");
 
     // 기본 구분자로만 구성된 입력
-    if (/^[,:]+$/.test(input.replace(/\s+/g, ''))) {
-      throw new Error('[ERROR] 구분자로만 이루어진 입력입니다.');
+    if (/^[,:]+$/.test(input.replace(/\s+/g, ""))) {
+      throw new Error("[ERROR] 구분자로만 이루어진 입력입니다.");
     }
 
     // 커스텀 구분자만 구성된 입력
     if(input.startsWith("//") && input.includes("\n")){
       const checknumber = input.split("\n")[1];
-      if(!checknumber || !/\d/.test(checknumber)) throw new Error('[ERROR] 구분자로만 이루어진 입력입니다.')
+      if(!checknumber || !/\d/.test(checknumber)) throw new Error("[ERROR] 구분자로만 이루어진 입력입니다.z");
     }
     return input;
   } catch (error) {
