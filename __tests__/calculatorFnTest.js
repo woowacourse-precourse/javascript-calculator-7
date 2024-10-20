ffa4061eef6a9acb36a96b94e6ba3fd728ad1e4e
hyeongdong <khd941018@gmail.com>
2024-10-20 17:51:28 +0900

    test(calculatorFnTest): 커스텀 구분자 테스트 항목 추가
    커스텀 구분자가 없는 문자열에 대해서 제대로 작동하는지 확인하는 테스트를 추가
    
    getSeperatorFrom()로 메서드명을 변경함

--- __tests__/calculatorFnTest.js
+++ __tests__/calculatorFnTest.js
@@ -2,12 +2,12 @@ import Calculator from "../src/Calculator";
 
 describe("문자열 계산기 함수 테스트", () => {
   test("커스텀 구분자를 문자열로부터 구분할 수 있다.", () => {
-    const inputs = ["//;\\n12345", "//.\\n12345", "//-\\n12345"];
-    const outputs = [";", ".", "-"];
+    const inputs = ["//;\\n12345", "//.\\n12345", "//-\\n12345", "12345k", ";\\n1234"];
+    const outputs = [";", ".", "-", 0, 0];
     const calculator = new Calculator();
 
     outputs.forEach((output, index) => {
-      expect(calculator.getUserSeperator(inputs[index])).toBe(output);
+      expect(calculator.getSeperatorFrom(inputs[index])).toBe(output);
     });
   });
 });
