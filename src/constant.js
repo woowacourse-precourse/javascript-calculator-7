fbf92c1c2dd15c105813cec9abfb10206e7848eb
solmee <kii5541@naver.com>
2024-10-21 05:02:01 +0900

    chore: 상수 파일 추가 및 에러메시지와 기본 구분자 추가

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGE = {
+  INVALID_INPUT: "[ERROR] 유효하지 않은 값을 입력하였습니다.",
+  NEGATIVE_NUMBER: "[ERROR] 음수는 허용되지 않습니다.",
+  CUSTOM_DELIMITER_ERROR: "[ERROR] 커스텀 구분자가 잘못되었습니다.",
+};
+
+export const DEFAULT_DELIMITER = /[,|:]/;
