6df4e0698bbbc49057b557d6a0533c07f5ea35cd
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-20 14:39:07 +0900

    refactor: 변수명 변경 및 reduce 사용
    - input_string을 inputString으로 변경하여 카멜 케이스 규칙 준수
    - 배열 덧셈에 forEach 대신 reduce 사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
-    let input_string = await MissionUtils.Console.readLineAsync(
+    let inputString = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    input_string = input_string.split(/[,:]/);
+    inputString = inputString.split(/[,:]/);
     let sum = 0;
-    input_string.forEach((n) => {
-      sum += Number(n);
-    });
-    // 덧셈결과 출력
+    sum = inputString.reduce(
+      (accumulator, currentValue) => accumulator + Number(currentValue),
+      0
+    );
     MissionUtils.Console.print(sum);
   }
 }
