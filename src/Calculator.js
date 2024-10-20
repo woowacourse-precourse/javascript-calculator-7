51926bfb643899168a103ed37759910a9f43d856
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-10-20 17:00:05 +0900

    refactor(calculator): 유효성 검사를 Validator의 static 메서드로 대체

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,6 @@
-import { checkForNaN } from './utils/checkForNaN.js';
-import { hasNegative } from './utils/hasNegative.js';
 import { splitByCustomSeparator } from './utils/splitByCustomSeparator.js';
 import { splitByDefaultSeparators } from './utils/splitByDefaultSeparators.js';
-import { throwError } from './utils/throwError.js';
+import Validator from './Validator.js';
 
 class Calculator {
 	#input = '';
@@ -13,33 +11,16 @@ class Calculator {
 	}
 
 	calculate() {
-		this.#validateInput();
+		Validator.checkNegativeInput(this.#input);
 		this.#parseAndSplitInput();
-		this.#validateSplitResult();
+		Validator.checkSplitResult(this.#input, this.#result);
 		return this.#calculateSum(this.#result);
-	}
-
-	#validateInput() {
-		if (hasNegative(this.#input)) {
-			throwError('[ERROR] : 양수만 입력할 수 있어요.');
-		}
-	}
-
-	#validateSplitResult() {
-		if (this.#result[0] === this.#input) {
-			throwError('[ERROR] : 구분자와 양수로 이루어진 값을 입력해주세요.');
-		}
-		if (checkForNaN(this.#result)) {
-			throwError('[ERROR] : 입력값에 숫자 이외의 값이 포함되어 있어요.');
-		}
 	}
 
 	#parseAndSplitInput() {
 		if (this.#input.startsWith('//')) {
 			const result = splitByCustomSeparator(this.#input);
-			if (result === null) {
-				throwError('[ERROR] : 커스텀 구분자를 확인할 수 없어요.');
-			}
+			Validator.checkResultNull(result);
 			this.#result = result;
 		} else {
 			this.#result = splitByDefaultSeparators(this.#input);
