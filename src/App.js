7005ab97c18ab64775ca88fe7da67065042e009f
sky121016 <sky121016@naver.com>
2024-10-20 21:02:39 +0900

    feat: 커스텀 구분자 파싱

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ class App {
   async run() {
 
     try{
+      let delimiters = [',',':'];
+
       // 입력
       const input = await this.getInput();
-      Console.print(input);
+      // Console.print(input);
 
-      // 커스텀 구분자 파싱
+      // 커스텀 구분자 파싱 후 숫자 입력만 반환
+      const inputNumbers = await this.customDelimiter(input, delimiters);
+      // Console.print(inputNumbers);
 
       // 숫자 파싱
 
@@ -23,10 +27,39 @@ class App {
     }
   }
 
+  // 입력
   async getInput(){
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
     return input;
   }
+
+
+  // 커스텀 구분자 처리
+  async customDelimiter(input, delimiters){
+    
+    let s = 0;
+    let e = 2;
+    
+    // 앞부분에서 // 와 \n 사이에 있으면 모두 구분자
+    while (input.substring(s, e) == "//" && e < input.length) {
+      while (e < input.length && input.substring(e, e + 2) !== "\\n") {
+        e++;
+      }
+      
+      const newDelimiter = input.substring(s + 2, e);
+
+      // 구분자 배열에 push
+      delimiters.push(newDelimiter);
+
+      // index 재설정
+      s = e + 2;
+      e = s + 2;
+    }
+
+    const inputNumbers = input.substring(s);
+    return inputNumbers;
+  }
+
 }
 
 export default App;
