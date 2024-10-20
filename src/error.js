48ecf98ac88a1b7e7d65e5ba6bcc0f893bad2c3f
abyss-s <xyz@inha.edu>
2024-10-20 15:53:29 +0900

    feat(error.js): WRONG_DELIMETER 에러 추가
    - 구분자 입력 형식이 올바르지 않을 경우 처리할 에러
    - ex. 동일한 구분자를 여러개 입력할 경우
    - ex2. 구분자가 문자가 아닌 경우

--- src/error.js
+++ src/error.js
@@ -3,6 +3,7 @@ const Errors = {
   EMPTY_INPUT: "입력된 문자열이 없습니다.",
   INVALID_FORMAT: "계산할 수 있는 형식이 아닙니다.",
   NEGATIVE_NUMBERS: "음수는 입력할 수 없습니다.",
+  WRONG_DELIMETER: "구분자 입력 형식이 올바르지 않습니다."
 };
 
 export default Errors;
