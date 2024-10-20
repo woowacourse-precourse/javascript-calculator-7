ff17c09154448da6e6b5746dfaef9df496548b31
boxi <boxiyoun@gmail.com>
2024-10-21 01:49:24 +0900

    입력값 받기 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+Console.print(1);
+
+
 class App {
-  async run() {
-    function startCal() {
-      console.log('덧셈할 문자열을 입력해 주세요.');
-    }
-    startCal();
+  #startCalculator() {
+    Console.readLineAsync('덧셈할 문자열을 입력해 주세요.', (num) => {
+      console.log(`숫자: ${num}`);
+    });
   }
-}
-
-let APP = new App();
-APP.run();
 
 
+  async run() {
+    await this.#startCalculator();
+  }
+};
 
 export default App;
