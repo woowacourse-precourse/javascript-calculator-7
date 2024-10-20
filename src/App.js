db13a4a01279ebc59de3237421bf7b42cb3e8566
jiyun1006 <jiyun4251@gmail.com>
2024-10-20 15:43:20 +0900

    ♻️ refactor : 입력문 코드 수정
    입력을 받아올 때, 불필요한 try ... catch 문 삭제

--- src/App.js
+++ src/App.js
@@ -9,37 +9,33 @@ class App {
 }
 
 async function getInputStr() {
-  try {
-    let inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const separator = [":", ","];
-    let customBoolean = false;
-    // 커스텀 구분자를 가지는지 확인.
-    customBoolean = isCustom(inputStr);
+  let inputStr = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+  const separator = [":", ","];
+  let customBoolean = false;
+  // 커스텀 구분자를 가지는지 확인.
+  customBoolean = isCustom(inputStr);
 
-    if (customBoolean) { // 커스텀 구분자를 가졌을 때,
-      const tmpSep = inputStr.slice(2, 3); // 커스텀 구분자
-      inputStr = inputStr.slice(5,); // 커스텀 구분자를 제외한 문자열
-      separator.push(tmpSep);
-    }
-
-    // 구분자를 이용한 분리
-    separator.forEach((sep) => {
-      inputStr = inputStr.split(sep).join('');
-    })
-    inputStr = inputStr.split('').map((el) => +el);
-
-    // 분리된 문자열에서 조건에 맞는지 검증
-    isNumber(inputStr);
-
-    const sum = inputStr.reduce((acc, cur) => {
-      return acc + cur;
-    }, 0);
-    // 구분자를 이용해 문자열 구분.
-    return sum;
-
-  } catch (error) {
-    throw new Error(error);
+  if (customBoolean) { // 커스텀 구분자를 가졌을 때,
+    const tmpSep = inputStr.slice(2, 3); // 커스텀 구분자
+    inputStr = inputStr.slice(5,); // 커스텀 구분자를 제외한 문자열
+    separator.push(tmpSep);
   }
+
+  // 구분자를 이용한 분리
+  separator.forEach((sep) => {
+    inputStr = inputStr.split(sep).join('');
+  })
+  inputStr = inputStr.split('').map((el) => +el);
+
+  // 분리된 문자열에서 조건에 맞는지 검증
+  isNumber(inputStr);
+
+  const sum = inputStr.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  // 구분자를 이용해 문자열 구분.
+  return sum;
+
 }
 
 // 입력문에서의 에러 상황 체크
@@ -50,7 +46,7 @@ const isCustom = (inputStr) => {
   const regExp2 = /(\/\/\D{1}\\n){1}.{0,}/;
   if (regExp.test(inputStr)) { // 커스텀 구분자의 형식을 가진 문자열 이라면,
     if (!regExp2.test(inputStr)) {
-      throw new Error("[ERROR]");
+      throw new Error("[ERROR] 커스텀 구분자의 형식이 잘못됐습니다.");
     }
     return true;
   }
@@ -61,7 +57,7 @@ const isCustom = (inputStr) => {
 // 2. 문자열이 숫자인지, 양수인지
 const isNumber = (inputStr) => {
   for (let i = 0; i < inputStr.length; i += 1){
-    if (isNaN(inputStr[i])) throw new Error("[ERROR]");
+    if (isNaN(inputStr[i])) throw new Error("[ERROR] 문자열 중 숫자가 아니거나 양수가 아닌 문자가 있습니다.");
   }
 }
 
