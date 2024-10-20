699a8acc13ff2796bc02eb330563d7214fc7e83b
은석 <modelism93@naver.com>
2024-10-20 16:43:45 +0900

    feat: 쉼표(,), 콜른(:) 구분자로 숫자합 반환 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    const result = await this.calculateSum(input);
+
+    Console.print(`결과: ${result}`);
+  }
+
+  // input에 따른 계산
+  calculateSum(input) {
+    if (!input) {
+      return 0;
+    }
+
+    const numbers = input.split(/,|:/).map((num) => Number(num));
+    let sum = 0;
+
+    numbers.forEach((num) => {
+      sum += num;
+    });
+
+    return sum;
+  }
 }
 
 export default App;
