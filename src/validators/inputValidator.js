53ca8043dd85c15e2176509c6c223870eaeb086a
silver0108 <kimsoeun0108@gmail.com>
2024-10-20 23:05:58 +0900

    feat: 유효성 검증 - 입력값에 음수가 있는 경우

--- src/validators/inputValidator.js
+++ src/validators/inputValidator.js
@@ -5,3 +5,8 @@ export const emptyInput = (input) => {
   if (!input) throwError("input is not found");
   return input;
 };
+
+export const negativeInput = (input) => {
+  if (/-\d/.test(input)) throwError("input is nagative number");
+  return input;
+};
