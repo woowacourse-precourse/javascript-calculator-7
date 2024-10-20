39eee8db9bb661597f1b018b5e239902de73ef19
Ray <rlawlsdn9583@gmail.com>
2024-10-21 01:32:01 +0900

    feat: ConsoleAPI를 이용하여 문자열 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-class App {
-  async run() {}
-}
+import { Console } from "@woowacourse/mission-utils";
 
+class App {
+  constructor(){
+    this.defaultSeparator = [',', ':'];
+  }
+  
+  async run() {
+      Console.print('덧셈할 문자열을 입력해주세요.');
+      const INPUT_DATA = await Console.readLineAsync("");
+  };
+}
 export default App;
