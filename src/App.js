8f4fec956cb292b72b27f0c61f9a5882d9085271
Kim_Yjoo <pas4976@gmail.com>
2024-10-21 04:04:32 +0900

    fix(test error): 유저 입력 문자열에 .includes() 함수 사용시 정의되지 않은 함수 에러 발생 오류, 테스트 진행 시 정상 작동을 확인 했지만 구분자 에러를 발생시키는 오류 해결
    - 첫 번째 오류 : 유저 입력을 바로 Console.readLineAsync 에서 받아왔던 이전 코드에서 String()으로 명시적 형변환을 통해 확실한 문자열로 변경시키는 과정을 추가했다. 이후 메서드 정상작동 확인
    
    - 두 번째 오류 : 문제 요구사항에 있지 않은 무한 입력을 구현 했지만, 이 기능을 없애니 테스트 정상 작동 확인 완료

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ class App {
     
     let userInput;
     let result;
-    while(true){
-      let spliter = new Set([",",":"]);
-      userInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    let spliter = new Set([",",":"]);
+    userInput = String(await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n'));
 
-      if(!this.isEmpty(userInput)) {
-        userInput = this.checkCustomSpliter(userInput, spliter);
-        this.doSum(userInput, spliter);
-      }
+    if(!this.isInputEmpty(userInput)) {
+      userInput = this.checkCustomSpliter(userInput, spliter);
 
+      this.doSum(userInput, spliter);
     }
+
+
 
   }
 
-  isEmpty(userInput) {
+  isInputEmpty(userInput) {
     return userInput === '';
   }
 
   checkCustomSpliter(userInput, spliter) {
+
     if(userInput.includes("//")){
+
       let indexOfEnd = userInput.indexOf('\\n');
       if(indexOfEnd === -1) {
         throw new Error("[ERROR] 잘못된 문장. (커스텀 문장 추가 에러)"); 
@@ -40,14 +42,11 @@ class App {
     let result = 0;
     let tmp = "";
     for(let idx of userInput) {
-      
-      
       if(!isNaN(idx)) {
         tmp += idx;
       }
       else{
         if(!spliter.has(idx)) {
-          Console.print(idx in spliter);
           throw new Error("[ERROR] 잘못된 문장. (구분자가 아닌 문자 사용)");
         } 
         result += Number(tmp)
@@ -58,6 +57,7 @@ class App {
     result += Number(tmp);
     Console.print("결과 : " + result);
   }
+
 }
 
 export default App;
