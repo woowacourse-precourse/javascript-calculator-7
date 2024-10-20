5acdcf4989e9f87c25fc190646eb614aeda4fdea
soulchicken <soulfever01@naver.com>
2024-10-20 19:25:29 +0900

    feat: 커스텀 구분자 예외처리

--- src/divider.js
+++ src/divider.js
@@ -4,9 +4,11 @@ export function extractDividerProcess(input, divider) {
     if (extractedString === "") {
       break;
     }
+    repetitionCheck(divider);
     divider.push(extractedString);
     input = newInput;
   }
+
   return { updatedInput: input, updatedDivider: divider };
 }
 
@@ -18,9 +20,32 @@ function extractCustomDvider(input) {
   if (startComment == 0 && endComment !== -1) {
     const extractedString = input.substring(startComment + 2, endComment).trim();
     const newInput = input.substring(endComment + 1);
+
+    validateDivider(extractedString);
+
     return { extractedString, newInput };
   }
   
   return { extractedString: "", newInput: input };
 }
 
+function validateDivider(divider) {
+  if (divider === "") {
+    throw new Error("[ERROR] 커스텀 구분자에 문자열이 없습니다.");
+  }
+  if (divider[0] === '-' && divider.length > 1 && !isNaN(divider[1])) {
+    throw new Error("[ERROR] 커스텀 구분자가 음수로 시작되고 있습니다.");
+  }
+
+  if (!isNaN(divider[0])) {
+    throw new Error("[ERROR] 커스텀 구분자가 숫자로 시작되고 있습니다.");
+  }
+  return;
+}
+
+function repetitionCheck(divider, extractedString) {
+  if (divider.includes(extractedString)) {
+    throw new Error("[ERROR] 구분자가 중복되었습니다.");
+  }
+  return;
+}
