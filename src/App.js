f80f610e49153780637ff341a852056eaf0f22dd
laire7 <syoungkwak@gmail.com>
2024-10-20 15:52:25 +0900

    feat(): 0. separates user input by semicolon(:) or (,)
    Change variable names to match Javascript Style Guide

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ class App {
   async run() {
     MissionUtils.Console.readLine(
       "덧셈할 문자열을 입력해 주세요.\n",
-      (answer) => {
-        console.log(`결과: ${answer}`);
+      (INPUT) => {
+        var NUM_LIST = [];
+        NUM_LIST = INPUT.split(":");
+        console.log(`결과: ${NUM_LIST}`);
       }
     );
   }
