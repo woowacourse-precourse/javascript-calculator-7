a8710f7e4040a3efcb3ca542da596fdfc75fdb4f
kimdongyoung <wwweric12@naver.com>
2024-10-20 16:41:46 +0900

    feat(parseIntArray): String배열 Number배열로 변환

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import getString from './utils/getString';
+import parseIntArray from './utils/parseIntArray';
 import splitBySymbol from './utils/splitBySymbol';
 
 class App {
   async run() {
     const inputValue = await getString();
-    const seperatedValue = splitBySymbol(inputValue)
+    const seperatedValue = splitBySymbol(inputValue);
+    const intArray = parseIntArray(seperatedValue);
+
   }
 }
 
