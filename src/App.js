e3e7ecf1477a03bf81a7276903920283b5e36501
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-10-20 22:36:31 +0900

    refactor: 숫자 덧셈 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Console } from "@woowacourse/mission-utils";
 import { GAME_MESSAGES } from "./constants/gameMessages.js";
 import splitNumber from "./utils/splitNumber.js";
 import validateNumber from "./utils/validateNumber.js";
 import getUserInput from "./utils/inputHandler.js";
 import printMessage from "./utils/outputHandler.js";
+import sumNumber from "./utils/sumNumber.js";
 
 class App {
   async run() {
     const userInput = await getUserInput(GAME_MESSAGES.START);
 
     const numbers = splitNumber(userInput).map(validateNumber);
-    const result = numbers.reduce((acc, cur) => acc + cur, 0);
+    const result = sumNumber(numbers);
 
     printMessage(GAME_MESSAGES.END + result);
   }
---
+++ src/utils/sumNumber.js
@@ -0,0 +1,5 @@
+const sumNumber = (numbers) => {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+};
+
+export default sumNumber;
