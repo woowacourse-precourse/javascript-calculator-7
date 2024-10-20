bfe9ceec8d2b0719ebe7fbcd50cf4ec79a2a70ba
“HYUNJIN105” <hyunjin.011005@gmail.com>
2024-10-21 00:44:12 +0900

    feat: 음수 입력값 검사 기능 추가

--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,13 @@
 export default function validator(input) {
   const is_Custom = /^\/\/(.*)\\n/;
-
+  const negative_num = /-\d+/;
+  if(negative_num.test(input)){
+    throw new Error('[ERROR]');
+  }
   if (!is_Custom.test(input)) {
     const default_pattern = /^[0-9,:\n]+$/;
     if (!default_pattern.test(input)) {
-      throw new Error('[ERROR] 잘못된 입력 형식.');
+      throw new Error('[ERROR]');
     } else {
       return input;
     }
@@ -18,7 +21,7 @@ export default function validator(input) {
       const new_pattern = new RegExp(`^[0-9${custom_delimiter},:\n]+$`);
 
       if (!new_pattern.test(Exp)) {
-        throw new Error('[ERROR] 잘못된 입력 형식.');
+        throw new Error('[ERROR]');
       } else {
         return Exp;
       }
