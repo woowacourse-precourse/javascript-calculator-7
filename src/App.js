52dd92429bbdfd74ac305ee62a45c956409c3ee5
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-10-20 18:35:15 +0900

    refactor : 코드 개선

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  
   async run() {
-    //기본연산자
     const basicOperators = [",",":"];
     let inputStr = String(await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n"));
-    //커스텀구분자 탐지
     if (isCustom(inputStr)){
-      // \n 탐지 후 스플릿  
       addCustomOperator(inputStr,basicOperators);
       inputStr = extractCustomString(inputStr);
     }
-    console.log(inputStr);
-    //문자열에서 기본연산자 추출 => 기댓값 : [1,2,3]
-    const nums = extractNums(inputStr,basicOperators);
-    Console.print(`결과 : ${calculateSum(nums)}`);
+    Console.print(`결과 : ${calculateSum(extractNums(inputStr,basicOperators))}`);
   }
 }
 
@@ -25,12 +18,10 @@ function isCustom(inputStr){
 
 function addCustomOperator(inputStr,basicOperators){
   let customOperator = "";
-  if (inputStr.includes("\\n")){
-    customOperator = inputStr.split("\\n")[0].replace("//","");
-  }else{
+  if (!inputStr.includes("\\n")){
     throw Error("[ERROR] : 커스텀 구분자 탐지에 실패했습니다.");
   }
-  console.log(`커스텀 문자는 ${customOperator}입니다.`);
+  customOperator = inputStr.split("\\n")[0].replace("//","");
   inspectCustomSeperator(customOperator);
   basicOperators.push(customOperator);
 }
