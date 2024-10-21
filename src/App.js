775ba7cea916ede96e08c902af5a46d7c2c2cd62
songhaeunsong <gkdms3469@naver.com>
2024-10-21 14:13:33 +0900

    feat: 구분자와 문자열 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { Console } from "@woowacourse/mission-utils";
 import Validator from "./Validator.js";
 class App {
-  async run() {}
+  async run() {
+    const input = await this.getString();
+    if (!input.length) return this.printAnswer(0);
+
+    const [separators, numberString] = this.getSeparatorAndNumber(input);
+  }
 
   getString() {
     return Console.readLineAsync(`덧셈할 문자열을 입력해 주세요.\n`);
   }
-  getSeparatorAndNumber() {}
-  findCustomSeparator() {}
+  getSeparatorAndNumber(str) {
+    const separators = [",", ":"];
+    if (str.startsWith("//")) {
+      const customSeparator = this.findCustomSeparator(str);
+      if (customSeparator === null) return [null, str];
+      separators.push(customSeparator);
+      return [separators, str.substr(5)];
+    }
+    return [separators, `${str}`];
+  }
+
+  findCustomSeparator(str) {
+    if (str[3] === "\\" && str[4] === "n") return str[2];
+    return null;
+  }
   changeToRegex() {}
   getNumberCutWithSeparator() {}
   calculateSum() {}
