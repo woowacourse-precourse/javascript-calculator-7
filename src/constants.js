697abe0bc6c2abe3ae684a6b2eeaba061a6a3036
seonae-j <wkdtjsdo135@naver.com>
2024-10-21 17:09:15 +0900

    docs: 예외 처리 확인 및 예외 요구사항 추가
    - 커스텀에서 허용하지 않는 구분자를 사용한 경우 checkde
    - 커스텀 구분자 연속으로 사용 시 ERROR 출력 요구사항 추가

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ export const ERROR = {
   NEGATIVE_NUMBER_ERROR: '[ERROR] 음수는 사용할 수 없습니다.',
   SEPARATOR_ERROR: '[ERROR] 허용되지 않은 구분자가 포함되어 있습니다.',
   CUSTOM_SEPERATE_ERROR: '[ERROR] 커스텀 구분자 생성 기호가 올바르지 않습니다.',
+  CUSTOM_SEPERATE_N_ERROR: '[ERROR] 커스텀 구분자 생성 기호에 n이 없습니다.',
   CUSTOM_SEPERATOR_ERROR: '[ERROR] 숫자는 커스텀 구분자로 사용될 수 없습니다.',
   CUSTOM_ORDER_ERROR: '[ERROR] 커스텀 구분자 생성 순서가 올바르지 않습니다.',
   CUSTOM_INPUT_ERROR:
