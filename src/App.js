867cc41c0df6fb1f229b4d22b405954262260bab
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2024-10-21 23:14:42 +0900

    feat: 문자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await this.getUserInput();
+  }
+
+  async getUserInput() {
+    const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    return input;
+  }
 }
 
 export default App;
