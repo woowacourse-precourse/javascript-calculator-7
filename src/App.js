997b6f2b49299a76a68993d3646d98b7523b733f
Yarrly <74090200+eunsoA@users.noreply.github.com>
2024-10-21 16:46:19 +0900

    style : readLineAsync시 \n 추가

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ class App {
     try {
       // [feat] 1. 사용자 입력
       const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
     } catch (error) {
       Console.print(error.message);
     }
   }
+
+
 }
 
 export default App;
