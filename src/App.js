0ff0db7b0d02912468c0aad013de00cd8a6456d0
OhSeChan <seb_s19@naver.com>
2024-10-21 22:03:52 +0900

    feat(App): 사용자로부터 입력 형식에 맞춰 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from './constant.js';
+
 class App {
-  async run() {}
+  async getInput() {
+    return await Console.readLineAsync(`${MESSAGE.INPUT}`);
+  }
+
+  async run() {
+    const input = await this.getInput();
+  }
 }
 
 export default App;
