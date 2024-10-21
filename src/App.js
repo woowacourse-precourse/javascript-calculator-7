b8dd84345f85231d211210c3dcf169af44bd774f
hwiy01 <vdocom02@naver.com>
2024-10-21 17:34:45 +0900

    fix: 입력 없을 경우 0반환하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
 
-// 숫자 리스트를 받아 합을 구해 반환하는 함수
 function adder(numList) {
   let sum = 0;
   for(let i = 0; i < numList.length; i++){
@@ -12,54 +11,49 @@ function adder(numList) {
   return sum;
 }
 
-// 기본 구분자 (,과 /)로 str을 구분하여 더하는 함수
-function divideWithDefault(INPUT){
+function divideWithDefault(INPUT) {
   let strList = INPUT.split(/[, :]/);
   
-  if(strList.some(element => isNaN(element))){
+  if (strList.some(element => isNaN(element))) {
     Console.print("[ERROR] 구분자 사이에는 숫자만 입력 가능합니다.");
     return;
   }
-  else{
-    let numList = strList.map(element => {
-      return parseInt(element, 10);
-  });
-  if(numList.some(element=> element <= 0)){
+
+  let numList = strList.map(element => parseInt(element, 10));
+  
+  if (numList.some(element => element <= 0)) {
     Console.print("[ERROR] 양수 값을 입력해주세요.");
     return;
   }
+
   let sum = adder(numList);
   Console.print(`결과 : ${sum}`);
-  }
 }
 
-// 커스텀 구분자 추출하여 커스텀 구분자로 구분하여 더하는 함수
-function divideWithCustom(INPUT){
-  if(!INPUT.includes('\\n')){
+function divideWithCustom(INPUT) {
+  if (!INPUT.includes('\\n')) {
     Console.print("[ERROR] 올바른 형식이 아닙니다.");
     return;
   }
-  // //와 문자열 \n 사이의 캡처 그룹을 추출하여 customDelimeter에 저장
+  
   const DELIMETER = INPUT.match(/\/\/(.+)\\n/)[1];
 
-if(DELIMETER.match(/[0-9]/)){
+  if (DELIMETER.match(/[0-9]/)) {
     Console.print("[ERROR] 구분자로 숫자 값은 불가능합니다.");
     return;
   }
 
-  const STR = INPUT.split("\\n")[1]; //나눌 문자
-  let strList = STR.split(DELIMETER);
+  const STR = INPUT.split('\\n')[1];
+  let strList = STR.split(new RegExp(`[${DELIMETER},:]`));
 
-  if(strList.some(element => isNaN(element))){
+  if (strList.some(element => isNaN(element))) {
     Console.print("[ERROR] 구분자 사이에는 숫자만 입력 가능합니다.");
     return;
   }
 
-  let numList = strList.map(element => {
-      return parseInt(element, 10);
-  });
+  let numList = strList.map(element => parseInt(element, 10));
 
-  if(numList.some(element=> element <= 0)){
+  if (numList.some(element => element <= 0)) {
     Console.print("[ERROR] 양수 값을 입력해주세요.");
     return;
   }
@@ -72,13 +66,13 @@ class App {
   async run() {
     const INPUT = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
 
-    if(INPUT.length == 0){
-      Console.print("[ERROR] 값을 입력하세요.");
-    }
-    else if(INPUT.startsWith('//')){
+    if (INPUT.length == 0) {
+      Console.print("결과 : 0");
+    } 
+    else if (INPUT.startsWith('//')) {
       divideWithCustom(INPUT);
-    }
-    else{
+    } 
+    else {
       divideWithDefault(INPUT);
     }
   }
