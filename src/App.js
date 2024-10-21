2d9536f7895e3b2856830ac6778f9d76c8f1bae2
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-21 18:23:41 +0900

    feat(App.js): 사용자 입력 및 프로그램 실행 로직 추가
    - 사용자의 입력을 받아 덧셈 결과를 출력,
    - 오류 발생 시 메시지를 출력을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-class App {
-  async run() {}
+const { Console } = require("@woowacourse/mission-utils");
+
+class StringCalculator {
+  run() {
+    Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n", (input) => {
+      try {
+        const result = this.add(input);
+        Console.print(`결과 : ${result}`);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    });
+  }
 }
 
 export default App;
