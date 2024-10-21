e1c5bf9b21668b61d0a622f87c9be93c3565b583
BoRamSon <onbrson1@gmail.com>
2024-10-21 23:31:32 +0900

    style: delete annotations and console.log

--- src/App.js
+++ src/App.js
@@ -2,84 +2,62 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    // [7-1][예외] try
     try {
-      // [1] 사용자 입력 받기, 앞 뒤 공백제거
       let inputValue = String(
         await Console.readLineAsync("덧셈 계산을 위한 문자열을 입력해주세요 : ")
       ).trim();
-      console.log(
-        `xxxxx(확인용 - 최초 입력값 = ${inputValue}) / type : ${typeof inputValue}`
-      );
 
-      // [7-3][ERROR] 입력값에 대한 예외사항 처리하기
-      // 1. 입력된 값이 ''라면, 0을 결과로 출력하고 종료.
       if (!inputValue) {
         Console.print(`결과 : ${0}`);
         return;
-        // 2. 구분자가 먼저 입력되었을 경우 에러.
       } else if (
-        inputValue.split('')[0] === ';' ||
-        inputValue.split('')[0] === ',' ||
-        inputValue.split('')[0] === ':' ||
-        inputValue.split('')[0] === '|'
+        inputValue.split("")[0] === ";" ||
+        inputValue.split("")[0] === "," ||
+        inputValue.split("")[0] === ":" ||
+        inputValue.split("")[0] === "|"
       ) {
-        throw Error('[ERROR] 구분자가 먼저 입력될 수 없습니다.');
-        // 3. 커스텀구분자도 아니고, 숫자와 구분자로 이루어진 일반구분자도 아니라면 에러.
+        throw Error("[ERROR] 구분자가 먼저 입력될 수 없습니다.");
       } else if (
-        !inputValue.startsWith('//') &&
+        !inputValue.startsWith("//") &&
         !/^[\d,|;:\- ]+$/.test(inputValue)
       ) {
         throw Error(
-          '[ERROR] 잘못된 입력입니다. 숫자와 구분자를 정확하게 입력해주세요.'
+          "[ERROR] 잘못된 입력입니다. 숫자와 구분자를 정확하게 입력해주세요."
         );
-        // 4. 구분자가 2번이상 연속으로 반복된다면 에러.
       } else if (/[,|;:]{2,}/.test(inputValue) || /[ ]{3,}/.test(inputValue)) {
         throw Error(
-          '[ERROR] 구분자를 2번이상 연속으로 반복하여 입력할 수 없습니다.'
+          "[ERROR] 구분자를 2번이상 연속으로 반복하여 입력할 수 없습니다."
         );
-        // 5. 0 이하의 음수인 경우 에러.
       } else {
         const allNumbers = /^\/\//.test(inputValue)
-          ? inputValue.split('\\n')[1].split(/[,|;: ]+/)
+          ? inputValue.split("\\n")[1].split(/[,|;: ]+/)
           : inputValue.split(/[,|;: ]+/);
         allNumbers.map((numStr) => {
           const num = Number(numStr);
           if (num < 0 || isNaN(num)) {
-            throw Error('[ERROR] 음수를 입력할 수 없습니다.');
+            throw Error("[ERROR] 음수를 입력할 수 없습니다.");
           }
         });
       }
 
-      // [2] 커스텀구분자 여부를 확인
       const CUSTOM_PATTERN = /^\/\//;
       const isCustom = CUSTOM_PATTERN.test(inputValue);
-      console.log(`xxxxx(확인용 - 커스텀 구분자인가요? : ${isCustom})`);
 
-      // [3] 커스텀구분자를 처리하는 함수 만들기
       const extractCustomDelimiter = (inputValue) => {
         const inputSplit = inputValue.split("\\n");
         const customDelimiter = inputSplit[0].replace("//", "");
         const customDelimiterNums = inputSplit[1];
         const customStringArray = customDelimiterNums.split(customDelimiter);
         const customNumbersArray = customStringArray.map(Number);
-        console.log(
-          `xxxxx(확인용 - 커스텀구분자 숫자배열 추출여부 : ${customNumbersArray})`
-        );
         return customNumbersArray;
       };
 
-      // [4] 일반구분자를 처리하는 함수 만들기
       const extractGeneralDelimiter = (inputValue) => {
         const generalStringArray = inputValue.split(/[,|;: ]+/);
         const generalNumbersArray = generalStringArray.map(Number);
-        console.log(
-          `xxxxx(확인용 - 일반구분자 숫자배열 추출여부 : ${generalNumbersArray})`
-        );
         return generalNumbersArray;
       };
 
-      // [5] 덧셈 계산 함수 만들기
       const sum = (numbersArray) => {
         return numbersArray.reduce(
           (accumulator, currentValue) => accumulator + currentValue,
@@ -87,24 +65,15 @@ class App {
         );
       };
 
-      // [6][동작] 구분자를 기준으로 숫자를 추출하고 숫자 모두 더하기
       if (isCustom) {
-        console.log(`xxxxx(확인용 - 커스텀구분자 if문 들어감`);
         const customNumbersArray = extractCustomDelimiter(inputValue);
         const customResult = sum(customNumbersArray);
-        console.log(
-          `xxxxx(확인용 - 커스텀 구분자 최종 결과 : ${customResult})`
-        );
         Console.print(`결과 : ${customResult}`);
       } else {
-        console.log(`xxxxx(확인용 - 일반구분자 if문 들어감`);
         const generalNumbersArray = extractGeneralDelimiter(inputValue);
         const generalResult = sum(generalNumbersArray);
-        console.log(`xxxxx(확인용 - 일반 구분자 최종 결과 : ${generalResult})`);
         Console.print(`결과 : ${generalResult}`);
       }
-
-      // [7-2][예외] catch
     } catch (error) {
       Console.print(error.message);
       throw error;
