ed621345a0d7470561101fa560e2b81efad7553c
SuJin <kimsujin001127@gmail.com>
2024-10-21 19:38:59 +0900

    feat: 쉼표와 콜론으로 숫자 구분 빛 합 반환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    function getArrSum(arr) {
+      return arr.reduce(
+        (accumulator, currentValue) =>
+          Number(accumulator) + Number(currentValue),
+        0
+      );
+    }
+
+    async function getUsername() {
+      try {
+        const str = await Console.readLineAsync(
+          "덧셈할 문자열을 입력해 주세요.\n"
+        );
+        let sum = 0;
+          const numArr = str.split(/[,: ]/);
+          sum = getArrSum(numArr);
+        Console.print("결과 : " + sum);
+      } catch (error) {
+        Console.print("에러가 발생했습니다: ", error);
+      }
+    }
+
+    getUsername();
+  }
 }
 
 export default App;
