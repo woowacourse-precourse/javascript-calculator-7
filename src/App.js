550fe15c39fe724cae36db51fbb2697f1107cb5f
main <0ksuyeon0@gmail.com>
2024-10-21 16:39:44 +0900

    fix: add exception and error handling for string to number conversion and calculations

--- src/App.js
+++ src/App.js
@@ -30,9 +30,10 @@ class App {
     const withoutCustomDelimiter = input.slice(endDelimiter + 2);
 
     // 커스텀 구분자가 공백이거나 숫자인 경우
-    if (!customDelimiter || Number(customDelimiter) === isNaN) {
-      throw "커스텀 구분자는 공백이나 숫자가 될 수 없습니다.";
-    }
+    if (!customDelimiter) throw "커스텀 구분자는 공백이 될 수 없습니다.";
+
+    if (typeof customDelimiter === Number)
+      throw "커스텀 구분자는 숫자가 될 수 없습니다.";
 
     // 커스텀 구분자를 제외한 문자열이 공백인 경우
     if (!withoutCustomDelimiter) {
@@ -48,7 +49,7 @@ class App {
       .split(customDelimiter)
       .filter((el) => el !== "");
 
-    Console.print(onlyInput);
+    if (onlyInput === "") throw "계산할 값이 적어도 하나는 존재해야 합니다.";
 
     this.validateStringInput(onlyInput);
   }
@@ -57,7 +58,7 @@ class App {
     // 기본 구분자
     const withoutCommaInput = input.replace(",", ":");
     const onlyInput = withoutCommaInput.split(":");
-    Console.print(onlyInput);
+    if (onlyInput === "") throw "계산할 값이 적어도 하나는 존재해야 합니다.";
 
     this.validateStringInput(onlyInput);
   }
@@ -66,6 +67,11 @@ class App {
   validateStringInput(onlyInput) {
     // 변수.map((el) => typeof Number(변수) === number);
     const numberInput = onlyInput.map((e) => Number(e));
+
+    for (let i = 0; i < numberInput.length; i++) {
+      if (isNaN(numberInput[i])) throw "계산은 숫자만 가능합니다.";
+      else if (numberInput[i] <= 0) throw "계산은 양수만 가능합니다.";
+    }
 
     this.sumPositiveNumber(numberInput);
   }
