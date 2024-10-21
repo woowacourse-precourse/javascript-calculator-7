9149c7da8e18e581c08d9029bcc3a1c93f3005f7
Eun0713 <daeunlee0713@naver.com>
2024-10-21 21:58:46 +0900

    feat: Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-class App {
-  async run() {}
-}
+import readline from 'readline';
 
-export default App;
+class App {
+    async run() {
+        this.calculate();
+    }
+
+    async calculate() {
+        const input = await this.readInput();
+        try {
+            const result = this.addNumbers(input);
+            console.log(`결과 : ${result}`);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    readInput() {
+        return new Promise((resolve) => {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+            });
+            rl.question("덧셈할 문자열을 입력해 주세요:\n", (input) => {
+                rl.close();
+                resolve(input);
+            });
+        });
+    }
