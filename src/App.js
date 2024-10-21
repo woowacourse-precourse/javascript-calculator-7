721f23b731a2cbe0de3f1f698fe61b1a6828c287
vgotu99 <vgotu99@gmail.com>
2024-10-21 12:55:49 +0900

    fix: 커스텀 구분자 처리 로직 개선, 정규식 수정

--- src/App.js
+++ src/App.js
@@ -2,54 +2,49 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
-    this.CUSTOM_DELIMITER_REGEX = /^\/\/(.+)\n(.*)/; // 커스텀 구분자를 다중 문자로 변경
+    this.CUSTOM_DELIMITER_REGEX = /^\/\/(.)\\n(.*)$/;
     this.DEFAULT_DELIMITER = ",|:";
     this.ERROR_PREFIX = "[ERROR]";
   }
 
   async run() {
-    const INPUT = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
     try {
-      const RESULT = this.calculate(INPUT);
-      Console.print(`결과 : ${RESULT}`);
+      const result = this.calculate(input);
+      Console.print(`결과 : ${result}`);
     } catch (error) {
       Console.print(error.message);
     }
   }
 
   calculate(input) {
-    const { DELIMITER, NUMBERS } = this.parseInput(input);
-    const PARSED_NUMBERS = this.splitAndParse(NUMBERS, DELIMITER);
-    this.validateNumbers(PARSED_NUMBERS);
-    return this.sum(PARSED_NUMBERS);
+    const { delimiter, numbers } = this.parseInput(input);
+    const parsedNumbers = this.splitAndParse(numbers, delimiter);
+    this.validateNumbers(parsedNumbers);
+    return this.sum(parsedNumbers);
   }
 
   parseInput(input) {
-    const MATCH = input.match(this.CUSTOM_DELIMITER_REGEX);
+    const match = input.match(this.CUSTOM_DELIMITER_REGEX);
 
-    if (MATCH) {
-      return { DELIMITER: this.escapeDelimiter(MATCH[1]), NUMBERS: MATCH[2] };
+    if (match) {
+      return { delimiter: this.escapeDelimiter(match[1]), numbers: match[2] };
     }
 
-    return { DELIMITER: this.DEFAULT_DELIMITER, NUMBERS: input };
+    return { delimiter: this.DEFAULT_DELIMITER, numbers: input };
   }
 
   escapeDelimiter(delimiter) {
-    // 정규표현식에서 특수문자를 구분자로 사용할 때 문제가 생길 수 있으므로 이스케이프 처리
     return delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
 
   splitAndParse(numbers, delimiter) {
     return numbers.split(new RegExp(delimiter)).map((num) => {
-      const PARSED = parseInt(num.trim(), 10);
-      if (isNaN(PARSED)) {
-        throw new Error(
-          `${this.ERROR_PREFIX} 유효하지 않은 입력값입니다: ${num}`
-        );
+      const parsed = parseInt(num.trim(), 10);
+      if (isNaN(parsed)) {
+        throw new Error(`${this.ERROR_PREFIX} 유효하지 않은 입력값입니다: ${num}`);
       }
-      return PARSED;
+      return parsed;
     });
   }
 
@@ -60,7 +55,7 @@ class App {
   }
 
   sum(numbers) {
-    return numbers.reduce((ACC, CURR) => ACC + CURR, 0);
+    return numbers.reduce((acc, curr) => acc + curr, 0);
   }
 }
 
