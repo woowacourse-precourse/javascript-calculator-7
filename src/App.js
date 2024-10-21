cd8affc1a08b65e085ee3ecc9ef0bff92059ba9e
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-21 15:53:22 +0900

    feat(App): 시작 메시지 출력 및 사용자 값 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+// Console.readLineAsync() / Console.print()
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+
+  }
 }
 
 export default App;
