20d645d4e6b67629f09830101b8f2e0c1b078392
yoouung <jyp101400@gmail.com>
2024-10-21 20:47:16 +0900

    feat: 문자열 덧셈 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
+    const result = this.processInput(input);
+
+    Console.print(`결과: ${result}`);
+  }
+
+  processInput(input) {
+    const numbers = this.extractNumbers(input);
+    const sum = this.calculateSum(numbers);
+    return sum;
+  }
+
+  extractNumbers(input) {
+    const numbers = input.split(/[, :]/).map((str) => parseInt(str, 10));
+    return numbers;
+  }
+
+  calculateSum(numbers) {
+    return numbers.reduce((acc, cur) => acc + cur, 0);
+  }
 }
 
 export default App;
