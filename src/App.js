15967cfa75a4947ae11b3bd1150e02491e800b7b
PangKiYeon <pdrkeamy@gmail.com>
2024-10-21 17:54:09 +0900

    feat (App.js): 커스텀 구분자도 존재할 때 각 숫자를 합하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
+import { awareCustomSeprator, extractNumbers } from './calculator.js';
 
 class App {
     async run() {
-        const input = await Console.readLineAsync(
+        let distinct = [',', ':'];
+
+        let input = await Console.readLineAsync(
             '덧셈할 문자열을 입력해 주세요.\n'
         );
+
+        if (input[0] == '/') {
+            [distinct, input] = awareCustomSeprator(input, distinct);
+        }
+
+        const result = extractNumbers(input, distinct);
     }
 }
 
