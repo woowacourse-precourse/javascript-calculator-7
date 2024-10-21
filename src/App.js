1599cbadf4c60bb34522bc7c84741ca034bbabe9
deokisys <sjb378@me.com>
2024-10-21 22:16:50 +0900

    [feat] 커스텀 구분자 등록

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
+    let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
     let delimiters = [",", ":"]; //구분자
 
+    //추가 커스텀 구분자확인
+    //앞의 두문자가 //인지 확인
+    if (input.indexOf("//") === 0) {
+      //\n의 위치 확인
+      let endIdx = input.indexOf("\\n");
+
+      //두 사이의 문자를 추출한다.
+      const newDelimiter = input.slice(2, endIdx);
+      //새로운 구분자로 추가
+      delimiters.push(newDelimiter);
+
+      //입력값을 구분자 부분 제거하여 진행
+      input = input.slice(endIdx + 2);
+    }
     //구분자들을 정규표현식을 이용하도록 조합
     const regex = new RegExp(`[${delimiters.join("")}]`);
     const result = input.split(regex).map(Number); //
