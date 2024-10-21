56f344dccd0a1ff7efc409c595d90dff74ee08e4
magik99 <hojun0810@naver.com>
2024-10-21 17:10:05 +0900

    feat(extractNum.js): 커스텀 구분자 기능 구현
    '//' 'n' 사이에 있는 문자를 구분자로 사용하는 커스텀 구분자 기능을 구현

--- src/extractNum.js
+++ src/extractNum.js
@@ -1,5 +1,21 @@
 const extractNum = (input) => {
-  const splitInput = input.split(/[,:]/);
+  let delimiter = /[,:]/;
+
+  if (input.startsWith('//')) {
+    const delimiterEnd = input.indexOf('n');
+    if (delimiterEnd !== -1) {
+      const customDelimiter = input.slice(2, delimiterEnd);
+      delimiter = new RegExp(`[${customDelimiter}]`);
+
+      input = input.slice(delimiterEnd + 1);
+    } else {
+      // '//'로 시작하지만 n으로 끝나지 않기 때문에 throw error
+    }
+  }
+
+  const splitInput = input.split(delimiter);
+
+  // 숫자인지 체크하고 아니면 throw error
 
   return splitInput.map(Number);
 };
