9a16a8d65fcbf605249573a40904b7a3c15cd6c2
Junseo Kim <semiwest2@gmail.com>
2024-10-21 21:36:38 +0900

    refactor: 결과 출력 함수에서 불필요 코드 제거
    - 항상 숫자라는 전제하에 유효성 검사 코드를 제거했습니다.

--- src/output/output.js
+++ src/output/output.js
@@ -1,10 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 export default function printResult(output) {
-  if (!isNaN(output)) {
-    MissionUtils.Console.print(`결과 : ${output}`);
-  } else {
-    //에러메세지 출력
-    MissionUtils.Console.print(output);
-  }
+  MissionUtils.Console.print(`결과 : ${output}`);
 }
