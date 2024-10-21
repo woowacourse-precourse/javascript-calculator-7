ff57febb533ba66911c1b3b2ce6108bb13e06ba6
wiseah <hyunah2765@gmail.com>
2024-10-21 17:48:05 +0900

    feat(App.js): add validation for negative or non-numeric inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ class App {
   async run() {
     Console.print("덧셈할 문자열을 입력해주세요.");
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.: ");
+    const reg = input.match(/\/\/(.*?)\\n(.*)/);
+    const result = reg ? reg[2].split(new RegExp(`[,:${reg[1]}]`)) : input.split(/[,:]/);
+
+    result.map((value) => {
+      const num = Number(value);
+      if (isNaN(num) || num <= 0) throw new Error("[ERROR]");
+    });
   }
 }
 
