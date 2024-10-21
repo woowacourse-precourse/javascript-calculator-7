718dcd3b63c6f99e27ca1590e81814f44195703b
Minho Kim <wweer4748@naver.com>
2024-10-21 20:55:50 +0900

    숫자의 합 기능

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App {
   run() {
     return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
       .then((input) => {
-        const numbers = this.parseNumbers(input);
-        Console.print(입력된 숫자들 : ${ numbers });
+        const result = this.calculate(input);
+        Console.print(`결과 : ${result}`);
       })
       .catch((error) => {
         Console.print(error.message);
@@ -16,11 +16,23 @@ class App {
       });
   }
 
+  calculate(input) {
+    const numbers = this.parseNumbers(input);
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
   parseNumbers(input) {
     let delimiters = [',', ':'];
     let numbersString = input;
 
+    const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)/);
+    if (customDelimiterMatch) {
+      delimiters.push(customDelimiterMatch[1]);
+      numbersString = customDelimiterMatch[2];
+    }
+    const escapedDelimiters = delimiters.map((d) => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'));
+    const regex = new RegExp(escapedDelimiters.join('|'));
+    return numbersString.split(regex).map((num) => parseInt(num, 10));
   }
 }
 
-export default App
+export default App;
