1fc3d357ec3a9ea7697c4956dc795931e212fa3f
6v6DS <6v6DS@ajou.ac.kr>
2024-10-21 23:29:56 +0900

    feat(App.js):예외사항 수정

--- src/App.js
+++ src/App.js
@@ -5,35 +5,39 @@ class App {
     Console.print('덧셈할 문자열을 입력해 주세요.');
     const input = await Console.readLineAsync('');
 
-    if (!input) {
+    if (input === "") {
       return Console.print("결과 : 0");
     }
 
     let deli = /[,|:]/;
-    let part = input;
+    let NPart = input;
 
     if (input.startsWith("//")) {
-      const [delimiterLine, ...numberLines] = input.split("\\n");
-      const custom = delimiterLine.slice(2);
-      const numbers = numberLines.join("");
-      deli = new RegExp(`[${custom},|:]`);
-      part = numbers;
-    } else {
-      if (isNaN(input[0]) || input[0] === "-") {
-        throw new Error("[ERROR] 커스텀 구분자를 사용하지 않는다면 양수로 시작해야 합니다.");
+      if (input.indexOf("\\n") !== 3 || !isNaN(input[2])) {
+        throw new Error("[ERROR] 커스텀 구분자 양식이 잘못됐습니다.");
       }
+      const parts = input.split("\\n");
+
+
+      const custom = parts[0].substring(2);
+
+
+      deli = new RegExp(`[${custom},|:]`);
+      NPart = parts[1];
+    } else if (isNaN(input[0]) || input[0] === "-") {
+      throw new Error("[ERROR] 커스텀 구분자를 사용하지 않는다면 양수로 시작해야 합니다.");
     }
 
-    const numbers = part.split(deli).map(Number);
-    for (const number of numbers) {
+    const numbers = NPart.split(deli).map(Number);
+
+  
+    for (let i = 0; i < numbers.length; i++) {
+      const number = numbers[i];
       if (isNaN(number) || number < 1) {
         throw new Error("[ERROR] 계산식에 구분자, 양수 이외의 값이 존재합니다.");
       }
     }
-
-    Console.print("결과: " + numbers.reduce((sum, number) => sum + number, 0));
+    Console.print("결과 : " + numbers.reduce((sum, number) => sum + number, 0));
   }
-
 }
-
 export default App;
