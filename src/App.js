bad9827afcd8d414b77c0a22b6c24761e4fa9bdb
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-10-21 23:54:13 +0900

    fix: 문자열 입력 검증 기능 수정

--- src/App.js
+++ src/App.js
@@ -7,13 +7,31 @@ class App {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
 
     // 문자열 입력 검증
-    if (this.validateDefaultDelimiters(input)) {
-      // 기본 구분자라면, 쉼표와 콜론으로 숫자 분리
-      const numbers = input.split(/[,:]/).map(Number)
-    } else {
-      // 커스텀 구분자가 있다면, 해당 구분자로 숫자 분리
-      const numbers = this.extractCustomDelimitersNumber(input)
+    if (input === "") {
+
+      Console.print("결과: 0")
+      return
+
     }
+
+    let numbers
+
+    if (this.validateDefaultDelimiters(input)) {
+
+      // 기본 구분자라면, 쉼표와 콜론으로 숫자 분리
+      numbers = input.split(/[,:]/).map(Number)
+      
+    } else {
+
+      // 커스텀 구분자가 있다면, 해당 구분자로 숫자 분리
+      numbers = this.extractCustomDelimitersNumber(input)
+      
+    }
+
+    const result = numbers.reduce((sum, num) => sum + num, 0)
+    Console.print(`결과: ${result}`)
+
+     
     
   }
 
