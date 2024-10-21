325ff6ba5303a6deedb656ed2f171cdde61247ae
Saebom <spring180@naver.com>
2024-10-21 14:04:31 +0900

    feat: 문자열 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let input = await Console.readLineAsync("Enter the numbers to calculate: ");
+  }
 }
 
 export default App;
