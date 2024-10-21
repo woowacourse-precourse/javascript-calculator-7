dff30b66dcaf33b4643e413f622528bb4b2d9cac
Daeuning <happyjde3@gmail.com>
2024-10-21 12:38:21 +0900

    feat(constants): 에러 메세지 상수화

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const ERROR_MESSAGE = Object.freeze({
+  EMPTY_INPUT: "입력된 값이 없어요. 다시 한 번 덧셈할 숫자를 입력해 주세요!",
+  NEGATIVE_INPUT: "양수만 입력해 주세요.",
+  INVALID_INPUT: `입력된 값에 지원하지 않는 문자가 있어요.
+  기본 구분자: ':' 또는 ','
+  커스텀 구분자 설정: //(구분자)\\n
+  위 정보를 참고하여 다시 입력해 주세요!`
+});
+
+
+export {
+  ERROR_MESSAGE
+};
