5bdca441073ee8ef81c4bc11c4da66ea44de6b56
yoojin <everywavyday@gmail.com>
2024-10-21 21:41:44 +0900

    feat: 분리된 값 중 양수가 아닌 값이 있는지 확인하는 에러 처리

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    try {
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
       const numbers = this.extractNumbers(input);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
   }
 
   // 문자열에서 숫자를 추출하는 함수
@@ -19,7 +23,17 @@ class App {
     
     const splitText = inputText.split(delimiter);
     const numbers = splitText.map(Number);
+    this.validateNumbers(numbers);
     return numbers;
+  }
+
+  // 예외 처리 함수
+  validateNumbers(numbers) {
+    for (const number of numbers) {
+      if (number < 0) {
+        throw new Error('음수는 허용되지 않습니다.');
+      }
+    }
   }
 }
 
