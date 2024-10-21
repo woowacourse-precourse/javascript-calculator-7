5bc90cff0eebe71f938fce700123c96e0cdd3a40
kimdongyoung <wwweric12@naver.com>
2024-10-21 22:06:11 +0900

    fix(getString): 숫자만있을때와 기호만있을때 에러 추가

--- src/utils/getString.js
+++ src/utils/getString.js
@@ -1,13 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { hasStringNumber, hasSymbol } from '../errorHandling';
 
 const getString = async() =>{
     try{
         const inputValue = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+        if(!hasSymbol(inputValue) || !hasStringNumber(inputValue)){
+            throw new Error("[ERROR] 입력을 잘못하셨습니다. 어플리케이션이 종료됩니다. ");
+        }
         return inputValue;
     }catch(error){
-        throw new Error("[Error] 입력을 잘못하셨습니다. 어플리케이션이 종료됩니다. ");
+        throw new Error("[ERROR] 입력을 잘못하셨습니다. 어플리케이션이 종료됩니다. ");
     }
-    
+
   
 }
 
