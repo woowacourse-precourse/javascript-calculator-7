106a8bbcd38ddfa43c9c45ba26f24aef5bb89dec
developer-kanghyun <dev.kanghyun@gmail.com>
2024-10-21 23:45:31 +0900

    feat: 구분자 ","로 통일

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync("입력하세요.\n");
+
+    const replacedString = input.replaceAll(":", ",");
+    let resultString = replacedString;
   }
 }
 
