586966c8ab927109402893c081b30098054fda51
Yarrly <74090200+eunsoA@users.noreply.github.com>
2024-10-21 16:37:27 +0900

    feat : 사용자 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // [feat] 1. 사용자 입력
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
