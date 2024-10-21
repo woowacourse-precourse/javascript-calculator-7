b38cc00938e05aa649a5e5df3c5881b7d7668303
lecto17 <khwoo_rpsit@naver.com>
2024-10-21 15:35:50 +0900

    feat: 입력 문자열 split 작업

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  isCustomDelimeterUsed(input) {
+    return input.startsWith("//") && input.includes("\\n");
+  }
+
+  replaceDelimeter(input, delimeter) {
+    let inputString = input;
+    if (delimeter) {
+      inputString = input.replaceAll(delimeter, " ");
+    }
+
+    return inputString.replaceAll(this.defaultDelimeter, " ");
+  }
+
   getAnswer(input) {
-    console.log(input);
+    let answer, inputString;
+
+    if (this.isCustomDelimeterUsed(input)) {
+      const delimeter = input.split("\\n")[0].slice(2);
+      const numbers = input.split("\\n")[1];
+
+      inputString = this.replaceDelimeter(numbers, delimeter);
+    }
+
+    inputString = this.replaceDelimeter(input);
   }
 
   async run() {
+    let endFlag = false;
+
     while (true) {
-      await Console.readLineAsync("숫자를 입력해주세요 : ")
-        .then((userInput) => {
-          return this.getAnswer(userInput);
-        })
-        .then((res) => Console.print(res));
+      endFlag = false;
+      try {
+        const userInput = await Console.readLineAsync(
+          "덧셈할 문자열을 입력해 주세요.\n"
+        );
+        this.getAnswer(userInput);
+      } catch (err) {
+        throw new Error(`[ERROR]: ${err.message}`);
+      }
+
+      if (endFlag) {
+        break;
+      }
     }
   }
 }
