91f6c82f4b1388c406780faa9aad663a548f6bef
sunkiteo-o <hyhy020301@gmail.com>
2024-10-21 17:35:04 +0900

    feat: #3 기능구현

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-import readline from "readline";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    //문자열 입력받기, 입력받은 문자열을 INPUT에 저장
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+    while (1) {
+      let INPUT = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
 
-    let INPUT = "";
-
-    rl.on("line", (line) => {
-      INPUT = line;
-      rl.close();
-    });
-    rl.on("close", () => {
-      let re = new RegExp("//(.*?)\\\\n"); //구분자 캡처를 위해 (.*?) 사용
+      let re = new RegExp("//(.*?)\\\\n");
+      const re_num = new RegExp("[0-9]");
       let split_input;
       let OUTPUT;
 
       if ((split_input = re.exec(INPUT)) !== null) {
-        // "//;\n1;2;3" => 6
-        //split_input에는 //;\n 이 부분만 들어감
-        let new_re = split_input[1]; //새로운 구분자를 new_re에 저장
+        let new_re = split_input[1];
         let new_input = INPUT.replace(re, "");
         OUTPUT = new_input.split(new_re).map(Number);
       } else {
-        // "" => 0, "1,2" => 3, "1,2,3" => 6, "1,2:3" => 6
         let default_re = new RegExp(",|:");
         OUTPUT = INPUT.split(default_re).map(Number);
       }
+      if (OUTPUT.includes(NaN)) {
+        Console.print("[ERROR]");
+        break;
+      }
       const sum = OUTPUT.reduce((acc, cur) => acc + cur, 0);
-      console.log(sum);
-    });
+      Console.print("결과 : " + sum);
+    }
   }
 }
 
