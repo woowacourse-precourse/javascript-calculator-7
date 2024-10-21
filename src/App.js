97eca8d2992c144ef1d7ea2ec727362a00b663bb
daeun <gbajs754@gmail.com>
2024-10-21 19:52:21 +0900

    feat : 사용자 입력을 받고 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해주세요.\n"
+    );
+
+    Console.print(`결과 : ${userInput}`);
+  }
 }
 
 export default App;
