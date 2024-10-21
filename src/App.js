378f32f968d4cc48c0b20169dbae310fa2cf7728
Juyudang <ssuw223@gmail.com>
2024-10-21 20:04:30 +0900

    feat: testing prepare-commit-msg
    Desribe
    - Change 1
    - Change 2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ class App {
   async run() {
     // 실행 후 에러 발생시 아래 catch문 발동
     try {
-      const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-      const result = readInput(input);
-      MissionUtils.Console.print("결과 : " + result);
+      const INPUT = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const RESULT = readInput(INPUT);
+      MissionUtils.Console.print("결과 : " + RESULT);
     } catch (error) {
       // 아래 throw 된 에러 메세지를 여기에 표출
       throw new Error("[ERROR]" + error.message);
@@ -14,7 +14,7 @@ class App {
   }
 }
 
-// get input and get sum of it
+// 입력받아 계산
 function readInput(input) {
   // 디폴트 regex 형태 생성
   let regex = /[,:]/
@@ -41,20 +41,20 @@ function myRegex(new_seperator) {
 // 문자열을 나누고 합을 리턴
 function mySum(inputs, regex) {
   // 문자열을 정규표현식 기준으로 나누고 array형태로 저장(String 형태)
-  const nums = inputs.split(regex)
+  const NUMS = inputs.split(regex)
   let sum = 0
-  for (let i = 0;i < nums.length;i++){
+  for (let i = 0;i < NUMS.length;i++){
     // Number함수를 통해 int와 같은 숫자 형태로 형변환 후 음수 처리
-    if (Number(nums[i])<0) {
+    if (Number(NUMS[i])<0) {
       // 에러 객체의 message에 저장
-      throw new Error("음수입니다.")
+      throw new Error("음수입니다.");
     }
     // 숫자가 아닌 입력 처리
-    if (isNaN(Number(nums[i]))) {
+    if (isNaN(Number(NUMS[i]))) {
       // 에러 객체의 message에 저장
       throw new Error("유효하지 않은 입력값입니다.");
     }
-    sum = sum + Number(nums[i])
+    sum = sum + Number(NUMS[i])
   }
   return sum
 }
