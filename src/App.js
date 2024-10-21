31192adc96227b8b102c8ad5194ab279d48cdf97
YJS96 <wnt9864@hotmail.com>
2024-10-21 14:15:06 +0900

    feat(App.js): 입출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n'
+      );
+      const result = this.calculateSum(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
+
+  calculateSum(str) {
+    return str;
+  }
 }
 
 export default App;
