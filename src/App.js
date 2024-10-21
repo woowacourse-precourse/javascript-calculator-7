889341bbe21660b9873515c1ab55b19a2cb6bccb
young24n <won8560@gmail.com>
2024-10-21 13:35:13 +0900

    refactor(delimiter): 구분자 추출 기능을 구분자 유효성 테스트 함수에서 분리
    -기타 코드 스타일 개선

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   splitLine(text) {
-    // Todo: REGEX 더 적합한 이름으로 변경
-    const IS_HAVE_DELIMITER = /^\/\/.*\\n/;
+    const CHECK_DELIMITER = /^\/\/.*\\n/;
     let delimiter_line = '';
     let number_line = '';
 
-    if (IS_HAVE_DELIMITER.test(text)) {
+    if (CHECK_DELIMITER.test(text)) {
       [delimiter_line, number_line] = text.split('\\n');
       
       return { delimiter_line, number_line };
@@ -21,8 +20,6 @@ class App {
     const CHECK_INVALID_NUMBER_REGEX = /^\/\/\d{1,}$/;
     const CHECK_EMPTY_DELIMITER_REGEX = /^\/\/$/;
     const CHECK_LONG_DELIMITER_REGEX = /^\/\/.{2,}$/;
-    const CHECK_EMPTY_REGEX = /^$/;
-    const CAPTURE_DELIMITER = /^\/\/(.)$/;
 
     if (CHECK_INVALID_NUMBER_REGEX.test(text)) {
       throw new Error('[ERROR] 구분자로 숫자를 사용할 수 없습니다!');
@@ -33,10 +30,12 @@ class App {
     if (CHECK_LONG_DELIMITER_REGEX.test(text)) {
       throw new Error('[ERROR] 구분자를 2자 이상으로 설정할 수 없습니다!');
     } 
-    if (CHECK_EMPTY_REGEX.test(text)) {
-      return text;
-    }
+    
+    return text;
+  }
 
+  extractDelimiter(text) {
+    const CAPTURE_DELIMITER = /^\/\/(.)$/;
     return text.match(CAPTURE_DELIMITER)[1];
   }
 
@@ -74,7 +73,7 @@ class App {
   sumNumbers(numbers) {
     let total_value = 0;
     numbers.forEach(number => {
-      total_value = total_value + number
+      total_value = total_value + number;
     });
 
     return total_value;
@@ -84,12 +83,12 @@ class App {
     try {
       const INPUT_DATA = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
       const LINES = this.splitLine(INPUT_DATA);
-      const VALIDATE_DELIMITER_LINE = await this.validateDelimiterLine(LINES.delimiter_line);
-      const VALIDATE_NUMBER_LINE = await this.validateNumberLine(LINES.number_line, VALIDATE_DELIMITER_LINE);
-      const EXTRACT_NUMBERS = await this.extractNumbers(VALIDATE_NUMBER_LINE, VALIDATE_DELIMITER_LINE);
+      const VALIDATE_DELIMITER_LINE = this.validateDelimiterLine(LINES.delimiter_line);
+      const VALIDATE_NUMBER_LINE = this.validateNumberLine(LINES.number_line, VALIDATE_DELIMITER_LINE);
+      const EXTRACT_NUMBERS = this.extractNumbers(VALIDATE_NUMBER_LINE, VALIDATE_DELIMITER_LINE);
       const VALUE = this.sumNumbers(EXTRACT_NUMBERS);
 
-      MissionUtils.Console.print(`결과 : ${VALUE}`)
+      MissionUtils.Console.print(`결과 : ${VALUE}`);
 
     } catch (error) {
       throw error;  
