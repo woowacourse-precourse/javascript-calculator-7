cd1d0449a0ec95dd5a2dcb3e74842002f780b385
JaiIn <di0308@naver.com>
2024-10-21 14:56:44 +0900

    fix: try catch사용 및 에러추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Calculator from "./calculator.js";
+import { Console } from "@woowacourse/mission-utils";
+import AddCalculator from "./AddCalculator.js";
 
 class App {
   async run() {
-    const myCalculator = new Calculator();
-    await myCalculator.run();
-  }
-}
+    const Calculator = new AddCalculator();
+    try {
+      await Calculator.run();
+    } catch (error) {
+      Console.print("[ERROR] : ",error);
+    }
+  };
+};
 
 export default App;
