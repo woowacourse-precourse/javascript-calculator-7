9c40c49164023e6e5493daa6ed96bc685c583064
songhaeunsong <gkdms3469@naver.com>
2024-10-21 14:09:51 +0900

    feat: App 클래스 메서드 및 구조 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+import Validator from "./Validator.js";
 class App {
   async run() {}
+
+  getString() {}
+  getSeparatorAndNumber() {}
+  findCustomSeparator() {}
+  changeToRegex() {}
+  getNumberCutWithSeparator() {}
+  calculateSum() {}
+  printAnswer() {}
+  printError() {}
+  validate() {}
 }
 
 export default App;
