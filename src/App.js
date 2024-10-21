cd9124e7cb77cb56f1a588f5307d27a1f85c5c21
deokisys <sjb378@me.com>
2024-10-21 20:21:01 +0900

    [feat] 문자열 배열로 변환, 배열의 값 더하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+
+    let delimiters = [",", ":"]; //구분자
+
+    //구분자들을 정규표현식을 이용하도록 조합
+    const regex = new RegExp(`[${delimiters.join("")}]`);
+    const result = input.split(regex).map(Number); //
+
+    //result들을 합하여 결과 출력
+    const output = result.reduce((acc, ele) => acc + ele, 0);
+    Console.print(`결과 : ${output}`);
+  }
 }
 
 export default App;
