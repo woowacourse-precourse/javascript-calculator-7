039b1e1a2218c5d5b507a18b14f02a6e381a34e3
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-21 11:32:06 +0900

    feat(App) 결과 출력하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    // 덧셈을 하기 위한 문자열 입력 받기
-    const inputValue = await MissionUtils.Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
-    // MissionUtils.Console.print(input);
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      const sum = this.calculate(input);
+      Console.print(`결과 : ${sum}`);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
   }
 }
 
