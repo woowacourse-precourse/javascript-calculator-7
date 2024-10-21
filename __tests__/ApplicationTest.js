18341c36c091d33d2c23bc9d810a4c86a16bfdbd
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-21 19:21:32 +0900

    ✨ feat(test): 테스트 케이스 추가
    더 다양한 예제가 통과되는지 확인하기 위해 다양한 테스트 케이스를 추가했습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -62,8 +62,68 @@ describe('문자열 계산기', () => {
     });
   });
 
+  test('커스텀 구분자에 숫자가 들어갔을 때', async () => {
+    const inputs = ['//123\\n412351236'];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ['결과 : 15'];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test('예외 테스트', async () => {
     const inputs = ['-1,2,3'];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test('커스텀 구분자가 잘못됐을 때 테스트', async () => {
+    const inputs = ['//;\\b1;2;3'];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test('사용자 입력없을 때', async () => {
+    const inputs = [''];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test('커스텀 구분자로 끝날 때', async () => {
+    const inputs = ['//.\\n1.2.3.4.'];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test('양수가 아닌 0이 입력되었을 때', async () => {
+    const inputs = ['0,1,2,3'];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test('숫자 외에 문자가 입력되었을 때', async () => {
+    const inputs = ['1,a,3'];
     mockQuestions(inputs);
 
     const app = new App();
