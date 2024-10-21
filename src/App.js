f542901788fd2a8edb8aa7603a381cbadd316ba0
wnsxk2 <wnsxk206@gmail.com>
2024-10-21 20:49:59 +0900

    refactor: 커스텀 구분자 확인 코드 분리
    입력받은 문자열을 커스텀 구분자인지 확인하고 반환하는 코드를 함수로 분리 하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import { Console } from '@woowacourse/mission-utils';
 
-// 커스텀 구분자 확인 regex
-const CUSTOM_DELIMITER_REGEX = /^\/\/(\D)\\n/;
+// 커스텀 구분자 확인 메서드
+function checkDelimiter(str) {
+
+  // 커스텀 구분자 확인 regex
+  const CUSTOM_DELIMITER_REGEX = /^\/\/(\D)\\n/;
+
+  let delimiter;
+  let numberStr;
+
+  const isCustomDelimiter = CUSTOM_DELIMITER_REGEX.test(str);
+
+  if (isCustomDelimiter) {
+    const match = str.match(CUSTOM_DELIMITER_REGEX);
+    delimiter = match[1];
+    numberStr = str.replace(CUSTOM_DELIMITER_REGEX, '');
+  } else {
+    delimiter = /[,:]/;
+    numberStr = str;
+  }
+
+  return { delimiter, numberStr };
+}
 
 class App {
+
   async run() {
     try {
-      // 변수 선언
-      let delimiter;
       // 문자열 입력
       let str = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
-      // 커스텀 구분자 확인
-      const isCustomDelimiter = CUSTOM_DELIMITER_REGEX.test(str);
 
-      if (isCustomDelimiter) {
-        const match = str.match(CUSTOM_DELIMITER_REGEX);
-        delimiter = match[1];
-        str = str.replace(CUSTOM_DELIMITER_REGEX, '');
-      } else {
-        delimiter = /[,:]/;
-      }
+      // 구분자와 문자열 리턴
+      const { delimiter, numberStr } = checkDelimiter(str);
 
       // 문자열을 구분자로 구분한 배열 리턴
-      const numberArr = str.split(delimiter);
+      const numberArr = numberStr.split(delimiter);
+
       // 배열의 총합을 리턴
       const sum = numberArr.reduce((total, num) => {
-        const a = Number(num);
+        const castedNumber = Number(num);
         // 예외 처리
-        if (isNaN(a) || num < 0) {
+        if (isNaN(castedNumber) || num < 0) {
           throw new Error('문자열은 양수과 구분자로만 구성되어야합니다.');
         }
-        return total + a;
+        return total + castedNumber;
       }, 0);
 
       Console.print(`결과 : ${sum}`);
