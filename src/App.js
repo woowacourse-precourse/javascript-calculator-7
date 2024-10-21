637939a01d5049238747db5c833cf4acf4e82236
Jin-Chanyong <cksdyd324@gmail.com>
2024-10-21 20:41:11 +0900

    refactor(App): 로직 분리 및 코드 개선
    run 함수에서 입력값 처리, 유효성 검사, 합계 계산을 하는 로직을 별도의 함수로 분리하여 가독성과 유지보수성을 개선하였고, 그 외에 ? 연산자와 flatMap 사용으로 코드를 개선함.
    
    App.js 에서 변경 사항:
    - makeNumbersArray 함수로 입력값 처리 로직 분리
    - validateNumberArray 함수로 입력값 유효성 검사 로직 분리
    - calculateSum 함수로 합계 계산 로직 분리
    - map 과 flat 함수를 따로 사용하여 문자열을 처리하기보다, flatMap 함수를 사용하여 통합
    - if 문 대신 ? 연산자를 사용하여 코드 양 감소

--- src/App.js
+++ src/App.js
@@ -6,38 +6,42 @@ class App {
       "덧셈할 문자열을 입력해 주세요.\n"
     );
 
-    const customSeparator = inputValue.startsWith("//") && inputValue[2];
+    const numbersArray = this.makeNumbersArray(inputValue);
 
-    const numbers = customSeparator ? inputValue.split("\\n")[1] : inputValue;
+    this.validateNumbersArray(numbersArray);
 
-    const splitedValueArray = numbers
-      .split(",")
-      .map((value) => {
-        if (value.includes(":")) {
-          return value.split(":");
-        }
-
-        return value;
-      })
-      .flat()
-      .map((value) => {
-        if (customSeparator && value.includes(customSeparator)) {
-          return value.split(customSeparator);
-        }
-
-        return value;
-      })
-      .flat();
-
-    const sum = splitedValueArray.reduce((acc, curr) => acc + Number(curr), 0);
-
-    if (splitedValueArray.some((value) => Number(value) <= 0)) {
-      throw new Error("[ERROR]양수의 올바른 숫자를 입력해 주세요.");
-    } else if (Number.isNaN(sum)) {
-      throw new Error("[ERROR]형식에 맞게 문자열을 입력해 주세요.");
-    }
+    const sum = this.calculateSum(numbersArray);
 
     Console.print(`결과 : ${sum}`);
+  }
+
+  makeNumbersArray(inputValue) {
+    const customSeparator = inputValue.startsWith("//") ? inputValue[2] : null;
+    const numbers = customSeparator ? inputValue.split("\\n")[1] : inputValue;
+
+    return numbers
+      .split(",")
+      .flatMap((value) => (value.includes(":") ? value.split(":") : value))
+      .flatMap((value) =>
+        customSeparator && value.includes(customSeparator)
+          ? value.split(customSeparator)
+          : value
+      );
+  }
+
+  validateNumbersArray(numbersArray) {
+    if (numbersArray.some((value) => Number(value) <= 0)) {
+      throw new Error("[ERROR] 양수의 올바른 숫자를 입력해 주세요.");
+    }
+  }
+
+  calculateSum(numbersArray) {
+    const sum = numbersArray.reduce((acc, curr) => acc + Number(curr), 0);
+
+    if (Number.isNaN(sum)) {
+      throw new Error("[ERROR] 형식에 맞게 문자열을 입력해 주세요.");
+    }
+    return sum;
   }
 }
 
