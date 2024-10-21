978cf52eaf8ab2bd49388f9545b3a4def298a939
sanghyeonLee <hyeon012366@gmail.com>
2024-10-21 23:12:38 +0900

    feat : 문자가 올 수 있다는 요구사항에 따라 예외처리 추가

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -4,6 +4,9 @@ export function handleCustomSeparatorError(param) {
     if (param === "") {
         throw new Error(ERROR_MESSAGE.WRONG_CUSTOM_SEPARATOR);
     }
+    if (!isNaN(param)) { //"//"와 "\n" 사이에 위치하는 "문자"를 커스텀 구분자로 사용한다.
+        throw new Error(ERROR_MESSAGE.WRONG_NOT_STRING_CUSTOM_SEPARATOR);
+    }
 }
 
 export function handleStringError(param) {
