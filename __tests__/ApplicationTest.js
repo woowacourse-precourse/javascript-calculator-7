08b42674df8016c2ace76379a944e0f811c65819
kanghaeun <xgkg0330@jnu.ac.kr>
2024-10-21 19:35:16 +0900

    test: 문자열 계산기 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,12 +32,88 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("음수 입력시 예외 테스트", async () => {
     const inputs = ["-1,2,3"];
     mockQuestions(inputs);
 
     const app = new App();
 
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 음수는 허용되지 않습니다."
+    );
+  });
+
+  test("숫자, 구분자 외에 값 입력시 예외 발생", async () => {
+    const inputs = ["1,2,a,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 구분자와 숫자가 아닌 값은 포함될 수 없습니다."
+    );
+  });
+
+  test("//으로 시작하지만 \n이 존재하지 않을시 예외 발생", async () => {
+    const inputs = ["//1,2:3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 잘못된 커스텀 구분자 형식입니다."
+    );
+  });
+
+  test("공백이 포함된 입력시 예외 발생", async () => {
+    const inputs = [" 1,2, 3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 공백이 포함될 수 없습니다."
+    );
+  });
+
+  test("커스텀 구분자가 빈 구분자인 경우", async () => {
+    const inputs = ["//\\n1"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 빈 구분자는 허용되지 않습니다."
+    );
+  });
+
+  test("빈 문자열 사용", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자와 기본 구분자 혼합해서 사용", async () => {
+    const inputs = ["//*\\n1,3*2:3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 9"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
 });
