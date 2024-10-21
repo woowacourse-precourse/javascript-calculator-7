6966d7270fba936f577c0e5b182dbef466cf29c3
silver93hq <gkagjsrb@gmail.com>
2024-10-21 12:40:29 +0900

    fix: 잘못 입력된 임포트 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import User from './User.js';
-import CustomDelimiterHandler from './handleCustomDelimiter.js';
+import CustomDelimiterHandler from './CustomDelimiterHandler.js';
 
 class App {
   async run() {
