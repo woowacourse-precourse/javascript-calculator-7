06fdf5d65ee96e7084c3d865c69964f95f33ff2c
sanghyeonLee <hyeon012366@gmail.com>
2024-10-21 20:25:53 +0900

    refactor : 함수 모듈화로 인해 조건문 사용시 코드의 가독성, 효율성 감소로 반환값으로 customSeparator 값 this 할당

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,6 @@
 import {Console} from "@woowacourse/mission-utils";
 import {getInput} from "./utils/inputHandler.js";
-import {isCustom, preprocessing, sliceString} from "./utils/stringProcessor.js";
+import {parseInput, preprocessing} from "./utils/stringProcessor.js";
 import {checkForErrors} from "./utils/errorHandler.js";
 
 class StringCalculator {
@@ -12,8 +12,9 @@ class StringCalculator {
         if (input === "") {
             return this.getOutput(); // 빈 문자열의 경우 결과 출력
         }
-        const argument = isCustom(input) ? sliceString(input).slicedString : input;
-        this.calculator(argument);
+        const {customSeparator, slicedString} = parseInput(input);
+        this.customSeparator = customSeparator;
+        this.calculator(slicedString);
         this.getOutput();
     }
 
@@ -24,7 +25,7 @@ class StringCalculator {
     calculator(param) {
         const numberArray = preprocessing(param, this.customSeparator);
         numberArray.forEach(elem => checkForErrors(elem)); // 에러 체크
-        this.sum = numberArray.reduce((a, b) => a + b, 0);
+        this.sum = numberArray.reduce((a, b) => Number(a) + Number(b), 0);
     }
 }
 
