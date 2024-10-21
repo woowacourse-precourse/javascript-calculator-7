5d1aa19095b78258d2ae0a249063a577a0d4862c
GSIII <kalseo901@gmail.com>
2024-10-21 15:52:38 +0900

    feat(view) : 덧셈 결과 출력

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { separatorSplit } from "./utils.js";
 class App {
   async run() {
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    separatorSplit(input);
+    let numbers = separatorSplit(input);
+
+    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+
+    Console.print(`결과 : ${sum}`);
   }
 }
 
