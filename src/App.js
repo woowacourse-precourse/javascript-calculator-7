416f75b4dc598f8146eed7d4eee0247b5a020411
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2024-10-21 22:55:07 +0900

    feat add calculate and input null check
    계산함수 생성 및 input이 비었는지 체크

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ class App {
   async run() {
     Console.print("덧셈할 문자열을 입력해 주세요: ");
     const input = await Console.readLineAsync();
-    Console.print("입력된 문자열: ${input}");
+  }
+
+  calculate(input) {
+    if (input == " ") {
+      return 0;
+    }
   }
 }
 
