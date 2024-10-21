7044d4e7a5e0a43cd50cef3481929e851db4bf4c
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-10-21 15:20:55 +0900

    feat(input): user input using Console API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 사용자 입력
+    const input = await Console.readLineAsync(
+      "구분자와 양수로 구성된 문자열을 입력해주세요: "
+    );
+  }
 }
 
 export default App;
