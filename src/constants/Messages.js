2985a471f280daf51dfa2c520976e8f2acf217b5
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-10-21 22:30:05 +0900

    feat: 콘솔 입력 및 출력 기능 구현

---
+++ src/constants/Messages.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  INVALID_INPUT: "[ERROR] 잘못된 입력입니다.",
+  NEGATIVE_NUMBER: "[ERROR] 양수만 입력 가능합니다.",
+  INVALID_CUSTOM_DELIMITER: "[ERROR] 잘못된 커스텀 구분자 형식입니다.",
+  CONTINUOUS_DELIMITER: "[ERROR] 연속된 구분자는 허용되지 않습니다.",
+};
