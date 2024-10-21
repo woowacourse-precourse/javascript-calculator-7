1d22dd66aae838676b7834a3816d3b80d7f1919b
김민주 <kminju2000@naver.com>
2024-10-21 16:03:54 +0900

    fix: 입력이 없을 때 출력값 수정

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App {
       const INPUT = await MissionUtils.Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n"
       );
+      if (INPUT.length == 0) {
+        MissionUtils.Console.print("결과 : 0");
+        return;
+      }
       if (this.isValidInput(INPUT)) {
         const RESULT_ARR = this.separateInput(INPUT);
         const RESULT = this.add(RESULT_ARR);
@@ -23,15 +27,16 @@ class App {
   // 유효한 입력값인지 확인하는 함수
   isValidInput(INPUT) {
     if (INPUT.length == 0) {
-      return false;
+      MissionUtils.Console.print("결과 : 0");
+      return;
     }
     return true;
   }
 
-  separateInput(input) {
+  separateInput(INPUT) {
     // 커스텀 구분자로 시작하는 경우
-    if (input.startsWith("/")) {
-      const [LETTER, NUMBER] = input.split(/\\n/);
+    if (INPUT.startsWith("/")) {
+      const [LETTER, NUMBER] = INPUT.split(/\\n/);
 
       if (!LETTER.startsWith("//") || !NUMBER) {
         throw Error("[ERROR]");
@@ -41,27 +46,27 @@ class App {
       const RESULT = NUMBER.split(SEPARATOR);
 
       return RESULT.map((item) => {
-        const num = Number(item);
-        if (isNaN(num)) {
+        const NUM = Number(item);
+        if (isNaN(NUM)) {
           throw Error("[ERROR]");
         }
-        if (num < 0) {
+        if (NUM < 0) {
           throw Error("[ERROR]");
         }
-        return num;
+        return NUM;
       });
-    } else if (!isNaN(Number(input[0]))) {
-      const RESULT = input.split(/[,;]/);
+    } else if (!isNaN(Number(INPUT[0]))) {
+      const RESULT = INPUT.split(/[,;]/);
 
       return RESULT.map((item) => {
-        const num = Number(item);
-        if (isNaN(num)) {
-          throw Error("[ERROR]");
+        const NUM = Number(item);
+        if (isNaN(NUM)) {
+          throw Error("[ERROR] 구분자를 제외하고는 숫자를 입력해주세요.");
         }
-        if (num < 0) {
-          throw Error("[ERROR]");
+        if (NUM < 0) {
+          throw Error("[ERROR] 양수를 입력해주세요.");
         }
-        return num;
+        return NUM;
       });
     } else {
       throw Error("[ERROR]");
@@ -69,11 +74,11 @@ class App {
   }
 
   add(RESULT_ARR) {
-    let sum = 0;
+    let SUM = 0;
     for (let i = 0; i < RESULT_ARR.length; i++) {
-      sum += RESULT_ARR[i];
+      SUM += RESULT_ARR[i];
     }
-    return sum;
+    return SUM;
   }
 }
 
