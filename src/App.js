1b0bde6f9d5ee6d24333c087a82fbd646a62d428
daeun <gbajs754@gmail.com>
2024-10-21 19:54:03 +0900

    feat : try-catch문으로 에러 핸들링

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const userInput = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해주세요.\n"
-    );
+    try {
+      const userInput = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해주세요.\n"
+      );
 
-    Console.print(`결과 : ${userInput}`);
+      Console.print(`결과 : ${userInput}`);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 }
 
