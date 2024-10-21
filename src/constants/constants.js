ea8e51a7cac8a8c0f604c04b5304666fba0070ee
leelsj2007 <leelsj2007@gmail.com>
2024-10-21 22:05:23 +0900

    feat : constants 부분의 오류 발생 시 에러 메시지 작성

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,3 +2,10 @@ export const INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n"
 export const OUTPUT_MESSAGE_TEMPLATE = "결과 : ";
 
 export const OUTPUT_MESSAGE = (result) => `${OUTPUT_MESSAGE_TEMPLATE}${result}`;
+
+const ERROR_PREFIX = "[ERROR]";
+export const ERROR_MESSAGE = {
+    invalidInputError : `${ERROR_PREFIX} 유효한 정수 값을 입력해야 합니다.`,
+    negativeInputError : `${ERROR_PREFIX} 음수값은 허용되지 않습니다.`,
+    separatorError : `${ERROR_PREFIX} 구분자가 맞지 않습니다.`,
+}
