77f41de7aaadd3b57a2745b2c4c5724fbd3ea325
junwoo <junwooha1@gmail.com>
2024-10-21 17:19:43 +0900

    feat(App.js): 사용자 입력을 처리하고 결과를 출력하는 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+import StringCalculator from './StringCalculator.js';
+
 class App {
-  async run() {}
+  run() {
+    Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
+      .then((input) => {
+        const stringCalculator = new StringCalculator();
+        try {
+          const result = stringCalculator.add(input);
+          Console.print(`결과 : ${result}`);
+        } catch (error) {
+          Console.print(error.message);
+        }
+      });
+  }
 }
 
 export default App;
