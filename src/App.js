6e5a3a9a602aa592278bb880ceab488305b0e0c4
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-21 20:57:03 +0900

    feat: 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   constructor() {
     this.cursor = 0;
@@ -6,7 +8,11 @@ class App {
     this.sum = 0;
   }
 
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const strArr = input.trim().split('');
+    console.log(strArr);
+  }
 }
 
 export default App;
