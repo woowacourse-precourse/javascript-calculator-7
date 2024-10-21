6d5dcab2dace8a023e19fa7cf1e38352ff960f39
zzzzzuuuuu <agu0830@naver.com>
2024-10-21 21:42:31 +0900

    feat: 에러 발생시 예외 던진 후 종료되도록 구현

--- src/App.js
+++ src/App.js
@@ -10,16 +10,13 @@ class App {
 
       let words;
 
-      // 문자열 구분
+      // 커스텀 구분자
       if (input.startsWith("//") && input.substring(3, 5) === "\\n") {
         words = input.substring(5).split(input[2]).map(Number);
       } else {
+        // 기본 구분자
         words = input.split(/[,|:]/).map(Number);
       }
-      Console.print(words);
-
-      // 양수인지 체크
-      const isPositiveInt = (word) => /^\d+$/.test(word);
 
       // 빈 문자열 입력시 0 출력 후 종료
       if (input.trim() === "") {
@@ -27,15 +24,22 @@ class App {
         return;
       }
 
+      // NaN, null, undefined 체크
       if (words.some(word => isNaN(word) || word === null || word === undefined)) {
         throw new Error("[ERROR] 올바른 값을 입력해주세요.");
-      } else if(!words.every(word => isPositiveInt(word))) {
-        Console.print("[ERROR] 양수만 입력해주세요.")
-      } else {
-        const sum = words.reduce((acc, curr) => acc + curr);
-        Console.print("결과 : " + sum);
       }
+
+      // 양수만 허용
+      const isPositiveInt = (word) => /^\d+$/.test(word);
+
+      if (!words.every(word => isPositiveInt(word))) {
+        throw new Error("[ERROR] 양수만 입력해주세요.")
+      }
+
+      const sum = words.reduce((acc, curr) => acc + curr);
+        Console.print("결과 : " + sum);
     } catch (error) {
+      Console.print(error.message);
       throw error;
     }
   }
