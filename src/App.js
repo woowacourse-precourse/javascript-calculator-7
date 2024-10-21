5d22fb86e74d44334f7d94c723cfed92cd96ae31
unknown <suyeonshim@gm.gist.ac.kr>
2024-10-21 19:25:23 +0900

    feat: 문자열 입출력 기본 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요: "
+      );
+      const result = input; // 계산 함수 추가
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
