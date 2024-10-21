cb5d9221cdb57f99ffcee05561f066e782d78eae
Saebom <spring180@naver.com>
2024-10-21 14:33:51 +0900

    feat: 구분자 처리

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    let input = await Console.readLineAsync("Enter the numbers to calculate: ");
+    const DEFAULT_SEPARATORS = [",", ":"];
+    const regex = new RegExp(DEFAULT_SEPARATORS.join("|")); // 정규 표현식: /,|:/
+    let result;
+
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+
+    if (input === "") {
+      result = 0;
+    } else {
+      const numbers = input.split(regex);
+      // numbers 배열을 사용하여 추가 작업 수행
+    }
+
+    Console.print(result);
   }
 }
 
