bb1a956a05a34ec29659f7322a8840cc3378a370
Erin <fls4804@gmail.com>
2024-10-21 20:08:15 +0900

    기본 및 커스텀 구분자 설정

--- src/App.js
+++ src/App.js
@@ -41,4 +41,28 @@ class App {
       calculateSum(input) {
         // 입력값이 없으면 0 반환
         if (!input) return 0; 
+    
+    
+        // 기본 구분자 설정 (, 또는 :)
+        let delimiter = /,|:/; 
+        // 커스텀 구분자 설정
+        if (input.startsWith("//")) {
+            const newlineIndex = input.indexOf("\\n");
+            if (newlineIndex === -1) {
+              throw new Error("잘못된 형식입니다."); 
+            }
+            
+          //커스텀 구분자 추출
+            const customDelimiter = input.substring(2, newlineIndex);
+            delimiter = new RegExp(this.escapeRegExp(customDelimiter));
+          //숫자 부분만 남기고 커스텀 구분자 제거
+            input = input.slice(newlineIndex + 2);
+          }
+      
+          const numbers = input.split(delimiter).map(Number);
+          
+
       }
+    }
+  }
+}
