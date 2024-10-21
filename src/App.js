27e8f9ea5de6156edfd80e84d520d6a9fecce70f
sieon <sieon.eve@gmail.com>
2024-10-21 20:44:44 +0900

    ✨ Feat: 입력한 문자열 구분자로 계산

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { match } from 'assert';
 import { log } from 'console';
 import readline from 'readline';
 
@@ -9,8 +10,22 @@ class App {
     });
 
     inandout_value.question('값을 입력하세요: ', (userInput) => {
-      console.log('결과:', userInput);
+      try {
+        const result = this.add(userInput);
+        console.log(`결과: ${result}`);
+      } catch (error) {
+        console.log("에러발생");
+      }
+      inandout_value.close();
     });
+  }
+
+  add(input) {
+    const rule = /,|:/;
+    const nums = input.split(rule).map((num) => {
+      return Number(num);
+    });
+    return nums.reduce((sum, num) => sum + num, 0);
   }
 }
 
