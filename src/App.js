48680118d4fae1717977f6bb1be6645795f7a293
Heesoo Jun <acesk123@gmail.com>
2024-10-21 17:50:12 +0900

    feat: 빈 문자열 처리

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ class App {
   async run() {
     try {
       const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-
       const result = this.calculate(input);
-
       MissionUtils.Console.print(`결과 : ${result}`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
-      return;  // 프로그램 흐름을 종료시키기 위해 return 추가
+      return;
     }
+  }
+
+  calculate(input) {
+    if (input === "") {
+      return 0;  // 공백 문자열 처리
+    }
+    return input;  
   }
 }
 
