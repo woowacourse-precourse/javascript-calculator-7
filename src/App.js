280ce1acb4aea9776a40bfb6b2c809b9c0e003ed
crolvlee <leecrolv@gmail.com>
2024-10-21 14:10:47 +0900

    fix: 정규표현식 정리

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { calculator } from "./calculator.js";
 class App {
   async run() {
     try {
-      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
       const result = calculator(input);
 
-      Console.print(`결과 : ${result}`)
+      Console.print(`결과 : ${result}`);
     } catch (error) {
-      throw error
+      throw error;
     }
-
   }
 }
 
--- src/calculator.js
+++ src/calculator.js
@@ -1,39 +1,41 @@
 export function calculator(input) {
-    let baseSeparators = /[,|:]/
+    let baseSeparators = /[,:]/;
     let customSeparator;
-    let numbersPart = input
-    let isCustom = false
-    let numbersArr = []
+    let numbersPart = input;
+    let isCustom = false;
+    let numbersArr = [];
 
     // 입력한 값이 없는 경우
     if (!input) {
         return 0;
     }
 
+    input = input.replace(/\\n/g, '\n');
+
     // 커스텀 구분자를 사용한 경우
     if (input.startsWith('//')) {
         if (!input.includes('\n')) {
-            throw new Error('[ERROR] 커스텀 구분자 뒤에는 \\n이 필요합니다.')
+            throw new Error('[ERROR] 커스텀 구분자 뒤에는 \\n이 필요합니다.');
         }
 
         isCustom = true
         customSeparator = input.substring(2, 3);
-        numbersPart = input.split('\n')[1]
+        numbersPart = input.split('\n')[1];
     }
 
     // 구분자를 기준으로 분리
     if (isCustom == true) {
-        numbersArr = numbersPart.split(new RegExp(`[${customSeparator}]`))
+        numbersArr = numbersPart.split(customSeparator);
     } else {
-        numbersArr = numbersPart.split(baseSeparators)
+        numbersArr = numbersPart.split(baseSeparators);
     }
 
     // 양수인지 검증
     numbersArr = numbersArr.map(Number);
     if (numbersArr.some(num => num <= 0)) {
-        throw new Error('[ERROR] 음수나 0은 입력할 수 없습니다.')
+        throw new Error('[ERROR] 음수나 0은 입력할 수 없습니다.');
     }
 
     // 합을 반환
-    return numbersArr.reduce((sum, num) => sum + num, 0)
+    return numbersArr.reduce((sum, num) => sum + num, 0);
 }
