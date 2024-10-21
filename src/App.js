13e1923bffd7016d918fb8854721111538568b5a
MyungJiwoo <1206jiwoo@gmail.com>
2024-10-21 20:32:03 +0900

    feat: 커스텀 구분자 덧셈 기능

--- src/App.js
+++ src/App.js
@@ -11,9 +11,22 @@ class App {
 
             // case1: 기본 구분자(,:) 케이스
             const NORMAL_SEPARATOR = /^[0-9,\:]+$/; // 기본 구분자와 숫자로만 구성되어 있는가?
-            let separatorCase = NORMAL_SEPARATOR.test(inputData);
-            if (separatorCase) {
+            let separatorNormalCase = NORMAL_SEPARATOR.test(inputData);
+            if (separatorNormalCase) {
                 const exportNumbers = inputData.split(/[, :]+/);
+                result = exportNumbers.reduce(
+                    (acc, num) => acc + Number(num),
+                    0
+                );
+            }
+
+            // case2: 커스텀 구분자 케이스
+            const CUSTOM_SEPARATOR = /^\/\/(.+?)\\n(.*)$/; // "//구분자\n"의 커스텀 구분자 케이스인가?
+            let separatorCustomCase = CUSTOM_SEPARATOR.exec(inputData); // 정규식에서 구분자와 계산식을 분리
+            if (separatorCustomCase) {
+                const customSeparator = separatorCustomCase[1];
+                const exportNumbers =
+                    separatorCustomCase[2].split(customSeparator);
                 result = exportNumbers.reduce(
                     (acc, num) => acc + Number(num),
                     0
@@ -24,6 +37,7 @@ class App {
             Console.print(`결과 : ${result}`);
         } catch (error) {
             Console.print(`[ERROR] : ${error.message}`);
+            throw error;
         }
     }
 }
