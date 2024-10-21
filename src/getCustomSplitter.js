037243025f5aaeccee81d4142aef14467898e5d2
Jaewon <wellbird@naver.com>
2024-10-21 14:11:43 +0900

    fix : 리턴 형태 수정

--- src/getCustomSplitter.js
+++ src/getCustomSplitter.js
@@ -1,10 +1,10 @@
 function getCustomSplitter(userInput) {
-  if (userInput.indexOf('//') !== 0) return null, userInput;
+  if (userInput.indexOf('//') !== 0) return [null, userInput];
 
   userInput = userInput.slice(2);
   const [splitter, string] = userInput.split('\\n');
 
-  return splitter, string;
+  return [splitter, string];
 }
 
 export default getCustomSplitter;
