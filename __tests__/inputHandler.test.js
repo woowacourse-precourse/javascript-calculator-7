bf1d4e9971f823184fe2355869e72636f8b0b3cf
Jaeho Cha <ischar@naver.com>
2024-10-21 13:39:15 +0900

    refactor(input): remove display message test in inputHandler.test.js
    - 문자열이 올바르게 표시되는 지 검증하는 테스트 삭제

--- __tests__/inputHandler.test.js
+++ __tests__/inputHandler.test.js
@@ -12,11 +12,6 @@ describe("inputHandler 테스트", () => {
     jest.restoreAllMocks();
   });
 
-  test("Console.print가 올바른 메시지를 출력하는지 확인", async () => {
-    await inputHandler();
-    expect(Console.print).toHaveBeenCalledWith("덧셈할 문자열을 입력해 주세요."); // 메시지를 출력하는 지 검증
-  });
-
   test("Console.readlineAsync로 입력을 받아서 반환하는지 확인", async () => {
     const INPUT = await inputHandler();
     expect(INPUT).toBe("1,2,3"); // 모킹된 값인 "1,2,3"이 반환되는지 검증
