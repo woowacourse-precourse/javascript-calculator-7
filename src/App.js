cb6a00e246041d0188d2cabcf38644de291c766f
sunkiteo-o <hyhy020301@gmail.com>
2024-10-21 18:23:34 +0900

    fix: 양수 처리 부분 수정

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     while (1) {
-      let INPUT = await Console.readLineAsync(
+      let input = await Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n"
       );
 
       let re = new RegExp("//(.*?)\\\\n");
-      const re_num = new RegExp("[0-9]");
       let split_input;
-      let OUTPUT;
+      let output;
 
-      if ((split_input = re.exec(INPUT)) !== null) {
+      if ((split_input = re.exec(input)) !== null) {
         let new_re = split_input[1];
-        let new_input = INPUT.replace(re, "");
-        OUTPUT = new_input.split(new_re).map(Number);
+        let new_input = input.replace(re, "");
+        output = new_input.split(new_re).map(Number);
       } else {
         let default_re = new RegExp(",|:");
-        OUTPUT = INPUT.split(default_re).map(Number);
+        output = input.split(default_re).map(Number);
+        if (input.includes("-")) {
+          Console.print("[ERROR]");
+          break;
+        }
       }
-      if (OUTPUT.includes(NaN)) {
+      if (output.includes(NaN)) {
         Console.print("[ERROR]");
         break;
       }
-      const sum = OUTPUT.reduce((acc, cur) => acc + cur, 0);
+      const sum = output.reduce((acc, cur) => acc + cur, 0);
       Console.print("결과 : " + sum);
     }
   }
