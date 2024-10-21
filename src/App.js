461e8688600337458e997957e4a2671dc93f84cb
dozin <dojin0345@gmail.com>
2024-10-21 17:07:48 +0900

    feat:console 입출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await this.readInput();
+      this.printResult(0);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
+  async readInput() {
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    return await Console.readLineAsync();
+  }
+
+  printResult(result) {
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
