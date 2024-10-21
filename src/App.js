9340765599567efb5640193f16470888733b604d
egg-silver <v6984@naver.com>
2024-10-21 19:43:12 +0900

    feat: 프로그램 실행 시 문자열 입력받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import { CALCULATION_MESSAGE } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    // "덧셈할 문자열을 입력해 주세요." 메시지 출력
+    Console.print(CALCULATION_MESSAGE.START_CALCULATION);
+
+    // 사용자로부터 값을 입력받기 (비동기 처리)
+    const input = await Console.readLineAsync('');
+  }
 }
 
-export default App;
+const app = new App();
+app.run();
