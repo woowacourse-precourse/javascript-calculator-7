51b59437739dd7e5055d50afb8b445fca33eab99
YongHyunKing <yhkim8246@gmail.com>
2024-10-21 09:25:31 +0900

    refator(Calculator): map과reduce를 사용한 코드 간략화

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,26 +2,16 @@ import { ErrorMessages, EMPTY_STRING } from "./Strings.js";
 
 class Calculator {
   static checkIsPositiveNumber(numStrArray) {
-    const result = [];
-    numStrArray.forEach((numStr) => {
-      if (numStr === EMPTY_STRING) {
-        result.push(0);
-        return;
-      }
+    return numStrArray.map(numStr => {
+      if (numStr === EMPTY_STRING) return 0;
       const parsedNum = Number(numStr);
       if (isNaN(parsedNum) || parsedNum <= 0) throw new Error(ErrorMessages.CALCULATOR_CHECK_IS_POSITIVENUMBER);
-      result.push(parsedNum);
+      return parsedNum;
     });
-
-    return result;
   }
 
   static addNumbers(numArray) {
-    let sum = 0;
-    numArray.forEach(num => {
-      sum += Number(num);
-    });
-    return sum;
+    return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
 
