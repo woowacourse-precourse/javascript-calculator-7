c6ff5adaa7f97dc986977a68d2ac805c49fcba88
심우석 <dntjr5741@naver.com>
2024-10-21 16:05:21 +0900

    fix : [ERROR]메시지와 함께 실제 Error를 발생시킨 후 종료시키기

--- src/App.js
+++ src/App.js
@@ -27,25 +27,26 @@ class App {
         const parsed = Number(num);
 
         if (isNaN(parsed)) {
-          throw new Error("[ERROR] 잘못된 입력입니다.");
+          throw new Error(" 잘못된 입력입니다.");
         }
 
         if (!Number.isInteger(parsed)) {
-          throw new Error("[ERROR] 소수는 입력할 수 없습니다.");
+          throw new Error(" 소수는 입력할 수 없습니다.");
         }
 
         return parsed;
       });
 
       if (parsedNumbers.some((num) => num < 0)) {
-        throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
+        throw new Error(" 음수는 입력할 수 없습니다.");
       }
 
       const result = parsedNumbers.reduce((sum, num) => sum + num, 0);
 
       MissionUtils.Console.print(`결과 : ${result}`);
     } catch (error) {
-      MissionUtils.Console.print(error.message);
+      MissionUtils.Console.print("[ERROR]" + error.message);
+      console.error(error);
     }
   }
 }
