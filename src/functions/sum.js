a94893ef3a14370187ccb12621a21c11ea4f7103
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-21 22:56:49 +0900

    fix: 에러 처리 수정

--- src/functions/sum.js
+++ src/functions/sum.js
@@ -2,9 +2,10 @@ export default function sum(sliced) {
   let sum = 0;
   console.log(sliced);
   for (const ELM of sliced) {
-    if (typeof Number(ELM) != NaN) {
-      sum += Number(ELM);
-    } else throw "[ERROR] 숫자만 더할 수 있어요.";
+    const value = Number(ELM);
+    if (value == NaN) throw new Error("[ERROR] 숫자만 더할 수 있어요.");
+    else if (value < 0) throw new Error("[ERROR] 양수만 더할 수 있어요.");
+    else sum += Number(ELM);
   }
   return sum;
 }
