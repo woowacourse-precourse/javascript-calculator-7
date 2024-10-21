b20873a436064a163ff8e6f816dd164bedc8354f
GSIII <kalseo901@gmail.com>
2024-10-21 15:03:33 +0900

    feat(logic) : 커스텀 구분자 추출 및 커스텀 구분자 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
-import { isValidInput } from "./utils.js";
+import { separatorSplit } from "./utils.js";
 
 class App {
   async run() {
     let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    isValidInput(input);
+    separatorSplit(input);
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,25 @@
-export const isValidInput = (input) => {
-  console.log(input);
+export const separatorSplit = (input) => {
+  if (!input || input.trim() === "") {
+    return [];
+  }
+
+  const basicSeparator = /[;:]/; // 기본 구분자 ; 또는 :
+  let customSeparator = "";
+
+  const customRegex = /\/\/([^\\n]+)(?=\\n)/;
+
+  // 사용자 정의 구분자가 있는지 확인
+  const match = input.match(customRegex);
+  if (match) {
+    customSeparator = match[1]; // 구분자 추출
+
+    if (customSeparator.length !== 1) {
+      throw new Error(
+        "[ERROR_INVALID_SEPARATOR] : 구분자에 문자열을 입력할 수 없습니다."
+      );
+    }
+
+    input = input.substring(match[0].length); // 숫자 부분 추출
+  }
+  input = input.replace(/\\n/g, "\n").trim(); // "\n"을 문자로 처리
 };
