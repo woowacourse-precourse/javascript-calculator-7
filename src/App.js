280feae10b2015e63d1aaea0557edf5d418e1c21
Minho Kim <wweer4748@naver.com>
2024-10-21 20:52:06 +0900

    숫자가 포함된 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  async run() { }
+  run() {
+    return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n')
+      .then((input) => {
+        const numbers = this.parseNumbers(input);
+        Console.print(입력된 숫자들 : ${ numbers });
+      })
+      .catch((error) => {
+        Console.print(error.message);
+        throw error;
+      });
+  }
+
+  parseNumbers(input) {
+    let delimiters = [',', ':'];
+    let numbersString = input;
+
+  }
 }
 
-export default App;
+export default App
