ba48b661f7435f4af820c2eff7c4739b540b99f4
정해성 <gotjd0230@naver.com>
2024-10-21 23:28:50 +0900

    refactor: 기본 구분자 관심사 분리

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import { Console } from '@woowacourse/mission-utils';
 import { validateLine, validateCustomPart, validateLinePart } from './utils/stringValidator.js';
 import {
   checkIsDefaultString,
-  sumString,
+  sumString as sumLine,
   divideCustom,
-  addCustomSeparator,
+  getFinalSeparators,
 } from './utils/stringParser.js';
-import { DEFAULT_SEPARATORS } from './constants.js';
 
 class App {
   async run() {
-    const separators = [...DEFAULT_SEPARATORS];
-
     const line = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
     validateLine(line);
     
-    if (checkIsDefaultString(line)) return Console.print(`결과 : ${sumString(line, separators)}`);
+    if (checkIsDefaultString(line)) return Console.print(`결과 : ${sumLine(line)}`);
 
     const [customPart, linePart] = divideCustom(line);
     validateCustomPart(customPart);
 
-    addCustomSeparator(separators, customPart)
+    const separators = getFinalSeparators(customPart);
+    Console.print(separators);
     validateLinePart(linePart, separators);
 
-    return Console.print(`결과 : ${sumString(linePart, separators)}`);
+    return Console.print(`결과 : ${sumLine(linePart, separators)}`);
   }
 }
 
--- src/utils/stringParser.js
+++ src/utils/stringParser.js
@@ -1,3 +1,4 @@
+import { Console } from '@woowacourse/mission-utils';
 import { DEFAULT_SEPARATORS } from '../constants.js';
 
 export const checkIsDefaultString = (line) => {
@@ -9,22 +10,23 @@ export const checkIsDefaultString = (line) => {
 export const divideCustom = (str) => {
   const customSeparatorPart = str.substring(0, 5);
   const targetStringPart = str.substring(5);
+
   return [customSeparatorPart, targetStringPart];
 };
 
-export const addCustomSeparator = (separators, customPart) => {
-  separators.push(customPart[2]);
-  return separators;
-};
+export const getFinalSeparators = (customPart) => [...DEFAULT_SEPARATORS, customPart[2]];
 
 export const separate = (str, separators) => {
   const replacedString = separators.reduce(
     (acc, cur) => acc.replaceAll(cur, ','),
     str
   );
+
   return replacedString.split(',').map(Number);
 };
 
-export const sum = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
+export const sumString = (line, separators = DEFAULT_SEPARATORS) => {
+  const separatedNumbers = separate(line, separators);
 
-export const sumString = (line, separators) => sum(separate(line, separators));
+  return separatedNumbers.reduce((acc, cur) => acc + cur, 0);
+};
