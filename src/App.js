17fadbd7d42676cba10f19ba8d471dcfc6b7183b
dozin <dojin0345@gmail.com>
2024-10-21 17:51:41 +0900

    feat: 쉼표, 콜론  처리

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ class App {
   async run() {
     try {
       const input = await this.readInput();
-      this.printResult(0);
+      const result = this.calculate(input);
+      this.printResult(result);
     } catch (error) {
       Console.print(error.message);
     }
@@ -18,14 +19,18 @@ class App {
   calculate(input) {
     if (input === "") return 0;
 
-    const number = parseInt(input, 10);
-    if (isNaN(number)) {
-      throw new Error("[ERROR] 유효하지 않은 입력값입니다.");
-    }
-    if (number < 0) {
-      throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
-    }
-    return number;
+    const normalizedInput = input.replaceAll(":", ",");
+    const numbers = normalizedInput.split(",").map((num) => {
+      const parsed = parseInt(num, 10);
+      if (isNaN(parsed)) {
+        throw new Error("[ERROR] 유효하지 않은 입력값입니다.");
+      }
+      if (parsed < 0) {
+        throw new Error("[ERROR] 음수는 입력할 수 없습니다.");
+      }
+      return parsed;
+    });
+    return numbers.reduce((sum, num) => sum + num, 0);
   }
 
   printResult(result) {
