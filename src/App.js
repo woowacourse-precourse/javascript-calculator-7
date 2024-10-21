b62bfb7faf089c19ecdcc6fe64bcc89e8e4e80be
norangsuji <sujihong93@gmail.com>
2024-10-21 15:43:33 +0900

    feat(app.js): add input functionality
    Implement INPUT to receive user input. If an empty string is entered, output 0 and terminate the program.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ class App {
   // 전체 실행 흐름
   async run() {
     Console.print("덧셈할 문자열을 입력해 주세요."); // 시작
+
+    const INPUT = await Console.readLineAsync(""); // 사용자 입력
+    // 빈 문자열일 경우 결과를 0으로 출력
+    if (INPUT.trim() === "") {
+      Console.print("결과 : 0"); // 결과
+      return; // 함수 종료
+    }
   }
 }
 
