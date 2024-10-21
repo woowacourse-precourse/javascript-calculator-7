1c81daa8120112b799c6d459a092879e2cfe8c7a
osw6858 <osw7875@naver.com>
2024-10-21 12:41:07 +0900

    feat: 입력값이 비어있을 경우 유효성 검사 추가

--- src/calculator.js
+++ src/calculator.js
@@ -2,12 +2,17 @@ import {Console} from '@woowacourse/mission-utils';
 
 export async function calculator() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.')
-    const parseResult = parseInput(input)
-    const result = add(parseResult)
-    Console.print(result)
+    try {
+        const parseResult = parseInput(input);
+        const result = add(parseResult);
+        Console.print(result);
+    } catch (error) {
+        Console.print(error.message);
+    }
 }
 
 function parseInput(input) {
+    validateInput(input)
     if(input.startsWith('//')) {
         const [custom, number] = input.split(/\\n/)
         const separator = custom.substring(2)
@@ -24,3 +29,10 @@ function parseInput(input) {
 function add(parseResult) {
     return parseResult.reduce((acc, cur) => acc + cur, 0)
 }
+
+function validateInput(input) {
+    if (input.trim() === '') {
+        throw new Error('입력값이 비어있습니다.');
+    }
+}
+
