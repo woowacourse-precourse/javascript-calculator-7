871ca37cea59ba53d417981f38d49bcba6587aa4
wiseah <hyunah2765@gmail.com>
2024-10-21 17:55:41 +0900

    fix(App.js): correct regular expression to handle newline for custom delimiters

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    Console.print("덧셈할 문자열을 입력해주세요.");
-    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.: ");
+    Console.print("덧셈할 문자열을 입력해주세요.")
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요. : ");
     const reg = input.match(/\/\/(.*?)\\n(.*)/);
-    const result = reg ? reg[2].split(new RegExp(`[,:${reg[1]}]`)) : input.split(/[,:]/);
-
+    const result = reg ? reg[2].split(new RegExp(`[,:${reg[1]}]`)) : input.split(/[,:]/)
     const sum = result.map((value) => {
       const num = Number(value);
       if (isNaN(num) || num <= 0) throw new Error("[ERROR]");
       return num;
     }).reduce((prev, now) => prev + now, 0);
-
     Console.print("결과 : " + sum);
   }
 }
