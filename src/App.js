f3ef28c32568253ea4fa1c4e959e0dbe8d8b0f68
vgotu99 <vgotu99@gmail.com>
2024-10-21 11:21:45 +0900

    feat(parser): 구분자를 이용한 숫자 분리 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.CUSTOM_DELIMITER_REGEX = /^\/\/(.)\n(.*)/;
-    this.DEFAULT_DELIMITER = ',|:';
+    this.DEFAULT_DELIMITER = ",|:";
   }
 
   async run() {
-    const INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const INPUT = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
     const { DELIMITER, NUMBERS } = this.parseInput(INPUT);
-    Console.print(`구분자: ${DELIMITER}`);
-    Console.print(`숫자 문자열: ${NUMBERS}`);
+    const PARSED_NUMBERS = this.splitAndParse(NUMBERS, DELIMITER);
+    Console.print(`분리된 숫자들: ${PARSED_NUMBERS.join(", ")}`);
   }
 
   parseInput(input) {
@@ -22,7 +24,12 @@ class App {
 
     return { DELIMITER: this.DEFAULT_DELIMITER, NUMBERS: input };
   }
-}
 
+  splitAndParse(numbers, delimiter) {
+    return numbers
+      .split(new RegExp(delimiter))
+      .map((num) => parseInt(num.trim(), 10));
+  }
+}
 
 export default App;
