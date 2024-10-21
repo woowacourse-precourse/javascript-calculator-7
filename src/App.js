cccf0e0a9c499708a63671ca2b13ce44959eb91e
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-21 13:07:32 +0900

    feat(App.js): 커스텀 구분자 지정 기능
     startsWith으로 //로 시작하고 indexOf로 \n을 포함하는걸 확인하여, 포함 한다면 새로운 DIVIDER(구분자)가 할당 되도록 함

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 class App {
-    async run(STR) {
+    async run(STR = "//,\n123:21,23") {
+        let DIVIDER = /[,|:]/;
+        let STR_NUMBERS = STR;
+
         if (STR === "") return 0;
-        else {
-            var STR_ARRAY = STR.split(/[,|:]/);
+        else if (STR.startsWith("//") && STR.indexOf("\n") !== -1) {
+            DIVIDER = new RegExp(`[${STR[2]}]`);
+            STR_NUMBERS = STR.slice(STR.indexOf("\n") + 1);
+            console.log(STR_NUMBERS);
+            console.log(STR_NUMBERS.split(DIVIDER));
         }
+        console.log(STR_NUMBERS.split(DIVIDER));
     }
 }
 
