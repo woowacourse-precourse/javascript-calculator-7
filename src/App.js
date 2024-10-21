b6fa3e8f7b5b91c417b035876794df9fb3ac7442
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-21 21:53:32 +0900

    refactor: Calculator 생성 시 검증 결과를 직접 전달하도록 코드 간소화 & 계산 결과를 변수에 저장하지 않고 직접 출력하도록 코드 간소화

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ class App {
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const validator = new InputValidator(input);
-    const validationResult = validator.validateInput();
-    const calculator = new Calculator(input, validationResult);
-    const sum = calculator.calculate();
+    const calculator = new Calculator(input, validator.validateInput());
 
-    Console.print(`결과 : ${sum}`);
+    Console.print(`결과 : ${calculator.calculate()}`);
   }
 }
 
