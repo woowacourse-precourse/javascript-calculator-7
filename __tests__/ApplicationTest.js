8dabd703ab10ae8af929ae75ad6ad6941f93ed8f
Jee Hwan <jeehwan98@naver.com>
2024-10-21 14:51:06 +0900

    feature: 커스텀 구분자를 사용했지만, 사용하지 않고 입력한 값이 숫자만일 때

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,8 +32,71 @@ describe("문자열 계산기", () => {
     });
   });
 
+  test("커스텀 구분자를 사용한 여러 번호들", async () => {
+    const inputs = ["//;\\n1;3;2"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", async () => {
     const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자를 썼지만 사용하지 않음", async () => {
+    const inputs = ["//;\\n1,3,2"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자를 썼지만 사용하지 않음", async () => {
+    const inputs = ["//***\\n1***3***2"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자를 정했지만 숫자는 입력하지 않은 상태", async () => {
+    const inputs = ["//;\\n"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자를 정했지만 존재하지 않는 커스텀 구분자 사용", async () => {
+    const inputs = ["//;\\n8[1[3"];
     mockQuestions(inputs);
 
     const app = new App();
--- src/App.js
+++ src/App.js
@@ -13,12 +13,13 @@ class App {
   }
 }
 
+// 구분자랑 숫자를 분리 시켜주는 함수
 function splitValues(input) {
   if (typeof input !== 'string') {
     throw new Error('[ERROR] 입력한 값이 문자열이 아닙니다');
   }
 
-  // 커스텀 구분자랑 같이 입력을 했으면
+  // 커스텀 구분자를 사용했으면
   if (input.startsWith('//')) {
     const { delimiter, numbers } = splitCustomDelimiter(input);
     return { delimiter, numbers };
@@ -27,6 +28,7 @@ function splitValues(input) {
   return { delimiter: commonDelimiter, numbers: input };
 }
 
+// 구분자를 분리해주는 함수
 function splitCustomDelimiter(input) {
   const lineIndex = input.indexOf('\\n');
 
@@ -37,11 +39,28 @@ function splitCustomDelimiter(input) {
     }
 
     const inputtedNumber = input.substring(lineIndex + 2);
+
     if (!inputtedNumber) {
       throw new Error('[ERROR] 값을 입력하지 않았습니다');
     }
 
+    // //;\n9,1:3:2일 경우 (구분자 정했지만 사용하지 않고 common 구분자를 사용할 때는 구분자를 common delimiter로 정한다)
     if (!inputtedNumber.includes(inputtedDelimiter)) {
+      console.log('inputted number⚡️:', inputtedNumber);
+      console.log('inputtedDelimiter:', inputtedDelimiter);
+
+      // 커스텀 구분자를 사용했지만 입력한 값은 숫자만 있을 때
+      if (+inputtedNumber > 0) {
+        return { delimiter: commonDelimiter, numbers: inputtedNumber };
+      }
+      // 입력한 값에 commonDelimiter가 존재하는지 확인하는 로직
+      const isUsingCommonDelimiter = commonDelimiter.some(delim => inputtedNumber.includes(delim));
+
+      if (!isUsingCommonDelimiter) {
+        throw new Error('[ERROR] 제공된 구분자 외 다른 구분자를 사용했습니다');
+      }
+
+      //;\n1
       return { delimiter: commonDelimiter, numbers: inputtedNumber };
     }
 
@@ -51,21 +70,25 @@ function splitCustomDelimiter(input) {
   }
 }
 
+// 숫자를 분리 해주는 함수
 function splitNumbers(delimiter, numbers) {
-  console.log("delimiter:", delimiter);
-  console.log("numbers:", numbers);
   if (Array.isArray(delimiter)) {
     const delimiterPattern = new RegExp(`[${delimiter.join('')}]`);
-    console.log('delimiterPattern:', delimiterPattern);
     return numbers.split(delimiterPattern);
+  }
+
+  if (!numbers.includes(delimiter)) {
+    throw new Error('[ERROR] 커스텀 구분자를 정해주세요');
   }
 
   return numbers.split(delimiter);
 }
 
+// 계산
 function calculate(values) {
   const negativeNumbers = values.filter(num => Number(num) < 0);
 
+  // 음수: 에러
   if (negativeNumbers.length > 0) {
     throw new Error('[ERROR] 음수는 허용되지 않습니다');
   }
