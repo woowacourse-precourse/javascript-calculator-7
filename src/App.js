54814f7637541b5c6270a887ad322e0c5d6e6a80
yewonhihi <yewon1118@naver.com>
2024-10-21 22:13:11 +0900

    feat(App.js): 커스텀 구분자가 있는지 확인

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    let delimiters = [',', ':'];
+    let expression = input.trim();
+
+    if (input.startsWith('//') && input.includes("\n")) {
+      const { customDelimiter, newExpression } = this.checkCustomDelimiter(input);
+      delimiters.push(customDelimiter);
+      expression = newExpression;
+    }
+  }
+
+  checkCustomDelimiter(str) {
+    const endOfCustomDelimiter = str.indexOf("\n");
+    const expression = str.substring(endOfCustomDelimiter + 1);
+    return { customDelimiter, expression };
   }
 }
 
