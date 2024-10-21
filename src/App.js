ba94bfa05c8472ef7f8685ad6b7ca9563af4f78a
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-10-21 22:26:35 +0900

    refactor(InputProcessor): 클래스 이름 변경
    - InputProcessor에서 IOProcessor로 클래스명 변경
    - 클래스에서 입력 뿐만 아닌 출력 기능까지 포함

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import InputProcessor from "./InputProcessor.js";
+import IOProcessor from "./IOProcessor.js";
 import StringParser from "./StringParser.js";
 import StringCalculator from "./StringCalculator.js";
 
 class App {
   constructor() {
-    this.inputProcessor = new InputProcessor();
+    this.ioProcessor = new IOProcessor();
     this.stringParser = new StringParser();
     this.StringCalculator = new StringCalculator();
   }
 
   async run() {
-    const input = await this.inputProcessor.processInput();
+    const input = await this.ioProcessor.processInput();
     const numbers = this.stringParser.parseString(input);
     const result = this.StringCalculator.calculate(numbers);
   }
