bbe0a725b6c36ca9307d598221efb99b27ea7dbd
AnCha <tory912@gmail.com>
2024-10-21 19:57:18 +0900

    feat: 사용자 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await Console.readLineAsync(
+      "덧셈할 문자열을 입력하여 주세요."
+    );
+  }
 }
 
 export default App;
