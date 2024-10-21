77102470c73f493346aff462d6b63b48fe8b952f
sanghyeonLee <hyeon012366@gmail.com>
2024-10-21 22:56:11 +0900

    feat : 커스텀 구분자 에러 처리 함수 호출

--- src/utils/stringProcessor.js
+++ src/utils/stringProcessor.js
@@ -1,3 +1,5 @@
+import {handleCustomSeparatorError} from "./errorHandler.js";
+
 export function parseInput(param) {
     const isCustom = param.startsWith("//") && param.includes("\\n");
     if (isCustom) {
@@ -9,6 +11,7 @@ export function parseInput(param) {
 export function sliceString(param) {
     const lastIdx = param.indexOf("\\n");
     const customSeparator = param.slice(2, lastIdx);
+    handleCustomSeparatorError(customSeparator)
     const slicedString = param.slice(lastIdx + 2);
     return {customSeparator, slicedString};
 }
