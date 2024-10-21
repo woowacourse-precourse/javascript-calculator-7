eb8428f560a64c7ffa120139d145ab0d3296d6cb
anonymousrecords <a.Records2022@gmail.com>
2024-10-21 19:05:22 +0900

    refactor(calculator): 중복된 코드 제거

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { ERROR_MESSAGES } from "./errorMessages.js";
 
 class App {
   async run() {
-    try {
-      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
-      const answer = Calculator.calculate(input);
-      Console.print(`결과 : ${answer}`);
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    const answer = Calculator.calculate(input);
+    Console.print(`결과 : ${answer}`);
   }
 }
 
@@ -19,64 +15,55 @@ class Calculator {
       return 0;
     }
 
-    let divider = this.setDivider(input);
-    let numberArray = this.createNumberArray(input, divider);
+    const { divider, numberString } = this.parseInput(input);
+    const numberArray = this.createNumberArray(numberString, divider);
     return this.sum(numberArray);
   }
 
-  static setDivider(input) {
-    let defaultDivider = [",", ":"];
-
+  static parseInput(input) {
     if (input.startsWith("//")) {
       const endIndex = input.indexOf("\\n");
+      const customDivider = input.slice(2, endIndex);
+      const numberString = input.slice(endIndex + 2);
 
-      return input.slice(2, endIndex);
+      if (numberString.length === 0) {
+        throw new Error(ERROR_MESSAGES.NO_NUMBERS_INPUT);
+      }
+      if (!numberString.includes(customDivider)) {
+        throw new Error(ERROR_MESSAGES.CUSTOM_DIVIDER_NOT_USED);
+      }
+
+      return { divider: customDivider, numberString };
     }
 
-    if (input.includes(",") || input.includes(":")) {
-      return defaultDivider;
+    const defaultDivider = [",", ":"];
+    if (defaultDivider.some((d) => input.includes(d))) {
+      return { divider: defaultDivider, numberString: input };
     }
 
     throw new Error(ERROR_MESSAGES.NO_VALID_DIVIDER);
   }
 
-  static createNumberArray(input, divider) {
-    let numberString = input;
-
-    if (numberString.startsWith("//")) {
-      const endIndex = numberString.indexOf("\\n");
-
-      numberString = numberString.slice(endIndex + 2);
-
-      if (numberString.length === 0) {
-        throw new Error(ERROR_MESSAGES.NO_NUMBERS_INPUT);
-      }
-
-      if (typeof divider === "string" && !numberString.includes(divider)) {
-        throw new Error(ERROR_MESSAGES.CUSTOM_DIVIDER_NOT_USED);
-      }
-
-      return numberString.split(divider).map(Number);
-    }
-
+  static createNumberArray(numberString, divider) {
     if (Array.isArray(divider)) {
       divider.forEach((d) => {
         numberString = numberString.split(d).join(",");
       });
-
-      return numberString.split(",").map(Number);
+      divider = ",";
     }
 
-    return numberString.split(divider).map(Number);
+    let numberArray = numberString.split(divider).map(Number);
+
+    return numberArray;
   }
 
   static sum(numberArray) {
-    if (numberArray.some((num) => num < 0)) {
-      throw new Error(ERROR_MESSAGES.NEGATIVE_NUMBER);
-    }
-
     if (numberArray.some(isNaN)) {
       throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    }
+    
+    if (numberArray.some((num) => num < 0)) {
+      throw new Error(ERROR_MESSAGES.NEGATIVE_NUMBER);
     }
 
     return numberArray.reduce((acc, cur) => acc + cur, 0);
