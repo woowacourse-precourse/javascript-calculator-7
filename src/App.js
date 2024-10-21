81537d81097bbc33c595c43eb8067555fea8b678
thingjin <thingjin@naver.com>
2024-10-21 21:20:17 +0900

    feat: 커스텀 구분자 여부에 따른 문자열 분리 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
+    let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+
+    if (input == "") {
+      Console.print(0);
+      return;
+    }
+
+    let numbers;
+    let customDelimiter;
+
+    // 커스텀 구분자가 있는지 확인
+    const customDelimiterMatch = input.match(/^\/\/(.)\\n/);
+    if (customDelimiterMatch) {
+      customDelimiter = customDelimiterMatch[1];
+      input = input.split("\\n")[1];
+    }
+
+    // 문자열을 기본 구분자(쉼표, 콜론) 또는 커스텀 구분자를 기준으로 분리
+    if (customDelimiter) {
+      numbers = input.split(new RegExp(`[${customDelimiter},:]`));
+    } else {
+      numbers = input.split(/[,|:]/);
+    }
   }
 }
 
