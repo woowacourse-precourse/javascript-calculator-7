58fa41a21401223f15b493fb0c66f985d336ed22
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-21 23:01:45 +0900

    feat(CustomCalculate): 커스텀 구분자에 대해서 덧셈하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -22,9 +22,24 @@ class App {
     }
 
     calculate(input) {
-        if (!input) return 0; // 빈 문자열일 경우 0 반환
+        if (!input) return 0;
 
         let delimiter = /,|:/; // 기본 구분자 (쉼표, 콜론)
+
+        // 커스텀 구분자 처리
+        if (input.startsWith('//')) {
+            // 정규 표현식 수정
+            const customDelimiterMatch = input.match(/^\/\/(.)\\n/); // //로 시작해야하고, 줄바꿈 문자 전의 커스텀 문자를 인식함
+
+            // 커스텀 구분자 추출
+            const customDelimiter = customDelimiterMatch[1];
+
+
+            delimiter = new RegExp(`[${customDelimiter}]`, 'g'); // 커스텀 구분자를 정규식으로 변환.
+
+            // 구분자 이후의 숫자 문자열로 변경
+            input = input.slice(customDelimiterMatch[0].length); // 커스텀 구분자 제거
+        }
 
         // 모든 구분자로 숫자 분리
         const numbers = input.split(delimiter).map((num) => {
