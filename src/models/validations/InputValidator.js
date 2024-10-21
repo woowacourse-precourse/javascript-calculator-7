689d3a81a85923177fc6bab86d1ec0211667c5a1
luminox93 <memoji1236@gmail.com>
2024-10-21 14:51:51 +0900

    fix: InputValidator 수정
    1. InputValidator 단순화

--- src/models/validations/InputValidator.js
+++ src/models/validations/InputValidator.js
@@ -1,51 +1,13 @@
-// models/InputValidator.js
-
-import { isValidNumbers } from './isValidNumbers.js';
+import { isValidInputType } from './isValidInputType.js';
 import { isValidCustomSeparatorDeclaration } from './isValidCustomSeparatorDeclaration.js';
-import { extractCustomSeparator } from '../utils/extractCustomSeparator.js';
+import { isValidNumbers } from './isValidNumbers.js';
 
 export class InputValidator {
-	constructor(input) {
-		this.input = input;
-		this.separators = [',', ':'];
+	static validateStrings(input) {
+		isValidInputType(input);
+		isValidCustomSeparatorDeclaration(input);
 	}
-
-	validateCustomSeparator(input) {
-		return isValidCustomSeparatorDeclaration(input);
-	}
-
-	getCustomSeparator(input) {
-		return extractCustomSeparator(input);
-	}
-
-	combineSeparators(defaultSeparators, customSeparators) {
-		return [...defaultSeparators, ...customSeparators];
-	}
-
-	extractNumbersPart(input) {
-		if (input.startsWith('//')) {
-			const index = input.indexOf('\n');
-			return input.slice(index + 1);
-		}
-		return input;
-	}
-
-	validateNumbers(numbersPart, separators) {
-		return isValidNumbers(numbersPart, separators);
-	}
-
-	validateInput() {
-		this.validateCustomSeparator(this.input);
-		const customSeparator = this.getCustomSeparator(this.input);
-		let combinedSeparators;
-		if (customSeparator) {
-			combinedSeparators = this.combineSeparators(this.separators, [
-				customSeparator,
-			]);
-		} else {
-			combinedSeparators = this.combineSeparators(this.separators, []);
-		}
-		const numbersPart = this.extractNumbersPart(this.input);
-		return this.validateNumbers(numbersPart, combinedSeparators);
+	static validateNumbers(numbers) {
+		isValidNumbers(numbers);
 	}
 }
