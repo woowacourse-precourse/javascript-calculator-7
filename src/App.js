f454bc93b4549954ced072d28dae788b73f1bd61
zziyuni <86959644+zziyuni@users.noreply.github.com>
2024-10-21 15:25:32 +0900

    feat(calculator): handle empty string input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  //문자열 덧셈 계산기 함수
+  calculator(numbers) {
+    // 예외 사항 1: 빈 문자열 처리
+    if (numbers === "") return 0;
+  }
   async run() {
-    // 사용자 입력
-    const input = await Console.readLineAsync(
-      "구분자와 양수로 구성된 문자열을 입력해주세요: "
-    );
+    try {
+      // 사용자 입력
+      const input = await Console.readLineAsync(
+        "구분자와 양수로 구성된 문자열을 입력해주세요: "
+      );
+      // 계산 결과
+      const result = this.calculator(input);
+    } catch (e) {}
   }
 }
 
