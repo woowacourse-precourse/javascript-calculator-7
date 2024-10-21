ead9bfd9ff5d674b1c9fdc54b33b5c32a2c00859
sunkiteo-o <hyhy020301@gmail.com>
2024-10-21 21:44:21 +0900

    fix: 에러 처리 기능 수정

--- src/App.js
+++ src/App.js
@@ -2,38 +2,35 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    while (1) {
-      let input = await Console.readLineAsync(
-        "덧셈할 문자열을 입력해 주세요.\n"
-      );
+    let input =
+      (await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")) || "";
 
-      let re = new RegExp("//(.*?)\\\\n");
-      let split_input;
-      let output;
-      let check = 0;
+    let re = new RegExp("//(.*?)\\\\n");
+    let split_input;
+    let output;
+    let check = false;
 
-      if ((split_input = re.exec(input)) !== null) {
-        let new_re = split_input[1];
-        let new_input = input.replace(re, "");
-        output = new_input.split(new_re).map(Number);
-      } else {
-        let default_re = new RegExp(",|:");
-        output = input.split(default_re).map(Number);
-      }
+    if ((split_input = re.exec(input)) !== null) {
+      let new_re = split_input[1];
+      let new_input = input.replace(re, "");
+      output = new_input.split(new_re).map(Number);
+    } else {
+      let default_re = new RegExp(",|:");
+      output = input.split(default_re).map(Number);
+    }
 
-      for (let i = 0; i < output.length; i++) {
-        let value = output[i];
-        if (value < 0 || value == NaN) {
-          check = 1;
-        }
+    for (let i = 0; i < output.length; i++) {
+      let value = output[i];
+      if (value < 0 || isNaN(value)) {
+        check = true;
       }
-      if (check == 0) {
-        const sum = output.reduce((acc, cur) => acc + cur, 0);
-        Console.print("결과 : " + sum);
-      } else {
-        Console.print("[ERROR]");
-        break;
-      }
+    }
+
+    if (!check) {
+      const sum = output.reduce((acc, cur) => acc + cur, 0);
+      Console.print("결과 : " + sum);
+    } else {
+      throw new Error("[ERROR]");
     }
   }
 }
