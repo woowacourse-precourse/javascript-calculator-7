2f986cbaf7d6b7db7652e90029d929dc3db5773d
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-21 15:30:02 +0900

    style(App.js) 공백 제거
    결과 출력 공백 제거

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ class App {
                 "덧셈할 문자열을 입력해 주세요.\n"
             );
 
+            str = str.replace("\\n", "\n");
+
             if (str === "") {
-                await Console.print(`결과: 0`);
+                await Console.print(`결과 : 0`);
                 return 0;
             }
 
-            str = str.replace("\\n", "\n");
             let dividerExp = /[,:]/;
             let strNumbers = str;
 
@@ -23,23 +24,24 @@ class App {
             }
 
             const numbers = strNumbers.split(dividerExp).map((NUM) => {
-                if (isNaN(NUM)) {
-                    throw new Error("잘못된 숫자가 포함되었습니다.");
+                const PARSE_NUM = Number(NUM.trim());
+
+                if (isNaN(PARSE_NUM)) {
+                    throw new Error("[ERROR] 잘못된 숫자가 포함되었습니다.");
                 }
 
-                if (NUM < 0) {
-                    throw new Error("음수는 허용되지 않습니다.");
+                if (PARSE_NUM < 0) {
+                    throw new Error(" [ERROR]음수는 허용되지 않습니다.");
                 }
-                const PARSE_NUM = Number(NUM.trim());
 
                 return PARSE_NUM;
             });
 
             const SUM = numbers.reduce((SUM, NUM) => SUM + NUM, 0);
-            await Console.print(`결과: ${SUM}`);
+            await Console.print(`결과 : ${SUM}`);
             return SUM;
         } catch (error) {
-            console.error("[ERROR] : ", error.message);
+            throw error;
         }
     }
 }
