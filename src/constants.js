49276300d70e0c7cbf2c22e44fb3c738097fed36
seonae-j <wkdtjsdo135@naver.com>
2024-10-21 14:57:53 +0900

    docs: 요구사항 수정 및 ERROR 상수 수정
    같은 의미의 요구사항 문장 삭제 및 ERROR 상수명 추가하여 가독성 높였습니다.
    - README.md : 요구사항 수정
    - constants.js : 에러명 수정 및 추가

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 export const ERROR = {
-  NUMBER_ERROR: '[ERROR] 올바른 숫자 형식이 아닙니다.',
-  SEPARATOR_ERROR: '[ERROR] 올바른 구분자 형식이 아닙니다.',
-  CUSTOM_SEPERATOR_ERROR:
-    '[ERROR] 커스텀 구분자 생성 기호가 올바르지 않습니다.',
+  FORMULA_ERROR: '[ERROR] 올바른 연산 형식이 아닙니다.',
+  NEGATIVE_NUMBER_ERROR: '[ERROR] 음수는 사용할 수 없습니다.',
+  SEPARATOR_ERROR: '[ERROR] 허용되지 않은 구분자가 포함되어 있습니다.',
+  CUSTOM_SEPERATE_ERROR: '[ERROR] 커스텀 구분자 생성 기호가 올바르지 않습니다.',
+  CUSTOM_SEPERATOR_ERROR: '[ERROR] 숫자는 커스텀 구분자로 사용될 수 없습니다.',
   CUSTOM_ORDER_ERROR: '[ERROR] 커스텀 구분자 생성 순서가 올바르지 않습니다.',
   CUSTOM_INPUT_ERROR:
     '[ERROR] 커스텀 구분자 생성 기호 위치가 올바르지 않습니다.',
