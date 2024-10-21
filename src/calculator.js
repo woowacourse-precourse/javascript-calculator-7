5d17240a59bc9bb958468a5a03bd2a6f50146a2c
PangKiYeon <pdrkeamy@gmail.com>
2024-10-21 15:54:16 +0900

    feat (calculator.js): 각 숫자를 합하는 기능 구현

--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,37 @@
 function extractNumbers(input, distinct) {
     let number = [];
+    let sum = 0;
 
     for (let i = 0; i < input.length; i++) {
         let currentChar = input[i];
         if (distinct.includes(currentChar)) {
-            number = [];
+            if (number != []) {
+                sum += setNumber(number);
+                number = [];
+            }
         } else {
             number.push(i);
+            if (i === input.length - 1) {
+                sum += setNumber(number);
+            }
         }
     }
+    return sum;
+}
+
+function setNumber(number) {
+    let currentNumber = 0;
+    let realNumber = number.map(Number);
+
+    if (realNumber.length > 1) {
+        realNumber.reverse();
+        realNumber.forEach((item, index) => {
+            item = item * 10 ** index;
+            currentNumber += item;
+        });
+    } else {
+        currentNumber = realNumber[0];
+    }
+
+    return currentNumber;
 }
