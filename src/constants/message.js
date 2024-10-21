36163d6170e26903ac1b41e365a2f5fe8f6d9f57
sanghyeonLee <hyeon012366@gmail.com>
2024-10-21 20:58:33 +0900

    feat : 메세지 상수 값 묶음 파일 추가

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGE = {
+    WRONG_INPUT: "[ERROR] 잘못된 입력 입니다.",
+    WRONG_MINUS_INPUT: "[ERROR] 공백은 입력할 수 없습니다.",
+    WRONG_EMPTY_INPUT: "[ERROR] 공백은 입력할 수 없습니다."
+}
+export const RESULT_MESSAGE = "결과 : "
+export const INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n"
