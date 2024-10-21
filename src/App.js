36938f90d6af7fe701380083d726137a25c7aee7
seung365 <bdh3659@naver.com>
2024-10-21 23:53:49 +0900

    feat(App): 메인 동작 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils"
+import add from "./add.js"
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+      const result = add(input)
+      Console.print(`결과 : ${result}`)
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`)
+      throw error
+    }
+  }
 }
 
-export default App;
+export default App
