3f74ef510d892302f7c1f04f9d2b66305d793290
JHLEE325 <ljntiger325@naver.com>
2024-10-21 12:54:27 +0900

    feat : 입력받으면 기본 구분자로 숫자를 구분하여 나눔

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+
+    try {
+      const input = await MissionUtils.Console.readLineAsync('숫자를 입력하세요: ');
+      const result = this.divide(input); // this로 divide 메소드 호출
+      MissionUtils.Console.print(`결과: ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
+  }
+
+  divide(input){
+    let symbol = [',',':'];
+  
+    const dividepattern = new RegExp (`[${symbol.join('')}]`);
+    const result = input.split(dividepattern);
+  
+    return result;
+  }
 }
 
 export default App;
