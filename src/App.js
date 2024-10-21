83dd832f0187cd6c062b6c00cd8834dbc8ba9cb8
ChangOKIM <kk104202@naver.com>
2024-10-21 14:55:17 +0900

    feat: 빈 문자열 입력 시 0을 반환하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import StringCalculator from "./StringCalculator.js";
+
 class App {
-  async run() {}
+    async run() {
+        try {
+            // 사용자 입력
+            const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+
+            // StringCalculator를 사용해 계산
+            const result = StringCalculator.add(input);
+
+            // 결과 출력
+            Console.print(`결과 : ${result}`);
+        } catch (error) {
+            // 에러 발생 처리
+            Console.print(error.message);
+        }
+    }
 }
 
 export default App;
---
+++ src/StringCalculator.js
@@ -0,0 +1,11 @@
+class StringCalculator {
+    static add(input) {
+        // 빈 문자열 처리
+        if (input === "") return 0;
+
+        // 추후 기능 구현
+        return 0;
+    }
+}
+
+export default StringCalculator;
