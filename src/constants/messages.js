edf075a2ad806ab17c67c298635f9e1bd46a92e3
10012 <nas7062@naver.com>
2024-10-21 09:21:15 +0900

    feat(messages) : 메세지 입력 추가

---
+++ src/constants/messages.js
@@ -0,0 +1,10 @@
+export const MESSAGES = {
+    START : "덧셈할 문자열을 입력해 주세요\n",
+    RESULT : "결과 : ",
+}
+
+export const VALIDATES = {
+    INPUT_ERROR : "[ERROR] 입력값이 유효하지 않습니다.",
+    IS_NAN : "[ERROR] 숫자로 변환하지 못하는 문자가 포함되어 있습니다.",
+    MINUS :  "[ERROR] 음수는 입력할 수 없습니다.",
+}
