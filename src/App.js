c09f66f93f8320d67d2cde87b58a8143a30554c5
giseon <pakox123@gmail.com>
2024-10-21 19:43:15 +0900

    feat: 음수 및 잘못된 입력 형식에 대한 에러 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -23,17 +23,26 @@ class App {
         let numbersStr = input;
 
         if (input.startsWith("//")) {
-            const customDelimiterMatch = input.match(/^\/\/(.)\n(.*)/);
+            const customDelimiterMatch = input.match(/^\/\/(.)\\n(.*)/);
             if (customDelimiterMatch) {
                 delimiters.push(customDelimiterMatch[1]);
-                numbersStr = customDelimiterMatch[2]; 
+                numbersStr = customDelimiterMatch[2];
             } else {
                 throw new Error("[ERROR] 잘못된 구분자 형식입니다.");
             }
         }
 
         const delimiterRegex = new RegExp(`[${delimiters.join("")}]`);
-        const numbers = numbersStr.split(delimiterRegex).map(Number); 
+        const numbers = numbersStr.split(delimiterRegex).map(Number);
+
+        const negatives = numbers.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error("[ERROR] 음수사용은 불가능합니다.: " + negatives.join(", "));
+        }
+
+        if (numbers.some(isNaN)) {
+            throw new Error("[ERROR] 숫자 형식이 잘못되었습니다.");
+        }
 
         return numbers.reduce((sum, num) => sum + num, 0); 
     }
