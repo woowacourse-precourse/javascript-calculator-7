b38b016d4054e549ca0a6600a851cbec22b9fd15
Mina Lee <mal0070@naver.com>
2024-10-21 11:42:20 +0900

    refactor: 유효하지 않은 숫자 에러처리 및 결과 합산 함수 생성

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ class App {
     Console.print('덧셈할 문자열을 입력해 주세요.');
     const input = await Console.readLineAsync('');
 
-    //빈 문자열
+    //에러케이스 1: 빈 문자열
     if (!input) {
       return Promise.reject(new Error('[ERROR] 문자열을 입력해주세요.'));
     }
     //쉼표 또는 콜론
     else if (/,|:/.test(input)) {
-      const result = input
-        .split(/,|:/)
-        .map(Number)
-        .filter(n => !isNaN(n)) //input이 숫자가 아닌 경우 filtering
-        .reduce((a, b) => a + b, 0); //합산
+      const result = this.calculateSum(input.split(/,|:/));
       this.printResult(result);
     }
 
@@ -27,21 +23,29 @@ class App {
 
       //분리할 문자열
       const str = input.replace(/\/\/.*?\\n/, '');
-      const result = str
-        .split(customChar)
-        .map(Number)
-        .filter(n => !isNaN(n))
-        .reduce((a, b) => a + b, 0); //합산
+      const result = this.calculateSum(str.split(customChar));
       this.printResult(result);
     }
 
-    //잘못된 값 입력
+    //에러케이스 2: 잘못된 값 입력
     else {
       return Promise.reject(
         new Error('[ERROR] 지원되지 않는 문자열 포맷입니다.')
       );
     }
   }
+  //합산 함수
+  calculateSum(numbers) {
+    //에러케이스 3: 유효하지 않은 숫자
+    const invaildNumbers = numbers.filter((n) => isNaN(Number(n)));
+    if (invaildNumbers) {
+      return Promise.reject(
+        new Error('[ERROR] 유효하지 않은 숫자가 포함되어 있습니다.')
+      );
+    }
+    return numbers.map(Number).reduce((a, b) => a + b, 0);
+  }
+
   //결과 출력 함수
   printResult(result) {
     Console.print('결과 : ' + result);
