bd78d5137a3bd0cacb5d0303dc503c06dc8b7ba8
6v6DS <6v6DS@ajou.ac.kr>
2024-10-21 22:57:01 +0900

    feat(App.js):문자열 추출 및 계산 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
-}
+  async run() {
+        Console.print('덧셈할 문자열을 입력해 주세요.');
+        const input = await Console.readLineAsync('');
+    
+        if (input === "") {
+          return Console.print("결과 : 0");
+        }
+
+        let deli = /[,|:]/;
+        let part = input;
+    
+        if (input.startsWith("//")) {
+        
+          const parts = input.split("\\n");
+
+          const custom = parts[0].substring(2);
+    
+          deli = new RegExp(`[${custom},|:]`);
+          part = parts[1];
+        }
+    
+        const numbers = part.split(deli).map(Number);
+  
+        Console.print("결과 : " + numbers.reduce((sum, number) => sum + number, 0));
+      }
+  }
+
+
 
 export default App;
