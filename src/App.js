582a0cf0703386a9e37f06c70c1621084922169e
seongmin <seongm22n@gmail.com>
2024-10-21 12:23:42 +0900

    refactor: 모듈화 작업
    - 커스텀구분자와 일반구분자 코드를 따로 모듈화시킴

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import validateAnswer from './validator.js';
+import customDelimiter from './customDelimiter.js';
+import delimiter from './delimiter.js';
 class App {
 	async run() {
 		const answer = await Console.readLineAsync(
@@ -12,19 +14,12 @@ class App {
 			if (answer.trim() === '') {
 				Console.print(`결과: 0`);
 			} else if (answer.startsWith('//')) {
-				//커스텀 구분자
-				const delimiter = answer.substring(2, 3);
-				const numbers = answer.split('\\n')[1].split(delimiter);
-				const sum = numbers.map(Number).reduce((a, b) => a + b, 0);
-				Console.print(`결과 : ${sum}`);
+				customDelimiter(answer);
 			} else {
 				if (!validateAnswer(answer)) {
 					throw new Error(`[ERROR] : 사용자가 잘못된 값 입력`);
 				}
-				//일반 구분자
-				const numbers = answer.split(/[:,]/);
-				const sum = numbers.map(Number).reduce((a, b) => a + b, 0);
-				Console.print(`결과 : ${sum}`);
+				delimiter(answer);
 			}
 		} catch (error) {
 			throw new Error(error.message);
