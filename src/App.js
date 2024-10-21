11c269318bfff2d21b646fad9cf30815fd622eb4
dyoon <dyoon@dyoonui-MacBookAir.local>
2024-10-21 19:03:20 +0900

    문구 출력 및 인풋 받기 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
 class App {
-  async run() {}
+  async run() {
+    // 사용자로부터 입력을 받는 비동기 함수
+    async function getUserInput() {
+      return await Console.readLineAsync("덧셈할 숫자를 입력해 주세요.\n");
+    }
+    // 사용자 입력을 받고 검증 실행
+    const userInput = await getUserInput();
+    }
 }
 
 export default App;
