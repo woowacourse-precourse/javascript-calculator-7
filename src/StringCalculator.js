37ceb1214f117fc9306a83906b3cbffcc7320246
ChangOKIM <kk104202@naver.com>
2024-10-21 15:00:37 +0900

    feat: 커스텀 구분자를 지원하는 기능 추가

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,8 +4,15 @@ class StringCalculator {
         if (input === "") return 0;
 
         // 기본 구분자 처리
-        let delimiter = /,|:/;
+        let delimiter = /,|:/; 
         let numbers = input;
+
+        // 커스텀 구분자 처리
+        if (numbers.startsWith("//")) {
+            const delimiterEndIndex = numbers.indexOf("\n");
+            delimiter = new RegExp(`[${numbers.substring(2, delimiterEndIndex)}]`);
+            numbers = numbers.substring(delimiterEndIndex + 1); // 구분자 이후 숫자 추출
+        }
 
         const numberArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
         return numberArray.reduce((sum, num) => sum + num, 0);
