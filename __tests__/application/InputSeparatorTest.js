e647bacc8370ab905a94022d04269ab68eb077a8
rheefine <rheefine@gmail.com>
2024-10-21 14:09:26 +0900

    test: getCustomDelimiter 메서드에 대한 테스트 코드 구현

--- __tests__/application/InputSeparatorTest.js
+++ __tests__/application/InputSeparatorTest.js
@@ -22,4 +22,29 @@ describe('hasCustomDelimiter 메서드 테스트', () => {
       expect(result).toBe(expected);
     });
   });
+
+  describe('getCustomDelimiter 메서드 테스트', () => {
+    test('getCustomDelimiter 메서드가 커스텀 구분자를 반환하는지 확인', () => {
+      const testCases = [
+        { input: '//:\\n2:3:4', expected: ':' },
+        { input: '//\\\\n', expected: '\\' },
+        { input: '', expected: null },
+        { input: '/', expected: null },
+        { input: '//', expected: null },
+        { input: '\\n', expected: null },
+        { input: '   \\n', expected: null },
+        { input: '//\\n', expected: null },
+        { input: '1,2,3', expected: null },
+        { input: 'sadfa', expected: null },
+      ];
+
+      const inputSeparator = new InputSeparator();
+
+      testCases.forEach(({ input, expected }) => {
+        inputSeparator.getCustomDelimiter(input);
+        const result = inputSeparator.getCustomDelimiter(input);
+        expect(result).toBe(expected);
+      });
+    });
+  });
 });
