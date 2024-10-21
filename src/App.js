007d40a4dfb6618e875cab5d3387c68abe517193
Mina Lee <mal0070@naver.com>
2024-10-21 12:35:25 +0900

    feat: 음수 처리 에러케이스 추가 / refactor: 에러 제출 형식 변경

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App {
 
     //에러케이스 1: 빈 문자열
     if (!input) {
-      return Promise.reject(new Error('[ERROR] 문자열을 입력해주세요.'));
+      throw new Error('[ERROR] 문자열을 입력해주세요.');
     }
     //쉼표 또는 콜론
     else if (/,|:/.test(input)) {
@@ -27,21 +27,22 @@ class App {
       this.printResult(result);
     }
 
-    //에러케이스 2: 잘못된 값 입력
+    //에러케이스 2: 잘못된 포맷 입력
     else {
-      return Promise.reject(
-        new Error('[ERROR] 지원되지 않는 문자열 포맷입니다.')
-      );
+      throw new Error('[ERROR] 지원되지 않는 문자열 포맷입니다.');
     }
   }
   //합산 함수
   calculateSum(numbers) {
     //에러케이스 3: 유효하지 않은 숫자
-    const invaildNumbers = numbers.filter((n) => isNaN(Number(n)));
-    if (invaildNumbers) {
-      return Promise.reject(
-        new Error('[ERROR] 유효하지 않은 숫자가 포함되어 있습니다.')
-      );
+    const invalidNumbers = numbers.filter((n) => isNaN(Number(n)));
+    if (invalidNumbers.length > 0) {
+      throw new Error('[ERROR] 유효하지 않은 숫자가 포함되어 있습니다.');
+    }
+    //에러케이스 4: 음수
+    const negativeNumbers = numbers.map(Number).filter((n) => n < 0);
+    if (negativeNumbers.length > 0) {
+      throw new Error(new Error('[ERROR] 음수를 입력할 수 없습니다.'));
     }
     return numbers.map(Number).reduce((a, b) => a + b, 0);
   }
