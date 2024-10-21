fdc6da848d41ed6390b11e7cc3d48f848f078e68
somm12 <sominan8744@gmail.com>
2024-10-21 16:15:17 +0900

    feature: 문자열 입력받기

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ class App {
   inputRequestText() {
     Console.print("덧셈할 문자열을 입력해 주세요.");
   }
+  async getInput() {
+    const inputValue = await Console.readLineAsync("");
+    Console.print(inputValue);
+  }
   async run() {
     this.inputRequestText();
+    this.getInput();
   }
 }
 
