b0147a4582e36a26ab296cd13677e8ed9e678ec2
suhwa <tnghk9611@naver.com>
2024-10-21 16:29:14 +0900

    feat: 사용자의 입력에 대한 예외 처리

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App {
         const input = await this.getInput("덧셈할 문자열을 입력해 주세요.");
         const numbers = this.splitInput(input);
 
+        this.checkValid(numbers);
+
         const sum = numbers.reduce((acc, cur) => acc + cur, 0);
         console.log("결과 :", sum);
         this.rl.close();
@@ -34,6 +36,18 @@ class App {
             return input.split(delimiter).map((item) => Number(item.trim()));
         } else {
             return input.split(/[,:]/).map((item) => Number(item.trim()));
+        }
+    }
+
+    checkValid(numbers) {
+        for (let num of numbers) {
+            if (isNaN(num)) {
+                throw new Error(`[ERROR] '${num}'은(는) 유효한 숫자가 아닙니다.`);
+            }
+
+            if (num < 0) {
+                throw new Error("[ERROR] 음수는 허용되지 않습니다.");
+            }
         }
     }
 }
