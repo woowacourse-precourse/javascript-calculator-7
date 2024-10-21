afa1722a9be3cb6d8cfdc9d7e8f05cb716e345f7
vgotu99 <vgotu99@gmail.com>
2024-10-21 11:20:29 +0900

    feat(parser): 기본 구분자, 커스텀 구분자 인식 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  constructor() {
+    this.CUSTOM_DELIMITER_REGEX = /^\/\/(.)\n(.*)/;
+    this.DEFAULT_DELIMITER = ',|:';
+  }
+
+  async run() {
+    const INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const { DELIMITER, NUMBERS } = this.parseInput(INPUT);
+    Console.print(`구분자: ${DELIMITER}`);
+    Console.print(`숫자 문자열: ${NUMBERS}`);
+  }
+
+  parseInput(input) {
+    const MATCH = input.match(this.CUSTOM_DELIMITER_REGEX);
+
+    if (MATCH) {
+      return { DELIMITER: MATCH[1], NUMBERS: MATCH[2] };
+    }
+
+    return { DELIMITER: this.DEFAULT_DELIMITER, NUMBERS: input };
+  }
 }
+
 
 export default App;
