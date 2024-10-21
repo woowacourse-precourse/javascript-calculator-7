e664042efcb1492f1e4d908d479c38adf53fd56f
thisisyejin <yejink321@gmail.com>
2024-10-21 18:57:10 +0900

    style: apply the JavaScript code convention

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import Calculator from "./calculator.js";
+import Calculator from "./Calculator";
 
 class App {
-  async run() { /* 프로그램의 시작점 */
-    MissionUtils.Console.print('덧셈할 문자열을 입력해주세요.')
-    const input = await MissionUtils.Console.readLineAsync('')
+  async run() {
+    MissionUtils.Console.print('덧셈할 문자열을 입력해주세요.');
+    const input = await MissionUtils.Console.readLineAsync('');
     try {
-      const calculator = new Calculator()
-      calculator.customizeSeparator(input)
-      MissionUtils.Console.print(`결과 : ${calculator.readString(input)}`)
+      const calculator = new Calculator();
+      calculator.customizeSeparator(input);
+      MissionUtils.Console.print(`결과 : ${calculator.readString(input)}`);
     } catch (err) {
-      console.error(err.message)
-      throw err
+      console.error(err.message);
+      throw err;
     }
   }
 }
