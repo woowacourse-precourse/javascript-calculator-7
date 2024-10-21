df119378d9c05e8f298d1b894b1f61605a2fe264
kimdongyoung <wwweric12@naver.com>
2024-10-21 14:17:26 +0900

    fix(splitBySymbol): /로 시작하는 에러케이스 대응 수정

--- src/utils/splitBySymbol.js
+++ src/utils/splitBySymbol.js
@@ -1,21 +1,7 @@
 import { hasInvalidSymbol, hasNegative } from '../errorHandling';
 
 const splitBySymbol = (value) => {
-    if(value[0] != "/"){
-        const commaSeparatedValue = value.split(",");
-        let seperatedValue = [];
-        commaSeparatedValue.forEach((item)=>{
-            seperatedValue = seperatedValue.concat(item.split(":"));
-        })
-
-        if(hasInvalidSymbol(seperatedValue)){
-           throw new Error("[ERROR] 기본 구분자는 콜론(:)또는 콤마(,) 형식으로 입력해주세요.어플리케이션이 종료됩니다. ");
-        }
-        if(hasNegative(seperatedValue)){
-            throw new Error("[ERROR] 숫자는 양수만 입력이 가능합니다. 어플리케이션이 종료됩니다. ");
-        }
-        return seperatedValue;
-    }else{
+    if(value.startsWith('//')){
         const index = 2;
         const lineBreakIndex = value.indexOf("\\n");
         const customSeperator = value.slice(index,lineBreakIndex);
@@ -29,7 +15,21 @@ const splitBySymbol = (value) => {
             throw new Error("[ERROR] 양수만 입력이 가능합니다. 어플리케이션이 종료됩니다. ");
         }
         return seperatedValue;
-    }  
+    }else{
+        const commaSeparatedValue = value.split(",");
+        let seperatedValue = [];
+        commaSeparatedValue.forEach((item)=>{
+            seperatedValue = seperatedValue.concat(item.split(":"));
+        })
+
+        if(hasInvalidSymbol(seperatedValue)){
+           throw new Error("[ERROR] 기본 구분자는 콜론(:)또는 콤마(,) 형식으로 입력해주세요.어플리케이션이 종료됩니다. ");
+        }
+        if(hasNegative(seperatedValue)){
+            throw new Error("[ERROR] 숫자는 양수만 입력이 가능합니다. 어플리케이션이 종료됩니다. ");
+        }
+        return seperatedValue;
+    }
 }
 
 export default splitBySymbol
