9a5b7d4edba6273bc2be0a99ace6758ecbea7151
suhwa <tnghk9611@naver.com>
2024-10-21 14:02:29 +0900

    feat: 사용자의 입력받는 기능 구현
    - readline 모듈으로 입력을 받도록 구현
    - 사용자의 입력을 받는 부분을 getInput 메서드로 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import readline from "readline";
+
 class App {
-  async run() {}
+    constructor() {
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+    }
+
+    async run() {
+        const input = await this.getInput("덧셈할 문자열을 입력해 주세요.");
+        this.rl.close();
+    }
+
+    async getInput(text) {
+        return new Promise((resolve) => {
+            this.rl.question(text, (input) => {
+                resolve(input);
+            });
+        });
+    }
 }
 
 export default App;
