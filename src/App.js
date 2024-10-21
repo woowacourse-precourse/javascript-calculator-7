bbe229c514bf5a818b0b17d762449be128e68991
syg0629 <syg0629@hanmail.net>
2024-10-21 21:26:34 +0900

    feat: 입력 기능 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    // 입력 기능 작성
+    const input = await MissionUtils.Console.readLineAsync(
+      '덧셈할 문자열을 입력해주세요.\n'
+    );
+    MissionUtils.Console.print(`결과 : ${input}`);
+  }
 }
 
 export default App;
