1821f7ca08987aab60225eef9bc6f1dd5b3e6d7f
Jaewon <wellbird@naver.com>
2024-10-21 13:46:26 +0900

    feat : 커스텀 구분자 유무 판단 및 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import getCustomSplitter from './getCustomSplitter.js';
 import input from './input.js';
 
 class App {
   async run() {
     const userInput = await input();
+    const [splitter, string] = getCustomSplitter(userInput);
   }
 }
 
