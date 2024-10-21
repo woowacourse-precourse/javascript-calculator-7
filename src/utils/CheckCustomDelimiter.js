8c2a940263320f5a21eb335d070261d82d93d6d6
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-21 21:18:07 +0900

    feat(utils) : 커스텀 구분자가 숫자면 에러 발생

--- src/utils/CheckCustomDelimiter.js
+++ src/utils/CheckCustomDelimiter.js
@@ -16,6 +16,11 @@ const checkCustomDelimiter = (input) => {
       // 매치된 커스텀 구분자를 가져옵니다. 첫번째 인덱스에 저장되어 있습니다.
       const CUSTOM_DELIMITER = CUSTOM_DELIMITER_MATCH[1];
 
+      //커스텀 구분자가 숫자면 에러를 발생시킵니다.
+      if (!isNaN(CUSTOM_DELIMITER)) {
+        throw new Error(Errors.NUMBER_AS_DELIMITER);
+      }
+
       // 매치된 커스텀 구분자가 한 글자인지 확인합니다.
       if (CUSTOM_DELIMITER.length === 1) {
         return CUSTOM_DELIMITER;
