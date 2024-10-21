df61ba42969ba0e96d0fa8d35fe05e8da17a7b2a
YSW2 <jrgyber@gmail.com>
2024-10-21 18:23:18 +0900

    feat(parse_string): 커스텀 구분자 추가 기능
    - 커스텀 구분자 추가 기능 구현
    - 올바르지 않은 입력시 Error 출력

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   parse_string = (input) => {
     let numbers;
-    let delimiter = /[,:]/;
+    let delimiter = [',', ':'];
 
-    numbers = input.split(delimiter).map((num) => {
+    if (input.startsWith('//')) {
+      const parts = input.split('\\n');
+      if (parts.length < 2) {
+        throw new Error(
+          '[ERROR] 커스텀 구분자 지정 후 올바른 입력이 없습니다.'
+        );
+      }
+      const new_delimiter = parts[0].slice(2);
+      delimiter.push(new_delimiter);
+      input = parts[1];
+    }
+
+    const delimiter_regex = new RegExp(
+      delimiter.map((d) => `\\${d}`).join('|')
+    );
+    numbers = input.split(delimiter_regex).map((num) => {
       const parsed = parseInt(num.trim(), 10);
       if (isNaN(parsed)) {
         throw new Error(`[ERROR] 유효하지 않은 입력값: ${num}`);
@@ -20,6 +35,8 @@ class App {
       const user_line = await Console.readLineAsync(
         '덧셈할 문자열을 입력해주세요.\n'
       );
+      if (user_line === '') return 0;
+
       const numbers = this.parse_string(user_line);
       Console.print(`결과: ${numbers}`);
     } catch (error) {
