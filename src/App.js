79f13bc118c9a518fd4b5358a657d6b73dff8ad6
thisisyejin <yejink321@gmail.com>
2024-10-21 17:10:03 +0900

    fix: fix an error-catching error
    After creating a calculator module, there is an error that does not catch
    the error.
    Solve by creating a calculator instance in the App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { customizeSeparator, readString } from "./calculator.js";
+import Calculator from "./calculator.js";
 
 class App {
   async run() { /* 프로그램의 시작점 */
     MissionUtils.Console.print('덧셈할 문자열을 입력해주세요.')
-    const input = await MissionUtils.Console.readLineAsync("")
+    const input = await MissionUtils.Console.readLineAsync('')
     try {
-      // 커스텀 구분자 지정
-      let customSeparator = true;
-      customSeparator = customizeSeparator(customSeparator, input)
-
-      // 문자열 탐색
-      let startIndex = 0
-      if ((typeof customSeparator) == 'string') startIndex = 5   
-      const result = readString(customSeparator, input, startIndex)
-
-      MissionUtils.Console.print(`결과 : ${result}`)
+      const calculator = new Calculator()
+      calculator.customizeSeparator(input)
+      MissionUtils.Console.print(`결과 : ${calculator.readString(input)}`)
     } catch (err) {
       console.error(err.message)
       throw err
--- src/calculator.js
+++ src/calculator.js
@@ -1,21 +1,27 @@
-const customizeSeparator = (customSeparator, string) => {
-    if ( string.startsWith('//') ) {
-        if ( string.slice(3, 5) == '\\n' ) {
-        customSeparator = string[2]
-        } else {
-        throw Error('[ERROR] 커스텀 구분자 지정에 실패했습니다.')
-        }
-    }
-    return customSeparator
-}  
+export default class Calculator {
+  constructor () {
+    this.startIndex = 0
+    this.customSeparator = true
+  }
 
-const readString = (customSeparator, string, startIndex) => {
+  customizeSeparator (string) {
+    if ( string.startsWith('//') ) {
+      if ( string.slice(3, 5) == '\\n' ) {
+        this.customSeparator = string[2]
+      } else {
+        throw Error('[ERROR] 커스텀 구분자 지정에 실패했습니다.')
+      }
+    }
+  }
+
+  readString (string) {
     let number = '0'; // 숫자를 인식할 변수
     let sum = 0;      // 덧셈을 계산할 변수
-    for (let i = startIndex; i < string.length; i++) {
-      if (!isNaN( parseInt(string[i]) ) && string[i] != customSeparator) {
+    if ((typeof this.customSeparator) === 'string') this.startIndex = 5
+    for (let i = this.startIndex; i < string.length; i++) {
+      if (!isNaN( parseInt(string[i]) ) && string[i] !== this.customSeparator) {
         number += string[i]
-      } else if (string[i] == ',' || string[i] == ':'|| string[i] == customSeparator) { 
+      } else if (string[i] == ',' || string[i] == ':'|| string[i] == this.customSeparator) { 
         sum += parseInt(number)
         number = '0'
       } else {
@@ -24,6 +30,5 @@ const readString = (customSeparator, string, startIndex) => {
     }
     sum += parseInt(number);
     return sum
+  }
 }
-
-export { customizeSeparator, readString };
