58a964cbb67e066d30853bf20c71015562898a21
Doyeon <duseh729@naver.com>
2024-10-21 22:16:05 +0900

    feat: 3. 예외처리 구현

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ class App {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
     try {
-      Console.print(`결과: ${this.addNumber(input)}`);
-    } catch {}
+      Console.print(`결과 : ${this.addNumber(input)}`);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   addNumber(input) {
@@ -19,15 +21,28 @@ class App {
     }
 
     let numArray = [cutInput];
-    
+
     for (let p of possible) {
       let tempArray = [];
       for (let item of numArray) {
-        tempArray.push(...item.split(p)); 
+        tempArray.push(...item.split(p));
       }
       numArray = tempArray;
     }
-    return numArray.reduce((a, b) => Number(a) + Number(b), 0);
+
+    const result = numArray.reduce((a, b) => {
+      const num1 = Number(a);
+      const num2 = Number(b);
+      if (num1 < 0) {
+        throw new Error("[ERROR] 음수는 처리되지 않습니다.");
+      }
+      return num1 + num2;
+    }, 0);
+
+    if (isNaN(result)) {
+      throw new Error("[ERROR] 잘못된 값을 입력했습니다.");
+    }
+    return result;
   }
 }
 
