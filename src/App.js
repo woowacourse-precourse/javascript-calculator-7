9fb376a015bedf114ca6245e8ea14f47925bb55f
jongbin26 <010jjj@naver.com>
2024-10-21 13:21:59 +0900

    fix(src): 예제 테스트 업데이트를 위한 커밋

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { input, print } from "./utils/ioHandler.js";
 import { calculator } from "./caclulator.js";
 import { IO_MESSAGE } from "./constant/index.js";
 class App {
-  async run() {
+  async run() { 
     try{
       const expression = await input(IO_MESSAGE.INPUT);
       const answer = calculator(expression);
