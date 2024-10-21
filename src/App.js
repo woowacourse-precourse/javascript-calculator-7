26ac1468efdef130abdc7b3e95ed0bf319e2e53c
lecto17 <khwoo_rpsit@naver.com>
2024-10-21 13:19:48 +0900

    feat: 입출력 기능 작업

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  getAnswer(input) {
+    console.log(input);
+  }
+
+  async run() {
+    while (true) {
+      await Console.readLineAsync("숫자를 입력해주세요 : ")
+        .then((userInput) => {
+          return this.getAnswer(userInput);
+        })
+        .then((res) => Console.print(res));
+    }
+  }
 }
 
 export default App;
