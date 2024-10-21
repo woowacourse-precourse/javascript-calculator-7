1fa087e79bc83231b0e8d8dcd44283e0e0cc83c3
Yongho Kim <kyhcraft@gmail.com>
2024-10-21 22:43:28 +0900

    feat: 에러 메시지

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+
 class App {
 
   exceptionHandler(condition, errorMessage) {
@@ -14,7 +15,7 @@ class App {
       inputString = inputString.replace("\\n", "\n");
       const delimiterPosition = inputString.indexOf("\n");
 
-      this.exceptionHandler(delimiterPosition === -1, "[ERROR]");
+      this.exceptionHandler(delimiterPosition === -1, "[ERROR] 커스텀 구분자 형식이 잘못되었습니다.");
 
       const customSeparator = inputString.substring(2, delimiterPosition);
       separator = new RegExp(`[${customSeparator}]`);
@@ -22,9 +23,10 @@ class App {
     }
 
     const numberArray = inputString.split(separator).map((value) => value.trim()).map(Number);
-    this.exceptionHandler(numberArray.length === 0, "[ERROR]");
-    this.exceptionHandler(numberArray.some(isNaN), "[ERROR]");
-    this.exceptionHandler(numberArray.some((num) => num < 0), "[ERROR]");
+
+    this.exceptionHandler(numberArray.length === 0, "[ERROR] 구분자에 따른 숫자가 입력되지 않았습니다.");
+    this.exceptionHandler(numberArray.some(isNaN), "[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+    this.exceptionHandler(numberArray.some((num) => num < 0), "[ERROR] 음수는 허용되지 않습니다.");
 
     return numberArray.reduce((total, currentValue) => total + currentValue, 0);
   }
@@ -33,7 +35,7 @@ class App {
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const inputString = await Console.readLineAsync("");
 
-    this.exceptionHandler(inputString === "", "[ERROR]");
+    this.exceptionHandler(inputString === "", "[ERROR] 입력된 문자열이 없습니다.");
 
     const totalSum = this.computeSum(inputString);
     Console.print(`결과 : ${totalSum}`);
