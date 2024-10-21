b244b1e23dcd8c3d9f5ce2b9fdc836ce0e3013a5
Erin <fls4804@gmail.com>
2024-10-21 22:07:31 +0900

    사용자로부터 문자열 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-class App {
-  async run() {}
-}
-
-export default App;
+const result = this.calculateSum(input);
+          Console.print(`결과 : ${result}`);
+        } catch (error) {
+          Console.print(`[ERROR] ${error.message}`);
+        }
+      }
+    
+      async inputStr() {
+        return await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      }
+    
+      calculateSum(input) {
+        // 입력값이 없으면 0 반환
+        if (!input) return 0; 
+    
+      }
