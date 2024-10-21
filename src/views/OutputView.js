47d9f7cadc2be0ca8e6353bdbadfc43b8723bd86
luminox93 <memoji1236@gmail.com>
2024-10-21 14:42:07 +0900

    fix: 오타 수정
    1. 스페이스바 누락 수정

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 export default class OutputView {
 	static printResult(result) {
-		Console.print(`결과: ${result}`);
+		Console.print(`결과 : ${result}`);
 	}
 	static printError(errorMessage) {
 		Console.print(errorMessage);
