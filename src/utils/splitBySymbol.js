33699914924e115c4af4873c1dfe0191da4f3d40
kimdongyoung <wwweric12@naver.com>
2024-10-21 14:51:21 +0900

    refactor(errorMessage): 에러메시지 \n으로 수정

--- src/utils/splitBySymbol.js
+++ src/utils/splitBySymbol.js
@@ -9,7 +9,7 @@ const splitBySymbol = (value) => {
         const seperatedValue = substringValue.split(customSeperator)
 
         if(hasInvalidSymbol(seperatedValue)){
-            throw new Error("[ERROR] 커스텀 구분자는 // 커스텀구분자 \n로 형식으로 입력해주세요. 어플리케이션이 종료됩니다. ");
+            throw new Error("[ERROR] 커스텀 구분자는 // 커스텀구분자 \\n로 형식으로 입력해주세요. 어플리케이션이 종료됩니다. ");
         }
         if(hasNegative(seperatedValue)){
             throw new Error("[ERROR] 양수만 입력이 가능합니다. 어플리케이션이 종료됩니다. ");
