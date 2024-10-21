c989a5e18868b2584a363e069eeb808776f66b18
Luna77877 <lsy77877korea@gmail.com>
2024-10-21 18:30:44 +0900

    feat(io): add input/output handling
    - Implement input handling for strings
    - Implement output handling for strings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    getString();
+
+    async function getString() {
+      try {
+        const STRING = await MissionUtils.Console.readLineAsync(
+          "덧셈할 문자열을 입력해 주세요.\n"
+        );
+
+        const RESULT = string;
+
+        MissionUtils.Console.print("결과 : " + result);
+      } catch (error) {
+        // reject 되는 경우
+      }
+    }
+  }
 }
 
 export default App;
