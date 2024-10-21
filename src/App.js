c2f1661bd07c6a5f5f0c790a96ab99e630481972
Whatdoyumin <lym4963@naver.com>
2024-10-21 22:05:44 +0900

    feat: 기본 구분자와 커스텀 구분자 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
+import DelimiterHandler from "./DelimiterHandler";
 class App {
   async run() {
     const input = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요.\n'
     );
+    try {
+      const numbers = DelimiterHandler(input);
 }
 
 export default App;
