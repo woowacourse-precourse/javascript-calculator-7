b466c7981f0999b1b12bb57e0f6fe68dd305a5b7
OhSeChan <seb_s19@naver.com>
2024-10-21 22:06:33 +0900

    feat(App): 기본 구분자를 기준으로 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { Console } from '@woowacourse/mission-utils';
-import { MESSAGE } from './constant.js';
+import { MESSAGE, DEFAULT_DELIMITERS } from './constant.js';
 
 class App {
   async getInput() {
     return await Console.readLineAsync(`${MESSAGE.INPUT}`);
   }
 
+  getDelimiters() {
+    return DEFAULT_DELIMITERS;
+  }
+
+  createRegex(delimiters) {
+    return new RegExp(`[${delimiters.join('')}]`);
+  }
+
+  parseInput(input, delimiters) {
+    const regex = this.createRegex(delimiters);
+
+    return input.split(regex).map(Number);
+  }
+
   async run() {
     const input = await this.getInput();
+    const delimiters = this.getDelimiters();
+    const numbers = this.parseInput(input, delimiters);
   }
 }
 
--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,5 @@
 export const MESSAGE = {
   INPUT: '덧셈할 문자열을 입력해 주세요. \n',
 };
+
+export const DEFAULT_DELIMITERS = [',', ':'];
