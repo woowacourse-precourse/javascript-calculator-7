1d0eb146fc66258e5cffbf3b6b4f1cb2de88a9da
luminox93 <memoji1236@gmail.com>
2024-10-21 14:48:50 +0900

    feat: isValidInputType 함수 생성
    1. input의 타입을 검증하는 함수 생성
    2. 문자열과 배열 타입 이외에는 받지 않도록 구현

--- src/models/validations/isValidInputType.js
+++ src/models/validations/isValidInputType.js
@@ -1,7 +1,7 @@
 import { ERROR_MESSAGES } from '../../constants/constants.js';
 
 export const isValidInputType = (input) => {
-	if (input === undefined || input === null) {
+	if (input === undefined || input === null || typeof input === Object) {
 		throw new TypeError(ERROR_MESSAGES.INVALID_TYPE);
 	}
 };
