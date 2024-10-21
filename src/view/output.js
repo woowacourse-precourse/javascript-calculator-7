55c1f4bd2fa0c4d9d942d3275d32364606f27bad
Dobbymin <gmin0701@knu.ac.kr>
2024-10-21 18:56:43 +0900

    fix(output): output 메세지 경로 수정 및 상수화 한 메세지 적용

--- src/view/output.js
+++ src/view/output.js
@@ -1,6 +1,8 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-import { OUTPUT_MESSAGE } from '../constants/message/output-message.js';
+import { CONSTANTS } from '../constants/constants.js';
+
+const { OUTPUT_MESSAGE } = CONSTANTS;
 
 export const printResult = (result) => {
   MissionUtils.Console.print(`${OUTPUT_MESSAGE}${result}`);
