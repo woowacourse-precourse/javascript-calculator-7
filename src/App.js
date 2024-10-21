3475e833f2086496c52ae97e4963744a39727606
이재준 <202010907@pukyong.ac.kr>
2024-10-21 12:00:36 +0000

    feat: 문자열 입력

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import readline from 'readline';
 class App {
   async run() {
     try {
-      // 1. '덧셈할 문자열을 입력해 주세요.' 문구를 출력한다.
+    // 1. '덧셈할 문자열을 입력해 주세요.' 문구를 출력한다.
       this.promptMessage();
-
+		
+	// 2. 문자열을 입력받는다.
+      const userInput = await this.getInput();
      
     } catch (error) {
       console.error(error.message);
@@ -16,7 +18,20 @@ class App {
   promptMessage() {
     console.log('덧셈할 문자열을 입력해 주세요.');
   }
-
+	
+	// 2. 문자열을 입력받는다 (Node.js 환경).
+  getInput() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+	return new Promise((resolve) => {
+    rl.question('', (input) => {
+        rl.close();
+        resolve(input); // 입력값을 반환
+      });
+    });
+  }
 }
 
 export default App;
