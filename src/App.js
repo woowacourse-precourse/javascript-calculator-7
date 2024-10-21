ab6610265df04084c9459d1a77738b955d11cefd
JaiIn <di0308@naver.com>
2024-10-21 15:00:59 +0900

    fix: 런타임 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
 import AddCalculator from "./AddCalculator.js";
 
 class App {
   async run() {
-    const Calculator = new AddCalculator();
-    try {
-      await Calculator.run();
-    } catch (error) {
-      Console.print("[ERROR] : ",error);
-    }
+      const Calculator = new AddCalculator();
+      await Calculator.run();  
   };
 };
 
