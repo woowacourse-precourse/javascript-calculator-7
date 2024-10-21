d345af426e6ef3bcb0ec1722e57bd7c03a3baa18
Heesoo Jun <acesk123@gmail.com>
2024-10-21 17:48:46 +0900

    feat: 기본 실행 구조

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+
+      const result = this.calculate(input);
+
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      return;  // 프로그램 흐름을 종료시키기 위해 return 추가
+    }
+  }
 }
 
 export default App;
