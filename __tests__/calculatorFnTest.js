b3c94de42b6fd6fd18bcf4030281fe6cd09e81be
hyeongdong <khd941018@gmail.com>
2024-10-21 23:52:09 +0900

    test(calculatorFnTest): 메서드 명 변경

--- __tests__/calculatorFnTest.js
+++ __tests__/calculatorFnTest.js
@@ -3,15 +3,6 @@ import Calculator from "../src/Calculator";
 describe("문자열 계산기 함수 테스트", () => {
   const calculator = new Calculator();
 
-  test("커스텀 구분자를 문자열로부터 구분할 수 있다.", () => {
-    const inputs = ["//;\\n12345", "//.\\n12345", "//-\\n12345", "12345k", ";\\n1234"];
-    const outputs = [";", ".", "-", 0, 0];
-
-    outputs.forEach((output, index) => {
-      expect(calculator.getSeperatorFrom(inputs[index])).toBe(output);
-    });
-  });
-
   test("숫자로 이루어진 배열의 합을 구할 수 있다.", () => {
     const inputs = [
       [1, 2, 3],
@@ -20,7 +11,7 @@ describe("문자열 계산기 함수 테스트", () => {
     const outputs = [6, 12];
 
     outputs.forEach((output, index) => {
-      expect(calculator.getSumOf(inputs[index])).toBe(output);
+      expect(calculator.sum(inputs[index])).toBe(output);
     });
   });
 
