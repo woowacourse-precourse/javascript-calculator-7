e033b4bc1e8947e6acee27ef8b18e102163c056e
seungwonme <seungwonan.me@gmail.com>
2024-10-21 17:52:39 +0900

    feat: 계산 기능 구현
    - `App` 클래스에 `calculate` 메서드 추가
    - 아무 입력을 못 받았을 때 에러 처리
    - 정규 표현식을 이용한 구분자를 추출 후 문자열을 분리
    - `index.js` 파일에서 `App`이 던지는 exception을 처리하게끔 수정

--- src/App.js
+++ src/App.js
@@ -5,8 +5,33 @@ class App {
     const line = await Console.readLineAsync(
       '덧셈할 문자열을 입력해 주세요.\n'
     );
-    Console.print(`결과 : ${line}`);
+    const result = this.calculate(line);
+    Console.print(`결과 : ${result}`);
   }
+
+  calculate = (line) => {
+    if (!line) {
+      throw new Error('[ERROR] 입력값이 없습니다.');
+    }
+
+    let delimiters = [',', ':'];
+    let numbersString = line;
+
+    const regex = new RegExp(`${delimiters.join('|')}`);
+
+    let numbers = [];
+
+    try {
+      numbers = numbersString.split(regex).map((num) => {
+        // console.log(`num: "${num}"`);
+        return parseInt(num, 10);
+      });
+    } catch (error) {
+      throw new Error('[ERROR] 잘못된 입력입니다.');
+    }
+
+    return numbers.reduce((acc, cur) => acc + cur, 0);
+  };
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import App from "./App.js";
+import App from './App.js';
 
 const app = new App();
-await app.run();
+app.run().catch((error) => {
+  console.error(error.message);
+});
