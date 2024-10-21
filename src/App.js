f3fda7ac279d89c973619e2f87fa68f09b67a424
YJS96 <wnt9864@hotmail.com>
2024-10-21 14:29:19 +0900

    feat:(App.js): 커스텀 구분자 기준으로 분리한 숫자의 합 반환

--- src/App.js
+++ src/App.js
@@ -15,7 +15,23 @@ class App {
   }
 
   calculateSum(str) {
-    const numbers = str.split(/[,:]/).map(Number);
+    let numbers;
+    // 기본 구분자
+    let customSeprator = ',|:';
+
+    // 커스텀 구분자
+    if (str.startsWith('//')) {
+      // '\n'으로 나누기
+      const splitedStr = str.split('\\n');
+      // '//' 다음의 문자를 구분자로 사용
+      customSeprator = splitedStr[0].substring(2);
+      // 실제 숫자 문자열
+      str = splitedStr.slice(1).join('');
+    }
+
+    // 문자열을 구분자로 분리하고 각 요소를 숫자로 변환
+    numbers = str.split(new RegExp(customSeprator)).map(Number);
+    // 숫자 배열의 합계 계산
     const sum = numbers.reduce((acc, curr) => acc + curr, 0);
 
     return sum;
