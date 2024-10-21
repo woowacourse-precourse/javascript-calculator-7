d4c2fd96f1ece95ad5fd5567febf245d94a7b538
SuJin <kimsujin001127@gmail.com>
2024-10-21 19:39:19 +0900

    feat: 커스텀 구분자 사용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    function isCustomSeparator(str) {
+      return str.includes("//") && str.includes("\\n");
+    }
+
+    function getCustomSeparator(str) {
+      return str.split("//")[1].split("\\n")[0];
+    }
+
     function getArrSum(arr) {
       return arr.reduce(
         (accumulator, currentValue) =>
@@ -16,8 +24,14 @@ class App {
           "덧셈할 문자열을 입력해 주세요.\n"
         );
         let sum = 0;
+        if (isCustomSeparator(str)) {
+          const numArr = str.split("\\n")[1].split(getCustomSeparator(str));
+
+          sum = getArrSum(numArr);
+        } else {
           const numArr = str.split(/[,: ]/);
           sum = getArrSum(numArr);
+        }
         Console.print("결과 : " + sum);
       } catch (error) {
         Console.print("에러가 발생했습니다: ", error);
