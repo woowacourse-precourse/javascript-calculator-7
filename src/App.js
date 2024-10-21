64761792089008ab0a99decc8d79a57a87b8f180
MyungJiwoo <1206jiwoo@gmail.com>
2024-10-21 16:04:30 +0900

    feat: 기본 구분자(,:) 케이스 덧셈 기능

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
     async run() {
         try {
+            let result = 0;
+
+            // 입력 : 사용자의 문자열 입력
             const inputData = await Console.readLineAsync(
                 "덧셈할 문자열을 입력해 주세요.\n"
             );
 
-            Console.print(`결과 : ${inputData}`);
+            // case1: 기본 구분자(,:) 케이스
+            const NORMAL_SEPARATOR = /^[0-9,\:]+$/; // 기본 구분자와 숫자로만 구성되어 있는가?
+            let separatorCase = NORMAL_SEPARATOR.test(inputData);
+            if (separatorCase) {
+                const exportNumbers = inputData.split(/[, :]+/);
+                result = exportNumbers.reduce(
+                    (acc, num) => acc + Number(num),
+                    0
+                );
+            }
+
+            // 출력 : 결과 값 출력
+            Console.print(`결과 : ${result}`);
         } catch (error) {
             Console.print(`[ERROR] : ${error.message}`);
         }
