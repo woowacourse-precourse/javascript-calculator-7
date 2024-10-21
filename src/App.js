5a9cab96a995052b09f64ba2fb5969e61fc02bb4
syg0629 <syg0629@hanmail.net>
2024-10-21 21:29:24 +0900

    feat: 입력된 문자열을 구분자로 분리하는 함수 작성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    // 입력 기능 작성
+    // 입력 기능
     const input = await MissionUtils.Console.readLineAsync(
       '덧셈할 문자열을 입력해주세요.\n'
     );
-    MissionUtils.Console.print(`결과 : ${input}`);
+
+    const result = this.getDelimiters(input);
+    MissionUtils.Console.print(`결과 : ${result}`);
+  }
+
+  // 쉼표, 콜론, 커스텀 구분자로 문자열을 분리하는 함수
+  getDelimiters(str) {
+    // 공백 문자열 처리
+    if (str.trim() === '') return 0;
+
+    // 기본 구분자
+    let delimiter = /,|:/;
+
+    // 기본 구분자 처리
+    if (str.match(delimiter)) {
+      return str.split(delimiter);
+    }
+
+    // 커스텀 구분자 처리
+    if (str.startsWith('//')) {
+      const [customDelimiter, rest] = str.split('\\n');
+      delimiter = customDelimiter[2];
+      return rest.split(delimiter);
+    }
   }
 }
 
