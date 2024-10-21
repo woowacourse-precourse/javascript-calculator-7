632e5db872df4c5329cbef0de3088078e5fe1982
ajeong <jenny2360@tukorea.ac.kr>
2024-10-21 22:53:08 +0900

    feat: 커스텀 구분자를 사용한 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,35 +3,50 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      const input = await Console.readLineAsync(
+      let input = await Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요: "
       );
 
+      // 입력값이 없는 경우 결과 0 출력
       if (!input) {
         Console.print("결과 : 0");
         return;
       }
+
+      let customPattern = ""; // 커스텀 구분자
+
+      // 커스텀 구분자 확인
+      const customCheck = input.includes("//") && input.includes("\\n");
+      if (customCheck) {
+        customPattern = input.match(/\/\/(.*?)\\n/);
+      }
+
+      // 커스텀 구분자를 제외한 문자열 추출
+      if (customPattern) {
+        input = input.split("\\n")[1];
+      }
+
+      let seperatePattern = new RegExp(`[${customPattern}|,|:]`);
+
+      const numbers = input.split(seperatePattern).map((value) => {
+        // 구분자가 아닌 문자열이나 음수가 입력된 경우 에러 처리
+        if (isNaN(value)) {
+          throw new Error("[ERROR] 바르지 않은 문자열이 포함됨");
+        } else if (Number(value) < 0) {
+          throw new Error("[ERROR] 음수가 포함됨");
+        }
+        return Number(value);
+      });
 
       // 문자열만 입력된 경우 에러 처리
       if (/^[^0-9]*$/.test(input)) {
         throw new Error("[ERROR] 숫자가 입력되지 않음");
       }
 
-      // 구분자가 없는 경우 에러 처리 (숫자만 입력된 경우)
+      // 구분자가 없는 경우 에러 처리(숫자만 입력된 경우)
       if (/^\d+$/.test(input)) {
-        throw new Error("[ERROR] 구분자가 포함되지 않은 문자열");
+        throw new Error("[ERROR] 구분자가 포함되지 않음");
       }
-
-      // 기본 구분자
-      const defaultPattern = /[,:]/;
-
-      const numbers = input.split(defaultPattern).map((value) => {
-        // 구분자가 아닌 문자열이나 음수가 입력된 경우 에러 처리
-        if (isNaN(value) || Number(value) < 0) {
-          throw new Error("[ERROR] 바르지 않은 문자열 혹은 음수가 포함됨");
-        }
-        return Number(value);
-      });
 
       const sum = numbers.reduce((acc, num) => acc + num, 0);
 
