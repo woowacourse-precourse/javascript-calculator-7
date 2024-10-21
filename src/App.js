5c5c1f4365765d7d1a6a5f4f65e78401e296fbf6
thwlstn <thwlstn23@gmail.com>
2024-10-21 18:23:21 +0900

    feat : thwlstn 커스텀 구분자를 처리하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,16 +10,24 @@ class App {
         });
     }
 
-    // 쉼표와 콜론을 기본 구분자로 처리하여 문자열을 분리하고 합산하는 함수
     calculateSum(input) {
         if (input === '') {
-            return 0; // 빈 문자열은 0을 반환
+            return 0;
         }
 
-        // 기본 구분자 쉼표(,)와 콜론(:)으로 분리
-        const numbers = input.split(/[,|:]/);
+        let numbers;
 
-        // 숫자 배열로 변환하고 합산
+        // 커스텀 구분자가 존재하는지 확인
+        if (input.startsWith('//')) {
+            // 커스텀 구분자 형식: //[구분자]\n[숫자들]
+            const [delimiterSection, numberSection] = input.split('\n');
+            const customDelimiter = delimiterSection.slice(2);
+            numbers = numberSection.split(customDelimiter);
+        } else {
+            // 기본 구분자 쉼표와 콜론을 처리
+            numbers = input.split(/[,|:]/);
+        }
+
         const sum = numbers.reduce((acc, curr) => acc + Number(curr), 0);
         return sum;
     }
