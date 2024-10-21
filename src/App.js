6e8b6301630c2345e0e1dddc1c59c2e575e298b9
(hoooooony) <(cjh41820@gmail.com)>
2024-10-21 16:40:35 +0900

    feat (계산기) :  문자열로 입력을 받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await this.userInput();
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  userInput() {
+    return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+  }
 }
 
 export default App;
