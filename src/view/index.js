1fcf440ac953545025f3b49104420a8a168c4971
kimgho <kgh67811@naver.com>
2024-10-21 15:17:28 +0900

    fix(view): 입출력 수정
    - 요구 사항에 맞는 입출력으로 변경

--- src/view/index.js
+++ src/view/index.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils"
 //I/O 작업 세팅
 export class View {
   static async input() {
-    return await Console.readLineAsync("문자열을 입력하세요");
+    return await Console.readLineAsync("덧셈할 문자열을 입력하세요.\n");
   }
 
   static printResult(result) {
