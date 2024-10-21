4d7612dd4de704ceb03b3a63d45fd8aeab185b0c
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-10-21 23:03:19 +0900

    refactor: calculate함수 중복 로직 별도의 함수로 추출

--- src/utils/calculationUtils.js
+++ src/utils/calculationUtils.js
@@ -1,5 +1,15 @@
-import { escapeRegExp, checkDefaultFormat } from "./formatUtils";
+import { escapeRegExp } from "./formatUtils";
 import { DEFAULT_DELIMITER } from "../constants/constant";
+
+function splitInput(input, delimiters) {
+  const escapedDelimiters = delimiters.map(escapeRegExp);
+  const regex = new RegExp(escapedDelimiters.join("|"));
+  return input.split(regex);
+}
+
+function sumNumbers(numbers) {
+  return numbers.reduce((sum, num) => sum + Number(num), 0);
+}
 
 export function calculate(input) {
   // 공백인 경우
@@ -8,17 +18,11 @@ export function calculate(input) {
   // 커스텀 구분자가 존재하는 경우
   if (input.slice(0, 2) === "//" && input.slice(3, 5) === "\\n") {
     const customDelimiter = [...DEFAULT_DELIMITER, input[2]];
-    let remain = input.slice(5);
-    const changedEscapeDelimiters = customDelimiter.map(escapeRegExp);
-    const cleanedRemain = remain.split(
-      new RegExp(changedEscapeDelimiters.join("|"))
-    );
-    return cleanedRemain.reduce((sum, num) => sum + Number(num), 0);
+    const splitNumbers = splitInput(input.slice(5), customDelimiter);
+    return sumNumbers(splitNumbers);
   }
 
   // 그 외
-  if (checkDefaultFormat(input)) {
-    const cleanedInput = input.split(new RegExp(DEFAULT_DELIMITER.join("|")));
-    return cleanedInput.reduce((sum, num) => sum + Number(num), 0);
-  }
+  const splitNumbers = splitInput(input, DEFAULT_DELIMITER);
+  return sumNumbers(splitNumbers);
 }
