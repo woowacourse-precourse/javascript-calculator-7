2009a75e3592de21c70a27bc73d62591d2bce633
norangsuji <sujihong93@gmail.com>
2024-10-21 15:33:56 +0900

    feat(app.js): print starting message
    Console.print to display the initial message " Please enter the string for addition.\

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  // 전체 실행 흐름
+  async run() {
+    Console.print("덧셈할 문자열을 입력해 주세요."); // 시작
+  }
 }
 
 export default App;
