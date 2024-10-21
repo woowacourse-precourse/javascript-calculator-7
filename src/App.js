a4a1f8f1bfe57cde57501a568b3ed2d4ef321ab7
이재준 <202010907@pukyong.ac.kr>
2024-10-21 11:58:59 +0000

    feat: 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import readline from 'readline';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 1. '덧셈할 문자열을 입력해 주세요.' 문구를 출력한다.
+      this.promptMessage();
+
+     
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  // 1. '덧셈할 문자열을 입력해 주세요.' 문구를 출력한다.
+  promptMessage() {
+    console.log('덧셈할 문자열을 입력해 주세요.');
+  }
+
 }
 
 export default App;
