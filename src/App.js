88c2c0a2eca10205949ddd1b76e6f2d5384f1d82
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-21 21:01:47 +0900

    feat: 커스텀 구분자 추가 함수 정의

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App {
       const char = strArr[this.cursor];
 
       if (char === '/') { // "/"로 시작하는 경우
-        this.handleCustomSeparator();
+        this.handleCustomSeparator(strArr);
       } else if (isNaN(Number(char))) { // 숫자가 아닌 경우
         this.handleNotNumber();
       } else { // 숫자인 경우
@@ -25,7 +25,20 @@ class App {
     }
   }
 
-  handleCustomSeparator() {}
+  handleCustomSeparator(strArr) {
+    if (this.cursor === 0) {
+      const customSeparatorPlaceholder = strArr.join('').substr(this.cursor, 6);
+      const customSeparatorReg = new RegExp(/\/\/(\D{1})\\n/);
+
+      if (customSeparatorReg.test(customSeparatorPlaceholder)) {
+        const customSeparator = customSeparatorPlaceholder.match(customSeparatorReg)[1];
+        this.separator.push(customSeparator);
+        this.cursor += 5;
+      } 
+    } else {
+      // handleNotNumber(); // "/"가 커스텀 구분자를 지정하기 위해 등장한게 아니라면 (숫자 사이 구분자로서 입력된 경우)
+    }
+  }
 
   handleNotNumber() {}
 
