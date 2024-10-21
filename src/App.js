1d0387c4bc8596a545c686d247c04b2a791896e9
songhaeunsong <gkdms3469@naver.com>
2024-10-21 15:17:28 +0900

    feat: 파싱된 숫자 예외 처리

--- src/App.js
+++ src/App.js
@@ -14,6 +14,36 @@ class App {
     const regex = this.changeToRegex(separators);
     const numbers = this.getNumberCutWithSeparator(regex, numberString);
 
+    for (let i = 0; i < numbers.length; i++) {
+      const num = numbers[i];
+
+      if (i === 0 && num === "") {
+        return this.printError("첫 글자가 구분자일 수 없습니다.");
+      }
+
+      if (i === numbers.length - 1 && num === "") {
+        return this.printError("마지막 글자가 구분자일 수 없습니다.");
+      }
+
+      if (num === "") {
+        return this.printError("구분자를 연속해서 작성할 수 없습니다.");
+      }
+
+      if (num.includes(" ")) {
+        return this.printError(
+          "구분자와 구분자 사이에 공백문자가 들어가 있습니다."
+        );
+      }
+
+      if (isNaN(Number(num))) {
+        return this.printError("숫자가 아닌 다른 값이 포함되어 있습니다.");
+      }
+
+      if (Number(num) < 0) {
+        return this.printError("음수는 입력할 수 없습니다.");
+      }
+    }
+
     const sum = this.calculateSum(numbers);
     return this.printAnswer(sum);
   }
