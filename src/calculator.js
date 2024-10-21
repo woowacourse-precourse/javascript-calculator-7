62e3da3a13f3c32e0ebf0232e5d5a088dcc9b483
osw6858 <osw7875@naver.com>
2024-10-21 12:29:41 +0900

    fix: 문자열배열이 아닌 숫자 배열을 리턴하도록 수정

--- src/calculator.js
+++ src/calculator.js
@@ -3,18 +3,21 @@ import {Console} from '@woowacourse/mission-utils';
 export async function calculator() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.')
     const parseResult = parseInput(input)
-    console.log('parseResult', parseResult)
+    Console.print(parseResult)
 }
 
 function parseInput(input) {
     if(input.startsWith('//')) {
         const [custom, number] = input.split(/\\n/)
-
         const separator = custom.substring(2)
+        const result = number.split(separator)
 
-        return number.split(separator)
+        return result.map((item) => Number(item))
     }else {
-        return input.split(/[,;]/)
+        const result =  input.split(/[,;]/)
+
+        return result.map((item) => Number(item))
     }
 }
+
 
