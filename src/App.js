175c08ef61ed27ff99b0a17dc95090e0e622e9ad
zzzzzuuuuu <agu0830@naver.com>
2024-10-21 20:59:34 +0900

    feat: 빈문자열 입력시 0 출력

--- src/App.js
+++ src/App.js
@@ -21,14 +21,20 @@ class App {
     const isPositiveInt = (word) => /^\d+$/.test(word);
 
     // 값 체크 및 출력
-    if (words.includes(NaN) || words.includes(null) || words.includes(undefined)) {
-      Console.print("[ERROR] 숫자나 구분자의 입력이 올바른지 확인해주세요.");
-    } else if(!words.every(word => isPositiveInt(word))) {
-      Console.print("[ERROR] 양수만 입력해주세요.")
+    if (words.length > 0) {
+      if (words.includes(NaN) || words.includes(null) || words.includes(undefined)) {
+        Console.print("[ERROR] 숫자나 구분자의 입력이 올바른지 확인해주세요.");
+      } else if(!words.every(word => isPositiveInt(word))) {
+        Console.print("[ERROR] 양수만 입력해주세요.")
+      } else {
+        const sum = words.reduce((acc, curr) => acc + curr);
+        Console.print("결과 : " + sum);
+      }
     } else {
-      const sum = words.reduce((acc, curr) => acc + curr);
-      Console.print("결과: " + sum);
+      return 0;
     }
+
+
   }
 }
 
