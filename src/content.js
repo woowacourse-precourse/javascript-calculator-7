0ae10611aeadc6aa73ed9aec779805685f394bf7
dudcns02 <duddl2460@naver.com>
2024-10-21 17:03:13 +0900

    refactor: Declaration of Global Constant Recycling messages

---
+++ src/content.js
@@ -0,0 +1,10 @@
+const MESSAGE = Object.freeze({
+  START_CALCULATOR: "덧셈할 문자열을 입력해 주세요.\n",
+  CALCULATOR_RESULT: "결과: ",
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  INCORRECT_VALUE: "[ERROR]",
+});
+
+export { MESSAGE, ERROR_MESSAGE };
