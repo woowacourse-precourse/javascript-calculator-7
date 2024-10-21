3e3059c158bb06069e01be33cd448ab43f9c451a
ChangOKIM <kk104202@naver.com>
2024-10-21 14:58:40 +0900

    feat: 쉼표와 콜론 구분자를 사용한 기본 덧셈 기능 추가

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,8 +3,12 @@ class StringCalculator {
         // 빈 문자열 처리
         if (input === "") return 0;
 
-        // 추후 기능 구현
-        return 0;
+        // 기본 구분자 처리
+        let delimiter = /,|:/;
+        let numbers = input;
+
+        const numberArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
+        return numberArray.reduce((sum, num) => sum + num, 0);
     }
 }
 
