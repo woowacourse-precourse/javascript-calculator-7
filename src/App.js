22597731b0f48ca4f03e3de0b9ff89532866987d
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-10-21 10:37:18 +0900

    fix: 테스트 케이스 실패 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const ERROR_MESSAGE = '[ERROR]: ';
+const ERROR_MESSAGE = '[ERROR]';
 class App {
   async run() {
     try {
@@ -12,7 +12,7 @@ class App {
         return;
       } else if (!(userInput.slice(0, 2) === '//' || !isNaN(userInput[0]))) {
         // 공백 제거후 값이 비어있거나 string이 '//' 혹은 숫자로 시작되지 않는경우 에러 반환
-        throw '입력 형식이 올바르지 않습니다.';
+        throw new Error('입력 형식이 올바르지 않습니다.');
       }
       if (userInput.slice(0, 2) === '//') {
         // "//"와 "\n"의 위치를 찾아서 그 사이 문자열을 추출
@@ -24,14 +24,16 @@ class App {
 
           if (!customSeparatorsFormat(customSeprator)) {
             // 커스텀 구분자에 숫자나 공백이 포함된 경우 에러반환
-            throw '커스텀 구분자에 공백이나 숫자는 포함할 수 없습니다.';
+            throw new Error(
+              '커스텀 구분자에 공백이나 숫자는 포함할 수 없습니다.',
+            );
           }
 
           separators.push(customSeprator);
           formula = userInput.substr(endIndex + 2);
         } else {
           // \n이 없는 경우 에러 반환
-          throw '커스텀 구분자를 사용하려면 \\n을 입력해주세요';
+          throw new Error('커스텀 구분자를 사용하려면 \\n을 입력해주세요');
         }
       } else {
         formula = userInput;
@@ -49,17 +51,18 @@ class App {
         .reduce((acc, current) => {
           if (acc === '' || current === '') {
             // 구분자를 연속해서 입력한 경우 에러 반환
-            throw '구분자를 연속해서 입력하셨습니다.';
+            throw new Error('구분자를 연속해서 입력하셨습니다.');
           }
           if (isNaN(acc) || isNaN(current))
-            throw '옳지 않은 구분자를 사용하였습니다.';
+            throw new Error('옳지 않은 구분자를 사용하였습니다.');
           // 숫자 덧셈 기능
           return Number(acc) + Number(current);
         }, 0);
 
-      Console.print(answer);
+      Console.print(`결과 : ${answer}`);
     } catch (error) {
-      Console.print(ERROR_MESSAGE + error);
+      // Console.print(ERROR_MESSAGE);
+      throw new Error(ERROR_MESSAGE);
     }
   }
 }
