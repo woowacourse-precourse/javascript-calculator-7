a34cd8f1f9ab3d65e0e9c9d255353301e6a2424e
GSIII <kalseo901@gmail.com>
2024-10-21 18:07:49 +0900

    fix(logic) : 구분자 문자열 허용, 커스텀 정규식 수정

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,26 @@
 export const separatorSplit = (input) => {
   if (!input || input.trim() === "") {
-    return [];
+    return [0];
   }
 
   const basicSeparator = /[,:]/; // 기본 구분자 , 또는 :
   let customSeparator = "";
 
-  const customRegex = /\/\/([^\\n]+)(?=\\n)/;
+  const customRegex = /\/\/(.+)\\n/;
 
   // 사용자 정의 구분자가 있는지 확인
   const match = input.match(customRegex);
   if (match) {
     customSeparator = match[1]; // 구분자 추출
 
-    if (customSeparator.length !== 1) {
-      throw new Error(
-        "[ERROR_INVALID_SEPARATOR] : 구분자에 문자열을 입력할 수 없습니다."
-      );
-    }
+    // if (customSeparator.length !== 1) {
+    //   throw new Error(
+    //     "[ERROR_INVALID_SEPARATOR] : 구분자에 문자열을 입력할 수 없습니다."
+    //   );
+    // }
 
     input = input.substring(match[0].length); // 숫자 부분 추출
   }
-  input = input.replace(/\\n/g, "\n").trim(); // "\n"을 문자로 처리
 
   // 구분자에 따라 문자열 분리
   const numbers = customSeparator
