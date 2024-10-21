69eb9ab1c5f436a0f468247c80062b723f3c1435
Dobbymin <gmin0701@knu.ac.kr>
2024-10-21 19:05:56 +0900

    refactor(calculate): controller 코드 포맷팅 변경

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,6 +1,9 @@
 import { getStringInput, getArrayInput } from '../view/input.js';
 import { printResult } from '../view/output.js';
-import { determineNumber, classificationDelimiter } from '../model/calculate.js';
+import {
+  determineNumber,
+  classificationDelimiter,
+} from '../model/calculate.js';
 import { CONSTANTS } from '../constants/constants.js';
 
 const { NUMBERS_FIRST_CHARACTER } = CONSTANTS;
@@ -13,7 +16,9 @@ class Controller {
     const inputArray = getArrayInput(inputNumber);
 
     // 문자의 첫글자로 판별하기
-    const initialCharacter = determineNumber(inputArray[NUMBERS_FIRST_CHARACTER]);
+    const initialCharacter = determineNumber(
+      inputArray[NUMBERS_FIRST_CHARACTER],
+    );
 
     // 구분자를 통해 계산 로직 분기 처리
     const result = classificationDelimiter(inputNumber, initialCharacter);
