282bd5b04744ad085211ee6b6adc4f16d529662a
GaYun Lee <prettygu2355@naver.com>
2024-10-21 23:48:18 +0900

    Update App.js with string calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
-class App {
-  async run() {}
+import { Console } from '@woowacourse/mission-utils';
+
+class StringCalculator {
+    static add(input) {
+        if (!input || input === "") {
+            return 0;
+        }
+
+        const customDelimiterMatch = input.match(/^\/\/(.)\n/);
+        let delimiters = /[,:]/;
+        let numbersString = input;
+
+        if (customDelimiterMatch) {
+            const customDelimiter = customDelimiterMatch[1];
+            delimiters = new RegExp(`[${customDelimiter}]`);
+            numbersString = input.split("\n")[1];
+        }
+
+        const numbers = numbersString.split(delimiters).map(Number);
+        this.validateNumbers(numbers);
+
+        return numbers.reduce((sum, num) => sum + num, 0);
+    }
+
+    static validateNumbers(numbers) {
+        numbers.forEach(num => {
+            if (isNaN(num) || num < 0) {
+                throw new Error("[ERROR] 잘못된 입력값입니다.");
+            }
+        });
+    }
 }
 
-export default App;
+class App {
+    async run() {
+        try {
+            Console.print("프로그램 시작");  // 디버깅 출력
+            const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+            Console.print(`입력값: ${input}`);  // 디버깅 출력
+            const result = StringCalculator.add(input);
+            Console.print(`결과 : ${result}`);
+        } catch (error) {
+            Console.print(error.message);
+        }
+    }
+}
+
+const app = new App();
+app.run();
