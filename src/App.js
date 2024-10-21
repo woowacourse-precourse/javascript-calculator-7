cfb110338ae288fb639e75bb9ee87a1f5feaaeb1
MyungJiwoo <1206jiwoo@gmail.com>
2024-10-21 15:12:40 +0900

    feat: 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+    async run() {
+        try {
+            const inputData = await Console.readLineAsync(
+                "덧셈할 문자열을 입력해 주세요.\n"
+            );
+
+            Console.print(`결과 : ${inputData}`);
+        } catch (error) {
+            Console.print(`[ERROR] : ${error.message}`);
+        }
+    }
 }
 
 export default App;
