8ef294ed0df026a39d52ccbc7428cf7ec24239fe
junwoo <junwooha1@gmail.com>
2024-10-21 16:44:32 +0900

    feat(StringCalculator.js): 빈 문자열 입력 시 0을 반환하는 기능 추가

---
+++ src/StringCalculator.js
@@ -0,0 +1,11 @@
+class StringCalculator {
+    add(numbers) {
+      // 1. 빈 문자열 입력 시 0을 반환
+      if (!numbers) {
+        return 0;
+      }
+    }
+  }
+  
+  export default StringCalculator;
+  
