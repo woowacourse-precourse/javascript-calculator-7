d70c0734693ef6f39b451e1d1218b7b3f3849b8f
COMPOSEDKIM <composedkim@naver.com>
2024-10-21 16:59:01 +0900

    feat: 구분자를 기준으로 분리한 각 숫자의 합을 반환 및 에러 처리

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ class App {
   async run() {
     const userString = await this.getUserString();
     const customDelimiter = this.getCustomDelimiter(userString);
-    //MissionUtils.Console.print(customDelimiter);
+    const calArray = this.getDelimiterArray(userString, customDelimiter);
+    const calsum = this.getArraysum(calArray);
+    MissionUtils.Console.print("결과 : " + calsum);
   }
 
   //문자열 입력 받기
   async getUserString() {
     try {
       const userString = await MissionUtils.Console.readLineAsync(
-        "덧셈할 문자열을 입력해 주세요 \n"
+        "덧셈할 문자열을 입력해 주세요. \n"
       );
       return userString;
     } catch (error) {}
@@ -25,6 +27,31 @@ class App {
     } else {
       return false;
     }
+  }
+
+  //구분자를 기준으로 배열 반환
+  getDelimiterArray(string, customDelimiter) {
+    if (customDelimiter) {
+      string = string.replace(/\/\/(.+)\\n/, "");
+      let delimiterPattern = new RegExp(`[:,${customDelimiter}]`);
+      return string.split(delimiterPattern);
+    } else {
+      return string.split(/[:,]/);
+    }
+  }
+
+  //배열의 합 구하기, 잘못된 값 입력 검증
+  getArraysum(cal) {
+    let sum = 0;
+    cal.forEach((num) => {
+      num = parseInt(num);
+      if (typeof num !== "number" || num <= 0) {
+        throw new Error("[ERROR]");
+      } else {
+        sum = sum + num;
+      }
+    });
+    return sum;
   }
 }
 
