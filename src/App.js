2202c30a27faddb16591cd3459cf80ab22a0ebe5
an hyeonjin <hyeonjinan096@naver.com>
2024-10-21 11:58:58 +0900

    feat: 문자열 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputMessage = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    let message = inputMessage.trim();
+
+    if (!message) {
+      Console.print("결과 : 0");
+      return;
+    }
+  }
 }
 
 export default App;
