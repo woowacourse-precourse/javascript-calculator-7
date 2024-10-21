09af2a35e45c2ef2ad4fa56fb9aa9da6384f4336
wine-dev <markwon56@naver.com>
2024-10-21 23:48:06 +0900

    refactor: 문자열 계산기 로직 개선 및 테스트 통과

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ class App {
     const INPUT_PROMPT = '덧셈할 문자열을 입력해 주세요: ';
     const userInputString = await Console.readLineAsync(INPUT_PROMPT);
 
-    // 아래 함수의 결과에 따른 예외최리
     try {
       const sumResult = this.calculateSum(userInputString);
-      Console.print(sumResult);
+      Console.print(`결과 : ${sumResult}`);
     } catch (error) {
       Console.print(error.message);
+      throw error;
     }
   }
 
   // 입력 받은 문자열 안의 숫자 합계 계산 (input: string, output: number)
   calculateSum(inputString) {
     // 입력이 비어있으면 0을 반환
-    if (inputString === '') {
+    if (inputString.trim() === '') {
       return 0;
     }
 
@@ -30,11 +30,11 @@ class App {
 
   // 입력 문자열을 파싱하여 숫자 부분과 커스텀 구분자를 추출
   parseInput(inputString) {
-    const customSeparatorPattern = /^\/\/(.+)\\n(.+)/;
+    const customSeparatorPattern = /^\/\/(.)\\n/;
     const matchResult = inputString.match(customSeparatorPattern);
     if (matchResult) {
       return {
-        numbersString: matchResult[2],
+        numbersString: inputString.replace(/^\/\/(.)\\n/, '').trim(),
         customSeparator: matchResult[1],
       };
     }
@@ -43,29 +43,35 @@ class App {
 
   // 기본 구분자 쉼표(,)와 콜론(:)를 기준으로 입력받은 문자열 분리 후 숫자로 변환 (input: string, output: number[])
   extractNumbers(numbersString, customSeparator) {
-    const allSeparators = [
-      ',',
-      ':',
-      ...(customSeparator ? [customSeparator] : []),
-    ];
-    const separatorRegex = new RegExp(`[${allSeparators.join('')}]`);
+    let separators = [',', ':'];
+    if (customSeparator) {
+      separators = [customSeparator];
+    }
+    const separatorRegex = new RegExp(
+      `[${separators
+        .map((sep) => sep.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'))
+        .join('')}]`,
+      'g'
+    );
+    const separatedInput = numbersString.replace(separatorRegex, ',');
+    const splitValues = separatedInput.split(',').map((num) => num.trim());
 
-    return numbersString
-      .split(separatorRegex)
-      .map((numString) => numString.trim())
-      .filter(Boolean)
-      .map(Number);
+    if (splitValues.some((value) => value === '')) {
+      throw new Error('[ERROR] 구분자 사이에 숫자가 없습니다');
+    }
+
+    return splitValues.map(Number);
   }
 
   validateNumbers(numberArray) {
     // 숫자가 아닌 값이 있으면 오류 발생
-    if (numberArray.some((num) => isNaN(num))) {
-      throw new Error('[ERROR] 잘못된 입력입니다.');
+    if (numberArray.some((num) => isNaN(num) || !Number.isInteger(num))) {
+      throw new Error('[ERROR] 잘못된 값이 입력되었습니다');
     }
 
-    // 음수가 포함되어 있으면 오류 발생
-    if (numberArray.some((num) => num < 0)) {
-      throw new Error('[ERROR] 음수는 허용되지 않습니다.');
+    // 0이나 음수가 포함되어 있으면 오류 발생
+    if (numberArray.some((num) => num <= 0)) {
+      throw new Error('[ERROR] 입력값은 양수로만 구성되어야 합니다');
     }
   }
 }
