c84177ab67a903fd2524869062c8dec712479910
0Jaemin0 <jaemin2167@naver.com>
2024-10-21 17:25:17 +0900

    refactor: 커스텀 구분자 추출 로직을 분리

--- src/utils/checkCustomDelimiter.js
+++ src/utils/checkCustomDelimiter.js
@@ -1,10 +1,10 @@
 import ERROR_MESSAGES from "../constants/errorMessages.js";
+import extractCustomDelimiter from "./extractCustomDelimiter.js";
 
 const checkCustomDelimiter = (input, delimiters) => {
   if (input.startsWith("//") && input.includes("\\n")) {
     const index = input.indexOf("\\n");
-    const customDelimiter = input.slice(2, index);
-    delimiters.push(...customDelimiter.split(""));
+    delimiters.push(...extractCustomDelimiter(input, index));
 
     return input.slice(index + 2);
   } else if (input.includes("//") || input.includes("\\n")) {
