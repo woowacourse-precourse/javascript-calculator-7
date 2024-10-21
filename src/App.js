3b68abf4cef9f62e6556c0e58134204ecc34c057
keemunji <kej20211496@gmail.com>
2024-10-21 22:02:28 +0900

    feat : 사용자 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 사용자 입력
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.");
+    const input = await MissionUtils.Console.readLineAsync();
+
+    // 사용자 출력과 에러 출력
+    try {
+      const result = this.calculateSum(input);
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch (error) {
+      MissionUtils.Console.print(`[ERROR] ${error.message}`);
+      throw error;
+    }
+  }
 }
 
 export default App;
