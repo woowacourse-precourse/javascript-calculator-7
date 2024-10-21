a98c52f84781a4f9babef63cafefea8e89d967bb
DongHyeonWon <dhe7700@naver.com>
2024-10-21 15:06:02 +0900

    refac: 에러 메시지 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,3 +2,8 @@ export const CONSOLE_MESSAGES = {
   GET_STRING: "덧셈할 문자열을 입력해 주세요.\n",
   GET_RESULT: (result) => `결과 : ${result}`,
 };
+
+export const ERROR_MESSAGES = {
+  EMPTY_INPUT: "[ERROR] 빈 값을 입력할 수 없습니다.",
+  CUSTOM_DELIMITER: "[ERROR] 커스텀 구분자가 올바르지 않습니다. \\n이 포함되어야 합니다.",
+};
--- src/validator/inputValidator.js
+++ src/validator/inputValidator.js
@@ -1,7 +1,9 @@
+import { ERROR_MESSAGES } from "../constants/messages.js";
+
 const inputValidator = {
   emptyInput: (inputString) => {
     if (inputString === "" || inputString.trim() === "") {
-      throw new Error("[ERROR] 빈 값을 입력할 수 없습니다.");
+      throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
     }
   },
 
@@ -9,7 +11,7 @@ const inputValidator = {
     const customDelimiterPattern = /^\/\//;
     const customDelimiterMatch = inputString.match(customDelimiterPattern);
     if (customDelimiterMatch && !inputString.includes("\\n")) {
-      throw new Error("[ERROR] 커스텀 구분자가 올바르지 않습니다. \\n이 포함되어야 합니다.");
+      throw new Error(ERROR_MESSAGES.CUSTOM_DELIMITER);
     }
   },
 };
