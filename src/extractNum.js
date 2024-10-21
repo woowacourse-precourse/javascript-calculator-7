150d3d5a386dba15bad9be6c447cfcae08ae726e
magik99 <hojun0810@naver.com>
2024-10-21 17:35:28 +0900

    feat(extractNum.js): 에러 메시지 출력 기능 구현
    input 형식이 올바르지 않을 경우 Error를 예외 처리하는 기능 구현

--- src/extractNum.js
+++ src/extractNum.js
@@ -3,21 +3,32 @@ const extractNum = (input) => {
 
   if (input.startsWith('//')) {
     const delimiterEnd = input.indexOf('\n');
+    console.log(delimiterEnd);
     if (delimiterEnd !== -1) {
       const customDelimiter = input.slice(2, delimiterEnd);
       delimiter = new RegExp(`[${customDelimiter}]`);
 
       input = input.slice(delimiterEnd + 1);
     } else {
-      new Error();
+      throw new Error('[ERROR] 커스텀 구분자가 마무리 되지 않았습니다!!!');
     }
   }
 
   const splitInput = input.split(delimiter);
 
   // 숫자인지 체크하고 아니면 throw error
+  const numbers = splitInput
+    .map((num) => num.trim())
+    .map((num) => {
+      if (isNaN(num) || num === '') {
+        throw new Error(
+          '[ERROR] 입력 값 형식이 올바르지 않습니다!!!(숫자 추출 오류)',
+        );
+      }
+      return Number(num);
+    });
 
-  return splitInput.map(Number);
+  return numbers;
 };
 
 export default extractNum;
