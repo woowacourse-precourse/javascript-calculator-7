7e25a41a12fb6c279dece8fc28f42baa681b6e89
JunHyeong Kim <junbro1908@gmail.com>
2024-10-21 10:16:13 +0900

    feat(calculator): add support for empty string handling
    If the input string is empty, the result will be 0.
    Example: "" => 0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+    const result = this.add(input);
+    Console.print(`결과 : ${result}`);
+  }
+
+  add(input) {
+    if (!input) {
+      return 0;
+    }
+  }
 }
 
 export default App;
