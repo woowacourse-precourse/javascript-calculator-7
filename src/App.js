c731489867a7503c8f5bb1ca23b1b41f61a20f6d
Yongho Kim <kyhcraft@gmail.com>
2024-10-21 22:36:45 +0900

    feat:  기본 구분자로 덧셈

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ class App {
     }
   }
 
+  computeSum(inputString) {
+    let separator = /[,:]/;
+
+    const numberArray = inputString.split(separator).map((value) => value.trim()).map(Number);
+    this.exceptionHandler(numberArray.length === 0, "[ERROR]");
+
+    return numberArray.reduce((total, currentValue) => total + currentValue, 0);
+  }
+
   async run() {
     Console.print("덧셈할 문자열을 입력해 주세요.");
     const inputString = await Console.readLineAsync("");
