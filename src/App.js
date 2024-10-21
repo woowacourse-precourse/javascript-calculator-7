64ea60cfc7cf58ded491be66b944fb546b7a3f0f
yuri295 <jin_astro0302@naver.com>
2024-10-21 14:13:12 +0900

    feat: 커스텀 구분자를 기준으로 분리

--- src/App.js
+++ src/App.js
@@ -7,15 +7,34 @@ class App {
         "덧셈할 문자열을 입력해 주세요.\n"
       );
 
-      const result = this.calculateSum(input);
-      MissionUtils.Console.print(`결과: ${result}`);
+      let numbers = input;
+      let separator = [",", ":"]; // 기본 구분자
+      let numberList;
+
+      numberList = this.extractNumbers(numbers, separator); // 숫자 리스트 생성
+      const result = this.calculateSum(numberList); // 숫자 합산
+      MissionUtils.Console.print(`결과 : ${result}`);
     } catch (error) {
       MissionUtils.Console.print(`[ERROR] ${error.message}`);
     }
   }
-  calculateSum(input) {
-    const numbers = input.split(/[, :]/).map(Number);
-    return numbers.reduce((sum, number) => sum + number, 0);
+
+  extractNumbers(numbers, separator) {
+    let numberList;
+    if (numbers.startsWith("//")) {
+      const list = numbers.split("\\n");
+      const customSeparator = list[0].slice(2); // 커스텀 구분자 추출
+      const separateNumbers = list[1]; // 계산할 부분 추출
+      const regex = new RegExp(`[${customSeparator}]`, "g");
+      numberList = separateNumbers.split(regex).map(Number);
+    } else {
+      numberList = numbers.split(new RegExp(separator.join("|"))).map(Number);
+    }
+    return numberList;
+  }
+
+  calculateSum(numberList) {
+    return numberList.reduce((sum, number) => sum + number, 0); // 숫자 합산
   }
 }
 
