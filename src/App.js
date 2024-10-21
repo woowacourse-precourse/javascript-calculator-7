7edb004ec71ad189c8134dc701210e5462c06de8
Jaeho Cha <ischar@naver.com>
2024-10-21 12:16:27 +0900

    feat(app): implement App class to handle input, process, and output
    - 메인 프로그램 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { convertNumberList } from "./features/convertNumberList.js";
+import { extractNumbersAsString } from "./features/extractNumbersAsString.js";
+import { sumNumberList } from "./features/sumNumberList.js";
+import { inputHandler } from "./handlers/inputHandler.js";
+import { outputHandler } from "./handlers/outputHandler.js";
+
 class App {
-  async run() {}
+  async run() {
+    const INPUT = await inputHandler();
+    const STRING_NUM_LIST = extractNumbersAsString(INPUT);
+    let output = "";
+
+    try {
+      const NUM_LIST = convertNumberList(STRING_NUM_LIST);
+      const SUM = sumNumberList(NUM_LIST);
+      output = `결과: ${SUM}`;
+    } catch (error) {
+      output = `[ERROR]: ${error.message}`;
+    } finally {
+      outputHandler(output);
+    }
+  }
 }
 
 export default App;
