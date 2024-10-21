cbbb013bfa218a7f9723e249a981dcf179c16986
inji0212 <in17100170@kakao.com>
2024-10-21 15:45:15 +0900

    feat ,와:로 문자열 구분 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('입력: ');
+    const numbers = input.split(/,|:/).map(Number);
+    Console.print(numbers);
+  }
 }
 
 export default App;
