8c019359b5581eb6a8aa5c264909e9b3571e5f43
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-22 05:47:04 +0900

    refactor: 에러 메시지 상수로 분리

--- src/StringParser.js
+++ src/StringParser.js
@@ -1,4 +1,5 @@
 import InputHandler from './IOHandler.js'; 
+import ERROR_MESSAGE from './constant/ErrorMsg.js';
 
 const CUSTOM_SEPARATOR_FORMAT_LENGTH = 5;
 
@@ -21,7 +22,7 @@ class StringParser {
         if (!this.separator.includes(customSeparator)) this.separator.push(customSeparator);
         this.cursor += 5;
       } else {
-        throw new Error('[ERROR] 입력 형식을 다시 확인해 주세요. 커스텀 구분자를 지정하려면 "//[1자리의 숫자가 아닌 문자]\n" 형식으로 입력해 주세요. (ex. "//;\n1;2;3")');
+        throw new Error(ERROR_MESSAGE.INVALID_CUSTOM_SEPARATOR_FORMAT);
       }
     } else {
       this.handleNotNumber(char, strArr); // "/"가 커스텀 구분자를 지정하기 위해 등장한게 아니라면 (숫자 사이 구분자로서 입력된 경우)
@@ -30,7 +31,7 @@ class StringParser {
 
   handleNotNumber(char, strArr) {
     if (this.cursor === 0 || this.cursor === strArr.length - 1) {
-      throw new Error('[ERROR] 입력 형식을 다시 확인해 주세요.');
+      throw new Error(ERROR_MESSAGE.INVALID_INPUT);
     }
 
     if (this.separator.includes(char)) {
@@ -38,13 +39,13 @@ class StringParser {
       this.number = [];
       this.cursor++;
     } else {
-      throw new Error('[ERROR] 구분자 외의 문자가 포함되어 있습니다.');
+      throw new Error(ERROR_MESSAGE.NOT_SEPARATOR);
     }
   }
 
   handleNumber(char, strArr) {
     if (char === '0' && this.number.length === 0) {
-      throw new Error('[ERROR] 입력값은 구분자와 양수로 구성되어야 합니다.');
+      throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
     }
 
     this.number.push(char);
---
+++ src/constant/errorMsg.js
@@ -0,0 +1,8 @@
+const ERROR_MESSAGE = {
+  INVALID_INPUT: '[ERROR] 입력 형식을 다시 확인해 주세요.',
+  NOT_SEPARATOR: '[ERROR] 구분자 외의 문자가 포함되어 있습니다.',
+  INVALID_NUMBER: '[ERROR] 숫자는 양수만 입력할 수 있어요.',
+  INVALID_CUSTOM_SEPARATOR_FORMAT: '[ERROR] 커스텀 구분자를 지정하려면 "//{1자리의 숫자가 아닌 문자}\\n" 형식으로 입력해 주세요. (ex. "//;\\n1;2;3")',
+};
+
+export default ERROR_MESSAGE;
