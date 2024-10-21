3bea6c74c5bb180dd5ec6c6714e611b417124150
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2024-10-21 23:19:02 +0900

    feat: 구분자 확인 후 문자열 분리 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await this.getUserInput();
+    const splitedString = this.stringSplitDelimiter(input);
   }
 
   async getUserInput() {
     const input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
     return input;
+  }
+
+  stringSplitDelimiter(inputString) {
+    let str = [];
+
+    // 커스텀 구분자 사용 시
+    if (inputString[0] === "/" && inputString[1] === "/") {
+      const index = inputString.indexOf("\\n");
+      const delimiter = inputString.substring(2, index);
+      const stringToSplit = inputString.substring(index + 2);
+      str = stringToSplit.split(delimiter).map(Number);
+    } else {
+      // 기본 구분자(쉼표, 콜론) 사용 시
+      str = inputString.split(/,|:/).map(Number);
+    }
+
+    return str;
   }
 }
 
