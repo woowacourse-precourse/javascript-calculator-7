8b11d7a6c41d73bd60ba4ef78a005f2399bb30ff
Jiyun Kim <wayandway@kakao.com>
2024-10-21 23:23:35 +0900

    fix: 구분자 이스케이프 처리, validateNumbers 개선, 정규식 패턴 수정

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,17 @@
 export function calculator(input) {
-    if (!input) return 0;
-  
-    const { customDelimiter, numbers } = parseInput(input);
-    const delimiter = customDelimiter ? new RegExp(`[${customDelimiter},:]`) : /[,:]/;
-    const splitNumbers = numbers.split(delimiter).map(Number);
-  
-    validateNumbers(splitNumbers);
-  
-    return splitNumbers.reduce((sum, num) => sum + num, 0);
-  }
-  
+  if (!input) return 0;
+
+  const { customDelimiter, numbers } = parseInput(input);
+  const delimiter = customDelimiter ? new RegExp(`[${escapeRegExp(customDelimiter)},:]`) : /[,:]/;
+  const splitNumbers = numbers.split(delimiter).map(Number);
+
+  validateNumbers(splitNumbers);
+
+  return splitNumbers.reduce((sum, num) => sum + num, 0);
+}
+
 function parseInput(input) {
-  const customDelimiterPattern = /^\/\/(.)\n/;
+  const customDelimiterPattern = /^\/\/(.)\\n/;
   const match = input.match(customDelimiterPattern);
 
   if (match) {
@@ -24,9 +24,13 @@ function parseInput(input) {
 }
 
 function validateNumbers(numbers) {
-  for (const num of numbers) {
-    if (isNaN(num) || num < 0) {
-      throw new Error('[ERROR] 음수는 허용되지 않습니다.');
-    }
+  const negativeNumbers = numbers.filter((num) => num < 0);
+
+  if (negativeNumbers.length > 0) {
+    throw new Error(`[ERROR] 음수는 허용되지 않습니다: ${negativeNumbers.join(', ')}`);
   }
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); 
 }
