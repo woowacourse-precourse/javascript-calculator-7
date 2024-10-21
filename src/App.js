67c5333e0c86179d5f2093f90967b2c567e948cc
seocylucky <seocylucky@github.com>
2024-10-21 11:59:36 +0900

    feat: `,`와 `:`의 기본 구분자 기준으로 각 문자 합산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console } from "@woowacourse/mission-utils";
+import { DEFAULT_SEPARATOR } from "./constant.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const inputStr = await Console.readLineAsync("문자열을 입력해주세요.\n");
+
+      const separator = new RegExp(`[${DEFAULT_SEPARATOR.join("")}]`);
+
+      const result = inputStr ? sumNumbers(inputStr, separator) : 0;
+
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      throw new Error(`[ERROR] ${error.message}`);
+    }
+  }
+}
+
+// 숫자 합산 함수
+function sumNumbers(numbers, separator) {
+  const splitNumbers = numbers.split(separator);
+
+  const sum = splitNumbers.reduce((acc, current) => {
+    const num = parseInt(current, 10);
+
+    return acc + num;
+  }, 0);
+
+  return sum;
 }
 
 export default App;
