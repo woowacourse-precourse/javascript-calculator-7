564169a598b714375b876fc92c8e361b37cda0a7
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-21 16:47:24 +0900

    feat: 입출력 형식 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+import preprocess from "./functions/preprocess";
+
 class App {
-  async run() {}
+  async run() {
+    // 입력
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해주세요.\n"
+    );
+
+    // 출력값
+    let output;
+    output = preprocess(output);
+
+    // 출력
+    Console.print(`결과 : ${output}`);
+  }
 }
 
 export default App;
