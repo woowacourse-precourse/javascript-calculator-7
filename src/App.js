550f5367c88e35758df0eeb280cc6ce880d0a48e
Chaesunbak <coanstjd0422@gmail.com>
2024-10-21 17:36:39 +0900

    feat : 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+
+      const INPUT = await App.getInput();
+
+    } catch(error) {
+      console.print(error.message);
+    }
+  }
+
+  static async getInput() {
+    const INPUT = await Console.readLineAsync(
+      '덧셈할 문자열을 입력해주세요.\n'
+    );
+
+    return INPUT;
+  }
 }
 
 export default App;
