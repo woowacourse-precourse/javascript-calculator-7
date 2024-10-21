c2efb5ade2e93dc0972df5a39490bfc8d9d8f98a
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-21 11:52:20 +0900

    feat(App) 음수가 입력될 경우, 숫자 형식이 아닌 문자열이 입력될 경우 에러 처리, 숫자의 합 반환

--- src/App.js
+++ src/App.js
@@ -37,6 +37,19 @@ class App {
 
     return { delimiter, numbersPart };
   }
+
+  sumNumbers(numbers) {
+    return numbers.reduce((total, num) => {
+      const number = parseInt(num, 10);
+      if (isNaN(number)) {
+        throw new Error("잘못된 숫자가 포함되어 있습니다.");
+      }
+      if (number < 0) {
+        throw new Error("음수는 허용되지 않습니다.");
+      }
+      return total + number;
+    }, 0);
+  }
 }
 
 export default App;
