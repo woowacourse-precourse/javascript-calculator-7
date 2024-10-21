a50a82da2937a1a86b156d9b19ea720be2b85e4a
JHLEE325 <ljntiger325@naver.com>
2024-10-21 13:34:12 +0900

    feat : 커스텀 구분자 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -14,11 +14,30 @@ class App {
 
   divide(input){
     let symbol = [',',':'];
+    let numstring = input
+
+    if (input.startsWith('//')) {
+      const symbolindex = input.indexOf('\\n');
+      MissionUtils.Console.print(`symbolindex: ${symbolindex}`);
+      if (symbolindex === -1) {
+          this.throwError("");  // 잘못된 형식이면 에러 발생
+      }
+
+      // 커스텀 구분자를 추출
+      const customsymbol = input.substring(2, symbolindex);
+      symbol.push(customsymbol);  // 커스텀 구분자를 기본 구분자 배열에 추가
+      console.log(symbol);
+      numstring = input.substring(symbolindex + 2);  // 숫자 부분 추출
+  }
   
     const dividepattern = new RegExp (`[${symbol.join('')}]`);
-    const result = input.split(dividepattern);
+    const result = numstring.split(dividepattern);
   
     return result;
+  }
+
+  throwError() {
+    throw new Error('[ERROR] 잘못된 입력입니다.');
   }
 }
 
