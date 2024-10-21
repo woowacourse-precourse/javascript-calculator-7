2194439042c4a9b546dbf5bda25cbfdea91caa7c
YeRimmm-Cho <em514@dgu.ac.kr>
2024-10-21 21:03:00 +0900

    feat(App): 빈 문자열 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -9,12 +9,26 @@ class App {
         "덧셈할 문자열을 입력해 주세요.\n"
       );
 
-      // 입력된 값을 그대로 출력
-      MissionUtils.Console.print(`입력된 값: ${input}`);
+      // 빈 문자열 처리
+      const result = this.calculate(input);
+
+      // 빈 문자열이 아닌 경우 입력된 값을 그대로 출력
+      MissionUtils.Console.print(`결과: ${result}`);
     } catch (error) {
-      // 오류
+      // 오류났을 때 출력
       MissionUtils.Console.print(error.message);
     }
+  }
+
+  calculate(input) {
+    // 빈 문자열일 경우에 0 반환
+    if (input === "") {
+      return 0;
+    }
+
+    //다른 로직 추가 예정
+
+    return input;
   }
 }
 
