ce0d0b16b715cd1b028e85b6633a6dced43dc265
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-10-21 15:35:42 +0900

    docs: input값 상수명 스네이크케이스로 수정

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     Console.print('덧셈할 문자열을 입력해 주세요.')
-    const input = await Console.readLineAsync('');
+    const INPUT = await Console.readLineAsync('');
 
-    if (input === ""){
+    if (INPUT === ""){
       Console.print('0')
     }
 
     let separation =  /[,|:]/;
     let numbers = [];
-    const CUSTOM_START_INDEX = input.startsWith('//')
-    const CUSTOM_END_INDEX = input.indexOf('\\n')
+    const CUSTOM_START_INDEX = INPUT.startsWith('//')
+    const CUSTOM_END_INDEX = INPUT.indexOf('\\n')
 
     if (CUSTOM_START_INDEX) {
       if (CUSTOM_END_INDEX !== -1){ 
-          const CUSTOM_SEPERATION = input.substring(2, CUSTOM_END_INDEX)
+          const CUSTOM_SEPERATION = INPUT.substring(2, CUSTOM_END_INDEX)
           separation = new RegExp(`[${CUSTOM_SEPERATION},|:]`);
-          numbers = input.substring(CUSTOM_END_INDEX+2).split(separation)
+          numbers = INPUT.substring(CUSTOM_END_INDEX+2).split(separation)
           }
           else{
             throw new Error("[ERROR]잘못된 입력입니다.");
           }
     }else{
-        numbers = input.split(separation)
+        numbers = INPUT.split(separation)
       }
 
     if(numbers.length > 0 && numbers.find((num)=>isNaN(num) || num <=0)){
