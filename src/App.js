cd175db076a0a072613be3e610a6c09db45dd3fa
Jin-Chanyong <cksdyd324@gmail.com>
2024-10-21 12:19:39 +0900

    feat(App): 문자열 분리 후 합 출력 구현
    App.js 에 이하 코드 추가:
    - 쉼표 또는 콜론을 구분자로 가지는 문자열 입력받음
    - 문자열을 구분하여 각각 더한 뒤, 합을 출력함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const inputValue = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+
+    const TempValueArray = inputValue.split(",");
+
+    const splitedValueArray = TempValueArray.map((value) => {
+      if (value.includes(":")) {
+        return value.split(":");
+      }
+
+      return value;
+    }).flat();
+
+    const sum = splitedValueArray.reduce((acc, curr) => acc + +curr, 0);
+
+    Console.print(`결과 : ${sum}`);
+  }
 }
 
 export default App;
