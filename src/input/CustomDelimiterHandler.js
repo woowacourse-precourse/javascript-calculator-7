5fdf0f9ef35103875141895afa1789db37a22ce7
Junseo Kim <semiwest2@gmail.com>
2024-10-21 21:31:26 +0900

    refactor: 가독성을 위해 커스텀 구분자 처리 함수의 null 반환 위치 변경
    - getCustomDelimiter 함수에서 커스텀 구분자가 없을 때 null을 반환하는 부분을 조건문 위로 이동했습니다.

--- src/input/CustomDelimiterHandler.js
+++ src/input/CustomDelimiterHandler.js
@@ -3,19 +3,20 @@ import { CUSTOM_DELIMITER_REGEX } from '../constant/constant.js';
 //커스텀 구분자가 있다면 추출해 반환하는 함수
 export function getCustomDelimiter(input) {
   const match = input.match(CUSTOM_DELIMITER_REGEX);
-  if (match) {
-    const customDelimiter = match[1];
-    if (/\d/.test(match[1])) {
-      throw new Error("[ERROR] : 커스텀 구분자는 숫자가 될 수 없습니다!")
-    }
+  if (!match)
+    return null;
 
-    if (/^\s+$/.test(customDelimiter)) {
-      throw new Error("[ERROR] : 커스텀 구분자는 공백일 수 없습니다!");
-    }
+  const customDelimiter = match[1];
 
-    return customDelimiter;
+  if (/\d/.test(match[1])) {
+    throw new Error("[ERROR] : 커스텀 구분자는 숫자가 될 수 없습니다!")
   }
-  return null;
+
+  if (/^\s+$/.test(customDelimiter)) {
+    throw new Error("[ERROR] : 커스텀 구분자는 공백일 수 없습니다!");
+  }
+
+  return customDelimiter;
 }
 
 //커스텀 구분자 부분을 문자열에서 제거하는 함수
