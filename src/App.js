4223de3906e867f22b579552ff078976fd717f4f
Jaewon <wellbird@naver.com>
2024-10-21 14:14:08 +0900

    feat : 입력 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import getCustomSplitter from './getCustomSplitter.js';
 import input from './input.js';
+import getCustomSplitter from './getCustomSplitter.js';
+import checkValidate from './checkValidate.js';
 
 class App {
   async run() {
     const userInput = await input();
     const [splitter, string] = getCustomSplitter(userInput);
+
+    if (!checkValidate(splitter, string)) {
+      throw new Error('[ERROR]잘못된 입력입니다.');
+    }
   }
 }
 
