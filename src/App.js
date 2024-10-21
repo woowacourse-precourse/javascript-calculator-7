114925ec170f85da90556fde3411cdbb7f663785
YSW2 <jrgyber@gmail.com>
2024-10-21 17:55:52 +0900

    feat(parse_string): 문자열 파싱 기능 함수 추가
    - 기본 구분자 ( , ), ( : )
    - 숫자가 아닌 문자 입력 시 Error 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  parse_string = (input) => {
+    let numbers;
+    let delimiter = /[,:]/;
+
+    numbers = input.split(delimiter).map((num) => {
+      const parsed = parseInt(num.trim(), 10);
+      if (isNaN(parsed)) {
+        throw new Error(`[ERROR] 유효하지 않은 입력값: ${num}`);
+      }
+      return parsed;
+    });
+
+    return numbers;
+  };
+  async run() {
+    try {
+      const user_line = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해주세요.\n'
+      );
+      const numbers = this.parse_string(user_line);
+      Console.print(`결과: ${numbers}`);
+    } catch (error) {
+      Console.print(error);
+    }
+  }
 }
 
 export default App;
