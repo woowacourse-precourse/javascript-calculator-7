fcbca78394d0c932b6d0ca08265f05b1ab1ad7c1
Doyeon <duseh729@naver.com>
2024-10-21 22:07:44 +0900

    feat: 2번 커스텀 문자열 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,33 +2,33 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
 
-    try{
-      Console.print(this.addNumber(input))
-    }catch{
-
-    }
+    try {
+      Console.print(`결과: ${this.addNumber(input)}`);
+    } catch {}
   }
 
   addNumber(input) {
-    const possible = [':', ','];
-    const num = [];
-  
-    const inputArray = input.split(possible[0]);
-    console.log(inputArray); 
-  
-    for (let i of inputArray) {
-      num.push(...i.split(possible[1]));
+    const possible = [":", ","];
+    let cutInput = input;
+
+    if (input.substr(0, 2) === "//") {
+      possible.push(input[2]);
+      cutInput = input.substr(5);
     }
-  
-    console.log(num); 
-  
-    return num.reduce((a, b) => Number(a) + Number(b), 0);
+
+    let numArray = [cutInput];
+    
+    for (let p of possible) {
+      let tempArray = [];
+      for (let item of numArray) {
+        tempArray.push(...item.split(p)); 
+      }
+      numArray = tempArray;
+    }
+    return numArray.reduce((a, b) => Number(a) + Number(b), 0);
   }
-  
 }
 
 export default App;
