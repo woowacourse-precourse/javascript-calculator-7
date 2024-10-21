27d7ac90cfea0f96f9ee93949cf8131f9b6e017f
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-21 15:01:03 +0900

    refactor(App.js) \\n관련 에러 핸들링
     입력할 때, \n이 문자 그대로 들어가서 이 경우, 입력된 \n을 실제 개행 문자로 변환해야 조건을 만족할 수 있었습니다.
    따라서, 입력된 문자열에서 \\n이 실제 개행 문자(\n)로 변환되도록 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App {
                 "덧셈할 문자열을 입력해 주세요.\n"
             );
 
-            str = str.replace(/^"|"$/g, "");
+            str = str.replace("\\n", "\n");
 
             let dividerExp = /[,:]/;
             let strNumbers = str;
@@ -17,30 +17,29 @@ class App {
                 return;
             }
 
-            if (str.startsWith("//") && (str.indexOf("\n") !== -1) !== -1) {
+            if (str.startsWith("//") && str.indexOf("\n") !== -1) {
                 const divider = str.slice(2, str.indexOf("\n"));
                 dividerExp = new RegExp(divider);
-                console.log("구분자 : ", dividerExp);
                 strNumbers = str.slice(str.indexOf("\n") + 1);
             }
-            console.log("문자열_숫자 : ", strNumbers);
-            // 숫자로 변환
+
             const numbers = strNumbers.split(dividerExp).map((NUM) => {
-                console.log("분리된 값: ", NUM);
-                const PARSE_NUM = Number(NUM.trim());
-                if (isNaN(PARSE_NUM)) {
+                if (isNaN(NUM)) {
                     throw new Error("잘못된 숫자가 포함되었습니다.");
                 }
-                if (PARSE_NUM < 0) {
+
+                if (NUM < 0) {
                     throw new Error("음수는 허용되지 않습니다.");
                 }
+                const PARSE_NUM = Number(NUM.trim());
+
                 return PARSE_NUM;
             });
 
             const SUM = numbers.reduce((SUM, NUM) => SUM + NUM, 0);
             await Console.print(`결과: ${SUM}`);
         } catch (error) {
-            console.error("[ERROR]: ", error.message); // 에러 메시지 출력
+            console.error("[ERROR]: ", error.message);
         }
     }
 }
