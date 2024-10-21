77ca15b4cfe5b0c9ce4d53476d860cb0d7c6840d
deokisys <sjb378@me.com>
2024-10-21 20:00:21 +0900

    [test] : 예상 입력과 에러 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -17,6 +17,51 @@ const getLogSpy = () => {
 };
 
 describe("문자열 계산기", () => {
+  test("기본 입력 테스트(쉼표)", async () => {
+    const inputs = ["1,2,3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("기본 입력 테스트(콜론)", async () => {
+    const inputs = ["1:2:3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("기본 입력 테스트(쉼표, 콜론)", async () => {
+    const inputs = ["1,2:3,4:5,6,7:11"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 39"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("커스텀 구분자 사용", async () => {
     const inputs = ["//;\\n1"];
     mockQuestions(inputs);
@@ -32,8 +77,31 @@ describe("문자열 계산기", () => {
     });
   });
 
+  test("커스텀 입력 테스트(쉼표, 콜론, 커스텀)", async () => {
+    const inputs = ["//;\\n1,2:3;4:5;6,7:11;12"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 51"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", async () => {
     const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("예외 테스트 등록 안된 구분자", async () => {
+    const inputs = ["1,2-3"];
     mockQuestions(inputs);
 
     const app = new App();
