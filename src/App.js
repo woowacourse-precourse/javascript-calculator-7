4aff6399db0264e1c7cbacfd3cda67b375b70f88
vgotu99 <vgotu99@gmail.com>
2024-10-21 11:22:57 +0900

    feat(validator): 숫자 유효성 검증 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ class App {
   constructor() {
     this.CUSTOM_DELIMITER_REGEX = /^\/\/(.)\n(.*)/;
     this.DEFAULT_DELIMITER = ",|:";
+    this.ERROR_PREFIX = "[ERROR]";
   }
 
   async run() {
     const INPUT = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    const { DELIMITER, NUMBERS } = this.parseInput(INPUT);
-    const PARSED_NUMBERS = this.splitAndParse(NUMBERS, DELIMITER);
-    Console.print(`분리된 숫자들: ${PARSED_NUMBERS.join(", ")}`);
+    try {
+      const { DELIMITER, NUMBERS } = this.parseInput(INPUT);
+      const PARSED_NUMBERS = this.splitAndParse(NUMBERS, DELIMITER);
+      this.validateNumbers(PARSED_NUMBERS);
+      Console.print(`유효한 숫자들: ${PARSED_NUMBERS.join(", ")}`);
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 
   parseInput(input) {
@@ -26,9 +32,21 @@ class App {
   }
 
   splitAndParse(numbers, delimiter) {
-    return numbers
-      .split(new RegExp(delimiter))
-      .map((num) => parseInt(num.trim(), 10));
+    return numbers.split(new RegExp(delimiter)).map((num) => {
+      const PARSED = parseInt(num.trim(), 10);
+      if (isNaN(PARSED)) {
+        throw new Error(
+          `${this.ERROR_PREFIX} 유효하지 않은 입력값입니다: ${num}`
+        );
+      }
+      return PARSED;
+    });
+  }
+
+  validateNumbers(numbers) {
+    if (numbers.some((num) => num < 0)) {
+      throw new Error(`${this.ERROR_PREFIX} 음수는 입력할 수 없습니다.`);
+    }
   }
 }
 
