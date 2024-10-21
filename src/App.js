bc655bce5ebba62b3d4b512d8bf2cc617f5f32ee
songhaeunsong <gkdms3469@naver.com>
2024-10-21 15:19:22 +0900

    refactor: Validator 클래스 분리 후 예외처리 메서드화

--- src/App.js
+++ src/App.js
@@ -2,46 +2,26 @@ import { Console } from "@woowacourse/mission-utils";
 import Validator from "./Validator.js";
 class App {
   async run() {
+    const validator = new Validator();
+
     const input = await this.getString();
     if (!input.length) return this.printAnswer(0);
 
     const [separators, numberString] = this.getSeparatorAndNumber(input);
 
-    if (separators === null) {
-      return this.printError("올바르지 않은 커스텀 구분자 형식입니다.");
+    try {
+      validator.validateCustomSeparator(separators);
+    } catch (error) {
+      return this.printError(error.message);
     }
 
     const regex = this.changeToRegex(separators);
     const numbers = this.getNumberCutWithSeparator(regex, numberString);
 
-    for (let i = 0; i < numbers.length; i++) {
-      const num = numbers[i];
-
-      if (i === 0 && num === "") {
-        return this.printError("첫 글자가 구분자일 수 없습니다.");
-      }
-
-      if (i === numbers.length - 1 && num === "") {
-        return this.printError("마지막 글자가 구분자일 수 없습니다.");
-      }
-
-      if (num === "") {
-        return this.printError("구분자를 연속해서 작성할 수 없습니다.");
-      }
-
-      if (num.includes(" ")) {
-        return this.printError(
-          "구분자와 구분자 사이에 공백문자가 들어가 있습니다."
-        );
-      }
-
-      if (isNaN(Number(num))) {
-        return this.printError("숫자가 아닌 다른 값이 포함되어 있습니다.");
-      }
-
-      if (Number(num) < 0) {
-        return this.printError("음수는 입력할 수 없습니다.");
-      }
+    try {
+      validator.validateNumbers(numbers);
+    } catch (error) {
+      return this.printError(error.message);
     }
 
     const sum = this.calculateSum(numbers);
@@ -67,11 +47,15 @@ class App {
     return null;
   }
   changeToRegex(separators) {
-    return new RegExp(separators.join("|"), "g");
+    const escapedSeparators = separators.map((sep) =>
+      sep.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+    );
+    return new RegExp(escapedSeparators.join("|"), "g");
   }
   getNumberCutWithSeparator(regex, str) {
     return str.split(regex);
   }
+
   calculateSum(arr) {
     return arr.reduce((acc, cur) => acc + +cur, 0);
   }
