871042277b08a6cd08ac3b2b5f99efbcc16d1479
rheefine <rheefine@gmail.com>
2024-10-21 14:32:15 +0900

    refactor: InputSeperator 클래스 테스트 케이스에 대한 상수화

--- __tests__/application/InputSeparatorTest.js
+++ __tests__/application/InputSeparatorTest.js
@@ -1,72 +1,46 @@
 import InputSeparator from '../../src/application/parser/InputSeparator.js';
 
-describe('hasCustomDelimiter 메서드 테스트', () => {
-  test('hasCustomDelimiter 메서드가 커스텀 구분자 존재 여부를 올바르게 판별하는지 확인', () => {
-    const testCases = [
-      { input: '//:\\n2:3:4', expected: true },
-      { input: '//\\\\n', expected: true },
-      { input: '', expected: false },
-      { input: '/', expected: false },
-      { input: '//', expected: false },
-      { input: '\\n', expected: false },
-      { input: '   \\n', expected: false },
-      { input: '//\\n', expected: false },
-      { input: '1,2,3', expected: false },
-      { input: 'sadfa', expected: false },
-    ];
+const TEST_CASES = [
+  { input: '//:\\n2:3:4', hasCustom: true, customDelimiter: ':', numberString: '2:3:4' },
+  { input: '//\\\\n', hasCustom: true, customDelimiter: '\\', numberString: '' },
+  { input: '', hasCustom: false, customDelimiter: null, numberString: '' },
+  { input: '/', hasCustom: false, customDelimiter: null, numberString: '/' },
+  { input: '//', hasCustom: false, customDelimiter: null, numberString: '//' },
+  { input: '\\n', hasCustom: false, customDelimiter: null, numberString: '\\n' },
+  { input: '   \\n', hasCustom: false, customDelimiter: null, numberString: '   \\n' },
+  { input: '//\\n', hasCustom: false, customDelimiter: null, numberString: '//\\n' },
+  { input: '1,2,3', hasCustom: false, customDelimiter: null, numberString: '1,2,3' },
+  { input: 'sadfa', hasCustom: false, customDelimiter: null, numberString: 'sadfa' },
+];
 
-    testCases.forEach(({ input, expected }) => {
-      const inputSeparator = new InputSeparator();
-      const result = inputSeparator.hasCustomDelimiter(input);
-      expect(result).toBe(expected);
+describe('InputSeparator', () => {
+  describe('hasCustomDelimiter 메서드 테스트', () => {
+    test('hasCustomDelimiter 메서드가 커스텀 구분자 존재 여부를 올바르게 판별하는지 확인', () => {
+      TEST_CASES.forEach(({ input, hasCustom }) => {
+        const inputSeparator = new InputSeparator();
+        const result = inputSeparator.hasCustomDelimiter(input);
+        expect(result).toBe(hasCustom);
+      });
     });
   });
 
   describe('getCustomDelimiter 메서드 테스트', () => {
     test('getCustomDelimiter 메서드가 커스텀 구분자를 반환하는지 확인', () => {
-      const testCases = [
-        { input: '//:\\n2:3:4', expected: ':' },
-        { input: '//\\\\n', expected: '\\' },
-        { input: '', expected: null },
-        { input: '/', expected: null },
-        { input: '//', expected: null },
-        { input: '\\n', expected: null },
-        { input: '   \\n', expected: null },
-        { input: '//\\n', expected: null },
-        { input: '1,2,3', expected: null },
-        { input: 'sadfa', expected: null },
-      ];
-
-      testCases.forEach(({ input, expected }) => {
+      TEST_CASES.forEach(({ input, customDelimiter }) => {
         const inputSeparator = new InputSeparator();
-        inputSeparator.getCustomDelimiter(input);
         const result = inputSeparator.getCustomDelimiter(input);
-        expect(result).toBe(expected);
+        expect(result).toBe(customDelimiter);
       });
     });
   });
-});
 
-describe('getNumberString 메서드 테스트', () => {
-  test('getNumberString 메서드가 숫자 문자열을 반환하는지 확인', () => {
-    const testCases = [
-      { input: '//:\\n2:3:4', expected: '2:3:4' },
-      { input: '//\\\\n', expected: '' },
-      { input: '', expected: '' },
-      { input: '/', expected: '/' },
-      { input: '//', expected: '//' },
-      { input: '\\n', expected: '\\n' },
-      { input: '   \\n', expected: '   \\n' },
-      { input: '//\\n', expected: '//\\n' },
-      { input: '1,2,3', expected: '1,2,3' },
-      { input: 'sadfa', expected: 'sadfa' },
-    ];
-
-    testCases.forEach(({ input, expected }) => {
-      const inputSeparator = new InputSeparator();
-      inputSeparator.getNumberString(input);
-      const result = inputSeparator.getNumberString(input);
-      expect(result).toBe(expected);
+  describe('getNumberString 메서드 테스트', () => {
+    test('getNumberString 메서드가 숫자 문자열을 반환하는지 확인', () => {
+      TEST_CASES.forEach(({ input, numberString }) => {
+        const inputSeparator = new InputSeparator();
+        const result = inputSeparator.getNumberString(input);
+        expect(result).toBe(numberString);
+      });
     });
   });
 });
