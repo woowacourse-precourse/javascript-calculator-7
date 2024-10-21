7742c56c9f0be901422130dfc64fceba1a9b8399
changyeon <changyeon@changyeonui-MacBookAir.local>
2024-10-21 20:36:01 +0900

    fix:negative number modify

--- src/calculate.js
+++ src/calculate.js
@@ -1,55 +1,60 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class Calculator {
-    constructor() {}
-  
-    add(input) {
-      if (input === "") {
-        return 0;
-      }
-  
-      let delimiter = /[,:]/;
-      let numbersString = input;
-  
-      if (input.startsWith("//")) {
-        const parts = input.split("\n");
-        if (parts.length < 2) {
-          throw new Error("[ERROR]");
-        }
-        const customDelimiter = parts[0].substring(2);
-        delimiter = new RegExp(`[${customDelimiter}]`);
-        numbersString = parts[1];
-      }
-      const numbers = numbersString.split(delimiter);
-      const sum = numbers.reduce((total, current) => {
-        const num = parseInt(current, 10);
-        if (isNaN(num) || num < 0) {
-          throw new Error("[ERROR]");
-        }
-        return total + num;
-      }, 0);
-  
-      return sum;
+  constructor() {}
+
+  add(input) {
+    if (input === "") {
+      return 0;
     }
-    async askForInput() {
-        while (true) {
-          try {
-            const userInput = await Console.readLineAsync(
-              "덧셈할 문자열을 입력해 주세요."
-            );
-    
-            Console.print(`결과 : ${this.add(userInput)}`);
-          } catch (error) {
-            Console.print("[ERROR]");
-            throw error;
-          }
-        }
+
+    let delimiter = /[,:]/;
+    let numbersString = input;
+
+    if (input.startsWith("//")) {
+      const parts = input.split("\n");
+      if (parts.length < 2) {
+        throw new Error("[ERROR]");
+      }
+      const customDelimiter = parts[0].substring(2);
+
+      delimiter = new RegExp(
+        `[${customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}]`
+      );
+      numbersString = parts[1];
+    }
+
+    const numbers = numbersString.split(delimiter);
+    const sum = numbers.reduce((total, current) => {
+      const num = parseInt(current, 10);
+      if (isNaN(num) || num < 0) {
+        throw new Error("[ERROR]");
+      }
+      return total + num;
+    }, 0);
+
+    return sum;
+  }
+
+  async askForInput() {
+    while (true) {
+      try {
+        const userInput = await Console.readLineAsync(
+          "덧셈할 문자열을 입력해 주세요."
+        );
+
+        Console.print(`결과 : ${this.add(userInput)}`);
+      } catch (error) {
+        Console.print("[ERROR]");
+        return;
       }
     }
-    
-    async function run() {
-      const calculator = new Calculator();
-      await calculator.askForInput();
-    }
-    
-    export { run };
+  }
+}
+
+async function run() {
+  const calculator = new Calculator();
+  await calculator.askForInput();
+}
+
+export { run };
