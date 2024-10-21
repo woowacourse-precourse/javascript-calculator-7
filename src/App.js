ec5380fad05b39f4850174e60439906ddf9c207e
김아영 <thay123028@naver.com>
2024-10-21 14:14:31 +0900

    fix: 에러 메시지 단순화

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ class App {
     return new Promise((resolve, reject) => {
       Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n").then((input) => {
         try {
-          const result = this.add(input);
-          Console.print(`결과 : ${result}`);
+          const RESULT = this.add(input);
+          Console.print(`결과 : ${RESULT}`);
           resolve(); // 성공적으로 실행되면 resolve 호출
         } catch (error) {
           Console.print(`[ERROR] ${error.message}`);
-          reject(); // 에러 발생 시 reject 호출
+          //reject(error); // 에러 발생 시 reject 호출
         }
       }).catch((error) => {
         Console.print(`[ERROR] ${error.message}`);
-        reject();
+        reject(error);
       });
     });
   }
@@ -30,24 +30,24 @@ class App {
     let delimiter = /,|:/; // 기본 구분자
 
     if (input.startsWith("//")) {
-      const parts = input.split("\\n");
-      delimiter = parts[0].slice(2); // 커스텀 구분자 추출
-      input = parts[1]; // 문자열 부분 추출
+      const PARTS = input.split("\\n");
+      delimiter = PARTS[0].slice(2); // 커스텀 구분자 추출
+      input = PARTS[1]; // 문자열 부분 추출
     }
 
     let numbers = input.split(delimiter).map(num => {
-      const parsedNum = Number(num);
+      const PARSEDNUM = Number(num);
       
-      if (isNaN(parsedNum)) {
+      if (isNaN(PARSEDNUM)) {
         throw new Error(`유효하지 않은 입력: ${num}`); // 숫자가 아닌 경우 에러 메시지
       }
       
-      return parsedNum;
+      return PARSEDNUM;
     });
 
-    const negativeNumbers = numbers.filter(num => num < 0);
-    if (negativeNumbers.length > 0) {
-      throw new Error(`음수는 허용되지 않습니다: ${negativeNumbers.join(", ")}`); // 숫자가 음수인 경우 에러 메시지
+    const NEGATIVENUMBERS = numbers.filter(num => num < 0);
+    if (NEGATIVENUMBERS.length > 0) {
+      throw new Error(`음수는 허용되지 않습니다: ${NEGATIVENUMBERS.join(", ")}`); // 숫자가 음수인 경우 에러 메시지
     }
 
     return this.sumNumbers(numbers); // 숫자의 합 계산
