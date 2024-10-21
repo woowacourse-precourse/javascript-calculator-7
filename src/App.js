663b93452f00fcaab878b01b7cf8679ca31e7118
jicho <jIcho6412@gmail.com>
2024-10-21 20:56:48 +0900

    덧셈할 문자열 전체를 우선 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 덧셈할 문자열 전체를 우선 입력받기
+    const printStr = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해주세요.\n"
+    );
+    Console.print(printStr);
+  }
 }
 
 export default App;
