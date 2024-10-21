e20f36d527e559447557e4723191fa0c1954077f
giseon <pakox123@gmail.com>
2024-10-21 19:38:48 +0900

    feat: 사용자 입력값 처리 및 결과 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+    async run() {
+        try {
+            const input = await this.getInput();
+            const result = this.add(input);
+            MissionUtils.Console.print(`결과 : ${result}`);
+        } catch (error) {
+            MissionUtils.Console.print(error.message);
+            throw error;
+        }
+    }
+
+    async getInput() {
+        return await MissionUtils.Console.readLineAsync();
+    }
+
+    add(input) {
+        return 0; 
+    }
 }
 
 export default App;
