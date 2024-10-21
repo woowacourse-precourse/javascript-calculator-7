60145e6f4841fe34365564354222bd9a332a0dd3
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2024-10-21 22:47:13 +0900

    feat add console message output and input
    기본 출력 메세지와 문자열 입력 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Console from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    Console.print("덧셈할 문자열을 입력해 주세요: ");
+    const input = await Console.readLineAsync();
+    Console.print("입력된 문자열: ${input}");
+  }
 }
 
 export default App;
