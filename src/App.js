dfaa1a5fea2e1fdd6f171c01415b43691a832a52
Jimin Lee <jmncrk04@gmail.com>
2024-10-21 22:04:53 +0900

    fix : 필요없는 코드 제거

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ class App {
     try {
       const input = await getInput();  // 사용자로부터 입력 받기
       
-      checkTypeAndCalc(input);  // 입력된 값을 checkType으로 전달하여 처리
+      checkTypeAndCalc(input);  // 입력된 값을 checkTypeAndCalc으로 전달하여 처리
       
     } catch (error) {
       printErrorMsg("입력 처리 중 오류가 발생했습니다.");
-      console.error(error);  // 디버깅을 위해 에러 로그 출력
     }
   }
 }
