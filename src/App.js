e968c22c1ffc94959242b2adfc10f336c6ce3aff
wnsxk2 <wnsxk206@gmail.com>
2024-10-21 20:52:18 +0900

    refactor: 문자열 덧셈 코드 분리
    구분자로 문자열을 배열로 반환하고 배열의 합을 반환하는 코드를 함수로 분리 하였습니다.

--- src/App.js
+++ src/App.js
@@ -23,30 +23,38 @@ function checkDelimiter(str) {
   return { delimiter, numberStr };
 }
 
+// 숫자로 이루어진 문자열 합 계산 메서드
+function sumAllNum(delimiter, numberStr) {
+  // 문자열을 구분자로 구분하여 배열 리턴
+  const numberArr = numberStr.split(delimiter);
+
+  // 배열의 총합 계산
+  const sum = numberArr.reduce((total, num) => {
+    const castedNumber = Number(num);
+    // 예외 처리
+    if (isNaN(castedNumber) || num < 0) {
+      throw new Error('문자열은 양수과 구분자로만 구성되어야합니다.');
+    }
+    return total + castedNumber;
+  }, 0);
+
+  return sum;
+}
+
 class App {
 
   async run() {
     try {
       // 문자열 입력
-      let str = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+      const answer = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
 
       // 구분자와 문자열 리턴
-      const { delimiter, numberStr } = checkDelimiter(str);
+      const { delimiter, numberStr } = checkDelimiter(answer);
 
-      // 문자열을 구분자로 구분한 배열 리턴
-      const numberArr = numberStr.split(delimiter);
+      // 숫자의 합 리턴
+      const result = sumAllNum(delimiter, numberStr);
 
-      // 배열의 총합을 리턴
-      const sum = numberArr.reduce((total, num) => {
-        const castedNumber = Number(num);
-        // 예외 처리
-        if (isNaN(castedNumber) || num < 0) {
-          throw new Error('문자열은 양수과 구분자로만 구성되어야합니다.');
-        }
-        return total + castedNumber;
-      }, 0);
-
-      Console.print(`결과 : ${sum}`);
+      Console.print(`결과 : ${result}`);
     } catch (error) {
       throw new Error(`[ERROR] ${error.message}`);
     }
