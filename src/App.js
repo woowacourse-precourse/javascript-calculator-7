8bd7ac3da93183b78021b39bdc17a95a0f6283ed
wowalswjd <alswjd5491@naver.com>
2024-10-21 11:38:26 +0900

    fix(App): 빈 문자열 커스텀 구분자 사용 불가 에러 추가

--- src/App.js
+++ src/App.js
@@ -25,8 +25,10 @@ class App {
   async handleCustomSeparator(input) {
     const match = this.extractCustomSeparator(input); // 커스텀 구분자 추출
 
-    if (match) { // 커스텀 구분자가 있으면
+    if (match) {
+      // 커스텀 구분자가 있으면
       const customSeparator = match[1];
+
       this.validateCustomSeparator(customSeparator); // 구분자 유효성 검사
       this.separators.push(customSeparator); // 구분자 배열에 커스텀 구분자 추가
       return input.slice(match[0].length); // 구분자 뒤에 있는 실제 문자열 반환
@@ -38,16 +40,18 @@ class App {
 
   // 커스텀 구분자를 추출하는 메서드
   extractCustomSeparator(input) {
-    const customSeparatorPattern = /\/\/(.+?)\\n/; // "//"로 시작하고 "\n"으로 끝나는 문자열 파악하는 정규식
+    const customSeparatorPattern = /\/\/(.*?)\\n/; // "//"로 시작하고 "\n"으로 끝나는 문자열 파악하는 정규식
     const match = input.match(customSeparatorPattern); // 매칭된 문자열 추출
     Console.print(match);
 
-    if (match && input.startsWith(match[0])) { // 매칭되는 문자열이 있고 그 문자열이 입력 문자열 맨 앞에서 시작한다면
+    if (match && input.startsWith(match[0])) {
+      // 매칭되는 문자열이 있고 그 문자열이 입력 문자열 맨 앞에서 시작한다면
       Console.print("커스텀 구분자가 있다");
       return match;
     }
 
-    if (match) { // 매칭되는 문자열이 있지만 맨 앞에서 시작하지 않는다면
+    if (match) {
+      // 매칭되는 문자열이 있지만 맨 앞에서 시작하지 않는다면
       throw new Error(
         "[ERROR] 커스텀 구분자는 입력 문자열 맨 앞에 위치해야 합니다."
       );
@@ -58,7 +62,15 @@ class App {
 
   // 커스텀 구분자의 유효성을 검사하는 메서드
   validateCustomSeparator(separator) {
-    if (separator.length !== 1) { // 커스텀 구분자의 길이 측정
+    // 빈 문자열인지 체크
+    if (separator === "") {
+      throw new Error(
+        "[ERROR] 빈 문자열은 커스텀 구분자로 사용할 수 없습니다."
+      );
+    }
+
+    if (separator.length !== 1) {
+      // 커스텀 구분자의 길이 측정
       throw new Error("[ERROR] 문자열은 커스텀 구분자로 사용할 수 없습니다.");
     }
 
