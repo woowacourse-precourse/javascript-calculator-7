db019650ea2b7fbee896c637842282a32aa24454
limielife <selim0401@gmail.com>
2024-10-21 18:11:24 +0900

    [fix] input

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import {StringAdder} from "./StringAdder.js";
 class App {
   async run() {
-    var input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하세요.");
+    var input = await MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력하세요.\n");
     //var input = "//;\n1;2;3";
     const stringAdder = new StringAdder(input);
     stringAdder.doCalculator();
