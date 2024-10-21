ef4bbff94b1db286e6c352882a25354a35ff23a4
10012 <nas7062@naver.com>
2024-10-21 09:37:37 +0900

    feat(test) : 테스트 함수 추가

---
+++ src/__tests__/Aplication.test.js
@@ -0,0 +1,76 @@
+import App from "../App.js"
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("문자열 계산기", () => {
+  test("커스텀 구분자 사용", async () => {
+    const inputs = ["//;\\n1"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 1"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+
+    
+  });
+  test("기본 구분자 사용", async () => {
+    const inputs = ["1,2:3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+
+  });
+
+  test("음수 테스트", async () => {
+    const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("isNaN 테스트", async () => {
+    const inputs = ["a,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("커스텀구분자 여러개 들어가있을 경우 ", async () => {
+    const inputs = ["//;;\n1;2;3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+});
