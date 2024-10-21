ba1ade422163ed435506fd4c2de2a60d393236dc
Yena Kim <120614713+ynkim-03@users.noreply.github.com>
2024-10-21 18:20:28 +0900

    refactor: 검증 관련 메서드 별도 클래스로 분리

--- src/App.js
+++ src/App.js
@@ -1,96 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
-import { DEFAULT_SEPARATOR, VALIDATION_DEFAULT, VALIDATION_CUSTOM, ERROR_MESSAGE } from './constants.js';
+import { VALIDATION_DEFAULT, VALIDATION_CUSTOM } from './constants.js';
+import InputValidator from './InputValidator.js';
 
 class App {
-  validateInput(input) {
-    if (this.isDefault(input)) {
-      return VALIDATION_DEFAULT;
-    } else if (this.isCustom(input)) {
-      return VALIDATION_CUSTOM;
-    }
-
-    throw new Error(ERROR_MESSAGE);
-  }
-
-  isDefault(input) {
-    const invalidInput = /[^0-9,:.]/;
-
-    if (!invalidInput.test(input)) {
-      this.validateDefaultFormat(input);
-      return true;
-    }
-    return false;
-  }
-
-  isCustom(input) {
-    const customSep = /^\/\/\D\\n/;
-
-    if (customSep.test(input)) {
-      this.validateCustomFormat(input);
-
-      if (input[2] !== '\\') {
-        const pattern = new RegExp(`[^0-9,:.${input[2]}]`);
-        if (pattern.test(input.slice(5)) || this.hasConsecutiveSeparators(input.slice(5), input[2])) {
-          throw new Error(ERROR_MESSAGE);
-        }
-      } else {
-        const pattern = new RegExp(/[^0-9,:.\\]/);
-        if (pattern.test(input.slice(5)) || this.hasConsecutiveSeparators(input.slice(5), '\\\\')) {
-          throw new Error(ERROR_MESSAGE);
-        }
-      }
-      return true;
-    }
-    return false;
-  }
-
-  startsAndEndsWithNumber(input) {
-    if (isNaN(input[0]) || isNaN(input[input.length - 1])) {
-      throw new Error(ERROR_MESSAGE);
-    }
-  }
-
-  hasConsecutiveSeparators(input, sep) {
-    const pattern = new RegExp(`[${sep}]{2,}`);
-    if (pattern.test(input)) {
-      throw new Error(ERROR_MESSAGE);
-    }
-  }
-
-  hasConsecutiveDecimalPoints(input) {
-    const pattern = new RegExp(/[.]{2,}/);
-    if (pattern.test(input)) {
-      throw new Error(ERROR_MESSAGE);
-    }
-  }
-
-  isCustomSeparatorDot(input) {
-    if (input[2] === '.') {
-      throw new Error(ERROR_MESSAGE);
-    }
-  }
-
-  validateDefaultFormat(input) {
-    if (input.length === 0) {
-      return;
-    }
-
-    this.startsAndEndsWithNumber(input);
-    this.hasConsecutiveSeparators(input, DEFAULT_SEPARATOR);
-    this.hasConsecutiveDecimalPoints(input);
-  }
-
-  validateCustomFormat(input) {
-    if (input.length === 5) {
-      return;
-    }
-
-    this.startsAndEndsWithNumber(input.slice(5));
-    this.hasConsecutiveSeparators(input.slice(5), DEFAULT_SEPARATOR);
-    this.hasConsecutiveDecimalPoints(input.slice(5));
-    this.isCustomSeparatorDot(input);
-  }
-
   convertToNumberArray(input, sep) {
     return input.split(sep).map(Number);
   }
@@ -105,7 +17,8 @@ class App {
 
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const validationResult = this.validateInput(input);
+    const validator = new InputValidator(input);
+    const validationResult = validator.validateInput();
 
     let sum = 0;
     if (validationResult === VALIDATION_DEFAULT) {
