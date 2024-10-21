8b66c24b7a4aa113028ecab66993364c28ccb2b6
inji0212 <in17100170@kakao.com>
2024-10-21 16:54:26 +0900

    feat 커스텀 구분 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const input = await Console.readLineAsync('입력: ');
-    const numbers = input.split(/,|:/).map(Number);
-    Console.print(numbers);
+    let sort = /,|:/;
+    let numbers = input;
+    //커스텀 구분-예시 @
+    // \\@\n2@3@6
+    // @임을 알아내기
+    const pattern_Custom = /\/\/(.*?)\\n/;
+
+    const sort_Custom = input.match(pattern_Custom);
+
+    if (sort_Custom) {
+      sort = sort_Custom[1];
+       // [0]= //@\n이니까  문자인 @이 나오게 1번째
+      numbers = input.slice(sort_Custom[0].length);
+    }
+    const result = numbers.split(sort).map(Number);
+    Console.print(result);
   }
 }
 
