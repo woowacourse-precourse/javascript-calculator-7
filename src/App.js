0dfd74b7387e958fd24bd6c386f9fd660f381c09
Doyeon <duseh729@naver.com>
2024-10-21 21:47:53 +0900

    feat: 1번 쉼표(,) 및 콜론(:) 구분자 숫자의 합 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요.\n"
+    );
+
+    try{
+      Console.print(this.addNumber(input))
+    }catch{
+
+    }
   }
+
+  addNumber(input) {
+    const possible = [':', ','];
+    const num = [];
+  
+    const inputArray = input.split(possible[0]);
+    console.log(inputArray); 
+  
+    for (let i of inputArray) {
+      num.push(...i.split(possible[1]));
+    }
+  
+    console.log(num); 
+  
+    return num.reduce((a, b) => Number(a) + Number(b), 0);
+  }
+  
 }
 
 export default App;
