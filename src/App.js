03ca2f81e47bd9943c39e53cbca771f6f712659b
Mina Lee <mal0070@naver.com>
2024-10-21 10:58:58 +0900

    refactor: 에러 케이스 추가 및 에러 전달 형식 수정

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     //문자열 입력
-    Console.print("덧셈할 문자열을 입력해 주세요.");
-    const input = await Console.readLineAsync("");
-  
-    try{
-      //문자열 분리
-      //쉼표 또는 콜론
-      if((/,|:/).test(input)){
-        const result = input.split((/,|:/)).map(Number).reduce((a,b)=>a+b,0); //합산
-        this.printResult(result);
-      } 
+    Console.print('덧셈할 문자열을 입력해 주세요.');
+    const input = await Console.readLineAsync('');
 
-      //커스텀 구분자
-      else if((/\/\/(.*?)\\n/).test(input)){
-        //커스텀 구분자 추출
-        const matchStr = input.match(/\/\/(.*?)\\n/);
-        const customChar = matchStr[1];
+    //빈 문자열
+    if (!input) {
+      return Promise.reject(new Error('[ERROR] 문자열을 입력해주세요.'));
+    }
+    //쉼표 또는 콜론
+    else if (/,|:/.test(input)) {
+      const result = input
+        .split(/,|:/)
+        .map(Number)
+        .filter(n => !isNaN(n)) //input이 숫자가 아닌 경우 filtering
+        .reduce((a, b) => a + b, 0); //합산
+      this.printResult(result);
+    }
 
-        //분리할 문자열
-        const str = input.replace(/\/\/.*?\\n/,'');
-        const result = str.split(customChar).map(Number).reduce((a,b)=>a+b,0); //합산
-        this.printResult(result);
-      } else {
-        throw new Error();
-      }
-      
-    } catch (err) {
-      Console.print(err);
-      Console.print("[ERROR] 잘못된 값을 입력하셨습니다. 앱을 다시 실행해주세요.");
+    //커스텀 구분자
+    else if (/\/\/(.*?)\\n/.test(input)) {
+      //커스텀 구분자 추출
+      const matchStr = input.match(/\/\/(.*?)\\n/);
+      const customChar = matchStr[1];
+
+      //분리할 문자열
+      const str = input.replace(/\/\/.*?\\n/, '');
+      const result = str
+        .split(customChar)
+        .map(Number)
+        .filter(n => !isNaN(n))
+        .reduce((a, b) => a + b, 0); //합산
+      this.printResult(result);
+    }
+
+    //잘못된 값 입력
+    else {
+      return Promise.reject(
+        new Error('[ERROR] 지원되지 않는 문자열 포맷입니다.')
+      );
     }
   }
-
   //결과 출력 함수
   printResult(result) {
-    Console.print("결과 : "+result);
+    Console.print('결과 : ' + result);
   }
-  
 }
 
 export default App;
