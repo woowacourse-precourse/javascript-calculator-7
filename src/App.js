52d7a635d462117ddb4f461d616b30f8dff360a6
Yerim1234 <kongsun0805@gmail.com>
2024-10-21 14:27:17 +0900

    refactor: 기본 입력 처리 기능 변수 이름 변경

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const STRING_INPUT = await Console.readLineAsync(
+    const stringInput = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요."
     );
 
-    if (STRING_INPUT.trim() === "") {
+    if (stringInput.trim() === "") {
       Console.print("결과 : 0");
       return;
     }
 
-    Console.print("결과 : " + STRING_INPUT);
+    Console.print("결과 : " + stringInput);
   }
 }
 
