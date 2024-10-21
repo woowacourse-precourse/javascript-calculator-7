f456d06c5b418a8fc07ad29e7b19b48b51da7fbe
wiseah <hyunah2765@gmail.com>
2024-10-21 17:46:59 +0900

    feat(App.js): add support for custom and default delimiters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     Console.print("덧셈할 문자열을 입력해주세요.");
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.: ");
   }
 }
 
