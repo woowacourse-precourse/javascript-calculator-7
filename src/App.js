617d8d0bf7057beaa449b1b64432c3d1d3ea02fd
sieon <sieon.eve@gmail.com>
2024-10-21 23:17:25 +0900

    Feat: 커스텀 구분자 배열에 더하기

--- src/App.js
+++ src/App.js
@@ -20,6 +20,15 @@ class App {
 
     //구분자 배열 생성
     let delimiterArray = [',', ':'];
+    // 커스텀 구분자
+    const customDelimiter = input.match(/^\/\/(.)\\n/);
+    let numString = input;
+
+    if (customDelimiter) {
+      //커스텀 구분자 배열에 추가
+      delimiterArray.push(customDelimiter[1]);
+      numString = input.split('\\n')[1];
+    }
 
     const nums = input.split(new RegExp(`[${delimiterArray.join('')}]`)).map(Number);
 
