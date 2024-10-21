69b2066e74b9511c059df5d992fd008b88c0943f
choiihwa <chldlghk@gmail.com>
2024-10-21 16:21:48 +0900

    feat: 빈 값 입력 시 결과 출력 - 문자열의 숫자 총 합 로직 메소드 분리

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ class App {
   async run() {
     // 결과값 변수
     let result = 0;
-    // 구분자 설정 - 기본 구분자 쉼표(,), 콜론(:)
-    let seperators = ',:';
-
 
     // 사용자 입력값을 받는다.
     const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    if(input.replace(' ', '').length > 0) {
+      result = caculateNumberOfString(input);
+    }
+
+
+     // 결과 출력
+     MissionUtils.Console.print(`결과 : ${result}`);
+  }
+}  
+
+function caculateNumberOfString(str) {
+    // 구분자 설정 - 기본 구분자 쉼표(,), 콜론(:)
+    let seperators = ',:';
 
     // 커스텀 구분자 추출
-    const customSeperators = getCustomSeperators(input);
+    const customSeperators = getCustomSeperators(str);
 
-    let str = input;    
     if(customSeperators.length > 0) {
       seperators += customSeperators;
       // 커스텀 구분자 설정을 제외한 문자열 추출
-      str = input.substring(input.indexOf(END_CUSTOM_SEPERATOR_SETTING)+2);
+      str = str.substring(str.indexOf(END_CUSTOM_SEPERATOR_SETTING)+2);
     }
 
     // 숫자와 구분자를 제외한 다른 형식의 문자가 포함되어 있는지 확인
@@ -31,11 +40,7 @@ class App {
     const arr = getOnlyNumberArr(str, seperators);
 
     // 숫자배열의 숫자 총 합 계산
-    result = getSumAllNumber(arr);
-
-     // 결과 출력
-     MissionUtils.Console.print(`결과 : ${result}`);
-  }
+    return getSumAllNumber(arr);
 }
 
 function getCustomSeperators(input) {
