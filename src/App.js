be60c83c35d0fd5a898f968d0aebc999a51bdfe0
MINJOO-KIM <gkminju2000@gmail.com>
2024-10-21 21:55:13 +0900

    refactor: Airbnb 자바스크립트 스타일 가이드에 따라 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,84 +1,75 @@
-import { parse } from "@babel/core";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
     try {
       // 입력을 받는다.
-      const INPUT = await MissionUtils.Console.readLineAsync(
+      const input = await MissionUtils.Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n"
       );
-      if (INPUT.length == 0) {
+
+      if (input.length === 0) {
         MissionUtils.Console.print("결과 : 0");
         return;
       }
-      if (this.isValidInput(INPUT)) {
-        const RESULT_ARR = this.separateInput(INPUT);
-        const RESULT = this.add(RESULT_ARR);
-        MissionUtils.Console.print(`결과 : ${RESULT}`);
+
+      if (this.isValidInput(input)) {
+        const resultArray = this.separateInput(input);
+        const result = this.add(resultArray);
+        MissionUtils.Console.print(`결과 : ${result}`);
       } else {
-        throw Error("[ERROR] 유효한 입력값이 아닙니다.");
+        throw new Error("[ERROR] 유효한 입력값이 아닙니다.");
       }
     } catch (error) {
-      throw Error("[ERROR]");
+      throw new Error("[ERROR]");
     }
   }
 
   // 유효한 입력값인지 확인하는 함수
-  isValidInput(INPUT) {
-    if (INPUT.length == 0) {
+  isValidInput(input) {
+    if (input.length === 0) {
       MissionUtils.Console.print("결과 : 0");
-      return;
+      return false;
     }
     return true;
   }
 
-  separateInput(INPUT) {
+  separateInput(input) {
     // 커스텀 구분자로 시작하는 경우
-    if (INPUT.startsWith("/")) {
-      const [LETTER, NUMBER] = INPUT.split(/\\n/);
-
-      if (!LETTER.startsWith("//") || !NUMBER) {
-        throw Error("[ERROR]");
+    if (input.startsWith("/")) {
+      const [letter, number] = input.split(/\\n/);
+      if (!letter.startsWith("//") || !number) {
+        throw new Error("[ERROR]");
       }
 
-      const SEPARATOR = LETTER.substring(2);
-      const RESULT = NUMBER.split(SEPARATOR);
-
-      return RESULT.map((item) => {
-        const NUM = Number(item);
-        if (isNaN(NUM)) {
-          throw Error("[ERROR]");
-        }
-        if (NUM < 0) {
-          throw Error("[ERROR]");
-        }
-        return NUM;
-      });
-    } else if (!isNaN(Number(INPUT[0]))) {
-      const RESULT = INPUT.split(/[,;]/);
-
-      return RESULT.map((item) => {
-        const NUM = Number(item);
-        if (isNaN(NUM)) {
-          throw Error("[ERROR] 구분자를 제외하고는 숫자를 입력해주세요.");
-        }
-        if (NUM < 0) {
-          throw Error("[ERROR] 양수를 입력해주세요.");
-        }
-        return NUM;
-      });
-    } else {
-      throw Error("[ERROR]");
+      const separator = letter.substring(2);
+      const result = number.split(separator);
+      return result.map((item) => this.parseNumber(item));
     }
+
+    if (!isNaN(Number(input[0]))) {
+      const result = input.split(/[,;]/);
+      return result.map((item) => this.parseNumber(item));
+    }
+
+    throw new Error("[ERROR]");
   }
 
-  add(RESULT_ARR) {
-    let SUM = 0;
-    for (let i = 0; i < RESULT_ARR.length; i++) {
-      SUM += RESULT_ARR[i];
+  parseNumber(item) {
+    const num = Number(item);
+    if (isNaN(num)) {
+      throw new Error("[ERROR] 구분자를 제외하고는 숫자를 입력해주세요.");
     }
-    return SUM;
+
+    if (num < 0) {
+      throw new Error("[ERROR] 양수를 입력해주세요.");
+    }
+
+    return num;
+  }
+
+  add(resultArray) {
+    return resultArray.reduce((sum, num) => sum + num, 0);
   }
 }
 
