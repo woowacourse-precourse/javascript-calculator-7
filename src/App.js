80771467c960fd8fde749890ff66a65784307a12
김민주 <kminju2000@naver.com>
2024-10-21 14:51:30 +0900

    fix: 입력 문구 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     try {
       // 입력을 받는다.
       const input = await MissionUtils.Console.readLineAsync(
-        "덧셈할 문자열을 입력해 주세요."
+        "덧셈할 문자열을 입력해 주세요.\n"
       );
       if (this.isValidInput(input)) {
         this.plus();
