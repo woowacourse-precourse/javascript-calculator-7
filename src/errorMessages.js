46207b5d84a719ca2f158f12e11a97f691d8655f
anonymousrecords <a.Records2022@gmail.com>
2024-10-21 14:43:59 +0900

    chore(error): error message 작성

---
+++ src/errorMessages.js
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+  NO_VALID_DIVIDER: "[ERROR] 올바른 구분자가 존재하지 않습니다.",
+  CUSTOM_DIVIDER_NOT_USED: "[ERROR] 커스텀 구분자가 사용되지 않았습니다.",
+  NO_NUMBERS_INPUT: "[ERROR] 숫자가 입력되지 않았습니다.",
+  NEGATIVE_NUMBER: "[ERROR] 음수는 허용되지 않습니다.",
+  INVALID_INPUT: "[ERROR] 유효하지 않은 입력입니다.",
+};
