c1fb18932b665d2162263aeb0878e5ccfe59560f
kr-younghoon <sullanta0802@gmail.com>
2024-10-21 19:04:09 +0900

    refactor(calculator): 코드 구조 개선 및 함수 분리
    기존 코드의 구조를 개선하고, 함수로 분리하여 가독성과 유지보수성을 높였어요.
    - 상수와 에러 메시지를 별도로 정의하여 코드 재사용성을 높였어요.
    - `getInput` 함수: 사용자 입력을 처리하는 함수로 분리했어요.
    - `parseDelimiter` 함수: 커스텀 구분자를 추출하고 정규식으로 변환하는 로직을 분리했어요.
    - `parseInput` 함수: 입력 값을 파싱하고 구분자와 숫자 처리를 분리하여 입력 로직을 단순화했어요.
    - `validateData` 함수: 입력된 값을 숫자로 변환하고 유효성을 검사하는 로직을 분리했어요.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,86 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const DEFAULT_DELIMITER = /[,|:]/;
+const ERROR_NEGATIVE_NUMBER =
+    '[ERROR] 음수는 허용되지 않습니다.';
+const ERROR_INVALID_FORMAT =
+    '[ERROR] 입력 형식이 잘못되었습니다.';
+const ERROR_INVALID_INPUT = '[ERROR] 잘못된 입력입니다.';
+
+// 사용자로부터 입력(문자열)을 받음
+function getInput() {
+    return Console.readLineAsync(
+        '덧셈할 문자열을 입력해주세요.\n'
+    );
+}
+
+function parseDelimiter(input) {
+    const end = input.indexOf('\\n');
+    let delimiter = input.substring(2, end);
+
+    const splitter = [
+        ...delimiter.matchAll(/\[(.*?)\]/g),
+    ].map((del) => del[1]);
+
+    delimiter =
+        splitter.length > 0
+            ? new RegExp(
+                  splitter
+                      .map((d) =>
+                          d.replace(
+                              /[.*+?^${}()|[\]\\]/g,
+                              '\\$&'
+                          )
+                      )
+                      .join('|')
+              )
+            : delimiter;
+
+    return { delimiter, input: input.substring(end + 2) };
+}
+
+function parseInput(input) {
+    // 압력이 빈 문자열인 경우, 0을 반환
+    if (input.trim() === '') return [0];
+    // 입력값이 한 개일 경우 해당 숫자를 그대로 반환
+    if (!isNaN(input)) return [Number(input.trim())];
+
+    // 커스텀 구분자가 있는지 확인
+    let delimiter = DEFAULT_DELIMITER;
+    if (input.startsWith('//')) {
+        // 여러 개의 커스텀 구분자 추출
+        // return delimiter , remainingInput
+        try {
+            const {
+                delimiter: customDelimiter,
+                input: remainingInput,
+            } = parseDelimiter(input);
+            delimiter = customDelimiter;
+            input = remainingInput;
+        } catch (error) {
+            throw new Error(ERROR_INVALID_FORMAT);
+        }
+    }
+    // 문자열을 쉼표,콜론 혹은 커스텀 구분자로 분리하여 숫자 배열로 변환 숫자를 합산
+    const numbers = input
+        .split(delimiter)
+        .map(validateData)
+        .reduce((acc, curr) => acc + curr, 0);
+    return numbers;
+}
+
+function validateData(input) {
+    const num = Number(input);
+    if (isNaN(num)) throw new Error(ERROR_INVALID_INPUT);
+    if (num < 0) throw new Error(ERROR_NEGATIVE_NUMBER);
+    return num;
+}
+
 class App {
     async run() {
         try {
-            // 사용자로부터 입력(문자열)을 받음
-            let input = await Console.readLineAsync(
-                '덧셈할 문자열을 입력해주세요.\n'
-            );
-            let delimiter = /[,|:]/;
-
-            // 압력이 빈 문자열인 경우, 0을 반환
-            if (input.trim() === '') {
-                return Console.print('0');
-            }
-
-            // 입력값이 한 개일 경우 해당 숫자를 그대로 반환
-            if (!isNaN(input)) {
-                return Console.print(input.trim());
-            }
-
-            // 커스텀 구분자가 있는지 확인
-            if (input.startsWith('//')) {
-                const end = input.indexOf('\\n');
-                delimiter = input.substring(2, end);
-
-                // 여러 개의 커스텀 구분자 추출
-                const splitter = [
-                    ...delimiter.matchAll(/\[(.*?)\]/g),
-                ].map((del) => del[1]);
-
-                if (splitter.length > 0) {
-                    delimiter = new RegExp(
-                        splitter
-                            .map((d) =>
-                                d.replace(
-                                    /[.*+?^${}()|[\]\\]/g,
-                                    '\\$&'
-                                )
-                            )
-                            .join('|')
-                    );
-                }
-
-                input = input.substring(end + 2);
-            }
-
-            // 문자열을 쉼표,콜론 혹은 커스텀 구분자로 분리하여 숫자 배열로 변환 숫자를 합산
-            const result = input
-                .split(delimiter)
-                .map((str) => {
-                    const num = Number(str);
-                    if (isNaN(num)) {
-                        throw new Error(
-                            '[ERROR] 유효하지 않은 입력입니다.'
-                        );
-                    }
-                    if (num < 0) {
-                        throw new Error(
-                            '[ERROR] 음수는 허용되지 않습니다.'
-                        );
-                    }
-                    return num;
-                })
-                .reduce((acc, curr) => acc + curr, 0);
-
+            const input = await getInput();
+            const result = parseInput(input);
             // 결과 출력
             Console.print(`결과 : ${result}`);
         } catch (error) {
