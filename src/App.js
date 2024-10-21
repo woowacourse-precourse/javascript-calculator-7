c41e82cbc0153bf59d8fb179d8c22660d6adc48f
rkdcodus <rcd7275@naver.com>
2024-10-21 17:07:34 +0900

    feat: Occur an incorrect input error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const REGEX_NORMAL = /^[0-9,:]+$/;
+const REGEX_CUSTOM = /^\/\/.\\n/;
+
 class App {
+  constructor() {
+    this.str = '';
+    this.separator = '';
+  }
+
+  static handleInput(input) {
+    if (!input.match(REGEX_NORMAL)) {
+      this.str = input;
+      this.separator = /,|:/;
+      return true;
+    }
+    if (input.match(REGEX_CUSTOM)) {
+      const separator = input[2];
+      const str = input.split('\\n')[1];
+      const regex = new RegExp(`^[0-9${separator}]+$`);
+
+      if (str.match(regex)) {
+        this.str = str;
+        this.separator = separator;
+        return true;
+      }
+    }
+    return false;
+  }
+
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요\n');
+    if (!this.handleInput(input)) return new Error('[ERROR] 올바른 형식을 입력해주세요.');
   }
 }
 
