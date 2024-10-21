89ea3ececd23d90ff215d423800888d543fe5409
mywhzy <cmj2393a@naver.com>
2024-10-21 18:13:53 +0900

    feat: 커스텀 구분자 지정 위치 관련 에러 메세지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -6,4 +6,5 @@ export const ERROR_MESSAGE = {
   positiveNum: '[ERROR] 문자열 덧셈은 1이상의 양수부터 가능합니다.',
   invalidFormat:
     '[ERROR] 유효하지 않은 형식입니다. 숫자와 기본 구분자(, 또는 :) 및 지정한 커스텀 구분자(`//지정문자\\n`)만 사용 가능합니다.',
+  customPosition: '[ERROR] 커스텀 구분자 지정은 입력 앞부분에서만 가능합니다.',
 };
