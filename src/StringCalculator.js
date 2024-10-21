6e0d7a7b9b47b19ce18deb929007ea10ac5602e6
junwoo <junwooha1@gmail.com>
2024-10-21 16:54:37 +0900

    feat(StringCalculator.js): 쉼표와 콜론 구분자를 사용한 기본 덧셈 기능 추가

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,6 +4,12 @@ class StringCalculator {
       if (!numbers) {
         return 0;
       }
+      // 2. 쉼표와 콜론 구분자를 사용한 기본 덧셈 기능 추가
+      const delimiters = /[,:]/; // 쉼표와 콜론을 구분자로 사용
+      const splitNumbers = numbers.split(delimiters);
+    
+      // 문자열을 숫자로 변환하고 합계 계산
+      return splitNumbers.reduce((sum, num) => sum + parseInt(num, 10), 0);
     }
   }
   
