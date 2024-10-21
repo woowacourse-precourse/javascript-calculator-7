474d42ad1308908e775f506ff3b31fe845892b90
zelkovaria <hyein396@gmail.com>
2024-10-21 14:31:19 +0900

    fix: checkInvalidDelimiter 수정

--- src/Validation.js
+++ src/Validation.js
@@ -20,7 +20,7 @@ export default class Validation {
   static checkInvalidDelimiter(userInput) {
     const invalidDelimiter = userInput
       .split(/[,:]/)
-      .some((num) => /[^0-9\s,:]/.test(num));
+      .some((num) => /[^0-9]/.test(num.trim()));
 
     if (invalidDelimiter) {
       throw new Error(ERROR_MESSAGE.INVALID_DELIMITER);
