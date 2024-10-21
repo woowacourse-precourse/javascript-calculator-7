0f8271a61ae589ceadefb99e6adf5e1db3db12df
sieon <sieon.eve@gmail.com>
2024-10-21 20:28:52 +0900

    ✨ Feat: 값 입력 및 출력
    값을 입력하고 출력되는지 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { log } from 'console';
+import readline from 'readline';
+
 class App {
-  async run() { }
+  async run() {
+    const inandout_value = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    inandout_value.question('값을 입력하세요: ', (userInput) => {
+      console.log('결과:', userInput);
+    });
+  }
 }
 
 export default App;
