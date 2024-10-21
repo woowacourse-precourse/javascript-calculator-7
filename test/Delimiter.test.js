32218cde1ff7fa433d6165b7b160820f10ce967e
L2HYUNN <donghyuni98@gmail.com>
2024-10-21 09:40:34 +0900

    test(delimiter): hasCustomDelimiter 테스트 추가

--- test/Delimiter.test.js
+++ test/Delimiter.test.js
@@ -8,6 +8,24 @@ describe('Delimiter', () => {
     delimiter = new Delimiter();
   });
 
+  describe('hasCustomDelimiter', () => {
+    it('커스텀 구분자를 가진 경우 true를 반환한다', () => {
+      const value = '//;\\n1;2;3';
+
+      const result = delimiter.hasCustomDelimiter(value);
+
+      expect(result).toBe(true);
+    });
+
+    it('커스텀 구분자가 없는 경우 false를 반환한다', () => {
+      const value = '1;2;3';
+
+      const result = delimiter.hasCustomDelimiter(value);
+
+      expect(result).toBe(false);
+    });
+  });
+
   describe('splitByDelimiters', () => {
     describe('커스텀 구분자가 없는 경우', () => {
       it('기본 구분자를 사용하여 입력한 문자열을 구분한 값을 반환한다', () => {
