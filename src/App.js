4de7160c39c4563b9c3669f96ea261196d834e6c
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-10-21 15:14:39 +0900

    feat: 사용자 입력 처리 및 결과 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
+import Calculator from "./calculator.js";
+
 class App {
-  async run() {}
+  async run() {
+    const USER_INPUT = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해주세요."
+    );
+
+    try {
+      const FORMATVALUE = USER_INPUT.replace(/\\n/g, "\n");
+      const CALCULATOR = new Calculator();
+      const RESULT = CALCULATOR.add(FORMATVALUE);
+
+      Console.print(`결과 : ${RESULT}`);
+    } catch (error) {
+      Console.print(error.message);
+
+      throw error;
+    }
+  }
 }
 
 export default App;
