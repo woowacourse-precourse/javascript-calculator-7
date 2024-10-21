1f55ce78243c0357da52f8250d023bfc446a4c3b
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-21 22:49:17 +0900

    feat(Calculate): 기본 구분자에 대해서 덧셈하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -21,7 +21,19 @@ class App {
         });
     }
 
+    calculate(input) {
+        if (!input) return 0; // 빈 문자열일 경우 0 반환
 
+        let delimiter = /,|:/; // 기본 구분자 (쉼표, 콜론)
+
+        // 모든 구분자로 숫자 분리
+        const numbers = input.split(delimiter).map((num) => {
+            const parsedNum = parseInt(num, 10);
+            return parsedNum;
+        });
+
+        return numbers.reduce((sum, current) => sum + current, 0); // 숫자의 합 계산
+    }
 }
 
 const app = new App();
