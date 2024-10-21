c5c56477ba5f1881140a7df269dd1e9f1ba5b0af
anonymousrecords <a.Records2022@gmail.com>
2024-10-21 14:45:04 +0900

    feat(calculator): 기본 구조 및 빈 문자열 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+    const answer = Calculator.calculate(input);
+    Console.print(`결과 : ${answer}`);
+  }
+}
+
+class Calculator {
+  static calculate(input) {
+    if (input === "") {
+      return 0;
+    }
+  }
 }
 
 export default App;
