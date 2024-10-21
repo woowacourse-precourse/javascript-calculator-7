f15127421321ff0b944292e33722424dff6bbfe9
egg-silver <v6984@naver.com>
2024-10-21 22:25:21 +0900

    fix: 에러 메시지 수정

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,9 @@
 const CALCULATION_MESSAGE = Object.freeze({
   START_CALCULATION: '덧셈할 문자열을 입력해 주세요.',
-  END_CALCULATION: '계산 종료',
 });
 
 const ERROR_MESSAGE = Object.freeze({
-  NEGATIVE_NUMBER: '[ERROR]',
+  NEGATIVE_NUMBER: '[ERROR] 음수가 입력되었습니다',
   INVALID_INPUT: '[ERROR] 잘못된 입력입니다.',
 });
 
