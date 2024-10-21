1f7a694cd9198a9f56d3b74cab5e7b4071e41770
magik99 <hojun0810@naver.com>
2024-10-21 18:09:44 +0900

    fix(extractNum.js): 특수문자 오류 처리
    정규 표현식에서 특수문자 처리로 인한 오류 수정

--- src/extractNum.js
+++ src/extractNum.js
@@ -2,15 +2,16 @@ const extractNum = (input) => {
   let delimiter = /[,:]/;
 
   if (input.startsWith('//')) {
-    const delimiterEnd = input.indexOf('\n');
-    console.log(delimiterEnd);
+    const delimiterEnd = input.indexOf('\\n');
     if (delimiterEnd !== -1) {
-      const customDelimiter = input.slice(2, delimiterEnd);
+      let customDelimiter = input.slice(2, delimiterEnd);
+      // 특수문자 정규표현식 오류 처리
+      customDelimiter = customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
       delimiter = new RegExp(`[${customDelimiter}]`);
 
-      input = input.slice(delimiterEnd + 1);
+      input = input.slice(delimiterEnd + 2);
     } else {
-      throw new Error('[ERROR] 커스텀 구분자가 마무리 되지 않았습니다!!!');
+      throw new Error('[ERROR]');
     }
   }
 
@@ -20,10 +21,8 @@ const extractNum = (input) => {
   const numbers = splitInput
     .map((num) => num.trim())
     .map((num) => {
-      if (isNaN(num) || num === '') {
-        throw new Error(
-          '[ERROR] 입력 값 형식이 올바르지 않습니다!!!(숫자 추출 오류)',
-        );
+      if (isNaN(num) || num === '' || Number(num) <= 0) {
+        throw new Error('[ERROR]');
       }
       return Number(num);
     });
