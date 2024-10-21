077e656738c80bab57db5e093c666d7ba7351b68
rheefine <rheefine@gmail.com>
2024-10-21 16:53:05 +0900

    feat: split 메서드 구현

---
+++ src/application/parser/NumberSplitter.js
@@ -0,0 +1,20 @@
+export default class NumberSplitter {
+  split(delimiters, str) {
+    const result = str.split('').reduce((acc, char) => {
+      if (delimiters.includes(char)) {
+        if (acc.currentNumber) {
+          acc.numbers.push(parseInt(acc.currentNumber, 10));
+          acc.currentNumber = '';
+        }
+      } else {
+        acc.currentNumber += char;
+      }
+      return acc;
+    }, { numbers: [], currentNumber: '' });
+    
+    if (result.currentNumber) {
+      result.numbers.push(parseInt(result.currentNumber, 10));
+    }
+    return result.numbers;
+  }
+}
