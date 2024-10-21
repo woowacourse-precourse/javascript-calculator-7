72ef1a5454ef2f758610b831ede22b1615bcb2ff
Yongho Kim <kyhcraft@gmail.com>
2024-10-21 22:36:04 +0900

    feat: 빈 문자열 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+
+  exceptionHandler(condition, errorMessage) {
+    if (condition) {
+      throw new Error(errorMessage);
+    }
+  }
+
+  async run() {
+    Console.print("덧셈할 문자열을 입력해 주세요.");
+    const inputString = await Console.readLineAsync("");
+
+    this.exceptionHandler(inputString === "", "[ERROR]");
+
+  }
 }
 
 export default App;
