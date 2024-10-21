ca0762369e701b4ebf70d469a64fe1870f597d21
AnCha <tory912@gmail.com>
2024-10-21 20:56:26 +0900

    feat: 커스텀 구분자 문자열 분리 구현
    `//`와 `\n` 사이에 커스텀 구분자가 있는 경우 커스텀 구분자로 문자열 분리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ class App {
     const userInput = await Console.readLineAsync(
       "덧셈할 문자열을 입력하여 주세요."
     );
+
+    if (userInput.startsWith("//") && userInput.indexOf("\\n") !== -1) {
+      const NUMBERS = userInput.substr(5).split(userInput[2]);
+    }
   }
 }
 
