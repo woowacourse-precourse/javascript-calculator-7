c6caf2e6467c0e8da2f0ffd6923f98e0d1410b04
yeji0214 <1225yj50@naver.com>
2024-10-21 15:12:02 +0900

    fix: remove unnecessary hasRun flag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import { runCalculator } from './Calculator.js';
 
-// App의 실행 여부를 관리하기 위한 변수
-let hasRun = false;
-
 class App {
   async run() {
-    // 이미 실행된 경우, 중복 실행 방지
-    if (hasRun) return;
-    hasRun = true;
-
     // Calculator 실행
     try {
       await runCalculator();
