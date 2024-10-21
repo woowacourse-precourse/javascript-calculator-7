5beb64813be9daf6560c7fe3223618ed1de3f85a
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-21 20:59:03 +0900

    feat: 문자열 종류에 따른 분기처리

--- src/App.js
+++ src/App.js
@@ -11,8 +11,25 @@ class App {
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
     const strArr = input.trim().split('');
-    console.log(strArr);
+    
+    while (this.cursor < strArr.length) {
+      const char = strArr[this.cursor];
+
+      if (char === '/') { // "/"로 시작하는 경우
+        this.handleCustomSeparator();
+      } else if (isNaN(Number(char))) { // 숫자가 아닌 경우
+        this.handleNotNumber();
+      } else { // 숫자인 경우
+        this.handleNumber();
+      }
+    }
   }
+
+  handleCustomSeparator() {}
+
+  handleNotNumber() {}
+
+  handleNumber() {}
 }
 
 export default App;
