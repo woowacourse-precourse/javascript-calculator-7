a2447de8cd17bea0252aed3f69d60080d142a5ac
지태 <wlxo402@naver.com>
2024-10-21 19:50:45 +0900

    feat:커스텀구분자 명시되는 부분 제거하기

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ class App {
     );
 
     // 구분자 저장하는 배열 만들기
-    const separators = [".", ";"];
+    const separators = [".", ":"];
 
     // 커스텀 구분자 저장 변수 : customSeparator
     const customSeparator = input.match(/^\/\/(.)\\n/);
 
+    let cleanedInput = input;
+
     // 만약 customSeparator가 존재한다면 해당 구분자 배열에 저장하기
     if (customSeparator) {
       separators.push(customSeparator[1]);
+
+      // 입력 문자열에서 커스텀 구분자 부분 제거
+      cleanedInput = input.replace(/^\/\/(.)\\n/, "");
     }
 
     // 테스트용
-    // Console.print(separators);
+    Console.print(separators);
+    Console.print(cleanedInput);
   }
 }
 
