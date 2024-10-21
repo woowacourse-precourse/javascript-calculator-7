f00cd80e0bb552b11fac600598dc21adfb24e314
junwoo <junwooha1@gmail.com>
2024-10-21 17:05:11 +0900

    feat(StringCalculator.js): 커스텀 구분자를 지원하는 기능 추가

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -5,9 +5,27 @@ class StringCalculator {
         return 0;
       }
       // 2. 쉼표와 콜론 구분자를 사용한 기본 덧셈 기능 추가
-      const delimiters = /[,:]/; // 쉼표와 콜론을 구분자로 사용
+      let delimiters = /[,:]/; // 쉼표와 콜론을 구분자로 사용
+
+      numbers = numbers.replace(/\\n/, "\n");
+
+      if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf("\n");
+        if (delimiterEndIndex === -1) {
+            throw new Error("[ERROR] 잘못된 입력입니다.");
+        }
+
+        const customDelimiter = numbers.substring(2, delimiterEndIndex);
+        if(customDelimiter.length === 0) {
+            throw new Error("[ERROR] 잘못된 구분자입니다.");
+        }
+
+        delimiters = new RegExp(`[${customDelimiter}]`);
+        numbers = numbers.substring(delimiterEndIndex + 1);
+      }
+
       const splitNumbers = numbers.split(delimiters);
-    
+
       // 문자열을 숫자로 변환하고 합계 계산
       return splitNumbers.reduce((sum, num) => sum + parseInt(num, 10), 0);
     }
