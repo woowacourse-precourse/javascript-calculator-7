598ec447e42a367343f642f9928d3725d6f53cdf
dudcns02 <duddl2460@naver.com>
2024-10-21 17:09:12 +0900

    chore: Deleting Unnecessary Annotations

--- src/AdditionCalculator.js
+++ src/AdditionCalculator.js
@@ -1,9 +1,4 @@
 import { getInput } from "./UserView.js";
-// import extractedNumbers from "./StringProcesing.js";
-// import { Console } from "@woowacourse/mission-utils";
-// import { printOutput } from "./UserView.js";
-// import { MESSAGE } from "./content.js";
-// import { numberSum } from "./StringProcesing.js";
 
 class AdditionCalculator {
   async start() {
