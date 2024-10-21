3290397686059499af5648d90286435825d453c6
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-21 22:46:32 +0900

    feat(Input): 문자열을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import readline from 'readline';
+
 class App {
-  async run() {}
+    constructor() {
+        this.RL = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+    }
+
+    async run() {
+        this.RL.question('덧셈할 문자열을 입력해주세요: ', (line) => {
+            try {
+                const result = this.calculate(line);
+                console.log(`결과: ${result}`);
+            } catch (error) {
+                console.error(error.message);
+            } finally {
+                this.RL.close();
+            }
+        });
+    }
+
+
 }
 
-export default App;
+const app = new App();
+app.run();
