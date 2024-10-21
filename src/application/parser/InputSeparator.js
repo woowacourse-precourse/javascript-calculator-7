c82eeb23cce607fc391697af48b3e06e8498df3d
rheefine <rheefine@gmail.com>
2024-10-21 14:10:13 +0900

    feat: getCustomDelimiter 메서드 구현

--- src/application/parser/InputSeparator.js
+++ src/application/parser/InputSeparator.js
@@ -1,5 +1,8 @@
 export default class InputSeparator {
+  #delimiter;
+
   constructor() {
+    this.#delimiter = null;
   }
 
   hasCustomDelimiter(rawInput) {
@@ -7,5 +10,12 @@ export default class InputSeparator {
       return true;
     }
     return false;
+  }
+
+  getCustomDelimiter(rawInput) {
+    if (this.hasCustomDelimiter(rawInput)) {
+      this.#delimiter = rawInput[2];
+    }
+    return this.#delimiter;
   }
 }
