45a4ddce8d2ce54c19c75f55236af4d9e80f12aa
Daeuning <happyjde3@gmail.com>
2024-10-21 12:39:02 +0900

    refactor(validators): 상수화된 에러메세지로 리팩토링

--- src/validators.js
+++ src/validators.js
@@ -1,29 +1,23 @@
 import { throwError } from './utils.js';
+import { ERROR_MESSAGE } from './constants.js'; 
 
 const validateEmptyInput = (input) => {
   if (input.trim() === '') {
-    throwError("입력된 값이 없어요. 다시 한 번 덧셈할 숫자를 입력해 주세요!");
+    throwError(ERROR_MESSAGE.EMPTY_INPUT);
   }
 };
 
 const validateNegativeNumbers = (numbersArray) => {
   const hasNegative = numbersArray.some((number) => Number(number) < 0);
-  
   if (hasNegative) {
-    throwError("양수만 입력해 주세요.");
+    throwError(ERROR_MESSAGE.NEGATIVE_INPUT);
   }
 };
 
 const validateInvalidNumbers = (numbersArray) => {
   const hasInvalid = numbersArray.some((number) => isNaN(Number(number)) || number.trim() === '');
-  
   if (hasInvalid) {
-    throwError(
-      `입력된 값에 지원하지 않는 문자가 있어요.
-      기본 구분자: ':' 또는 ','
-      커스텀 구분자 설정: //(구분자)\\n
-      위 정보를 참고하여 다시 입력해 주세요!`
-    );
+    throwError(ERROR_MESSAGE.INVALID_INPUT);
   }
 };
 
