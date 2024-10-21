688e04298edc54952c948f9dba2b9bc532913a21
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-10-21 20:27:51 +0900

    feat: 기본 구분자 외에 커스텀 구분자를 지정하여 숫자 합산 기능 구현
    - //와 \n 사이의 커스텀 구분자를 기준으로 숫자들을 나누어 합산

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ class App {
     let inputString = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+    let numberArray;
 
-    if (
+    // 커스텀구분자 확인
+    if (inputString.startsWith("//")) {
+      // 커스텀구분자 찾기
+      const lines = inputString.split("\\n");
+      // 숫자가 뒤에 붙었는지 확인
+      if (lines.length < 2 || !lines[1] || lines[1].trim() === "") {
+        throw new Error(
+          "[ERROR] 커스텀구분자 뒤 숫자가 없습니다. 프로그램을 종료합니다."
+        );
+      }
+      const delimiter = inputString.charAt(2);
+      numberArray = lines[1].split(delimiter);
+    }
+
+    // 기본구분자 확인
+    else if (
       !inputString ||
       inputString.trim() === "" ||
       inputString.split(/[,:]/).length === 1
@@ -13,22 +29,22 @@ class App {
       throw new Error(
         "[ERROR] 입력값이 없거나 구분자가 없습니다. 프로그램을 종료합니다."
       );
-    }
+    } else numberArray = inputString.split(/[,:]/);
 
-    inputString = inputString.split(/[,:]/);
-
-    for (let value of inputString) {
+    for (let value of numberArray) {
       value = Number(value);
       if (Number.isNaN(value)) {
         throw new Error(
-          "[ERROR] 입력값이 숫자가 아닙니다. 프로그램을 종료합니다."
+          "[ERROR] 구분자로 나뉘어진 값이 숫자로만 이루어져 있지 않습니다. 프로그램을 종료합니다."
         );
       } else if (value < 0) {
-        throw new Error("[ERROR] 입력값이 음수입니다. 프로그램을 종료합니다.");
+        throw new Error(
+          "[ERROR] 구분자로 나뉘어진 값에 음수가 있습니다. 프로그램을 종료합니다."
+        );
       }
     }
 
-    const sum = inputString.reduce(
+    const sum = numberArray.reduce(
       (accumulator, currentValue) => accumulator + Number(currentValue),
       0
     );
