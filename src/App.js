fb8c0d9b6890c2773edee77d95161c1b82f94e95
wine-dev <markwon56@naver.com>
2024-10-21 20:19:50 +0900

    feat: 기본 구분자를 이용한 계산 기능 구현
    - `,` 또는 `:` 구분자로 분리된 숫자들의 합 계산 기능 추가
    - 빈 문자열 입력 시 0 반환 처리
    - 예시: "1,2" => 3, "1,2:3" => 6 구현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const INPUT_PROMPT = await Console.readLineAsync(
-      '덧셈할 문자열을 입력해 주세요: '
-    );
+    const PROMPT_MESSAGE = '덧셈할 문자열을 입력해 주세요: ';
+    const userInput = await Console.readLineAsync(PROMPT_MESSAGE);
+    const result = this.calculateSum(userInput);
 
-    Console.print(INPUT_PROMPT);
+    Console.print(result);
+  }
+
+  // 입력 받은 문자열 안의 숫자 합계 계산
+  // (input: string, output: number)
+  calculateSum(input) {
+    if (input === '') {
+      return 0;
+    }
+
+    const numbers = this.extractNumbers(input);
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
+
+  // 기본 구분자 쉼표(,)와 콜론(:)를 기준으로 입력받은 문자열 분리 후 숫자로 변환
+  // (input: string, output: number[])
+  extractNumbers(input) {
+    return input
+      .split(/[,:]/)
+      .map(Number)
+      .filter((num) => !isNaN(num));
   }
 }
 
