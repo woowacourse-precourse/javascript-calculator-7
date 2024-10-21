417ebf1994437f3661b56c2d16d4942d84d1bd9e
sanghyeonLee <hyeon012366@gmail.com>
2024-10-21 18:12:08 +0900

    refactor : 에러 처리 관련 메소드 모듈화

---
+++ src/utils/stringProcessor.js
@@ -0,0 +1,23 @@
+export function isCustom(param) {
+    const lastIdx = param.indexOf("\\n");
+    return param.startsWith("//") && lastIdx !== -1;
+}
+
+export function sliceString(param) {
+    const lastIdx = param.indexOf("\\n");
+    const customSeparator = param.slice(2, lastIdx);
+    return {
+        customSeparator,
+        slicedString: param.slice(lastIdx + 2)
+    };
+}
+
+export function preprocessing(param, customSeparator) {
+    const defaultSeparator = /[:,]/g;
+    const argument = customSeparator
+        ? new RegExp(`[${customSeparator}:,]`, 'g')
+        : defaultSeparator;
+
+    const splitArray = param.split(argument);
+    return splitArray.map(elem => Number(elem));
+}
