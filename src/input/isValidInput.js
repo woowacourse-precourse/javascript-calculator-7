2e525a3317761504c66810d1ac53c4b0cfa7db99
Junseo Kim <semiwest2@gmail.com>
2024-10-21 21:13:49 +0900

    feat: 기본 구분자 상수 배열 `DEFAULT_DELIMITERS`로 정의
    - 기본 구분자로 사용할 `,`와 `;`를 포함한 상수 배열 `DEFAULT_DELIMITERS` 추가
    
    - `isValidInput` 함수에서 기본 구분자 배열 `DEFAULT_DELIMITERS`를 활용하여 입력 유효성 검사 로직 개선

--- src/input/isValidInput.js
+++ src/input/isValidInput.js
@@ -1,11 +1,15 @@
+import { DEFAULT_DELIMITERS } from '../constant/constant.js';
+
 export default function isValidInput(cleanedString, customDelimiter) {
   if (cleanedString.trim() === "") {
-    return true;
+    return;
   }
+
+  const allDelimiters = [...DEFAULT_DELIMITERS, customDelimiter].filter(Boolean);
 
   let regex;
   if (customDelimiter !== null) {
-    regex = new RegExp(`^[0-9,;${customDelimiter}]+$`);
+    regex = new RegExp(`^[0-9,;${allDelimiters.join("")}]+$`);
   } else {
     regex = /^[\\d,;]+$/;
   }
