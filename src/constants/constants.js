edea66cf622639c163acb17e353cb3c438b660df
luminox93 <memoji1236@gmail.com>
2024-10-21 14:52:45 +0900

    fix: constants 수정
    1. 스펠링 수정
    2. PATTERN 수정

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGES = {
+	INVALID_TYPE: '잘못된 타입입니다.',
+	INVALID_INPUT: '잘못된 입력입니다.',
+	NEGATIVE_NUMBER: '음수는 입력할 수 없습니다.',
+	INVALID_NUMBER: '양의 정수가 아닙니다.',
+	INVALID_SEPARATER: '잘못된 구분자입니다.',
+};
+
+export const PATTERN = /^\/\/([^\n]+)\n/gm;
