e585bec8d6cc77d02df386f469fb2aaa7f470710
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-21 14:10:26 +0900

    style(App.js) 변수명 카멜케이스로 변경
    상수가 아닌 변수는 카멜케이스로 변경 ex) DIVIDER_EXP -> dividerExp

--- src/App.js
+++ src/App.js
@@ -2,39 +2,35 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
     async run() {
-        const STR = await Console.readLineAsync(
+        const str = await Console.readLineAsync(
             "덧셈할 문자열을 입력해 주세요.\n"
         );
 
-        let DIVIDER = /[,|:]/;
-        let STR_NUMBERS = STR;
+        let dividerExp = /[,|:]/;
+        let strNumbers = str;
 
-        if (STR === "") {
+        if (str === "") {
             await Console.print(`결과: 0`);
-            return 0;
         }
 
-        if (STR.startsWith("//") && STR.indexOf("\n") !== -1) {
-            const DIVIDER = STR.slice(2, STR.indexOf("\n"));
-            DIVIDER_EXP = new RegExp(`[${DIVIDER}]`);
-            STR_NUMBERS = STR.slice(STR.indexOf("\n") + 1);
+        if (str.startsWith("//") && str.indexOf("\n") !== -1) {
+            const divider = str.slice(2, str.indexOf("\n"));
+            dividerExp = new RegExp(`[${divider}]`);
+            strNumbers = str.slice(str.indexOf("\n") + 1);
         }
 
-        const NUMBERS = STR_NUMBERS.split(DIVIDER_EXP).map((NUM) => {
+        const numbers = strNumbers.split(dividerExp).map((NUM) => {
             const PARSE_NUM = Number(NUM);
             if (isNaN(PARSE_NUM) || PARSE_NUM < 0) {
                 throw new Error(
                     `[ERROR] 구분자 외의 잘못된 값이 포함되었습니다. \n 잘못된 값:  ${PARSE_NUM}`
                 );
             }
-            return PARSE_NUM;
         });
 
-        const SUM = NUMBERS.reduce((SUM, NUM) => SUM + NUM, 0);
-
+        const SUM = numbers.reduce((SUM, NUM) => SUM + NUM, 0);
+        console.log(SUM);
         await Console.print(`결과: ${SUM}`);
-
-        return SUM;
     }
 }
 
