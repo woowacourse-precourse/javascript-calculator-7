9d7ccb5d41b9611c01634927b23c4863dd742152
ncb6206 <ncb6206@gmail.com>
2024-10-21 15:53:28 +0900

    feat : 문자열 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  getSentence() {
+    const sentence = MissionUtils.Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    return sentence;
+  }
+
+  async run() {
+    try {
+      const sentence = await this.getSentence();
+    } catch (error) {
+      throw new Error("[ERROR]");
+    }
+  }
 }
 
 export default App;
