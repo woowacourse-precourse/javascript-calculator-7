5e4d38566d94854b2fc045932a3b9ce99e389bef
nerdchanii <nerdchanii@gmail.com>
2024-10-21 16:01:42 +0900

    test(parser): add input parser test

--- __tests__/InputParserTest.js
+++ __tests__/InputParserTest.js
@@ -54,22 +54,22 @@ describe('InputParser 테스트', () => {
     });
 })
 
-// describe('InputParser 예외 테스트', ()=>{
-//     test("커스텀 구분자에 아무것도 들어오지 않은 경우", () => {
-//       const userInput = "//\\n1:2,3";
-//       const parser = new InputParser();
-//       expect(parser.parse(userInput)).reject.toThrow("[ERROR]");
-//     });
+describe('InputParser 예외 테스트', ()=>{
+    test("커스텀 구분자에 아무것도 들어오지 않은 경우", () => {
+      const userInput = "//\\n1:2,3";
+      const parser = new InputParser();
+      expect(()=> parser.parse(userInput)).toThrow("[ERROR]");
+    });
     
-//     test("커스텀 구분자가 아닌 문자가 식에 들어온 경우", () => {
-//       const userInput = "//a\\n1:2h3";
-//       const parser = new InputParser();
-//       expect(parser.parse(userInput)).reject.toThrow("[ERROR]");
-//     });
+    test("커스텀 구분자가 아닌 문자가 식에 들어온 경우", () => {
+      const userInput = "//a\\n1:2h3";
+      const parser = new InputParser();
+      expect(()=> parser.parse(userInput)).toThrow("[ERROR]");
+    });
 
-//     test("식에 들어온 수가 음수인 경우", ()=>{
-//       const userInput = "//n\\n1:2:-3";
-//       const parser = new InputParser();
-//       expect(parser.parse(userInput)).reject.toThrow("[ERROR]");
-//     });
-// })
+    test("식에 들어온 수가 음수인 경우", ()=>{
+      const userInput = "//n\\n1:2:-3";
+      const parser = new InputParser();
+      expect(()=> parser.parse(userInput)).toThrow("[ERROR]");
+    });
+})
