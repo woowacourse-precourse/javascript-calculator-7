323a9ec2c649eaf1d163bd86c853667a022f8aaa
yoojin <everywavyday@gmail.com>
2024-10-21 20:25:02 +0900

    feat: 입력된 문자열에서 기본 구분자와 커스텀 구분자를 제외한 숫자를 분리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+      const numbers = this.extractNumbers(input);
+  }
+
+  // 문자열에서 숫자를 추출하는 함수
+  extractNumbers(input) {
+    let delimiter = /,|:/;
+    let inputText = input;
+
+    if (input.startsWith('//')) {
+      const endIndex = input.indexOf('\n');
+      delimiter = new RegExp(input.substring(2, endIndex));
+      inputText = input.substring(endIndex + 1);
+    }
+    
+    const splitText = inputText.split(delimiter);
+    const numbers = splitText.map(Number);
+    return numbers;
   }
 }
 
