3791caea778211548a1de458677d34b67872e377
mywhzy <cmj2393a@naver.com>
2024-10-21 17:31:57 +0900

    feat: 유효하지 않은 형식일 때 에러 메세지 추가

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,6 @@ export const CUSTOM_SEPARATOR_REGEX = /(?<=\/\/).+(?=\\n)/;
 
 export const ERROR_MESSAGE = {
   positiveNum: '[ERROR] 문자열 덧셈은 1이상의 양수부터 가능합니다.',
+  invalidFormat:
+    '[ERROR] 유효하지 않은 형식입니다. 숫자와 기본 구분자(, 또는 :) 및 지정한 커스텀 구분자(`//지정문자\\n`)만 사용 가능합니다.',
 };
