894b93c9828886331579e5083f96b91fe9c887cc
Luna77877 <lsy77877korea@gmail.com>
2024-10-21 21:54:14 +0900

    fix(error): Fix Error Handling Scope

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    getString();
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
 
-    async function getString() {
-      try {
-        const userInput = await MissionUtils.Console.readLineAsync(
-          "덧셈할 문자열을 입력해 주세요.\n"
-        );
+      const result = extractSeperator(userInput);
 
-        const result = extractSeperator(userInput);
-
-        MissionUtils.Console.print("결과 : " + result);
-      } catch (error) {
-        throw new Error("[ERROR] This is an error.", error);
-      }
+      MissionUtils.Console.print("결과 : " + result);
+    } catch (error) {
+      throw new Error("[ERROR] This is an error.", error);
     }
   }
 }
