a8f43f36fae7fe8b6aacbac7e197fdd2e49af80d
Junseo Kim <semiwest2@gmail.com>
2024-10-21 21:28:33 +0900

    refactor: splitByDelimiters 함수에서 기본 구분자를 상수에서 가져오도록 수정

--- src/parser/splitByDelimiters.js
+++ src/parser/splitByDelimiters.js
@@ -1,7 +1,9 @@
+import { DEFAULT_DELIMITERS } from '../constant/constant.js';
+
 // 문자열을 구분자에 따라 나누고, 숫자 배열로 변환하는 함수
 
 export default function splitByDelimiters(cleanedString, customDelimiter) {
-  const delimeters = [",", ";", customDelimiter].filter(Boolean);
+  const delimeters = [...DEFAULT_DELIMITERS, customDelimiter].filter(Boolean);
   const regex = new RegExp(`[${delimeters.join("")}]`);
   const parsedNumberList = cleanedString.split(regex).map(Number);
   return parsedNumberList;
