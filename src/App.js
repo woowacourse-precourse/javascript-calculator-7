484ac91bf466efdb59fafd867971705d243d4aea
unknown <kevin123488@naver.com>
2024-10-21 17:56:52 +0900

    fix(code): 코드 주석 정리

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ function returnSum(inputText) {
   let TEMP_NUM = ''
   for (let i = 0; i < inputText.length; i++) {
     if (i === 0) {
-      // returnAns += parseInt(inputText[i])
       TEMP_NUM += inputText[i]
     } else if (inputText[i] === ':' || inputText[i] === ',') {
       returnAns += parseInt(TEMP_NUM)
@@ -56,7 +55,6 @@ function checkInput(inputText) {
     try {
       for (let i = 0; i < inputText.length; i++) {
         if (inputText[i] === '\\' && inputText[i + 1] === 'n') {
-        // if (inputText[i] === '\n') {
           // \n의 위치를 파악했다면 N_START_IDX에 넣자
           // 이 값을 바탕으로 커스텀 구분자를 확인할 수 있다
           N_START_IDX = i
@@ -71,17 +69,11 @@ function checkInput(inputText) {
       // 구분자 파악해서 계산 가능한 입력값을 받았는지 확인
       const SAPARATOR = inputText.slice(2, N_START_IDX)
       const NUMBER_PART = inputText.slice(N_START_IDX + 2)
-      // console.log(SAPARATOR)
-      // console.log(NUMBER_PART)
       // 구분자 메타문자 변경
       const ES_SAPARATOR = SAPARATOR.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
       // 숫자가 구분자에 의해 올바르게 구분되고 있는지 확인. 동적으로 사용해야 하므로 new RegExp 사용해 객체 생성
       const CUSTOM_PATTERN = new RegExp(`^\\d+(${ES_SAPARATOR}\\d+)*$`)
-      // return CUSTOM_PATTERN.test(NUMBER_PART)
-      // console.log(ES_SAPARATOR)
-      // console.log(CUSTOM_PATTERN)
       if (CUSTOM_PATTERN.test(NUMBER_PART)) {
-        // console.log(SAPARATOR, NUMBER_PART)
         return [SAPARATOR, NUMBER_PART]
       } else {
         return false
@@ -93,7 +85,6 @@ function checkInput(inputText) {
 
   // 3. 커스텀 입력값이 아닐 경우 유효성 검사
   const CHECK_NON_CUSTOM = /^\d+([,:]\d+)*$/;
-  // return CHECK_NON_CUSTOM.test(inputText);
   if (CHECK_NON_CUSTOM.test(inputText)) {
     return 'non custom pattern'
   } else {
@@ -105,20 +96,17 @@ class App {
   async run() {
     // 입력 기능 구현
     // 1. 덧셈할 문자열을 입력해 주세요. 문구를 띄워주자
-    // MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.")
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.")
     const STRING_INPUT = await MissionUtils.Console.readLineAsync('')
 
     // 입력 형식에 맞지 않을 경우 ERROR 출력 후 종료
     // 1. STRING_INPUT 값을 확인하고, error 여부를 판단하여 throw
     // 2. index.js 파일의 try catch문 작동
     const CHECK_INPUT = checkInput(STRING_INPUT)
-    // console.log(CHECK_INPUT)
     if (!CHECK_INPUT) {
       throw new Error("[ERROR]")
     } else {
-      // 입력 형식이 올바를 경우 returnSum 함수를 통해 STRING_INPUT에 대한 결과를 저장
-      // return RETURN_ANS
-      // if (CHECK_INPUT === 'answer zero') {
+      // 입력 형식이 올바를 경우 Sum 함수를 통해 STRING_INPUT에 대한 결과를 저장
       let result;
       if (STRING_INPUT.length === 0) {
         result = 0
