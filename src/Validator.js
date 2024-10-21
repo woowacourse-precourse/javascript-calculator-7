a581db3bde0e6c7f595c5f2772cc6cf06d751ef4
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-21 15:48:45 +0900

    feat: 숫자 형식 검증 구현
    공백이 있지는 않은지, 소수점이 있지는 않은지 검사

--- src/Validator.js
+++ src/Validator.js
@@ -1,5 +1,6 @@
 class Validator {
   #input;
+  #numbers;
   #delimiter;
 
   /**
@@ -11,6 +12,8 @@ class Validator {
     this.#delimiter = ',|:';
 
     this.#extractCustomDelimiter();
+    this.#parseInput();
+    this.#validate();
   }
 
   // 커스텀 구분자 추출
@@ -32,6 +35,26 @@ class Validator {
   // 특수문자 이스케이프 처리
   #escaperRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
+  // 입력값을 구분자로 나눠서 배열에 저장
+  #parseInput() {
+    const regex = new RegExp(`[${this.#delimiter}]`);
+    this.#numbers = this.#input.split(regex);
+  }
+
+  // 구분자로 나눈 값에 대하여 전부 검증
+  #validate() {
+    for (const num of this.#numbers) {
+      this.#validateNumberFormat(num);
+    }
+  }
+
+  // 숫자 형식 검증
+  #validateNumberFormat(num) {
+    if (!/^\d+$/.test(num)) {
+      throw new Error(`[ERROR] '${num}'은(는) 유효한 숫자 형식이 아닙니다.`);
+    }
   }
 }
 
