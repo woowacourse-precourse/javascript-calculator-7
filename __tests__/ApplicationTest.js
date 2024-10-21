91823e71e2f1fc5fe99abfb24c1e12d2cfa6b974
chickenisamazing <dieswing@naver.com>
2024-10-21 23:05:54 +0900

    test(test): add some test case

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,8 +32,56 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("커스텀 구분자가 슬래시인 경우에는?", async () => {
+    const inputs = ["///\\n353"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 353"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자가 숫자인 경우에는?", async () => {
+    const inputs = ["//3\\n2365"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 67"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("음수인 정수값 포함시 테스트", async () => {
     const inputs = ["-1,2,3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("적절하지 않은 형태의 문자열 입력시", async () => {
+    const inputs = ["11@2,325A"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자를 지정했지만 기본 구분자를 사용할 경우", async () => {
+    const inputs = ["//3\n:5"];
     mockQuestions(inputs);
 
     const app = new App();
