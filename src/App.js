ff55a4d642c65720b0f6e9124b776b198d9680df
AnCha <tory912@gmail.com>
2024-10-21 22:06:58 +0900

    feat: 빈값 입력에 대해 예외처리

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    let numbers = [];
+
     const userInput = await Console.readLineAsync(
       "덧셈할 문자열을 입력하여 주세요.\n"
     );
 
     if (userInput.startsWith("//") && userInput.indexOf("\\n") !== -1) {
-      const NUMBERS = userInput.substr(5).split(userInput[2]);
+      numbers = userInput.substr(5).split(userInput[2]);
     } else {
-      const NUMBERS = userInput.split(/,|:/);
+      numbers = userInput.split(/,|:/);
+    }
+    Console.print(numbers);
+
+    if (numbers.includes("")) {
+      throw new Error("[ERROR]빈 값이 입력되었습니다.");
     }
   }
 }
