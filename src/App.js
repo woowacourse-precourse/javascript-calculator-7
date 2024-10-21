a429b91f65581ab8b320bcac9c78c62a5ccbb198
xxxjinn <tlsguswls319@naver.com>
2024-10-21 22:30:17 +0900

    feat(App.js): 빈 문자열의 경우 0 출력하기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ class App {
   async run() {
     let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
+    let answer = 999999;
+
+    if (input === "") {
+      answer = 0;
+    } 
+
+    Console.print(`결과 : ${answer}`);
   }
 }
 
