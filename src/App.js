2f5de9287a2aecd3b4890760c0696f8502ddf618
Heesoo Jun <acesk123@gmail.com>
2024-10-21 22:22:09 +0900

    Refactor: 커스텀 구분자 함수 구조 변경

--- src/App.js
+++ src/App.js
@@ -3,45 +3,46 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     try {
-      let inputNumbers;
-
       const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요\n");
-
-      if (input.length <= 1 && !isNaN(input)) {
-        this.printResult(Number(input));
-        return;
-      }
-
-      const customSeparator = input.match(/\/\/(.)\n/);
-
-      if (customSeparator) {
-        inputNumbers = input.split('\n')[1].split(customSeparator[1]); // 구분자 기준으로 분리
-      } else if (input.match(/[,|:]/)) {
-        inputNumbers = input.split(/[,|:]/);
-      } else {
-        throw new Error("[ERROR]");
-      }
-
-      inputNumbers = inputNumbers.map((num) => {
-        const number = Number(num);
-        if (isNaN(number)) {
-          throw new Error("[ERROR]");
-        }
-        if (number < 0) {
-          throw new Error("[ERROR]");
-        }
-        return number;
-      });
-
-      const sum = inputNumbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue;
-      });
-
+      const inputNumbers = this.splitInput(input);
+      const sum = this.sumNumbers(inputNumbers);
       this.printResult(sum);
     } catch (error) {
       Console.print(error.message);
       throw error;
     }
+  }
+
+  splitInput(input) {
+    let inputNumbers;
+
+    if (input.length <= 1 && !isNaN(input)) {
+      return [Number(input)];
+    }
+
+    const customSeparator = input.match(/\/\/(.)\\n/);
+
+    if (customSeparator) {
+      inputNumbers = input.split(customSeparator[1]);
+      inputNumbers.splice(0, 1);
+      inputNumbers[0] = inputNumbers[0].substring(2);
+    } else if (input.match(/[,|:]/)) {
+      inputNumbers = input.split(/[,|:]/);
+    } else {
+      throw new Error("[ERROR]");
+    }
+
+    return inputNumbers.map((value) => {
+      const number = Number(value);
+      if (isNaN(number) || number < 0) {
+        throw new Error("[ERROR]");
+      }
+      return number;
+    });
+  }
+
+  sumNumbers(inputNumbers) {
+    return inputNumbers.reduce((sum, currentNumber) => sum + currentNumber, 0);
   }
 
   printResult(result) {
