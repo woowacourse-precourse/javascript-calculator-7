b035134c7586adaa6559493a8f56cf91885600d2
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-21 16:03:07 +0900

    feat(App): 구분자 목록 생성 및 커스텀 구분자 처리

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    let input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
-
+    const separator = [',', ':'];
+  
+    while(input.match(/^\/\//g)) {
+      if(input.match(/\/\/(.)\\n/)) {
+        const match = input.match(/\/\/(.)\\n/);
+        const customSep = match[1];
+        separator.push(customSep);
+        input = input.substr(match[0].length);
+      } else {
+        // Error 처리
+      }
+    }
   }
 }
 
