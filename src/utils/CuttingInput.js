51f47aa996f00a24cb52cc7e8ef10fbeee174935
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-21 21:53:05 +0900

    fix(utils) : 알파벳 a만 통과되는 버그 수정

--- src/utils/CuttingInput.js
+++ src/utils/CuttingInput.js
@@ -1,3 +1,5 @@
+import Errors from "../constants/Errors.js";
+
 /**
  * 문자열을 구분자에 맞게 잘라 숫자 배열로 변환하는 함수
  * @param {string} input
@@ -11,7 +13,13 @@ const cuttingInput = (input, customDelimiter) => {
 
   const DELIMITER_REGEX = new RegExp(`[,:${customDelimiter}]`);
 
-  return PROCESSED_INPUT.split(DELIMITER_REGEX).map(Number);
+  return PROCESSED_INPUT.split(DELIMITER_REGEX).map((item) => {
+    const TRIMMED_ITEM = item.trim();
+    if (!/^-?\d+$/.test(TRIMMED_ITEM)) {
+      throw new Error(Errors.NOT_NUMBER);
+    }
+    return Number(TRIMMED_ITEM);
+  });
 };
 
 export default cuttingInput;
