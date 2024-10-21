eaca70e553c05e0ad54c3bd10010372e94cddeb4
SudoSzune <robin1415@kookmin.ac.kr>
2024-10-21 23:05:43 +0900

    feat(ManageError): 커스텀 구분자를 두 글자 이상 사용하거나, //로 시작해 \n으로 끝나지 않는 문자열의 경우에 예외를 반환한다. 10진수로 변환했을 때 숫자가 아닌경우에 예외를 반환한다

--- src/App.js
+++ src/App.js
@@ -31,11 +31,13 @@ class App {
             // 정규 표현식 수정
             const customDelimiterMatch = input.match(/^\/\/(.)\\n/); // //로 시작해야하고, 줄바꿈 문자 전의 커스텀 문자를 인식함
 
+            if (!customDelimiterMatch) {
+                throw new Error('[ERROR] 잘못된 입력 형식입니다.');
+            }
+
             // 커스텀 구분자 추출
             const customDelimiter = customDelimiterMatch[1];
-
-
-            delimiter = new RegExp(`[${customDelimiter}]`, 'g'); // 커스텀 구분자를 정규식으로 변환.
+            delimiter = new RegExp(`[${customDelimiter}]`, 'g'); // 커스텀 구분자를 정규식으로 변환. 문자열 내 모든 패턴 검색
 
             // 구분자 이후의 숫자 문자열로 변경
             input = input.slice(customDelimiterMatch[0].length); // 커스텀 구분자 제거
@@ -44,6 +46,9 @@ class App {
         // 모든 구분자로 숫자 분리
         const numbers = input.split(delimiter).map((num) => {
             const parsedNum = parseInt(num, 10);
+            if (isNaN(parsedNum) || parsedNum < 0) {
+                throw new Error('[ERROR] 유효하지 않은 숫자 입력입니다.');
+            }
             return parsedNum;
         });
 
