9360c0501985ef9765f1288cf40f8f08bc1af941
(hoooooony) <(cjh41820@gmail.com)>
2024-10-21 16:42:52 +0900

    feat (계산기) : 커스텀 구분자 구분자에 추가하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const CUSTOM_SEPARATOR_PATTERN = /^\/\/(.)\\n/;
+
 class App {
+  constructor() {
+    this.separators = [",", ":"];
+  }
+
   async run() {
     try {
       const input = await this.userInput();
-
+      this.checkSeparator(input);
     } catch (error) {
       throw error;
     }
@@ -13,6 +19,18 @@ class App {
   userInput() {
     return Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
   }
+
+  checkSeparator(input) {
+    const match = input.match(CUSTOM_SEPARATOR_PATTERN);
+    if (match) {
+      this.separators.push(match[1]);
+      const regex = new RegExp(`[${this.separators}]`);
+      if (regex.test(input) && input.match(regex).some((el) => el === "")) {
+        throw new Error("[ERROR] 연속된 구분자는 허용되지 않습니다.");
+      }
+    }
+  }
+
 }
 
 export default App;
