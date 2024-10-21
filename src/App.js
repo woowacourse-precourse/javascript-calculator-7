8c7877929fd27e71e28184ca01c5d982341e71dc
EUNSEMIN <dmstpals104@ajou.ac.kr>
2024-10-21 11:44:07 +0900

    feat: Handle empty string or null input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 class App {
-  async run() {}
+    // run: main
+    async run() {
+      try {
+        Console.print("덧셈할 문자열을 입력해 주세요.\n");
+        const input = await Console.readLineAsync();
+  
+        if (!input.trim()) {
+          throw new Error("[ERROR] 입력값 오류");
+        }
+  
+        const result = this.calculate(input.trim());
+        Console.print(`결과 : ${result}`);
+      } catch (error) {
+        Console.print(error.message);
+        throw error; 
+      }
+    }
+  
 }
 
 export default App;
