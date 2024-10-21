b9967caa5324edab501906e765a59c1d5b48e95e
luminox93 <memoji1236@gmail.com>
2024-10-21 14:50:00 +0900

    fix: isValidCustomSeparatorDeclaration.js
    1.버그 수정
    2. 관심사 분리
    3. 보조 함수 로직 수정

--- src/models/validations/isValidCustomSeparatorDeclaration.js
+++ src/models/validations/isValidCustomSeparatorDeclaration.js
@@ -1,39 +1,54 @@
-// isValidCustomSeparatorDeclaration.js
-
-import { PATTERN, ERROR_MESSAGES } from '../../constants/contants.js';
+import { PATTERN, ERROR_MESSAGES } from '../../constants/constants.js';
+import { extractCustomSeparator } from '../../utils/extractCustomSeparator.js';
 
 export const isValidCustomSeparatorDeclaration = (input) => {
-	const matches = captureCustomSeparators(input);
-	if (hasMultipleSeparators(matches)) {
+	if (!isValidInput(input)) {
 		throw new Error(ERROR_MESSAGES.INVALID_INPUT);
 	}
-	if (isIncompleteDeclaration(input, matches)) {
+	const matches = captureCustomSeparators(input);
+
+	validateMatches(matches, input);
+	return true;
+};
+
+const validateMatches = (matches, input) => {
+	if (hasMultipleDeclarations(matches)) {
 		throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+	}
+	if (matches.length > 0 && !isDeclarationComplete(input)) {
+		throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+	}
+};
+const isValidInput = (input) => {
+	return input && typeof input === 'string';
+};
+
+const captureCustomSeparators = (input) => {
+	const matches = [...input.matchAll(PATTERN)];
+	return matches.map((match) => extractCustomSeparator(match[1]));
+};
+
+const hasMultipleDeclarations = (matches) => {
+	return matches && matches.length > 1;
+};
+
+const isDeclarationComplete = (input) => {
+	const lines = input.split('\n');
+	let declarationEnded = false;
+
+	return lines.every((line) => {
+		if (isDeclarationLine(line)) {
+			return handleDeclarationLine(declarationEnded);
+		}
+		return true;
+	});
+};
+
+const handleDeclarationLine = (declarationEnded) => {
+	if (declarationEnded) {
+		return false;
 	}
 	return true;
 };
 
-const captureCustomSeparators = (input) => {
-	const regex = new RegExp(PATTERN, 'g');
-	const matches = input.match(regex);
-	if (!matches) return [];
-	return matches.map((match) => {
-		const separatorMatch = match.match(new RegExp(PATTERN));
-		return separatorMatch[1];
-	});
-};
-
-const hasMultipleSeparators = (matches) => {
-	return matches && matches.length > 1;
-};
-
-const isIncompleteDeclaration = (input, matches) => {
-	const hasNoMatches = !matches || matches.length === 0;
-	return (
-		hasNoMatches && input.startsWith('//') && !isDeclarationComplete(input)
-	);
-};
-
-const isDeclarationComplete = (input) => {
-	return PATTERN.test(input);
-};
+const isDeclarationLine = (line) => /^\/\//.test(line);
