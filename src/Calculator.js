83d733e3b0cc9af9b117ced41de179642a4d78c4
yeji0214 <1225yj50@naver.com>
2024-10-21 15:37:57 +0900

    refactor: avoid global state by passing 'delimiters' as a parameter

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,17 +2,15 @@ import { Console } from '@woowacourse/mission-utils';
 import { Validator } from './Validator.js'; // 사용자 입력 유효성 검사
 import { INPUT_PROMPTS, DEFAULT_DELIMITERS, DELIMITER_CONSTANTS } from './constants.js';
 
-// 구분자 저장 리스트
-const delimiters = [...DEFAULT_DELIMITERS];
-
 // 덧셈 계산기 실행 메서드
 export const runCalculator = async () => {
+    const delimiters = [...DEFAULT_DELIMITERS]; // 구분자 저장 리스트
     const userInput = await getUserInput(); // 계산식 입력 받기
 
     try {
         Validator.validateInput(userInput);
-        const definedUserInput = defineDelimiters(userInput); // 구분자 정의
-        const splitedString = splitStringByDelimiters(definedUserInput); // 구분자 기준으로 분리
+        const definedUserInput = defineDelimiters(userInput, delimiters); // 구분자 정의
+        const splitedString = splitStringByDelimiters(definedUserInput, delimiters); // 구분자 기준으로 분리
         Validator.validateNumbers(splitedString);
         const numbers = convertToNumberList(splitedString);
         Console.print(`결과 : ${calculateSum(numbers)}`);
@@ -29,7 +27,7 @@ const getUserInput = async () => {
 }
 
 // 구분자 정의 메서드
-const defineDelimiters = (userInput) => {
+const defineDelimiters = (userInput, delimiters) => {
     if (userInput.slice(0, 2) === DELIMITER_CONSTANTS.CUSTOM_DELIMITER_START && userInput.slice(3, 5) === DELIMITER_CONSTANTS.CUSTOM_DELIMITER_END) {
         Validator.validateCorrectlyDeclaredDelimiters(userInput);
         delimiters.push(userInput[2]);
@@ -43,7 +41,7 @@ const defineDelimiters = (userInput) => {
 }
 
 // 문자열 분리 메서드 (구분자를 기준으로)
-const splitStringByDelimiters = (userInput) => {
+const splitStringByDelimiters = (userInput, delimiters) => {
     const regex = new RegExp(`[${delimiters.join('')}]`, 'g');
     const splitedString = userInput.split(regex);
 
