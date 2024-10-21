212ea70bcdc481dc1cc1bcf58648cdcbba45e4b3
Yerim1234 <kongsun0805@gmail.com>
2024-10-21 14:22:10 +0900

    feat: 기본 입력 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const STRING_INPUT = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요."
+    );
+
+    if (STRING_INPUT.trim() === "") {
+      Console.print("결과 : 0");
+      return;
+    }
+
+    MissionUtils.Console.print("결과 : " + STRING_INPUT);
+  }
 }
 
 export default App;
