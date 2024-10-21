4788dcd90c69704df4c802448d02b4d7c51934fe
김민주 <kminju2000@naver.com>
2024-10-21 15:23:34 +0900

    feat: 커스텀 구분자가 있을 때 문자열 분리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { parse } from "@babel/core";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
     try {
       // 입력을 받는다.
-      const input = await MissionUtils.Console.readLineAsync(
+      const INPUT = await MissionUtils.Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n"
       );
-      if (this.isValidInput(input)) {
-        this.plus();
+      if (this.isValidInput(INPUT)) {
+        const RESULT_ARR = this.separateInput(INPUT);
+        // this.add(RESULT_ARR);
       } else {
         MissionUtils.Console.print("[ERROR] 유효한 입력값이 아닙니다.");
       }
@@ -18,15 +20,39 @@ class App {
   }
 
   // 유효한 입력값인지 확인하는 함수
-  isValidInput(input) {
-    if (input.length == 0) {
+  isValidInput(INPUT) {
+    if (INPUT.length == 0) {
       return false;
     }
     return true;
   }
 
-  // 숫자의 합을 계산하는 함수
-  plus() {}
+  separateInput(INPUT) {
+    // 커스텀 구분자로 시작하는 경우
+    if (INPUT.startsWith("/")) {
+      const [LETTER, NUMBER] = INPUT.split(/\\n/);
+
+      if (!LETTER.startsWith("//") || !NUMBER) {
+        throw Error("[ERROR]");
+      }
+
+      const SEPARATOR = LETTER.substring(2);
+      const RESULT = NUMBER.split(SEPARATOR);
+
+      return RESULT.map((item) => {
+        const NUM = Number(item);
+        if (isNaN(NUM)) {
+          throw Error("[ERROR] 구분자를 제외하고 숫자로 입력해주세요.");
+        }
+        if (NUM < 0) {
+          throw Error("[ERROR] 양수를 입력하세요.");
+        }
+        return NUM;
+      });
+    }
+  }
+
+  // add(RESULT_ARR) {}
 }
 
 export default App;
