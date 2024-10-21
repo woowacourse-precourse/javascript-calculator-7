a8f1d07ce85f473c3de225cc969d22a5d9a00399
DongHyeonWon <dhe7700@naver.com>
2024-10-21 14:48:36 +0900

    refac: 결과 출력 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,3 +1,4 @@
 export const CONSOLE_MESSAGES = {
   GET_STRING: "덧셈할 문자열을 입력해 주세요.\n",
+  GET_RESULT: (result) => `결과 : ${result}`,
 };
--- src/view/output.js
+++ src/view/output.js
@@ -1,8 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
+import { CONSOLE_MESSAGES } from "../constants/messages.js";
 
 const output = {
   result: (result) => {
-    Console.print(`결과 : ${result}`);
+    Console.print(CONSOLE_MESSAGES.GET_RESULT(result));
   },
 };
 
