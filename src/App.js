0ef64834052ec5942cadd7480e266a1bc2889225
yoouung <jyp101400@gmail.com>
2024-10-21 21:32:58 +0900

    feat: 커스텀 구분자 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
+  delimiter = [",", ":"];
+
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.\n");
     const result = this.processInput(input);
@@ -9,13 +11,28 @@ class App {
   }
 
   processInput(input) {
+    if (this.hasCustomDelimiter(input)) {
+      input = input.slice(5);
+    }
     const numbers = this.extractNumbers(input);
     const sum = this.calculateSum(numbers);
     return sum;
   }
 
+  hasCustomDelimiter(input) {
+    if (input.startsWith("//") && input.slice(3, 5) === "\\n") {
+      const customDelimiter = input[2];
+      this.delimiter.push(customDelimiter);
+
+      return true;
+    }
+    return false;
+  }
+
   extractNumbers(input) {
-    const numbers = input.split(/[, :]/).map((str) => parseInt(str, 10));
+    const regex = new RegExp(`[${this.delimiter.join(" ")}]`);
+    const numbers = input.split(regex).map((str) => parseInt(str, 10));
+
     return numbers;
   }
 
