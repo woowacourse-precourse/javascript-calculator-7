40b2259a2765ce7d2e517144907332022bd30a60
yewonhihi <yewon1118@naver.com>
2024-10-21 22:56:51 +0900

    feat(App.js): 올바르지 않은 입력값 및 음수 예외 처리

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    let delimiters = [',', ':'];
+    let delimiters = [",", ":"];
     let expression = input.trim();
 
-    if (input.startsWith('//') && input.includes("\n")) {
+    if (input.startsWith("//") && input.includes("\n")) {
       const { customDelimiter, newExpression } = this.checkCustomDelimiter(input);
       delimiters.push(customDelimiter);
       expression = newExpression;
@@ -22,6 +22,16 @@ class App {
   calculateSum(expression, delimiters) {
     const regExpOfDelimiter = new RegExp(delimiters.join('|'));
     const numbers = expression.split(regExpOfDelimiter);
+    let sum = 0;
+    
+    for (const number of numbers) {
+      if (isNaN(number)) {
+        this.throwError(`올바르지 않은 입력값이 있습니다. (${number})`);
+      } else if (Number(number) <= 0) {
+        this.throwError(`덧셈값은 양수만 입력할 수 있습니다. (${number})`);
+        
+      }
+    }
   }
 
   throwError(message) {
