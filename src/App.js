73ce46185bb9380df1db03d00d476f96a5559cab
AnCha <tory912@gmail.com>
2024-10-21 21:15:29 +0900

    refactor: 사용자 입력 `\n` 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const userInput = await Console.readLineAsync(
-      "덧셈할 문자열을 입력하여 주세요."
+      "덧셈할 문자열을 입력하여 주세요.\n"
     );
 
     if (userInput.startsWith("//") && userInput.indexOf("\\n") !== -1) {
