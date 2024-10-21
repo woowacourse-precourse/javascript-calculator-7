2e8ccf3abbe8e69931f7889bdb793c15e66513d7
deokisys <sjb378@me.com>
2024-10-21 22:57:30 +0900

    [test] 구분자입력 관련 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -77,6 +77,48 @@ describe("문자열 계산기", () => {
     });
   });
 
+  test("커스텀 구분자 사용 공백사용", async () => {
+    const inputs = ["// \\n1 2 3 4"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("예외 커스텀 구분자 잘못된 사용 (/를 사용)", async () => {
+    const inputs = ["/;\\n1"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 커스텀 구분자 잘못된 사용 (//가 없음)", async () => {
+    const inputs = [";\\n1"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 커스텀 구분자 잘못된 사용 (\\n가 없음)", async () => {
+    const inputs = ["//;1"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
   test("커스텀 구분자 사용 두글자이상", async () => {
     const inputs = ["//abc\\n1abc2abc15abc2"];
     mockQuestions(inputs);
@@ -118,6 +160,15 @@ describe("문자열 계산기", () => {
 
   test("예외 테스트 등록 안된 구분자", async () => {
     const inputs = ["1,2-3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트 등록 안된 구분자(공백)", async () => {
+    const inputs = ["1,2 3"];
     mockQuestions(inputs);
 
     const app = new App();
