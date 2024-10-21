85a2ed7bac561897381c41f89e25052406586974
thisisyejin <yejink321@gmail.com>
2024-10-21 09:47:33 +0900

    feat(app): custom separator
    To add a function to specify a custom separator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() { /* 프로그램의 시작점 */
+    MissionUtils.Console.print('덧셈할 문자열을 입력해주세요.')
+    const input = await MissionUtils.Console.readLineAsync("")
+
+    let number = '0'; // 숫자를 인식할 변수
+    let sum = 0;      // 덧셈을 계산할 변수
+    
+    try {
+      // 커스텀 구분자 지정
+      let customseparator = true;
+      if ( input.startsWith('//') ) {
+        if ( input.slice(3, 5) == '\\n' ) {
+          customseparator = input[2]
+        } else {
+          throw Error('[ERROR] 커스텀 구분자 지정에 실패했습니다.')
+        }
+      }
+    } catch (err) {
+      console.error(err.message)
+      throw err
+    }
+  }
 }
 
 export default App;
