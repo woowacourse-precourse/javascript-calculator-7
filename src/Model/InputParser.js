42453991128986c7accf6140df40d6524e4fb8d5
anonymousrecords <a.Records2022@gmail.com>
2024-10-21 22:21:19 +0900

    feat: InputParser 구현

---
+++ src/Model/InputParser.js
@@ -0,0 +1,60 @@
+import { ERROR_MESSAGES } from "../constants.js";
+
+class InputParser {
+  parse(input) {
+    if (input === "") {
+      return [0];
+    }
+
+    const { divider, numberString } = this.parseInput(input);
+    const numberArray = this.createNumberArray(numberString, divider);
+    return this.validateNumber(numberArray);
+  }
+
+  parseInput(input) {
+    if (input.startsWith("//")) {
+      return this.parseCustomDivider(input);
+    }
+
+    const defaultDividers = [",", ":"];
+    if (defaultDividers.some((div) => input.includes(div))) {
+      return { divider: defaultDividers, numberString: input };
+    }
+
+    throw new Error(ERROR_MESSAGES.NO_VALID_DIVIDER);
+  }
+
+  parseCustomDivider(input) {
+    const endIndex = input.indexOf("\\n");
+    const customDivider = input.slice(2, endIndex);
+    const numberString = input.slice(endIndex + 2);
+
+    if (numberString.length === 0) {
+      throw new Error(ERROR_MESSAGES.NO_NUMBERS_INPUT);
+    }
+
+    return { divider: [customDivider], numberString };
+  }
+
+  createNumberArray(numberString, divider) {
+    divider.forEach((divider) => {
+      numberString = numberString.split(divider).join(",");
+    });
+
+    return numberString.split(",").map(Number);
+  }
+
+  validateNumber(numberArray) {
+    if (numberArray.some(isNaN)) {
+      throw new Error(ERROR_MESSAGES.INVALID_INPUT);
+    }
+
+    if (numberArray.some((num) => num < 0)) {
+      throw new Error(ERROR_MESSAGES.NEGATIVE_NUMBER);
+    }
+
+    return numberArray;
+  }
+}
+
+export default InputParser;
