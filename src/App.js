abaab4e4235c95a5fd056457f133ee85527310ca
sieon <sieon.eve@gmail.com>
2024-10-21 23:06:08 +0900

    Feat: input과 output 그리고 error 구조 설정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    try {
+
+      // 문자열을 입력받는다.
+      const input = await Console.readLineAsync("계산할 문자열을 입력해 주세요: ");
+
+      const result = this.calculate(input);
+      Console.print(`결과: ${result}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+
   }
+ 
 }
+
 
 export default App;
