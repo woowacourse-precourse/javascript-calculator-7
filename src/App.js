0776b50ab1b388d1ff8dfbadad729036d2ed1e96
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-10-21 23:05:55 +0900

    feat: 문자열 검증 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
+    
     // 문자열 입력 받음
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
+
+    // 문자열 입력 검증
+    if (this.validateDefaultDelimiters(input)) {
+      // 기본 구분자라면, 쉼표와 콜론으로 숫자 분리
+      const numbers = input.split(/[,:]/).map(Number)
+    } else {
+      // 커스텀 구분자가 있다면, 해당 구분자로 숫자 분리
+      const numbers = this.extractCustomDelimitersNumber(input)
+    }
+    
+  }
+
+  extractCustomDelimitersNumber(input) {
+    
+  }
+
+  validateDefaultDelimiters(input) {
+
   }
 }
 
