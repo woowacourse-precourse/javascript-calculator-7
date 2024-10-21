741cdb8514f53624db9c2ce598e1ef744d4b8916
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-10-21 23:22:55 +0900

    feat: 커스텀 구분자 형식을 검증하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    
+
     // 문자열 입력 받음
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n")
 
@@ -17,8 +17,41 @@ class App {
     
   }
 
+  // 커스텀 구분자를 바탕으로 숫자를 분리해주는 함수
   extractCustomDelimitersNumber(input) {
+
+    // 커스텀 구분자 형식 검사
+    const customDelimiterPattern = /^\/\/(.)\\n(.+)/;
+    const match = input.match(customDelimiterPattern);
+    Console.print(match)
+
+    if (match) {
+      
+      // 커스텀 구분자 형식을 갖춘 경우
+      const tokens = input.slice(2).split("\\n")
+      const delimiter = new RegExp(tokens[0]) // 구분자를 정규식으로 변환
+      const numberWithDelimiter = tokens[1] // 나머지 부분은 구분자를 포함한 숫자로 변환
+
+      if (this.validateCustomDelimiters(numberWithDelimiter, delimiter)) {
+
+        // 커스텀 구분자만 포함하고 있는 경우
+        return input.split(delimiter).map(Number)
+
+      } else {
+
+        throw new Error("[ERROR] 커스텀 구분자 이외의 구분자가 발견되었습니다.")
+
+      }
+      
+    } else {
+      
+      throw new Error("[ERROR] 커스텀 구분자 형식을 지키지 않았습니다.")
     
+    }
+  }
+
+  validateCustomDelimiters(input, delimiter) {
+
   }
 
   validateDefaultDelimiters(input) {
