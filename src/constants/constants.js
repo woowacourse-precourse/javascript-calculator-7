e6650ac2a7836a068e3dc8489df57f074973f82a
leelsj2007 <leelsj2007@gmail.com>
2024-10-21 17:23:36 +0900

    feat : 입출력 상수 작성

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.\n"
+export const OUTPUT_MESSAGE_TEMPLATE = "결과 : ";
+
+export const OUTPUT_MESSAGE = (result) => `${OUTPUT_MESSAGE_TEMPLATE}${result}`;
