c8de1a81470b652d1973e618cb96546cc63d3548
ChanLee_KR <cl700120@gmail.com>
2024-10-23 11:30:27 +0900

    refactor: 인풋 가이드 메시지 상수 이동

--- src/constant/input.js
+++
@@ -1,1 +0,0 @@
-export const INPUT_GUIDE_MESSAGE = '덧셈할 문자열을 입력해 주세요.';
--- src/view/Input.js
+++ src/view/Input.js
@@ -1,14 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
-import { INPUT_GUIDE_MESSAGE } from '../constant/input.js';
 import { ERROR_MESSAGE } from '../constant/error.js';
 
 class Input {
+  INPUT_GUIDE_MESSAGE = '덧셈할 문자열을 입력해 주세요.';
+
   /**
    * @returns {Promise<string>}
    */
   static async readLine() {
     try {
-      return await Console.readLineAsync(INPUT_GUIDE_MESSAGE + '\n');
+      return await Console.readLineAsync(this.INPUT_GUIDE_MESSAGE + '\n');
     } catch {
       throw new Error(ERROR_MESSAGE.FAILED_READ_LINE);
     }
