56d1f7bfedc337413dcee57102318cd7a00ba093
chan8632 <duscks8632@gmail.com>
2024-12-16 15:55:24 +0900

    처음부터 다시 시작(24/12/16)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-
 class App {
-  async run() {
-    MissionUtils.Console.print("덧셈을 입력하세요");
-    let divider = [":", ","];
-    let resultNumber = [];
-    let userInput = await MissionUtils.Console.readLineAsync("");
-    if (userInput.startsWith("//")) {
-      const parts = userInput.split("\\n");
-      const customDivider = parts[0].substring(2);
-      divider.push(customDivider);
-      userInput = parts[1];
-    }
-    const dividerPattern = new RegExp(`[${divider.join("")}]`);
-    const numberList = userInput.split(dividerPattern);
-    numberList.forEach((num) => {
-      if (num) {
-        const value = parseInt(num);
-        if (isNaN(value)) {
-          throw new Error("[ERROR] 잘못된 입력입니다.");
-        }
-        if (value < 0) {
-          throw new Error("[ERROR] 음수는 허용되지 않습니다.");
-        }
-        resultNumber.push(value);
-      }
-    });
-    MissionUtils.Console.print(resultNumber);
-    const sum = resultNumber.reduce((sum, cur) => sum + cur, 0);
-    MissionUtils.Console.print(`결과 : ${sum}`);
-  }
+  async run() {}
 }
 
 export default App;
